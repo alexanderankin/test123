d4a884d0de22e10be85cac6fa7a3ddf1d47339d0
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2000-01-16 00:03:32 +0000

    This commit was manufactured by cvs2svn to create branch 'V1'.

---
+++ plugins/TextTools/branches/V1/TextToolsPlugin.java
@@ -0,0 +1,54 @@
+/*
+ * TextToolsPlugin.java - Plugin for a number of text related functions
+ * Copyright (C) 1999 mike dillon, Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+import java.util.Vector;
+import javax.swing.JMenu;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
+
+public class TextToolsPlugin extends EditPlugin
+{
+	public void start()
+	{
+		jEdit.addAction(new remove_trailing_ws());
+		jEdit.addAction(new sort_lines());
+		jEdit.addAction(new revsort_lines());
+		jEdit.addAction(new reverse());
+		jEdit.addAction(new rot13());
+		jEdit.addAction(new format());
+		jEdit.addAction(new to_lower());
+		jEdit.addAction(new to_upper());
+		jEdit.addAction(new insert_date());
+		jEdit.addAction(new transpose_chars());
+		jEdit.addAction(new select_word());
+	}
+
+	public void stop()
+	{
+	}
+
+	public void createMenuItems(View view, Vector menus, Vector menuItems)
+	{
+		JMenu myMenu = GUIUtilities.loadMenu(view, "text-tools");
+
+		menus.addElement(myMenu);
+	}
+}
