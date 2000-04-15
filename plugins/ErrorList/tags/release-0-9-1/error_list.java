312cf3fa9ed25145aa05d4dff8706798ffb780a4
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2000-04-15 04:08:29 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-9-1'.

---
+++ plugins/ErrorList/tags/release-0-9-1/error_list.java
@@ -0,0 +1,51 @@
+/*
+ * error_list.java
+ * Copyright (C) 1999 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import org.gjt.sp.jedit.*;
+
+public class error_list extends EditAction
+{
+	public error_list()
+	{
+		super("error-list");
+	}
+
+	public void actionPerformed(ActionEvent evt)
+	{
+		View view = getView(evt);
+		if(evt.getActionCommand() == null
+			&& ErrorListPlugin.isErrorListShowing(view))
+			ErrorListPlugin.getErrorList(view).close();
+		else
+			ErrorListPlugin.getErrorList(view);
+	}
+
+	public boolean isToggle()
+	{
+		return true;
+	}
+
+	public boolean isSelected(Component comp)
+	{
+		return ErrorListPlugin.isErrorListShowing(getView(comp));
+	}
+}
