0c289a2cbfd3335a27a3f1e340bc3fc3b8ff183c
sjakob <sjakob@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2000-05-08 04:45:52 +0000

    Abstracted template processing to new Template class. TemplateFile will now act merely as a proxy for a Template.

--- plugins/Templates/trunk/TemplateFile.java
+++ plugins/Templates/trunk/TemplateFile.java
@@ -23,9 +23,9 @@
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
-import gnu.regexp.*;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.util.Log;
+import gnu.regexp.*;
 /** 
  * A TemplateFile is similar to a java.io.File object as it acts as a reference
  * to a template file, but it also contains information describing the template 
@@ -35,114 +35,51 @@ public class TemplateFile implements ActionListener
 {
 	protected String label;
 	protected File templateFile;
-	private StringBuffer templateText;
-	private Hashtable vars = new Hashtable();
-	private TemplateVar lastVar = null;
-	private static RE ctpragmaFilter = null;
+	private Template myTemplate = null;
 	private static RE ctpragmaLabelFilter = null;
-	private static RE ctpragmaTest = null;
-	private org.gjt.sp.jedit.View view = null;
 
 	//Constructors
 	public TemplateFile(File templateFile) {
 		super();
 		this.templateFile = templateFile;
 		this.label = templateFile.getName();
+		createREs();
 		if (!this.isDirectory()) {
 			String s = null;
 			try {
-				s = readTemplateLabel(templateFile);
-			} catch (IOException e) {
+				s = readTemplateLabel(this.getBufferedReader());
+			} catch (Exception e) {
 				Log.log(Log.ERROR,this,jEdit.getProperty("plugin.TemplatesPlugin.error.template-label")
 							+ templateFile.getName());
+				Log.log(Log.ERROR,this,e);
 				// System.out.println("Error creating template label for file: " + f.getName());
 			}
 			if (s != null)
 				label = s;
 		}
-		createREs();
 	}
 
 	//Accessors & Mutators
 	public String getLabel() { return label; }
 	public void setLabel(String labelVal) { label = labelVal; }
-	public View getView() { return view; }
 
 	//Implementors
 	public boolean isDirectory() { return false; }
 	
 	public void actionPerformed(ActionEvent evt) {
-		view = EditAction.getView(evt);
-		view.getTextArea().setSelectedText(getTemplateText(this.templateFile));
-	}
-	
-	private String getTemplateText(File f) {
-		try {
-			BufferedReader in = null;
-			in = new BufferedReader(new FileReader(f));
-			templateText = new StringBuffer();
-			parsePragmas(in);
-			processTemplateFile();
-		} catch (FileNotFoundException fe) { }
-		catch (IOException ie) { }
-		return this.templateText.toString();
-	}
-	
-	/**
-	 * Scan the file for #ctpragma directives, and compile a list. 
-	 * All lines which are not #ctpragma directives are inserted into 
-	 * the templateText StringBuffer for later processing.
-	 * @param in The BufferedReader from which the template is read.
-	 */
-	private void parsePragmas(BufferedReader in) throws IOException{
-		String line;
-		while ((line = in.readLine()) != null)
-			if (containsCtpragma(line)) {
-				REMatch pragmaMatch = ctpragmaFilter.getMatch(line);
-				if (pragmaMatch != null) {
-					String pragmaType = pragmaMatch.toString(2);
-					String pragmaValue = pragmaMatch.toString(4);
-					processPragma(pragmaType.toUpperCase(), pragmaValue);
-				}
-			}
-			else {
-				templateText.append(line + "\n");
-			}
-	}
-	
-	private void processPragma(String type, String value) {
-		if (type.equals("VAR")) {
-			this.lastVar = new TemplateVar(value);
-			this.vars.put(value, this.lastVar);
-		} else if (type.equals("VAR_PROMPT") && this.lastVar != null) {
-			this.lastVar.setPrompt(value);
-		} else if (type.equals("VAR_DEFAULT") && this.lastVar != null) {
-			this.lastVar.setDefaultValue(value);
-		} else if (type.equals("VAR_HELP") && this.lastVar != null) {
-			this.lastVar.setHelpText(value);
+		if (myTemplate == null) {
+			myTemplate = new Template(this);
 		}
+		myTemplate.processTemplate(EditAction.getView(evt));
 	}
 	
-	public TemplateVar lookupVar(String key) {
-		return (TemplateVar)vars.get(key);
+	public BufferedReader getBufferedReader() throws FileNotFoundException {
+		return new BufferedReader(new FileReader(this.templateFile));
 	}
 	
-	/**
-	 * Process the template file, removing #ctpragma directives, and 
-	 * replacing substitution variables.
-	 */
-	private void processTemplateFile() {
-		Enumeration e = vars.elements();
-		while (e.hasMoreElements()) {
-			System.out.println(e.nextElement());
-		}
-	}
-	
-	private static String readTemplateLabel(File f) throws IOException{
+	private static String readTemplateLabel(BufferedReader in) throws IOException{
 		String templateLabel = null;
-		BufferedReader in = null;
 		try {
-			in = new BufferedReader(new FileReader(f));
 			String line;
 			if ((line = in.readLine()) != null) {
 				REMatch labelMatch = ctpragmaLabelFilter.getMatch(line);
@@ -162,32 +99,21 @@ public class TemplateFile implements ActionListener
 		return templateLabel;
 	}
 
-	/**
-	* Creates a RE to parse #ctpragma directives. Each directive is composed of 4 parts:<P>
-	* <LI> #ctpragma
-	* <LI> the directive type (eg. LABEL, NAME, etc.)
-	* <LI> an equals ("=") sign
-	* <LI> the value to assign for this directive type
-	*/
 	private static void createREs() {
 		try {
 			String exp = "(\\s*#ctpragma\\s*)(LABEL|NAME)(\\s*=\\s*)(\\S+.*)";
 			ctpragmaLabelFilter = new RE(exp,RE.REG_ICASE);
-			exp = "(\\s*#ctpragma\\s*)(VAR|VAR_PROMPT|VAR_DEFAULT|VAR_HELP)(\\s*=\\s*)(\\S+.*)";
-			ctpragmaFilter = new RE(exp,RE.REG_ICASE);
-			exp = "\\s*#ctpragma.*";
-			ctpragmaTest = new RE(exp,RE.REG_ICASE);
 		} catch (gnu.regexp.REException e) { }		// this shouldn't happen
-	}
-	
-	private static boolean containsCtpragma(String testString) {
-		return ctpragmaTest.isMatch(testString);
 	}
 	
 }
 	/*
 	 * Change Log:
 	 * $Log$
+	 * Revision 1.3  2000/05/08 04:45:52  sjakob
+	 * Abstracted template processing to new Template class.
+	 * TemplateFile will now act merely as a proxy for a Template.
+	 *
 	 * Revision 1.2  2000/05/02 03:22:51  sjakob
 	 * Added TemplateVar.java to template variable info.
 	 * Modified TemplateFile to parse new #ctpragma directives, and to create
