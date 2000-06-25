e596d28185ec5f23443f7648bf03c4868c628eb5
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2000-06-25 15:46:56 +0000

    This commit was manufactured by cvs2svn to create branch 'akaplan'.

---
+++ plugins/Code2HTML/branches/akaplan/code2html_current_selection.java
@@ -0,0 +1,43 @@
+/*
+ * code2html_current_selection.java
+ * Copyright (c) 2000 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+import java.awt.event.ActionEvent;
+
+import org.gjt.sp.jedit.*;
+
+import org.gjt.sp.util.Log;
+
+public class code2html_current_selection 
+	extends EditAction
+{
+	public code2html_current_selection() {
+		super("code2html.current-selection");
+	}
+
+	public void actionPerformed(ActionEvent evt) {
+		View v = EditAction.getView(evt);
+		Buffer b = (v == null) ? null : v.getBuffer();
+
+		if (v != null && b != null) {
+			Code2HTML code2html = new Code2HTML();
+			code2html.toHTML(v, b, true);
+		}
+	}
+}
+
