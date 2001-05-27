1d149723acde99017a9524cfab98abddd29c21b5
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-05-27 23:54:26 +0000

    This commit was manufactured by cvs2svn to create tag 'archive-0_3_1'.

---
+++ plugins/Archive/tags/archive-0_3_1/ArchiveCommand.java
@@ -0,0 +1,48 @@
+/*
+ * ArchiveCommand.java
+ * Copyright (c) 2001 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+
+import com.ice.tar.TarInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Hashtable;
+
+import java.util.zip.ZipInputStream;
+
+
+public abstract class ArchiveCommand {
+    abstract Hashtable getDirectories(String archiveProtocol, String archivePath)
+        throws IOException;
+
+
+    abstract InputStream createInputStream(String path) throws IOException;
+
+
+    public static ArchiveCommand getCommand(InputStream in) {
+        if (in instanceof TarInputStream) {
+            return new TarCommand((TarInputStream) in);
+        } else if (in instanceof ZipInputStream) {
+            return new ZipCommand((ZipInputStream) in);
+        } else {}
+
+        return null;
+    }
+}
---
+++ plugins/Archive/tags/archive-0_3_1/ZipVFS.java
@@ -0,0 +1,45 @@
+/*
+ * ZipVFS.java
+ * Copyright (c) 2000, 2001 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.gjt.sp.jedit.io.VFS;
+
+
+public class ZipVFS extends ArchiveVFS {
+    public static final String PROTOCOL = "zip";
+
+
+    public ZipVFS() {
+        super(ZipVFS.PROTOCOL);
+    }
+
+
+    public int getCapabilities() {
+        return VFS.READ_CAP;
+    }
+
+
+    protected InputStream openArchiveStream(InputStream in) throws IOException {
+        return ArchiveUtilities.openZipStream(in);
+    }
+}
+
