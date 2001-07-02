e942203374621e8d8dc7a0c3b384da49adc3850c
dmoebius <dmoebius@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-07-02 23:53:56 +0000

    moved JCompilerPlugin.java to package jcompiler

--- plugins/JCompiler/trunk/jcompiler/JCompilerPlugin.java
+++ plugins/JCompiler/trunk/jcompiler/JCompilerPlugin.java
@@ -2,6 +2,8 @@
  * JCompilerPlugin.java - JCompiler plugin
  * Copyright (c) 1999, 2000 Kevin A. Burton and Aziz Sharif
  *
+ * :tabSize=4:indentSize=4:noTabs=false:maxLineLen=0:
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -17,6 +19,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+
+package jcompiler;
+
+
 import java.util.Vector;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.jedit.gui.*;
@@ -28,7 +34,6 @@ public class JCompilerPlugin extends EditPlugin
 {
 
 	public void start() {
-		jEdit.unsetProperty("jcompiler.usejavacp"); // obsolete
 		shell = new JCompilerShell();
 		EditBus.addToNamedList(console.Shell.SHELLS_LIST, shell);
 	}
@@ -48,11 +53,60 @@ public class JCompilerPlugin extends EditPlugin
 	}
 
 
+	/**
+	 * Returns a shared JCompilerShell singleton instance.
+	 */
 	public static JCompilerShell getShell() {
 		return shell;
 	}
 
 
-	// private members
+	/**
+	 * Executes an arbitrary command in the JCompiler shell.
+	 * @see  JCompilerShell  for a list of supported commands.
+	 */
+	public static void executeCommand(View view, String command) {
+		// ensure Console window is visible:
+		DockableWindowManager wm = view.getDockableWindowManager();
+		wm.addDockableWindow("console");
+		// set current Console shell to JCompilerShell:
+		console.Console console = (console.Console) wm.getDockableWindow("console");
+		console.setShell(shell);
+		// run the command:
+		console.run(command);
+		// bugfix: textarea looses focus after compile:
+		view.getTextArea().requestFocus();
+	}
+
+
+	/**
+	 * Invokes the command "compile" on the Shell instance,
+	 * compiling the current buffer.
+	 */
+	public static void compileFile(View view) {
+		executeCommand(view, "compile");
+	}
+
+
+	/**
+	 * Invokes the command "compilepkg" on the Shell instance,
+	 * compiling outdated files of the source tree containing current buffer.
+	 */
+	public static void compilePackage(View view) {
+		executeCommand(view, "compilepkg");
+	}
+
+
+	/**
+	 * Invokes the command "rebuildpkg" on the Shell instance,
+	 * recompiling all files of the source tree containing current buffer.
+	 */
+	public static void rebuildPackage(View view) {
+		executeCommand(view, "rebuildpkg");
+	}
+
+
+	/** holds a shared instance of the JCompiler shell. */
 	private static JCompilerShell shell;
+
 }
