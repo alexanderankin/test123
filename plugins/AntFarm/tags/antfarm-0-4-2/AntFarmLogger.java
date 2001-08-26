4894e007d8138035bec8c55f3f8b9bd56c97fabb
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-08-26 09:27:18 +0000

    This commit was manufactured by cvs2svn to create tag 'antfarm-0-4-2'.

---
+++ plugins/AntFarm/tags/antfarm-0-4-2/AntFarmLogger.java
@@ -0,0 +1,442 @@
+/*
+ * AntFarmLogger.java - Ant build utility plugin for jEdit
+ * Copyright (C) 2000 Chris Scott
+ * Other contributors: Rick Gibbs, Todd Papaioannou
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999, 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/**
+  @author Chris Scott, Rick Gibbs
+*/
+
+import java.io.*;
+import java.util.*;
+import org.apache.tools.ant.*;
+
+public class AntFarmLogger implements BuildLogger
+{
+
+  private PrintStream output;
+  private PrintStream err;
+  protected int msgOutputLevel = Project.MSG_INFO;
+  private long startTime = System.currentTimeMillis();
+  private BuildMessage currentBuildMessage = new BuildMessage();
+  private AntFarmPlugin farm;
+  private AntFarm window;
+
+  protected static String lSep = System.getProperty("line.separator");
+
+  protected boolean emacsMode = false;
+
+  public AntFarmLogger( AntFarmPlugin farm, AntFarm window )
+  {
+    this.farm = farm;
+    this.window = window;
+  }
+
+  /**
+   * Set the msgOutputLevel this logger is to respond to.
+   *
+   * Only messages with a message level lower than or equal to the given level are
+   * output to the log.
+   *
+   * @param level the logging level for the logger.
+   */
+  public void setMessageOutputLevel(int level)
+  {
+    this.msgOutputLevel = level;
+  }
+
+  public void setOutputPrintStream(PrintStream output)
+  {
+    this.output = output;
+  }
+
+  public void setErrorPrintStream(PrintStream err)
+  {
+    this.err = err;
+  }
+
+  public PrintStream getOutputPrintStream()
+  {
+    return this.output;
+  }
+
+  public PrintStream getErrorPrintStream()
+  {
+    return this.err;
+  }
+
+
+  public void setEmacsMode(boolean emacsMode)
+  {}
+
+
+  public void buildStarted(BuildEvent event)
+  {
+    startTime = System.currentTimeMillis();
+  }
+
+  /**
+   *  Prints whether the build succeeded or failed, and
+   *  any errors the occured during the build.
+   */
+
+  // FIXME: For some reason this is not being called.  No biggie, its just
+  //     annoying not to know why
+  public void buildFinished(BuildEvent event)
+  {
+    System.out.println("FINISHED");
+    Throwable error = event.getException();
+
+    if (error == null)
+    {
+      output.println(lSep + "BUILD SUCCESSFUL");
+    }
+    else
+    {
+      err.println(lSep + "BUILD FAILED" + lSep);
+
+      if (error instanceof BuildException)
+      {
+        err.println(error.toString());
+
+        Throwable nested = ((BuildException)error).getException();
+
+        if (nested != null)
+        {
+          System.out.println("h1");
+          nested.printStackTrace(err);
+        }
+      }
+      else
+      {
+        System.out.println("h2");
+        error.printStackTrace(err);
+      }
+    }
+
+    //output.println(lSep + "Total time: " + formatTime(System.currentTimeMillis() - startTime));
+  }
+
+  public void targetStarted(BuildEvent event)
+  {
+    if (msgOutputLevel <= Project.MSG_INFO)
+    {
+      farm.handleBuildMessage( window, new BuildMessage( lSep + event.getTarget().getName() + ":") );
+    }
+  }
+
+  public void targetFinished(BuildEvent event)
+  {}
+
+  public void taskStarted(BuildEvent event)
+  {}
+
+  public void taskFinished(BuildEvent event)
+  {}
+
+  public void messageLogged(BuildEvent event)
+  {
+    // A flag we'll use to denote which compiler we are using
+    boolean usingJikes = false;
+
+    if (event.getPriority() <= msgOutputLevel)
+    {
+      // Get the name of the task
+      String name = event.getTask().getTaskName();
+
+      // Make sure we are in the javac task
+      if (name.equals("javac"))
+      {
+        // Find out which compiler we are using
+        String compiler =
+          event.getTask().getProject().getProperty("build.compiler");
+
+        if (compiler != null && compiler.equals("jikes"))
+          usingJikes = true;
+
+        // Retrieve the message
+        String line = event.getMessage();
+
+        // Make sure it's not a blank line
+        if (!line.trim().equals(""))
+        {
+          // check to see if this is the first line of the error message
+          if (isMessageLine(line, usingJikes))
+          {
+            if (usingJikes)
+              currentBuildMessage = parseJikesMessage(line);
+            else
+              currentBuildMessage = parseJavacMessage(line);
+          }
+          else
+          {
+            if (!usingJikes)
+            {
+              // check for the column pos of the error
+              int column = getLocationPos(line);
+
+              if ( (column>=0) && (currentBuildMessage!=null) )
+              {
+                currentBuildMessage.setColumn( column );
+                farm.handleBuildMessage( window, currentBuildMessage );
+                currentBuildMessage = null;
+              }
+            }
+            else
+            {
+              if (line.startsWith("Compiling "))
+              {
+                farm.handleBuildMessage(window,new BuildMessage(line));
+              }
+              // If the line starts like this it's the message line
+              else if (line.startsWith("***"))
+              {
+                currentBuildMessage.setMessage(getJikesBuildErrorMessage(line));
+                farm.handleBuildMessage(window,currentBuildMessage);
+              }
+              // Ok, the next line will give us the line, column and the message
+              else if (line.lastIndexOf("-") >= 0)
+              {
+                currentBuildMessage.setColumn(line.length());
+              }
+              else if (line.indexOf(".") >= 0)
+              {
+                if (line.indexOf(". . .") == -1 )
+                {
+                  int lineNumber = getJikesLineNumber(line);
+                  currentBuildMessage.setLine(lineNumber);
+                }
+              }
+
+            }
+          } // end of not a message line
+        }
+      }
+      else
+      {
+        farm.handleBuildMessage( window, new BuildMessage( event.getMessage() ) );
+      }
+    }
+  }
+
+  /**
+   * Get the error message from a jikes message
+   **/
+  private String getJikesBuildErrorMessage(String line)
+  {
+
+    // Get rid of the *'s
+    int newStart = line.lastIndexOf("*");
+
+    String message = line.substring(newStart + 1);
+    return message;
+
+  } // end of getJikesBuildErrorMessage
+
+  /**
+   * Get the line number from a jikes message
+   **/
+  private int getJikesLineNumber(String message)
+  {
+    // What we will return
+    int Result;
+
+    // Ok, let's see if there is the usual period
+    int dot = message.indexOf(".");
+
+    // Now, trim down the message
+    String number = message.substring(0, dot);
+    number = number.trim();
+
+    // Ok, what does that give us?
+    Result = Integer.parseInt(number);
+
+    return Result;
+  }
+
+  /**
+   * Parse the first line that jikes returns to see if it is a compile message
+   **/
+  private BuildMessage parseJikesMessage(String line)
+  {
+    // First of all, let's work out what the file name is
+    int firstQuote = line.indexOf("\"");
+    int endOfName = line.indexOf(".java");
+
+    // Ok, this is ugly, but we need to adjust our numbers.
+    String filename = line.substring(firstQuote + 1, endOfName + 5);
+
+    BuildMessage theMessage = new BuildMessage();
+    theMessage.setFileName(filename);
+
+    // Now work out if this is an error
+    int i = line.indexOf("error");
+    if (i != -1)
+      theMessage.setError(true);
+
+    // Or a warning
+    i = line.indexOf("warning");
+    if (i != -1)
+      theMessage.setWarning(true);
+
+    return theMessage;
+  }
+
+  private BuildMessage parseJavacMessage(String line)
+  {
+    int p1 = line.indexOf(".java:");
+
+    if(p1>=0)
+      p1+=5;
+
+    int p2 = line.indexOf(":",p1+1);
+
+    if(p1>=0 && p2>=0)
+    {
+      int p0 = line.indexOf("[javac]");
+
+      if(p0>=0)
+        p0+=7;
+
+      //only if ".java:" and a second ":" are found
+      //the line includes a correct message to display
+
+      //extract the filename
+      String f = line.substring(p0>=0 ? p0 : 0,p1).trim();
+
+      //get the line number
+      String l = line.substring(p1+1, p2).trim();
+
+      //and finally get the message itself
+      String m = line.substring(p2+1).trim();
+
+      // return a new error message for the error list:
+      return new BuildMessage(f, Integer.parseInt(l), "error: " + m);
+    }
+
+    return null;
+  }
+
+  private boolean isMessageLine(String line, boolean jikes)
+  {
+    if (!jikes)
+    {
+      // Get the position of
+      int p1 = line.indexOf(".java:");
+
+      if(p1>=0)
+        p1+=5;
+
+      int p2 = line.indexOf(":",p1+1);
+
+      if(p1>=0 && p2>=0)
+      {
+        try
+        {
+          Integer.parseInt(line.substring(p1+1,p2).trim());
+          return true;
+        }
+        catch(NumberFormatException e)
+        {
+          return false;
+        }
+      }
+
+      //check for last line of the format "LineNr String" like "2 warnings"
+      int x = line.indexOf(' ');
+
+      try
+      {
+        if(x>0)
+          Integer.parseInt(line.substring(0,x)); //line starts with a number??
+
+        String s = line.substring(x+1).toLowerCase();
+
+        if(s.startsWith("error") || s.startsWith("warning"))
+          return true;
+      }
+      catch (NumberFormatException e)
+      {
+        // do nothing
+      }
+
+      return false;
+    }
+    else
+    {
+      // Trim the line
+      line.trim();
+
+      // Does it start with Compiling?
+      if (line.startsWith("Compiling"))
+        return false;
+
+      // Does it start with "Found" (the string that jikes uses to signify an
+      // error.
+      if (line.startsWith("Found"))
+        return true;
+    }
+
+    return false;
+
+  } // end of isMessageLine
+
+  private int getLocationPos(String line)
+  {
+    return line.indexOf('^');
+  }
+
+}
---
+++ plugins/AntFarm/tags/antfarm-0-4-2/AntFarmPlugin.java
@@ -0,0 +1,247 @@
+/*
+ * AntFarmPlugin.java - Ant build utility plugin for jEdit
+ * Copyright (C) 2000 Chris Scott
+ * Other contributors: Rick Gibbs
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999, 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Ant", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/**
+  @author Chris Scott, Rick Gibbs
+*/
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.util.Log;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import java.util.Vector;
+import java.io.*;
+
+/**
+ * The 'Plugin' class is the interface between jEdit and the plugin.
+ * Plugins can either extend EditPlugin or EBPlugin. EBPlugins have
+ * the additional property that they receive EditBus messages.
+ */
+
+public class AntFarmPlugin extends EBPlugin
+{
+  /**
+   * The 'name' of our dockable window.
+   */
+  public static final String NAME = "antfarm";
+
+  private static DefaultErrorSource errorSource;
+
+  // save System.out and err in case we need them
+  static PrintStream out = System.out;
+  static PrintStream err = System.err;
+
+  private View theView;
+  /**
+   * Method called by jEdit to initialize the plugin.
+   */
+  public void start()
+  {
+    String javaHome = System.getProperty("java.home");
+    if (javaHome.toLowerCase().endsWith(File.separator + "jre"))
+      javaHome = javaHome.substring(0, javaHome.length() - 4);
+    String toolsPath = MiscUtilities.constructPath(javaHome,"lib","tools.jar");
+    String javaVersion = System.getProperty("java.version");
+    boolean isJDK12 = MiscUtilities.compareVersions(javaVersion, "1.2") >= 0;
+
+    // find and add tools.jar to the list of jEdit plugins on JDK 1.2 or higher:
+    // (tools.jar contains the compiler class)
+    if (isJDK12 && new File(toolsPath).exists())
+    {
+      EditPlugin.JAR jar = jEdit.getPluginJAR(toolsPath);
+      if (jar == null)
+      {
+        Log.log(Log.DEBUG, this, "JDK 1.2 or higher detected, adding " + toolsPath + " to jEdit plugins");
+        try
+        {
+          jEdit.addPluginJAR(new EditPlugin.JAR(toolsPath, new JARClassLoader(toolsPath)));
+        }
+        catch (IOException ioex)
+        {
+          Log.log(Log.ERROR, this, "Could not add tools.jar to jEdit plugins, reason follows...");
+          Log.log(Log.ERROR, this, ioex);
+        }
+      }
+    }
+
+    // add our own ErrorSource to the list of error sources
+    errorSource = new DefaultErrorSource("antfarm");
+    EditBus.addToNamedList(ErrorSource.ERROR_SOURCES_LIST,errorSource);
+    EditBus.addToBus(errorSource);
+
+    // add our dockable to the dockables 'named list'
+    EditBus.addToNamedList(DockableWindow.DOCKABLE_WINDOW_LIST,NAME);
+  }
+
+  /**
+   * Method called by jEdit before exiting. Usually, nothing
+   * needs to be done here.
+   */
+  //public void stop() {}
+
+  /**
+   * Method called every time a view is created to set up the
+   * Plugins menu. Menus and menu items should be loaded using the
+   * methods in the GUIUtilities class, and added to the list.
+   * @param menuItems Add the menu item here
+   */
+  public void createMenuItems(Vector menuItems)
+  {
+    menuItems.addElement(GUIUtilities.loadMenuItem("antfarm"));
+  }
+
+  /**
+   * Method called every time the plugin options dialog box is
+   * displayed. Any option panes created by the plugin should be
+   * added here.
+   * @param optionsDialog The plugin options dialog box
+   *
+   * @see OptionPane
+   * @see OptionsDialog#addOptionPane(OptionPane)
+   */
+  //public void createOptionPanes(OptionsDialog optionsDialog) {}
+
+  /**
+   * Handles a message sent on the EditBus. The default
+   * implementation ignores the message.
+   */
+  public void handleMessage(EBMessage message)
+  {
+    /* upon receiving a CreateDockableWindow, we check if
+     * the name of the requested window is 'hello-dockable',
+     * and create it if it is so.
+     */
+
+    if(message instanceof CreateDockableWindow)
+    {
+      CreateDockableWindow cmsg = (CreateDockableWindow)message;
+      theView = cmsg.getView();
+
+      if(cmsg.getDockableWindowName().equals(NAME))
+        cmsg.setDockableWindow(new AntFarm(this, cmsg.getView()));
+    }
+  }
+
+  /**
+   * Handle all of ANT's build messages including System.out and System.err
+   *
+   * Each build message filename, line no, column, of the error that it is
+   * reporting if it in fact is an Error.  You can use this info some how
+   * to publish an event to the Error List
+   *
+   * TODO: handle System.err messages with a different color.
+   */
+  void handleBuildMessage( AntFarm antFarm, BuildMessage message )
+  {
+    handleBuildMessage( antFarm, message, null);
+  }
+
+  void handleBuildMessage( AntFarm antFarm, BuildMessage message, Color lineColor )
+  {
+    if( message.isError() )
+    {
+      // publish this message to the ErrorList
+      addError( ErrorSource.ERROR, message.getAbsoluteFilename(),
+                message.getLine(), message.getColumn(),
+                message.toString() ); //message.getMessage() );
+      // "error-list" should be ErrorListPlugin.NAME but the
+      // jar could be in a couple of different places...
+      // theView.getDockableWindowManager().addDockableWindow("error-list");
+    }
+    else if( message.isWarning() )
+    {
+      // publish this message to the ErrorList
+      addError( ErrorSource.WARNING, message.getAbsoluteFilename(),
+                message.getLine(), message.getColumn(),
+                message.toString() ); //message.getMessage() );
+      // "error-list" should be ErrorListPlugin.NAME but the
+      // jar could be in a couple of different places...
+      // theView.getDockableWindowManager().addDockableWindow("error-list");
+    }
+    else
+    {
+      // publish this message to the buildResults text area
+      //out.println("[MESSAGE]: " + message.toString() );
+      if (lineColor != null)
+        antFarm.appendToTextArea(message.toString(), lineColor);
+      else
+        antFarm.appendToTextArea(message.toString());
+    }
+  }
+
+
+  void addError(int type, String file, int line, int column, String message)
+  {
+    /** this method call is a hack.. the signature of the method call is:
+    * addError(int type, String path, int lineIndex, int start, int end, String error)
+    * I don't know what 'start' and 'end' do
+    * The -1 is a hack to make the error point to the correct place
+    */
+
+    errorSource.addError(type,file,line - 1,0,0,message);
+  }
+
+  void clearErrors()
+  {
+    errorSource.clear();
+  }
+
+}
