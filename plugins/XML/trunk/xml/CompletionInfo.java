b1fb8fbc30e77ca8022ea8a9545ef38591235efe
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-09-03 05:53:28 +0000

    Tag completion for HTML almost done

--- plugins/XML/trunk/xml/CompletionInfo.java
+++ plugins/XML/trunk/xml/CompletionInfo.java
@@ -14,6 +14,10 @@ package xml;
 
 import java.util.Hashtable;
 import java.util.Vector;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+import org.xml.sax.XMLReader;
+import org.xml.sax.SAXException;
 
 class CompletionInfo
 {
@@ -37,4 +41,59 @@ class CompletionInfo
 		this.entityHash = entityHash;
 		this.ids = ids;
 	}
+
+	static CompletionInfo getCompletionInfo(EditPane editPane)
+	{
+		CompletionInfo info = (CompletionInfo)editPane.getClientProperty(
+			XmlPlugin.COMPLETION_INFO_PROPERTY);
+		if(info != null)
+			return info;
+		else
+			return getCompletionInfo(editPane.getBuffer().getMode().getName());
+	}
+
+	static CompletionInfo getCompletionInfo(String mode)
+	{
+		String resource = jEdit.getProperty("mode." + mode
+			+ "." + XmlPlugin.COMPLETION_INFO_PROPERTY);
+		if(resource == null)
+			return null;
+
+		CompletionInfo info = (CompletionInfo)completionInfo.get(resource);
+		if(info != null)
+			return info;
+
+		Log.log(Log.DEBUG,CompletionInfo.class,"Loading " + resource);
+
+		CompletionInfoHandler handler = new CompletionInfoHandler();
+
+		try
+		{
+			XMLReader parser = new org.apache.xerces.parsers.SAXParser();
+			parser.setFeature("http://apache.org/xml/features/validation/dynamic",true);
+			parser.setErrorHandler(handler);
+			parser.setEntityResolver(handler);
+			parser.setContentHandler(handler);
+			parser.parse(resource);
+		}
+		catch(SAXException se)
+		{
+			Throwable e = se.getException();
+			if(e == null)
+				e = se;
+			Log.log(Log.ERROR,CompletionInfo.class,e);
+		}
+		catch(Exception e)
+		{
+			Log.log(Log.ERROR,CompletionInfo.class,e);
+		}
+
+		info = handler.getCompletionInfo();
+		completionInfo.put(resource,info);
+
+		return info;
+	}
+
+	// private members
+	private static Hashtable completionInfo = new Hashtable();
 }
