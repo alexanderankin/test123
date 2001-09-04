b17d64224286004ccd35a7d15f97968be7b7ba82
Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>
2001-09-04 06:34:56 +0000

    Added progress dialog

---
+++ plugins/XSLT/trunk/xslt/XSLTProgressDialog.java
@@ -0,0 +1,25 @@
+package xslt;
+
+
+import javax.swing.*;
+import org.apache.xalan.trace.*;
+
+public class XSLTProgressDialog extends JDialog implements TraceListener
+{
+
+	public void selected(SelectionEvent event) throws javax.xml.transform.TransformerException
+	{
+
+	}
+
+	public void trace(TracerEvent event)
+	{
+
+	}
+
+	public void generated(GenerateEvent event)
+	{
+
+	}
+
+}
