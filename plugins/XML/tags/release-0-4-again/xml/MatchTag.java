cc533de6eb1e99b132f02ffa5482aa7284858eb7
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-09-05 08:24:28 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-4-again'.

---
+++ plugins/XML/tags/release-0-4-again/xml/MatchTag.java
@@ -0,0 +1,213 @@
+/*
+ * MatchTag.java
+ * Copyright (C) 2000 Scott Wyatt, 2001 Andre Kaplan
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml;
+
+import java.util.StringTokenizer;
+
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.textarea.Selection;
+
+public class MatchTag
+{
+	public static void matchTag(JEditTextArea textArea)
+	{
+		String text = textArea.getText();
+		TagAttribute tag_attr = getSelectedTag(textArea.getCaretPosition(), text);
+		if (tag_attr != null && (tag_attr.tag).charAt(0) != '/')
+		{
+			TagAttribute endtag_attr = null;
+			if ((endtag_attr = findEndTag(text, tag_attr.tag, tag_attr.endpos + 1, 0)) != null)
+			{
+				textArea.setSelection(new Selection.Range(
+					endtag_attr.startpos, endtag_attr.endpos
+				));
+			}
+			else
+				textArea.getToolkit().beep();
+		}
+		if (tag_attr != null && (tag_attr.tag).charAt(0) == '/')
+		{
+			TagAttribute starttag_attr = null;
+			if ((starttag_attr = findStartTag(text, (tag_attr.tag).substring(1), (tag_attr.startpos - 1), 0)) != null)
+			{
+				textArea.setSelection(new Selection.Range(
+					starttag_attr.startpos, starttag_attr.endpos
+				));
+			}
+			else
+				textArea.getToolkit().beep();
+		}
+	}
+
+
+	public static TagAttribute getSelectedTag(int pos, String text)
+	{
+		int startpos = -1;
+		int endpos   = -1;
+
+		for (int i = pos; i >= 0; --i)
+		{
+			if (i < pos && text.charAt(i) == '<')
+			{
+				startpos = i;
+				break;
+			}
+			if (i < (pos - 1) && text.charAt(i) == '>')
+			{
+				return null;
+			}
+		}
+
+		if (startpos != -1)
+		{
+			for (int i = startpos; i < text.length(); ++i)
+			{
+				if (text.charAt(i) == '>')
+				{
+					endpos = i;
+					break;
+				}
+				if (i > startpos && text.charAt(i) == '<')
+					return null;
+			}
+		}
+
+		if ((startpos != -1) && (endpos != -1))
+		{
+			StringTokenizer st1 = new StringTokenizer(
+				text.substring(startpos + 1, endpos), "<>"
+			);
+
+			if (st1.hasMoreTokens())
+			{
+				StringTokenizer st2 = new StringTokenizer(st1.nextToken());
+				if (st2.hasMoreTokens())
+				{
+					TagAttribute tag_attr = new TagAttribute();
+					tag_attr.startpos = startpos;
+					tag_attr.endpos   = endpos;
+					tag_attr.tag	  = st2.nextToken();
+
+					return tag_attr;
+				}
+			}
+
+			return null;
+		}
+
+		return null;
+	}
+
+
+	public static TagAttribute findEndTag(String text, String endtag, int offset, int count)
+	{
+		for (int i = offset; i < text.length(); ++i) 
+		{
+			String tag = null;
+			if ((text.charAt(i)) == '<')
+			{
+				int index = readForwardUntil(text, i + 1, '>');
+				if (index != -1) {
+					tag = text.substring(i + 1, index).toLowerCase();
+					endtag = endtag.toLowerCase();
+					if (tag.startsWith(endtag))
+						return findEndTag(text, endtag, (index + 1), ++count);
+
+					if (tag.startsWith("/" + endtag)
+					) {
+						if(count > 0)
+							return findEndTag(text, endtag, (index + 1), --count);
+						else
+						{
+							TagAttribute tagattr = new TagAttribute();
+							tagattr.startpos = i;
+							tagattr.endpos = (index + 1);
+							tagattr.tag = tag;
+							return tagattr;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+
+	public static TagAttribute findStartTag(String text, String starttag, int offset, int count)
+	{
+		for (int i = offset; i >= 0; --i)
+		{
+			String tag = null;
+			if ((text.charAt(i)) == '>')
+			{
+				int index = readBackwardUntil(text, i, '<');
+				if (index != -1)
+				{
+					tag = text.substring((index + 1), i).toLowerCase().trim();
+					starttag = starttag.toLowerCase();
+					if (tag.startsWith("/" + starttag))
+						return findStartTag(text, starttag, (index - 1), ++count);
+
+					if (tag.startsWith(starttag))
+					{
+						if(count > 0)
+							return findStartTag(text, starttag, (index - 1), --count);
+						else
+						{
+							TagAttribute tagattr = new TagAttribute();
+							tagattr.startpos = index;
+							tagattr.endpos = (i + 1);
+							tagattr.tag = tag;
+							return tagattr;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+
+	private static int readForwardUntil(String text, int offset, char delimiter)
+	{
+		for(int i = offset; i < text.length(); ++i)
+		{
+			char value = text.charAt(i);
+			if(value == delimiter)
+				return i;
+		}
+		return -1;
+	}
+
+
+	private static int readBackwardUntil(String text, int offset, char delimiter)
+	{
+		for (int i = offset; i >= 0; --i)
+		{
+			char value = text.charAt(i);
+			if (value == delimiter)
+				return i;
+		}
+		return -1;
+	}
+
+
+	public static class TagAttribute
+	{
+		int startpos = -1;
+		int endpos = -1;
+		String tag = null;
+	}
+}
+
---
+++ plugins/XML/tags/release-0-4-again/xml/XmlInsert.java
@@ -0,0 +1,293 @@
+/*
+ * XmlInsert.java
+ * Copyright (C) 2001 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml;
+
+import javax.swing.event.*;
+import javax.swing.table.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.*;
+
+public class XmlInsert extends JPanel implements DockableWindow, EBComponent
+{
+	public XmlInsert(View view)
+	{
+		this.view = view;
+		editPaneHandler = new EditPaneHandler();
+
+		setLayout(new GridLayout(2,1));
+
+		JPanel elementPanel = new JPanel(new BorderLayout());
+		elementPanel.add(BorderLayout.NORTH,
+			new JLabel(jEdit.getProperty("xml-insert.elements")));
+
+		elementList = new JList();
+		elementList.setCellRenderer(new XmlListCellRenderer());
+		elementList.addMouseListener(new MouseHandler());
+		elementList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		elementPanel.add(BorderLayout.CENTER,
+			new JScrollPane(elementList));
+
+		add(elementPanel);
+
+		JPanel entityPanel = new JPanel(new BorderLayout());
+		entityPanel.add(BorderLayout.NORTH,
+			new JLabel(jEdit.getProperty("xml-insert.entities")));
+
+		entityList = new JList();
+		entityList.setCellRenderer(new XmlListCellRenderer());
+		entityList.addMouseListener(new MouseHandler());
+		entityList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		entityPanel.add(BorderLayout.CENTER,
+			new JScrollPane(entityList));
+
+		add(entityPanel);
+
+		update();
+	}
+
+	public String getName()
+	{
+		return XmlPlugin.INSERT_NAME;
+	}
+
+	public Component getComponent()
+	{
+		return this;
+	}
+
+	public void addNotify()
+	{
+		super.addNotify();
+		EditBus.addToBus(this);
+	}
+
+	public void removeNotify()
+	{
+		super.removeNotify();
+		EditBus.removeFromBus(this);
+	}
+
+	public void handleMessage(EBMessage msg)
+	{
+		if(msg instanceof EditPaneUpdate)
+		{
+			EditPaneUpdate emsg = (EditPaneUpdate)msg;
+			EditPane editPane = emsg.getEditPane();
+			if(emsg.getWhat() == EditPaneUpdate.BUFFER_CHANGED
+				&& editPane == view.getEditPane())
+			{
+				update();
+			}
+			else if(emsg.getWhat() == EditPaneUpdate.CREATED
+				&& editPane.getView() == view)
+			{
+				JEditTextArea textArea = editPane.getTextArea();
+				textArea.addFocusListener(editPaneHandler);
+				textArea.addCaretListener(editPaneHandler);
+			}
+			else if(emsg.getWhat() == EditPaneUpdate.DESTROYED
+				&& editPane.getView() == view)
+			{
+				JEditTextArea textArea = editPane.getTextArea();
+				textArea.removeFocusListener(editPaneHandler);
+				textArea.removeCaretListener(editPaneHandler);
+			}
+		}
+		else if(msg instanceof BufferUpdate)
+		{
+			BufferUpdate bmsg = (BufferUpdate)msg;
+			if((bmsg.getWhat() == BufferUpdate.MODE_CHANGED
+				|| bmsg.getWhat() == BufferUpdate.LOADED)
+				&& bmsg.getBuffer() == view.getBuffer())
+			{
+				update();
+			}
+		}
+	}
+
+	// package-private members
+	void setDeclaredElements(Vector elements)
+	{
+		this.elements = elements;
+
+		if(elements == null)
+		{
+			DefaultListModel model = new DefaultListModel();
+			model.addElement(jEdit.getProperty("xml-insert.not-parsed"));
+			elementList.setModel(model);
+		}
+		else
+		{
+			// inefficent
+			DefaultListModel model = new DefaultListModel();
+			for(int i = 0; i < elements.size(); i++)
+			{
+				model.addElement(elements.elementAt(i));
+			}
+			elementList.setModel(model);
+
+			if(model.getSize() != 0)
+			{
+				Rectangle cellBounds = elementList
+					.getCellBounds(0,0);
+				if(cellBounds != null)
+				{
+					elementList.setFixedCellHeight(cellBounds.height);
+				}
+			}
+		}
+	}
+
+	void setDeclaredEntities(Vector entities)
+	{
+		if(entities == null)
+		{
+			DefaultListModel model = new DefaultListModel();
+			model.addElement(jEdit.getProperty("xml-insert.not-parsed"));
+			entityList.setModel(model);
+		}
+		else
+		{
+			// inefficent
+			DefaultListModel model = new DefaultListModel();
+			for(int i = 0; i < entities.size(); i++)
+			{
+				model.addElement(entities.elementAt(i));
+			}
+			entityList.setModel(model);
+
+			if(model.getSize() != 0)
+			{
+				Rectangle cellBounds = entityList
+					.getCellBounds(0,0);
+				if(cellBounds != null)
+				{
+					entityList.setFixedCellHeight(cellBounds.height);
+				}
+			}
+		}
+
+	}
+
+	// private members
+	private View view;
+	private EditPaneHandler editPaneHandler;
+	private Vector elements;
+	private JList elementList;
+	private JList entityList;
+
+	private void update()
+	{
+		CompletionInfo completionInfo = CompletionInfo.getCompletionInfo(
+			view.getEditPane());
+		if(completionInfo != null)
+		{
+			setDeclaredElements(completionInfo.elements);
+			setDeclaredEntities(completionInfo.entities);
+		}
+		else
+			showNotParsedMessage();
+	}
+
+	private void showNotParsedMessage()
+	{
+		setDeclaredElements(null);
+		setDeclaredEntities(null);
+	}
+
+	private void updateTagList()
+	{
+		// TODO: only show tags that can be inserted at caret
+		// position
+	}
+
+	class EditPaneHandler implements FocusListener, CaretListener
+	{
+		public void focusGained(FocusEvent evt)
+		{
+			showNotParsedMessage();
+		}
+
+		public void focusLost(FocusEvent evt)
+		{
+		}
+
+		public void caretUpdate(CaretEvent evt)
+		{
+			if(evt.getSource() == view.getTextArea())
+				updateTagList();
+		}
+	}
+
+	class MouseHandler extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent evt)
+		{
+			if(evt.getSource() == elementList)
+			{
+				int index = elementList.locationToIndex(
+					evt.getPoint());
+				if(index == -1)
+					return;
+
+				Object obj = elementList.getModel().getElementAt(index);
+				if(!(obj instanceof ElementDecl))
+					return;
+
+				ElementDecl element = (ElementDecl)obj;
+
+				EditPane editPane = view.getEditPane();
+				JEditTextArea textArea = editPane.getTextArea();
+
+				if(GUIUtilities.isPopupTrigger(evt))
+				{
+					// RMB click doesn't show edit tag dialog
+					textArea.setSelectedText("<" + element.name
+						+ (element.empty && !element.html
+						? " />" : ">"));
+
+					textArea.requestFocus();
+				}
+				else
+				{
+					// show edit tag dialog box
+					XmlActions.showEditTagDialog(view,element);
+				}
+			}
+			else if(evt.getSource() == entityList)
+			{
+				int index = entityList.locationToIndex(
+					evt.getPoint());
+				if(index == -1)
+					return;
+
+				Object obj = entityList.getModel().getElementAt(index);
+				if(!(obj instanceof EntityDecl))
+					return;
+
+				EntityDecl entity = (EntityDecl)obj;
+
+				JEditTextArea textArea = view.getTextArea();
+
+				textArea.setSelectedText("&" + entity.name + ";");
+				textArea.requestFocus();
+			}
+		}
+	}
+}
