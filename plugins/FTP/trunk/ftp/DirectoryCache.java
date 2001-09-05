e09fb8d8226cecd673f0eb05737b87d11ca2d045
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-09-05 05:30:02 +0000

    I've started working on the FTP plugin again

--- plugins/FTP/trunk/ftp/DirectoryCache.java
+++ plugins/FTP/trunk/ftp/DirectoryCache.java
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+package ftp;
+
 import java.io.*;
 import java.util.Enumeration;
 import java.util.Hashtable;
--- plugins/FTP/trunk/ftp/LoginDialog.java
+++ plugins/FTP/trunk/ftp/LoginDialog.java
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+package ftp;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.event.*;
