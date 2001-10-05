2413197efc708a379312083923e5899094bd71a1
brik <brik@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-10-05 06:35:06 +0000

    AntBrowser import

--- plugins/AntFarm/trunk/plugin/integration/PluginBridge.java
+++ plugins/AntFarm/trunk/plugin/integration/PluginBridge.java
@@ -1,72 +1,44 @@
 /*
- * The Apache Software License, Version 1.1
+ *  PluginBridge.java - Plugin for running Ant builds from jEdit.
+ *  Copyright (C) 2001 Brian Knowles
  *
- * Copyright (c) 1999, 2000 The Apache Software Foundation.  All rights
- * reserved.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "The Jakarta Project", "Ant", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package plugin.integration;
 
 import org.gjt.sp.jedit.*;
 
-
 /**
- * A bridge from one plugin to another.  Subclasses of should make sure
- * they implement a default constructor.
+ *  A bridge from one plugin to another. Subclasses of should make sure they
+ *  implement a default constructor.
+ *
+ * @author     steinbeck
+ * @created    September 14, 2001
  */
-public interface PluginBridge {
-  
-  /**
-   * Enable the bridge.
-   *
-   * @return <code>true</code> if the bridge was able to enable itself.
-   */
-  public boolean enable( EditPlugin srcPlugin, EditPlugin tgtPlugin, View view );
-  
+public interface PluginBridge
+{
+
+	/**
+	 *  Enable the bridge.
+	 *
+	 * @param  srcPlugin  Description of Parameter
+	 * @param  tgtPlugin  Description of Parameter
+	 * @param  view       Description of Parameter
+	 * @return            <code>true</code> if the bridge was able to enable itself.
+	 */
+	public boolean enable( EditPlugin srcPlugin, EditPlugin tgtPlugin, View view );
+
 }
+
