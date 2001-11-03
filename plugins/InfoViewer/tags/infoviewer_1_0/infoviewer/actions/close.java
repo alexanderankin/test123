dea91e28cb77f9fd045d85505062847bcab94032
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2001-11-03 10:26:48 +0000

    This commit was manufactured by cvs2svn to create tag 'infoviewer_1_0'.

---
+++ plugins/InfoViewer/tags/infoviewer_1_0/infoviewer/actions/close.java
@@ -0,0 +1,52 @@
+/*
+ * close.java - close action for InfoViewer
+ * Copyright (C) 1999-2001 Dirk Moebius
+ *
+ * :tabSize=4:indentSize=4:noTabs=true:maxLineLen=0:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package infoviewer.actions;
+
+import infoviewer.InfoViewer;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import org.gjt.sp.jedit.View;
+
+
+public class close extends InfoViewerAction
+{
+
+    public close()
+    {
+        super("infoviewer.close");
+    }
+
+
+    public void actionPerformed(ActionEvent evt)
+    {
+        Frame frame = getFrame(evt);
+
+        if (frame == null)
+            return;
+
+        if (frame instanceof View)
+            ((View)frame).getDockableWindowManager().removeDockableWindow(InfoViewer.DOCKABLE_NAME);
+        else
+            frame.dispose();
+    }
+}
+
