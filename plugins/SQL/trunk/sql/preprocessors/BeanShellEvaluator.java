0764c2909f5ea9bc286bef9c8098a73ba45273be
svu <svu@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-02-23 01:00:17 +0000

    Preprocessing is introduced (thanks to Stefano). Now every type of preprocessor can be switched on/off separately

---
+++ plugins/SQL/trunk/sql/preprocessors/BeanShellEvaluator.java
@@ -0,0 +1,40 @@
+/**
+ * BeanShellEvaluator.java - Sql Plugin
+ * Copyright (C) 2001 Sergey V. Udaltsov
+ * svu@users.sourceforge.net
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package sql.preprocessors;
+
+import sql.*;
+
+/**
+ *  Description of the Class
+ *
+ * @author     svu
+ * @created    22 Февраль 2002 г.
+ */
+public class BeanShellEvaluator extends Preprocessor
+{
+  public String doProcess( String text )
+  {
+    System.out.println("BSH Preprocessing " + text );
+    return
+        (String)org.gjt.sp.jedit.BeanShell.eval( view, text, false );
+  }
+}
+
