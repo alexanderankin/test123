bd871cbfc81804907db520da3f5257c098d60abb
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-03-01 03:36:59 +0000

    This commit was manufactured by cvs2svn to create tag 'tags-rel-1-0-5'.

---
+++ plugins/Tags/tags/tags-rel-1-0-5/tags/ChooseTagListPopup.java
@@ -0,0 +1,267 @@
+/*
+ * ChooseTagListPopup.java
+ * Copyright (c) 2001, 2002 Kenrick Drew, Slava Pestov
+ *
+ * This file is part of TagsPlugin
+ *
+ * TagsPlugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * TagsPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/* This is pretty much ripped from gui/CompleteWord.java */
+
+package tags;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.gui.KeyEventWorkaround;
+
+class ChooseTagListPopup extends JWindow 
+{
+  
+  /***************************************************************************/
+	private TagsParser parser_;
+  private View view_;
+  private boolean openNewView_;
+  private Vector tagIdentifiers_;
+	private ChooseTagList tagIdentifierList_;
+  private boolean numberKeyProcessed_ = false;
+
+  /***************************************************************************/
+	public ChooseTagListPopup(TagsParser parser, View view, boolean openNewView) 
+  {
+		
+    super(view);
+
+    parser_ = parser;
+		view_ = view;
+    openNewView_ = openNewView;
+    
+    createComponents();
+    setupComponents();
+    placeComponents();
+    
+    GUIUtilities.requestFocus(this,tagIdentifierList_);
+    
+    pack();
+    setLocation();
+   
+    super.show();
+
+    KeyHandler keyHandler = new KeyHandler();
+		addKeyListener(keyHandler);
+		getRootPane().addKeyListener(keyHandler);
+		tagIdentifierList_.addKeyListener(keyHandler);
+    view_.setKeyEventInterceptor(keyHandler);
+	}
+
+  /***************************************************************************/
+  protected void createComponents() 
+  {
+    tagIdentifierList_ = parser_.getCollisionListComponent(view_);
+  }
+
+  /***************************************************************************/
+  protected void setupComponents() 
+  {
+		tagIdentifierList_.addMouseListener(new MouseHandler());  // not
+  }
+  
+  /***************************************************************************/
+  protected void placeComponents() 
+  {
+		// stupid scrollbar policy is an attempt to work around
+		// bugs people have been seeing with IBM's JDK -- 7 Sep 2000
+		JScrollPane scroller = new JScrollPane(tagIdentifierList_,
+			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    
+		getContentPane().add(scroller, BorderLayout.CENTER);
+  }
+  
+  /***************************************************************************/
+  public void setLocation() 
+  {
+		JEditTextArea textArea = view_.getTextArea();
+	
+		int caretLine = textArea.getCaretLine();
+		int lineIdx = textArea.getCaretPosition() - // offsets from beg of file
+									textArea.getLineStartOffset(caretLine);
+	
+    Point location = textArea.offsetToXY(textArea.getCaretPosition());
+    // modify Y to be below cursor
+    location.setLocation(location.getX(), 
+                         location.getY() + 
+                            textArea.getPainter().getFontMetrics().getHeight());
+                            
+		SwingUtilities.convertPointToScreen(location, textArea.getPainter());
+	
+		// make sure it fits on screen
+		Dimension d = getSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		if (location.x + d.width > screenSize.width)
+    {
+      if (d.width >= screenSize.width)
+        /* In this intance we should actually resize the number of columns in 
+         * the tag index filename, but for now just position it so that you 
+         * can at least read the left side of the dialog
+         */
+        location.x = 0;
+      else
+        location.x = screenSize.width - d.width;
+    }
+		if (location.y + d.height > screenSize.height)
+			location.y = screenSize.height - d.height;
+	
+		setLocation(location);
+}
+  
+  /***************************************************************************/
+	public void dispose()	
+  {
+    view_.setKeyEventInterceptor(null);
+		super.dispose();
+	}
+
+  /***************************************************************************/
+	private void selected()	
+  {
+    Tags.processTagLine(tagIdentifierList_.getSelectedIndex(), 
+                        view_, openNewView_, parser_.getTag());
+    
+		dispose();
+	}
+
+  /***************************************************************************/
+	class KeyHandler extends KeyAdapter	
+  {
+    public void keyTyped(KeyEvent evt)
+    {
+      evt = KeyEventWorkaround.processKeyEvent(evt);
+      if (evt == null)
+        return;
+
+      switch (evt.getKeyChar())
+      {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          if (numberKeyProcessed_) // Since many components have this handeler
+            return;
+
+          /* There may actually be more than 9 items in the list, but since
+           * the user would have to scroll to see them either with the mouse
+           * or with the arrow keys, then they can select the item they want
+           * with those means.
+           */
+          int selected = Character.getNumericValue(evt.getKeyChar()) - 1;
+          if (selected >= 0 && 
+              selected < tagIdentifierList_.getModel().getSize())
+          {
+            tagIdentifierList_.setSelectedIndex(selected);
+            selected();
+            numberKeyProcessed_ = true;
+          }
+          evt.consume();
+      }
+    }
+    
+		public void keyPressed(KeyEvent evt) 
+    {
+      evt = KeyEventWorkaround.processKeyEvent(evt);
+      if (evt == null)
+        return;
+      
+			switch(evt.getKeyCode()) 
+      {
+        case KeyEvent.VK_TAB:
+        case KeyEvent.VK_ENTER:
+          selected();
+          evt.consume();
+          break;
+        case KeyEvent.VK_ESCAPE:
+          dispose();
+          evt.consume();
+          break;
+        case KeyEvent.VK_UP:
+          if (getFocusOwner() == tagIdentifierList_)
+            return;
+
+          int selected = tagIdentifierList_.getSelectedIndex();
+          if (selected == 0)
+            selected = tagIdentifierList_.getModel().getSize() - 1;
+          else
+            selected = selected - 1;
+	
+          tagIdentifierList_.setSelectedIndex(selected);
+          tagIdentifierList_.ensureIndexIsVisible(selected);
+
+          evt.consume();
+          break;
+        case KeyEvent.VK_DOWN:
+          if (getFocusOwner() == tagIdentifierList_)
+            return;
+
+          selected = tagIdentifierList_.getSelectedIndex();
+          if (selected == tagIdentifierList_.getModel().getSize() - 1)
+            selected = 0;
+          else
+            selected = selected + 1;
+
+          tagIdentifierList_.setSelectedIndex(selected);
+          tagIdentifierList_.ensureIndexIsVisible(selected);
+
+          evt.consume();
+          break;
+        case KeyEvent.VK_1:
+        case KeyEvent.VK_2:
+        case KeyEvent.VK_3:
+        case KeyEvent.VK_4:
+        case KeyEvent.VK_5:
+        case KeyEvent.VK_6:
+        case KeyEvent.VK_7:
+        case KeyEvent.VK_8:
+        case KeyEvent.VK_9:
+          evt.consume();  /* so that we don't automatically dismiss */
+          break;
+
+        default:
+          dispose();
+          evt.consume();
+          break;
+			}
+		}
+	}
+
+  /***************************************************************************/
+  class MouseHandler extends MouseAdapter	
+  {
+		public void mouseClicked(MouseEvent evt) 
+    {
+      selected();
+    }
+  }
+}
---
+++ plugins/Tags/tags/tags-rel-1-0-5/tags/ExuberantCTagsParser.java
@@ -0,0 +1,164 @@
+/*
+ * ExuberantCTagsParser.java
+ * Copyright (c) 2001, 2002 Kenrick Drew
+ * kdrew@earthlink.net
+ *
+ * This file is part of TagsPlugin
+ *
+ * TagsPlugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * TagsPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package tags;
+
+import java.io.*;
+import java.lang.System.*;
+import java.util.*;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+
+/*
+ Exuberant C Tags are of the form:
+ 
+   --format=2 (default)
+     tag_name<TAB>file_path_name<TAB>ex_cmd;"<TAB>extension_fields
+   --format=1 
+     tag_name<TAB>file_path_name<TAB>ex_cmd
+     
+     ex_cmd can be of the form:
+       /^  search string $/
+     or
+       line_number
+     
+*/
+
+class ExuberantCTagsParser extends GenericTagsParser {
+
+  /***************************************************************************/
+  public ExuberantCTagsParser() { super(); }
+  
+  /***************************************************************************/
+  public TagLine createTagLine(final String tagLine, final String tagIndexFile)
+  {
+    if (tagLine == null)
+      return null;
+
+    /*** Get the definition file name ***/
+    String tagDefinitionFileName = null;      
+      
+    StringTokenizer st = new StringTokenizer(tagLine);
+    if (st.hasMoreTokens())  // skip tag from tag line
+      st.nextToken();
+    if (st.hasMoreTokens()) {
+      tagDefinitionFileName = st.nextToken();  // get file name
+    }
+
+    /* Exuberant C Tags comes with Cygwin.  However the path names are in a 
+     * form that Cygwin emulates Unix with.  The path /cygdrive/c/* isn't 
+     * actually a path but a mapped path to c:/*.  Since this will screw up
+     * JEdit, we will test for this type of path and convert to one that 
+     * JEdit knows...
+     */
+    if (tagDefinitionFileName.startsWith("/cygdrive/")) {
+      char driveLetter = tagDefinitionFileName.charAt(10);
+      tagDefinitionFileName = driveLetter + ":" +
+                               tagDefinitionFileName.substring(11);
+    }
+
+    /* resolve relative path names here */
+    File tagFile = new File(tagDefinitionFileName);
+    if (!tagFile.isAbsolute())
+    {
+      File tagIndexFilePath = new File(tagIndexFile);
+      tagDefinitionFileName = tagIndexFilePath.getParent() + 
+                              System.getProperty("file.separator") +
+                              tagDefinitionFileName;
+      tagIndexFilePath = null;
+    }
+    tagFile = null;
+    
+    /*** Get the search string ***/
+    String tagDefinitionSearchString = null;
+    
+    Log.log(Log.DEBUG, this, "----------------------------- Creating tag line");
+    
+    // get search string
+    if (tagLine.lastIndexOf(";\"") == -1)    // --format=1
+      tagDefinitionSearchString = st.nextToken("");
+    else                                     // --format=2 (default)
+    {
+      // We use '"' b/c we can't use the multi character delimiter that
+      // Exuberant C Tags uses (which is ;").  We will add on the "$/" for 
+      // the string massager.
+      tagDefinitionSearchString = st.nextToken("\"");
+      
+      // Loop off ; of the format 2 delim of ;"
+      tagDefinitionSearchString = tagDefinitionSearchString.substring(0,
+                           tagDefinitionSearchString.length() - 1);
+      Log.log(Log.DEBUG, this, "Search string from tokenizer:  " + 
+              tagDefinitionSearchString);
+    }
+    // Check to see if the search string is a number.  Number search
+    // strings are actually line numbers of #define tags
+    boolean isNumber = true;
+    int lineNumber = 0;
+    tagDefinitionSearchString = tagDefinitionSearchString.trim();
+    try {
+      lineNumber = Integer.parseInt(tagDefinitionSearchString);
+      tagDefinitionSearchString = null; // forget that it is a search string
+    } catch (NumberFormatException nfe) {
+      isNumber = false;
+    }
+
+    String origTagDefinitionSearchString = null;
+    if (!isNumber && tagDefinitionSearchString != null) 
+    {
+      origTagDefinitionSearchString = removeUnwantedChars(
+                            tagDefinitionSearchString.substring(2,
+                                       tagDefinitionSearchString.length() - 2));
+      tagDefinitionSearchString = 
+                              massageSearchString(tagDefinitionSearchString);
+    }
+
+    /*** Parse exuberant item info ***/
+    Vector exuberantInfoItems = null;
+    if (st.hasMoreTokens()) 
+    {
+      st.nextToken(" \t\n\r\f");  // get rid of ;"
+      exuberantInfoItems = new Vector(5);
+    }
+    String exuberantItemInfoString = null;
+    while (st.hasMoreTokens())
+    {
+      exuberantItemInfoString = st.nextToken();
+      exuberantInfoItems.addElement(
+                               new ExuberantInfoItem(exuberantItemInfoString));
+      Log.log(Log.DEBUG, null, "Item info:  " + exuberantItemInfoString);
+    }
+    exuberantItemInfoString = null;
+    
+    TagLine tl = new TagLine(tag_, tagDefinitionFileName,
+                             origTagDefinitionSearchString, 
+                             tagDefinitionSearchString, lineNumber,
+                             tagIndexFile);
+    if (exuberantInfoItems != null)
+      tl.exuberantInfoItems_ = exuberantInfoItems;
+      
+    return tl;
+  }
+  
+  /***************************************************************************/
+  public String toString() { return "Exuberant C Tags"; }
+}
