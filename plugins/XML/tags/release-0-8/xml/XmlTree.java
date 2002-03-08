883b7d419ada661dc436d74a3fbc04e95bc188d5
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-03-08 05:30:16 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-8'.

---
+++ plugins/XML/tags/release-0-8/xml/XmlTree.java
@@ -0,0 +1,384 @@
+/*
+ * XmlTree.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2000, 2001 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml;
+
+//{{{ Imports
+import javax.swing.event.*;
+import javax.swing.tree.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Vector;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.*;
+import org.gjt.sp.jedit.*;
+import xml.parser.*;
+//}}}
+
+public class XmlTree extends JPanel implements EBComponent
+{
+	//{{{ XmlTree constructor
+	public XmlTree(View view, boolean docked)
+	{
+		super(new BorderLayout());
+
+		this.view = view;
+
+		// create toolbar with parse button
+		JToolBar buttonBox = new JToolBar();
+		buttonBox.setFloatable(false);
+
+		parseBtn = new JButton(GUIUtilities.loadIcon("Refresh24.gif"));
+		parseBtn.setToolTipText(jEdit.getProperty("xml-tree.parse"));
+		parseBtn.setMargin(new Insets(0,0,0,0));
+		parseBtn.setRequestFocusEnabled(false);
+		parseBtn.addActionListener(new ActionHandler());
+		buttonBox.add(parseBtn);
+		buttonBox.add(Box.createGlue());
+
+		add(BorderLayout.NORTH,buttonBox);
+
+		// create a faux model that will do until a real one arrives
+		DefaultTreeModel emptyModel = new DefaultTreeModel(
+			new DefaultMutableTreeNode(null));
+		tree = new CustomTree(emptyModel);
+		if(docked)
+			tree.addMouseMotionListener(new MouseHandler());
+		tree.putClientProperty("JTree.lineStyle", "Angled");
+		tree.setVisibleRowCount(10);
+		tree.setCellRenderer(new Renderer());
+
+		add(BorderLayout.CENTER,new JScrollPane(tree));
+
+		propertiesChanged();
+
+		CaretHandler caretListener = new CaretHandler();
+
+		EditPane[] editPanes = view.getEditPanes();
+		for(int i = 0; i < editPanes.length; i++)
+		{
+			editPanes[i].getTextArea().addCaretListener(
+				caretListener);
+		}
+
+		update();
+	} //}}}
+
+	//{{{ requestDefaultFocus() method
+	public boolean requestDefaultFocus()
+	{
+		tree.requestFocus();
+		return true;
+	} //}}}
+
+	//{{{ addNotify() method
+	public void addNotify()
+	{
+		super.addNotify();
+		EditBus.addToBus(this);
+	} //}}}
+
+	//{{{ removeNotify() method
+	public void removeNotify()
+	{
+		super.removeNotify();
+		EditBus.removeFromBus(this);
+	} //}}}
+
+	//{{{ update() method
+	public void update()
+	{
+		DefaultTreeModel model = (DefaultTreeModel)view.getEditPane()
+			.getClientProperty(XmlPlugin.ELEMENT_TREE_PROPERTY);
+		if(model == null)
+		{
+			DefaultMutableTreeNode root = new DefaultMutableTreeNode(view.getBuffer().getName());
+			model = new DefaultTreeModel(root);
+
+			root.insert(new DefaultMutableTreeNode(
+				jEdit.getProperty("xml-tree.not-parsed")),0);
+
+			model.reload(root);
+			tree.setModel(model);
+		}
+		else
+		{
+			tree.setModel(model);
+			expandTagAt(view.getTextArea().getCaretPosition());
+		}
+	} //}}}
+
+	//{{{ handleMessage() method
+	public void handleMessage(EBMessage msg)
+	{
+		//{{{ EditPaneUpdate
+		if(msg instanceof EditPaneUpdate)
+		{
+			EditPaneUpdate epu = (EditPaneUpdate)msg;
+			EditPane editPane = epu.getEditPane();
+
+			if(epu.getWhat() == EditPaneUpdate.CREATED)
+				editPane.getTextArea().addCaretListener(new CaretHandler());
+		} //}}}
+		if(msg instanceof PropertiesChanged)
+			propertiesChanged();
+	} //}}}
+
+	//{{{ Private members
+
+	//{{{ Instance variables
+	private JButton parseBtn;
+	private JTree tree;
+
+	private int showAttributes;
+	private boolean parse;
+
+	private View view;
+	private Timer caretTimer;
+	//}}}
+
+	//{{{ propertiesChanged() method
+	private void propertiesChanged()
+	{
+		showAttributes = jEdit.getIntegerProperty("xml.show-attributes",0);
+	} //}}}
+
+	//{{{ expandTagWithDelay() method
+	private void expandTagWithDelay()
+	{
+		// if keystroke parse timer is running, do nothing
+		// if(keystrokeTimer != null && keystrokeTimer.isRunning())
+			// return;
+
+		if(caretTimer != null)
+			caretTimer.stop();
+
+		caretTimer = new Timer(0,new ActionListener()
+		{
+			public void actionPerformed(ActionEvent evt)
+			{
+				expandTagAt(view.getTextArea().getCaretPosition());
+			}
+		});
+
+		caretTimer.setInitialDelay(500);
+		caretTimer.setRepeats(false);
+		caretTimer.start();
+	} //}}}
+
+	//{{{ expandTagAt() method
+	private void expandTagAt(int dot)
+	{
+		DefaultMutableTreeNode root = (DefaultMutableTreeNode)tree
+			.getModel().getRoot();
+
+		if(root.getChildCount() == 0)
+			return;
+
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+			root.getChildAt(0);
+		if(node.getUserObject() instanceof XmlTag)
+		{
+			Vector _path = new Vector();
+			expandTagAt(node,dot,_path);
+			_path.addElement(node);
+			_path.addElement(root);
+
+			Object[] path = new Object[_path.size()];
+			for(int i = 0; i < path.length; i++)
+				path[i] = _path.elementAt(path.length - i - 1);
+
+			TreePath treePath = new TreePath(path);
+			tree.expandPath(treePath);
+			tree.setSelectionPath(treePath);
+			tree.scrollPathToVisible(treePath);
+		}
+	} //}}}
+
+	//{{{ expandTagAt() method
+	private boolean expandTagAt(TreeNode node, int dot, Vector path)
+	{
+		int childCount = node.getChildCount();
+		Object userObject = ((DefaultMutableTreeNode)node).getUserObject();
+		XmlTag tag = (XmlTag)userObject;
+
+		if(childCount == 0 && userObject instanceof XmlTag)
+		{
+			// check if the caret in inside this tag
+			if(dot >= tag.start.getOffset()
+				&& (tag.end == null
+				|| dot <= tag.end.getOffset()))
+				return true;
+		}
+		else
+		{
+			// check if any of our children contain the caret
+			for(int i = childCount - 1; i >= 0; i--)
+			{
+				TreeNode _node = node.getChildAt(i);
+				if(expandTagAt(_node,dot,path))
+				{
+					path.addElement(_node);
+					return true;
+				}
+			}
+
+			// check if the caret in inside this tag
+			if(dot >= tag.start.getOffset()
+				&& (tag.end == null
+				|| dot <= tag.end.getOffset()))
+				return true;
+		}
+
+		return false;
+	} //}}}
+
+	//}}}
+
+	//{{{ Inner classes
+
+	//{{{ CustomTree class
+	class CustomTree extends JTree
+	{
+		CustomTree(TreeModel model)
+		{
+			super(model);
+		}
+
+		protected void processMouseEvent(MouseEvent evt)
+		{
+			switch(evt.getID())
+			{
+			//{{{ MOUSE_PRESSED...
+			case MouseEvent.MOUSE_PRESSED:
+				TreePath path = getPathForLocation(
+					evt.getX(),evt.getY());
+				if(path != null)
+				{
+					Object value = ((DefaultMutableTreeNode)path
+						.getLastPathComponent()).getUserObject();
+
+					if(value instanceof XmlTag)
+					{
+						XmlTag tag = (XmlTag)value;
+
+						JEditTextArea textArea = view.getTextArea();
+
+						textArea.setCaretPosition(tag.start.getOffset());
+						if(evt.getClickCount() == 2)
+						{
+							expandPath(path);
+							XmlActions.showEditTagDialog(view);
+							return;
+						}
+					}
+				}
+
+				super.processMouseEvent(evt);
+				break; //}}}
+			//{{{ MOUSE_EXITED...
+			case MouseEvent.MOUSE_EXITED:
+				view.getStatus().setMessage(null);
+				super.processMouseEvent(evt);
+				break; //}}}
+			default:
+				super.processMouseEvent(evt);
+				break;
+			}
+		}
+	} //}}}
+
+	//{{{ ActionHandler class
+	class ActionHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			XmlPlugin.getParser(view).parse(true);
+		}
+	} //}}}
+
+	//{{{ CaretHandler class
+	class CaretHandler implements CaretListener
+	{
+		public void caretUpdate(CaretEvent evt)
+		{
+			if(evt.getSource() == view.getTextArea())
+				expandTagWithDelay();
+		}
+	} //}}}
+
+	//{{{ MouseHandler class
+	class MouseHandler extends MouseMotionAdapter
+	{
+		public void mouseMoved(MouseEvent evt)
+		{
+			TreePath path = tree.getPathForLocation(
+				evt.getX(),evt.getY());
+			if(path == null)
+				view.getStatus().setMessage(null);
+			else
+			{
+				Object value = ((DefaultMutableTreeNode)path
+					.getLastPathComponent()).getUserObject();
+
+				if(value instanceof XmlTag)
+				{
+					view.getStatus().setMessage(((XmlTag)value)
+						.attributeString);
+				}
+			}
+		}
+	} //}}}
+
+	//{{{ Renderer class
+	class Renderer extends DefaultTreeCellRenderer
+	{
+		public Component getTreeCellRendererComponent(JTree tree,
+			Object value, boolean sel, boolean expanded,
+			boolean leaf, int row, boolean hasFocus)
+		{
+			super.getTreeCellRendererComponent(tree,value,sel,
+				expanded,leaf,row,hasFocus);
+
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+			Object nodeValue = node.getUserObject();
+			if(nodeValue instanceof XmlTag)
+			{
+				XmlTag tag = (XmlTag)node.getUserObject();
+
+				switch(showAttributes)
+				{
+				case 0:
+					setText(tag.name);
+					break;
+				case 1:
+					setText(tag.idAttributeString);
+					break;
+				case 2:
+					setText(tag.attributeString);
+					break;
+				}
+			}
+
+			setIcon(null);
+
+			return this;
+		}
+	} //}}}
+
+	//}}}
+}
---
+++ plugins/XML/tags/release-0-8/xml/parser/TagParser.java
@@ -0,0 +1,238 @@
+/*
+ * TagParser.java - a few very simple but fast methods used for tag highlighting
+ * and such
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2000 Scott Wyatt, 2001 Andre Kaplan
+ * Portions copyright (C) 2001, 2002 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml.parser;
+
+//{{{ Imports
+import java.util.*;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.textarea.Selection;
+//}}}
+
+public class TagParser
+{
+	public static final int T_STANDALONE_TAG = 0;
+	public static final int T_START_TAG = 1;
+	public static final int T_END_TAG = 2;
+
+	//{{{ getTagAtOffset() method
+	public static Tag getTagAtOffset(String text, int pos)
+	{
+		if(pos < 0 || pos > text.length())
+			return null;
+
+		// Get the last '<' before current position.
+		int startTag = text.lastIndexOf('<', pos - 1);
+		if(startTag == -1 || startTag + 2 >= text.length()) // at least 2 chars after '<'
+			return null;
+
+		int endTag = text.indexOf('>', startTag + 1) + 1;
+		if(endTag == -1 || endTag < pos)
+			return null;
+
+		int tagType = T_START_TAG;
+		int startTagName = startTag + 1;
+		if(text.charAt(startTagName) == '/')
+		{
+			tagType = T_END_TAG;
+			++startTagName;
+		}
+		else if(text.charAt(startTagName) == '?'
+			|| text.charAt(startTagName) == '!')
+		{
+			return null;
+		}
+
+		int endTagName = endTag - 1;
+		for(int i = startTagName; i < endTag - 1; i++)
+		{
+			char ch = text.charAt(i);
+			if(Character.isWhitespace(ch))
+			{
+				if(endTagName == endTag - 1)
+					endTagName = i;
+			}
+			else if(ch == '<')
+				return null;
+			else if(ch == '/' && i == endTag - 2)
+			{
+				endTagName = i;
+				tagType = T_STANDALONE_TAG;
+			}
+		}
+
+		Tag tag = new Tag();
+		tag.start = startTag;
+		tag.end   = endTag;
+		tag.tag   = text.substring(startTagName, endTagName);
+		tag.type  = tagType;
+
+		return tag;
+	} //}}}
+
+	//{{{ getMatchingTag() method
+	public static Tag getMatchingTag(String text, Tag tag)
+	{
+		if (tag.type == T_START_TAG)
+			return findEndTag(text, tag);
+		else if (tag.type == T_END_TAG)
+			return findStartTag(text, tag);
+		return null;
+	} //}}}
+
+	//{{{ findLastOpenTag() method
+	public static Tag findLastOpenTag(String text, int pos,
+		HashMap elementDecls)
+	{
+		Stack tagStack = new Stack();
+
+loop:		for (int i = text.lastIndexOf('<', pos);
+			i != -1; i = text.lastIndexOf('<', --i))
+		{
+			Tag tag = getTagAtOffset(text,i + 1);
+			if (tag == null)
+				continue;
+			else
+			{
+				ElementDecl decl = (ElementDecl)elementDecls.get(tag.tag);
+				if(tag.type == T_STANDALONE_TAG
+					|| (decl != null && decl.empty))
+				{
+					continue;
+				}
+				else if(tag.type == T_START_TAG)
+				{
+					if(tagStack.empty())
+						return tag;
+	
+					int unwindIndex = -1;
+	
+					for(int j = tagStack.size() - 1;
+						j >= 0; j--)
+					{
+						if(tagStack.get(j).equals(tag.tag))
+						{
+							unwindIndex = j;
+							break;
+						}
+					}
+
+					if(unwindIndex == -1)
+						return tag;
+					else
+					{
+						while(unwindIndex != tagStack.size())
+							tagStack.remove(unwindIndex);
+					}
+				}
+				else if(tag.type == T_END_TAG)
+				{
+					tagStack.push(tag.tag);
+				}
+			}
+		}
+
+		return null;
+	} //}}}
+
+	//{{{ Private members
+
+	//{{{ findEndTag() method
+	private static Tag findEndTag(String text, Tag startTag)
+	{
+		int tagCounter = 0;
+
+loop:		for (int i = text.indexOf('<', startTag.end);
+			i != -1; i = text.indexOf('<', ++i))
+		{
+			Tag tag = getTagAtOffset(text,i + 1);
+			if (tag == null)
+				continue;
+			else if(tag.tag.equals(startTag.tag))
+			{
+				if(tag.type == T_END_TAG)
+				{
+					if(tagCounter == 0)
+						return tag;
+					else
+						tagCounter--;
+				}
+				else if(tag.type == T_START_TAG)
+				{
+					tagCounter++;
+				}
+			}
+		}
+
+		return null;
+	} //}}}
+
+	//{{{ findStartTag() method
+	private static Tag findStartTag(String text, Tag endTag)
+	{
+		int tagCounter = 0;
+
+loop:		for (int i = text.lastIndexOf('<', endTag.start - 1);
+			i != -1; i = text.lastIndexOf('<', --i))
+		{
+			Tag tag = getTagAtOffset(text,i + 1);
+			if (tag == null)
+				continue;
+			else if(tag.tag.equals(endTag.tag))
+			{
+				if(tag.type == T_START_TAG)
+				{
+					if(tagCounter == 0)
+						return tag;
+					else
+						tagCounter++;
+				}
+				else if(tag.type == T_END_TAG)
+					tagCounter++;
+			}
+		}
+
+		return null;
+	} //}}}
+
+	//}}}
+
+	//{{{ Tag class
+	public static class Tag {
+		public String tag = null;
+		public int type = -1;
+		public int start = -1;
+		public int end = -1;
+
+		public String toString()
+		{
+			switch(type)
+			{
+			case T_START_TAG:
+				return "<" + tag + ">";
+			case T_STANDALONE_TAG:
+				return "<" + tag + " />";
+			case T_END_TAG:
+				return "</" + tag + ">";
+			default:
+				throw new InternalError();
+			}
+		}
+	} //}}}
+}
+
