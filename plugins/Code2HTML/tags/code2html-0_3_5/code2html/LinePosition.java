9f51dc1df3f4645e74647c0c2ca0fc1f128c0a2b
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-03-17 17:29:36 +0000

    This commit was manufactured by cvs2svn to create tag 'code2html-0_3_5'.

---
+++ plugins/Code2HTML/tags/code2html-0_3_5/code2html/LinePosition.java
@@ -0,0 +1,50 @@
+/*
+ * LinePosition.java
+ * Copyright (c) 2000, 2001, 2002 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+package code2html;
+
+import java.util.Vector;
+
+import org.gjt.sp.util.Log;
+
+
+public class LinePosition
+{
+    private int pos = 0;
+
+
+    public LinePosition() {}
+
+
+    public int getPos() {
+        return this.pos;
+    }
+
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+
+    public void incPos(int inc) {
+        this.pos += inc;
+    }
+}
+
