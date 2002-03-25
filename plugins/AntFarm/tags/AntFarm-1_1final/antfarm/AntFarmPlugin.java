292dccb64ac181f95d1aa72b4bac2739704f9784
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-03-25 08:23:01 +0000

    This commit was manufactured by cvs2svn to create tag 'AntFarm-1_1final'.

---
+++ plugins/AntFarm/tags/AntFarm-1_1final/antfarm/AntFarmPlugin.java
@@ -0,0 +1,164 @@
+/*
+ *  AntFarmPlugin.java - Plugin for running Ant builds from jEdit.
+ *  Copyright (C) 2001 Brian Knowles
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package antfarm;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+import java.util.*;
+import javax.swing.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.browser.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.io.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.util.*;
+
+import console.*;
+import plugin.integration.*;
+import errorlist.*;
+import projectviewer.*;
+
+public class AntFarmPlugin extends EditPlugin
+{
+
+	final static String NAME = "antfarm";
+	final static String OPTION_PREFIX = "options." + NAME + ".";
+	final static String ANT_HOME = "ant.home";
+	final static AntFarmShell ANT_SHELL = new AntFarmShell();
+
+	private static DefaultErrorSource _errorSource;
+
+
+	public static DefaultErrorSource getErrorSource()
+	{
+		return _errorSource;
+	}
+
+
+	static Console getConsole( View view )
+	{
+		return getConsole( view, true );
+	}
+
+
+	static Console getConsole( View view, boolean bringToFront )
+	{
+		DockableWindowManager mgr = view.getDockableWindowManager();
+		String CONSOLE = "console";
+		// Get the current console instance
+		Console console =
+			(Console) mgr.getDockable( CONSOLE );
+		if(console == null)
+		{
+			mgr.addDockableWindow( CONSOLE );
+			console = (Console) mgr.getDockable( CONSOLE );
+		}
+
+		console.setShell( AntFarmPlugin.ANT_SHELL );
+
+		if ( bringToFront )
+			view.getDockableWindowManager().showDockableWindow( CONSOLE );
+
+		return console;
+	}
+
+
+	static Properties getGlobalProperties()
+	{
+		Properties properties = new Properties();
+
+		String name;
+		int counter = 1;
+		while ( ( name = jEdit.getProperty( PropertiesOptionPane.PROPERTY + counter + PropertiesOptionPane.NAME ) ) != null ) {
+			properties.put( name, jEdit.getProperty( PropertiesOptionPane.PROPERTY + counter + PropertiesOptionPane.VALUE ) );
+			counter++;
+		}
+		return properties;
+	}
+
+	public void createMenuItems( Vector menuItems )
+	{
+		menuItems.addElement( GUIUtilities.loadMenuItem( "antfarm" ) );
+	}
+
+
+	public void createOptionPanes( OptionsDialog od )
+	{
+		OptionGroup grp = new OptionGroup( NAME );
+		grp.addOptionPane( new AntFarmOptionPane() );
+		grp.addOptionPane( new PropertiesOptionPane() );
+		od.addOptionGroup( grp );
+	}
+
+
+	public void start()
+	{
+		//IntegrationManager integration = new IntegrationManager( this );
+		//integration.addBridge( "projectviewer.ProjectPlugin", "antfarm.ProjectBridge" );
+
+		// initalize error source
+		_errorSource = new DefaultErrorSource( NAME );
+		ErrorSource.registerErrorSource(_errorSource);
+		
+		// check whether tools.jar is available on JDK 1.2 or higher:
+		if ( !MiscUtilities.isToolsJarAvailable() ) {
+			String warning = "This will cause problems when trying to use the modern and classic compilers.\n" +
+				"If you want AntFarm to work properly, please make sure tools.jar is\n" +
+				"in one of the above locations, and restart jEdit.";
+			Log.log( Log.WARNING, this, warning );
+			GUIUtilities.message(null, "tools-jar-missing", null);
+		}
+		
+		// Register the ant shell with the console plugin.
+		Shell.registerShell( ANT_SHELL );
+
+		// put the ant home in the environment for scripts to use.
+		System.getProperties().put( ANT_HOME, getAntFarmPath() );
+
+		jEdit.propertiesChanged();
+	}
+
+
+	String getAntFarmPath()
+	{
+		EditPlugin.JAR jar = getJAR();
+		String path = MiscUtilities.getParentOfPath( jar.getPath() );
+		Log.log( Log.DEBUG, this, "path: " + path );
+		return MiscUtilities.constructPath( path, "AntFarm" );
+	}
+
+
+	static boolean useProjectBridge()
+	{
+		return jEdit.getBooleanProperty(
+			AntFarmPlugin.OPTION_PREFIX + "use-project-bridge"
+			 );
+	}
+	
+	static boolean supressSubTargets()
+	{
+		return jEdit.getBooleanProperty(
+			AntFarmPlugin.OPTION_PREFIX + "supress-sub-targets"
+			 );
+	}
+}
+
---
+++ plugins/AntFarm/tags/AntFarm-1_1final/antfarm/AntPrintStream.java
@@ -0,0 +1,124 @@
+/*
+ *  AntPrintStream.java - Plugin for running Ant builds from jEdit.
+ *  Copyright (C) 2001 Brian Knowles
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package antfarm;
+import console.*;
+import errorlist.*;
+
+import java.io.*;
+import java.util.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.*;
+
+public class AntPrintStream extends PrintStream
+{
+
+	String cache = "";
+	View _view;
+
+
+	public AntPrintStream( PrintStream stream, View view )
+	{
+		super( stream );
+		_view = view;
+	}
+
+
+	public void println( String msg )
+	{
+// 		Log.log( Log.DEBUG, this, cache + msg );
+		parseLine( cache + msg );
+		resetCache();
+	}
+
+
+	public void println( char[] msg )
+	{
+		parseLine( cache + new String( msg ) );
+		resetCache();
+	}
+
+
+	public void print( String msg )
+	{
+		// Ant 1.4 org.apache.tools.ant.taskdefs.compilers.Javac13 sends output
+		// incorrectly. It must be tokenized and sent to println().
+		if ( msg.indexOf( "\n" ) > 0 ) {
+			StringTokenizer tokenizer = new StringTokenizer( msg, "\n" );
+			while ( tokenizer.hasMoreElements() )
+				println( tokenizer.nextToken() );
+		}
+		else {
+			cache += msg;
+		}
+	}
+
+	
+	public void write(byte b[], int i, int j)
+	{
+		  String msg = new String(b,i,j);
+		  print(msg);
+	}
+
+	
+	public void flush()
+	{
+		super.flush();
+		parseLine( cache );
+	}
+
+
+	private void resetCache()
+	{
+		cache = "";
+	}
+
+
+	private void parseLine( String line )
+	{
+		Console console =
+			(Console) _view.getDockableWindowManager().getDockable( "console" );
+
+		// should be project dir
+		String dir = System.getProperty( "user.dir" );
+
+		if ( console == null )
+			return;
+
+		int type = ConsolePlugin.parseLine( line, dir, AntFarmPlugin.getErrorSource() );
+
+		switch ( type ) {
+			case ErrorSource.ERROR:
+				console.print( console.getErrorColor(), line );
+				break;
+			case ErrorSource.WARNING:
+				console.print( console.getWarningColor(), line );
+				break;
+			default:
+				if ( line.trim().equals( "BUILD SUCCESSFUL" ) )
+					console.print( console.getInfoColor(), line );
+				else if ( line.trim().equals( "BUILD FAILED" ) )
+					console.print( console.getErrorColor(), line );
+				else
+					console.print( null, line );
+				break;
+		}
+	}
+
+}
+
