cb305d2728405fdfa2c2404931492a38ec3be748
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-05-26 18:41:41 +0000

    This commit was manufactured by cvs2svn to create tag 'xslt-0_2'.

---
+++ plugins/XSLT/tags/xslt-0_2/xslt/XPathTool.java
@@ -0,0 +1,190 @@
+/*
+ * XPathTool.java - GUI for evaluating XPath expressions
+ *
+ * Copyright (c) 2002 Greg Merrill
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+package xslt;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.MessageFormat;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.xml.transform.TransformerException;
+import org.apache.xerces.parsers.DOMParser;
+import org.apache.xpath.XPathAPI;
+import org.apache.xpath.objects.XObject;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * GUI for evaluating XPath expressions. 
+ */
+public class XPathTool extends JPanel {
+
+  public XPathTool (View view) {
+    super(new GridBagLayout());
+    this.view = view;
+
+    expressionPanel = new ExpressionPanel();
+    evaluatePanel = new EvaluatePanel();
+    resultsPanel = new ResultsPanel();
+
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.weightx = gbc.weighty = 1;
+    add(expressionPanel, gbc);
+
+    gbc = new GridBagConstraints();
+    gbc.gridy = 1;
+    gbc.fill = GridBagConstraints.NONE;
+    add(evaluatePanel, gbc);
+
+    gbc = new GridBagConstraints();
+    gbc.gridy = 2;
+    gbc.weightx = 1;
+    gbc.weighty = 4;
+    gbc.fill = GridBagConstraints.BOTH;
+    add(resultsPanel, gbc);
+  }
+
+  /**
+   * @param xObject XObject to be converted
+   * @return user-friendly string describing the supplied XObject
+   */
+  private String xObjectToFriendlyString (XObject xObject) throws TransformerException {
+    if (xObject.getType() == XObject.CLASS_NODESET) {
+      StringBuffer buf = new StringBuffer();
+      NodeList nodelist = xObject.nodelist();
+      buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.results.nodesFound"), 
+        new Object[]{new Integer(nodelist.getLength())}));
+      buf.append("\n");
+      for (int i=0; i < nodelist.getLength(); i++) {
+        buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.results.node"),
+          new Object[]{nodelist.item(i).getNodeName(), nodelist.item(i).getNodeValue()}));
+        buf.append("\n");
+      }
+      return buf.toString();
+    }
+    return xObject.toString();
+  }
+
+  /**
+   * @return "Evaluate" buttons
+   */
+  public JButton getEvaluateButton () { return evaluatePanel.button; }
+
+  /**
+   * Panel housing the "XPath Expression" label & text area
+   */
+  class ExpressionPanel extends JPanel {
+    ExpressionPanel () {
+      super(new BorderLayout());
+      JPanel topPanel = new JPanel(new BorderLayout());
+      topPanel.add(new JLabel(jEdit.getProperty("XPathTool.xpathExpression.label")), BorderLayout.WEST);
+      add(topPanel, BorderLayout.NORTH);
+      textArea = new JTextArea();
+      String text = jEdit.getProperty("XPathTool.lastExpression");
+      textArea.setText((text == null) ? "" : text);
+      textArea.getDocument().addDocumentListener(new DocumentListener() {
+        public void changedUpdate (DocumentEvent e) { updateProps(); }
+        public void insertUpdate (DocumentEvent e) { updateProps(); }
+        public void removeUpdate (DocumentEvent e) { updateProps(); }
+        private void updateProps () {
+          String text = ExpressionPanel.this.textArea.getText();
+          jEdit.setProperty("XPathTool.lastExpression", (text == null) ? "" : text);
+        }
+      });
+      add(new JScrollPane(textArea));
+    }
+    JTextArea textArea;
+  }
+
+  /**
+   * Panel housing the "Evaluate" button
+   */
+  class EvaluatePanel extends JPanel {
+    EvaluatePanel () {
+      button = new JButton(jEdit.getProperty("XPathTool.evaluate.button"));
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed (ActionEvent event) {
+          try {
+            DOMParser parser = new DOMParser();
+            parser.setFeature("http://xml.org/sax/features/validation", false);
+            Buffer buffer = view.getBuffer();
+            String text = buffer.getText(0, buffer.getLength());
+            InputSource inputSource = new InputSource(new StringReader(text));
+            inputSource.setSystemId(buffer.getFile().getPath());
+            parser.parse(inputSource);
+            XObject xObject = XPathAPI.eval(parser.getDocument(), expressionPanel.textArea.getText());
+            resultsPanel.textArea.setText(xObjectToFriendlyString(xObject));
+          }
+          catch (SAXException e) { // parse problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.bufferUnparseable"), XPathTool.this);
+          }
+          catch (IOException e) { // parse problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.bufferUnparseable"), XPathTool.this);
+          }
+          catch (TransformerException e) { // evaluation problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.expressionUnevaluateable"), XPathTool.this);
+          }
+          catch (Exception e) { // catch-all
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.unkownProblem"), XPathTool.this);
+          }
+        }
+      });
+      add(button);
+    }
+    private JButton button;
+  }
+
+  /**
+   * Panel housing the "Results" label & text area
+   */
+  class ResultsPanel extends JPanel {
+    ResultsPanel () {
+      super(new BorderLayout());
+      add(new JLabel(jEdit.getProperty("XPathTool.results.label")), BorderLayout.NORTH);
+      textArea = new JTextArea();
+      textArea.setEditable(false);
+      add(new JScrollPane(textArea));
+    }
+    JTextArea textArea;
+  }
+
+  private View view;
+  private ExpressionPanel expressionPanel;
+  private EvaluatePanel evaluatePanel;
+  private ResultsPanel resultsPanel;
+
+}
+
---
+++ plugins/XSLT/tags/xslt-0_2/xslt/XSLTPlugin.java
@@ -0,0 +1,71 @@
+/*
+ * XSLTPlugin.java - XSLT Plugin
+ *
+ * Copyright (c) 2002 Greg Merrill
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+package xslt;
+
+import java.awt.Component;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.MessageFormat;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.util.Log;
+
+/**
+ * EditPlugin implementation for the XSLT plugin.
+ */
+public class XSLTPlugin extends EditPlugin {
+
+  /**
+   * Register xerces as the SAX Parser provider
+   */
+  public void start () {
+    System.setProperty("javax.xml.transform.TransformerFactory", 
+      "org.apache.xalan.processor.TransformerFactoryImpl");
+    System.setProperty("javax.xml.parsers.SAXParserFactory", 
+      "org.apache.xerces.jaxp.SAXParserFactoryImpl");
+    String indentAmount = jEdit.getProperty("XSLTProcessor.transform.indentAmount");
+    XSLTUtilities.setIndentAmount(indentAmount);
+  }
+
+  /**
+   * Adds appropriate actions to the plugins menu
+   */
+  public void createMenuItems (Vector menuItems) {
+		menuItems.addElement(GUIUtilities.loadMenu("xslt-menu"));
+  }
+
+  /**
+   * Displays a user-friendly error message to go with the supplied exception.
+   */
+  static void processException (Exception e, String message, Component component) {
+    StringWriter writer = new StringWriter();
+    e.printStackTrace(new PrintWriter(writer));
+    Log.log(Log.DEBUG, Thread.currentThread(), writer.toString());
+    String msg = MessageFormat.format(jEdit.getProperty("xslt.error"),
+      new Object[]{message, e.getMessage()});
+    JOptionPane.showMessageDialog(component, msg.toString());
+  }
+
+}
+
