5b23975f6029d9491677020be2074fa5cc10afb4
spestov <spestov@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-06-01 09:19:29 +0000

    added two exceptions

---
+++ plugins/FTP/trunk/ftp/FtpException.java
@@ -0,0 +1,33 @@
+/*
+ * FtpException.java - FTP error
+ * Copyright (C) 2002 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package ftp;
+
+import com.fooware.net.*;
+import java.io.IOException;
+import org.gjt.sp.jedit.jEdit;
+
+public class FtpException extends IOException
+{
+	public FtpException(FtpResponse response)
+	{
+		super(jEdit.getProperty("ftperror",new String[] {
+			response.toString() }));
+	}
+}
