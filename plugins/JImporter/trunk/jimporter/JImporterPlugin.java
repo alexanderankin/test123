baac189c35036e3f4851c267d49e67b34bfbc318
Matt Flower <695370+MattFlower@users.noreply.github.com>
2002-09-13 17:06:01 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.

---
+++ plugins/JImporter/trunk/jimporter/JImporterPlugin.java
@@ -0,0 +1,80 @@
+/*
+ *  JImporterPlugin.java - Plugin for add java imports to the top of a java file.
+ *  Copyright (C) 2002 Matthew Flower (MattFlower@yahoo.com)
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package jimporter;
+
+import java.util.Vector;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.gui.OptionsDialog;
+
+/**
+ * This class is designed to follow the JEdit API specification for adding a 
+ * plugin, or extension to the JEdit Extension.  This class is responsible for
+ * defining which menus will appear and which option panes will appear.  There
+ * are also facilities for initialization and finalization code.
+ *
+ * @author Matthew Flower
+ * @since 08/29/2002
+ */
+public class JImporterPlugin extends EditPlugin {
+    /** 
+     * The name method can be used as a prefix for any properties stored in a
+     * properties file or by jEdit.setProperty().
+     */    
+    public String NAME = "jimporter";
+    /** 
+     * All options are stored with the prefix "options.jimporter".  This variable makes
+     * that knowledge available throughout the class.
+     */    
+    public String OPTION_PREFIX = "options." + NAME + "." ;
+    
+    /** 
+     * This method initializes the JImporter plugin.  Currently, it doesn't do any
+     * initialization in advance.
+     */    
+    public void start() {
+    }
+    
+    /** 
+     * This method could perform extra handling when the JImporter plugin was stopped.
+     * Right now it performs no special functions.
+     */    
+    public void stop() {
+    }
+    
+    /** 
+     * Add the JImporter menu items to JEdit.
+     *
+     * @param menuItems A vector of already existing menuItems that JImporter will add it's menu to.
+     */    
+    public void createMenuItems(Vector menuItems) {
+        menuItems.addElement(GUIUtilities.loadMenu("jimporter-menu"));
+    }
+    
+    /** 
+     * Create the JImporter options page that will be used to capture configuration
+     * information.
+     *
+     * @param dialog The dialog box to which JImporter will add it's 
+     * configuration-capturing option pane.
+     */    
+    public void createOptionPanes(OptionsDialog dialog) {
+        dialog.addOptionPane(new JImporterOptionPane());
+    }
+}
---
+++ plugins/JImporter/trunk/jimporter/options/OptionSaveListener.java
@@ -0,0 +1,26 @@
+/*
+ *  OptionSaveListener.java -   
+ *  Copyright (C) 2002  Matthew Flower (MattFlower@yahoo.com)
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package jimporter.options;
+
+import java.util.EventListener;
+
+public interface OptionSaveListener extends EventListener {
+    public void saveChanges();
+}
+
