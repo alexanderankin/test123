549be65b23dd110990663825d2de2e5cc2987c82
Rob McKinnon <3447+robmckinnon@users.noreply.github.com>
2002-10-21 04:38:31 +0000

    added_two_new_XPath_result_panels,_one_for_XPath_string-value_of_expression_result,_the_second_for_an_XML_fragment_view_of_the_expression_result

--- plugins/XSLT/trunk/xslt/XPathTool.java
+++ plugins/XSLT/trunk/xslt/XPathTool.java
@@ -2,6 +2,7 @@
  * XPathTool.java - GUI for evaluating XPath expressions
  *
  * Copyright (c) 2002 Greg Merrill
+ * Portions copyright (c) 2002 Robert McKinnon
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,44 +21,44 @@
 
 package xslt;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import org.apache.xerces.parsers.DOMParser;
+import org.apache.xpath.XPathAPI;
+import org.apache.xpath.objects.XObject;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.xml.transform.TransformerException;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.StringReader;
 import java.text.MessageFormat;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.xml.transform.TransformerException;
-import org.apache.xerces.parsers.DOMParser;
-import org.apache.xpath.XPathAPI;
-import org.apache.xpath.objects.XObject;
-import org.gjt.sp.jedit.Buffer;
-import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.View;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 /**
- * GUI for evaluating XPath expressions. 
+ * GUI for evaluating XPath expressions.
+ *
+ * @author Greg Merrill
+ * @author Robert McKinnon
  */
 public class XPathTool extends JPanel {
 
-  public XPathTool (View view) {
+  public XPathTool(View view) {
     super(new GridBagLayout());
     this.view = view;
 
     expressionPanel = new ExpressionPanel();
     evaluatePanel = new EvaluatePanel();
-    resultsPanel = new ResultsPanel();
+    summaryResultsPanel = new ResultsPanel(jEdit.getProperty("XPathTool.result.summary.label"));
+    xmlResultsPanel = new ResultsPanel(jEdit.getProperty("XPathTool.result.xml.string.label"));
+    xpathResultsPanel = new ResultsPanel(jEdit.getProperty("XPathTool.result.xpath.string.label"));
 
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.fill = GridBagConstraints.BOTH;
@@ -71,43 +72,231 @@ public class XPathTool extends JPanel {
 
     gbc = new GridBagConstraints();
     gbc.gridy = 2;
+    gbc.weightx = gbc.weighty = 1;
+    gbc.fill = GridBagConstraints.BOTH;
+    add(summaryResultsPanel, gbc);
+
+    gbc = new GridBagConstraints();
+    gbc.gridy = 3;
+    gbc.weightx = gbc.weighty = 1;
+    gbc.fill = GridBagConstraints.BOTH;
+    add(xpathResultsPanel, gbc);
+
+    gbc = new GridBagConstraints();
+    gbc.gridy = 4;
     gbc.weightx = 1;
     gbc.weighty = 4;
     gbc.fill = GridBagConstraints.BOTH;
-    add(resultsPanel, gbc);
+    add(xmlResultsPanel, gbc);
+  }
+
+
+  private String getNodeTypeString(short nodeType) {
+    switch(nodeType) {
+      case Node.ATTRIBUTE_NODE:
+        return "attribute";
+      case Node.CDATA_SECTION_NODE:
+        return "CDATA section";
+      case Node.COMMENT_NODE:
+        return "comment";
+      case Node.DOCUMENT_FRAGMENT_NODE:
+        return "document fragment";
+      case Node.DOCUMENT_NODE:
+        return "document";
+      case Node.DOCUMENT_TYPE_NODE:
+        return "document type";
+      case Node.ELEMENT_NODE:
+        return "element";
+      case Node.ENTITY_NODE:
+        return "entity";
+      case Node.ENTITY_REFERENCE_NODE:
+        return "entity reference";
+      case Node.NOTATION_NODE:
+        return "notation";
+      case Node.PROCESSING_INSTRUCTION_NODE:
+        return "processing instruction";
+      case Node.TEXT_NODE:
+        return "text";
+      default:
+        throw new IllegalArgumentException();
+    }
   }
 
   /**
    * @param xObject XObject to be converted
    * @return user-friendly string describing the supplied XObject
    */
-  private String xObjectToFriendlyString (XObject xObject) throws TransformerException {
-    if (xObject.getType() == XObject.CLASS_NODESET) {
-      StringBuffer buf = new StringBuffer();
+  private String getSummaryString(XObject xObject) throws TransformerException {
+    StringBuffer buf = new StringBuffer();
+
+    if(xObject.getType() == XObject.CLASS_NODESET) {
       NodeList nodelist = xObject.nodelist();
-      buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.results.nodesFound"), 
-        new Object[]{new Integer(nodelist.getLength())}));
+      Object[] messageArgs = new Object[]{new Integer(nodelist.getLength())};
+      buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.result.nodes.summary"), messageArgs));
       buf.append("\n");
-      for (int i=0; i < nodelist.getLength(); i++) {
-        buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.results.node"),
-          new Object[]{nodelist.item(i).getNodeName(), nodelist.item(i).getNodeValue()}));
+
+      for(int i = 0; i < nodelist.getLength(); i++) {
+        Node node = nodelist.item(i);
+        String nodeType = getNodeTypeString(node.getNodeType());
+        String value = node.getNodeValue();
+        value = (value == null) ? value : value.trim();
+
+        messageArgs = new Object[]{"" + i, nodeType, node.getNodeName(), value};
+        buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.result.node"), messageArgs));
         buf.append("\n");
       }
-      return buf.toString();
+
+    } else {
+      Object[] messageArgs = new Object[]{xObject.getTypeString().substring(1).toLowerCase()};
+      buf.append(MessageFormat.format(jEdit.getProperty("XPathTool.result.non-nodes.summary"), messageArgs));
+      buf.append("\n");
+      buf.append(xObject.toString());
     }
-    return xObject.toString();
+
+    return buf.toString();
   }
+
+
+  /**
+   * @param xObject XObject to be converted
+   * @return xml representing nodes in the supplied XObject, if it had nodes
+   */
+  private String getXmlString(XObject xObject) throws TransformerException {
+    StringBuffer buf = new StringBuffer("");
+
+    if(xObject.getType() == XObject.CLASS_NODESET) {
+      NodeList nodelist = xObject.nodelist();
+
+      for(int i = 0; i < nodelist.getLength(); i++) {
+        Node node = nodelist.item(i);
+        appendNodeString(node, buf, 0, false);
+      }
+    }
+
+    return buf.toString();
+  }
+
+
+  private void appendNodeString(Node node, StringBuffer buf, int indentLevel, boolean insideElement) {
+    if(node.getNodeType() == Node.ELEMENT_NODE) {
+      appendElementNode(node, buf, indentLevel);
+    } else if(node.getNodeType() == Node.TEXT_NODE) {
+      appendTextNode(node, buf, insideElement);
+    } else if(node.getNodeType() == Node.ATTRIBUTE_NODE) {
+      appendAttributeNode(node, buf);
+    } else if(node.getNodeType() == Node.DOCUMENT_NODE) {
+      appendNodeString(node.getFirstChild(), buf, indentLevel, true);
+    } else {
+      appendNodeString(node.getNextSibling(), buf, indentLevel, true);
+    }
+  }
+
+
+  private void appendTextNode(Node node, StringBuffer buf, boolean insideElement) {
+    String value = node.getNodeValue().trim();
+    if(value.length() > 0 || !insideElement) {
+      buf.append(value);
+    }
+    if(!insideElement) {
+      buf.append('\n');
+    }
+  }
+
+
+  private static final int NO_INDENT = -1;
+
+  private void appendElementNode(Node node, StringBuffer buf, int indentLevel) {
+    if(indentLevel != NO_INDENT) {
+      for(int i = 0; i < indentLevel; i++) {
+        buf.append("  ");
+      }
+    }
+    buf.append('<');
+    buf.append(node.getNodeName());
+
+    NamedNodeMap attributes = node.getAttributes();
+
+    if(attributes.getLength() > 0) {
+      for(int i = 0; i < attributes.getLength(); i++) {
+        Attr attribute = (Attr)attributes.item(i);
+        buf.append(' ');
+        appendAttribute(attribute, buf);
+      }
+    }
+
+    NodeList nodes = node.getChildNodes();
+
+    if(nodes.getLength() > 0) {
+      buf.append('>');
+
+      Node firstNode = nodes.item(0);
+
+      if(firstNode.getNodeType() == Node.TEXT_NODE && firstNode.getNodeValue().trim().length() > 0) {
+        appendTextNode(nodes.item(0), buf, true);
+        appendChildNodes(nodes, 1, buf, NO_INDENT);
+      } else {
+        buf.append(System.getProperty("line.separator"));
+        appendChildNodes(nodes, 0, buf, indentLevel);
+        for(int i = 0; i < indentLevel; i++) {
+          buf.append("  ");
+        }
+      }
+
+      buf.append("</");
+      buf.append(node.getNodeName());
+      buf.append('>');
+
+      if(indentLevel != NO_INDENT) {
+        buf.append(System.getProperty("line.separator"));
+      }
+    } else {
+      buf.append("/>");
+      if(indentLevel != NO_INDENT) {
+        buf.append(System.getProperty("line.separator"));
+      }
+    }
+  }
+
+
+  private void appendAttribute(Attr attribute, StringBuffer buf) {
+    buf.append(attribute.getName());
+    buf.append("=\"");
+    buf.append(attribute.getValue());
+    buf.append('\"');
+  }
+
+  private void appendAttributeNode(Node node, StringBuffer buf) {
+    buf.append(node.getNodeName());
+    buf.append("=\"");
+    buf.append(node.getNodeValue());
+    buf.append('\"');
+    buf.append(System.getProperty("line.separator"));
+  }
+
+  private void appendChildNodes(NodeList nodes, int index, StringBuffer buf, int indentLevel) {
+    if(indentLevel != NO_INDENT) {
+      indentLevel++;
+    }
+
+    for(int i = index; i < nodes.getLength(); i++) {
+      appendNodeString(nodes.item(i), buf, indentLevel, true);
+    }
+  }
+
 
   /**
    * @return "Evaluate" buttons
    */
-  public JButton getEvaluateButton () { return evaluatePanel.button; }
+  public JButton getEvaluateButton() {
+    return evaluatePanel.button;
+  }
+
 
   /**
    * Panel housing the "XPath Expression" label & text area
    */
   class ExpressionPanel extends JPanel {
-    ExpressionPanel () {
+    ExpressionPanel() {
       super(new BorderLayout());
       JPanel topPanel = new JPanel(new BorderLayout());
       topPanel.add(new JLabel(jEdit.getProperty("XPathTool.xpathExpression.label")), BorderLayout.WEST);
@@ -116,27 +305,38 @@ public class XPathTool extends JPanel {
       String text = jEdit.getProperty("XPathTool.lastExpression");
       textArea.setText((text == null) ? "" : text);
       textArea.getDocument().addDocumentListener(new DocumentListener() {
-        public void changedUpdate (DocumentEvent e) { updateProps(); }
-        public void insertUpdate (DocumentEvent e) { updateProps(); }
-        public void removeUpdate (DocumentEvent e) { updateProps(); }
-        private void updateProps () {
+        public void changedUpdate(DocumentEvent e) {
+          updateProps();
+        }
+
+        public void insertUpdate(DocumentEvent e) {
+          updateProps();
+        }
+
+        public void removeUpdate(DocumentEvent e) {
+          updateProps();
+        }
+
+        private void updateProps() {
           String text = ExpressionPanel.this.textArea.getText();
           jEdit.setProperty("XPathTool.lastExpression", (text == null) ? "" : text);
         }
       });
       add(new JScrollPane(textArea));
     }
+
     JTextArea textArea;
   }
+
 
   /**
    * Panel housing the "Evaluate" button
    */
   class EvaluatePanel extends JPanel {
-    EvaluatePanel () {
+    EvaluatePanel() {
       button = new JButton(jEdit.getProperty("XPathTool.evaluate.button"));
       button.addActionListener(new ActionListener() {
-        public void actionPerformed (ActionEvent event) {
+        public void actionPerformed(ActionEvent event) {
           try {
             DOMParser parser = new DOMParser();
             parser.setFeature("http://xml.org/sax/features/validation", false);
@@ -146,45 +346,52 @@ public class XPathTool extends JPanel {
             inputSource.setSystemId(buffer.getFile().getPath());
             parser.parse(inputSource);
             XObject xObject = XPathAPI.eval(parser.getDocument(), expressionPanel.textArea.getText());
-            resultsPanel.textArea.setText(xObjectToFriendlyString(xObject));
-          }
-          catch (SAXException e) { // parse problem
-            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.bufferUnparseable"), XPathTool.this);
-          }
-          catch (IOException e) { // parse problem
-            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.bufferUnparseable"), XPathTool.this);
-          }
-          catch (TransformerException e) { // evaluation problem
-            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.expressionUnevaluateable"), XPathTool.this);
-          }
-          catch (Exception e) { // catch-all
-            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.results.error.unkownProblem"), XPathTool.this);
+
+            summaryResultsPanel.textArea.setText(getSummaryString(xObject));
+            xpathResultsPanel.textArea.setText(xObject.str());
+            xmlResultsPanel.textArea.setText(getXmlString(xObject));
+
+            summaryResultsPanel.textArea.setCaretPosition(0);
+            xpathResultsPanel.textArea.setCaretPosition(0);
+            xmlResultsPanel.textArea.setCaretPosition(0);
+          } catch(SAXException e) { // parse problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.result.error.bufferUnparseable"), XPathTool.this);
+          } catch(IOException e) { // parse problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.result.error.bufferUnparseable"), XPathTool.this);
+          } catch(TransformerException e) { // evaluation problem
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.result.error.expressionUnevaluateable"), XPathTool.this);
+          } catch(Exception e) { // catch-all
+            XSLTPlugin.processException(e, jEdit.getProperty("XPathTool.result.error.unkownProblem"), XPathTool.this);
           }
         }
       });
       add(button);
     }
+
     private JButton button;
   }
+
 
   /**
    * Panel housing the "Results" label & text area
    */
   class ResultsPanel extends JPanel {
-    ResultsPanel () {
+    ResultsPanel(String label) {
       super(new BorderLayout());
-      add(new JLabel(jEdit.getProperty("XPathTool.results.label")), BorderLayout.NORTH);
+      add(new JLabel(label), BorderLayout.NORTH);
       textArea = new JTextArea();
       textArea.setEditable(false);
       add(new JScrollPane(textArea));
     }
+
     JTextArea textArea;
   }
 
   private View view;
   private ExpressionPanel expressionPanel;
   private EvaluatePanel evaluatePanel;
-  private ResultsPanel resultsPanel;
+  private ResultsPanel summaryResultsPanel;
+  private ResultsPanel xmlResultsPanel;
+  private ResultsPanel xpathResultsPanel;
 
 }
-
