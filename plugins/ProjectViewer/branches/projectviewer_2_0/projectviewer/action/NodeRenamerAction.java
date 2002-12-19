dc180c8b8b9e137467e49a801c56c331b08048b4
vanza <vanza@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-12-19 23:26:11 +0000

    implemented renaming; needs lots of testing for things that are not files

--- plugins/ProjectViewer/branches/projectviewer_2_0/projectviewer/action/NodeRenamerAction.java
+++ plugins/ProjectViewer/branches/projectviewer_2_0/projectviewer/action/NodeRenamerAction.java
@@ -19,23 +19,26 @@
 package projectviewer.action;
 
 //{{{ Imports
+import java.io.File;
+import java.util.Iterator;
 import java.awt.event.ActionEvent;
 
 import javax.swing.Icon;
-import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.Buffer;
 
 import projectviewer.ProjectViewer;
 import projectviewer.ProjectManager;
 import projectviewer.vpt.VPTNode;
+import projectviewer.vpt.VPTFile;
 import projectviewer.vpt.VPTProject;
+import projectviewer.vpt.VPTDirectory;
 //}}}
 
 /**
- *	Action that when executed creating a new project or editing an existing
- *	one.
+ *	Action for renaming files, directories and projects.
  *
  *	@author		Marcelo Vanzin
  *	@version	$Id$
@@ -55,22 +58,130 @@ public class NodeRenamerAction extends Action {
 	} //}}}
 	
 	//{{{ actionPerformed(ActionEvent) method
-	/** Creates a new project. */
+	/** Renames a node. */
 	public void actionPerformed(ActionEvent e) {
+		VPTNode node = viewer.getSelectedNode();
+		boolean isValid = false;
+		String newName = null;
 		
+		while (!isValid) {
+			newName = JOptionPane.showInputDialog(viewer,
+				jEdit.getProperty("projectviewer.action.rename.message"),
+				jEdit.getProperty("projectviewer.action.rename.title"),
+				JOptionPane.PLAIN_MESSAGE);
+			
+			if (newName == null || newName.length() == 0) {
+				return;
+			}
+			
+			// checks the input
+			if (node.isFile() || node.isDirectory()) {
+				if (newName.indexOf('/') != -1 || newName.indexOf('\\') != -1) {
+					JOptionPane.showMessageDialog(viewer,
+						jEdit.getProperty("projectviewer.action.rename.file_error"),
+						jEdit.getProperty("projectviewer.action.rename.title"),
+						JOptionPane.ERROR_MESSAGE);
+				} else {
+					isValid = true;
+				}
+			} else if (node.isProject()) {
+				if (ProjectManager.getInstance().hasProject(newName)) {
+					JOptionPane.showMessageDialog(viewer,
+						jEdit.getProperty("projectviewer.project.options.name_exists"),
+						jEdit.getProperty("projectviewer.action.rename.title"),
+						JOptionPane.ERROR_MESSAGE);
+				} else {
+					isValid = true;
+				}
+			}
+		}
+
+		// renames the node		
+		if (node.isFile()) {
+			VPTFile f = (VPTFile) node;
+
+			// updates all files from the old directory to point to the new one
+			while (!node.isProject()) {
+				node = (VPTNode) node.getParent();
+			}
+
+			((VPTProject)node).unregisterFile(f);
+			
+			if (!renameFile(f, new File(f.getFile().getParent(), newName))) {
+				JOptionPane.showMessageDialog(viewer,
+						jEdit.getProperty("projectviewer.action.rename.rename_error"),
+						jEdit.getProperty("projectviewer.action.rename.title"),
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			((VPTProject)node).registerFile(f);
+			ProjectViewer.nodeChanged(f);
+		} else if (node.isDirectory()) {
+			VPTDirectory dir = (VPTDirectory) node;
+			String oldDir = dir.getFile().getAbsolutePath();
+			String newDir = dir.getFile().getParent() + File.separator + newName;
+			File newFile = new File(newDir);
+
+			if (!dir.getFile().renameTo(newFile)) {
+				JOptionPane.showMessageDialog(viewer,
+						jEdit.getProperty("projectviewer.action.rename.rename_error"),
+						jEdit.getProperty("projectviewer.action.rename.title"),
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			dir.setFile(newFile);
+			
+			// updates all files from the old directory to point to the new one
+			while (!node.isProject()) {
+				node = (VPTNode) node.getParent();
+			}
+			
+			for (Iterator i = ((VPTProject)node).getFiles().iterator(); i.hasNext(); ) {
+				VPTFile f = (VPTFile) i.next();
+				if (f.getNodePath().startsWith(oldDir)) {
+					renameFile(f, new File(dir.getFile(), f.getName()));
+				}
+			}
+			ProjectViewer.nodeChanged(dir);
+		} else if (node.isProject()) {
+			String oldName = node.getName();
+			node.setName(newName);
+			ProjectManager.getInstance().renameProject(oldName, newName);
+			ProjectViewer.nodeChanged(node);
+		}
+	
 	} //}}}
 
 	//{{{ prepareForNode(VPTNode) method
 	/** Disable action only for the root node. */
 	public void prepareForNode(VPTNode node) {
-		if (cmItem != null) {
-			if (node != null) {
-				cmItem.setVisible(!node.isRoot());
-			} else {
-				cmItem.setVisible(false);
-			}
-		}
+		cmItem.setVisible(node != null && 
+			(node.isFile() || node.isDirectory() || node.isProject()));
 	} //}}}
 	
+	//{{{ renameFile(VPTFile, String) method
+	/** Renames a file and tries not to mess up jEdit's current buffer. */
+	private boolean renameFile(VPTFile f, File newFile) {
+		Buffer b = jEdit.getActiveView().getBuffer();
+		if (b.getPath().equals(f.getNodePath())) {
+			b = null;
+		}
+		boolean open = f.isOpened();
+		f.close();
+		if (!f.getFile().renameTo(newFile)) {
+			return false;
+		}
+		f.setFile(newFile);
+		if (open) {
+			// this is an ugly hack to avoid "file has been modified on
+			// disk" warnings that shouldn't happen, but do.
+			try { Thread.sleep(1); } catch (Exception e) { }
+			f.open();
+			if (b != null) 
+				jEdit.getActiveView().setBuffer(b);
+		}
+		return true;
+	} //}}}
 }
 
