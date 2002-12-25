c3abf4dd7719ec04ee2dfe27a6ca52f755b202df
cyu <cyu@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2002-12-25 06:41:01 +0000

    initial commit

---
+++ plugins/Docker/trunk/docker/DockHandler.java
@@ -0,0 +1,65 @@
+/*
+ * DockerPlugin.java
+ * :tabSize=3:indentSize=3:noTabs=true:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2001, 2002 Calvin Yu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package docker;
+
+import java.awt.Component;
+import org.gjt.sp.jedit.gui.PanelWindowContainer;
+
+/**
+ * A handler for handling the collapsing of a particular dock.
+ */
+public class DockHandler
+{
+
+   private PanelWindowContainer dock;
+   private String dockName;
+   private Component dockComponent;
+
+   /**
+    * Create a new <code>DockHandler</code>.
+    */
+   public DockHandler(String aDockName,
+                      PanelWindowContainer aDock,
+                      Component aDockComponent)
+   {
+      dock = aDock;
+      dockName = aDockName;
+      dockComponent = aDockComponent;
+   }
+
+   /**
+    * Detach this handler from the dock.
+    */
+   public void detach()
+   {
+   }
+
+   /**
+    * Collapse this dock.
+    */
+   public void collapse()
+   {
+      dock.show(null);
+   }
+
+}
