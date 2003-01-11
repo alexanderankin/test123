168b02f41dfa54fd33414824691432d398a61008
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-01-11 18:53:24 +0000

    This commit was manufactured by cvs2svn to create tag 'release-3-4-again'.

---
+++ plugins/Console/tags/release-3-4-again/console/ConsoleBeanShell.java
@@ -0,0 +1,90 @@
+/*
+ * ConsoleBeanShell.java - Executes commands in jEdit's BeanShell interpreter
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2000, 2001 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package console;
+
+//{{{ Imports
+import bsh.EvalError;
+import bsh.NameSpace;
+import org.gjt.sp.jedit.BeanShell;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.util.Log;
+//}}}
+
+public class ConsoleBeanShell extends Shell
+{
+	//{{{ ConsoleBeanShell constructor
+	public ConsoleBeanShell()
+	{
+		super("BeanShell");
+	} //}}}
+
+	//{{{ printInfoMessage() method
+	public void printInfoMessage(Output output)
+	{
+		output.print(null,jEdit.getProperty("console.beanshell.info"));
+	} //}}}
+
+	//{{{ execute() method
+	public void execute(Console console, Output output, String command)
+	{
+		View view = console.getView();
+
+		NameSpace ns = org.gjt.sp.jedit.BeanShell.getNameSpace();
+		try
+		{
+			ns.setVariable("console",console);
+			ns.setVariable("output",output);
+			Object retVal = BeanShell._eval(view,
+				BeanShell.getNameSpace(),command);
+
+			if(retVal != null)
+			{
+				ns.setVariable("retVal",retVal);
+				BeanShell._eval(view,BeanShell.getNameSpace(),
+					"print(retVal);");
+				ns.setVariable("retVal",null);
+			}
+
+			ns.setVariable("console",null);
+			ns.setVariable("output",null);
+		}
+		catch(Exception e)
+		{
+			output.print(console.getErrorColor(),e.toString());
+		}
+
+		output.commandDone();
+	} //}}}
+
+	//{{{ stop() method
+	public void stop(Console console)
+	{
+	} //}}}
+
+	//{{{ waitFor() method
+	public boolean waitFor(Console console)
+	{
+		return true;
+	} //}}}
+}
