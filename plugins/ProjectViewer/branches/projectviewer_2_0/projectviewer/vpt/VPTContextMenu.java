43290ff8813f76e1d06d6e0f8d7bad139aecfad2
Marcelo Vanzin <1694083+vanzin@users.noreply.github.com>
2003-01-21 20:32:21 +0000

    some cleanup; saner way of registering "internal" actions.

--- plugins/ProjectViewer/branches/projectviewer_2_0/projectviewer/vpt/VPTContextMenu.java
+++ plugins/ProjectViewer/branches/projectviewer_2_0/projectviewer/vpt/VPTContextMenu.java
@@ -19,33 +19,23 @@
 package projectviewer.vpt;
 
 //{{{ Imports
-import java.io.File;
 import java.util.Iterator;
 import java.util.ArrayList;
-import java.util.Hashtable;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 
 import javax.swing.JTree;
-import javax.swing.JMenuItem;
-import javax.swing.JMenu;
 import javax.swing.JPopupMenu;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.tree.TreePath;
+import javax.swing.SwingUtilities;
 
-// Import jEdit
-import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.Buffer;
-
-// Import ProjectViewer
 import projectviewer.ProjectViewer;
 
 import projectviewer.action.Action;
 import projectviewer.action.SearchAction;
 import projectviewer.action.ArchiveAction;
+import projectviewer.action.ReimportAction;
 import projectviewer.action.FileImportAction;
 import projectviewer.action.EditProjectAction;
 import projectviewer.action.NodeRemoverAction;
@@ -53,7 +43,6 @@ import projectviewer.action.NodeRenamerAction;
 import projectviewer.action.OpenWithAppAction;
 import projectviewer.action.LaunchBrowserAction;
 
-import projectviewer.config.ProjectViewerConfig;
 import projectviewer.config.AppLauncher;
 //}}}
 
@@ -67,12 +56,27 @@ import projectviewer.config.AppLauncher;
 public class VPTContextMenu extends MouseAdapter {
 
 	//{{{ Static Members
-	
+
 	private static final ArrayList actions = new ArrayList();
+	private static final ArrayList intActions = new ArrayList();
 	private static long lastMod = System.currentTimeMillis();
+
+	/** Initializes the internal action list. */
+	static {
+		intActions.add(new EditProjectAction());
+		intActions.add(new FileImportAction());
+		intActions.add(new NodeRemoverAction(false));
+		intActions.add(new NodeRemoverAction(true));
+		intActions.add(new NodeRenamerAction());
+		intActions.add(new LaunchBrowserAction());
+		intActions.add(new OpenWithAppAction());
+		intActions.add(new SearchAction());
+		intActions.add(new ReimportAction());
+		intActions.add(new ArchiveAction());
+	}
 	
 	//{{{ registerAction(Action) method
-	/** 
+	/**
 	 *	Adds an action to be shown on the context menu. Actions are shown in the
 	 *	same order as they are registered.
 	 */
@@ -80,27 +84,26 @@ public class VPTContextMenu extends MouseAdapter {
 		actions.add(action);
 		lastMod = System.currentTimeMillis();
 	} //}}}
-	
+
 	//{{{ unregisterAction(Action) method
 	/** Removes an action from the context menu. */
 	public static void unregisterAction(Action action) {
 		actions.remove(action);
 		lastMod = System.currentTimeMillis();
 	} //}}}
-	
+
 	//}}}
-	
+
 	//{{{ Instance Variables
 	private final ProjectViewer viewer;
-	private AppLauncher appList;
-	private JPopupMenu popupMenu;
-  
-	private ArrayList	internalActions;
+	private final AppLauncher appList;
+	private final JPopupMenu popupMenu;
+	private final ArrayList internalActions;
 	private long pmLastBuilt;
 	//}}}
-	
+
 	//{{{ Constructors
-	
+
 	/**
 	 *  Constructs a listener that will ask the provided viewer instance for
 	 *  information about the nodes clicked.
@@ -112,11 +115,11 @@ public class VPTContextMenu extends MouseAdapter {
 		popupMenu = new JPopupMenu();
 		loadGUI();
 	}
-	
+
 	//}}}
-	
+
 	//{{{ Event Handling
-	
+
 	//{{{ mousePressed() method
 	/** Context-menus are shown on the "pressed" event. */
 	public void mousePressed(MouseEvent me) {
@@ -137,7 +140,7 @@ public class VPTContextMenu extends MouseAdapter {
 			handleMouseEvent(me);
 		}
 	} //}}}
-	
+
 	//{{{ mouseReleased() method
 	/** Context-menus are shown on the "pressed" event. */
 	public void mouseReleased(MouseEvent me) {
@@ -145,78 +148,44 @@ public class VPTContextMenu extends MouseAdapter {
 			handleMouseEvent(me);
 		}
 	} //}}}
-	
+
 	//}}}
-	
+
 	//{{{ Private Methods
-	
+
 	//{{{ handleMouseEvent() method
 	/** Handles the mouse event internally. */
 	private void handleMouseEvent(MouseEvent me) {
 		JTree tree = viewer.getCurrentTree();
-		
+
 		if (tree.getSelectionCount() == 0) {
 			return;
 		} else {
-			prepareMenu( tree.getSelectionCount() > 1 ? null : viewer.getSelectedNode() ); 
+			prepareMenu( tree.getSelectionCount() > 1 ? null : viewer.getSelectedNode() );
 			popupMenu.show(me.getComponent(), me.getX(), me.getY());
 		}
 	} //}}}
-	
+
 	//{{{ loadGUI() method
 	/** Constructs the menus' GUI. */
 	private void loadGUI() {
 		internalActions.clear();
 		popupMenu.removeAll();
-		
-		Action a = new EditProjectAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
 
-		a = new FileImportAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
+		Action a;
+		
+		for (Iterator it = intActions.iterator(); it.hasNext(); ) {
+			a = (Action) it.next();
+			a = (Action) a.clone();
+			a.setViewer(viewer);
+			internalActions.add(a);
+			popupMenu.add(a.getMenuItem());
+		}
 
-		a = new NodeRemoverAction(false);
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
 
-		a = new NodeRemoverAction(true);
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-		
-		a = new NodeRenamerAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-
-		a = new LaunchBrowserAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-		
-		a = new OpenWithAppAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-		
-		a = new SearchAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-		
-		a = new ArchiveAction();
-		a.setViewer(viewer);
-		popupMenu.add(a.getMenuItem());
-		internalActions.add(a);
-		
 		if (actions.size() > 0)
 			popupMenu.addSeparator();
-		
+
 		for (Iterator it = actions.iterator(); it.hasNext(); ) {
 			a = (Action) it.next();
 			a = (Action) a.clone();
@@ -224,10 +193,10 @@ public class VPTContextMenu extends MouseAdapter {
 			internalActions.add(a);
 			popupMenu.add(a.getMenuItem());
 		}
-		
+
 		pmLastBuilt = System.currentTimeMillis();
 	} //}}}
-	
+
 	//{{{ prepareMenu(VPTNode) method
 	/**
 	 *	Prepares the context menu for the given node. Shows only menu items
@@ -236,17 +205,18 @@ public class VPTContextMenu extends MouseAdapter {
 	 *	nodes are selected, and chooses the appropriate entries.
 	 */
 	private void prepareMenu(VPTNode selectedNode) {
-		
+
 		if (pmLastBuilt < lastMod) {
 			loadGUI();
 		}
-		
+
 		for (Iterator it = internalActions.iterator(); it.hasNext(); ) {
 			((Action)it.next()).prepareForNode(selectedNode);
 		}
-		
+
 	} //}}}
-	
+
 	//}}}
 
 }
+
