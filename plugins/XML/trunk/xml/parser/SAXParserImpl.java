7fd9369dc781e345ee8b1eb6099f034d98548c33
Slava Pestov <sviatoslav.pestov@gmail.com>
2003-03-04 20:35:52 +0000

    debug message

--- plugins/XML/trunk/xml/parser/SAXParserImpl.java
+++ plugins/XML/trunk/xml/parser/SAXParserImpl.java
@@ -433,7 +433,10 @@ public class SAXParserImpl extends XmlParser
 			}
 
 			if(!buffer.getPath().equals(currentURI))
+			{
+				Log.log(Log.DEBUG,this,"mismatch: " + buffer.getPath() + " : " + currentURI);
 				return;
+			}
 
 			buffer.readLock();
 
