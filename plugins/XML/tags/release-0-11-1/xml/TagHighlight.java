7ba019825640a3c1972d3c7029050e30c52a05b8
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-03-08 23:28:07 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-11-1'.

---
+++ plugins/XML/tags/release-0-11-1/xml/TagHighlight.java
@@ -0,0 +1,313 @@
+/*
+ * TagHighlight.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (c) 2000 Andre Kaplan
+ * Portions copyright (c) 2002 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml;
+
+//{{{ Imports
+import javax.swing.event.*;
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.awt.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.buffer.BufferChangeAdapter;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.*;
+import sidekick.SideKickPlugin;
+import xml.parser.*;
+//}}}
+
+public class TagHighlight extends TextAreaExtension
+{
+	//{{{ TagHighlight constructor
+	public TagHighlight(View view)
+	{
+		this.view = view;
+		this.textArea = view.getTextArea();
+		this.buffer = view.getBuffer();
+
+		bufferHandler = new BufferHandler();
+		caretHandler = new CaretHandler();
+
+		timer = new Timer(0,new ActionListener()
+		{
+			public void actionPerformed(ActionEvent evt)
+			{
+				updateHighlight();
+			}
+		});
+
+		textArea.getPainter().addExtension(this);
+
+		buffer.addBufferChangeListener(bufferHandler);
+		textArea.addCaretListener(caretHandler);
+		updateHighlightWithDelay();
+
+		returnValue = new Point();
+	} //}}}
+
+	//{{{ dispose() method
+	public void dispose()
+	{
+		timer.stop();
+		buffer.removeBufferChangeListener(bufferHandler);
+		textArea.getPainter().removeExtension(this);
+	} //}}}
+
+	//{{{ paintValidLine() method
+	public void paintValidLine(Graphics2D gfx, int screenLine,
+		int physicalLine, int start, int end, int y)
+	{
+		if(match != null)
+			paintHighlight(gfx,screenLine,physicalLine,start,end,y,match);
+	} //}}}
+
+	//{{{ propertiesChanged() method
+	public static void propertiesChanged()
+	{
+		tagHighlightColor = jEdit.getColorProperty(
+			"xml.tag-highlight-color");
+	} //}}}
+
+	//{{{ Private members
+	private static Color tagHighlightColor;
+
+	//{{{ Instance variables
+	private Timer timer;
+
+	private BufferHandler bufferHandler;
+	private CaretHandler caretHandler;
+	private TagParser.Tag current;
+	private TagParser.Tag match;
+	private boolean bufferChanged;
+
+	private View view;
+	private JEditTextArea textArea;
+	private Buffer buffer;
+
+	private Point returnValue;
+	//}}}
+
+	//{{{ paintHighlight() method
+	private void paintHighlight(Graphics gfx, int screenLine, int physicalLine,
+		int start, int end, int y, TagParser.Tag tag)
+	{
+		if(tag.start >= end || tag.end < start)
+			return;
+
+		int tagStartLine = textArea.getScreenLineOfOffset(tag.start);
+		int tagEndLine = textArea.getScreenLineOfOffset(tag.end);
+
+		FontMetrics fm = textArea.getPainter().getFontMetrics();
+		int height = fm.getHeight();
+
+		int x1, x2;
+
+		if(tagStartLine == screenLine)
+		{
+			x1 = tag.start - buffer.getLineStartOffset(
+				buffer.getLineOfOffset(tag.start));
+		}
+		else
+			x1 = 0;
+
+		if(tagEndLine == screenLine)
+		{
+			x2 = tag.end - buffer.getLineStartOffset(
+				buffer.getLineOfOffset(tag.end));
+		}
+		else
+		{
+			x2 = textArea.getScreenLineEndOffset(screenLine)
+				- textArea.getScreenLineStartOffset(screenLine);
+		}
+
+		x1 = textArea.offsetToXY(physicalLine,x1,returnValue).x;
+		x2 = textArea.offsetToXY(physicalLine,x2,returnValue).x;
+
+		gfx.setColor(tagHighlightColor);
+
+		gfx.drawLine(x1,y,x1,y + height - 1);
+		gfx.drawLine(x2,y,x2,y + height - 1);
+
+		if(tagStartLine == screenLine || screenLine == 0)
+			gfx.drawLine(x1,y,x2,y);
+		else
+		{
+			int prevX1, prevX2;
+
+			if(tagStartLine == screenLine - 1)
+			{
+				prevX1 = tag.start - buffer.getLineStartOffset(
+					buffer.getLineOfOffset(tag.start));
+			}
+			else
+				prevX1 = 0;
+
+			prevX2 = textArea.getScreenLineEndOffset(screenLine - 1)
+				- textArea.getScreenLineStartOffset(screenLine - 1);
+
+			prevX1 = textArea.offsetToXY(physicalLine - 1,prevX1,returnValue).x;
+			prevX2 = textArea.offsetToXY(physicalLine - 1,prevX2,returnValue).x;
+
+			gfx.drawLine(Math.min(x1,prevX1),y,
+				Math.max(x1,prevX1),y);
+			gfx.drawLine(Math.min(x2,prevX2),y,
+				Math.max(x2,prevX2),y);
+		}
+
+		if(tagEndLine == screenLine)
+			gfx.drawLine(x1,y + height - 1,x2,y + height - 1);
+	} //}}}
+
+	//{{{ updateHighlightWithDelay() method
+	private void updateHighlightWithDelay()
+	{
+		if(timer.isRunning())
+			timer.stop();
+
+		if(!buffer.isLoaded())
+			return;
+
+		timer.setInitialDelay(300);
+		timer.setRepeats(false);
+		timer.start();
+	} //}}}
+
+	//{{{ updateHighlight() method
+	private void updateHighlight()
+	{
+		int caret = textArea.getCaretPosition();
+
+		if(bufferChanged || current == null
+			|| caret <= current.start
+			|| caret >= current.end)
+		{
+
+			if(match != null)
+			{
+				if(match.start < buffer.getLength()
+					&& match.end <= buffer.getLength())
+				{
+					textArea.invalidateLineRange(
+						textArea.getLineOfOffset(match.start),
+						textArea.getLineOfOffset(match.end)
+					);
+				}
+
+				match = null;
+			}
+
+			if(XmlPlugin.isDelegated(textArea))
+				return;
+
+			String text = textArea.getText();
+
+			current = TagParser.getTagAtOffset(text,caret);
+
+			if(current == null)
+			{
+				match = null;
+			}
+			else
+			{
+				match = TagParser.getMatchingTag(text,current);
+
+				if(match != null)
+				{
+					int offset = buffer.getLineOfOffset(match.start);
+
+					int line = textArea.physicalToVirtual(
+						offset);
+					if(line < textArea.getFirstLine()
+						|| line >= textArea.getFirstLine()
+						+ textArea.getVisibleLines() - 1)
+					{
+						view.getStatus().setMessageAndClear(
+							jEdit.getProperty("view.status.bracket",
+							new String[] { text.substring(
+							match.start,match.end) }));
+					}
+
+					textArea.invalidateLineRange(
+						buffer.getLineOfOffset(match.start),
+						buffer.getLineOfOffset(match.end)
+					);
+				}
+			}
+		}
+
+		bufferChanged = false;
+	} //}}}
+
+	//}}}
+
+	//{{{ BufferHandler class
+	class BufferHandler extends BufferChangeAdapter
+	{
+		public void contentInserted(Buffer buffer, int startLine,
+			int offset, int numLines, int length)
+		{
+			bufferChanged = true;
+			if(match != null)
+			{
+				if(match.start >= offset)
+					match.start += length;
+				if(match.end >= offset)
+					match.end += length;
+			}
+
+			updateHighlightWithDelay();
+		}
+
+		public void contentRemoved(Buffer buffer, int startLine,
+			int offset, int numLines, int length)
+		{
+			bufferChanged = true;
+			if(match != null)
+			{
+				if(match.start >= offset)
+				{
+					if(match.start < offset + length)
+						match.start = offset;
+					else
+						match.start -= length;
+				}
+				if(match.end >= offset)
+				{
+					if(match.end < offset + length)
+						match.end = offset;
+					else
+						match.end -= length;
+				}
+			}
+
+			updateHighlightWithDelay();
+		}
+	} //}}}
+
+	//{{{ CaretHandler class
+	class CaretHandler implements CaretListener
+	{
+		public void caretUpdate(CaretEvent evt)
+		{
+			// hack
+			if(textArea.getBuffer() != buffer)
+				return;
+
+			updateHighlightWithDelay();
+		}
+	} //}}}
+}
---
+++ plugins/XML/tags/release-0-11-1/xml/options/GeneralOptionPane.java
@@ -0,0 +1,143 @@
+/*
+ * GeneralOptionPane.java - XML general options panel
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2001 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml.options;
+
+//{{{ Imports
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Hashtable;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.*;
+//}}}
+
+public class GeneralOptionPane extends AbstractOptionPane
+{
+	//{{{ GeneralOptionPane constructor
+	public GeneralOptionPane()
+	{
+		super("xml.general");
+	} //}}}
+
+	//{{{ _init() method
+	protected void _init()
+	{
+		addComponent(validate = new JCheckBox(jEdit.getProperty(
+			"options.xml.general.validate")));
+		validate.setSelected(jEdit.getBooleanProperty("xml.validate"));
+
+		String[] networkValues = {
+			jEdit.getProperty("options.xml.general.network-off"),
+			jEdit.getProperty("options.xml.general.network-cache"),
+			jEdit.getProperty("options.xml.general.network-always")
+		};
+		addComponent(jEdit.getProperty("options.xml.general.network-mode"),
+			network = new JComboBox(networkValues));
+		if(jEdit.getBooleanProperty("xml.network"))
+		{
+			if(jEdit.getBooleanProperty("xml.cache"))
+				network.setSelectedIndex(1);
+			else
+				network.setSelectedIndex(2);
+		}
+		else
+			network.setSelectedIndex(0);
+
+		tagHighlight = new JCheckBox(jEdit.getProperty(
+			"options.xml.general.tag-highlight-enabled"));
+		tagHighlight.setSelected(jEdit.getBooleanProperty(
+			"xml.tag-highlight"));
+		tagHighlight.addActionListener(new ActionHandler());
+
+		tagHighlightColor = new ColorWellButton(
+			jEdit.getColorProperty("xml.tag-highlight-color"));
+
+		addComponent(tagHighlight,tagHighlightColor,
+			GridBagConstraints.VERTICAL);
+
+		tagHighlightColor.setEnabled(tagHighlight.isSelected());
+
+		String[] showAttributeValues = {
+			jEdit.getProperty("options.xml.general.show-attributes.none"),
+			jEdit.getProperty("options.xml.general.show-attributes.id-only"),
+			jEdit.getProperty("options.xml.general.show-attributes.all")
+		};
+
+		addComponent(jEdit.getProperty("options.xml.general.show-attributes"),
+			showAttributes = new JComboBox(showAttributeValues));
+		showAttributes.setSelectedIndex(jEdit.getIntegerProperty(
+			"xml.show-attributes",0));
+
+		addComponent(closeComplete = new JCheckBox(jEdit.getProperty(
+			"options.xml.general.close-complete")));
+		closeComplete.setSelected(jEdit.getBooleanProperty(
+			"xml.close-complete"));
+		closeComplete.addActionListener(new ActionHandler());
+
+		addComponent(closeCompleteOpen = new JCheckBox(jEdit.getProperty(
+			"options.xml.general.close-complete-open")));
+		closeCompleteOpen.setSelected(jEdit.getBooleanProperty(
+			"xml.close-complete-open"));
+		closeCompleteOpen.addActionListener(new ActionHandler());
+
+		addComponent(standaloneExtraSpace = new JCheckBox(jEdit.getProperty(
+			"options.xml.general.standalone-extra-space")));
+		standaloneExtraSpace.setSelected(jEdit.getBooleanProperty(
+			"xml.standalone-extra-space"));
+	} //}}}
+
+	//{{{ _save() method
+	protected void _save()
+	{
+		jEdit.setBooleanProperty("xml.validate",validate.isSelected());
+		jEdit.setBooleanProperty("xml.cache",network.getSelectedIndex() == 1);
+		jEdit.setBooleanProperty("xml.network",network.getSelectedIndex() >= 1);
+		jEdit.setBooleanProperty("xml.tag-highlight",
+			tagHighlight.isSelected());
+		jEdit.setProperty("xml.tag-highlight-color",
+			GUIUtilities.getColorHexString(
+			tagHighlightColor.getSelectedColor()));
+		jEdit.setIntegerProperty("xml.show-attributes",
+			showAttributes.getSelectedIndex());
+		jEdit.setBooleanProperty("xml.close-complete",
+			closeComplete.isSelected());
+		jEdit.setBooleanProperty("xml.close-complete-open",
+			closeCompleteOpen.isSelected());
+		jEdit.setBooleanProperty("xml.standalone-extra-space",
+			standaloneExtraSpace.isSelected());
+	} //}}}
+
+	//{{{ Private members
+	private JCheckBox validate;
+	private JComboBox network;
+	private JCheckBox tagHighlight;
+	private ColorWellButton tagHighlightColor;
+	private JComboBox showAttributes;
+	private JCheckBox closeCompleteOpen;
+	private JCheckBox closeComplete;
+	private JCheckBox standaloneExtraSpace;
+	//}}}
+
+	//{{{ ActionHandler class
+	class ActionHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			if(evt.getSource() == tagHighlight)
+				tagHighlightColor.setEnabled(tagHighlight.isSelected());
+		}
+	} //}}}
+}
