8ef59066a0295655cc84beca4ca89ad30ee7bcf9
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-03-13 17:26:34 +0000

    This commit was manufactured by cvs2svn to create tag 'pv_2_0_1'.

---
+++ plugins/ProjectViewer/tags/pv_2_0_1/projectviewer/action/EditProjectAction.java
@@ -0,0 +1,134 @@
+/*
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package projectviewer.action;
+
+//{{{ Imports
+import java.awt.event.ActionEvent;
+
+import javax.swing.Icon;
+import javax.swing.JMenuItem;
+
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.GUIUtilities;
+
+import projectviewer.ProjectViewer;
+import projectviewer.ProjectManager;
+import projectviewer.vpt.VPTNode;
+import projectviewer.vpt.VPTProject;
+import projectviewer.config.ProjectOptions;
+import projectviewer.importer.RootImporter;
+//}}}
+
+/**
+ *	Action that when executed creates a new project or edits an existing one.
+ *
+ *	@author		Marcelo Vanzin
+ *	@version	$Id$
+ */
+public class EditProjectAction extends Action {
+
+	//{{{ Private members
+	private boolean forceNew;
+	//}}}
+
+	//{{{ Constructors
+
+	/** Default constructor. */
+	public EditProjectAction() {
+		this(false);
+	}
+
+	/** If forceNew is true, creation of new project will be forced. */
+	public EditProjectAction(boolean forceNew) {
+		this.forceNew = forceNew;
+	}
+
+	//}}}
+
+	//{{{ getText() method
+	/** Returns the text to be shown on the button and/or menu item. */
+	public String getText() {
+		return jEdit.getProperty("projectviewer.action.a_e_project");
+	} //}}}
+
+	//{{{ getIcon() method
+	/**
+	 *	Returns the icon to be shown on the toolbar button. The default
+	 *	implementation returns "null" so that actions that will only be
+	 *	used in the context menu don't need to implement this.
+	 */
+	public Icon getIcon() {
+		return GUIUtilities.loadIcon("Drive.png");
+	} //}}}
+
+	//{{{ actionPerformed(ActionEvent) method
+	/** Creates a new project. */
+	public void actionPerformed(ActionEvent e) {
+		VPTNode selected = viewer.getSelectedNode();
+		if (selected == null || !selected.isProject())
+			selected  = viewer.getRoot();
+
+		VPTProject proj = null;
+		boolean add = false;
+		String oldName = null;
+		String oldRoot = null;
+		if (!forceNew && selected != null && !selected.isRoot()) {
+			proj = VPTNode.findProjectFor(selected);
+			oldName = proj.getName();
+			oldRoot = proj.getRootPath();
+		} else {
+			add = true;
+		}
+
+		proj = ProjectOptions.run(proj);
+		if (proj != null) {
+			if (add) {
+				ProjectManager.getInstance().addProject(proj);
+				RootImporter ipi = new RootImporter(proj, viewer, jEdit.getActiveView());
+				ipi.doImport();
+				viewer.setProject(proj);
+			} else {
+				if (!proj.getName().equals(oldName)) {
+					ProjectManager.getInstance().renameProject(oldName, proj.getName());
+				}
+				if (!proj.getRootPath().equals(oldRoot)) {
+					RootImporter ipi = new RootImporter(proj, viewer, jEdit.getActiveView());
+					ipi.doImport();
+				}
+			}
+		}
+
+	} //}}}
+
+	//{{{ prepareForNode(VPTNode) method
+	/** Enable action only for the root node. */
+	public void prepareForNode(VPTNode node) {
+		if (node != null && (node.isRoot() || node.isProject())) {
+			cmItem.setVisible(true);
+			((JMenuItem)cmItem).setText( node.isRoot() ?
+				jEdit.getProperty("projectviewer.action.add_project") :
+				jEdit.getProperty("projectviewer.action.edit_project"));
+		} else {
+			cmItem.setVisible(false);
+		}
+	} //}}}
+
+}
+
---
+++ plugins/ProjectViewer/tags/pv_2_0_1/projectviewer/importer/Importer.java
@@ -0,0 +1,323 @@
+/*
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package projectviewer.importer;
+
+//{{{ Imports
+import java.io.File;
+import java.util.Stack;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+import javax.swing.SwingUtilities;
+import javax.swing.tree.DefaultTreeModel;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.io.VFSManager;
+
+import projectviewer.vpt.VPTFile;
+import projectviewer.vpt.VPTNode;
+import projectviewer.vpt.VPTProject;
+import projectviewer.vpt.VPTDirectory;
+
+import projectviewer.ProjectViewer;
+import projectviewer.ProjectManager;
+import projectviewer.config.ProjectViewerConfig;
+//}}}
+
+/**
+ *	Base class for importers. Importers are classes that select a set of nodes
+ *	(which can be of any kind) and add them to a given node.
+ *
+ *	<p>Trees are updated in the following manner: when a node is inserted, the
+ *	folder tree is updates immediately. The other two trees (which are a "flat"
+ *	version of the file list, basically) are updated once at the end of the
+ *	importing (to make the code simpler for those trees).</p>
+ *
+ *	<p>Since a lot of time may be required to import large number of files, the
+ *	importing is done in its own separate thread, so that the GUI may be
+ *	used normally during the process. This behaviour can be prevented if
+ *	wished. If importing in a separate thread, the PV GUI will be blocked
+ *	during the import phase (but the rest of jEdit will be usable).</p>
+ *
+ *	@author		Marcelo Vanzin
+ *	@version	$Id$
+ */
+public abstract class Importer implements Runnable {
+
+	//{{{ Instance variables
+
+	/** The node to where the imported nodes will be added. */
+	protected final ProjectViewer viewer;
+
+	protected VPTNode		selected;
+	protected VPTProject	project;
+	private	 boolean	noThread;
+
+	/**
+	 *	An action to be executed after the import occurs. It will be executed
+	 *	in the AWT thread.
+	 */
+	protected Runnable		postAction;
+
+	//}}}
+
+	//{{{ Constructors
+
+	/**
+	 *	If noThread is true, inhibits the use of a separate Thread to do the
+	 *	importing. If you know just a few nodes will be imported, you may use
+	 *	this.
+	 */
+	public Importer(VPTNode node, ProjectViewer viewer, boolean noThread) {
+		if (node.isFile()) {
+			node = (VPTNode) node.getParent();
+		}
+		selected = node;
+		project = VPTNode.findProjectFor(node);
+		if (project == null) {
+			throw new IllegalArgumentException("Cannot add to root node.");
+		}
+		this.viewer = viewer;
+		this.noThread = noThread;
+		this.postAction = null;
+	}
+
+	public Importer(VPTNode node, ProjectViewer viewer) {
+		this(node, viewer, false);
+	}
+
+	public Importer(VPTNode node) {
+		this(node, null, false);
+	}
+
+	public Importer(VPTNode node, boolean noThread) {
+		this(node, null, noThread);
+	}
+
+	//}}}
+
+	//{{{ doImport() method
+	/**
+	 *	Main import method. It starts a new thread to actually do the importing,
+	 *	so that the UI is not blocked during the process, which can take some
+	 *	time for large numbers of files.
+	 */
+	public void doImport() {
+		if (noThread) {
+			run();
+		} else {
+			VFSManager.getIOThreadPool().addWorkRequest(this, false);
+		}
+	} //}}}
+
+	//{{{ importNode(VPTNode, VPTNode) method
+	/**
+	 *	Imports a child into the given parent.
+	 *
+	 *	<p>Important: when using a separate thread, wrap a call to this method
+	 *	in a call to SwingUtilities.invokeLater() or
+	 *	SwingUtilities.invokeAndWait(), to ensure thread safety.</p>
+	 */
+	protected void importNode(VPTNode child, VPTNode where) {
+		ProjectViewer.insertNodeInto(child, where);
+	} //}}}
+
+	//{{{ importNode(VPTNode) method
+	/**
+	 *	Imports the node into the selected parent. The selected parent is
+	 *	defined in the constructor for the importer (and generally should
+	 *	be a selected node in the tree).
+	 *
+	 *	<p>Important: when using a separate thread, wrap a call to this method
+	 *	in a call to SwingUtilities.invokeLater() or
+	 *	SwingUtilities.invokeAndWait(), to ensure thread safety.</p>
+	 */
+	protected void importNode(VPTNode node) {
+		ProjectViewer.insertNodeInto(node, selected);
+	} //}}}
+
+	//{{{ internalDoImport() method
+	/**
+	 *	Method to be called when importing nodes. The implementation should
+	 *	eeturn a list of nodes to be added to the selected node. The method
+	 *	{@link #importNode(VPTNode) importNode(VPTNode)} will be called for
+	 *	each element of the collection.
+	 */
+	protected abstract Collection internalDoImport(); //}}}
+
+	//{{{ findDirectory(String, VPTNode) method
+	/**
+	 *	Looks, in the children list for the given parent, for a directory with
+	 *	the given path. If it exists, return it. If not, creates a new directory
+	 *	node if <i>create</i> is true, or else return null.
+	 *
+	 *	@param	dir		The directory to look for.
+	 *	@param	parent	The node where to look for the directory.
+	 *	@param	create	Whether to create a new node if a corresponding path is
+	 *					not found in the parent node.
+	 */
+	protected VPTNode findDirectory(File dir, VPTNode parent, boolean create) {
+		Enumeration e = parent.children();
+		while (e.hasMoreElements()) {
+			VPTNode n = (VPTNode) e.nextElement();
+			if (n.getNodePath().equals(dir.getAbsolutePath())) {
+				return n;
+			}
+		}
+		return (create) ? new VPTDirectory(dir) : null;
+	} //}}}
+
+	//{{{ makePathTo(String) method
+	/**
+	 *	Inserts all nodes from the root up to and including the given path, if
+	 *	the nodes are not yet inserted. Returns the node representing the given
+	 *	path.
+	 */
+	protected VPTNode makePathTo(String path, ArrayList added) {
+		Stack dirs = new Stack();
+		while (!path.equals(project.getRootPath())) {
+			File pf = new File(path);
+			dirs.push(pf);
+			path = pf.getParent();
+		}
+
+		VPTNode where = project;
+		while (!dirs.isEmpty()) {
+			File curr = (File) dirs.pop();
+			VPTNode n = findDirectory(curr, where, false);
+			if (n == null) {
+				n = new VPTDirectory(curr);
+				if (where == project) {
+					added.add(n);
+				} else {
+					where.insert(n, where.findIndexForChild(n));
+				}
+			}
+			where = n;
+		}
+		return where;
+	} //}}}
+
+	//{{{ registerFile(VPTFile) method
+	/**
+	 *	Registers the file in the project. Also, checks if the file's absolute
+	 *	path is equal to the canonical path, and registers the canonical path
+	 *	in the project in case they differ.
+	 */
+	protected void registerFile(VPTFile file) {
+		project.registerFile(file);
+		String canPath = file.getCanonicalPath();
+		if (!canPath.equals(file.getNodePath())) {
+			project.registerCanonicalPath(canPath, file);
+		}
+	} //}}}
+
+	//{{{ run() method
+	public void run() {
+		if (!noThread) {
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					public void run() {
+						viewer.setStatus(jEdit.getProperty("projectviewer.import.wait_msg"));
+						viewer.setEnabled(false);
+					}
+				});
+			} catch (InterruptedException ie) {
+				// not gonna happen
+			} catch (java.lang.reflect.InvocationTargetException ite) {
+				// not gonna happen
+			}
+		}
+		try {
+			final Collection c = internalDoImport();
+			if (c != null && c.size() > 0) {
+				if (!noThread) {
+					try {
+						SwingUtilities.invokeAndWait(new Runnable() {
+							public void run() {
+								for (Iterator i = c.iterator(); i.hasNext(); ) {
+									VPTNode n = (VPTNode) i.next();
+									importNode(n);
+								}
+								ProjectViewer.nodeStructureChangedFlat(project);
+							}
+						});
+					} catch (InterruptedException ie) {
+						// not gonna happen
+					} catch (java.lang.reflect.InvocationTargetException ite) {
+						// not gonna happen
+					}
+				} else {
+					for (Iterator i = c.iterator(); i.hasNext(); ) {
+						importNode((VPTNode)i.next());
+					}
+					ProjectViewer.nodeStructureChangedFlat(project);
+				}
+				if (ProjectViewerConfig.getInstance().getSaveOnChange()) {
+					ProjectManager.getInstance().saveProject(project);
+				}
+			}
+		} finally {
+			// in case any RuntimeException occurs, let's be cautious...
+			if (!noThread) {
+				try {
+					SwingUtilities.invokeAndWait( new Runnable() {
+						public void run() {
+							viewer.setEnabled(true);
+						}
+					});
+				} catch (InterruptedException ie) {
+					// not gonna happen
+				} catch (java.lang.reflect.InvocationTargetException ite) {
+					// not gonna happen
+				}
+			}
+		}
+		if (postAction != null)
+			SwingUtilities.invokeLater(postAction);
+	} //}}}
+
+	//{{{ ShowNode class
+	/** Makes sure a node is visible. */
+	protected class ShowNode implements Runnable {
+
+		private VPTNode toShow;
+
+		public ShowNode(VPTNode toShow) {
+			this.toShow = toShow;
+		}
+
+		public void run() {
+			JTree tree = viewer.getCurrentTree();
+			if (tree != null) {
+				DefaultTreeModel tModel = (DefaultTreeModel) tree.getModel();
+				TreeNode[] nodes = tModel.getPathToRoot(toShow);
+				tree.makeVisible(new TreePath(nodes));
+			}
+		}
+
+	} //}}}
+
+}
+
