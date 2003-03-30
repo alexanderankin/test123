7485c0d18f0d221532311cfce57e708bb46db763
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-03-30 23:55:28 +0000

    This commit was manufactured by cvs2svn to create branch 'duggaraju_exp'.

---
+++ plugins/Background/branches/duggaraju_exp/background/BackgroundHighlight.java
@@ -0,0 +1,273 @@
+/*
+ * BackgroundHighlight.java
+ * Copyright (c) 2002, 2003 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+package background;
+
+import java.awt.*;
+import java.util.Hashtable;
+import java.util.Vector;
+import javax.swing.*;
+
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.textarea.TextAreaExtension;
+import org.gjt.sp.jedit.textarea.TextAreaPainter;
+import org.gjt.sp.util.Log;
+
+
+public class BackgroundHighlight extends TextAreaExtension
+{
+    // (EditPane, BackgroundHighlight) association
+    private static Hashtable highlights = new Hashtable();
+
+    private static String    iconName   = null;
+    private static ImageIcon icon       = null;
+    private static boolean   blend      = false;
+    private static Color     blendColor = null;
+    private static int       blendAlpha = 127;
+    private static Color     alphaColor = null;
+
+    private static AlphaComposite alphaComposite = AlphaComposite.getInstance(
+        AlphaComposite.SRC_OVER
+    );
+
+    private boolean enabled = true;
+
+    private JEditTextArea textArea;
+
+
+    static {
+        iconName = jEdit.getProperty("background.file", (String) null);
+
+        if ("".equals(iconName)) { iconName = null; }
+
+        try {
+            if (iconName != null) {
+                icon = new ImageIcon(iconName);
+            }
+        } catch (Exception e) {
+            iconName = null;
+            icon     = null;
+            Log.log(Log.DEBUG, BackgroundHighlight.class, e);
+        }
+
+        blend      = jEdit.getBooleanProperty("background.blend", false);
+        blendColor = jEdit.getColorProperty(
+            "background.blend-color",
+            jEdit.getColorProperty("view.bgColor", Color.white)
+        );
+        blendAlpha = jEdit.getIntegerProperty(
+            "background.blend-alpha", 127
+        );
+        if (blendAlpha < 0)   { blendAlpha = 0; }
+        if (blendAlpha > 255) { blendAlpha = 255; }
+
+        alphaColor = new Color(
+            blendColor.getRed(), blendColor.getGreen(), blendColor.getBlue(), blendAlpha
+        );
+    }
+
+
+    private BackgroundHighlight(JEditTextArea textArea) {
+        this.textArea = textArea;
+    }
+
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+
+    public void toggleEnabled() {
+        this.enabled = !this.enabled;
+    }
+
+
+    private void paintLine(final Graphics2D gfx, final int lineY) {
+        if (!this.isEnabled()) { return; }
+
+        if (icon == null) { return; }
+
+        if (icon.getImageLoadStatus() != MediaTracker.COMPLETE) { return; }
+
+        TextAreaPainter painter = this.textArea.getPainter();
+        FontMetrics fm = painter.getFontMetrics();
+
+        final int lineX = 0;
+
+        // Line width
+        int width  = painter.getWidth();
+
+        // Line height
+        int height = fm.getHeight();
+
+        if ((lineY + height) >= painter.getHeight()) {
+            height = painter.getHeight() - lineY;
+        }
+
+        int x0 = 0; // (lineX / icon.getIconWidth()) * icon.getIconWidth();
+        int x1 = ((lineX + width - 1) / icon.getIconWidth()) * icon.getIconWidth();
+
+        int y0 = (lineY / icon.getIconHeight()) * icon.getIconHeight();
+        int y1 = ((lineY + height - 1) / icon.getIconHeight()) * icon.getIconHeight();
+
+        // Remember the original clip bounds
+        Rectangle rect = gfx.getClipBounds();
+
+        gfx.setClip(lineX, lineY, width, height);
+
+        for (int x = x0; x <= x1; x += icon.getIconWidth()) {
+            for (int y = y0; y <= y1; y += icon.getIconHeight()) {
+                icon.paintIcon(this.textArea, gfx, x, y);
+            }
+        }
+
+        if (blend) {
+            // Remember the original color and composite
+            Color     color     = gfx.getColor();
+            Composite composite = gfx.getComposite();
+
+            gfx.setColor(alphaColor);
+            gfx.setComposite(alphaComposite);
+
+            gfx.fillRect(lineX, lineY, width, height);
+
+            // Restore the original color and composite
+            gfx.setColor(color);
+            gfx.setComposite(composite);
+        }
+
+        // Restore the original clip bounds
+        gfx.setClip(rect);
+    }
+
+
+    public void paintValidLine(
+            final Graphics2D gfx, final int screenLine, final int physicalLine,
+            final int start, final int end, final int y
+    ) {
+        this.paintLine(gfx, y);
+    }
+
+
+    public void paintInvalidLine(
+            final Graphics2D gfx, final int screenLine, final int y
+    ) {
+        this.paintLine(gfx, y);
+    }
+
+
+    private void updateTextArea() {
+        if (this.textArea == null) { return; }
+
+        this.textArea.repaint();
+    }
+
+
+    public static TextAreaExtension addHighlightTo(EditPane editPane) {
+        TextAreaExtension textAreaHighlight = new BackgroundHighlight(editPane.getTextArea());
+        highlights.put(editPane, textAreaHighlight);
+        return textAreaHighlight;
+    }
+
+
+    public static void removeHighlightFrom(EditPane editPane) {
+        highlights.remove(editPane);
+    }
+
+
+    public static BackgroundHighlight getHighlightFor(EditPane editPane) {
+        return (BackgroundHighlight) highlights.get(editPane);
+    }
+
+
+    public static boolean isHighlightEnabledFor(EditPane editPane) {
+        BackgroundHighlight highlight = getHighlightFor(editPane);
+
+        if (highlight == null) { return false; }
+
+        return highlight.isEnabled();
+    }
+
+
+    public static void toggleHighlightEnabledFor(EditPane editPane) {
+        BackgroundHighlight highlight = getHighlightFor(editPane);
+
+        if (highlight == null) { return; }
+
+        highlight.toggleEnabled();
+        highlight.updateTextArea();
+    }
+
+
+    public static void propertiesChanged() {
+        String    newIconName = jEdit.getProperty("background.file", (String) null);
+        ImageIcon newIcon     = null;
+
+        if ("".equals(newIconName)) { newIconName = null; }
+
+        try {
+            if (newIconName != null) {
+                newIcon = new ImageIcon(newIconName);
+            }
+        } catch (Exception e) {
+            newIconName = null;
+            newIcon     = null;
+            Log.log(Log.DEBUG, BackgroundHighlight.class, e);
+        }
+
+        iconName = newIconName;
+        icon     = newIcon;
+
+        blend      = jEdit.getBooleanProperty("background.blend", false);
+        blendColor = jEdit.getColorProperty(
+            "background.blend-color",
+            jEdit.getColorProperty("view.bgColor", Color.white)
+        );
+        blendAlpha = jEdit.getIntegerProperty(
+            "background.blend-alpha", 127
+        );
+        if (blendAlpha < 0)   { blendAlpha = 0; }
+        if (blendAlpha > 255) { blendAlpha = 255; }
+
+        alphaColor = new Color(
+            blendColor.getRed(), blendColor.getGreen(), blendColor.getBlue(), blendAlpha
+        );
+
+        // Propagate the changes to all textareas
+        View[] views = jEdit.getViews();
+        for (int i = 0; i < views.length; i++) {
+            EditPane[] editPanes = views[i].getEditPanes();
+            BackgroundHighlight highlight;
+
+            for (int j = 0; j < editPanes.length; j++) {
+                highlight = (BackgroundHighlight) highlights.get(editPanes[j]);
+                if (highlight == null) { continue; }
+
+                if (highlight.isEnabled()) {
+                    highlight.updateTextArea();
+                }
+            }
+        }
+    }
+}
+
---
+++ plugins/Background/branches/duggaraju_exp/background/BackgroundOptionPane.java
@@ -0,0 +1,167 @@
+/*
+ * BackgroundOptionPane.java
+ * Copyright (c) 2002 Andre Kaplan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+package background;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
+import javax.swing.*;
+
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+
+public class BackgroundOptionPane extends AbstractOptionPane
+{
+    private JButton    btnBackground;
+    private JTextField tfBackground;
+    private JCheckBox  blend;
+    private JButton    blendColor;
+    private JSlider    blendAlpha;
+
+
+    public BackgroundOptionPane() {
+        super("background");
+    }
+
+
+    protected void _init() {
+        this.tfBackground = new JTextField();
+        this.tfBackground.setText(jEdit.getProperty(
+              "background.file"
+        ));
+        this.btnBackground = new JButton(jEdit.getProperty(
+            "options.background.choose-file"
+        ));
+        this.btnBackground.addActionListener(new FileActionHandler());
+
+        JPanel filePanel = new JPanel(new BorderLayout(5, 0));
+        filePanel.add(this.tfBackground,  BorderLayout.CENTER);
+        filePanel.add(this.btnBackground, BorderLayout.EAST);
+
+        addComponent(
+            jEdit.getProperty("options.background.file"),
+            filePanel
+        );
+
+        this.blend = new JCheckBox(
+            jEdit.getProperty("options.background.blend"),
+            jEdit.getBooleanProperty("background.blend", false)
+        );
+        this.blend.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                boolean selected = BackgroundOptionPane.this.blend.isSelected();
+                BackgroundOptionPane.this.blendColor.setEnabled(selected);
+                BackgroundOptionPane.this.blendAlpha.setEnabled(selected);
+            }
+        });
+        addComponent(this.blend);
+
+        this.blendColor = this.createColorButton(
+            "background.blend-color", jEdit.getColorProperty("view.bgColor", Color.white)
+        );
+        this.blendColor.setEnabled(this.blend.isSelected());
+        addComponent(
+            jEdit.getProperty("options.background.blend-color"),
+            this.blendColor
+        );
+
+        int alpha = jEdit.getIntegerProperty("background.blend-alpha", 127);
+        if (alpha < 0) { alpha = 0; }
+        if (alpha > 255) { alpha = 255; }
+        this.blendAlpha = new JSlider(0, 255, alpha);
+        this.blendAlpha.setEnabled(this.blend.isSelected());
+        this.blendAlpha.setPaintTicks(true);
+        this.blendAlpha.setMajorTickSpacing(32);
+        Hashtable blendAlphaLabels = new Hashtable();
+        blendAlphaLabels.put(new Integer(0), new JLabel("Transparent"));
+        blendAlphaLabels.put(new Integer(127), new JLabel("Translucent"));
+        blendAlphaLabels.put(new Integer(255), new JLabel("Opaque"));
+        this.blendAlpha.setPaintLabels(true);
+        this.blendAlpha.setLabelTable(blendAlphaLabels);
+
+        addComponent(
+            jEdit.getProperty("options.background.blend-alpha"),
+            this.blendAlpha
+        );
+
+    }
+
+
+    protected void _save() {
+        jEdit.setProperty(
+            "background.file", this.tfBackground.getText()
+        );
+
+        jEdit.setBooleanProperty(
+            "background.blend", this.blend.isSelected()
+        );
+
+        jEdit.setColorProperty(
+            "background.blend-color", this.blendColor.getBackground()
+        );
+
+        jEdit.setIntegerProperty(
+            "background.blend-alpha", this.blendAlpha.getValue()
+        );
+    }
+
+
+    private class FileActionHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent evt) {
+            String[] paths = GUIUtilities.showVFSFileDialog(
+                null, null, JFileChooser.OPEN_DIALOG, false
+            );
+
+            if (paths != null) {
+                BackgroundOptionPane.this.tfBackground.setText(paths[0]);
+            }
+        }
+    }
+
+
+    private JButton createColorButton(String property, Color defaultColor) {
+        JButton b = new JButton(" ");
+        b.setBackground(jEdit.getColorProperty(property, defaultColor));
+        b.addActionListener(new ColorActionHandler());
+        b.setRequestFocusEnabled(false);
+        return b;
+    }
+
+
+    private class ColorActionHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent evt) {
+            JButton button = (JButton) evt.getSource();
+            Color c = JColorChooser.showDialog(BackgroundOptionPane.this,
+                jEdit.getProperty("colorChooser.title"),
+                button.getBackground()
+            );
+            if (c != null) {
+                button.setBackground(c);
+            }
+        }
+    }
+}
+
