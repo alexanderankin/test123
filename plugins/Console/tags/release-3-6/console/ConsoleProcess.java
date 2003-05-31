ca7bbd642e99d48d341d5851b2a26f9145688425
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-05-31 01:58:37 +0000

    This commit was manufactured by cvs2svn to create tag 'release-3-6'.

---
+++ plugins/Console/tags/release-3-6/console/ConsoleProcess.java
@@ -0,0 +1,400 @@
+/*
+ * ConsoleProcess.java - A running process
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1999, 2003 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package console;
+
+//{{{ Imports
+import gnu.regexp.*;
+import java.awt.Color;
+import java.io.*;
+import java.util.Stack;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.search.RESearchMatcher;
+import org.gjt.sp.util.Log;
+import errorlist.*;
+//}}}
+
+class ConsoleProcess
+{
+	//{{{ ConsoleProcess constructor
+	ConsoleProcess(Console console, String input, Output output,
+		Output error, String[] args, String[] env,
+		boolean foreground)
+	{
+		SystemShell.ConsoleState consoleState
+			= SystemShell.getConsoleState(console);
+
+		this.args = args;
+		this.env = env;
+		this.currentDirectory = consoleState.currentDirectory;
+
+		// for parsing error messages from 'make'
+		currentDirectoryStack = new Stack();
+		currentDirectoryStack.push(currentDirectory);
+
+		if(foreground)
+		{
+			console.getErrorSource().clear();
+
+			this.console = console;
+			this.output = output;
+			this.error = error;
+			this.consoleState = consoleState;
+
+			ConsoleProcess runningProc = consoleState.process;
+			if(runningProc != null)
+				runningProc.stop();
+
+			consoleState.process = this;
+		}
+
+		try
+		{
+			process = ProcessRunner.getProcessRunner()
+				.exec(args,env,currentDirectory);
+			stdin = new InputThread(input,process.getOutputStream());
+			stdin.start();
+			stdout = new StreamThread(process.getInputStream());
+			stdout.start();
+			stderr = new StreamThread(process.getErrorStream());
+			stderr.start();
+		}
+		catch(Exception e)
+		{
+			String[] pp = { e.toString() };
+			error.print(console.getErrorColor(),
+				jEdit.getProperty("console.shell.error",pp));
+			output.commandDone();
+			stop();
+		}
+	} //}}}
+
+	//{{{ detach() method
+	void detach()
+	{
+		if(console != null)
+		{
+			Object[] pp = { args[0] };
+			error.print(console.getErrorColor(),
+				jEdit.getProperty("console.shell.detached",pp));
+			output.commandDone();
+			error.commandDone();
+		}
+
+		consoleState.process = null;
+		consoleState = null;
+		console = null;
+	} //}}}
+
+	//{{{ stop() method
+	void stop()
+	{
+		if(process != null)
+		{
+			process.destroy();
+			process = null;
+			stdin.abort();
+			stdout.abort();
+			stderr.abort();
+
+			if(console != null)
+			{
+				Object[] pp = { args[0] };
+				error.print(console.getErrorColor(),
+					jEdit.getProperty("console.shell.killed",pp));
+			}
+
+			ConsolePlugin.finishErrorParsing(console.getErrorSource());
+
+			output.commandDone();
+			error.commandDone();
+		}
+
+		if(consoleState != null)
+			consoleState.process = null;
+	} //}}}
+
+	//{{{ getExitStatus() method
+	boolean getExitStatus()
+	{
+		return (exitCode == 0);
+	} //}}}
+
+	//{{{ Private members
+
+	private static RE makeEntering, makeLeaving;
+
+	//{{{ Class initializer
+	static
+	{
+		try
+		{
+			makeEntering = new RE(jEdit.getProperty("console.error.make.entering"),
+				0,RESearchMatcher.RE_SYNTAX_JEDIT);
+			makeLeaving = new RE(jEdit.getProperty("console.error.make.leaving"),
+				0,RESearchMatcher.RE_SYNTAX_JEDIT);
+		}
+		catch(REException re)
+		{
+			Log.log(Log.ERROR,ConsoleProcess.class,re);
+		}
+	} //}}}
+
+	//{{{ Instance variables
+	private SystemShell.ConsoleState consoleState;
+	private String currentDirectory;
+	private Stack currentDirectoryStack; // for make
+	private Console console;
+	private Output output;
+	private Output error;
+	private String[] args;
+	private String[] env;
+	private Process process;
+	private InputThread stdin;
+	private StreamThread stdout;
+	private StreamThread stderr;
+	private int threadDoneCount;
+	private int exitCode;
+	//}}}
+
+	//{{{ threadDone() method
+	private synchronized void threadDone()
+	{
+		threadDoneCount++;
+		if(process != null && threadDoneCount == 3)
+		{
+			try
+			{
+				exitCode = process.waitFor();
+			}
+			catch(InterruptedException e)
+			{
+				Log.log(Log.ERROR,this,e);
+				return;
+			}
+
+			if(console != null && output != null && error != null)
+			{
+				Object[] pp = { args[0], new Integer(exitCode) };
+
+				String msg = jEdit.getProperty("console.shell.exited",pp);
+
+				if(exitCode == 0)
+					error.print(console.getInfoColor(),msg);
+				else
+					error.print(console.getErrorColor(),msg);
+
+				ConsolePlugin.finishErrorParsing(
+					console.getErrorSource());
+
+				output.commandDone();
+				error.commandDone();
+			}
+
+			process = null;
+
+			if(consoleState != null)
+				consoleState.process = null;
+		}
+
+		notifyAll();
+	} //}}}
+
+	//}}}
+
+	//{{{ InputThread class
+	class InputThread extends Thread
+	{
+		boolean aborted;
+		String input;
+		BufferedWriter out;
+		String lineSep;
+
+		//{{{ InputThread constructor
+		InputThread(String input, OutputStream outputStream)
+		{
+			setName("" + InputThread.class + args);
+			this.input = input;
+			out = new BufferedWriter(new OutputStreamWriter(
+				outputStream));
+			lineSep = System.getProperty("line.separator");
+		} //}}}
+
+		//{{{ run() method
+		public void run()
+		{
+			try
+			{
+				if(input != null)
+				{
+					for(int i = 0; i < input.length(); i++)
+					{
+						char ch = input.charAt(i);
+						if(ch == '\n')
+							out.write(lineSep);
+						else
+							out.write(ch);
+					}
+				}
+				out.close();
+			}
+			catch(Exception e)
+			{
+				if(!aborted)
+				{
+					Log.log(Log.ERROR,this,e);
+
+					if(console != null)
+					{
+						String[] args = { e.toString() };
+						error.print(console.getErrorColor(),
+							jEdit.getProperty(
+							"console.shell.error",args));
+					}
+				}
+			}
+			finally
+			{
+				threadDone();
+			}
+		} //}}}
+
+		//{{{ abort() method
+		public void abort()
+		{
+			aborted = true;
+			try
+			{
+				out.close();
+			}
+			catch(IOException io)
+			{
+			}
+		} //}}}
+	} //}}}
+
+	//{{{ StreamThread class
+	class StreamThread extends Thread
+	{
+		boolean aborted;
+		BufferedReader in;
+
+		//{{{ StreamThread constructor
+		StreamThread(InputStream inputStream)
+		{
+			setName("" + StreamThread.class + args);
+			//setPriority(Thread.MIN_PRIORITY + 2);
+			in = new BufferedReader(new InputStreamReader(
+				inputStream));
+		} //}}}
+
+		//{{{ run() method
+		public void run()
+		{
+			try
+			{
+				String line;
+				while((line = in.readLine()) != null)
+				{
+					if(console == null || output == null)
+						continue;
+
+					if(aborted)
+						break;
+
+					Color color = null;
+
+					REMatch match = makeEntering.getMatch(line);
+					if(match == null)
+					{
+						match = makeLeaving.getMatch(line);
+						if(match == null)
+						{
+							String _currentDirectory;
+							if(currentDirectoryStack.isEmpty())
+							{
+								// should not happen...
+								_currentDirectory = currentDirectory;
+							}
+							else
+								_currentDirectory = (String)currentDirectoryStack.peek();
+
+							int type = ConsolePlugin.parseLine(
+								console.getView(),line,
+								_currentDirectory,
+								console.getErrorSource());
+							switch(type)
+							{
+							case ErrorSource.ERROR:
+								color = console.getErrorColor();
+								break;
+							case ErrorSource.WARNING:
+								color = console.getWarningColor();
+								break;
+							}
+						}
+						else if(!currentDirectoryStack.isEmpty())
+							currentDirectoryStack.pop();
+					}
+					else
+						currentDirectoryStack.push(match.toString(1));
+
+					output.print(color,line);
+				}
+				in.close();
+			}
+			catch(Exception e)
+			{
+				if(!aborted)
+				{
+					Log.log(Log.ERROR,this,e);
+
+					if(console != null)
+					{
+						String[] args = { e.toString() };
+						error.print(console.getErrorColor(),
+							jEdit.getProperty(
+							"console.shell.error",args));
+					}
+				}
+			}
+			finally
+			{
+				threadDone();
+			}
+		} //}}}
+
+		//{{{ abort() method
+		public void abort()
+		{
+			aborted = true;
+			try
+			{
+				in.close();
+			}
+			catch(IOException io)
+			{
+			}
+		} //}}}
+
+	} //}}}
+}
---
+++ plugins/Console/tags/release-3-6/console/ConsoleTextField.java
@@ -0,0 +1,180 @@
+/*
+ * ConsoleTextField.java - History text field with completion
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2002, 2003 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package console;
+
+//{{{ Imports
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Arrays;
+import org.gjt.sp.jedit.gui.HistoryTextField;
+import org.gjt.sp.jedit.*;
+//}}}
+
+public class ConsoleTextField extends HistoryTextField
+{
+	//{{{ ConsoleTextField constructor
+	public ConsoleTextField(View view, Console console,
+		ConsoleToolBar toolBar)
+	{
+		super(null);
+		this.view = view;
+		this.console = console;
+		this.toolBar = toolBar;
+	} //}}}
+
+	//{{{ getFocusTraversalKeysEnabled() method
+	public boolean getFocusTraversalKeysEnabled()
+	{
+		return false;
+	} //}}}
+
+	//{{{ processKeyEvent() method
+	protected void processKeyEvent(KeyEvent evt)
+	{
+		if(evt.getID() == KeyEvent.KEY_PRESSED)
+		{
+			if(evt.getKeyCode() == KeyEvent.VK_TAB)
+			{
+				complete();
+				return;
+			}
+		}
+
+		super.processKeyEvent(evt);
+	} //}}}
+
+	//{{{ Private members
+	private View view;
+	private Console console;
+	private ConsoleToolBar toolBar;
+
+	//{{{ complete() method
+	private void complete()
+	{
+		Shell shell;
+		if(toolBar != null)
+			shell = toolBar.getShell();
+		else
+			shell = console.getShell();
+
+		Shell.CompletionInfo info = shell.getCompletions(console,
+			getText().substring(0,getCaretPosition()));
+
+		if(info == null || info.completions.length == 0)
+			getToolkit().beep();
+		else if(info.completions.length == 1)
+		{
+			select(info.offset, getCaretPosition());
+			replaceSelection(info.completions[0]);
+		}
+		else //if(info.completions.length > 1)
+		{
+			// Find a partial completion
+			String longestCommonStart = findLongestCommonStart(info.completions);
+			if(longestCommonStart.length() != 0)
+			{
+				if(getCaretPosition() - info.offset
+					!= longestCommonStart.length())
+				{
+					select(info.offset,getCaretPosition());
+					replaceSelection(longestCommonStart);
+					return;
+				}
+			}
+
+			Console consoleInstance;
+
+			if(console == null)
+			{
+				view.getDockableWindowManager().addDockableWindow("console");
+				consoleInstance = (Console)view.getDockableWindowManager()
+					.getDockable("console");
+				consoleInstance.getTextField().setText(getText());
+				consoleInstance.getTextField().setCaretPosition(getCaretPosition());
+				setText(null);
+			}
+			else
+				consoleInstance = console;
+
+			consoleInstance.print(console.getInfoColor(),
+				jEdit.getProperty(
+				"console.completions"));
+
+			Arrays.sort(info.completions,new MiscUtilities
+				.StringICaseCompare());
+
+			for(int i = 0; i < info.completions.length; i++)
+			{
+				consoleInstance.print(null,info.completions[i]);
+			}
+
+			consoleInstance.print(console.getInfoColor(),
+				jEdit.getProperty(
+				"console.completions-end"));
+		}
+	} //}}}
+
+	//{{{ findLongestCommonStart() method
+	/**
+	 * Returns the longest substring starting at the beginning of the string
+	 * of the strings in the given array. The comparison of strings is done
+	 * in a way that respects the OS case sensitivity.
+	 */
+	private static String findLongestCommonStart(String [] strings)
+	{
+		if (strings.length == 0)
+			return "";
+		if (strings.length == 1)
+			return strings[0];
+
+		boolean isOSCaseSensitive = ProcessRunner.getProcessRunner().isCaseSensitive();
+		String longestCommonStart = strings[0];
+		int longestCommonStartLength = longestCommonStart.length();
+		for(int i = 0; i < strings.length; i++){
+			int commonStartLength = 0;
+			int strLength = strings[i].length();
+			while((commonStartLength < strLength) && (commonStartLength < longestCommonStartLength))
+			{
+				char c1 = strings[i].charAt(commonStartLength);
+				char c2 = longestCommonStart.charAt(commonStartLength);
+
+				if(!isOSCaseSensitive)
+				{
+					c1 = Character.toLowerCase(c1);
+					c2 = Character.toLowerCase(c2);
+				}
+
+				if(c1 != c2)
+					break;
+				commonStartLength++;
+			}
+			longestCommonStart = longestCommonStart.substring(0, commonStartLength);
+			longestCommonStartLength = commonStartLength;
+		}
+
+		return longestCommonStart;
+	} //}}}
+
+	//}}}
+}
