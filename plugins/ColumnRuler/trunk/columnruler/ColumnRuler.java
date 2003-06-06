1dd63db0faee8fea1b2eedbeacd78f64299dc4f9
bemace <bemace@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-06-06 06:46:18 +0000

    Added selection and caret indicators works when scrolled horizontally updates on caret events and horizontal scrolls

--- plugins/ColumnRuler/trunk/columnruler/ColumnRuler.java
+++ plugins/ColumnRuler/trunk/columnruler/ColumnRuler.java
@@ -1,41 +1,71 @@
 package columnruler;
 
 import java.awt.*;
+import javax.swing.event.*;
 
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.jedit.textarea.*;
 
 /**
- * Description of the Class             
+ * Description of the Class
  *
  * @author     mace
  * @created    June 5, 2003
- * @modified   $Date: 2003-06-05 21:51:44 $ by $Author: bemace $
- * @version    $Revision: 1.1.1.1 $
+ * @modified   $Date: 2003-06-06 06:46:18 $ by $Author: bemace $
+ * @version    $Revision: 1.2 $
  */
-public class ColumnRuler extends Component {
+public class ColumnRuler extends Component implements CaretListener, ScrollListener {
 	private JEditTextArea _textArea;
+
 
 	public ColumnRuler(JEditTextArea textArea) {
 		_textArea = textArea;
+		_textArea.addCaretListener(this);
+		_textArea.addScrollListener(this);
 	}
-
+	//{{{ paint() method
 	public void paint(Graphics gfx) {
-		
+
 		int textAreaWidth = _textArea.getWidth();
 		int lineHeight = getLineHeight();
 		FontMetrics fm = _textArea.getPainter().getFontMetrics();
 		int charHeight = _textArea.getFont().getSize();
 		int charWidth = fm.charWidth('X');
 		int xOffset = _textArea.getGutter().getWidth();
-		
+		int hScroll = _textArea.getHorizontalOffset();
+		Selection[] selection = _textArea.getSelection();
+
+		//{{{ Draw background
 		gfx.setColor(getBackground());
 		gfx.fillRect(0, 0, getWidth(), getHeight());
+		//}}}
 
+		//{{{ Highlight selection columns
+		if (selection != null && selection.length > 0) {
+			gfx.setColor(getHighlight());
+			int start;
+			int end;
+			if (selection[0] instanceof Selection.Rect) {
+				Selection.Rect rect = (Selection.Rect) selection[0];
+				start = xOffset + rect.getStartColumn(_textArea.getBuffer()) * charWidth;
+				end = xOffset + rect.getEndColumn(_textArea.getBuffer()) * charWidth;
+			} else {
+				start = xOffset + (int) _textArea.offsetToXY(selection[0].getStart()).getX();
+				end = xOffset + (int) _textArea.offsetToXY(selection[0].getEnd()).getX();
+			}
+			if (start < end) {
+				gfx.fillRect(start, 0, end - start, lineHeight);
+			} else {
+				gfx.fillRect(xOffset, 0, end, lineHeight);
+				gfx.fillRect(start, 0, getWidth() - start, lineHeight);
+			}
+		}
+		//}}}
+
+		//{{{ Draw tick marks
 		gfx.setColor(getForeground());
-		// Draw tick marks
-		for (int col = 0; col < textAreaWidth / charWidth; col++) {
-			int x = xOffset + col *charWidth;
+		for (int col = 0; col < (textAreaWidth-hScroll) / charWidth; col++) {
+			int x = xOffset + hScroll + col * charWidth;
 			switch (col % 10) {
 				case 0:
 				case 5:
@@ -45,35 +75,73 @@ public class ColumnRuler extends Component {
 					gfx.drawLine(x, lineHeight, x, 4 * lineHeight / 5);
 			}
 		}
-		// Draw numbers
+		//}}}
+
+		//{{{ Draw caret indicator
+		gfx.setColor(_textArea.getPainter().getCaretColor());
+		Point caret = _textArea.offsetToXY(_textArea.getCaretPosition());
+		if (caret != null) {
+			int caretX = xOffset + (int) caret.getX();
+			gfx.drawLine(caretX, 0, caretX, lineHeight);
+		}
+		// }}}
+
+		//{{{ Draw numbers
+		gfx.setColor(getForeground());
 		gfx.setFont(gfx.getFont().deriveFont(Font.BOLD));
-		for (int n = 0; n < textAreaWidth / charWidth; n += 10) {
-			gfx.drawString(n + "", xOffset + (n * charWidth) - (fm.stringWidth(n + "")) / 2 + 1, charHeight-2);
+		for (int n = 0; n < (textAreaWidth-hScroll) / charWidth; n += 10) {
+			gfx.drawString(n + "", xOffset + (n * charWidth) - (fm.stringWidth(n + "")) / 2 + 1 + hScroll, charHeight - 2);
+		}
+		//}}}
+	}//}}}
+
+	public void caretUpdate(CaretEvent e) {
+		repaint();
+	}
+	
+	public void scrolledVertically(JEditTextArea textArea) {}
+	
+	public void scrolledHorizontally(JEditTextArea textArea){
+		if (textArea.equals(_textArea)) {
+			repaint();
 		}
 	}
+
+	void destroy() {
+		_textArea.removeCaretListener(this);
+		_textArea.removeScrollListener(this);
+	}
 	
+	private int getLineHeight() {
+		return _textArea.getPainter().getFontMetrics().getHeight();
+	}
+
+	//{{{ Accessors
+
 	public Dimension getMinimumSize() {
-		return getPreferredSize();	
+		return getPreferredSize();
 	}
-	
+
 	public Dimension getPreferredSize() {
-		return new Dimension(getWidth(),getLineHeight());	
+		return new Dimension(getWidth(), getLineHeight());
 	}
-	
+
 	public Color getForeground() {
 		return jEdit.getColorProperty(ColumnRulerPlugin.OPTION_PREFIX + "foreground", Color.BLACK);
 	}
-	
+
 	public Color getBackground() {
 		return jEdit.getColorProperty(ColumnRulerPlugin.OPTION_PREFIX + "background", Color.WHITE);
 	}
-		
-	private int getLineHeight() {
-		return _textArea.getPainter().getFontMetrics().getHeight();
+
+	public Color getHighlight() {
+		return _textArea.getPainter().getSelectionColor();
 	}
 
 	public String toString() {
 		return "Column Ruler";
 	}
+
+	//}}}
 }
 
