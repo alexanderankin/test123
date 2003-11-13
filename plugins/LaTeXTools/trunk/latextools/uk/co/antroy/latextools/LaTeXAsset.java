eb9deda16430c40556ae1f1877232705cfe96640
ensonic <ensonic@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2003-11-13 09:53:52 +0000

    added image and code for verbatim environments fixed list icon transparency

--- plugins/LaTeXTools/trunk/latextools/uk/co/antroy/latextools/LaTeXAsset.java
+++ plugins/LaTeXTools/trunk/latextools/uk/co/antroy/latextools/LaTeXAsset.java
@@ -1,4 +1,4 @@
-package uk.co.antroy.latextools; 
+package uk.co.antroy.latextools;
 
 import sidekick.*;
 import javax.swing.Icon;
@@ -11,7 +11,7 @@ import org.gjt.sp.jedit.jEdit;
 
 //This must implement features of TagPair.
  public class LaTeXAsset extends Asset implements Comparable{
-    
+
     private int level = 0;
     private int iconType = 0;
     public static final int DEFAULT_ICON = 0;
@@ -20,15 +20,16 @@ import org.gjt.sp.jedit.jEdit;
     public static final int THEOREM_ICON = 3;
     public static final int TABLE_ICON = 4;
     public static final int LIST_ICON = 5;
-    
+    public static final int VERBATIM_ICON = 6;
+
     public LaTeXAsset(String name){
       super(name);
     }
-    
+
     public Icon getIcon(){
-      
+
       StringBuffer filename = new StringBuffer("/");
-      
+
       switch (iconType){
         case DEFAULT_ICON: filename.append("images/default.png"); break;
         case SECTION_ICON: filename.append("images/sections.png"); break;
@@ -36,9 +37,10 @@ import org.gjt.sp.jedit.jEdit;
         case THEOREM_ICON: filename.append("images/theorem.png"); break;
         case TABLE_ICON: filename.append("images/table.png"); break;
         case LIST_ICON: filename.append("images/list.png"); break;
+        case VERBATIM_ICON: filename.append("images/verbatim.png"); break;
         default: filename = null;
       }
-      
+
       Icon icon;
       if (filename == null) {
         icon = null;
@@ -52,57 +54,57 @@ import org.gjt.sp.jedit.jEdit;
       }
       return icon;
     }
-      
+
     public String getShortString(){
       return name;
     }
-    
+
     public String getLongString(){
       return name;
     }
-    
+
     public void setLevel(int lev){
-     level = lev; 
+     level = lev;
     }
 
     public int getLevel(){
       return level;
     }
-    
+
     public void setIconType(int type){
       iconType = type;
     }
-    
+
     public int getIconType(){
       return iconType;
     }
-    
+
     public static LaTeXAsset createAsset(String name, Position start, Position end, int icon_type, int lev){
       LaTeXAsset asset = createAsset(name, start, end, icon_type);
       asset.setLevel(lev);
       return asset;
     }
-    
+
     public static LaTeXAsset createAsset(String name, Position start, Position end, int icon_type){
       LaTeXAsset asset = createAsset(name, start, end);
       asset.setIconType(icon_type);
       return asset;
     }
-    
+
     public static LaTeXAsset createAsset(String name, Position start, Position end){
-      
+
       LaTeXAsset la = new LaTeXAsset(name);
       la.start = start;
       la.end = end;
-      
+
       return la;
     }
-    
+
     public int compareTo(LaTeXAsset asset) {
 
       int offset = start.getOffset();
       int assetOffset = asset.start.getOffset();
-      
+
       if (offset == assetOffset){
         String comp = offset + getLongString();
         return comp.compareTo(assetOffset + asset.getLongString());
@@ -114,7 +116,7 @@ import org.gjt.sp.jedit.jEdit;
   public int compareTo(Object o) {
     return compareTo((LaTeXAsset) o);
   }
-    
+
   public boolean equals(Object o){
     if (o instanceof LaTeXAsset){
       return equals((LaTeXAsset) o);
@@ -122,7 +124,7 @@ import org.gjt.sp.jedit.jEdit;
       return false;
     }
   }
-  
+
   public boolean equals(LaTeXAsset o){
     boolean out = true;
     out = out && (name.equals(o.name));
@@ -130,7 +132,7 @@ import org.gjt.sp.jedit.jEdit;
     out = out && (end.getOffset() == o.end.getOffset());
     return out;
   }
-    
+
   public int hashCode(){
     int out = 13;
     out = out *37;
@@ -141,7 +143,7 @@ import org.gjt.sp.jedit.jEdit;
     out += end.getOffset();
     return out;
   }
-    
+
   public String toString(){
     return getShortString();
   }
