671d82f5ac99c31b6cc718d33fecd433727357d0
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-02-01 18:05:03 +0000

    This commit was manufactured by cvs2svn to create tag 'MacroManager1_2_rc'.

---
+++ plugins/MacroManager/tags/MacroManager1_2_rc/macroManager/MacroListDownloadProgress.java
@@ -0,0 +1,138 @@
+/*
+ * MacroListDownloadProgress.java - Macro list download progress dialog
+ * Copyright (C) 2002 Carmine Lucarelli
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package macroManager;
+
+import com.microstar.xml.XmlException;
+import javax.swing.border.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.InterruptedIOException;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+
+class MacroListDownloadProgress extends JDialog
+{
+	MacroListDownloadProgress(Dialog dialog, boolean refresh)
+	{
+		super(JOptionPane.getFrameForComponent(dialog), jEdit.getProperty("macro-list.progress.title"),true);
+
+		this.window = window;
+		this.refresh = refresh;
+
+		JPanel content = new JPanel(new BorderLayout());
+		content.setBorder(new EmptyBorder(12,12,12,12));
+		setContentPane(content);
+
+		JLabel caption = new JLabel(jEdit.getProperty("macro-list.progress.caption"));
+		caption.setBorder(new EmptyBorder(0,0,12,0));
+		content.add(BorderLayout.NORTH,caption);
+
+		Box box = new Box(BoxLayout.X_AXIS);
+		box.add(Box.createGlue());
+		JButton stop = new JButton(jEdit.getProperty("macro-list.progress.stop"));
+		stop.addActionListener(new ActionHandler());
+		stop.setMaximumSize(stop.getPreferredSize());
+		box.add(stop);
+		box.add(Box.createGlue());
+		content.add(BorderLayout.CENTER,box);
+
+		addWindowListener(new WindowHandler());
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setSize(50, 50);
+		pack();
+		setLocationRelativeTo(window);
+		setResizable(false);
+		show();
+	}
+
+	MacroList getMacroList()
+	{
+		return list;
+	}
+
+	// private members
+	private Frame window;
+	private MacroList list;
+	private DownloadThread thread;
+
+	private boolean refresh;
+
+	class DownloadThread extends Thread
+	{
+		public void run()
+		{
+			try
+			{
+				list = new MacroList(refresh);
+				dispose();
+			}
+			catch(XmlException xe)
+			{
+				dispose();
+
+				int line = xe.getLine();
+				String path = jEdit.getProperty("macro-manager.url");
+				String message = xe.getMessage();
+				Log.log(Log.ERROR,this,path + ":" + line
+					+ ": " + message);
+				String[] pp = { path, String.valueOf(line), message };
+				GUIUtilities.error(window,"macro-list.xmlerror",pp);
+			}
+			catch(Exception e)
+			{
+				dispose();
+
+				Log.log(Log.ERROR,this,e);
+				String[] pp = { e.toString() };
+				GUIUtilities.error(window,"macro-list.ioerror",pp);
+			}
+		}
+	}
+
+	class ActionHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			thread.stop();
+			dispose();
+		}
+	}
+
+	class WindowHandler extends WindowAdapter
+	{
+		boolean done;
+
+		public void windowOpened(WindowEvent evt)
+		{
+			if(done)
+				return;
+
+			done = true;
+			thread = new DownloadThread();
+			thread.start();
+		}
+
+		public void windowClosing(WindowEvent evt)
+		{
+			thread.stop();
+		}
+	}
+}
---
+++ plugins/MacroManager/tags/MacroManager1_2_rc/macroManager/Roster.java
@@ -0,0 +1,224 @@
+/*
+ * Roster.java - A list of things to do, used in various places
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2002 Carmine Lucarelli
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package macroManager;
+
+//{{{ Imports
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import java.awt.Component;
+import java.io.*;
+import java.net.*;
+import java.util.zip.*;
+import java.util.*;
+import org.gjt.sp.jedit.io.VFSManager; // we use VFSManager.error() method
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+//}}}
+
+class Roster
+{
+	//{{{ Roster constructor
+	Roster()
+	{
+		operations = new Vector();
+	} //}}}
+
+	//{{{ addOperation() method
+	void addOperation(Operation op)
+	{
+		for(int i = 0; i < operations.size(); i++)
+		{
+			if(operations.elementAt(i).equals(op))
+				return;
+		}
+
+		operations.addElement(op);
+	} //}}}
+
+	//{{{ getOperationCount() method
+	int getOperationCount()
+	{
+		return operations.size();
+	} //}}}
+
+	//{{{ isEmpty() method
+	boolean isEmpty()
+	{
+		return operations.size() == 0;
+	} //}}}
+
+	//{{{ performOperations() method
+	boolean performOperations(MacroManagerProgress progress)
+	{
+		for(int i = 0; i < operations.size(); i++)
+		{
+			Operation op = (Operation)operations.elementAt(i);
+			if(op.perform(progress))
+				progress.done(true);
+			else
+			{
+				progress.done(false);
+				return false;
+			}
+
+			if(Thread.interrupted())
+				return false;
+		}
+
+		return true;
+	} //}}}
+
+	//{{{ Private members
+	private Vector operations;
+
+	static interface Operation
+	{
+		boolean perform(MacroManagerProgress progress);
+		boolean equals(Object o);
+	} //}}}
+
+	//{{{ Install class
+	static class Install implements Operation
+	{
+		Install(String fileName, String url, String installDirectory)
+		{
+			// catch those hooligans passing null urls
+			if(url == null)
+				throw new NullPointerException();
+
+			this.fileName = fileName;
+			this.url = url;
+			this.installDirectory = installDirectory;
+		}
+
+		public boolean perform(final MacroManagerProgress progress)
+		{
+			try
+			{
+				fileName = fileName.replace(' ', '_') + ".bsh";
+				progress.downloading(fileName);
+				String path = download(progress,fileName,url);
+				if(path == null)
+				{
+					// interrupted download
+					return false;
+				}
+
+				return true;
+			}
+			catch(InterruptedIOException iio)
+			{
+				// do nothing, user clicked 'Stop'
+				return false;
+			}
+			catch(final IOException io)
+			{
+				Log.log(Log.ERROR,this,io);
+
+				SwingUtilities.invokeLater(new Runnable()
+				{
+					public void run()
+					{
+						String[] args = { io.getMessage() };
+						GUIUtilities.error(null,"ioerror",args);
+					}
+				});
+
+				return false;
+			}
+			catch(Exception e)
+			{
+				Log.log(Log.ERROR,this,e);
+
+				return false;
+			}
+		}
+
+		public boolean equals(Object o)
+		{
+			if(o instanceof Install
+				&& ((Install)o).url.equals(url))
+			{
+				/* even if installDirectory is different */
+				return true;
+			}
+			else
+				return false;
+		}
+
+		// private members
+		private String fileName;
+		private String url;
+		private String installDirectory;
+
+		private String download(MacroManagerProgress progress,
+			String fileName, String url) throws Exception
+		{
+			URLConnection conn = new URL(url).openConnection();
+			progress.setMaximum(Math.max(0,conn.getContentLength()));
+
+			String path = MiscUtilities.constructPath(installDirectory, fileName);
+
+			if(!copy(progress,conn.getInputStream(),
+				new FileOutputStream(path),true,true))
+				return null;
+
+			return path;
+		}
+
+		private boolean copy(MacroManagerProgress progress,
+			InputStream in, OutputStream out, boolean canStop,
+			boolean doProgress) throws Exception
+		{
+			in = new BufferedInputStream(in);
+			out = new BufferedOutputStream(out);
+
+			byte[] buf = new byte[4096];
+			int copied = 0;
+loop:			for(;;)
+			{
+				int count = in.read(buf,0,buf.length);
+				if(count == -1)
+					break loop;
+
+				if(doProgress)
+				{
+					copied += count;
+					progress.setValue(copied);
+				}
+
+				out.write(buf,0,count);
+				if(canStop && Thread.interrupted())
+				{
+					in.close();
+					out.close();
+					return false;
+				}
+			}
+
+			in.close();
+			out.close();
+			return true;
+		}
+	} //}}}
+}
