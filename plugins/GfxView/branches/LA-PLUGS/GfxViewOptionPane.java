9d08e104ce38b9224f54678c5be1241a8ca5eeb5
lanciaux <lanciaux@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-02-20 18:00:50 +0000

    Initial import

---
+++ plugins/GfxView/branches/LA-PLUGS/GfxViewOptionPane.java
@@ -0,0 +1,43 @@
+//{{{ imports
+import java.awt.*;
+import javax.swing.*;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.io.*;
+import org.gjt.sp.jedit.msg.*;
+//}}}
+
+public class GfxViewOptionPane extends AbstractOptionPane {
+//	private JTextField historyFile;
+	private JTextField historyLimits;
+
+	//{{{ +GfxViewOptionPane() : <init>
+	public GfxViewOptionPane() {
+		super(GfxViewPlugin.NAME);
+	}//}}}
+	
+	//{{{ +_init() : void
+	public void _init() {
+/*
+		historyFile = new JTextField(
+			jEdit.getProperty(GfxViewPlugin.OPTION_PREFIX + "historyfile"));
+		addComponent(jEdit.getProperty(GfxViewPlugin.OPTION_PREFIX + "historyfile.label"),
+			historyFile);
+*/
+		historyLimits = new JTextField(
+			jEdit.getProperty(GfxViewPlugin.OPTION_PREFIX + "historylimits"));
+		addComponent(jEdit.getProperty(GfxViewPlugin.OPTION_PREFIX + "historylimits.label"),
+			historyLimits);
+	}//}}}
+
+	//{{{ +_save() : void
+	public void _save() {
+/*
+		jEdit.setProperty(GfxViewPlugin.OPTION_PREFIX + "historyfile",
+			historyFile.getText());
+*/
+		jEdit.setProperty(GfxViewPlugin.OPTION_PREFIX + "historylimits",
+			historyLimits.getText());
+	}//}}}
+}
