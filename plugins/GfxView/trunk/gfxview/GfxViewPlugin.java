59243d5ab33acbc13b51a545cab080f68dd20293
Anciaux <6277601+lanciaux@users.noreply.github.com>
2004-02-28 22:36:09 +0000

    Docs updated

--- plugins/GfxView/trunk/gfxview/GfxViewPlugin.java
+++ plugins/GfxView/trunk/gfxview/GfxViewPlugin.java
@@ -9,14 +9,6 @@ import org.gjt.sp.jedit.GUIUtilities;
 public class GfxViewPlugin extends EditPlugin {
     public static final String NAME = "gfxview";
     public static final String OPTION_PREFIX = "gfxview.options.";
-
-	// 4.1 API Interface
-	public void createMenuItems(Vector menuItems) {
-			menuItems.addElement(GUIUtilities.loadMenu("gfview.menu"));
-	}
-	public void createOptionPanes(OptionsDialog od) {
-        od.addOptionPane(new GfxViewOptionPane());
-	}
 }
 
 /* :folding=explicit:tabSize=2:indentSize=2: */
