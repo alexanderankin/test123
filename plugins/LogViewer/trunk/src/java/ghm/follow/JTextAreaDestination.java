1d163e36133b8e136413b328c1e333c8128af259
Dale <danson@grafidog.com>
2004-03-11 04:43:50 +0000

    modifications for jEdit 4.2 and a few updates

--- plugins/LogViewer/trunk/src/java/ghm/follow/JTextAreaDestination.java
+++ plugins/LogViewer/trunk/src/java/ghm/follow/JTextAreaDestination.java
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright (C) 2000, 2001 Greg Merrill (greghmerrill@yahoo.com)
 
 This file is part of Follow (http://follow.sf.net).
@@ -26,46 +26,52 @@ import javax.swing.text.Document;
 /**
 Implementation of {@link OutputDestination} which appends Strings to a
 {@link JTextArea}.
-
+ 
 @see OutputDestination
 @see JTextArea
 @author <a href="mailto:greghmerrill@yahoo.com">Greg Merrill</a>
 */
 public class JTextAreaDestination implements OutputDestination {
 
-  /**
-  Construct a new JTextAreaDestination.
-  @param jTextArea text will be appended to this text area
-  @param autoPositionCaret if true, caret will be automatically moved 
-    to the bottom of the text area when text is appended
-  */
-  public JTextAreaDestination (JTextArea jTextArea, boolean autoPositionCaret) {
-    jTextArea_ = jTextArea;
-    autoPositionCaret_ = autoPositionCaret;
-  }
+   /**
+   Construct a new JTextAreaDestination.
+   @param jTextArea text will be appended to this text area
+   @param autoPositionCaret if true, caret will be automatically moved 
+     to the bottom of the text area when text is appended
+   */
+   public JTextAreaDestination ( JTextArea jTextArea, boolean autoPositionCaret ) {
+      jTextArea_ = jTextArea;
+      autoPositionCaret_ = autoPositionCaret;
+   }
 
-  public JTextArea getJTextArea () { return jTextArea_; }
-  public void setJTextArea (JTextArea jTextArea) { jTextArea_ = jTextArea; }
+   public JTextArea getJTextArea () {
+      return jTextArea_;
+   }
+   public void setJTextArea ( JTextArea jTextArea ) {
+      jTextArea_ = jTextArea;
+   }
 
-  /** @return whether caret will be automatically moved to the bottom of the text area when
-    text is appended */
-  public boolean autoPositionCaret () { return autoPositionCaret_; }
+   /** @return whether caret will be automatically moved to the bottom of the text area when
+     text is appended */
+   public boolean autoPositionCaret () {
+      return autoPositionCaret_;
+   }
 
-  /** @param autoPositionCaret if true, caret will be automatically moved to the bottom of 
-    the text area when text is appended */
-  public void setAutoPositionCaret (boolean autoPositionCaret) { 
-    autoPositionCaret_ = autoPositionCaret;
-  }
+   /** @param autoPositionCaret if true, caret will be automatically moved to the bottom of
+     the text area when text is appended */
+   public void setAutoPositionCaret ( boolean autoPositionCaret ) {
+      autoPositionCaret_ = autoPositionCaret;
+   }
 
-  public void print (String s) {
-    jTextArea_.append(s);
-    if (autoPositionCaret_) {
-      jTextArea_.setCaretPosition(jTextArea_.getDocument().getLength());
-    }
-  }
+   public void print ( String s ) {
+      jTextArea_.append( s );
+      if ( autoPositionCaret_ ) {
+         jTextArea_.setCaretPosition( jTextArea_.getDocument().getLength() );
+      }
+   }
 
-  protected JTextArea jTextArea_;
-  protected boolean autoPositionCaret_;
+   protected JTextArea jTextArea_;
+   protected boolean autoPositionCaret_;
 
 }
 
--- plugins/LogViewer/trunk/src/java/logviewer/DefaultSystemInterface.java
+++ plugins/LogViewer/trunk/src/java/logviewer/DefaultSystemInterface.java
@@ -1,19 +1,19 @@
 /*
- *  Copyright (C) 2003 Don Brown (mrdon@techie.com)
- *  Copyright (C) 2000, 2001 Greg Merrill (greghmerrill@yahoo.com)
- *  This file is part of Log Viewer, a plugin for jEdit (http://www.jedit.org).
- *  It is heavily  based off Follow (http://follow.sf.net).
- *  Log Viewer is free software; you can redistribute it and/or modify
- *  it under the terms of version 2 of the GNU General Public
- *  License as published by the Free Software Foundation.
- *  Log Viewer is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  You should have received a copy of the GNU General Public License
- *  along with Log Viewer; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+*  Copyright (C) 2003 Don Brown (mrdon@techie.com)
+*  Copyright (C) 2000, 2001 Greg Merrill (greghmerrill@yahoo.com)
+*  This file is part of Log Viewer, a plugin for jEdit (http://www.jedit.org).
+*  It is heavily  based off Follow (http://follow.sf.net).
+*  Log Viewer is free software; you can redistribute it and/or modify
+*  it under the terms of version 2 of the GNU General Public
+*  License as published by the Free Software Foundation.
+*  Log Viewer is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with Log Viewer; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package logviewer;
 
 import java.awt.Cursor;
@@ -28,33 +28,33 @@ import javax.swing.JFileChooser;
  */
 class DefaultSystemInterface implements SystemInterface {
 
-    LogViewer app;
+   LogViewer app;
 
-    /**
-     *  Constructor for the DefaultSystemInterface object
-     *
-     * @param  app  The main class
-     */
-    public DefaultSystemInterface(LogViewer app) {
-        this.app = app;
-    }
+   private JFileChooser chooser = null;
 
-    /**
-     *  Gets the desired file from the user
-     *
-     * @return    The user selected file
-     */
-    public File getFileFromUser() {
-        app.setCursor(Cursor.WAIT_CURSOR);
-        JFileChooser chooser = new JFileChooser(
-                app.attributes_.getLastFileChooserDirectory()
-                );
-        app.setCursor(Cursor.DEFAULT_CURSOR);
-        int returnVal = chooser.showOpenDialog(app);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            return chooser.getSelectedFile();
-        }
-        return null;
-    }
+   /**
+    *  Constructor for the DefaultSystemInterface object
+    *
+    * @param  app  The main class
+    */
+   public DefaultSystemInterface( LogViewer app ) {
+      this.app = app;
+   }
+
+   /**
+    *  Gets the desired file from the user
+    *
+    * @return    The user selected file
+    */
+   public File getFileFromUser() {
+      File f = app.attributes_.getLastFileChooserDirectory();
+      chooser = new JFileChooser();
+      chooser.setCurrentDirectory( f );
+      int returnVal = chooser.showOpenDialog( app );
+      if ( returnVal == JFileChooser.APPROVE_OPTION ) {
+         return chooser.getSelectedFile();
+      }
+      return null;
+   }
 }
 
