530c4025e52b1bdcd5dc6cce8c264966b68a9e67
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-03-12 18:06:06 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-4-again'.

---
+++ plugins/Archive/tags/release-0-4-again/archive/ArchivePlugin.java
@@ -0,0 +1,83 @@
+/*
+ * ArchivePlugin.java
+ *
+ * :tabSize=4:indentSize=4:noTabs=true:
+ *
+ * Copyright (c) 2000, 2001, 2002 Andre Kaplan
+ * Portions copyright (C) 2004 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+package archive;
+
+import java.io.File;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.msg.VFSUpdate;
+import org.gjt.sp.util.Log;
+
+public class ArchivePlugin extends EBPlugin
+{
+    private static Object tempLock = new Object();
+    private static String tempDirectory;
+    private static int tmpFileCount;
+
+    /**
+     * This is not really safe!
+     * Check for null return value.
+     */
+    public static String tempFileName() {
+        synchronized(tempLock) {
+            if(tempDirectory == null)
+                return null;
+            else {
+                tmpFileCount++;
+                long time = System.currentTimeMillis();
+                return MiscUtilities.constructPath(tempDirectory,
+                    "cache-" + tmpFileCount + "-" + time + ".tmp");
+            }
+        }
+    }
+
+    public void start() {
+        String settingsDirectory = jEdit.getSettingsDirectory();
+        if(settingsDirectory == null)
+        {
+            Log.log(Log.WARNING,ArchiveDirectoryCache.class,"-nosettings "
+                + "command line switch specified; archive directories");
+            Log.log(Log.WARNING,ArchiveDirectoryCache.class,"will not be cached.");
+        }
+        else
+        {
+            tempDirectory = MiscUtilities.constructPath(settingsDirectory,
+                "archive");
+            new File(tempDirectory).mkdirs();
+        }
+    }
+
+    public void stop() {
+		// Clear cached directory listings
+		ArchiveDirectoryCache.clearAllCachedDirectories();
+	}
+
+    public void handleMessage(EBMessage msg) {
+        if (msg instanceof VFSUpdate) {
+            VFSUpdate vmsg = (VFSUpdate) msg;
+            ArchiveDirectoryCache.clearCachedDirectory(vmsg.getPath());
+        }
+    }
+}
+
