9f126d540c9973c8b53f82396e8c3cfc1a5a0f47
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-04-23 14:37:58 +0000

    This commit was manufactured by cvs2svn to create tag 'tags-1_1_1'.

---
+++ plugins/Tags/tags/tags-1_1_1/tags/ChooseTagList.java
@@ -0,0 +1,68 @@
+/*
+ * ChooseTagList.java
+ * Copyright (c) 2001, 2002 Kenrick Drew
+ *
+ * This file is part of TagsPlugin
+ *
+ * TagsPlugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * TagsPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * $Id$
+ */
+
+package tags;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
+
+import org.gjt.sp.jedit.*;
+
+class ChooseTagList extends JList {
+  
+  /***************************************************************************/
+  private ExuberantCTagsParser parser_;
+  private Vector tagIdentifiers_;
+  private View view_;
+  
+  /***************************************************************************/
+	public ChooseTagList(View view, ExuberantCTagsParser parser) {
+    super();
+
+    view_ = view;
+    parser_ = parser;
+    
+    // Setup items for JList
+    tagIdentifiers_ = parser_.getTagLines();
+
+    if (tagIdentifiers_ != null)
+      setListData(tagIdentifiers_);
+    
+    // Setup JList
+    /* Generally 8 is the magic number for the number of visible items/rows in 
+     * a list or menu, but we do 8 to 12 b/c each item is actually 2 or 3 rows
+     */
+    if (tagIdentifiers_ != null)
+      setVisibleRowCount(Math.min(tagIdentifiers_.size(),4));  
+    else
+      setVisibleRowCount(4);        
+    setSelectedIndex(0);
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    // Setup renderer
+    setCellRenderer(new TagListCellRenderer());
+	}
+
+}
