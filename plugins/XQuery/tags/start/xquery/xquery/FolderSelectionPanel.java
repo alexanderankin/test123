0b75f70eae49eeeb2a5f7ab2ea84ca45f273b189
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2004-05-19 10:14:33 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ plugins/XQuery/tags/start/xquery/xquery/FolderSelectionPanel.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Feb 11, 2004
+ * @author Wim Le Page
+ * @author Pieter Wellens
+ *
+ */
+package xquery;
+
+import java.awt.event.ActionEvent;
+
+
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.browser.VFSBrowser;
+
+/**
+ * @author Wim Le Page
+ * @author Pieter Wellens
+ * @version Feb 11, 2004
+ *
+ */
+public class FolderSelectionPanel extends SelectionPanel {
+
+	/**
+	 * @param view
+	 * @param propLabel
+	 */
+	public FolderSelectionPanel(View view, String propLabel) {
+		super(view, propLabel);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent event) {
+		String[] selections = GUIUtilities.showVFSFileDialog(jEdit.getActiveView(), "", VFSBrowser.CHOOSE_DIRECTORY_DIALOG, false);
+		  if(selections != null) {
+			sourceField.setText(selections[0]);
+			jEdit.setProperty(propLabel + ".last-source", selections[0]);
+		  }	
+	}
+
+}
