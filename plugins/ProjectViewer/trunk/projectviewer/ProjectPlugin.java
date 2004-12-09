a7d085f31fab347110c02622168ca20df9e2329e
Marcelo Vanzin <1694083+vanzin@users.noreply.github.com>
2004-12-09 05:24:35 +0000

    - create a PluginUpdate event for all loaded plugins at load time, even if they're not activated yet.

--- plugins/ProjectViewer/trunk/projectviewer/ProjectPlugin.java
+++ plugins/ProjectViewer/trunk/projectviewer/ProjectPlugin.java
@@ -134,15 +134,12 @@ public final class ProjectPlugin extends EBPlugin {
 		// check plugins that are already loaded
 		EditPlugin[] plugins = jEdit.getPlugins();
 		for (int i = 0; i < plugins.length; i++) {
-			if (jEdit.getBooleanProperty("plugin.projectviewer." +
-					plugins[i].getClassName() + ".pv-extension")) {
-				// create a "fake" PluginUpdate message
-				PluginUpdate msg =
-					new PluginUpdate(plugins[i].getPluginJAR(),
-										PluginUpdate.LOADED,
-										false);
-				checkPluginUpdate(msg);
-			}
+			// create a "fake" PluginUpdate message
+			PluginUpdate msg =
+				new PluginUpdate(plugins[i].getPluginJAR(),
+									PluginUpdate.LOADED,
+									false);
+			checkPluginUpdate(msg);
 		}
 		// check to see if we should activate some project when loading
 		final VPTNode last = ProjectViewerConfig.getInstance().getLastNode();
