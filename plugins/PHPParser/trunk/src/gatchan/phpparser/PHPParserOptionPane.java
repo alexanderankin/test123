0cb023a5f1a82dbac67d0ae3855f933144ebfbeb
Matthieu Casanova <chocolat.mou@gmail.com>
2004-12-20 14:27:21 +0000

    Warnings added for all ugly syntax

--- plugins/PHPParser/trunk/src/gatchan/phpparser/PHPParserOptionPane.java
+++ plugins/PHPParser/trunk/src/gatchan/phpparser/PHPParserOptionPane.java
@@ -15,6 +15,15 @@ import java.awt.event.ActionEvent;
  */
 public class PHPParserOptionPane extends AbstractOptionPane {
   private JCheckBox shortOpenTag;
+  private JCheckBox forEndFor;
+  private JCheckBox switchEndSwitch;
+  private JCheckBox ifEndSwitch;
+  private JCheckBox whileEndWhile;
+  public static final String PROP_WARN_SHORT_OPENTAG = "gatchan.phpparser.warnings.shortOpenTag";
+  public static final String PROP_WARN_FORENDFOR = "gatchan.phpparser.warnings.forEndFor";
+  public static final String PROP_WARN_SWITCHENDSWITCH = "gatchan.phpparser.warnings.switchEndSwitch";
+  public static final String PROP_WARN_IFENDIF = "gatchan.phpparser.warnings.ifEndSwitch";
+  public static final String PROP_WARN_WHILEENDWHILE = "gatchan.phpparser.warnings.whileEndWhile";
 
   public PHPParserOptionPane() {
     super("gatchan.phpparser.files");
@@ -24,12 +33,32 @@ public class PHPParserOptionPane extends AbstractOptionPane {
     JLabel warningLabels = new JLabel("Warnings");
 
     shortOpenTag = new JCheckBox("'<?' used instead of '<?php'");
-    shortOpenTag.setSelected(jEdit.getBooleanProperty("gatchan.phpparser.warnings.shortOpenTag"));
+    shortOpenTag.setSelected(jEdit.getBooleanProperty(PROP_WARN_SHORT_OPENTAG));
+
+    forEndFor= new JCheckBox("report for() : endfor; syntax");
+    forEndFor.setSelected(jEdit.getBooleanProperty(PROP_WARN_FORENDFOR));
+
+    switchEndSwitch= new JCheckBox("report switch() : switch; syntax");
+    switchEndSwitch.setSelected(jEdit.getBooleanProperty(PROP_WARN_SWITCHENDSWITCH));
+
+    ifEndSwitch= new JCheckBox("report if() : endif; syntax");
+    ifEndSwitch.setSelected(jEdit.getBooleanProperty("gatchan.phpparser.warnings.ifEndSwitch"));
+
+    whileEndWhile= new JCheckBox("report while() : endwhile; syntax");
+    whileEndWhile.setSelected(jEdit.getBooleanProperty(PROP_WARN_WHILEENDWHILE));
     addComponent(warningLabels);
     addComponent(shortOpenTag);
+    addComponent(forEndFor);
+    addComponent(switchEndSwitch);
+    addComponent(ifEndSwitch);
+    addComponent(whileEndWhile);
   }
 
   protected void _save() {
-    jEdit.setBooleanProperty("gatchan.phpparser.warnings.shortOpenTag", shortOpenTag.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_SHORT_OPENTAG, shortOpenTag.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_FORENDFOR, forEndFor.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_SWITCHENDSWITCH, switchEndSwitch.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_IFENDIF, ifEndSwitch.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_WHILEENDWHILE, whileEndWhile.isSelected());
   }
 }
