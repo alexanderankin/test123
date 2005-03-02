43bd8feb80a8ecee6bae1431fb49f0f2a5cb1adc
Matthieu Casanova <chocolat.mou@gmail.com>
2005-03-02 10:52:16 +0000

    refactoring with message type DELETED

--- plugins/PHPParser/trunk/src/gatchan/phpparser/project/PHPProjectChangedMessage.java
+++ plugins/PHPParser/trunk/src/gatchan/phpparser/project/PHPProjectChangedMessage.java
@@ -8,12 +8,25 @@ import org.gjt.sp.jedit.EBMessage;
 public final class PHPProjectChangedMessage extends EBMessage {
   private final Project selectedProject;
 
-  public PHPProjectChangedMessage(Object source,Project selectedProject) {
+  public static final Object SELECTED = "SELECTED";
+  public static final Object DELETED = "DELETED";
+
+  private Object what;
+  public PHPProjectChangedMessage(Object source,Project selectedProject,Object what) {
     super(source);
     this.selectedProject = selectedProject;
+    this.what = what;
   }
 
   public Project getSelectedProject() {
     return selectedProject;
   }
+
+  public Object getWhat() {
+    return what;
+  }
+
+  public String toString() {
+    return "PHPProjectChangedMessage "+what+' '+selectedProject;
+  } //}}}
 }
