2259af02e7a73f43378f6af45e0bcc0ad91ff1db
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-04-03 00:50:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v_1_02'.

---
+++ plugins/SQL/tags/v_1_02/sql/preprocessors/VariableSubstitutor.java
@@ -0,0 +1,79 @@
+/**
+ * VariableSubstitutor.java - Sql Plugin
+ * Copyright (C) 2001 Sergey V. Udaltsov
+ * svu@users.sourceforge.net
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package sql.preprocessors;
+
+import javax.swing.*;
+
+import sql.*;
+
+/**
+ *  Description of the Class
+ *
+ * @author     svu
+ */
+public class VariableSubstitutor extends Preprocessor
+{
+  protected final static String VAR_SUBST_PATTERN = "=?";
+
+
+  /**
+   *Description of the Method
+   *
+   * @param  text  Description of Parameter
+   * @return       Description of the Returned Value
+   * @since
+   */
+  public String doProcess( String text )
+  {
+    int curPos = text.indexOf( VAR_SUBST_PATTERN );
+
+    while ( curPos > -1 )
+    {
+      text = substituteFragment( text, curPos + 1 );
+      curPos = text.indexOf( VAR_SUBST_PATTERN, curPos + VAR_SUBST_PATTERN.length() );
+    }
+
+    return text;
+  }
+
+
+  /**
+   *Description of the Method
+   *
+   * @param  text  Description of Parameter
+   * @param  pos   Description of Parameter
+   * @return       Description of the Returned Value
+   * @since
+   */
+  public String substituteFragment( String text, int pos )
+  {
+    final String value = JOptionPane.showInputDialog( view,
+        text.substring( 0,
+        pos ) );
+    if ( value == null )
+      return text;
+    return text.substring( 0, pos ) +
+        value +
+        text.substring( pos + 1 );
+  }
+
+}
+
