8789e51667ddcb91749d53aaf326d34bddb06d46
copy_paste <copy_paste@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-06-03 02:41:13 +0000

    Add XPath auto-complete feature. This triggers on / and [ characters in the XPath expression to popup a list of possible completions. This correctly handles as many syntax combinations as possible without being a full XPath expression parser. It currently doesn't handle expressions that use XPath functions or nested filters. For these, no syntax completions are presented. Add DOM document caching. This is done as an optimazation so the buffer or file will not have to be re-parsed while typing. This required handling of EditBus and BufferChanged notifications so the cached document can be removed when the buffer is changed, reloaded or closed. Add Integration of hypersearch with the node set fragments panel. When text is selected in the panel, right click to hypersearch with the selected text. This required removing the read-only attribute of the panel's textarea.

--- plugins/XSLT/trunk/xslt/XPathExpressionPanel.java
+++ plugins/XSLT/trunk/xslt/XPathExpressionPanel.java
@@ -21,19 +21,39 @@
 package xslt;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Window;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.xml.transform.TransformerException;
 
+import org.apache.xpath.XPathAPI;
+import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.gui.DefaultFocusComponent;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Panel housing the "XPath Expression" label & text area.
@@ -50,10 +70,13 @@ public class XPathExpressionPanel extends JPanel implements KeyListener,
 	private int historyLength = Integer.parseInt(jEdit.getProperty("xpath.expression.history-length"));
 	private int historyIndex;
 
+    private Window popup;
+    private JList popupList;
+    private View view;
 
-	public XPathExpressionPanel() {
+	public XPathExpressionPanel(View viewParm) {
 		super(new BorderLayout());
-
+		view = viewParm;
 		String label = jEdit.getProperty("xpath.expression.label");
 		String text = jEdit.getProperty(LAST_EXPRESSION);
 
@@ -65,8 +88,154 @@ public class XPathExpressionPanel extends JPanel implements KeyListener,
 		add(new JScrollPane(textArea));
 
 		historyIndex = getExpressionList().size() - 1;
+
+		textArea.addCaretListener(new CaretListener() {
+            public void caretUpdate(javax.swing.event.CaretEvent evt) {
+            	textAreaCaretUpdate(evt);
+            }
+        });
+        
+		popupList = new JList();
+        popupList.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                String selection=(String)popupList.getModel().getElementAt(popupList.locationToIndex(e.getPoint()));
+                textArea.append(selection);
+                popup.hide();
+                textArea.requestFocus();
+            }
+        });
+        
+        popupList.addKeyListener(new java.awt.event.KeyAdapter() {
+        	public void keyTyped(java.awt.event.KeyEvent evt) {
+        		if (popup == null)
+        			return;
+	            if (evt.getKeyChar() == '@' || evt.getKeyChar() == '/') {
+	            	popup.hide();
+	            	textArea.append(Character.toString(evt.getKeyChar()));        		
+	        	}
+        	}
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+        		if (popup == null)
+        			return;
+                if (evt.getKeyCode() == KeyEvent.VK_ESCAPE ||
+                		(evt.getKeyCode() == KeyEvent.VK_BACK_SPACE
+                		|| evt.getKeyCode() == KeyEvent.VK_RIGHT
+						|| evt.getKeyCode() == KeyEvent.VK_LEFT)) {
+                    popup.hide();
+                    textArea.requestFocus();
+                } else if (evt.getKeyCode() == KeyEvent.VK_ENTER || evt.getKeyCode() == KeyEvent.VK_TAB) {
+                    int curSel = popupList.getSelectedIndex();
+                    String selection=(String)popupList.getModel().getElementAt(curSel);
+                    textArea.append(selection);
+                    popup.hide();
+                    textArea.requestFocus();
+                }
+            }
+        });
+
 	}
 
+	private void textAreaCaretUpdate(CaretEvent evt) {
+        XPathTool xpathTool = (XPathTool)view.getDockableWindowManager().getDockable("xpath-tool");
+        if (!xpathTool.isAutoCompleteEnabled())
+        	return;
+        int dot = evt.getDot();
+        String txt = textArea.getText().trim();
+        if (dot < txt.length())
+        	return;
+        Rectangle caretCoords = new Rectangle(0,0,0,0);
+        try {
+            caretCoords = textArea.modelToView(dot);
+        } catch (BadLocationException ble) {
+            return;
+        }
+        if (caretCoords == null)
+        	return;
+        Point caretPos = new Point(caretCoords.x, caretCoords.y);
+        SwingUtilities.convertPointToScreen(caretPos, textArea);
+		
+        txt = txt.replaceAll("\n", "");
+        String wildCardExpr = null;
+        short elementSearchType = 0;
+        // TODO: display completions for string constants
+        // TODO: correctly handle nested [
+        if (txt.endsWith("/")) {
+        	wildCardExpr = txt.concat("*");
+        	elementSearchType = Node.ELEMENT_NODE;
+        } else if (txt.endsWith("::")) {
+        	for (int ndx = txt.length() - 1; ndx >= 0; ndx--) {
+        		if (txt.charAt(ndx) == '/')
+        			break;
+        		if (txt.charAt(ndx) == '[') {
+        			txt = txt.substring(0, ndx).concat("/").concat(txt.substring(ndx + 1));
+        			break;
+        		}
+        	}
+        	wildCardExpr = txt.concat("*");
+        	elementSearchType = Node.ELEMENT_NODE;
+        } else if (txt.endsWith("@")) {
+        	for (int ndx = txt.length() - 1; ndx >= 0; ndx--) {
+        		if (txt.charAt(ndx) == '/')
+        			break;
+        		if (txt.charAt(ndx) == '[') {
+        			txt = txt.substring(0, ndx).concat("/@");
+        			break;
+        		}
+        	}
+        	wildCardExpr = txt.concat("*");
+        	elementSearchType = Node.ATTRIBUTE_NODE;	
+        } else if (txt.endsWith("[")) {
+        	int bracketPos = txt.lastIndexOf("[");
+        	wildCardExpr = txt.substring(0, bracketPos).concat("/*");
+        	elementSearchType = Node.ELEMENT_NODE;
+        }
+        
+        if (wildCardExpr != null) {
+			Document xdoc = null;
+			try {
+				xdoc = xpathTool.getCurrentDocument();
+			} catch (Exception e) {
+				XSLTPlugin.processException(e, jEdit.getProperty("xpath.result.message.buffer-unparseable"), 
+						XPathExpressionPanel.this);
+			}
+			popupList.setListData(new Object[]{""});
+			NodeList nodes = null;
+			try {
+				nodes = XPathAPI.selectNodeList(xdoc, wildCardExpr);
+			} catch (TransformerException e1) {
+				return;
+			}
+			SortedSet names = new TreeSet();
+			if (nodes.getLength() == 0)
+				return;
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Node node = nodes.item(i);
+				if (node.getNodeType() != elementSearchType)
+					continue;
+				names.add(node.getNodeName());
+			}
+			Object namelist[] = new Object[names.size()];
+			namelist = names.toArray();
+			popupList.setListData(namelist);     
+			popupList.setSelectedIndex(0);
+	        
+	        //Container cc = xpathTool.getParent();
+			Container cc = (Container)xpathTool;
+			JFrame jf = (JFrame)view;
+			while ((cc = cc.getParent()) != null) {
+				try {
+					jf = (JFrame)cc;
+				} catch (Exception ex) {
+				}
+			}
+	        popup = new Window(jf);
+	        popup.add(new JScrollPane(popupList));
+	        popup.pack();
+
+	        popup.setLocation(caretPos.x + 5, caretPos.y);
+            popup.show();
+        }
+	}
 
 	/**
 	 * Stores the current expression at the end of the expression history, and then returns the expression.
@@ -182,4 +351,5 @@ public class XPathExpressionPanel extends JPanel implements KeyListener,
 			return false;
 		}
 	}
+
 }
