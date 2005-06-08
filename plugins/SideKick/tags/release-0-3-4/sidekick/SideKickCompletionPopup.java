7ff938756781c3b52fa69d5bb6a840cfce5944f4
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-06-08 21:19:40 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-3-4'.

---
+++ plugins/SideKick/tags/release-0-3-4/sidekick/SideKickCompletionPopup.java
@@ -0,0 +1,342 @@
+/*
+ * SideKickCompletionPopup.java - Completer popup
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright 2000, 2005 Slava Pestov
+ *		   2005 Robert McKinnon
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package sidekick;
+
+//{{{ Imports
+import javax.swing.event.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import org.gjt.sp.jedit.gui.KeyEventWorkaround;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.*;
+//}}}
+
+public class SideKickCompletionPopup extends JWindow
+{
+	//{{{ fitInScreen() method
+	public static Point fitInScreen(Point p, Window w, int lineHeight)
+	{
+		Rectangle screenSize = w.getGraphicsConfiguration().getBounds();
+		if(p.y + w.getHeight() >= screenSize.height)
+			p.y = p.y - w.getHeight() - lineHeight;
+		return p;
+	} //}}}
+	
+	//{{{ SideKickCompletionPopup constructor
+	public SideKickCompletionPopup(View view, SideKickParser parser,
+		int caret, SideKickCompletion complete)
+	{
+		super(view);
+
+		this.view = view;
+		this.parser = parser;
+		this.textArea = view.getTextArea();
+
+		list = new JList();
+
+		list.addMouseListener(new MouseHandler());
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		// stupid scrollbar policy is an attempt to work around
+		// bugs people have been seeing with IBM's JDK -- 7 Sep 2000
+		JScrollPane scroller = new JScrollPane(list,
+			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		getContentPane().add(scroller, BorderLayout.CENTER);
+
+		KeyHandler keyHandler = new KeyHandler();
+		addKeyListener(keyHandler);
+		getRootPane().addKeyListener(keyHandler);
+		list.addKeyListener(keyHandler);
+		list.addListSelectionListener(new ListHandler());
+		view.setKeyEventInterceptor(keyHandler);
+
+		GUIUtilities.requestFocus(this,list);
+
+		this.complete = complete;
+		updateListModel();
+
+		Point location = textArea.offsetToXY(caret - complete.getTokenLength());
+		location.y += textArea.getPainter().getFontMetrics().getHeight();
+
+		SwingUtilities.convertPointToScreen(location,
+			textArea.getPainter());
+
+		setLocation(fitInScreen(location,this,
+			textArea.getPainter().getFontMetrics()
+			.getHeight()));
+
+		handleFocusOnDispose = true;
+		textAreaFocusListener = new FocusAdapter() {
+			public void focusLost(FocusEvent e) {
+				handleFocusOnDispose = false;
+				dispose();
+			}
+		};
+
+		show();
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				textArea.requestFocus(); // fix for focus problems under windows - Rob
+                textArea.addFocusListener(textAreaFocusListener);
+			}
+		});
+	} //}}}
+
+	//{{{ dispose() method
+	public void dispose()
+	{
+		view.setKeyEventInterceptor(null);
+		textArea.removeFocusListener(textAreaFocusListener);
+		super.dispose();
+		if (handleFocusOnDispose) {
+			SwingUtilities.invokeLater(new Runnable()
+			{
+				public void run()
+				{
+					view.getTextArea().requestFocus();
+				}
+			});
+		}
+	} //}}}
+
+	//{{{ Private members
+
+	//{{{ Instance variables
+	private View view;
+	private JEditTextArea textArea;
+	private JList list;
+	private SideKickParser parser;
+	private SideKickCompletion complete;
+	private FocusListener textAreaFocusListener;
+	private boolean handleFocusOnDispose;
+	//}}}
+
+	//{{{ updateListModel() method
+	private void updateListModel()
+	{
+		if(complete == null || complete.size() == 0)
+		{
+			list.setListData(new String[] {
+				jEdit.getProperty("sidekick-complete.none")
+			});
+			list.setCellRenderer(new DefaultListCellRenderer());
+			list.setVisibleRowCount(1);
+		}
+		else
+		{
+			setListModel(complete);
+			list.setCellRenderer(complete.getRenderer());
+			list.setVisibleRowCount(Math.min(8,complete.size()));
+			list.setFixedCellHeight(list.getCellBounds(0,0).height);
+		}
+
+		pack();
+	} //}}}
+
+	//{{{ setListModel() method
+	private void setListModel(final SideKickCompletion items)
+	{
+		ListModel model = new ListModel()
+		{
+			public int getSize()
+			{
+				return items.size();
+			}
+
+			public Object getElementAt(int index)
+			{
+				return items.get(index);
+			}
+
+			public void addListDataListener(ListDataListener l) {}
+			public void removeListDataListener(ListDataListener l) {}
+		};
+
+		list.setModel(model);
+		list.setSelectedIndex(0);
+	} //}}}
+
+	//{{{ updateSelection() method
+	void updateSelection()
+	{
+		int index = list.getSelectedIndex();
+		if(index == -1)
+			return;
+
+		String description = complete.getCompletionDescription(index);
+		view.getStatus().setMessageAndClear(description);
+	} //}}}
+
+	//}}}
+
+	//{{{ KeyHandler class
+	class KeyHandler extends KeyAdapter
+	{
+		//{{{ keyPressed() method
+		public void keyPressed(KeyEvent evt)
+		{
+			evt = KeyEventWorkaround.processKeyEvent(evt);
+			if(evt == null)
+				return;
+
+			switch(evt.getKeyCode())
+			{
+			case KeyEvent.VK_ENTER:
+				keyTyped('\n');
+				evt.consume();
+				break;
+			case KeyEvent.VK_TAB:
+				keyTyped('\t');
+				evt.consume();
+				break;
+			case KeyEvent.VK_ESCAPE:
+				dispose();
+				evt.consume();
+				break;
+			case KeyEvent.VK_UP:
+				int selected = list.getSelectedIndex();
+
+				if(selected == 0)
+					selected = list.getModel().getSize() - 1;
+				else if(getFocusOwner() == list)
+					return;
+				else
+					selected = selected - 1;
+
+				list.setSelectedIndex(selected);
+				list.ensureIndexIsVisible(selected);
+
+				evt.consume();
+				break;
+			case KeyEvent.VK_DOWN:
+				/* int */ selected = list.getSelectedIndex();
+
+				if(selected == list.getModel().getSize() - 1)
+					selected = 0;
+				else if(getFocusOwner() == list)
+					return;
+				else
+					selected = selected + 1;
+
+				list.setSelectedIndex(selected);
+				list.ensureIndexIsVisible(selected);
+
+				evt.consume();
+				break;
+			case KeyEvent.VK_SPACE:
+				break;
+			case KeyEvent.VK_BACK_SPACE:
+				 if(parser.canHandleBackspace())
+				 {
+					 keyPressedDefault(evt);
+				 }
+				 else {
+					 dispose();
+					 view.processKeyEvent(evt,true);
+				 }
+				 break;
+			case KeyEvent.VK_DELETE:
+				dispose();
+				view.processKeyEvent(evt,true);
+				break;
+			default:
+				keyPressedDefault(evt);
+				break;
+			}
+		} //}}}
+
+		private void keyPressedDefault(KeyEvent evt) {
+			// from DefaultInputHandler
+			if(!(evt.isControlDown() || evt.isAltDown() || evt.isMetaDown()))
+			{
+				if(!evt.isActionKey())
+				{
+					return;
+				}
+			}
+
+			dispose();
+			view.processKeyEvent(evt,true);
+			return;
+		}
+
+		//{{{ keyTyped() method
+		public void keyTyped(KeyEvent evt)
+		{
+			evt = KeyEventWorkaround.processKeyEvent(evt);
+			if(evt == null)
+				return;
+
+			char ch = evt.getKeyChar();
+			if(ch == '\b' && !parser.canHandleBackspace())
+				return;
+
+			keyTyped(ch);
+
+			evt.consume();
+		} //}}}
+
+		//{{{ keyTyped() method
+		private void keyTyped(char ch)
+		{
+			if(complete == null)
+			{
+				view.getTextArea().userInput(ch);
+				dispose();
+			}
+			else if(complete.handleKeystroke(list.getSelectedIndex(), ch))
+			{
+				EditPane editPane = view.getEditPane();
+				int caret = editPane.getTextArea().getCaretPosition();
+				if(!complete.updateInPlace(editPane, caret))
+				{
+					complete = parser.complete(editPane, caret);
+				}
+				updateListModel();
+			}
+			else {
+				dispose();
+			}
+		} //}}}
+	} //}}}
+
+	//{{{ ListHandler class
+	class ListHandler implements ListSelectionListener
+	{
+		public void valueChanged(ListSelectionEvent evt)
+		{
+			updateSelection();
+		}
+	} //}}}
+	
+	//{{{ MouseHandler class
+	class MouseHandler extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent evt)
+		{
+			// XXX
+			if(complete != null)
+				complete.handleKeystroke(list.getSelectedIndex(),'\n');
+			dispose();
+		}
+	} //}}}
+}
---
+++ plugins/SideKick/tags/release-0-3-4/sidekick/SideKickTree.java
@@ -0,0 +1,443 @@
+/*
+ * SideKickTree.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2000, 2003 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package sidekick;
+
+//{{{ Imports
+import javax.swing.event.*;
+import javax.swing.tree.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Vector;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+//}}}
+
+/**
+ * The Structure Browser dockable.  One instance is created for each View.
+ */
+public class SideKickTree extends JPanel implements EBComponent,
+DefaultFocusComponent
+{
+        //{{{ SideKickTree constructor
+        public SideKickTree(View view, boolean docked)
+        {
+                super(new BorderLayout());
+
+                this.view = view;
+
+                // create toolbar with parse button
+                JToolBar buttonBox = new JToolBar();
+                buttonBox.setFloatable(false);
+
+                parseBtn = new RolloverButton(GUIUtilities.loadIcon("Parse.png"));
+                parseBtn.setToolTipText(jEdit.getProperty("sidekick-tree.parse"));
+                parseBtn.setMargin(new Insets(0,0,0,0));
+                parseBtn.setRequestFocusEnabled(false);
+                parseBtn.addActionListener(buildActionListener());
+                buttonBox.add(parseBtn);
+                buttonBox.add(Box.createGlue());
+
+                add(BorderLayout.NORTH,buttonBox);
+
+                // create a faux model that will do until a real one arrives
+                DefaultTreeModel emptyModel = new DefaultTreeModel(
+                        new DefaultMutableTreeNode(null));
+                tree = buildTree(emptyModel);
+                tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+                tree.addKeyListener(new KeyHandler());
+                if(docked)
+                        tree.addMouseMotionListener(new MouseHandler());
+
+                // looks bad with the OS X L&F, apparently...
+                if(!OperatingSystem.isMacOSLF())
+                        tree.putClientProperty("JTree.lineStyle", "Angled");
+
+                tree.setVisibleRowCount(10);
+                tree.setCellRenderer(new Renderer());
+
+                add(BorderLayout.CENTER,new JScrollPane(tree));
+
+                propertiesChanged();
+
+                CaretHandler caretListener = new CaretHandler();
+
+                EditPane[] editPanes = view.getEditPanes();
+                for(int i = 0; i < editPanes.length; i++)
+                {
+                        editPanes[i].getTextArea().addCaretListener(
+                                caretListener);
+                }
+
+                update();
+        } //}}}
+
+        //{{{ focusOnDefaultComponent() method
+        public void focusOnDefaultComponent()
+        {
+                tree.requestFocus();
+        } //}}}
+
+        //{{{ addNotify() method
+        public void addNotify()
+        {
+                super.addNotify();
+                EditBus.addToBus(this);
+        } //}}}
+
+        //{{{ removeNotify() method
+        public void removeNotify()
+        {
+                super.removeNotify();
+                EditBus.removeFromBus(this);
+        } //}}}
+
+        //{{{ handleMessage() method
+        public void handleMessage(EBMessage msg)
+        {
+                if(msg instanceof EditPaneUpdate)
+                {
+                        EditPaneUpdate epu = (EditPaneUpdate)msg;
+                        EditPane editPane = epu.getEditPane();
+
+                        if(epu.getWhat() == EditPaneUpdate.CREATED)
+                                editPane.getTextArea().addCaretListener(new CaretHandler());
+                }
+                else if(msg instanceof PropertiesChanged)
+                        propertiesChanged();
+                else if(msg instanceof SideKickUpdate)
+                {
+                        if(((SideKickUpdate)msg).getView() == view)
+                                update();
+                }
+        } //}}}
+
+        //{{{ update() method
+        protected void update()
+        {
+                data = SideKickParsedData.getParsedData(view);
+                if(SideKickPlugin.getParserForBuffer(view.getBuffer()) == null
+                        || data == null)
+                {
+                        DefaultMutableTreeNode root = new DefaultMutableTreeNode(view.getBuffer().getName());
+                        root.insert(new DefaultMutableTreeNode(
+                                jEdit.getProperty("sidekick-tree.not-parsed")),0);
+
+                        tree.setModel(new DefaultTreeModel(root));
+                }
+                else
+                {
+                        tree.setModel(data.tree);
+                        if(treeFollowsCaret)
+                                expandTreeAt(view.getTextArea().getCaretPosition());
+                }
+        } //}}}
+
+        //{{{ buildTree() method
+        protected JTree buildTree(DefaultTreeModel model)
+        {
+                return new CustomTree(model);
+        }//}}}
+        
+        //{{{ buildActionListener() method
+        /**
+         * Creates an action listener for the parse button.
+         */
+        protected ActionListener buildActionListener()
+        {
+                return new ActionHandler();
+        }//}}}
+        
+        //{{{ Private members
+
+        //{{{ Instance variables
+        private RolloverButton parseBtn;
+        protected JTree tree;
+
+        protected boolean treeFollowsCaret;
+
+        protected View view;
+        private Timer caretTimer;
+
+        protected SideKickParsedData data;
+        //}}}
+
+        //{{{ propertiesChanged() method
+        private void propertiesChanged()
+        {
+                treeFollowsCaret = jEdit.getBooleanProperty("sidekick-tree.follows-caret");
+        } //}}}
+
+        //{{{ expandTreeWithDelay() method
+        /**
+         * Expands the tree after a delay.  
+         * The delay timer is restarted each time this method is called.
+         */
+        protected void expandTreeWithDelay()
+        {
+                // if keystroke parse timer is running, do nothing
+                // if(keystrokeTimer != null && keystrokeTimer.isRunning())
+                        // return;
+
+                if(caretTimer != null)
+                        caretTimer.stop();
+
+                caretTimer = new Timer(0,new ActionListener()
+                {
+                        public void actionPerformed(ActionEvent evt)
+                        {
+                                JEditTextArea textArea = view.getTextArea();
+                                int caret = textArea.getCaretPosition();
+                                Selection s = textArea.getSelectionAtOffset(caret);
+                                expandTreeAt(s == null ? caret : s.getStart());
+                        }
+                });
+
+                caretTimer.setInitialDelay(500);
+                caretTimer.setRepeats(false);
+                caretTimer.start();
+        } //}}}
+
+        //{{{ expandTreeAt() method
+        protected void expandTreeAt(int dot)
+        {
+                if(data == null)
+                        return;
+
+                TreePath treePath = data.getTreePathForPosition(dot);
+                if(treePath != null)
+                {
+                        tree.expandPath(treePath);
+                        tree.setSelectionPath(treePath);
+                        tree.scrollPathToVisible(treePath);
+                }
+        } //}}}
+
+        //}}}
+
+        //{{{ Inner classes
+
+        //{{{ CustomTree class
+        /**
+         * A JTree with added mouse handling.  Other plugins providing similar trees 
+         * can extend CustomTree and override the mouse methods.
+         */
+        protected class CustomTree extends JTree
+        {
+                protected CustomTree(TreeModel model)
+                {
+                        super(model);
+                }
+
+                protected void processMouseEvent(MouseEvent evt)
+                {
+                        switch(evt.getID())
+                        {
+                        //{{{ MOUSE_PRESSED...
+                        case MouseEvent.MOUSE_PRESSED:
+                                TreePath path = getPathForLocation(
+                                        evt.getX(),evt.getY());
+                                if(path != null)
+                                {
+                                        Object value = ((DefaultMutableTreeNode)path
+                                                .getLastPathComponent()).getUserObject();
+
+                                        if(value instanceof IAsset)
+                                        {
+                                                IAsset asset = (IAsset)value;
+
+                                                JEditTextArea textArea = view.getTextArea();
+
+                                                if(evt.getClickCount() == 2)
+                                                {
+                                                        doubleClicked(view,asset,path);
+                                                }
+                                                else if(evt.isShiftDown())
+                                                {
+                                                        shiftClick(view,asset,path);
+                                                }
+                                                else if(evt.isControlDown())
+                                                {
+                                                        controlClick(view,asset,path);
+                                                }
+                                                else
+                                                        textArea.setCaretPosition(asset.getStart().getOffset());
+                                        }
+                                }
+
+                                super.processMouseEvent(evt);
+                                break; //}}}
+                        //{{{ MOUSE_EXITED...
+                        case MouseEvent.MOUSE_EXITED:
+                                view.getStatus().setMessage(null);
+                                super.processMouseEvent(evt);
+                                break; //}}}
+                        default:
+                                super.processMouseEvent(evt);
+                                break;
+                        }
+                }
+                
+                protected void doubleClicked(View view, IAsset asset, TreePath path)
+                {
+                }
+                
+                protected void shiftClick(View view, IAsset asset, TreePath path)
+                {
+                        JEditTextArea textArea = view.getTextArea();
+                        textArea.setCaretPosition(asset.getEnd().getOffset());
+                        textArea.addToSelection(
+                                new Selection.Range(
+                                        asset.getStart().getOffset(),
+                                        asset.getEnd().getOffset()));
+                }
+                
+                protected void controlClick(View view, IAsset asset, TreePath path)
+                {
+                        JEditTextArea textArea = view.getTextArea();
+                        textArea.getDisplayManager().narrow(
+                                textArea.getLineOfOffset(asset.getStart().getOffset()),
+                                textArea.getLineOfOffset(asset.getEnd().getOffset()));
+                }
+        } //}}}
+
+        //{{{ ActionHandler class
+        class ActionHandler implements ActionListener
+        {
+                public void actionPerformed(ActionEvent evt)
+                {
+                        SideKickPlugin.parse(view,true);
+                }
+        } //}}}
+
+        //{{{ CaretHandler class
+        class CaretHandler implements CaretListener
+        {
+                public void caretUpdate(CaretEvent evt)
+                {
+                        if(evt.getSource() == view.getTextArea() && treeFollowsCaret)
+                                expandTreeWithDelay();
+                }
+        } //}}}
+
+        //{{{ KeyHandler class
+        class KeyHandler extends KeyAdapter
+        {
+                public void keyPressed(KeyEvent evt)
+                {
+                        if(caretTimer != null)
+                                caretTimer.stop();
+
+                        if(evt.getKeyCode() == KeyEvent.VK_ENTER)
+                        {
+                                evt.consume();
+
+                                TreePath path = tree.getSelectionPath();
+
+                                if(path != null)
+                                {
+                                        Object value = ((DefaultMutableTreeNode)path
+                                                .getLastPathComponent()).getUserObject();
+
+                                        if(value instanceof IAsset)
+                                        {
+                                                IAsset asset = (IAsset)value;
+
+                                                JEditTextArea textArea = view.getTextArea();
+
+                                                if(evt.isShiftDown())
+                                                {
+                                                        textArea.setCaretPosition(asset.getEnd().getOffset());
+                                                        textArea.addToSelection(
+                                                                new Selection.Range(
+                                                                        asset.getStart().getOffset(),
+                                                                        asset.getEnd().getOffset()));
+                                                }
+                                                else
+                                                        textArea.setCaretPosition(asset.getStart().getOffset());
+                                        }
+                                }
+                        }
+                }
+        } //}}}
+
+        //{{{ MouseHandler class
+        class MouseHandler extends MouseMotionAdapter
+        {
+                public void mouseMoved(MouseEvent evt)
+                {
+                        TreePath path = tree.getPathForLocation(
+                                evt.getX(),evt.getY());
+                        if(path == null)
+                                view.getStatus().setMessage(null);
+                        else
+                        {
+                                Object value = ((DefaultMutableTreeNode)path
+                                        .getLastPathComponent()).getUserObject();
+
+                                if(value instanceof IAsset)
+                                {
+                                        view.getStatus().setMessage(((IAsset)value)
+                                                .getLongString());
+                                }
+                        }
+                }
+        } //}}}
+
+        //{{{ Renderer class
+        class Renderer extends DefaultTreeCellRenderer
+        {
+                public Component getTreeCellRendererComponent(JTree tree,
+                        Object value, boolean sel, boolean expanded,
+                        boolean leaf, int row, boolean hasFocus)
+                {
+                        super.getTreeCellRendererComponent(tree,value,sel,
+                                expanded,leaf,row,hasFocus);
+
+                        DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+                        Object nodeValue = node.getUserObject();
+                        if(nodeValue instanceof IAsset)
+                        {
+                                IAsset asset = (IAsset)node.getUserObject();
+
+                                setText(asset.getShortString());
+                                setIcon(asset.getIcon());
+                        }
+                        // is root?
+                        else if(node.getParent() == null)
+                        {
+                                setIcon(org.gjt.sp.jedit.browser.FileCellRenderer
+                                        .fileIcon);
+                        }
+                        else
+                                setIcon(null);
+
+                        return this;
+                }
+        } //}}}
+
+        //}}}
+}
