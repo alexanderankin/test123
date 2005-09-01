5cb40b374ee117347fc4f73f4a9cded424bb99b7
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-09-01 09:15:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_2_1'.

---
+++ plugins/PHPParser/tags/v1_2_1/src/gatchan/phpparser/PHPErrorSource.java
@@ -0,0 +1,102 @@
+package gatchan.phpparser;
+
+import errorlist.DefaultErrorSource;
+import errorlist.ErrorSource;
+import gatchan.phpparser.parser.PHPParseErrorEvent;
+import gatchan.phpparser.parser.PHPParseMessageEvent;
+import gatchan.phpparser.parser.PHPParserListener;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.util.Log;
+
+/**
+ * The PHP Error source that will receive the errors from the parser and give them to the ErrorList api.
+ *
+ * @author Matthieu Casanova
+ */
+public final class PHPErrorSource implements PHPParserListener {
+
+  private DefaultErrorSource errorSource;
+
+  private boolean shortOpenTagWarning;
+  private boolean forEndFor;
+  private boolean whileEndWhile;
+  private boolean ifEndIf;
+  private boolean switchEndSwitch;
+  private boolean foreachEndForeach;
+  private boolean unusedParameters;
+  private boolean variableMayBeUnassigned;
+  private boolean unnecessaryGlobal;
+  private boolean caseSemicolon;
+  private boolean deprecatedVarToken;
+  private boolean conditionalExpressionCheck;
+  private boolean methodFieldsNameCheck;
+
+  /** Instantiate the PHP error source. */
+  public PHPErrorSource() {
+    Log.log(Log.DEBUG, PHPErrorSource.class, "New PHPErrorSource");
+    propertiesChanged();
+  }
+
+  public void setErrorSource(DefaultErrorSource errorSource) {
+    this.errorSource = errorSource;
+    propertiesChanged();
+  }
+
+  public void parseError(PHPParseErrorEvent e) {
+    if (e.getBeginLine() == e.getEndLine()) {
+      errorSource.addError(ErrorSource.ERROR,
+                           e.getPath(),
+                           e.getBeginLine() - 1,
+                           e.getBeginColumn() - 1,
+                           e.getEndColumn(),
+                           e.getMessage());
+    } else {
+      errorSource.addError(ErrorSource.ERROR,
+                           e.getPath(),
+                           e.getBeginLine() - 1,
+                           e.getBeginColumn() - 1,
+                           e.getBeginColumn(),
+                           e.getMessage());
+    }
+  }
+
+  public void parseMessage(PHPParseMessageEvent e) {
+    if ((!shortOpenTagWarning && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_SHORT_OPEN_TAG) ||
+        (!forEndFor && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_FOR_ENDFOR_TAG) ||
+        (!ifEndIf && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_IF_ENDIF_TAG) ||
+        (!switchEndSwitch && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_SWITCH_ENDSWITCH_TAG) ||
+        (!foreachEndForeach && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_FOREACH_ENDFOREACH_TAG) ||
+        (!unusedParameters && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_UNUSED_PARAMETERS) ||
+        (!variableMayBeUnassigned && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_VARIABLE_MAY_BE_UNASSIGNED) ||
+        (!unnecessaryGlobal && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_UNNECESSARY_GLOBAL) ||
+        (!caseSemicolon && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_CASE_SEMICOLON) ||
+        (!deprecatedVarToken && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_DEPRECATED_VAR_TOKEN) ||
+        (!conditionalExpressionCheck && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_CONDITIONAL_EXPRESSION_CHECK) ||
+        (!methodFieldsNameCheck && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_METHOD_FIELD_WITH_SAME_NAME) ||
+        (!whileEndWhile && e.getMessageClass() == PHPParseMessageEvent.MESSAGE_WHILE_ENDWHILE_TAG)) {
+      return;
+    }
+    errorSource.addError(ErrorSource.WARNING,
+                         e.getPath(),
+                         e.getBeginLine() - 1,
+                         e.getBeginColumn() - 1,
+                         e.getEndColumn(),
+                         e.getMessage());
+  }
+
+  private void propertiesChanged() {
+    shortOpenTagWarning = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_SHORT_OPENTAG);
+    forEndFor = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_FORENDFOR);
+    ifEndIf = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_IFENDIF);
+    whileEndWhile = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_WHILEENDWHILE);
+    switchEndSwitch = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_SWITCHENDSWITCH);
+    foreachEndForeach = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_FOREACHENDFOREACH);
+    unusedParameters = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_UNUSED_PARAMETERS);
+    variableMayBeUnassigned = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_VARIABLE_MAY_BE_UNASSIGNED);
+    unnecessaryGlobal = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_UNNECESSARY_GLOBAL);
+    caseSemicolon = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_CASE_SEMICOLON);
+    deprecatedVarToken = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_DEPRECATED_VAR_TOKEN);
+    conditionalExpressionCheck = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_CONDITIONAL_EXPRESSION_CHECK);
+    methodFieldsNameCheck = jEdit.getBooleanProperty(PHPParserOptionPane.PROP_WARN_MESSAGE_METHOD_FIELD_WITH_SAME_NAME);
+  }
+}
---
+++ plugins/PHPParser/tags/v1_2_1/src/gatchan/phpparser/PHPParserOptionPane.java
@@ -0,0 +1,105 @@
+package gatchan.phpparser;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+import javax.swing.*;
+
+/**
+ * The option pane of the PHPParserPlugin.
+ *
+ * @author Matthieu Casanova
+ */
+public final class PHPParserOptionPane extends AbstractOptionPane {
+  private JCheckBox shortOpenTag;
+  private JCheckBox forEndFor;
+  private JCheckBox switchEndSwitch;
+  private JCheckBox ifEndSwitch;
+  private JCheckBox whileEndWhile;
+  private JCheckBox foreachEndForeach;
+
+  private JCheckBox unusedParameter;
+  private JCheckBox unassignedVariable;
+  private JCheckBox unnecessaryGlobal;
+  private JCheckBox caseSemicolon;
+  private JCheckBox php5Enabled;
+  private JCheckBox loadOnStartup;
+  private JCheckBox deprecatedVarToken;
+  private JCheckBox conditionalExpressionCheck;
+  private JCheckBox methodFieldsSameName;
+
+  public static final String PROP_PHP5_SUPPORT = "gatchan.phpparser.php5support";
+  public static final String PROP_WARN_SHORT_OPENTAG = "gatchan.phpparser.warnings.shortOpenTag";
+  public static final String PROP_WARN_FORENDFOR = "gatchan.phpparser.warnings.forEndFor";
+  public static final String PROP_WARN_SWITCHENDSWITCH = "gatchan.phpparser.warnings.switchEndSwitch";
+  public static final String PROP_WARN_IFENDIF = "gatchan.phpparser.warnings.ifEndSwitch";
+  public static final String PROP_WARN_WHILEENDWHILE = "gatchan.phpparser.warnings.whileEndWhile";
+  public static final String PROP_WARN_FOREACHENDFOREACH = "gatchan.phpparser.warnings.foreachEndForeach";
+  public static final String PROP_WARN_UNUSED_PARAMETERS = "gatchan.phpparser.warnings.methodanalysis.unusedParameters";
+  public static final String PROP_WARN_VARIABLE_MAY_BE_UNASSIGNED = "gatchan.phpparser.warnings.methodanalysis.unassignedVariable";
+  public static final String PROP_WARN_CASE_SEMICOLON = "gatchan.phpparser.warnings.warnings.caseSemicolon";
+  public static final String PROP_WARN_UNNECESSARY_GLOBAL = "gatchan.phpparser.warnings.methodanalysis.unnecessaryGlobal";
+  public static final String PROP_WARN_DEPRECATED_VAR_TOKEN = "gatchan.phpparser.warnings.deprecatedphp4.varToken";
+  public static final String PROP_WARN_CONDITIONAL_EXPRESSION_CHECK = "gatchan.phpparser.warnings.types.conditionalExpressionCheck";
+  public static final String PROP_WARN_MESSAGE_METHOD_FIELD_WITH_SAME_NAME = "gatchan.phpparser.warnings.classes.method_field_same_name";
+
+  /** Instantiate the option pane of the PHP Parser. */
+  public PHPParserOptionPane() {
+    super("gatchan.phpparser.files");
+  }
+
+  /** Initialize the form. This method is automatically called by jEdit */
+  protected void _init() {
+    addComponent(loadOnStartup = new JCheckBox(jEdit.getProperty("options.gatchan.phpparser.loadOnStartup.text")));
+    String startupMode = jEdit.getProperty("plugin.gatchan.phpparser.PHPParserPlugin.activate");
+    loadOnStartup.setSelected("startup".equals(startupMode));
+
+    addComponent(php5Enabled = createCheckBox(PROP_PHP5_SUPPORT));
+    addComponent(new JLabel("Warnings"));
+    addComponent(deprecatedVarToken = createCheckBox(PROP_WARN_DEPRECATED_VAR_TOKEN));
+    addComponent(shortOpenTag = createCheckBox(PROP_WARN_SHORT_OPENTAG));
+    addComponent(forEndFor = createCheckBox(PROP_WARN_FORENDFOR));
+    addComponent(switchEndSwitch = createCheckBox(PROP_WARN_SWITCHENDSWITCH));
+    addComponent(ifEndSwitch = createCheckBox(PROP_WARN_IFENDIF));
+    addComponent(whileEndWhile = createCheckBox(PROP_WARN_WHILEENDWHILE));
+    addComponent(foreachEndForeach = createCheckBox(PROP_WARN_FOREACHENDFOREACH));
+    addComponent(caseSemicolon = createCheckBox(PROP_WARN_CASE_SEMICOLON));
+    addComponent(conditionalExpressionCheck = createCheckBox(PROP_WARN_CONDITIONAL_EXPRESSION_CHECK));
+    addComponent(new JLabel("Method analysis"));
+    addComponent(unusedParameter = createCheckBox(PROP_WARN_UNUSED_PARAMETERS));
+    addComponent(unassignedVariable = createCheckBox(PROP_WARN_VARIABLE_MAY_BE_UNASSIGNED));
+    addComponent(unnecessaryGlobal = createCheckBox(PROP_WARN_UNNECESSARY_GLOBAL));
+
+    addComponent(new JLabel("Class analysis"));
+    addComponent(methodFieldsSameName = createCheckBox(PROP_WARN_MESSAGE_METHOD_FIELD_WITH_SAME_NAME));
+  }
+
+  private JCheckBox createCheckBox(String property) {
+    JCheckBox checkbox = new JCheckBox(jEdit.getProperty(property + ".text"));
+    checkbox.setSelected(jEdit.getBooleanProperty(property));
+    return checkbox;
+  }
+
+  /** Save the properties. This method is automatically called by jEdit */
+  protected void _save() {
+    if (loadOnStartup.isSelected()) {
+      jEdit.setProperty("plugin.gatchan.phpparser.PHPParserPlugin.activate", "startup");
+    } else {
+      jEdit.setProperty("plugin.gatchan.phpparser.PHPParserPlugin.activate", "defer");
+    }
+    jEdit.setBooleanProperty(PROP_PHP5_SUPPORT, php5Enabled.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_DEPRECATED_VAR_TOKEN, deprecatedVarToken.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_SHORT_OPENTAG, shortOpenTag.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_FORENDFOR, forEndFor.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_SWITCHENDSWITCH, switchEndSwitch.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_IFENDIF, ifEndSwitch.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_WHILEENDWHILE, whileEndWhile.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_FOREACHENDFOREACH, foreachEndForeach.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_UNUSED_PARAMETERS, unusedParameter.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_VARIABLE_MAY_BE_UNASSIGNED, unassignedVariable.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_UNNECESSARY_GLOBAL, unnecessaryGlobal.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_CASE_SEMICOLON, caseSemicolon.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_CONDITIONAL_EXPRESSION_CHECK, conditionalExpressionCheck.isSelected());
+    jEdit.setBooleanProperty(PROP_WARN_MESSAGE_METHOD_FIELD_WITH_SAME_NAME, methodFieldsSameName.isSelected());
+  }
+}
