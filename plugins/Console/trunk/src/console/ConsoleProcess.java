efcf7832330969b8b96cec403c4a10d4bb562f60
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-09-12 01:15:45 +0000

    *** empty log message ***

---
+++ plugins/Console/trunk/src/console/ConsoleProcess.java
@@ -0,0 +1,271 @@
+/*
+ * ConsoleProcess.java - A running process
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1999, 2004 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package console;
+
+//{{{ Imports
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.util.Log;
+//}}}
+
+class ConsoleProcess
+{
+	//{{{ ConsoleProcess constructor
+	ConsoleProcess(Console console, Output output,
+		Output error, String[] args, String[] env,
+		SystemShell.ConsoleState consoleState, boolean foreground)
+	{
+		this.args = args;
+		this.env = env;
+		this.currentDirectory = consoleState.currentDirectory;
+
+		if(foreground)
+		{
+			this.console = console;
+			this.output = output;
+			this.error = error;
+			this.consoleState = consoleState;
+		}
+
+		try
+		{
+			pipeIn = new PipedInputStream();
+			pipeOut = new PipedOutputStream(pipeIn);
+
+			process = ProcessRunner.getProcessRunner()
+				.exec(args, this.env, currentDirectory);
+			stdin = new InputThread(this,
+				process.getOutputStream());
+			stdin.start();
+			stdout = new StreamThread(this,
+				process.getInputStream(),null);
+			stdout.start();
+			stderr = new StreamThread(this,
+				process.getErrorStream(),
+				console.getErrorColor());
+			stderr.start();
+		}
+		catch(Exception e)
+		{
+			String[] pp = { e.toString() };
+			error.print(console.getErrorColor(),
+				jEdit.getProperty("console.shell.error",pp));
+			output.commandDone();
+			error.commandDone();
+			stop();
+		}
+	} //}}}
+
+	//{{{ detach() method
+	void detach()
+	{
+		if(console != null)
+		{
+			Object[] pp = { args[0] };
+			error.print(console.getErrorColor(),
+				jEdit.getProperty("console.shell.detached",pp));
+			output.commandDone();
+			error.commandDone();
+		}
+
+		consoleState.process = null;
+		consoleState = null;
+		console = null;
+	} //}}}
+
+	//{{{ stop() method
+	void stop()
+	{
+		if(process != null)
+		{
+			stopped = true;
+
+			stdin.abort();
+			stdout.abort();
+			stderr.abort();
+
+			try
+			{
+				pipeOut.close();
+			}
+			catch(IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+
+			process.destroy();
+			process = null;
+
+			if(console != null)
+			{
+				Object[] pp = { args[0] };
+				error.print(console.getErrorColor(),
+					jEdit.getProperty("console.shell.killed",pp));
+			}
+
+			ConsolePlugin.finishErrorParsing(console.getErrorSource());
+
+			output.commandDone();
+			error.commandDone();
+		}
+
+		if(consoleState != null)
+			consoleState.process = null;
+	} //}}}
+
+	//{{{ isRunning() method
+	boolean isRunning()
+	{
+		return (process != null);
+	} //}}}
+	
+	//{{{ getExitStatus() method
+	boolean getExitStatus()
+	{
+		return (exitCode == 0);
+	} //}}}
+
+	//{{{ getConsole() method
+	Console getConsole()
+	{
+		return console;
+	} //}}}
+	
+	//{{{ getOutput() method
+	Output getOutput()
+	{
+		return output;
+	} //}}}
+	
+	//{{{ getErrorOutput() method
+	Output getErrorOutput()
+	{
+		return error;
+	} //}}}
+	
+	//{{{ getCurrentDirectory() method
+	String getCurrentDirectory()
+	{
+		return currentDirectory;
+	} //}}}
+	
+	//{{{ getPipeInput() method
+	PipedInputStream getPipeInput()
+	{
+		return pipeIn;
+	} //}}}
+	
+	//{{{ getPipeOutput() method
+	PipedOutputStream getPipeOutput()
+	{
+		return pipeOut;
+	} //}}}
+	
+	//{{{ threadDone() method
+	synchronized void threadDone()
+	{
+		threadDoneCount++;
+		if(process == null)
+			return;
+
+		if(!stopped)
+		{
+			// we don't want unkillable processes to hang
+			// jEdit
+
+			try
+			{
+				exitCode = process.waitFor();
+			}
+			catch(InterruptedException e)
+			{
+				Log.log(Log.ERROR,this,e);
+				notifyAll();
+				return;
+			}
+			
+			try
+			{
+				pipeOut.close();
+			}
+			catch(IOException io)
+			{
+			}
+		}
+
+		if(threadDoneCount == 3)
+		{
+			if(console != null && output != null && error != null)
+			{
+				Object[] pp = { args[0], new Integer(exitCode) };
+
+				String msg = jEdit.getProperty("console.shell.exited",pp);
+
+				if(exitCode == 0)
+					error.print(console.getInfoColor(),msg);
+				else
+					error.print(console.getErrorColor(),msg);
+
+				ConsolePlugin.finishErrorParsing(
+					console.getErrorSource());
+
+				output.commandDone();
+				error.commandDone();
+				
+				jEdit.checkBufferStatus(jEdit.getActiveView());
+			}
+
+			process = null;
+
+			if(consoleState != null)
+				consoleState.process = null;
+
+			notifyAll();
+		}
+	} //}}}
+	
+	//{{{ Private members
+	private SystemShell.ConsoleState consoleState;
+	private String currentDirectory;
+	private Console console;
+	private Output output;
+	private Output error;
+	private String[] args;
+	private String[] env;
+	private Process process;
+	private InputThread stdin;
+	private StreamThread stdout;
+	private StreamThread stderr;
+	private int threadDoneCount;
+	private int exitCode;
+	private boolean stopped;
+
+	/* AWT thread writes stdin to this pipe, and the input thread
+	writes it to the process. */
+	private PipedInputStream pipeIn;
+	private PipedOutputStream pipeOut;
+	//}}}
+}
---
+++ plugins/Console/trunk/src/sae/utils/StringList.java
@@ -0,0 +1,71 @@
+/*
+ * Created on 19-Jul-2005
+ *
+ */
+package sae.utils;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Analogous to the QStringList, with some special helpers for working with
+ * Strings. Also supports my favorite operations, split and join, making it a
+ * very convenient class for dealing with tabular data.
+ * 
+ * @author sae at uvic dot ca
+ *
+ */
+public class StringList extends LinkedList<String> {
+	private static final long serialVersionUID = 4287704227878304132L;
+
+	public StringList(Object[] array) {
+        for (Object o: array)
+            add(o.toString());
+    }
+    
+    public StringList() {}
+    
+    public static StringList split(String orig, Object delim) {
+    	if (orig == null) return new StringList();
+        return new StringList(orig.split(delim.toString()));
+    }
+
+    public String toString() {
+        return join("\n");
+    }
+    
+    public static String join(Collection c, String delim) {
+        StringList sl = new StringList();
+        Iterator itr = c.iterator();
+        while (itr.hasNext()) {
+            Object o = itr.next();
+            String s = o.toString();
+            sl.add(s);
+        }
+        return sl.join(delim);
+    }
+    
+    public String join(String delim) {
+        int s = size();
+        if (s < 1) return "";
+        if (s == 1) return get(0).toString();
+        else {
+           StringBuffer retval = new StringBuffer();
+           retval.append(get(0));
+           for (int i=1; i<s; ++i) 
+                retval.append(delim + get(i));
+           return retval.toString();
+        }
+
+    }
+    
+    public static void main(String args[]) {
+           String teststr = "a,b,c,d,e,f";
+           StringList sl = StringList.split(teststr, ",");
+           String joinstr = sl.join(",");
+           assert(teststr.equals(joinstr));
+           System.out.println ("Test Passed");
+        
+    }
+
+}
