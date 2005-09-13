9a28e27806de6103b90f2d52e63b5ccab0de4a8d
Alan Ezust <1688991+ezust@users.noreply.github.com>
2005-09-13 23:31:01 +0000

    Fixed indentation. Fixed bug where toolbar only showed up in one view. Got rid of WARNINGS got rid of reference to bad xmllint.xml file

--- plugins/Console/trunk/console/commando/CommandoToolBar.java
+++ plugins/Console/trunk/console/commando/CommandoToolBar.java
@@ -22,10 +22,12 @@
 
 package console.commando;
 
-//{{{ Imports
+// {{{ Imports
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -42,101 +44,115 @@ import org.gjt.sp.jedit.msg.DynamicMenuChanged;
 
 import console.ConsolePlugin;
 
-//}}}
+// }}}
 
-public class CommandoToolBar extends JToolBar implements EBComponent {
+public class CommandoToolBar extends JToolBar implements EBComponent
+{
 	private static final long serialVersionUID = -3664442180644166264L;
-	
-	static CommandoToolBar smInstance = null;
-	static View smDockable = null;
-	
-	
-	public static void init() {
 
-		if (smInstance == null) {
-			View[] view = jEdit.getViews();
-			for (int i=0; i<view.length; ++i) {
-				if(view[i].isPlainView()) continue;
-				smDockable = view[i];
-				smInstance = new CommandoToolBar(smDockable);
-				break;
+	/**
+	 * For each view, we might add a toolbar.
+	 * This map keeps track of what
+	 * views had toolbars added to them.
+	 */
+	static HashMap smToolBarMap = new HashMap();
+
+	public static void init()
+	{
+		remove();
+		if (!jEdit.getBooleanProperty("commando.toolbar.enabled"))
+			return;
+		View[] view = jEdit.getViews();
+		for (int i = 0; i < view.length; ++i)
+		{
+			if (view[i].isPlainView())
+				continue;
+			CommandoToolBar tb = new CommandoToolBar(view[i]);
+			view[i].addToolBar(tb);
+			smToolBarMap.put(view[i], tb);
+		}
+	}
+
+	/** Remove the instance from the view */
+	public static void remove()
+	{
+		Iterator itr = smToolBarMap.keySet().iterator();
+		while (itr.hasNext()) 
+		{
+			View v = (View) itr.next();
+			CommandoToolBar tb =(CommandoToolBar) smToolBarMap.get(v);
+			if (tb != null) {
+				v.removeToolBar(tb);
 			}
 		}
-		if (smInstance == null) return;
-		smInstance.updateButtons();		
-		if(jEdit.getBooleanProperty("commando.toolbar.enabled")) {
-   			    smDockable.addToolBar(smInstance);
-		}
-		else {
-			   remove();
-		}
-}
-
-/** Remove the instance from the view */
-public static void remove() {
-	if (smInstance != null) {
-		View v = smInstance.view;
-		v.removeToolBar(smInstance);
-		smInstance = null;
+		smToolBarMap.clear();
 	}
-}
-//{{{ Private members
-private View view;
 
-// {{{ CommandoToolBar constructor
-private CommandoToolBar(View dockable) {
-	view = dockable;
-	setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-	setFloatable(false);
-	updateButtons();
-	
-} // }}}
+	// {{{ Private members
+	private View view;
 
-// {{{ addNotify() method
-public void addNotify() {
-	super.addNotify();
-	EditBus.addToBus(this);
-} // }}}
-
-// {{{ removeNotify() method
-public void removeNotify() {
-	super.removeNotify();
-	EditBus.removeFromBus(this);
-} // }}}
-
-// {{{ handleMessage() method
-public void handleMessage(EBMessage msg) {
-	if (msg instanceof DynamicMenuChanged
-			&& ConsolePlugin.MENU.equals(((DynamicMenuChanged) msg)
-					.getMenuName())) {
+	// {{{ CommandoToolBar constructor
+	private CommandoToolBar(View dockable)
+	{
+		view = dockable;
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		setFloatable(false);
 		updateButtons();
-	}
-} // }}}
 
+	} // }}}
 
-// {{{ updateButtons() method
-private void updateButtons() {
-	removeAll();
-	
-	ActionListener actionHandler = new ActionListener() {
-		public void actionPerformed(ActionEvent evt) {
-			new CommandoDialog(view, evt.getActionCommand());
+	// {{{ addNotify() method
+	public void addNotify()
+	{
+		super.addNotify();
+		EditBus.addToBus(this);
+	} // }}}
+
+	// {{{ removeNotify() method
+	public void removeNotify()
+	{
+		super.removeNotify();
+		EditBus.removeFromBus(this);
+	} // }}}
+
+	// {{{ handleMessage() method
+	public void handleMessage(EBMessage msg)
+	{
+		if (msg instanceof DynamicMenuChanged
+				&& ConsolePlugin.MENU.equals(((DynamicMenuChanged) msg)
+						.getMenuName()))
+		{
+			updateButtons();
 		}
-	};
-	
-	EditAction[] commands = ConsolePlugin.getCommandoCommands();
-	for (int i = 0; i < commands.length; i++) {
-		EditAction command = commands[i];
-		JButton button = new JButton(command.getLabel());
-		button.setActionCommand(command.getName());
-		button.addActionListener(actionHandler);
-		button.setRequestFocusEnabled(false);
-		button.setMargin(new Insets(1, 2, 1, 2));
-		add(button);
-	}
-	
-	add(Box.createGlue());
-} // }}}
+	} // }}}
 
-// }}}
+	// {{{ updateButtons() method
+	private void updateButtons()
+	{
+		removeAll();
+
+		ActionListener actionHandler = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent evt)
+			{
+				new CommandoDialog(view, evt.getActionCommand());
+			}
+		};
+
+		EditAction[] commands = ConsolePlugin.getCommandoCommands();
+		for (int i = 0; i < commands.length; i++)
+		{
+			EditAction command = commands[i];
+			JButton button = new JButton(command.getLabel());
+			button.setActionCommand(command.getName());
+			button.addActionListener(actionHandler);
+			button.setRequestFocusEnabled(false);
+			button.setMargin(new Insets(1, 2, 1, 2));
+			add(button);
+		}
+
+		add(Box.createGlue());
+	} // }}}
+
+	// }}}
 }
