07d5930467403ce1d6f40cf3edf2aedaf671b8f5
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-10-21 11:51:02 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_1'.

---
+++ plugins/RecentBufferSwitcher/tags/release_0_1/recentbuffer/RecentBufferSwitcherPlugin.java
@@ -0,0 +1,26 @@
+package recentbuffer;
+
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.*;
+
+/**
+ * The RecentBufferSwitcher plugin, shows a dialog of most recently uses buffer
+ *
+ * @author Michael Thornhill
+ * @version   $Revision: 1.1 $ $Date: 2005-10-21 11:48:39 $
+ */
+public class RecentBufferSwitcherPlugin extends EditPlugin
+{
+	public static final String NAME = "recentbufferswitcher";
+	public static final String OPTION_PREFIX = "options.recentbufferswitcher.";
+	public static BufferAccessMonitor bufAccessObj = new BufferAccessMonitor();
+	
+	/**
+	 * Default Constructor for the <tt>RecentBufferSwitcherPlugin</tt> object
+	 */
+	public void RecentBufferSwitcherPlugin() {}
+	
+	public static void openBufferSwitcher(View view) {		
+		RecentBuffer rb = new RecentBuffer(jEdit.getActiveView(), bufAccessObj);
+	}
+}
