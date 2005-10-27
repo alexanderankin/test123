61e2525f527acad15151bba3dd9c9d8f38d7b775
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-10-27 03:58:43 +0000

    Initial checkin of optional plugin - soon to contain tabbed combined options as a plugin for Jedit. [still under construction]

---
+++ plugins/Optional/trunk/optional/CombinedOptions.java
@@ -0,0 +1,41 @@
+package optional;
+
+import java.awt.Dialog;
+import java.awt.Frame;
+
+
+
+import org.gjt.sp.jedit.jEdit;
+
+/**
+ * 
+ * An OptionDialog which combines all of jEdit's options into 3 tabs on a single dialog.
+ * 
+ * @author ezust
+ *
+ */
+public class CombinedOptions extends TabbedOptionDialog 
+{
+	GlobalOptionGroup globalOptions;
+	PluginOptionGroup pluginOptions;
+	BufferOptionPane bufferOptions;
+	
+	public CombinedOptions(Dialog parent) {
+		super(parent, "options");
+		init();
+	}
+	public CombinedOptions(Frame parent) {
+		super(parent, "options");
+		init();
+	}
+	
+	public void init() {
+		String title = jEdit.getProperty("options.title");
+		setTitle(title);
+		addOptionGroup(new GlobalOptionGroup());
+		addOptionGroup(new PluginOptionGroup());
+		addOptionPane(new BufferOptionPane());
+		show();
+	}
+	
+}
---
+++ plugins/Optional/trunk/optional/OptionalPlugin.java
@@ -0,0 +1,8 @@
+package optional;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class OptionalPlugin extends EditPlugin
+{
+
+}
