d427c33bc460f963b00c520aa7a83ff12ca4aa56
Alan Ezust <1688991+ezust@users.noreply.github.com>
2005-10-27 03:58:43 +0000

    Initial checkin of optional plugin - soon to contain tabbed combined options as a plugin for Jedit. [still under construction]

---
+++ plugins/Optional/trunk/optional/OptionalOptions.java
@@ -0,0 +1,26 @@
+package optional;
+
+import javax.swing.JCheckBox;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+public class OptionalOptions extends AbstractOptionPane
+{
+	JCheckBox useCombined;
+	OptionalOptions() 
+	{
+		super("optional");
+	}
+	protected void _init() {
+		String useCombinedOptions = jEdit.getProperty("optional.combined.label");
+		useCombined = new JCheckBox(useCombinedOptions);
+		useCombined.setSelected(jEdit.getBooleanProperty("optional.combined"));
+		addComponent(useCombined);
+	}
+
+	protected void _save() {
+		boolean combined = useCombined.isSelected();
+		jEdit.setBooleanProperty("optional.combined", combined);
+	}
+}
