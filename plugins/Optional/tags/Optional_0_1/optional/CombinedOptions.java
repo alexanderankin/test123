6004bad0fa93f9a113e80e963858b30e29e5d022
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-11-05 22:27:40 +0000

    This commit was manufactured by cvs2svn to create tag 'Optional_0_1'.

---
+++ plugins/Optional/tags/Optional_0_1/optional/CombinedOptions.java
@@ -0,0 +1,50 @@
+package optional;
+
+import java.awt.Dialog;
+import java.awt.Frame;
+
+import org.gjt.sp.jedit.jEdit;
+
+/**
+ * 
+ * An OptionDialog which combines all of jEdit's options into 3 tabs on a single
+ * dialog.
+ * 
+ * @author ezust
+ * 
+ */
+public class CombinedOptions extends TabbedOptionDialog
+{
+	GlobalOptionGroup globalOptions;
+
+	PluginOptionGroup pluginOptions;
+
+	BufferOptionPane bufferOptions;
+
+	int startingIndex = 0;
+
+	public CombinedOptions(Frame parent, int tabIndex)
+	{
+		super(parent, "options");
+		startingIndex = tabIndex;
+		_init();
+
+	}
+
+	public CombinedOptions(Frame parent)
+	{
+		this(parent, 0);
+	}
+
+	public void _init()
+	{
+		String title = jEdit.getProperty("options.title");
+		setTitle(title);
+		addOptionGroup(new GlobalOptionGroup());
+		addOptionGroup(new PluginOptionGroup());
+		addOptionPane(new BufferOptionPane());
+		setSelectedIndex(startingIndex);
+		setVisible(true);
+	}
+
+}
