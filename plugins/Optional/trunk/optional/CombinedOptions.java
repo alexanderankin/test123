e241813d47163a9dcc23ef8a13f8b84bb83802fe
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-11-05 22:04:39 +0000

    Reformatted some code, added a TextListener interface for the OptionGroupPane. Add a Jedit category and fix the titles.

--- plugins/Optional/trunk/optional/CombinedOptions.java
+++ plugins/Optional/trunk/optional/CombinedOptions.java
@@ -3,35 +3,40 @@ package optional;
 import java.awt.Dialog;
 import java.awt.Frame;
 
-
-
 import org.gjt.sp.jedit.jEdit;
 
 /**
  * 
- * An OptionDialog which combines all of jEdit's options into 3 tabs on a single dialog.
+ * An OptionDialog which combines all of jEdit's options into 3 tabs on a single
+ * dialog.
  * 
  * @author ezust
- *
+ * 
  */
-public class CombinedOptions extends TabbedOptionDialog 
+public class CombinedOptions extends TabbedOptionDialog
 {
 	GlobalOptionGroup globalOptions;
+
 	PluginOptionGroup pluginOptions;
+
 	BufferOptionPane bufferOptions;
+
 	int startingIndex = 0;
-	public CombinedOptions(Frame parent, int tabIndex) {
+
+	public CombinedOptions(Frame parent, int tabIndex)
+	{
 		super(parent, "options");
 		startingIndex = tabIndex;
 		_init();
-		
+
 	}
-	
-	public CombinedOptions(Frame parent) {
+
+	public CombinedOptions(Frame parent)
+	{
 		this(parent, 0);
 	}
-	
-	public void _init() 
+
+	public void _init()
 	{
 		String title = jEdit.getProperty("options.title");
 		setTitle(title);
@@ -39,7 +44,7 @@ public class CombinedOptions extends TabbedOptionDialog
 		addOptionGroup(new PluginOptionGroup());
 		addOptionPane(new BufferOptionPane());
 		setSelectedIndex(startingIndex);
-		show();
+		setVisible(true);
 	}
-	
+
 }
