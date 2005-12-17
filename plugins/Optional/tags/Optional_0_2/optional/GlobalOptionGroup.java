f6009008069426aa4458dae15f6bcfdd0ea03808
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-12-17 03:26:49 +0000

    This commit was manufactured by cvs2svn to create tag 'Optional_0_2'.

---
+++ plugins/Optional/tags/Optional_0_2/optional/GlobalOptionGroup.java
@@ -0,0 +1,82 @@
+/*
+ * GlobalOptionGroup.java -
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1998, 2003 Slava Pestov
+ * Portions copyright (C) 1999 mike dillon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package optional;
+
+import org.gjt.sp.jedit.OptionGroup;
+
+/**
+ * A model for all of the Global Options.
+ * 
+ */
+
+public class GlobalOptionGroup extends OptionGroup
+{
+	OptionGroup root;
+	public GlobalOptionGroup()
+	{
+		this(null);
+	}
+
+	public GlobalOptionGroup(OptionGroup rootGroup)
+	{
+		super("options");
+		root = rootGroup;
+		OptionGroup jedit = new OptionGroup("jedit");
+		
+		jedit.addOptionPane("abbrevs");
+		jedit.addOptionPane("appearance");
+		jedit.addOptionPane("context");
+		jedit.addOptionPane("docking");
+		jedit.addOptionPane("editing");
+		jedit.addOptionPane("general");
+		jedit.addOptionPane("gutter");
+		jedit.addOptionPane("mouse");
+		jedit.addOptionPane("print");
+		jedit.addOptionPane("plugin-manager");
+		jedit.addOptionPane("firewall");
+		jedit.addOptionPane("save-back");
+		jedit.addOptionPane("shortcuts");
+		jedit.addOptionPane("status");
+		jedit.addOptionPane("syntax");
+		jedit.addOptionPane("textarea");
+		jedit.addOptionPane("toolbar");
+		jedit.addOptionPane("view");
+		addGroup(jedit);
+		OptionGroup browserGroup = new OptionGroup("browser");
+		browserGroup.addOptionPane("browser.general");
+		browserGroup.addOptionPane("browser.colors");
+		addGroup(browserGroup);
+	}
+	
+	void addGroup(OptionGroup group) {
+		if (root != null)
+		{
+			root.addOptionGroup(group);
+		}
+		else
+		{
+			addOptionGroup(group);
+		}
+	}
+}
