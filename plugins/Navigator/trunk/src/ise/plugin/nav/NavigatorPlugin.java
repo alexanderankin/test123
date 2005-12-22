fc74cafdf5772afe689208fc6a4fc0eb88902085
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2005-12-22 18:39:26 +0000

    Now it's properly listening to EditBus messages for when new views are created.

--- plugins/Navigator/trunk/src/ise/plugin/nav/NavigatorPlugin.java
+++ plugins/Navigator/trunk/src/ise/plugin/nav/NavigatorPlugin.java
@@ -9,162 +9,242 @@ import java.util.Iterator;
 
 import javax.swing.JComponent;
 
+import org.gjt.sp.jedit.EBMessage;
 import org.gjt.sp.jedit.EditAction;
 import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.msg.ViewUpdate;
 
 /**
  * NavigatorPlugin, mostly static methods, allows one Navigator per View.
- * @author    Dale Anson, danson@germane-software.com
- * @version   $Revision$
- * @since     Oct 25, 2003
+ * 
+ * @author Dale Anson, danson@germane-software.com
+ * @version $Revision$
+ * @since Oct 25, 2003
  */
-public class NavigatorPlugin extends EditPlugin {
+public class NavigatorPlugin extends EBPlugin
+{
 
-    /** Description of the Field */
-    public final static String NAME = "Navigator";
-    /** Description of the Field */
-    public final static String MENU = "Navigator.menu";
-    /** View/Navigator map */
-    private final static HashMap map = new HashMap();
+	/** Description of the Field */
+	public final static String NAME = "Navigator";
 
-    public void start() {
-	    View[] views = jEdit.getViews();
-	    for (int i=0; i<views.length; ++i) {
-		    createNavigator(views[i]);
-	    }
-    }
+	/** Description of the Field */
+	public final static String MENU = "Navigator.menu";
 
-    public static void setToolBars() {
-/*	    
-	    if ( jEdit.getBooleanProperty("navigator.showOnToolbar") ) {
-		    String toolBarActions = jEdit.getProperty("view.toolbar");
-		    StringTokenizer st = new StringTokenizer(toolBarActions);
-		    boolean found=false;
-		    while (st.hasMoreTokens()) {
-			    String actionName = st.nextToken();
-			    if (actionName.equals("navigator.back") ||
-				 actionName.equals("navigator.forward") ) {
-				    found = true;
-			    	    break;
-			    }
-		    }
-		    
-		    if (!found) {
-			toolBarActions = "navigator.back navigator.forward - " + toolBarActions;
-			jEdit.setProperty("view.toolbar", toolBarActions);
-		    }
-	    }
-	    */
-    }
-    
-    public void stop() {
-	/*    
-	    if ( jEdit.getBooleanProperty("navigator.showOnToolbar") ) {
-		    String toolBarActions = jEdit.getProperty("view.toolbar");
-		    StringTokenizer st = new StringTokenizer(toolBarActions);
-		    LinkedList ll = new LinkedList();
-		    boolean found=false;
-		    while (st.hasMoreTokens()) {
-			    String actionName = st.nextToken();
-			    if (actionName.equals("navigator.back") ||
-				 actionName.equals("navigator.forward") ) { 
-				    found=true;
-			    }
-			    else ll.add(actionName);
-		    }
-		    
-	
-		    if (found) {
-			    StringBuffer sb = new StringBuffer();
-			    Iterator itr = ll.iterator();
-			    while (itr.hasNext()) {
-				    sb.append(itr.next() + " ");
-			    }
-			jEdit.setProperty("view.toolbar", sb.toString());
-		    }
-	    }
-	    */
-    }
+	/** View/Navigator map */
+	private final static HashMap map = new HashMap();
 
-    /**
-     * create the menu items for the Plugins menu
-     *
-     * @param menuItems
-     */
-    public void createMenuItems( Vector menuItems ) {
-        menuItems.addElement( GUIUtilities.loadMenu( MENU ) );
-    }
+	public void start()
+	{
+		View[] views = jEdit.getViews();
+		for (int i = 0; i < views.length; ++i)
+		{
+			createNavigator(views[i]);
+		}
+		setToolBars();
+	}
 
-    /**
-     * Adds a Navigator. Navigators are tracked by view.
-     *
-     * @param view       The view for the Navigator
-     * @param navigator  The Navigator
-     */
-    public static void addNavigator( View view, Navigator navigator ) {
-        if ( view == null ) {
-            return ;
-        }
-        if ( map.containsKey( view ) )
-            return ;
-        map.put( view, navigator );
-    }
+	public static void showButtons()
+	{
+		jEdit.setBooleanProperty("navigator.showOnToolbar", true);
+		setToolBars();
+	}
 
-    public static JComponent getToolBar(View view) {
-	    Navigator nav = getNavigator(view);
-	    NavToolBar toolBar = new NavToolBar(nav);
-	    return toolBar;
-    }
-    
-    
-    public static void removeNavigator( View view ) {
-        if ( view == null ) {
-            return ;
-        }
-        if ( !map.containsKey( view ) )
-            return ;
-        map.remove( view );
-    }
+	public static void hideButtons()
+	{
+		jEdit.setBooleanProperty("navigator.showOnToolbar", false);
+		clearToolBars();
+	}
 
-    public static Navigator getNavigator( View view ) {
-        return ( Navigator ) map.get( view );
-    }
+	public static void revalidateViews()
+	{
+		View views[] = jEdit.getViews();
+		for (int i = 0; i < views.length; ++i)
+		{
+			if (views[i] != null)
+			{
+				views[i].getRootPane().revalidate();
+			}
+		}
+	}
 
-    public static Navigator createNavigator( View view ) {
-        Navigator navigator = getNavigator( view );
-        if ( navigator == null ) {
-            navigator = new Navigator( view );
-            addNavigator( view, navigator );
-        }
-        return navigator;
-    }
+	public static void setToolBars()
+	{
 
-    /**
-     * Wrapper for the 'goBack' method of the Navigator for the given view.
-     *
-     * @param view The view for the Navigator
-     */
-    public static void goBack( View view ) {
-        Navigator navigator = ( Navigator ) map.get( view );
-        if ( navigator != null ) {
-            navigator.goBack();
-        }
-    }
+		if (jEdit.getBooleanProperty("navigator.showOnToolbar"))
+		{
+			String toolBarActions = jEdit.getProperty("view.toolbar");
+			StringTokenizer st = new StringTokenizer(toolBarActions);
+			boolean found = false;
+			while (st.hasMoreTokens())
+			{
+				String actionName = st.nextToken();
+				if (actionName.equals("navigator.back")
+					|| actionName.equals("navigator.forward"))
+				{
+					found = true;
+					break;
+				}
+			}
 
-    /**
-     * Wrapper for the 'goForward' method of the Navigator for the given view.
-     *
-     * @param view The view for the Navigator
-     */
-    public static void goForward( View view ) {
-        Navigator navigator = ( Navigator ) map.get( view );
-        if ( navigator != null ) {
-            navigator.goForward();
-        }
-    }
+			if (!found)
+			{
+				toolBarActions = "navigator.back navigator.forward - "
+					+ toolBarActions;
+				jEdit.setProperty("view.toolbar", toolBarActions);
+			}
+		}
+		revalidateViews();
+	}
+
+	public void stop()
+	{
+		clearToolBars();
+	}
+
+	public static void clearToolBars()
+	{
+		if (jEdit.getBooleanProperty("navigator.showOnToolbar"))
+		{
+			String toolBarActions = jEdit.getProperty("view.toolbar");
+			StringTokenizer st = new StringTokenizer(toolBarActions);
+			LinkedList ll = new LinkedList();
+			boolean found = false;
+			while (st.hasMoreTokens())
+			{
+				String actionName = st.nextToken();
+				if (actionName.equals("navigator.back")
+					|| actionName.equals("navigator.forward"))
+				{
+					found = true;
+				}
+				else
+					ll.add(actionName);
+			}
+
+			if (found)
+			{
+				StringBuffer sb = new StringBuffer();
+				Iterator itr = ll.iterator();
+				while (itr.hasNext())
+				{
+					sb.append(itr.next() + " ");
+				}
+				jEdit.setProperty("view.toolbar", sb.toString());
+			}
+		}
+		revalidateViews();
+
+	}
+
+	/**
+	 * create the menu items for the Plugins menu
+	 * 
+	 * @param menuItems
+	 */
+	public void createMenuItems(Vector menuItems)
+	{
+		menuItems.addElement(GUIUtilities.loadMenu(MENU));
+	}
+
+	/**
+	 * Adds a Navigator. Navigators are tracked by view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 * @param navigator
+	 *                The Navigator
+	 */
+	public static void addNavigator(View view, Navigator navigator)
+	{
+		if (view == null)
+		{
+			return;
+		}
+		if (map.containsKey(view))
+			return;
+		map.put(view, navigator);
+	}
+
+	public static JComponent getToolBar(View view)
+	{
+		Navigator nav = getNavigator(view);
+		NavToolBar toolBar = new NavToolBar(nav);
+		return toolBar;
+	}
+
+	public static void removeNavigator(View view)
+	{
+		if (view == null)
+		{
+			return;
+		}
+		if (!map.containsKey(view))
+			return;
+		map.remove(view);
+	}
+
+	public static Navigator getNavigator(View view)
+	{
+		return (Navigator) map.get(view);
+	}
+
+	public static Navigator createNavigator(View view)
+	{
+		Navigator navigator = getNavigator(view);
+		if (navigator == null)
+		{
+			navigator = new Navigator(view);
+			addNavigator(view, navigator);
+		}
+		return navigator;
+	}
+
+	/**
+	 * Wrapper for the 'goBack' method of the Navigator for the given view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void goBack(View view)
+	{
+		Navigator navigator = (Navigator) map.get(view);
+		if (navigator != null)
+		{
+			navigator.goBack();
+		}
+	}
+
+	/**
+	 * Wrapper for the 'goForward' method of the Navigator for the given
+	 * view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void goForward(View view)
+	{
+		Navigator navigator = (Navigator) map.get(view);
+		if (navigator != null)
+		{
+			navigator.goForward();
+		}
+	}
+
+	public void handleMessage(EBMessage message)
+	{
+		System.out.println(message.toString());
+		if (message instanceof ViewUpdate)
+		{
+			ViewUpdate vu = (ViewUpdate) message;
+			View v = vu.getView();
+			if (vu.getWhat() == vu.CREATED)
+			{
+				createNavigator(v);
+			}
+		}
+	}
 }
-
