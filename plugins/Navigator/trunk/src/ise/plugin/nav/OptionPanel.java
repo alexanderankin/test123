86145fcdde5a0e8c55e1cd760183fd5fd1431715
Alan Ezust <1688991+ezust@users.noreply.github.com>
2005-12-28 17:37:41 +0000

    ... and Code formatting.

--- plugins/Navigator/trunk/src/ise/plugin/nav/OptionPanel.java
+++ plugins/Navigator/trunk/src/ise/plugin/nav/OptionPanel.java
@@ -8,31 +8,34 @@ import org.gjt.sp.jedit.jEdit;
 /**
  * @author Dale Anson
  */
-public class OptionPanel extends AbstractOptionPane {
+public class OptionPanel extends AbstractOptionPane
+{
 
-   private JCheckBox showOnToolbar = null;
+	private JCheckBox showOnToolbar = null;
 
-   public OptionPanel( String name ) {
-      super( name );
-   }
+	public OptionPanel(String name)
+	{
+		super(name);
+	}
 
-   public void _init() {
-	   String[] stackChoices = new String[] {
-		     jEdit.getProperty("navigator.back.label"),
-		     jEdit.getProperty("navigator.forward.label") };
+	public void _init()
+	{
+		String[] stackChoices = new String[] { jEdit.getProperty("navigator.back.label"),
+			jEdit.getProperty("navigator.forward.label") };
 
-	      addComponent( new JLabel( "<html><h3>Navigator</h3>" ) );
-	      showOnToolbar = new JCheckBox( "Show Navigator on toolbar (buggy)" );
-	      
-   }
-   
+		addComponent(new JLabel("<html><h3>Navigator</h3>"));
+		showOnToolbar = new JCheckBox("Show Navigator on toolbar (buggy)");
 
-   public void _save() {
-	   boolean useToolBars = showOnToolbar.isSelected();
-	   jEdit.setBooleanProperty( getName() + ".showOnToolbar", useToolBars  );
-   }
+	}
 
-   public String getName() {
-      return "navigator";
-   }
+	public void _save()
+	{
+		boolean useToolBars = showOnToolbar.isSelected();
+		jEdit.setBooleanProperty(getName() + ".showOnToolbar", useToolBars);
+	}
+
+	public String getName()
+	{
+		return "navigator";
+	}
 }
