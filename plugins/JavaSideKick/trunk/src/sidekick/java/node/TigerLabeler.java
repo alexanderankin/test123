450e6f8b6158ab922d858042994aee6aac0d071c
Dale <danson@grafidog.com>
2006-01-08 05:47:05 +0000

    fixed possible NPE, updated files to show BSD license

--- plugins/JavaSideKick/trunk/src/sidekick/java/node/TigerLabeler.java
+++ plugins/JavaSideKick/trunk/src/sidekick/java/node/TigerLabeler.java
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2005, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, 
+    this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, 
+    this list of conditions and the following disclaimer in the documentation 
+    and/or other materials provided with the distribution.
+    * Neither the name of the <ORGANIZATION> nor the names of its contributors 
+    may be used to endorse or promote products derived from this software without 
+    specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package sidekick.java.node;
 
@@ -24,7 +51,9 @@ public class TigerLabeler {
 
     // various icons for display
     protected static Icon CU_ICON = GUIUtilities.loadIcon( "OpenFile.png" );
+    protected static ImageIcon ERROR_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/Error.gif" ) );
     protected static ImageIcon CLASS_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/Class.gif" ) );
+    protected static ImageIcon INNER_CLASS_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/InnerClass.gif" ) );
     protected static ImageIcon EXTENDS_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/Extends.gif" ) );
     protected static ImageIcon IMPLEMENTS_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/Implements.gif" ) );
     protected static ImageIcon INTERFACE_ICON = new ImageIcon( TigerLabeler.class.getClassLoader().getResource( "sidekick/java/icons/Interface.gif" ) );
@@ -49,11 +78,14 @@ public class TigerLabeler {
         
         if ( options.getShowIcons() ) {
             switch ( tn.getOrdinal() ) {
+                case TigerNode.ERROR:
+                    icon = ERROR_ICON;
+                    break;
                 case TigerNode.COMPILATION_UNIT:
                     icon = CU_ICON;
                     break;
                 case TigerNode.CLASS:
-                    icon = CLASS_ICON;
+                    icon = ((ClassNode)tn).isInnerClass() ? INNER_CLASS_ICON : CLASS_ICON;
                     break;
                 case TigerNode.EXTENDS:
                     icon = EXTENDS_ICON;
