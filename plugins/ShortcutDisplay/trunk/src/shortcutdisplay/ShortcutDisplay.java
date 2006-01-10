4605e73472c81089726f653a7350617d442d2fe9
jchoyt <jchoyt@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-01-10 18:29:49 +0000

    remembering last window location for pop up

--- plugins/ShortcutDisplay/trunk/src/shortcutdisplay/ShortcutDisplay.java
+++ plugins/ShortcutDisplay/trunk/src/shortcutdisplay/ShortcutDisplay.java
@@ -1,6 +1,6 @@
 /*
- * :tabSize=4:indentSize=4:noTabs=true:
- * 
+ *  :tabSize=4:indentSize=4:noTabs=true:
+ *
  *
  *  $Source$
  *  Copyright (C) 2004 Jeffrey Hoyt
@@ -29,73 +29,74 @@ import org.gjt.sp.jedit.*;
 import shortcutdisplay.*;
 import org.gjt.sp.jedit.msg.*;
 import org.gjt.sp.jedit.textarea.*;
-import org.gjt.sp.util.*; 
+import org.gjt.sp.util.*;
 import org.gjt.sp.jedit.gui.*;
 
 /**
  *  Controlling class for the ShortcutDisplay popup
  *
- *@author	jchoyt
- *@created	April 29, 2004
+ *@author     jchoyt
+ *@created    April 29, 2004
  */
 public class ShortcutDisplay
 {
-	
-	static ArrayList shortcuts = null;
-	/**
-	 *  a HashMap of prefix, List of shortcuts that start with that prefix. This
-	 *  should make processing much faster as we can pre-compute the prefixes
-	 *  and drop all the Shortcuts that don't have prefixes
-	 */
-	protected static HashMap prefixes = null;
-	static ShortcutDialog currentDialog = null; 
 
-	
-	/**
-	 *  Constructor for the ShortcutDisplay object
-	 */
-	public ShortcutDisplay() { } 
+    static ArrayList shortcuts = null;
+    /**
+     *  a HashMap of prefix, List of shortcuts that start with that prefix. This
+     *  should make processing much faster as we can pre-compute the prefixes
+     *  and drop all the Shortcuts that don't have prefixes
+     */
+    protected static HashMap prefixes = null;
+    static ShortcutDialog currentDialog = null;
 
-	
-	/**
-	 *  Description of the Method
-	 *
-	 *@param  bindings  Description of the Parameter
-	 */
-	public static void displayShortcuts(Map bindings)
-	{
-		if (currentDialog != null)
-		{
-			disposeShortcuts();
-		}
-		if (!jEdit.getBooleanProperty("options.shortcuts.displaypopup", true))
-		{
-			return;
-		}
-		currentDialog = new ShortcutDialog(bindings);
-		currentDialog.pack();
-		currentDialog.show();
-	}
 
-	
-	/**
-	 *  Description of the Methodoo
-	 */
-	public static void disposeShortcuts()
-	{
-		if (currentDialog == null)
-		{
-			return;
-		}
-		else
-		{
-			currentDialog.hide();
-			currentDialog.dispose();
-			currentDialog = null;
-			return;
-		}
-	} 
+    /**
+     *  Constructor for the ShortcutDisplay object
+     */
+    public ShortcutDisplay() { }
+
+
+    /**
+     *  Description of the Method
+     *
+     *@param  bindings  Description of the Parameter
+     */
+    public static void displayShortcuts( Map bindings )
+    {
+        if ( currentDialog != null )
+        {
+            disposeShortcuts();
+        }
+        if ( !jEdit.getBooleanProperty( "options.shortcuts.displaypopup", true ) )
+        {
+            return;
+        }
+        currentDialog = new ShortcutDialog( bindings );
+        currentDialog.pack();
+        currentDialog.show();
+    }
+
+
+    /**
+     *  Description of the Methodoo
+     */
+    public static void disposeShortcuts()
+    {
+        if ( currentDialog == null )
+        {
+            return;
+        }
+        else
+        {
+            jEdit.setIntegerProperty( "shortcutdisplay.xlocation", currentDialog.getX() );
+            jEdit.setIntegerProperty( "shortcutdisplay.ylocation", currentDialog.getY() );
+            currentDialog.hide();
+            currentDialog.dispose();
+            currentDialog = null;
+            return;
+        }
+    }
 }
-
 
 
