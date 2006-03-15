e04d506ed4b7ae9ea034c6feb5d368edee9e43c1
malyvelky <malyvelky@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-03-15 16:12:38 +0000

    + comments

--- plugins/TextAutocomplete/trunk/net/jakubholy/jedit/autocomplete/TextAutocompletePane.java
+++ plugins/TextAutocomplete/trunk/net/jakubholy/jedit/autocomplete/TextAutocompletePane.java
@@ -1,5 +1,21 @@
-/**
+/* 
+ * TextAutocompletePlugin.java
+ * $id$
+ * author Jakub (Kuba) Holy, 2005
  * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package net.jakubholy.jedit.autocomplete;
 
@@ -16,7 +32,7 @@ import org.gjt.sp.jedit.jEdit;
 
 /**
  * OptionPane for the TextAutocomplete lugin.
- * @author <a href="mailto:jakubholy@jakubholy.net">Jakub Holy</a>
+ * @author Jakub Holý
  *
  */
 public class TextAutocompletePane extends AbstractOptionPane 
