16ac48965513ccf334efed3cf2292fa8090bb6c6
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-03-20 17:30:27 +0000

    This commit was manufactured by cvs2svn to create tag 'javasidekick-2-0-2'.

---
+++ plugins/JavaSideKick/tags/javasidekick-2-0-2/src/sidekick/java/JavaCompletion.java
@@ -0,0 +1,69 @@
+package sidekick.java;
+
+import java.util.List;
+
+import sidekick.java.node.*;
+
+import sidekick.SideKickCompletion;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.buffer.JEditBuffer;
+import org.gjt.sp.jedit.textarea.*;
+
+public class JavaCompletion extends SideKickCompletion {
+    
+    
+    // PARTIAL means replace all of the text
+    public static final int PARTIAL = 2;
+    
+    // DOT type means to replace everything after the last dot in the text
+    public static final int DOT = 3;
+    
+    private int insertionType = PARTIAL;
+    
+    public JavaCompletion(View view, String text, List choices) {
+        super(view, text, choices);  
+        determineInsertionType();
+    }
+    
+    public JavaCompletion(View view, String text, int type, List choices) {
+        super(view, text, choices);
+        this.insertionType = type;
+    }
+    
+    private void determineInsertionType() {
+        if (text.endsWith("."))
+            insertionType = DOT;
+        else
+            insertionType = PARTIAL;
+    }
+    
+    public void setInsertionType(int type) {
+        insertionType = type;   
+    }
+    
+    public void insert( int index ) {
+        String to_replace = text;
+        String to_insert = String.valueOf(get(index));
+
+        if (insertionType == DOT) {
+            int dot_index = text.lastIndexOf(".");
+            if (dot_index > 0) 
+                to_replace = text.substring(dot_index + 1);
+        }
+        
+        int caret = textArea.getCaretPosition();
+        Selection s = textArea.getSelectionAtOffset( caret );
+        int start = ( s == null ? caret : s.getStart() );
+        int end = ( s == null ? caret : s.getEnd() );
+        JEditBuffer buffer = textArea.getBuffer();
+        
+        try {
+            buffer.beginCompoundEdit();
+            buffer.remove( start - to_replace.length(), to_replace.length() );
+            buffer.insert( start - to_replace.length(), to_insert );
+        }
+        finally {
+            buffer.endCompoundEdit();
+        }
+    }
+}
