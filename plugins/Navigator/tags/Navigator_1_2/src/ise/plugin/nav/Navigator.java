8de7cf6e0aa03522ffb7452787eadf1771d848e7
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-04-16 05:28:11 +0000

    This commit was manufactured by cvs2svn to create tag 'Navigator_1_2'.

---
+++ plugins/Navigator/tags/Navigator_1_2/src/ise/plugin/nav/Navigator.java
@@ -0,0 +1,364 @@
+package ise.plugin.nav;
+
+import java.awt.event.*;
+import java.util.Stack;
+
+import javax.swing.*;
+
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+
+/**
+ * NavigatorPlugin for keeping track of where we were and where.
+ * 
+ * @author Dale Anson
+ * @author Alan Ezust
+ * 
+ * @version $Id$
+ */
+public class Navigator implements ActionListener
+{
+	private class DontBother extends Exception
+	{
+	}
+
+	/** Action command to go to the previous item. */
+	public final static String BACK = "back";
+
+	/** Action command to go to the next item. */
+	public final static String FORWARD = "forward";
+
+	/** Action command to indicate that it is okay to go back. */
+	public final static String CAN_GO_BACK = "canGoBack";
+
+	/** Action command to indicate that it is not okay to go back. */
+	public final static String CANNOT_GO_BACK = "cannotGoBack";
+
+	/** Action command to indicate that it is okay to go forward. */
+	public final static String CAN_GO_FORWARD = "canGoForward";
+
+	/** Action command to indicate that it is not okay to go forward. */
+	public final static String CANNOT_GO_FORWARD = "cannotGoForward";
+
+	private Stack backStack;
+
+	private Stack forwardStack;
+
+	private DefaultButtonModel backButtonModel;
+
+	private DefaultButtonModel forwardButtonModel;
+
+	private Object currentNode = null;
+
+	private int maxStackSize = 512;
+
+	private View view;
+
+	private boolean ignoreUpdates;
+
+	public Navigator(View view)
+	{
+		this(view, "");
+	}
+
+	/**
+	 * Constructor for Navigator
+	 * 
+	 * @param vw
+	 * @param position
+	 */
+	public Navigator(View vw, String position)
+	{
+		this.view = vw;
+
+		backButtonModel = new DefaultButtonModel();
+		forwardButtonModel = new DefaultButtonModel();
+		ignoreUpdates = false;
+
+		backButtonModel.setActionCommand(Navigator.BACK);
+		forwardButtonModel.setActionCommand(Navigator.FORWARD);
+		backButtonModel.addActionListener(this);
+		forwardButtonModel.addActionListener(this);
+
+		// set up the history stacks
+		backStack = new Stack();
+		forwardStack = new Stack();
+		clearStacks();
+		update();
+
+		/*
+		 * add a mouse listener to the view. Each mouse click on a text
+		 * area in the view is stored
+		 */
+
+		view.getTextArea().getPainter().addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent ce)
+			{
+				update();
+			}
+		});
+
+	}
+
+	public ButtonModel getBackModel()
+	{
+		return backButtonModel;
+	}
+
+	NavPosition currentPosition() throws DontBother
+	{
+		Buffer b = view.getBuffer();
+		JEditTextArea ta = view.getTextArea();
+		if (ta == null)
+			throw new DontBother();
+		int cp = ta.getCaretPosition();
+		if ((cp == 0) && b.getName().startsWith("Untitled"))
+			throw new DontBother();
+		NavPosition retval = new NavPosition(b, cp);
+		return retval;
+	}
+
+	/** push current position onto the forward stack */
+	public void pushForward()
+	{
+		try
+		{
+			forwardStack.push(currentPosition());
+		}
+		catch (DontBother e)
+		{
+		}
+	}
+
+	public void update()
+	{
+		if (ignoreUpdates)
+		{
+			return;
+		}
+		try
+		{
+			update(currentPosition());
+		}
+		catch (DontBother db)
+		{
+		}
+	}
+
+	public ButtonModel getForwardModel()
+	{
+		return forwardButtonModel;
+	}
+
+	/**
+	 * Updates the stacks and button state based on the given node. Pushes
+	 * the node on to the "back" history, clears the "forward" history.
+	 * 
+	 * @param node
+	 *                an instance of NavPosition.
+	 */
+	private void update(NavPosition node)
+	{
+		if (node != currentNode)
+		{
+			backStack.push(node);
+			if (backStack.size() > maxStackSize)
+				backStack.removeElementAt(0);
+		}
+		currentNode = node;
+		forwardStack.clear();
+		setButtonState();
+	}
+
+	/**
+	 * The action handler for this class. Actions can be invoked by calling
+	 * this method and passing an ActionEvent with one of the action
+	 * commands defined in this class (BACK, FORWARD, etc).
+	 * 
+	 * @param ae
+	 *                the action event to kick a response.
+	 */
+	public void actionPerformed(ActionEvent ae)
+	{
+		if (ae.getActionCommand().equals(BACK))
+		{
+			goBack();
+		}
+		else if (ae.getActionCommand().equals(FORWARD))
+		{
+			goForward();
+		}
+		else if (ae.getActionCommand().equals(CAN_GO_BACK))
+		{
+			backButtonModel.setEnabled(true);
+		}
+		else if (ae.getActionCommand().equals(CANNOT_GO_BACK))
+		{
+			backButtonModel.setEnabled(false);
+		}
+		else if (ae.getActionCommand().equals(CAN_GO_FORWARD))
+		{
+			forwardButtonModel.setEnabled(true);
+		}
+		else if (ae.getActionCommand().equals(CANNOT_GO_FORWARD))
+		{
+			forwardButtonModel.setEnabled(false);
+		}
+	}
+
+	/**
+	 * Removes an invalid node from the navigation history.
+	 * 
+	 * @param node
+	 *                an invalid node
+	 */
+	public void remove(Object node)
+	{
+		backStack.remove(node);
+		forwardStack.remove(node);
+	}
+
+	public void setMaxHistorySize(int size)
+	{
+		maxStackSize = size;
+	}
+
+	public int getMaxHistorySize()
+	{
+		return maxStackSize;
+	}
+
+	public void clearStacks()
+	{
+		backStack.clear();
+		forwardStack.clear();
+		setButtonState();
+	}
+
+	/** Sets the state of the navigation buttons. */
+	private void setButtonState()
+	{
+		backButtonModel.setEnabled(!backStack.empty());
+		forwardButtonModel.setEnabled(!forwardStack.empty());
+	}
+
+	/**
+	 * Sets the position of the cursor to the given NavPosition.
+	 * 
+	 * @param o
+	 *                The new NavPosition value
+	 */
+	public void setPosition(Object o)
+	{
+		NavPosition np = (NavPosition) o;
+		Buffer buffer = np.buffer;
+		// JEditBuffer buffer = np.buffer; // for jEdit 4.3
+		int caret = np.caret;
+
+		if (buffer.equals(view.getBuffer()))
+		{
+			// nav in current buffer, just set cursor position
+			view.getTextArea().setCaretPosition(caret, true);
+			return;
+		}
+
+		// Stop listening to EditBus events while we are changing
+		// buffers
+		ignoreUpdates = true;
+
+		// check if buffer is open
+		Buffer[] buffers = jEdit.getBuffers();
+		for (int i = 0; i < buffers.length; i++)
+		{
+			if (buffers[i].equals(buffer))
+			{
+				// found it
+				view.goToBuffer(buffer);
+				view.getTextArea().setCaretPosition(caret, true);
+				ignoreUpdates = false;
+				return;
+			}
+		}
+
+		// buffer isn't open
+		String path = buffer.getPath();
+		buffer = jEdit.openFile(view, path);
+
+		// Now we can listen to events again
+		ignoreUpdates = false;
+
+		if (buffer == null)
+		{
+			remove(np);
+			return; // nowhere to go, maybe the file got deleted?
+		}
+
+		if (caret >= buffer.getLength())
+		{
+			caret = buffer.getLength() - 1;
+		}
+		if (caret < 0)
+		{
+			caret = 0;
+		}
+		try
+		{
+			view.getTextArea().setCaretPosition(caret, true);
+		}
+		catch (NullPointerException npe)
+		{
+			// sometimes Buffer.markTokens throws a NPE here, catch
+			// it
+			// and silently ignore it.
+		}
+		view.getTextArea().requestFocus();
+	}
+
+	/** Moves to the previous item in the "back" history. */
+	public void goBack()
+	{
+		if (!backStack.empty())
+		{
+			try
+			{
+				forwardStack.push(currentPosition());
+			}
+			catch (DontBother db)
+			{
+			}
+
+			if (forwardStack.size() > maxStackSize)
+				forwardStack.removeElementAt(0);
+			currentNode = backStack.pop();
+			setPosition(currentNode);
+			setButtonState();
+		}
+
+	}
+
+	/** Moves to the next item in the "forward" history. */
+	public void goForward()
+	{
+
+		if (!forwardStack.empty())
+		{
+			try
+			{
+				currentNode = currentPosition();
+				backStack.push(currentNode);
+			}
+			catch (DontBother db)
+			{
+			}
+
+			if (backStack.size() > maxStackSize)
+				backStack.removeElementAt(0);
+			currentNode = forwardStack.pop();
+			setPosition(currentNode);
+			setButtonState();
+		}
+	}
+}
