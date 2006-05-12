14914f2ea9e34b2710a5d136d6f1cc2bf7ee254e
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-05-12 04:33:48 +0000

    Version 1.5.6 -   History, Back and Forward actions rewritten - now the current position   gets pushed onto the forward stack when back is pressed, and vice-versa.

--- plugins/InfoViewer/trunk/infoviewer/TitledURLEntry.java
+++ plugins/InfoViewer/trunk/infoviewer/TitledURLEntry.java
@@ -21,68 +21,77 @@ package infoviewer;
 
 
 /**
- * a TitledURLEntry stores an URL together with a title string. If the URL
- * represents an HTML document, the title (from the &lt;TITLE&gt; tag)
- * will be stored there. The URL is stored as string. No checking for
+ * a TitledURLEntry stores an URL together with a title string and possibly a scrollbar
+ * value. If the URL represents an HTML document, the title (from the &lt;TITLE&gt; tag) will be
+ * stored there. The URL is stored as string. No checking for
  * MalformedURLExceptions is done on the URL.
  */
-public class TitledURLEntry implements Cloneable {
-    
-    private String title = null;
-    private String url = null;
-    private int historyPos = -1;
+public class TitledURLEntry implements Cloneable
+{
 
-    /** new TitledURLEntry with title and url */    
-    public TitledURLEntry(String title, String url) {
-        this.title = title;
-        this.url = url;
-    }
+	private String title = null;
 
-    public String getTitle() {
-        return title;
-    }
-    
-    public String getURL() {
-        return url;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public void setURL(String url) {
-        this.url = url;
-    }
-     
-    /** 
-     * if this TitledURLEntry is part of a history, store its internal
-     * position number.
-     */
-    public void setHistoryPos(int historyPos) {
-        this.historyPos = historyPos;
-    }
+	private String url = null;
 
-    /** 
-     * if this TitledURLEntry is part of a history, return its internal
-     * position number.
-     * @return the internal position number of the history, or -1 if this
-     *         TitledURLEntry is not part of a history.
-     */
-    public int getHistoryPos() {
-        return historyPos;
-    }
-    
-    public TitledURLEntry getClone() {
-        TitledURLEntry e = new TitledURLEntry(title, url);
-        e.historyPos = this.historyPos;
-        return e;
-    }
+	private int scrollBarPos = -1;
 
-    public Object clone() {
-        return getClone();
-    }
-    
-    public String toString() {
-        return url;
-    }
+	/** new TitledURLEntry with title and url */
+	public TitledURLEntry(String title, String url)
+	{
+		this.title = title;
+		this.url = url;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public String getURL()
+	{
+		return url;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public void setURL(String url)
+	{
+		this.url = url;
+	}
+
+	public int getScrollBarPos() {
+		return scrollBarPos;
+	}
+	
+	public void setScrollBarPos( int newPos) {
+		scrollBarPos = newPos;
+	}
+	
+	public boolean equals(TitledURLEntry other)
+	{
+		if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+
+	public TitledURLEntry getClone()
+	{
+		TitledURLEntry e = new TitledURLEntry(title, url);
+		e.scrollBarPos = this.scrollBarPos;
+		return e;
+	}
+
+	public Object clone()
+	{
+		return getClone();
+	}
+
+	public String toString()
+	{
+		return url;
+	}
+
 }
