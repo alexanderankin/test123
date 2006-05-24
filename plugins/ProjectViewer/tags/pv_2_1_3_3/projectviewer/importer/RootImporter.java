2f6e31d4965f5729535de44e1763bb0d69b62785
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-05-24 05:01:22 +0000

    This commit was manufactured by cvs2svn to create tag 'pv_2_1_3_3'.

---
+++ plugins/ProjectViewer/tags/pv_2_1_3_3/projectviewer/importer/RootImporter.java
@@ -0,0 +1,181 @@
+/*
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package projectviewer.importer;
+
+//{{{ Imports
+import java.io.File;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+
+import java.awt.Component;
+
+import org.gjt.sp.jedit.jEdit;
+
+import projectviewer.ProjectViewer;
+import projectviewer.gui.ImportDialog;
+import projectviewer.vpt.VPTFile;
+import projectviewer.vpt.VPTNode;
+import projectviewer.vpt.VPTDirectory;
+//}}}
+
+/**
+ *	Imports files and/or directories from the project root. Optionally, can
+ *	remove all existing files under the root before doing a fresh import.
+ *
+ *	@author		Marcelo Vanzin
+ *	@version	$Id$
+ */
+public class RootImporter extends FileImporter {
+
+	//{{{ Private members
+	private boolean clean;
+	protected Component parent;
+	protected String oldRoot;
+	//}}}
+
+	//{{{ +RootImporter(VPTNode, String, ProjectViewer, Component) : <init>
+	/**
+	 *	Creates an Importer that uses a component other than the ProjectViewer
+	 *	as the parent of the dialogs shown to the user. If "oldRoot" is not null,
+	 *	files under this directory will be removed from the root node of the
+	 *	project.
+	 */
+	public RootImporter(VPTNode node, String oldRoot, ProjectViewer viewer, Component parent) {
+		super(node, viewer);
+		if (parent != null) {
+			this.parent = parent;
+		} else if (viewer != null) {
+			this.parent = viewer;
+		} else {
+			this.parent = jEdit.getActiveView();
+		}
+		clean = (oldRoot != null);
+		this.oldRoot = oldRoot;
+	} //}}}
+
+	//{{{ +RootImporter(VPTNode, ProjectViewer, boolean) : <init>
+	/**
+	 *	Imports files from the root of the project. If "clean" is "true", the
+	 *	existing nodes that are below the root of the project will be removed
+	 *	before the importing.
+	 */
+	public RootImporter(VPTNode node, ProjectViewer viewer, boolean clean) {
+		this(node, null, viewer, null);
+		this.clean = clean;
+	} //}}}
+
+	//{{{ #internalDoImport() : Collection
+	/** Asks if the user wants to import files from the chosen project root. */
+	protected Collection internalDoImport() {
+		fileCount = 0;
+
+		String dlgTitle = (project.getChildCount() == 0)
+						? "projectviewer.import.msg_proj_root.title"
+						: "projectviewer.import.msg_reimport.title";
+		ImportDialog id = getImportDialog();
+		id.setTitle(jEdit.getProperty(dlgTitle));
+		loadImportFilterStatus(project, id);
+		id.show();
+
+		if (!id.isApproved()) {
+			return null;
+		}
+
+		String state = null;
+		if (viewer != null) {
+			state = viewer.getFolderTreeState(project);
+		}
+
+		if (clean) {
+			if (oldRoot == null) {
+				oldRoot = project.getRootPath();
+			}
+			Enumeration e = project.children();
+			ArrayList toRemove = new ArrayList();
+			removed = new ArrayList();
+			while (e.hasMoreElements()) {
+				VPTNode n = (VPTNode) e.nextElement();
+				// need to handle "virtual directories", which mess up the
+				// value of getNodePath() when the path doesn't exist anymore.
+				if (n.getNodePath().startsWith(oldRoot)
+					|| (n.isDirectory()
+						&& ((VPTDirectory)n).getFile().getAbsolutePath().startsWith(oldRoot)))
+				{
+					toRemove.add(n);
+				}
+			}
+			if (toRemove.size() > 0) {
+				for (Iterator i = toRemove.iterator(); i.hasNext(); ) {
+					VPTNode n = (VPTNode) i.next();
+					if (n.isDirectory()) {
+						unregisterFiles((VPTDirectory)n);
+					} else if (n.isFile()) {
+						unregisterFile((VPTFile)n);
+					}
+					if (n.getChildCount() == 0)
+						n.removeFromParent();
+				}
+			}
+		}
+
+		addTree(new File(project.getRootPath()),
+				project,
+				id.getImportFilter(),
+				id.getFlattenFilePaths());
+		if (state != null) {
+			postAction = new NodeStructureChange(project, state);
+		}
+
+		showFileCount();
+		saveImportFilterStatus(project, id);
+		return null;
+	} //}}}
+
+	//{{{ #unregisterFiles(VPTDirectory) : void
+	/** Unregisters all files in the directory from the project, recursively. */
+	protected void unregisterFiles(VPTDirectory dir) {
+		for (int i = 0; i < dir.getChildCount(); i++) {
+			VPTNode n = (VPTNode) dir.getChildAt(i);
+			if (n.isDirectory()) {
+				unregisterFiles((VPTDirectory)n);
+				if (n.getChildCount() == 0)
+					dir.remove(i--);
+			} else if (n.isFile()) {
+				unregisterFile((VPTFile)n);
+				dir.remove(i--);
+			}
+		}
+		if (dir.getChildCount() == 0)
+			dir.removeFromParent();
+	} //}}}
+
+	//{{{ #getImportDialog() : ImportDialog
+	protected ImportDialog getImportDialog() {
+		ImportDialog dlg = super.getImportDialog();
+		dlg.hideFileChooser();
+		dlg.lockTraverse();
+		dlg.hideNewNode();
+		return dlg;
+	} //}}}
+
+}
+
---
+++ plugins/ProjectViewer/tags/pv_2_1_3_3/projectviewer/importer/VFSFileImporter.java
@@ -0,0 +1,129 @@
+/*
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package projectviewer.importer;
+
+//{{{ Imports
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.MiscUtilities;
+
+import org.gjt.sp.jedit.io.VFSManager;
+import org.gjt.sp.jedit.browser.VFSBrowser;
+
+import projectviewer.ProjectViewer;
+import projectviewer.vpt.VPTNode;
+import projectviewer.vpt.VPTFile;
+import projectviewer.vpt.VFSFile;
+import projectviewer.vpt.VPTDirectory;
+//}}}
+
+/**
+ *	Imports files from a VFS into a project. If any selected file is actually
+ *	from the "File" VFS, a normal file is added instead of using the VFS
+ *	facilities.
+ *
+ *	@author		Marcelo Vanzin
+ *	@version	$Id$
+ */
+public class VFSFileImporter extends Importer {
+
+	//{{{ Protected Members
+	protected int fileCount;
+	//}}}
+
+	//{{{ +VFSFileImporter(VPTNode, ProjectViewer) : <init>
+
+	public VFSFileImporter(VPTNode node, ProjectViewer viewer) {
+		super(node, viewer, true);
+	}
+
+	//}}}
+
+	//{{{ #internalDoImport() : Collection
+	/**
+	 *	Queries the user for files to be added by showing a jEdit file chooser
+	 *	dialog.
+	 *
+	 *	@return	A collection of VPTNode instances.
+	 */
+	protected Collection internalDoImport() {
+		fileCount = 0;
+
+		String[] chosen = null;
+		if (selected.isDirectory() && ((VPTDirectory)selected).getFile().exists()) {
+			chosen = GUIUtilities.showVFSFileDialog(viewer.getView(), selected.getNodePath() + "/",
+							VFSBrowser.OPEN_DIALOG, true);
+		} else {
+			chosen = GUIUtilities.showVFSFileDialog(viewer.getView(), project.getRootPath() + "/",
+							VFSBrowser.OPEN_DIALOG, true);
+		}
+
+		if (chosen == null || chosen.length == 0) {
+			return null;
+		}
+
+		ArrayList lst = new ArrayList();
+
+		boolean asked = false, recurse = false;
+		for (int i = 0; i < chosen.length; i++) {
+			VPTNode node = null;
+
+			if (VFSManager.getVFSForPath(chosen[i]) == VFSManager.getFileVFS()) {
+				File f;
+				if (MiscUtilities.isURL(chosen[i])) {
+					String proto = MiscUtilities.getProtocolOfURL(chosen[i]);
+					f = new File(chosen[i].substring(proto.length()));
+				} else {
+					f = new File(chosen[i]);
+				}
+				// it's a normal file
+				node = findDirectory(f, selected, false);
+				if (node == null) {
+					VPTFile file = new VPTFile(f);
+					registerFile(file);
+					lst.add(file);
+					fileCount++;
+				}
+			} else {
+				node = new VFSFile(chosen[i]);
+				lst.add(node);
+				fileCount++;
+			}
+
+		}
+
+		showFileCount();
+		return lst;
+	} //}}}
+
+	//{{{ #showFileCount() : void
+	/** Shows a message in the status bar indicating how many files were imported. */
+	protected void showFileCount() {
+		viewer.setStatus(
+			jEdit.getProperty("projectviewer.import.msg_result",
+				new Object[] { new Integer(fileCount) }));
+	} //}}}
+
+}
+
