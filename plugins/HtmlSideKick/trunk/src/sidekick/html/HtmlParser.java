5aea3b2d6a0a9c7dbf6a4ad3d2067731557acb55
Dale <danson@grafidog.com>
2006-05-30 14:58:42 +0000

    Added "go to matching tag" feature.

--- plugins/HtmlSideKick/trunk/src/sidekick/html/HtmlParser.java
+++ plugins/HtmlSideKick/trunk/src/sidekick/html/HtmlParser.java
@@ -52,7 +52,7 @@ public class HtmlParser extends SideKickParser implements EBComponent {
     public HtmlParser() {
         super( "html" );
     }
-    
+
     public void activate( EditPane editPane ) {
         super.activate( editPane );
         currentView = editPane.getView();
@@ -63,14 +63,14 @@ public class HtmlParser extends SideKickParser implements EBComponent {
         super.deactivate( editPane );
         EditBus.removeFromBus( this );
     }
-    
-    public void handleMessage(EBMessage msg) {
+
+    public void handleMessage( EBMessage msg ) {
         if ( msg instanceof PropertiesChanged ) {
             parse();
         }
     }
-    
-    
+
+
     public void parse() {
         if ( currentView != null ) {
             parse( currentView.getBuffer(), null );
@@ -85,57 +85,74 @@ public class HtmlParser extends SideKickParser implements EBComponent {
         String filename = buffer.getPath();
         SideKickParsedData parsedData = new HtmlSideKickParsedData( filename );
         DefaultMutableTreeNode root = parsedData.root;
-        
-        StringReader reader = new StringReader(buffer.getText( 0, buffer.getLength() ));
+
+        StringReader reader = new StringReader( buffer.getText( 0, buffer.getLength() ) );
         HtmlTreeBuilder builder = null;
         try {
             // parse
-            sidekick.html.parser.html.HtmlParser parser = new sidekick.html.parser.html.HtmlParser(reader);
+            sidekick.html.parser.html.HtmlParser parser = new sidekick.html.parser.html.HtmlParser( reader );
             HtmlDocument document = parser.HtmlDocument();
-            document.setShowBrackets(jEdit.getBooleanProperty("options.sidekick.html.showBrackets", true));
-            document.setShowTagAttributes(jEdit.getBooleanProperty("options.sidekick.html.showTagAttributes", true));
-            document.setShowCoreAttributes(jEdit.getBooleanProperty("options.sidekick.html.showCoreAttributes", true));
-            document.setShowLangAttributes(jEdit.getBooleanProperty("options.sidekick.html.showLangAttributes", true));
-            document.setShowScriptAttributes(jEdit.getBooleanProperty("options.sidekick.html.showScriptAttributes", true));
-            document.setShowJspTags(jEdit.getBooleanProperty("options.sidekick.html.showJspElements", true));
-            
+            document.setShowBrackets( jEdit.getBooleanProperty( "options.sidekick.html.showBrackets", true ) );
+            document.setShowTagAttributes( jEdit.getBooleanProperty( "options.sidekick.html.showTagAttributes", true ) );
+            document.setShowCoreAttributes( jEdit.getBooleanProperty( "options.sidekick.html.showCoreAttributes", true ) );
+            document.setShowLangAttributes( jEdit.getBooleanProperty( "options.sidekick.html.showLangAttributes", true ) );
+            document.setShowScriptAttributes( jEdit.getBooleanProperty( "options.sidekick.html.showScriptAttributes", true ) );
+            document.setShowJspTags( jEdit.getBooleanProperty( "options.sidekick.html.showJspElements", true ) );
+
             // collect and clean
             document.accept( new HtmlCollector() );
-            document.accept( new HtmlScrubber( HtmlScrubber.DEFAULT_OPTIONS | HtmlScrubber.TRIM_SPACES) );
-            
+            document.accept( new HtmlScrubber( HtmlScrubber.DEFAULT_OPTIONS | HtmlScrubber.TRIM_SPACES ) );
+
             // make a tree
-            builder = new HtmlTreeBuilder(root);
-            builder.setShowAll(jEdit.getBooleanProperty("options.sidekick.html.showAllElements", true));
-            builder.setShowAll(showAll);
+            builder = new HtmlTreeBuilder( root );
+            builder.setShowAll( jEdit.getBooleanProperty( "options.sidekick.html.showAllElements", true ) );
+            builder.setShowAll( showAll );
             document.accept( builder );
-            
-            /* need to convert the HtmlDocument.HtmlElements that are currently the 
+
+            /* need to convert the HtmlDocument.HtmlElements that are currently the
             user objects in the tree nodes to SideKick Assets */
-            convert(buffer, root);
-            
-        } catch ( Exception e ) {
+            convert( buffer, root );
+
+        }
+        catch ( Exception e ) {
             e.printStackTrace();
-        } finally {
+        }
+        finally {
             reader.close();
         }
         return parsedData;
     }
-    
-    private void convert(Buffer buffer, DefaultMutableTreeNode node) {
+
+    private void convert( Buffer buffer, DefaultMutableTreeNode node ) {
         // convert the children of the node
         Enumeration children = node.children();
-        while(children.hasMoreElements()) {
-            convert(buffer, (DefaultMutableTreeNode)children.nextElement());
+        while ( children.hasMoreElements() ) {
+            convert( buffer, ( DefaultMutableTreeNode ) children.nextElement() );
         }
-        
+
         // convert the node itself
-        if (!(node.getUserObject() instanceof IAsset)) {
-            HtmlDocument.HtmlElement userObject = (HtmlDocument.HtmlElement)node.getUserObject();
-            HtmlAsset asset = new HtmlAsset(userObject.toString());
-            asset.setLongString(userObject.toLongString());
-            asset.setStart(createStartPosition(buffer, userObject));
-            asset.setEnd(createEndPosition(buffer, userObject));
-            node.setUserObject(asset);    
+        if ( !( node.getUserObject() instanceof IAsset ) ) {
+            HtmlDocument.HtmlElement userObject = ( HtmlDocument.HtmlElement ) node.getUserObject();
+            Position start_position = null;
+            Position end_position = null;
+            if (userObject instanceof HtmlDocument.TagBlock) {
+                HtmlDocument.TagBlock block = (HtmlDocument.TagBlock)userObject;
+                HtmlDocument.Tag start_tag = block.startTag;
+                HtmlDocument.EndTag end_tag = block.endTag;
+                start_position = createStartPosition(buffer, start_tag);
+                createEndPosition(buffer, start_tag);
+                createStartPosition(buffer, end_tag);
+                end_position = createEndPosition(buffer, end_tag);
+            }
+            else {
+                start_position = createStartPosition(buffer, userObject);
+                end_position = createEndPosition(buffer, userObject);
+            }
+            HtmlAsset asset = new HtmlAsset( userObject );
+            asset.setLongString( userObject.toLongString() );
+            asset.setStart( start_position );
+            asset.setEnd( end_position );
+            node.setUserObject( asset );
         }
     }
 
@@ -147,15 +164,17 @@ public class HtmlParser extends SideKickParser implements EBComponent {
      * the Buffer, need to use getOffsetOfVirtualColumn to account for soft and
      * hard tab handling.
      */
-    private Position createStartPosition( Buffer buffer, HtmlDocument.HtmlElement child ) {
+    public static Position createStartPosition( Buffer buffer, HtmlDocument.HtmlElement child ) {
         final int line_offset = buffer.getLineStartOffset( Math.max( child.getStartLocation().line - 1, 0 ) );
         final int col_offset = buffer.getOffsetOfVirtualColumn( Math.max( child.getStartLocation().line - 1, 0 ),
                 Math.max( child.getStartLocation().column - 1, 0 ), null );
-        return new Position() {
-                   public int getOffset() {
-                       return line_offset + col_offset;
-                   }
-               };
+        Position p = new Position() {
+                    public int getOffset() {
+                        return line_offset + col_offset;
+                    }
+                };
+        child.setStartPosition( p );
+        return p;
     }
 
 
@@ -166,16 +185,17 @@ public class HtmlParser extends SideKickParser implements EBComponent {
      * the Buffer, need to use getOffsetOfVirtualColumn to account for soft and
      * hard tab handling.
      */
-    private Position createEndPosition( Buffer buffer, HtmlDocument.HtmlElement child ) {
+    public static Position createEndPosition( Buffer buffer, HtmlDocument.HtmlElement child ) {
         final int line_offset = buffer.getLineStartOffset( Math.max( child.getEndLocation().line - 1, 0 ) );
         final int col_offset = buffer.getOffsetOfVirtualColumn( Math.max( child.getEndLocation().line - 1, 0 ),
                 Math.max( child.getEndLocation().column - 1, 0 ), null );
-        return new Position() {
-                   public int getOffset() {
-                       return line_offset + col_offset;
-                   }
-               };
+        Position p = new Position() {
+                    public int getOffset() {
+                        return line_offset + col_offset;
+                    }
+                };
+        child.setEndPosition( p );
+        return p;
     }
 
 }
-
