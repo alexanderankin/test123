c4dd269a6c2f2fa6df1627c2ebd96be68c15408c
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-06-02 16:58:01 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-17'.

---
+++ plugins/XML/tags/release-0-17/xml/XmlActions.java
@@ -0,0 +1,778 @@
+/*
+ * XmlActions.java - Action implementations
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2000, 2003 Slava Pestov
+ *               1998, 2000 Ollie Rutherfurd
+ *               2000, 2001 Andre Kaplan
+ *               1999 Romain Guy
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml;
+
+//{{{ Imports
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.Segment;
+
+import org.gjt.sp.jedit.BeanShell;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.Macros;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.buffer.JEditBuffer;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.textarea.Selection;
+import org.gjt.sp.util.Log;
+
+import sidekick.SideKickParsedData;
+import xml.completion.ElementDecl;
+import xml.parser.TagParser;
+//}}}
+
+public class XmlActions
+{
+	//{{{ showEditTagDialog() method
+	public static void showEditTagDialog(View view)
+	{
+		JEditTextArea textArea = view.getTextArea();
+
+		if(XmlPlugin.isDelegated(textArea))
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		Buffer buffer = view.getBuffer();
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		String text = buffer.getText(0,buffer.getLength());
+
+		int caret = textArea.getCaretPosition();
+
+		TagParser.Tag tag = TagParser.getTagAtOffset(text,caret);
+		if(tag == null || tag.type == TagParser.T_END_TAG)
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		// use a StringTokenizer to parse the tag
+		HashMap attributes = new HashMap();
+		String attributeName = null;
+		boolean seenEquals = false;
+		boolean empty = false;
+
+		/* StringTokenizer does not support disabling or changing
+		 * the escape character, so we have to work around it here. */
+		char backslashSub = 127;
+		StreamTokenizer st = new StreamTokenizer(new StringReader(
+			text.substring(tag.start + tag.tag.length() + 1,
+			tag.end - 1)
+			.replace('\\',backslashSub)));
+		st.resetSyntax();
+		st.wordChars('!',255);
+		st.whitespaceChars(0,' ');
+		st.quoteChar('"');
+		st.quoteChar('\'');
+		st.ordinaryChar('/');
+		st.ordinaryChar('=');
+
+		Map entityHash = data.getNoNamespaceCompletionInfo().entityHash;
+
+		//{{{ parse tag
+		try
+		{
+loop:			for(;;)
+			{
+				switch(st.nextToken())
+				{
+				case StreamTokenizer.TT_EOF:
+					if(attributeName != null)
+					{
+						// in HTML, can have attributes
+						// without values.
+						attributes.put(attributeName,
+							attributeName);
+					}
+					break loop;
+				case '=':
+					seenEquals = true;
+					break;
+				case StreamTokenizer.TT_WORD:
+					if(attributeName == null)
+					{
+						attributeName = (data.html
+							? st.sval.toLowerCase()
+							: st.sval);
+						break;
+					}
+					else
+						/* fall thru */;
+				case '"':
+				case '\'':
+					if(attributeName != null)
+					{
+						if(seenEquals)
+						{
+							attributes.put(attributeName,
+								entitiesToCharacters(
+								st.sval.replace(backslashSub,'\\'),
+								entityHash));
+							seenEquals = false;
+						}
+						else if(data.html)
+						{
+							attributes.put(attributeName,
+								Boolean.TRUE);
+						}
+						attributeName = null;
+					}
+					break;
+				case '/':
+					empty = true;
+					break;
+				}
+			}
+		}
+		catch(IOException io)
+		{
+			// won't happen
+		} //}}}
+
+		ElementDecl elementDecl = data.getElementDecl(tag.tag);
+		if(elementDecl == null)
+		{
+			String[] pp = { tag.tag };
+			GUIUtilities.error(view,"xml-edit-tag.undefined-element",pp);
+			return;
+		}
+
+		EditTagDialog dialog = new EditTagDialog(view,tag.tag,
+			elementDecl,attributes,empty,
+			elementDecl.completionInfo.entityHash,
+			data.ids,data.html);
+
+		String newTag = dialog.getNewTag();
+
+		if(newTag != null)
+		{
+			try
+			{
+				buffer.beginCompoundEdit();
+
+				buffer.remove(tag.start,tag.end - tag.start);
+				buffer.insert(tag.start,newTag);
+			}
+			finally
+			{
+				buffer.endCompoundEdit();
+			}
+		}
+	} //}}}
+
+	//{{{ showEditTagDialog() method
+	public static void showEditTagDialog(View view, ElementDecl elementDecl)
+	{
+		Buffer buffer = view.getBuffer();
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		String newTag;
+		String closingTag;
+		if(elementDecl.attributes.size() == 0)
+		{
+			newTag = "<" + elementDecl.name
+				+ (!data.html && elementDecl.empty
+				? XmlActions.getStandaloneEnd() : ">");
+			if(elementDecl.empty)
+				closingTag = "";
+			else
+				closingTag = "</" + elementDecl.name + ">";
+		}
+		else
+		{
+			EditTagDialog dialog = new EditTagDialog(view,elementDecl.name,elementDecl,
+				new HashMap(),elementDecl.empty,
+				elementDecl.completionInfo.entityHash,
+				data.ids,data.html);
+
+			newTag = dialog.getNewTag();
+			if(dialog.isEmpty())
+				closingTag = "";
+			else
+				closingTag = "</" + elementDecl.name + ">";
+		}
+
+		if(newTag != null)
+		{
+			JEditTextArea textArea = view.getTextArea();
+
+			Selection[] selection = textArea.getSelection();
+
+			if(selection.length > 0)
+			{
+				try
+				{
+					buffer.beginCompoundEdit();
+
+					for(int i = 0; i < selection.length; i++)
+					{
+						buffer.insert(selection[i].getStart(),
+							newTag);
+						buffer.insert(selection[i].getEnd(),
+							closingTag);
+					}
+				}
+				finally
+				{
+					buffer.endCompoundEdit();
+				}
+			}
+			else
+			{
+				textArea.setSelectedText(newTag);
+				int caret = textArea.getCaretPosition();
+				textArea.setSelectedText(closingTag);
+				textArea.setCaretPosition(caret);
+			}
+
+			textArea.selectNone();
+			textArea.requestFocus();
+		}
+	} //}}}
+
+	//{{{ insertClosingTag() method
+	public static void insertClosingTag(View view)
+	{
+		JEditTextArea textArea = view.getTextArea();
+		Buffer buffer = view.getBuffer();
+
+		if(XmlPlugin.isDelegated(textArea) || !buffer.isEditable())
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		TagParser.Tag tag = TagParser.findLastOpenTag(
+			buffer.getText(0,textArea.getCaretPosition()),
+			textArea.getCaretPosition(),data);
+
+		if(tag != null)
+			textArea.setSelectedText("</" + tag.tag + ">");
+		else
+		{
+			view.getToolkit().beep();
+		}
+	} //}}}
+
+	//{{{ split() method
+	/**
+	 * If the DTD allows this tag to be split, split at the cursor.
+	 *
+	 * Note that this can be used to do a kind of 'fast-editing', eg when
+	 * editing an HTML &lt;p&gt; this will insert an end tag (if necessary)
+	 * and then place the cursor inside a new &lt;p&gt;.
+	 *
+	 * TODO: Syntax Checking
+	 */
+	public static void split(View view)
+	{
+		JEditTextArea textArea = view.getTextArea();
+		Buffer buffer = view.getBuffer();
+
+		if(XmlPlugin.isDelegated(textArea) || !buffer.isEditable())
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		TagParser.Tag tag = TagParser.findLastOpenTag(
+			buffer.getText(0,textArea.getCaretPosition()),
+			textArea.getCaretPosition(),data);
+
+		if(tag != null)
+		{
+			int pos;
+
+			Segment wsBefore = new Segment();
+			pos = getPrevNonWhitespaceChar( buffer, tag.start - 1 ) + 1;
+			buffer.getText( pos, tag.start-pos, wsBefore );
+			//System.err.println( "wsBefore: [" + wsBefore + "]" );
+
+			Segment wsAfter = new Segment();
+			pos = getNextNonWhitespaceChar( buffer, tag.end );
+			//Need to do this otherwise the ws in empty tags
+			//just gets bigger and bigger and bigger...
+			pos = Math.min( pos, textArea.getCaretPosition() );
+			buffer.getText( tag.end, pos - tag.end, wsAfter );
+			//System.err.println( "wsAfter: [" + wsAfter + "]" );
+
+			int lineStart = buffer.getLineStartOffset(
+				buffer.getLineOfOffset( tag.start ) );
+			String tagIndent = buffer.getText( lineStart, tag.start-lineStart );
+
+			//Note that the number of blank lines BEFORE the end tag will
+			//be the number AFTER the start tag, for symmetry's sake.
+			int crBeforeEndTag = countNewLines( wsAfter );
+			int crAfterEndTag = countNewLines( wsBefore );
+
+			StringBuffer insert = new StringBuffer();
+			if ( crBeforeEndTag>0 ) {
+				for ( int i=0; i<crBeforeEndTag; i++ ) {
+					insert.append( "\n" );
+				}
+				insert.append(tagIndent);
+			}
+			insert.append("</" + tag.tag + ">");
+			insert.append( wsBefore );
+			insert.append("<" + tag.tag + ">");
+			insert.append( wsAfter );
+
+			//Move the insertion point to here
+			textArea.setSelectedText(insert.toString());
+		}
+	}
+	//}}}
+
+	//{{{ removeTags() method
+	public static void removeTags(Buffer buffer)
+	{
+		if(!buffer.isEditable())
+		{
+			Toolkit.getDefaultToolkit().beep();
+			return;
+		}
+
+		int off = 0;
+		int len = buffer.getLength();
+		long startTime = System.currentTimeMillis();
+		int total = 0;
+		try
+		{
+			buffer.beginCompoundEdit();
+
+			String text = buffer.getText(off,len);
+			for (int i = text.indexOf('<');
+				i != -1; i = text.indexOf('<', ++i))
+			{
+				TagParser.Tag tag = TagParser.getTagAtOffset(text,i + 1);
+				if (tag == null)
+					continue;
+				else
+				{
+					int length = tag.end - tag.start;
+					buffer.remove(tag.start - total,length);
+					total += length;
+				}
+			}
+		}
+		finally
+		{
+			buffer.endCompoundEdit();
+		}
+		long endTime = System.currentTimeMillis();
+		Log.log(Log.DEBUG, XmlActions.class,
+			"removeTags time: " + (endTime - startTime) + " ms");
+	} //}}}
+
+	//{{{ matchTag() method
+	public static void matchTag(JEditTextArea textArea)
+	{
+		String text = textArea.getText();
+		TagParser.Tag tag = TagParser.getTagAtOffset(text,textArea.getCaretPosition());
+		if (tag != null)
+		{
+			TagParser.Tag matchingTag = TagParser.getMatchingTag(text, tag);
+			if (matchingTag != null)
+			{
+				textArea.setSelection(new Selection.Range(
+					matchingTag.start, matchingTag.end
+				));
+				textArea.moveCaretPosition(matchingTag.end);
+			}
+			else
+				textArea.getToolkit().beep();
+		}
+	} //}}}
+
+	//{{{ insertClosingTagKeyTyped() method
+	public static void insertClosingTagKeyTyped(View view)
+	{
+		JEditTextArea textArea = view.getTextArea();
+
+		Macros.Recorder recorder = view.getMacroRecorder();
+		if(recorder != null)
+			recorder.recordInput(1,'>',false);
+		textArea.userInput('>');
+
+		Buffer buffer = view.getBuffer();
+
+		if(XmlPlugin.isDelegated(textArea) || !buffer.isEditable()
+			|| !closeCompletionOpen)
+			return;
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+			return;
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		int caret = textArea.getCaretPosition();
+
+		String text = buffer.getText(0,caret);
+
+		TagParser.Tag tag = TagParser.getTagAtOffset(text,caret - 1);
+		if(tag == null)
+			return;
+
+		ElementDecl decl = data.getElementDecl(tag.tag);
+		if(tag.type == TagParser.T_STANDALONE_TAG
+			|| (decl != null && decl.empty))
+			return;
+
+		tag = TagParser.findLastOpenTag(text,caret,data);
+
+		if(tag != null)
+		{
+			String insert = "</" + tag.tag + ">";
+			if(recorder != null)
+				recorder.recordInput(insert,false);
+			textArea.setSelectedText(insert);
+
+			String code = "textArea.setCaretPosition("
+				+ "textArea.getCaretPosition() - "
+				+ insert.length() + ");";
+			if(recorder != null)
+				recorder.record(code);
+			BeanShell.eval(view,BeanShell.getNameSpace(),code);
+		}
+	} //}}}
+
+	//{{{ completeClosingTag() method
+	public static void completeClosingTag(View view, boolean insertSlash)
+	{
+		JEditTextArea textArea = view.getTextArea();
+
+		Macros.Recorder recorder = view.getMacroRecorder();
+
+		if(insertSlash)
+		{
+			if(recorder != null)
+				recorder.recordInput(1,'/',false);
+			textArea.userInput('/');
+		}
+
+		JEditBuffer buffer = textArea.getBuffer();
+
+		if(XmlPlugin.isDelegated(textArea))
+			return;
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+			return;
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		if(!buffer.isEditable() || !closeCompletion)
+		{
+			return;
+		}
+
+		int caret = textArea.getCaretPosition();
+		if(caret == 1)
+			return;
+
+		String text = buffer.getText(0,buffer.getLength());
+
+		if(text.charAt(caret - 2) != '<')
+			return;
+
+		// check if caret is inside a tag
+		if(TagParser.getTagAtOffset(text,caret) != null)
+			return;
+
+		TagParser.Tag tag = TagParser.findLastOpenTag(text,caret - 2,data);
+
+		if(tag != null)
+		{
+			String insert = tag.tag + ">";
+			if(recorder != null)
+				recorder.recordInput(insert,false);
+			textArea.setSelectedText(insert);
+		}
+	} //}}}
+
+	//{{{ charactersToEntities() method
+	public static String charactersToEntities(String s, Map hash)
+	{
+		StringBuffer buf = new StringBuffer();
+		for(int i = 0; i < s.length(); i++)
+		{
+			char ch = s.charAt(i);
+			if(ch >= 0x7f
+				|| ch == '<'
+				|| ch == '>'
+				|| ch == '&'
+				|| ch == '"'
+				|| ch == '\'')
+			{
+				Character c = new Character(ch);
+				String entity = (String)hash.get(c);
+				if(entity != null)
+				{
+					buf.append('&');
+					buf.append(entity);
+					buf.append(';');
+
+					continue;
+				}
+			}
+
+			buf.append(ch);
+		}
+
+		return buf.toString();
+	} //}}}
+
+	//{{{ entitiesToCharacters() method
+	public static String entitiesToCharacters(String s, Map hash)
+	{
+		StringBuffer buf = new StringBuffer();
+		for(int i = 0; i < s.length(); i++)
+		{
+			char ch = s.charAt(i);
+			if(ch == '&')
+			{
+				int index = s.indexOf(';',i);
+				if(index != -1)
+				{
+					String entityName = s.substring(i + 1,index);
+					Character c = (Character)hash.get(entityName);
+					if(c != null)
+					{
+						buf.append(c.charValue());
+						i = index;
+						continue;
+					}
+				}
+			}
+
+			buf.append(ch);
+		}
+
+		return buf.toString();
+	} //}}}
+
+	//{{{ charactersToEntities() method
+	public static void charactersToEntities(View view)
+	{
+		Buffer buffer = view.getBuffer();
+		JEditTextArea textArea = view.getTextArea();
+
+		if(XmlPlugin.isDelegated(textArea) || !buffer.isEditable())
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		Map entityHash = data.getNoNamespaceCompletionInfo().entityHash;
+
+		Selection[] selection = textArea.getSelection();
+		for(int i = 0; i < selection.length; i++)
+		{
+			textArea.setSelectedText(selection[i],
+				charactersToEntities(textArea.getSelectedText(
+				selection[i]),entityHash));
+		}
+	} //}}}
+
+	//{{{ entitiesToCharacters() method
+	public static void entitiesToCharacters(View view)
+	{
+		Buffer buffer = view.getBuffer();
+		JEditTextArea textArea = view.getTextArea();
+
+		if(XmlPlugin.isDelegated(textArea) || !buffer.isEditable())
+		{
+			view.getToolkit().beep();
+			return;
+		}
+
+		SideKickParsedData _data = SideKickParsedData.getParsedData(view);
+
+		if(!(_data instanceof XmlParsedData))
+		{
+			GUIUtilities.error(view,"xml-no-data",null);
+			return;
+		}
+
+		XmlParsedData data = (XmlParsedData)_data;
+
+		Map entityHash = data.getNoNamespaceCompletionInfo().entityHash;
+
+		Selection[] selection = textArea.getSelection();
+		for(int i = 0; i < selection.length; i++)
+		{
+			textArea.setSelectedText(selection[i],
+				entitiesToCharacters(textArea.getSelectedText(
+				selection[i]),entityHash));
+		}
+	} //}}}
+
+	//{{{ getStandaloneEnd() method
+	public static String getStandaloneEnd()
+	{
+		return (standaloneExtraSpace ? " />" : "/>");
+	} //}}}
+
+	//{{{ propertiesChanged() method
+	static void propertiesChanged()
+	{
+		closeCompletion = jEdit.getBooleanProperty(
+			"xml.close-complete");
+		closeCompletionOpen = jEdit.getBooleanProperty(
+			"xml.close-complete-open");
+		standaloneExtraSpace = jEdit.getBooleanProperty(
+			"xml.standalone-extra-space");
+	} //}}}
+
+	//{{{ Private members
+
+	//{{{ Static variables
+	private static Segment seg = new Segment();
+	private static boolean closeCompletion;
+	private static boolean closeCompletionOpen;
+	private static boolean standaloneExtraSpace;
+	//}}}
+
+	//{{{ getPrevNonWhitespaceChar() method
+	/**
+	 * Find the offset of the previous non whitespace character.
+	 */
+	private static int getPrevNonWhitespaceChar( Buffer buf, int start )
+	{
+		//It might be more efficient if there were a getCharAt() method on the buffer?
+
+		//This is trying to conserve memory by not creating strings all the time
+		Segment seg = new Segment( new char[1], 0, 1 );
+		int pos = start;
+		while ( pos>0 )
+		{
+			buf.getText( pos, 1, seg );
+			if ( ! Character.isWhitespace( seg.first() ) )
+				break;
+			pos--;
+		}
+		return pos;
+	}
+	//}}}
+
+	//{{{ getNextNonWhitespaceChar() method
+	/**
+	 * Find the offset of the next non-whitespace character.
+	 */
+	private static int getNextNonWhitespaceChar( Buffer buf, int start )
+	{
+		//It might be more efficient if there were a getCharAt() method on the buffer?
+
+		//This is trying to conserve memory by not creating strings all the time
+		Segment seg = new Segment( new char[1], 0, 1 );
+		int pos = start;
+		while ( pos < buf.getLength() )
+		{
+			buf.getText( pos, 1, seg );
+			//System.err.println( "NNWS Testing: " + seg.first() + " at " + pos );
+			if ( ! Character.isWhitespace( seg.first() ) )
+				break;
+			pos++;
+		}
+
+		return pos;
+	}
+	//}}}
+
+	//{{{ countNewLines() method
+	/**
+	 * Count the number of newlines in the given segment.
+	 */
+	private static int countNewLines( Segment seg )
+	{
+		//It might help if there were a getCharAt() method on the buffer
+		//or the buffer itself implemented CharaterIterator?
+
+		//This is trying to conserve memory by not creating strings all the time
+		int count = 0;
+		for ( int pos = seg.getBeginIndex(); pos<seg.getEndIndex(); pos++ ) {
+			if ( seg.array[pos] == '\n' ) {
+				count++;
+			}
+		}
+		return count;
+	}
+	//}}}
+
+	//}}}
+}
---
+++ plugins/XML/tags/release-0-17/xml/completion/CompletionInfo.java
@@ -0,0 +1,269 @@
+/*
+ * CompletionInfo.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2001, 2003 Slava Pestov
+ *
+ * The XML plugin is licensed under the GNU General Public License, with
+ * the following exception:
+ *
+ * "Permission is granted to link this code with software released under
+ * the Apache license version 1.1, for example used by the Xerces XML
+ * parser package."
+ */
+
+package xml.completion;
+
+//{{{ Imports
+import gnu.regexp.*;
+import java.util.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+import org.xml.sax.XMLReader;
+import org.xml.sax.SAXException;
+import xml.parser.*;
+//}}}
+
+public class CompletionInfo
+{
+	public ArrayList elements;
+	public HashMap elementHash;
+	public ArrayList entities;
+	public HashMap entityHash;
+	public ArrayList elementsAllowedAnywhere;
+
+	//{{{ CompletionInfo constructor
+	public CompletionInfo()
+	{
+		this(new ArrayList(), new HashMap(),
+			new ArrayList(), new HashMap(),
+			new ArrayList());
+
+		addEntity(EntityDecl.INTERNAL,"lt","<");
+		addEntity(EntityDecl.INTERNAL,"gt",">");
+		addEntity(EntityDecl.INTERNAL,"amp","&");
+		addEntity(EntityDecl.INTERNAL,"quot","\"");
+		addEntity(EntityDecl.INTERNAL,"apos","'");
+	} //}}}
+
+	//{{{ CompletionInfo constructor
+	public CompletionInfo(ArrayList elements, HashMap elementHash,
+		ArrayList entities, HashMap entityHash,
+		ArrayList elementsAllowedAnywhere)
+	{
+		this.elements = elements;
+		this.elementHash = elementHash;
+		this.entities = entities;
+		this.entityHash = entityHash;
+		this.elementsAllowedAnywhere = elementsAllowedAnywhere;
+	} //}}}
+
+	//{{{ addEntity() method
+	public void addEntity(int type, String name, String value)
+	{
+		addEntity(new EntityDecl(type,name,value));
+	} //}}}
+
+	//{{{ addEntity() method
+	public void addEntity(int type, String name, String publicId, String systemId)
+	{
+		addEntity(new EntityDecl(type,name,publicId,systemId));
+	} //}}}
+
+	//{{{ addEntity() method
+	public void addEntity(EntityDecl entity)
+	{
+		entities.add(entity);
+		if(entity.type == EntityDecl.INTERNAL
+			&& entity.value.length() == 1)
+		{
+			Character ch = new Character(entity.value.charAt(0));
+			entityHash.put(entity.name,ch);
+			entityHash.put(ch,entity.name);
+		}
+	} //}}}
+
+	//{{{ addElement() method
+	public void addElement(ElementDecl element)
+	{
+		elementHash.put(element.name,element);
+		elements.add(element);
+	} //}}}
+
+	//{{{ getAllElements() method
+	public void getAllElements(String prefix, List out)
+	{
+		for(int i = 0; i < elements.size(); i++)
+		{
+			out.add(((ElementDecl)elements.get(i)).withPrefix(prefix));
+		}
+	} //}}}
+
+	//{{{ toString() method
+	public String toString()
+	{
+		StringBuffer buf = new StringBuffer();
+
+		buf.append("<element-list>\n\n");
+
+		for(int i = 0; i < elements.size(); i++)
+		{
+			buf.append(elements.get(i));
+			buf.append('\n');
+		}
+
+		buf.append("\n</element-list>\n\n<entity-list>\n\n");
+
+		buf.append("<!-- not implemented yet -->\n");
+		/* for(int i = 0; i < entities.size(); i++)
+		{
+			buf.append(entities.get(i));
+			buf.append('\n');
+		} */
+
+		buf.append("\n</entity-list>");
+
+		return buf.toString();
+	} //}}}
+
+	//{{{ getCompletionInfoForBuffer() method
+	public static CompletionInfo getCompletionInfoForBuffer(Buffer buffer)
+	{
+		Iterator iter = globs.keySet().iterator();
+		while(iter.hasNext())
+		{
+			RE re = (RE)iter.next();
+			if(re.isMatch(buffer.getName()))
+				return getCompletionInfoFromResource((String)globs.get(re));
+		}
+
+		String resource = jEdit.getProperty("mode."
+			+ buffer.getMode().getName()
+			+ ".xml.completion-info");
+		if(resource != null)
+		{
+			CompletionInfo info = getCompletionInfoFromResource(resource);
+
+			if(info != null)
+				return info;
+		}
+
+		return null;
+	} //}}}
+
+	//{{{ getCompletionInfoForNamespace() method
+	public static CompletionInfo getCompletionInfoForNamespace(String namespace)
+	{
+		synchronized(lock)
+		{
+			Object obj = completionInfoNamespaces.get(namespace);
+			if(obj instanceof String)
+			{
+				CompletionInfo info = getCompletionInfoFromResource((String)obj);
+				completionInfoNamespaces.put(namespace,info);
+				return info;
+			}
+			else
+				return (CompletionInfo)obj;
+		}
+	} //}}}
+
+	//{{{ getCompletionInfoFromResource() method
+	public static CompletionInfo getCompletionInfoFromResource(String resource)
+	{
+		synchronized(lock)
+		{
+			CompletionInfo info = (CompletionInfo)completionInfoResources.get(resource);
+			if(info != null)
+				return info;
+
+			Log.log(Log.DEBUG,CompletionInfo.class,"Loading " + resource);
+
+			CompletionInfoHandler handler = new CompletionInfoHandler();
+
+			try
+			{
+				XMLReader parser = new org.apache.xerces.parsers.SAXParser();
+				parser.setFeature("http://apache.org/xml/features/validation/dynamic",true);
+				parser.setErrorHandler(handler);
+				parser.setEntityResolver(handler);
+				parser.setContentHandler(handler);
+				parser.parse(resource);
+			}
+			catch(SAXException se)
+			{
+				Throwable e = se.getException();
+				if(e == null)
+					e = se;
+				Log.log(Log.ERROR,CompletionInfo.class,e);
+			}
+			catch(Exception e)
+			{
+				Log.log(Log.ERROR,CompletionInfo.class,e);
+			}
+
+			info = handler.getCompletionInfo();
+			completionInfoResources.put(resource,info);
+
+			return info;
+		}
+	} //}}}
+
+	//{{{ clone() method
+	public Object clone()
+	{
+		return new CompletionInfo(
+			(ArrayList)elements.clone(),
+			(HashMap)elementHash.clone(),
+			(ArrayList)entities.clone(),
+			(HashMap)entityHash.clone(),
+			(ArrayList)elementsAllowedAnywhere.clone()
+		);
+	} //}}}
+
+	//{{{ Private members
+	private static HashMap globs;
+	private static HashMap completionInfoResources;
+	private static HashMap completionInfoNamespaces;
+	private static Object lock;
+
+	//{{{ Class initializer
+	static
+	{
+		completionInfoResources = new HashMap();
+		globs = new HashMap();
+		int i = 0;
+		String glob;
+		while((glob = jEdit.getProperty("xml.completion.glob." + i + ".key")) != null)
+		{
+			String info = jEdit.getProperty("xml.completion.glob." + i + ".value");
+			try
+			{
+				globs.put(new RE(MiscUtilities.globToRE(glob),
+					RE.REG_ICASE),info);
+			}
+			catch(REException re)
+			{
+				Log.log(Log.ERROR,CompletionInfo.class,re);
+			}
+
+			i++;
+		}
+
+		completionInfoNamespaces = new HashMap();
+		i = 0;
+		String namespace;
+		while((namespace = jEdit.getProperty("xml.completion.namespace." + i + ".key")) != null)
+		{
+			String info = jEdit.getProperty("xml.completion.namespace." + i + ".value");
+			completionInfoNamespaces.put(namespace,info);
+
+			i++;
+		}
+
+		lock = new Object();
+	} //}}}
+
+	//}}}
+}
