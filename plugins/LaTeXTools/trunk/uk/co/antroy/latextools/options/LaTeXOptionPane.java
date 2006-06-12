b313d58e9d7474b1e9c337019689797f8a376c11
Alan Ezust <1688991+ezust@users.noreply.github.com>
2006-06-12 05:15:38 +0000

    Redesigned the option panes.

--- plugins/LaTeXTools/trunk/uk/co/antroy/latextools/options/LaTeXOptionPane.java
+++ plugins/LaTeXTools/trunk/uk/co/antroy/latextools/options/LaTeXOptionPane.java
@@ -21,13 +21,10 @@ package uk.co.antroy.latextools.options;
 
 
 //{{{ Imports
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
 
-import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.gjt.sp.jedit.AbstractOptionPane;
@@ -69,81 +66,75 @@ public class LaTeXOptionPane
     }
 
     private void initGeneral() {
-        addComponent(new JLabel("<html><h3>Compilation Options"));
+	    addSeparator("options.latextools.general.compilation");
+        
         compileCommandField = new JTextField(30);
         compileCommandField.setText(jEdit.getProperty("latex.compile.command"));
 
-        JPanel p1 = new JPanel();
-        p1.add(new JLabel("Default Compile Command"));
-        p1.add(compileCommandField);
-        compileExtField = new JTextField(8);
-        compileExtField.setText(jEdit.getProperty("latex.compile.ext"));
-
-        JPanel p2 = new JPanel();
-        p2.add(new JLabel("Default Compile Extension"));
-        p2.add(compileExtField);
+//        JPanel p1 = new JPanel();
+        JLabel compileLabel = new JLabel("Default Compile Command");
+        addComponent(compileLabel, compileCommandField);
+        
+        
+        compileExtField = new JTextField(jEdit.getProperty("latex.compile.ext"));
+        JLabel compileExtLabel = new JLabel("Default Compile Extension");
+        addComponent(compileExtLabel, compileExtField);
+        
         compileDetach = new JCheckBox("Detach Compilation Process?");
         compileDetach.getModel().setSelected(jEdit.getBooleanProperty(
                                                          "latex.compile.detach"));
-        cStyleErrors = new JCheckBox("Show Errors in Error List? (Passes a switch to latex - see the Latex error style switch)");
+        addComponent(compileDetach);
+        
+        cStyleErrors = new JCheckBox(jEdit.getProperty("options.latextools.general.errorlist"));
+        cStyleErrors.setToolTipText(jEdit.getProperty("options.latextools.general.errorlist.tooltip"));
+        
         cStyleErrors.getModel().setSelected(jEdit.getBooleanProperty(
                                                         "latex.compile.parse-errors"));
+        addComponent(cStyleErrors);
+        
         compileShowErrorsSwitch = new JTextField(30);
         compileShowErrorsSwitch.setText(jEdit.getProperty("latex.compile.c-errors"));
-        String showErrTooltip = "Passed to latex; use -c-style-errors for MiKTeX"
-    		+" and -file-line-error-style for linux distributions";
+        String showErrTooltip = jEdit.getProperty("options.latextools.general.compilation.errortooltip");
         compileShowErrorsSwitch.setToolTipText(showErrTooltip);
-        JPanel pShowErr1 = new JPanel();
-        pShowErr1.add(new JLabel("Latex error style switch"));
-        pShowErr1.add(compileShowErrorsSwitch);
-        JLabel showErrDetailLabel = new JLabel("\t("+showErrTooltip+")");
-        /*JPanel pShowErr2 = new JPanel();
-        pShowErr2.setLayout(new BoxLayout(pShowErr2,BoxLayout.Y_AXIS));
-        pShowErr2.add(pShowErr1);
-        pShowErr2.add(showErrDetailLabel);*/
         
-        addComponent(p1);
-        addComponent(p2);
-        addComponent(compileDetach);
-        addComponent(cStyleErrors);
-        addComponent(pShowErr1);
-        addComponent(showErrDetailLabel);
-        bibtexCommandField = new JTextField(30);
-        bibtexCommandField.setText(jEdit.getProperty("latex.bibtex.command"));
+        JLabel errorStyleLabel = new JLabel("Latex error style switch");
 
-        JPanel pbib = new JPanel();
-        pbib.add(new JLabel("Default BibTeX Command"));
-        pbib.add(bibtexCommandField);
+        addComponent(errorStyleLabel, compileShowErrorsSwitch);
+
+        bibtexCommandField = new JTextField(jEdit.getProperty("latex.bibtex.command"));
+        JLabel dfbclabel = new JLabel("Default BibTeX Command");
+        addComponent(dfbclabel, bibtexCommandField);
+        
         bibtexDetach = new JCheckBox("Detach BibTeX Process?");
         bibtexDetach.getModel().setSelected(jEdit.getBooleanProperty(
                                                         "latex.bibtex.detach"));
-        addComponent(pbib);
         addComponent(bibtexDetach);
-        addComponent(new JLabel("<html><h3>Viewer Options"));
+        
+        addSeparator("options.latextools.general.viewer");
+        
         viewerCommandField = new JTextField(30);
         viewerCommandField.setText(jEdit.getProperty(
                                                "latex.viewoutput.command"));
 
-        JPanel p3 = new JPanel();
-        p3.add(new JLabel("Default Viewer Command"));
-        p3.add(viewerCommandField);
-        viewerExtField = new JTextField(30);
-        viewerExtField.setText(jEdit.getProperty("latex.viewoutput.ext"));
-
-        JPanel p4 = new JPanel();
-        p4.add(new JLabel("Default Viewer Extension"));
-        p4.add(viewerExtField);
+        JLabel dfc = new JLabel("Default Viewer Command");
+        addComponent(dfc, viewerCommandField);
+        
+        viewerExtField = new JTextField(jEdit.getProperty("latex.viewoutput.ext"));
+        JLabel dfe = new JLabel("Default Viewer Extension");
+        addComponent(dfe, viewerExtField );
+        
         viewerDetach = new JCheckBox("Detach Viewer Process?");
         viewerDetach.getModel().setSelected(jEdit.getBooleanProperty(
                                                         "latex.viewoutput.detach"));
-        addComponent(p3);
-        addComponent(p4);
+        
+        
         addComponent(viewerDetach);
-        addComponent(new JLabel("<html><h3>Classpath Options"));
-        addComponent(new JLabel("Provide a semi-colon delimited list of directories to search for imports:"));
-        classpathField = new JTextField(30);
-        classpathField.setText(jEdit.getProperty("latex.classpath.dirs"));
-        addComponent(classpathField);
+        addSeparator("options.latextools.general.classpath");
+        
+        classpathField = new JTextField( jEdit.getProperty("latex.classpath.dirs") , 40);
+        
+        classpathField.setToolTipText(jEdit.getProperty("options.latextools.general.classpath.tooltip"));
+        addComponent(classpathField, GridBagConstraints.HORIZONTAL);
     }
 
     private void saveGeneral() {
