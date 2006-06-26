257ef50bbccc8038cfcc2e5aa560c1b1cde4a715
Dale <danson@grafidog.com>
2006-06-26 04:58:30 +0000

    new stuff, might not work...

---
+++ plugins/JavaSideKick/trunk/src/sidekick/java/PVHelper.java
@@ -0,0 +1,40 @@
+package sidekick.java;
+
+import java.util.*;
+import org.gjt.sp.jedit.*;
+import projectviewer.*;
+import projectviewer.vpt.*;
+import sidekick.java.util.*;
+
+public class PVHelper {
+
+    public static String getProjectNameForFile( String filename ) {
+        if (!isProjectViewerAvailable())
+            return null;
+        ProjectManager pm = ProjectManager.getInstance();
+        for ( Iterator it = pm.getProjects(); it.hasNext(); ) {
+            VPTProject project = ( VPTProject ) it.next();
+            VPTNode node = project.getChildNode( filename );
+            if ( node != null ) {
+                return project.getName();
+            }
+        }
+        return null;
+    }
+
+    public static boolean isProjectViewerAvailable() {
+        EditPlugin pv = jEdit.getPlugin( "projectviewer.ProjectPlugin", false );
+        return pv != null;
+    }
+    
+    public static Path getClassPathForProject(String projectName) {
+        boolean useJavaClasspath = jEdit.getBooleanProperty( "sidekick.java.pv." + projectName + ".useJavaClasspath" );
+        String classpath = jEdit.getProperty("sidekick.java.pv." + projectName + ".optionalClasspath", "");
+        Path path = new Path(classpath);
+        if (useJavaClasspath) {
+            path.concatSystemClassPath();   
+        }
+        System.out.println(path.toString());
+        return path;
+    }
+}
