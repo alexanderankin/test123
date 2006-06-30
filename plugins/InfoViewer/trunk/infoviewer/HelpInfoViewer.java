d4a090f17f1ba210d194de6a5d7e42a8bb96bb38
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-06-30 20:55:08 +0000

    Fixed ESC key handling for Infoviewer.

--- plugins/InfoViewer/trunk/infoviewer/HelpInfoViewer.java
+++ plugins/InfoViewer/trunk/infoviewer/HelpInfoViewer.java
@@ -63,6 +63,7 @@ public class HelpInfoViewer extends InfoViewer implements HelpViewerInterface {
 	public HelpInfoViewer(View view, String position, boolean sidebar) 
 	{
 		super(view, position);
+		setName("helpviewer");
 		showSideBar = sidebar;
 		
 		// Log.log(Log.WARNING, this.getClass(), "HelpInfoViewer!");
