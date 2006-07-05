ba0e71001ac1d99bb85fda3b9497a02ce09bf7c0
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-05 05:30:07 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1-5-8'.

---
+++ plugins/InfoViewer/tags/release-1-5-8/infoviewer/HelpInfoViewer.java
@@ -0,0 +1,207 @@
+package infoviewer;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+
+import org.gjt.sp.jedit.MiscUtilities;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.help.HelpSearchPanel;
+import org.gjt.sp.jedit.help.HelpTOCPanel;
+import org.gjt.sp.jedit.help.HelpViewerInterface;
+import org.gjt.sp.util.Log;
+
+/**
+ * An infoviewer with a helpful side-bar. Reuses the same sidebar from the
+ * original HelpViewer but is intended to be extended with other kinds of views
+ * later.
+ * 
+ * @author ezust
+ * @version $Id$
+ */
+public class HelpInfoViewer extends InfoViewer implements HelpViewerInterface {
+
+	public void toggleSideBar() {
+		showSideBar = aToggleSidebar.isSelected();
+		// remove(centralComponent);
+		innerPanel.remove(centralComponent);
+		if (showSideBar) 
+		{
+			splitter.setLeftComponent(tabs);
+			splitter.setRightComponent(scrViewer);
+			centralComponent = splitter;
+			splitter.setDividerLocation(100);
+		} else 
+		{
+			centralComponent = scrViewer;
+		}
+		innerPanel.add(BorderLayout.CENTER, centralComponent);
+		scrViewer.repaint();
+		repaint();
+	}
+
+	public HelpInfoViewer() 
+	{
+		this(null, null, true);
+	}
+
+	public HelpInfoViewer(View view, String position) 
+	{
+		this(view, position, true);
+	}
+	
+	public HelpInfoViewer(View view, String position, boolean sidebar) 
+	{
+		super(view, position);
+		if (sidebar) setName("helpviewer");
+		showSideBar = sidebar;
+		
+		// Log.log(Log.WARNING, this.getClass(), "HelpInfoViewer!");
+		tabs = new JTabbedPane();
+		toc = new HelpTOCPanel(this);
+		tabs.addTab(jEdit.getProperty("helpviewer.toc.label"), toc);
+		searchPanel = new HelpSearchPanel(this);
+		tabs.addTab(jEdit.getProperty("helpviewer.search.label"), searchPanel);
+		tabs.setMinimumSize(new Dimension(0, 20));
+		scrViewer.setMinimumSize(new Dimension(0, 20));
+
+		splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, tabs, scrViewer);
+		splitter.setBorder(null);
+		if (showSideBar) {
+			centralComponent = splitter;
+		}
+		else 
+		{
+			centralComponent = scrViewer;
+		}
+		innerPanel.add(BorderLayout.CENTER, centralComponent);
+		queueTOCReload();
+		setVisible(true);
+		SwingUtilities.invokeLater(new Runnable() 
+			{
+			public void run() 
+			{
+				splitter.setDividerLocation(jEdit.getIntegerProperty(
+						"infoviewer.splitter", 250));
+				requestFocus();
+			}
+		});
+	}
+
+	// {{{ dispose() method
+	public void dispose() 
+	{
+		jEdit.setIntegerProperty("infoviewer.splitter", splitter
+				.getDividerLocation());
+	} // }}}
+
+	// {{{ queueTOCReload() method
+
+	public void queueTOCReload() 
+	{
+		SwingUtilities.invokeLater(new Runnable() 
+			{
+			public void run() {
+				queuedTOCReload = false;
+				toc.load();
+			}
+		});
+	} // }}}
+
+	// {{{ ActionHandler class
+	class ActionHandler implements ActionListener {
+		// {{{ actionPerformed() class
+		public void actionPerformed(ActionEvent evt) {
+			Object source = evt.getSource();
+			String url = evt.getActionCommand();
+			if (!url.equals("")) {
+				gotoURL(url, false, -1);
+				return;
+			}
+		} // }}}
+	} // }}}
+
+	public void setTitle(String newTitle) {
+		firePropertyChange("title", null, newTitle);
+	}
+
+	public Component getComponent() {
+		return this;
+	}
+
+	public void gotoURL(String url, boolean addToHistory, int scrollPos) {
+		if (baseURL == null) try {
+			baseURL = new File(MiscUtilities.constructPath(
+					jEdit.getJEditHome(), "doc")).toURL().toString();
+
+		} catch (MalformedURLException mu) {
+			Log.log(Log.ERROR, this, mu);
+			// what to do?
+		}
+		if (url == null)
+			return;
+		url = url.trim();
+		if (url.length() == 0)
+			return;
+		String shortURL;
+		if(MiscUtilities.isURL(url))
+		{
+			if(url.startsWith(baseURL))
+			{
+				shortURL = url.substring(baseURL.length());
+				if(shortURL.startsWith("/"))
+					shortURL = shortURL.substring(1);
+			}
+			else
+			{
+				shortURL = url;
+			}
+		}
+		else
+		{
+			shortURL = url;
+			if(baseURL.endsWith("/"))
+				url = baseURL + url;
+			else
+				url = baseURL + '/' + url;
+		}
+		
+		try {
+			URL u = new URL(url);
+			gotoURL(u, addToHistory, scrollPos);
+		} catch (MalformedURLException mu) {
+			urlField.setText(url);
+			showError(jEdit.getProperty("infoviewer.error.badurl.message",
+					new Object[] { mu }));
+		}
+	}
+
+	// {{{ Private members
+
+	// {{{ Instance members
+	private JSplitPane splitter;
+
+	private JTabbedPane tabs;
+
+	private HelpSearchPanel searchPanel;
+
+	private HelpTOCPanel toc;
+
+	private boolean queuedTOCReload;
+
+	private boolean showSideBar = true;
+
+	private Component centralComponent;
+	// }}}
+
+}
