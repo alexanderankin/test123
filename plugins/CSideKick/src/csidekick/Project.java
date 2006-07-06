461d3d93e2944f0cb31a6656807af32b908c1809
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-06 02:24:34 +0000

    Initial files.

---
+++ plugins/CSideKick/src/csidekick/Project.java
@@ -0,0 +1,22 @@
+package csidekick;
+
+import projectviewer.vpt.VPTProject;
+
+public class Project
+{
+	VPTProject project;
+	
+	public String getName() {
+		return project.getName();
+	}
+	public void save() {
+		// TODO
+	}
+	public void load(String projectName) {
+		// TODO
+	}
+	
+	
+	
+
+}
