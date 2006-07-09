00970a213a0cf7ee1af493a234990b49ef5544ba
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-09 03:27:59 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1-4-3'.

---
+++ plugins/CodeBrowser/tags/release-1-4-3/com/bitart/codebrowser/CodeBrowserPlugin.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+*	Copyright 2002 BITart Gerd Knops. All rights reserved.
+*
+*	Project	: CodeBrowser
+*	File	: CodeBrowserPlugin.java
+*	Author	: Gerd Knops gerti@BITart.com
+*
+*******************************************************************************
+*                                    :mode=java:folding=indent:collapseFolds=1:
+*	History:
+*	2002-05-10 Creation of file
+*   2006-07-07 updated for jedit 4.3 API (ezust)
+*
+*******************************************************************************
+*
+*	Description:
+*	Simple jEdit plugin class.
+*
+*	$Id$
+*
+*******************************************************************************
+*
+* DISCLAIMER
+*
+* BITart and Gerd Knops make no warranties, representations or commitments
+* with regard to the contents of this software. BITart and Gerd Knops
+* specifically disclaim any and all warranties, wether express, implied or
+* statutory, including, but not limited to, any warranty of merchantability
+* or fitness for a particular purpose, and non-infringement. Under no
+* circumstances will BITart or Gerd Knops be liable for loss of data,
+* special, incidental or consequential damages out of the use of this
+* software, even if those damages were forseeable, or BITart or Gerd Knops
+* was informed of their potential.
+*
+******************************************************************************/
+package com.bitart.codebrowser;
+/******************************************************************************
+* Imports
+******************************************************************************/
+
+	import java.awt.*;
+	import java.awt.event.*;
+	import java.io.*;
+	import java.util.*;
+	import javax.swing.*;
+	
+	import org.gjt.sp.jedit.*;
+	import org.gjt.sp.jedit.msg.*;
+	import org.gjt.sp.jedit.textarea.*;
+	import org.gjt.sp.util.*;
+	import org.gjt.sp.jedit.gui.*;
+	
+/*****************************************************************************/
+public class CodeBrowserPlugin extends EditPlugin
+{
+/******************************************************************************
+* Vars
+******************************************************************************/
+
+    public static final String NAME="codebrowser";
+    public static final String MENU="codebrowser.menu";
+	
+	
+}
+/*************************************************************************EOF*/
+
