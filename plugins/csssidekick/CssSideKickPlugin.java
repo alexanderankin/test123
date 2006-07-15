4eebb408b0337e8177f59d335df5b2dd1f87b68a
oswaldcz <oswaldcz@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-15 00:32:35 +0000

    Import of CSS SideKick plugin.
    CSS SideKick plugin  extends SideKick plugin and shows a structure of selectors in SideKick docable window and context-sensitive completion for CSS properties and values.
    
    Completion is defined in css-completion.xml file (which should be - for now manualy - placed into $JEDIT_SETTINGS). For each property are there defined values. There are also "sets" of values, which can be included inside property element.
    
    When you are writing a property, which can contains "url" value, in completion list is shown special value "url..". If you choose this one, then a VFS dialog is opened and if you select some file, then is inserted "url('relative/path/to.file')". If you select just "url", then is inserted url('|').
    
    After selected property is automatically inserted color and space - this is configurable in plugin options, you can also choose type of quotes (single/double).
    
    Under menu Plugins > CSS SideKick is item "Insert relative path" which can be called anytime for inserting relative path from current buffet to some other file. In this menu is also item "Reload completion config".
    
    
    Known bugs:
    
    - when are used  some at-rules (@import, @media, etc.), structure in sidekick is incorrect
    
    Depends on:
    - jEdit 4.3pre3
    - JDK 1.4
    - SideKick 0.4
    - XercesPlugin 2.8.0

---
+++ plugins/csssidekick/CssSideKickPlugin.java
@@ -0,0 +1,33 @@
+/**
+ * CssSidekickPlugin.java
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2006 Jakub Roztocil
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package csssidekick;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+
+public class CssSideKickPlugin extends EditPlugin {
+	public static final String NAME = "csssidekick";
+	public static final String OPTION_PREFIX = "options.csssidekick.";
+	
+	public void start(){super.start();}
+	public void stop(){}
+}
