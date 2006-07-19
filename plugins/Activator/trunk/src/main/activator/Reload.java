1af90cb859e094d70fef20edec917f21bc55c883
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-19 01:37:13 +0000

    If you reload a plugin that has dependent plugins,    before it just deactivated the dependent plugins.    Now it reloads them too.
       Plugins with circular dependences also get unloaded properly.

--- plugins/Activator/trunk/src/main/activator/Reload.java
+++ plugins/Activator/trunk/src/main/activator/Reload.java
@@ -1,6 +1,9 @@
 package activator;
 
 import java.awt.event.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.util.*;
@@ -8,20 +11,37 @@ import org.gjt.sp.util.*;
 import common.gui.actions.*;
 
 class Reload extends CustomAction {
+	private String name;
+	private PluginList.Plugin plugin;
 	private PluginJAR jar;
-	public Reload(PluginJAR jar, String name) {
-		super(name);
+	
+	public Reload(PluginJAR pluginJAR, String dispName) {
+		super(dispName);
+		name = dispName;
+		jar = pluginJAR;
+		
+	}
+	
+	public Reload(PluginList.Plugin plugin) {
+		super(plugin.getJAR().getPlugin().getClassName());
+		this.plugin = plugin;
+		jar = plugin.getJAR();
+		name = jar.getPlugin().getClassName();
 		setToolTipText("Click to reload: " + jar.toString());
 //		setName(jar.getPlugin().getClassName());
-		this.jar=jar;
+		
 	}
 	
 	public void actionPerformed(ActionEvent event) {
+		PluginList pl = PluginList.getInstance();
 		Log.log(Log.DEBUG,this,"Reloading "+jar);
+		HashSet<String> unloaded = PluginManager.unloadPluginJAR(jar);
 		if (jar != null) {
 			PluginManager.unloadPluginJAR(jar);
 		}
-		
 		PluginManager.loadPluginJAR(jar.getPath());
+		// Reload the dependent plugins that were unloaded before
+		for (String path: unloaded) 
+			PluginManager.loadPluginJAR(path);
 	}
 }
