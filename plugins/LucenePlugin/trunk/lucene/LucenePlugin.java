6056415535d2eb61a71636ce5de6e4eb063adf20
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-23 03:19:52 +0000

    New plugin: apache Lucene.

---
+++ plugins/LucenePlugin/trunk/lucene/LucenePlugin.java
@@ -0,0 +1,13 @@
+package lucene;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+/**
+* really a no-op here. provided to make plugin manager happy
+*/
+
+public class LucenePlugin extends EditPlugin {
+    
+
+
+}
