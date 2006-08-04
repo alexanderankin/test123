13976d582f1e35a692dbef22506c93e10ae27542
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-08-04 17:41:58 +0000

    Tagging a version of Optional for 4.3pre6.

---
+++ plugins/Optional/tags/release-0-2-1/optional/OptionalPlugin.java
@@ -0,0 +1,39 @@
+/*
+ * OptionalPlugin.java -
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 1998, 2003 Slava Pestov
+ * Portions copyright (C) 1999 mike dillon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package optional;
+
+import org.gjt.sp.jedit.BeanShellAction;
+import org.gjt.sp.jedit.EditAction;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.jEdit;
+
+/**
+  Plugin for replacing option dialogs.
+  @author Alan Ezust
+*/
+public class OptionalPlugin extends EditPlugin
+{
+	static public String NAME="optional";
+
+	
+}
