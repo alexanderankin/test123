4f805789f50c6b5f1b8eddac1c3547fad134ed3e
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-08-10 23:17:13 +0000

    added 'convert' utility method.

--- plugins/SideKick/trunk/sidekick/util/ElementUtil.java
+++ plugins/SideKick/trunk/sidekick/util/ElementUtil.java
@@ -1,9 +1,41 @@
+/*
+Copyright (c) 2006, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+* Neither the name of the <ORGANIZATION> nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package sidekick.util;
 
+import java.util.Enumeration;
 import javax.swing.text.Position;
+import javax.swing.tree.DefaultMutableTreeNode;
 import org.gjt.sp.jedit.Buffer;
+import sidekick.IAsset;
 
 public class ElementUtil {
+    
     /**
      * Convert the start of a Location to a Position.
      * Need to create Positions for each node. The javacc parser finds line and
@@ -61,5 +93,39 @@ public class ElementUtil {
         child.setEndPosition(p);
         return p;
     }
+    
+	/**
+	 * Assumes the user objects in the given node, and child nodes, are
+	 * objects that implement SideKickElement.  This method removes the
+	 * SideKickElement from each node and replaces it with a SideKickAsset
+	 * that wraps the original SideKickElement.
+	 * @param buffer the Buffer representing the text that is to be displayed
+	 * in SideKick.  This is used to calculate positions for the individual
+	 * tree nodes.
+	 * @param node the root node of the tree to convert.
+	 */
+    public static void convert(Buffer buffer, DefaultMutableTreeNode node) {
+        // convert the children of the node
+        Enumeration children = node.children();
+        while (children.hasMoreElements()) {
+            convert(buffer, (DefaultMutableTreeNode) children.nextElement());
+        }
+
+        // convert the node itself
+        if (!(node.getUserObject() instanceof IAsset)) {
+            SideKickElement userObject = (SideKickElement)node.getUserObject();
+            Position start_position = createStartPosition(buffer, userObject);
+            Position end_position = createEndPosition(buffer, userObject);
+            SideKickAsset asset = new SideKickAsset(userObject);
+            if (asset.getName().indexOf("page") >= 0) {
+                System.out.println("+=+=+ " + asset.getName() + userObject.getStartLocation() + ":" + userObject.getEndLocation());
+                System.out.println("+++++ " + asset.getName() + start_position.getOffset() + ":" + end_position.getOffset());   
+            }
+            asset.setStart(start_position);
+            asset.setEnd(end_position);
+            node.setUserObject(asset);
+        }
+    }
+    
 
 }
