ed945addd1cb6990a646ab8abe9c1c5d79971337
Dale <danson@grafidog.com>
2006-08-11 18:25:33 +0000

    added better exception handling and error list reporting

--- plugins/XML/trunk/sidekick/css/CSS2SideKickParser.java
+++ plugins/XML/trunk/sidekick/css/CSS2SideKickParser.java
@@ -29,7 +29,6 @@ import java.util.*;
 
 import javax.swing.text.Position;
 import javax.swing.tree.DefaultMutableTreeNode;
-import errorlist.DefaultErrorSource;
 
 import org.gjt.sp.jedit.Buffer;
 import org.gjt.sp.jedit.EBComponent;
@@ -39,6 +38,9 @@ import org.gjt.sp.jedit.EditPane;
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.msg.PropertiesChanged;
+
+import errorlist.DefaultErrorSource;
+import errorlist.ErrorSource;
 
 import sidekick.*;
 import sidekick.enhanced.*; 
@@ -132,6 +134,22 @@ public class CSS2SideKickParser extends SideKickParser {
             // in the tree nodes to SideKick Assets
             ElementUtil.convert(buffer, root);
 
+            if ( !buffer.isDirty() && errorSource != null ) {
+                /* only handle errors when buffer is saved. Otherwise, there will be a lot
+                of spurious errors shown when code completion is on and the user is in the
+                middle of typing something. */
+                List<ParseError> parseErrors = parser.getParseErrors();
+                for (ParseError pe : parseErrors) {
+                    String message = pe.message;
+                    Range range = pe.range;
+                    // addError is lame -- what if the error spans more than one line?
+                    // Need to just deal with it...
+                    if (range.endLine != range.startLine) {
+                        range.endColumn = range.startColumn;   
+                    }
+                    errorSource.addError( ErrorSource.ERROR, filename, range.startLine, range.startColumn, range.endColumn, message );
+                }
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
