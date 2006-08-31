457f8bb9bbb8fd99a35574e842f6a1e05cfa39c7
jigerjava <jigerjava@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-08-31 11:21:33 +0000

    Releasing FastOpen 2.1

---
+++ plugins/FastOpen/tags/FastOpen_2_1/com/patelsoft/fastopen/IndexListener.java
@@ -0,0 +1,19 @@
+package com.patelsoft.fastopen;
+
+/**
+*
+*	@author jiger
+*	@version 1.0
+*	@created 19/Apr/2005
+*/
+
+
+public interface IndexListener
+{
+	public void indexingStarted(IndexManager manager);
+	public void indexingCompleted(IndexManager manager);
+}
+
+
+
+
