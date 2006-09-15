2a6afeb4171f362a54b45de71e5f76a80d58b059
Alan Ezust <1688991+ezust@users.noreply.github.com>
2006-09-15 02:17:22 +0000

    Tagging 0.8.0 for release.

---
+++ plugins/FTP/tags/release-0-8-0/ftp/FtpLoginException.java
@@ -0,0 +1,34 @@
+/*
+ * FtpLoginException.java - FTP error
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2002 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package ftp;
+
+import com.fooware.net.FtpResponse;
+import java.io.IOException;
+
+public class FtpLoginException extends FtpException
+{
+	public FtpLoginException(FtpResponse response)
+	{
+		super(response);
+	}
+}
