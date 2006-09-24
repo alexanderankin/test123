c0b5fa74d164539d2b806a7a59ffcab9d54e7f77
Alan Ezust <1688991+ezust@users.noreply.github.com>
2006-09-24 05:20:38 +0000

    Tagging 2.3.1 for release.

---
+++ plugins/JavaSideKick/tags/javasidekick-2-3-1/src/sidekick/java/PVHelper.java
@@ -0,0 +1,49 @@
+package sidekick.java;
+
+import java.util.*;
+import org.gjt.sp.jedit.*;
+import projectviewer.*;
+import projectviewer.vpt.*;
+import sidekick.java.util.*;
+
+
+public class PVHelper {
+
+    public static String getProjectNameForFile( String filename ) {
+        if (!isProjectViewerAvailable())
+            return null;
+        ProjectManager pm = ProjectManager.getInstance();
+        for ( Iterator it = pm.getProjects(); it.hasNext(); ) {
+            VPTProject project = ( VPTProject ) it.next();
+            VPTNode node = project.getChildNode( filename );
+            if ( node != null ) {
+                return project.getName();
+            }
+        }
+        return null;
+    }
+
+    public static boolean isProjectViewerAvailable() {
+        EditPlugin pv = jEdit.getPlugin( "projectviewer.ProjectPlugin", false );
+        return pv != null;
+    }
+
+    public static Path getClassPathForProject(String projectName) {
+        boolean useJavaClasspath = jEdit.getBooleanProperty( "sidekick.java.pv." + projectName + ".useJavaClasspath" );
+        //System.out.println("+++++ useJavaClasspath = " + useJavaClasspath);
+        String classpath = jEdit.getProperty("sidekick.java.pv." + projectName + ".optionalClasspath", "");
+        Path path = new Path(classpath);
+        if (useJavaClasspath) {
+            path.concatSystemClassPath();
+        }
+        //System.out.println("+++++ PVHelper, classpath for project " + projectName + " = " + path.toString());
+        return path;
+    }
+
+    public static Path getSourcePathForProject(String projectName) {
+        String sourcepath = jEdit.getProperty("sidekick.java.pv." + projectName + ".optionalSourcepath", "");
+        Path path = new Path(sourcepath);
+        return path;
+    }
+
+}
