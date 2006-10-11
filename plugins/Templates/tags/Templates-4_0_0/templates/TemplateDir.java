120bbb426a17341e3c23673215800ae474fa766d
sjakob <sjakob@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-10-11 19:05:43 +0000

    Created tag Templates-4_0_0.

---
+++ plugins/Templates/tags/Templates-4_0_0/templates/TemplateDir.java
@@ -0,0 +1,215 @@
+/*
+ *  TemplateDir.java - Represents a directory within the templates
+ *  directory hierarchy.
+ *  Copyright (C) 1999 Steve Jakob
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package templates;
+import java.io.File;
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.tree.TreeNode;
+import gnu.regexp.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+/**
+ * A TemplateDir is a type of TemplateFile which is a container for other
+ * TemplateFiles. In this way we can create a tree of TemplateFiles similar to a
+ * directory tree or menu hierarchy.
+ *
+ * @author   Steve Jakob
+ */
+public class TemplateDir extends TemplateFile
+{
+	private Vector templateFiles;
+	private static RE backupFilter;
+
+	//Constructors
+	public TemplateDir(TemplateDir parent, File templateFile)
+	{
+		super(parent, templateFile);
+	}
+
+	//Accessors & Mutators
+
+	//Implementors
+	public boolean isDirectory()
+	{
+		return true;
+	}
+
+	/**
+	 * Scans the templates directory and creates a Hashtable mapping template names
+	 * to template files. Backup files are ignored based on the values of the
+	 * backup prefix and suffix in the "Global Options" settings.
+	 */
+	public void refreshTemplates()
+	{
+		File f;
+		this.templateFiles = new Vector();
+		try
+		{
+			// Make sure we always have an up to date backup filter
+			createBackupFilter();
+
+			String[] files = this.templateFile.list();
+			for (int i = 0; i < files.length; i++)
+			{
+				f = new File(this.templateFile, files[i]);
+				if (f.isDirectory())
+				{
+					// Add subdirectory as a TemplateDir
+					TemplateDir submenu = new TemplateDir(this, f);
+					this.templateFiles.addElement(submenu);
+					submenu.refreshTemplates();
+				} else if (!backupFilter.isMatch(files[i]))
+				{// if not a backup file
+					TemplateFile tf = new TemplateFile(this, f);
+					this.templateFiles.addElement(tf);
+				}
+			}
+		}
+		catch (gnu.regexp.REException ree)
+		{
+			Log.log(Log.ERROR, this, jEdit.getProperty(
+						"plugin.TemplatesPlugin.error.bad-backup-filter"));
+		}
+		Collections.sort(this.templateFiles);
+	}
+
+	private static void createBackupFilter() throws gnu.regexp.REException
+	{
+		String exp = jEdit.getProperty("backup.prefix") +
+				"\\S+" +
+				jEdit.getProperty("backup.suffix");// RE for jEdit backups
+		if (exp.equals("\\S+"))
+		{
+			exp = "";
+		}
+		backupFilter = new RE(exp, RE.REG_ICASE);
+	}
+
+	/**
+	 * Add a menu item to the given menu object for each TemplateFile contained
+	 * within this TemplateDir. Recursively process any TemplateDir objects
+	 * contained within this TemplateDir.
+	 *
+	 * @param menu    The menu to which the new JMenuItem objects will be added.
+	 * @param parent  The path of the parent directory
+	 */
+	public void createMenus(JMenu menu, String parent)
+	{
+		Object o;
+		JMenu submenu;
+		TemplateAction myAction;
+		TemplateDir td;
+		TemplateFile tf;
+		Enumeration e;
+		if (!parent.endsWith(File.separator))
+		{
+			// check for root of templates tree
+			if (!"".equals(parent))
+			{
+				parent = parent + File.separator;
+			}
+		}
+		if (templateFiles == null)
+		{
+			this.refreshTemplates();
+		}
+		ActionSet myActions = new ActionSet();
+		e = this.templateFiles.elements();
+		while (e.hasMoreElements())
+		{
+			o = e.nextElement();
+			if (o instanceof TemplateDir)
+			{
+				td = (TemplateDir) o;
+				submenu = new JMenu(td.getLabel());
+				menu.add(submenu);// Add subdirectory as a sub-menu
+				td.createMenus(submenu, parent + td.getLabel());
+			} else
+			{
+				tf = (TemplateFile) o;
+				String actionName = TemplateAction.getUniqueActionName();
+				myAction = new TemplateAction(actionName,
+						tf.getLabel(), tf.getRelativePath());
+				myActions.addAction(myAction);
+				menu.add(GUIUtilities.loadMenuItem(jEdit.getActionContext(), actionName, false));
+			}
+		}
+		jEdit.addActionSet(myActions);
+	}
+
+	public Enumeration children()
+	{
+		return templateFiles.elements();
+	}
+
+	public boolean getAllowsChildren()
+	{
+		return true;
+	}
+
+	public TreeNode getChildAt(int index)
+	{
+		return (TreeNode) templateFiles.elementAt(index);
+	}
+
+	public int getChildCount()
+	{
+		return templateFiles.size();
+	}
+
+	public int getIndex(TreeNode child)
+	{
+		return templateFiles.indexOf(child);
+	}
+
+	public boolean isLeaf()
+	{
+		return false;
+	}
+
+	/**
+	 * Generates a string representation of the template hierarchy. This method is
+	 * intended as a debugging tool.
+	 *
+	 * @return   A String representation of the template hierarchy, with each
+	 *      directory and filename on a separate line.
+	 */
+	public String printDir()
+	{
+		String newLine = System.getProperty("line.separator");
+		StringBuffer retStr = new StringBuffer("Dir: " + this.getLabel() + newLine);
+		Enumeration kids = this.children();
+		while (kids.hasMoreElements())
+		{
+			TemplateFile f = (TemplateFile) kids.nextElement();
+			if (f.isDirectory())
+			{
+				retStr.append(((TemplateDir) f).printDir());
+			} else
+			{
+				retStr.append(f.getRelativePath() + newLine);
+			}
+		}
+		return retStr.toString();
+	}
+
+}
+
---
+++ plugins/Templates/tags/Templates-4_0_0/templates/TemplateFile.java
@@ -0,0 +1,226 @@
+/*
+ *  TemplateFile.java - Represents a file within the templates
+ *  directory hierarchy.
+ *  Copyright (C) 1999 Steve Jakob
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package templates;
+
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.tree.TreeNode;
+import gnu.regexp.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.util.Log;
+/**
+ * A TemplateFile is similar to a java.io.File object as it acts as a reference
+ * to a template file, but it also contains information describing the template
+ * (eg. a label to be used on the Templates menu).
+ *
+ * @author   Steve Jakob
+ */
+public class TemplateFile implements TreeNode, Comparable
+{
+	private final static String labelRE =
+			"(\\s*##\\s*)(TEMPLATE)(\\s*=\\s*)(\\S+.*)";
+	protected String label;
+	protected File templateFile;
+	private static RE ctpragmaLabelFilter = null;
+	private TemplateDir parent;
+
+	//Constructors
+	public TemplateFile(TemplateDir parent, File templateFile)
+	{
+		super();
+		this.parent = parent;
+		this.templateFile = templateFile;
+		this.label = templateFile.getName();
+		createREs();
+		if (!this.isDirectory())
+		{
+			String s = null;
+			try
+			{
+				s = readTemplateLabel(this.getBufferedReader());
+			}
+			catch (Exception e)
+			{
+				Log.log(Log.ERROR, this, jEdit.getProperty(
+						"plugin.TemplatesPlugin.error.template-label")
+						 + templateFile.getName());
+				Log.log(Log.ERROR, this, e);
+			}
+			if (s != null)
+			{
+				label = s;
+			}
+		}
+	}
+
+	//Accessors & Mutators
+	public String getLabel()
+	{
+		return label;
+	}
+
+	public void setLabel(String labelVal)
+	{
+		label = labelVal;
+	}
+
+	public String getPath()
+	{
+		return templateFile.getPath();
+	}
+
+	/**
+	 * Determine the relative path of the file from the templates directory, given
+	 * the file's absolute path.
+	 *
+	 * @return   The relativePath value
+	 */
+	public String getRelativePath()
+	{
+		String absolutePath = templateFile.getPath();
+		if (absolutePath.startsWith(TemplatesPlugin.getTemplateDir()))
+		{
+			return absolutePath.substring(TemplatesPlugin.getTemplateDir().length());
+		}
+		return absolutePath;
+	}
+
+	//Implementors
+	public boolean isDirectory()
+	{
+		return false;
+	}
+
+
+	/**
+	 * Convenience method to create a BufferedReader to the template file.
+	 *
+	 * @return                           A BufferedReader object corresponding to
+	 *      the underlying file.
+	 * @exception FileNotFoundException  Thrown if the template file is not found
+	 */
+	public BufferedReader getBufferedReader() throws FileNotFoundException
+	{
+		return new BufferedReader(new FileReader(this.templateFile));
+	}
+
+	private static String readTemplateLabel(BufferedReader in) throws IOException
+	{
+		String templateLabel = null;
+		try
+		{
+			String line;
+			if ((line = in.readLine()) != null)
+			{
+				REMatch labelMatch = ctpragmaLabelFilter.getMatch(line);
+				if (labelMatch != null)
+				{
+					templateLabel = labelMatch.toString(4);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			throw e;
+		}// In case of problems, throw the exception to the caller
+		finally
+		{// but close the file, also.
+			try
+			{
+				in.close();
+			}
+			catch (IOException ioe)
+			{}
+		}
+		return templateLabel;
+	}
+
+	/**
+	 * Creates a RE to parse #ctpragma directives. Each directive is composed of 4
+	 * parts:<P>
+	 *
+	 *
+	 * <LI> #ctpragma
+	 * <LI> the directive type (eg. LABEL, NAME, etc.)
+	 * <LI> an equals ("=") sign
+	 * <LI> the value to assign for this directive type
+	 */
+	private static void createREs()
+	{
+		try
+		{
+			ctpragmaLabelFilter = new RE(labelRE, RE.REG_ICASE);
+		}
+		catch (gnu.regexp.REException e)
+		{}// this shouldn't happen
+	}
+
+	public int compareTo(Object o)
+	{
+		TemplateFile t2 = (TemplateFile) o;
+		return this.toString().compareToIgnoreCase(t2.toString());
+	}
+
+	public String toString()
+	{
+		return label;
+	}
+
+	//
+	// The next seven methods satisfy the TreeNode interface requirements.
+	//
+	public Enumeration children()
+	{
+		return null;
+	}
+
+	public boolean getAllowsChildren()
+	{
+		return false;
+	}
+
+	public TreeNode getChildAt(int index)
+	{
+		return null;
+	}
+
+	public int getChildCount()
+	{
+		return 0;
+	}
+
+	public int getIndex(TreeNode child)
+	{
+		return -1;
+	}
+
+	public TreeNode getParent()
+	{
+		return parent;
+	}
+
+	public boolean isLeaf()
+	{
+		return true;
+	}
+
+}
+
