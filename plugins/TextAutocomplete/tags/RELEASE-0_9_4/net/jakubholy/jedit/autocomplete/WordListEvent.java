4fefa690ae0eb192d2719cefa4941f91fe6f1af0
nobody <nobody@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-10-22 16:55:18 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-0_9_4'.

---
+++ plugins/TextAutocomplete/tags/RELEASE-0_9_4/net/jakubholy/jedit/autocomplete/WordListEvent.java
@@ -0,0 +1,16 @@
+package net.jakubholy.jedit.autocomplete;
+
+import java.util.EventObject;
+
+/**
+ * Carry information about a change to the observed word list.
+ * @see net.jakubholy.jedit.autocomplete.WordList 
+ * @author Jakub Holy
+ */
+public class WordListEvent extends EventObject 
+{
+
+	public WordListEvent(Object source) 
+	{ super(source); }
+
+}
