8f73971f8a9c45a25476ac1a4b5bd50eb83948ec
bhawkins1 <bhawkins1@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-11-03 04:17:55 +0000

    + Migrated to 4.2 plugin API for dynamic loading + Moved option pane definition into the jimporter.props + Fixed a class cast bug in the "move up" and "move down" buttons of the sorting option pane, and put the whitespace button back in (though it does not work yet) + Repaired parsing of the word at the caret + Implemented "Insert FQ Classname at Point", using auto-import when enabled and possible

--- plugins/JImporter/trunk/jimporter/JImporterPlugin.java
+++ plugins/JImporter/trunk/jimporter/JImporterPlugin.java
@@ -30,7 +30,7 @@ import org.gjt.sp.jedit.gui.OptionsDialog;
 import org.gjt.sp.jedit.jEdit;
 
 /**
- * This class is designed to follow the JEdit API specification for adding a 
+ * This class is designed to follow the JEdit API specification for adding a
  * plugin, or extension to the JEdit Extension.  This class is responsible for
  * defining which menus will appear and which option panes will appear.  There
  * are also facilities for initialization and finalization code.
@@ -39,53 +39,53 @@ import org.gjt.sp.jedit.jEdit;
  * @since 08/29/2002
  */
 public class JImporterPlugin extends EditPlugin {
-    /** 
-     * The name method can be used as a prefix for any properties stored in a
-     * properties file or by jEdit.setProperty().
-     */    
-    public String NAME = "jimporter";
-    /** 
-     * All options are stored with the prefix "options.jimporter".  This variable makes
-     * that knowledge available throughout the class.
-     */    
-    public String OPTION_PREFIX = "options." + NAME + "." ;
-    
-    /** 
-     * This method initializes the JImporter plugin.  Currently, it doesn't do any
-     * initialization in advance.
-     */    
-    public void start() {
-    }
-    
-    /** 
-     * This method could perform extra handling when the JImporter plugin was stopped.
-     * Right now it performs no special functions.
-     */    
-    public void stop() {
-    }
-    
-    /** 
-     * Add the JImporter menu items to JEdit.
-     *
-     * @param menuItems A vector of already existing menuItems that JImporter will add it's menu to.
-     */    
-    public void createMenuItems(Vector menuItems) {
-        menuItems.addElement(GUIUtilities.loadMenu("jimporter-menu"));
-    }
-    
-    /** 
-     * Create the JImporter options page that will be used to capture configuration
-     * information.
-     *
-     * @param dialog The dialog box to which JImporter will add it's 
-     * configuration-capturing option pane.
-     */    
-    public void createOptionPanes(OptionsDialog dialog) {
-        OptionGroup og = new OptionGroup("jimporter");
-        og.addOptionPane(new ClasspathOptionPane());
-        og.addOptionPane(new SortingOptionPane());
-        og.addOptionPane(new MiscellaneousOptionPane());
-        
-        dialog.addOptionGroup(og);
-    }
-}
+	/**
+	 * The name method can be used as a prefix for any properties stored in a
+	 * properties file or by jEdit.setProperty().
+	 */
+	public String NAME = "jimporter";
+	/**
+	 * All options are stored with the prefix "options.jimporter".  This variable makes
+	 * that knowledge available throughout the class.
+	 */
+	public String OPTION_PREFIX = "options." + NAME + "." ;
+
+	/**
+	 * This method initializes the JImporter plugin.  Currently, it doesn't do any
+	 * initialization in advance.
+	 */
+	public void start() {
+	}
+
+	/**
+	 * This method could perform extra handling when the JImporter plugin was stopped.
+	 * Right now it performs no special functions.
+	 */
+	public void stop() {
+	}
+
+	/**
+	 * Add the JImporter menu items to JEdit.
+	 *
+	 * @param menuItems A vector of already existing menuItems that JImporter will add it's menu to.
+	 */
+/* 	public void createMenuItems(Vector menuItems) {
+		menuItems.addElement(GUIUtilities.loadMenu("plugin.jimporter.JImporterPlugin"));
+	}
+ */
+	/**
+	 * Create the JImporter options page that will be used to capture configuration
+	 * information.
+	 *
+	 * @param dialog The dialog box to which JImporter will add it's
+	 * configuration-capturing option pane.
+	 */
+/* 	public void createOptionPanes(OptionsDialog dialog) {
+		OptionGroup og = new OptionGroup("jimporter");
+		og.addOptionPane(new ClasspathOptionPane());
+		og.addOptionPane(new SortingOptionPane());
+		og.addOptionPane(new MiscellaneousOptionPane());
+
+		dialog.addOptionGroup(og);
+	}
+ */}
--- plugins/JImporter/trunk/jimporter/importer/ClassImporter.java
+++ plugins/JImporter/trunk/jimporter/importer/ClassImporter.java
@@ -7,83 +7,85 @@ import jimporter.sorting.SortOnImportOption;
 import jimporter.sorting.ImportSorter;
 
 public abstract class ClassImporter{
-    protected String importClass;
-    protected Buffer sourceBuffer;
-    protected boolean sortImportStatements = false;
+	protected String importClass;
+	protected Buffer sourceBuffer;
+	protected boolean sortImportStatements = false;
 
-    /**
-     * Set the buffer that we are going to add the import to.
-     *
-     * @param source The source buffer we are going to add the import to.
-     * @see #getSourceBuffer
-     */
-    public void setSourceBuffer(Buffer source) {
-        sourceBuffer = source;
-    }
+	/**
+	 * Set the buffer that we are going to add the import to.
+	 *
+	 * @param source The source buffer we are going to add the import to.
+	 * @see #getSourceBuffer
+	 */
+	public void setSourceBuffer(Buffer source) {
+		sourceBuffer = source;
+	}
 
-    /**
-     * Get the buffer we are going to add the import to.
-     *
-     * @return A <code>Buffer</code> that is the buffer we are going to add the
-     * import into.
-     * @see #setSourceBuffer
-     */
-    public Buffer getSourceBuffer() {
-        return sourceBuffer;
-    }
+	/**
+	 * Get the buffer we are going to add the import to.
+	 *
+	 * @return A <code>Buffer</code> that is the buffer we are going to add the
+	 * import into.
+	 * @see #setSourceBuffer
+	 */
+	public Buffer getSourceBuffer() {
+		return sourceBuffer;
+	}
 
-    /**
-     * Sets the fully qualified name of the class we are going to import.
-     *
-     * @param fqClassName The new importClass value
-     * @see #getImportClass
-     */
-    public void setImportClass(String fqClassName) {
-        importClass = fqClassName;
-    }
+	/**
+	 * Sets the fully qualified name of the class we are going to import.
+	 *
+	 * @param fqClassName The new importClass value
+	 * @see #getImportClass
+	 */
+	public void setImportClass(String fqClassName) {
+		importClass = fqClassName;
+	}
 
-    /**
-     * Gets the name of the class we are going to import.
-     *
-     * @return A <code>String</code> value containing the fq class name of the
-     * class we are going to import.
-     * @see #setImportClass
-     */
-    public String getImportClass() {
-        return importClass;
-    }
+	/**
+	 * Gets the name of the class we are going to import.
+	 *
+	 * @return A <code>String</code> value containing the fq class name of the
+	 * class we are going to import.
+	 * @see #setImportClass
+	 */
+	public String getImportClass() {
+		return importClass;
+	}
 
-    public boolean addImportToBuffer() {
-        //Check prerequisites
-        if (importClass == null) {
-            throw new MissingParameterException("importClass", "findInsertLocation()");
-        }
-        if (sourceBuffer == null) {
-            throw new MissingParameterException("sourceBuffer", "findInsertLocation()");
-        }
+	public boolean addImportToBuffer() {
+		//Check prerequisites
+		if (importClass == null) {
+			throw new MissingParameterException("importClass", "findInsertLocation()");
+		}
+		if (sourceBuffer == null) {
+			throw new MissingParameterException("sourceBuffer", "findInsertLocation()");
+		}
 
-        ImportList importList = ImportListFactory.getInstance(sourceBuffer.getMode().getName());
+		ImportList importList = ImportListFactory.getInstance(sourceBuffer.getMode().getName());
 
-        //Make sure that the class hasn't already been imported
-        if (importList.checkForDuplicateImport(importClass, importList)) {
-            jEdit.getActiveView().getStatus().setMessageAndClear(importClass + " has already been imported.  Your request to import again has been ignored.");
-            return false;
-        }
+		importList.setSourceBuffer(sourceBuffer);
 
-        //Get the location we are going to do our insert at
-        int insertLocation = findInsertLocation(importList);
+		//Make sure that the class hasn't already been imported
+		if (importList.checkForDuplicateImport(importClass, importList)) {
+			jEdit.getActiveView().getStatus().setMessageAndClear(importClass + " has already been imported.  Your request to import again has been ignored.");
+			return false;
+		}
 
-        //Add the import to the source buffer
-        sourceBuffer.insert(insertLocation, importClass);
+		//Get the location we are going to do our insert at
+		int insertLocation = findInsertLocation(importList);
 
-        //See if we are supposed to sort on import.  If so, do it.
-        if (new SortOnImportOption().state()) {
-            new ImportSorter(jEdit.getActiveView()).sort();
-        }
+		//Add the import to the source buffer
+		sourceBuffer.insert(insertLocation, importClass);
 
-        //Everything seems to have gone well, indicate success to the caller
-        return true;
-    }
+		//See if we are supposed to sort on import.  If so, do it.
+		if (new SortOnImportOption().state()) {
+			new ImportSorter(jEdit.getActiveView()).sort();
+		}
 
-    public abstract int findInsertLocation(ImportList importList);
+		//Everything seems to have gone well, indicate success to the caller
+		return true;
+	}
+
+	public abstract int findInsertLocation(ImportList importList);
 }
