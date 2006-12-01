92a44b0dbf58c5a117905b88f2bd75ddb813834a
Sergey Udaltsov <sergey.udaltsov@gmail.com>
2006-12-01 15:03:19 +0000

    reformatting copyright statements

--- plugins/SQL/trunk/sql/serverTypes/progress/StoredProcedureObjectType.java
+++ plugins/SQL/trunk/sql/serverTypes/progress/StoredProcedureObjectType.java
@@ -1,24 +1,26 @@
-/*
- *  StoredProcedureObjectType.java - Sql Plugin
+/**
+ * StoredProcedureObjectType.java - Sql Plugin
  * :tabSize=8:indentSize=8:noTabs=false:
- *  Copyright (C) 2003 Gerke Kok
- *  gkokmdam@zonnet.nl ( for as long as Zonnet stays for free :-) )
+ *
+ * Copyright (C) 2003 Gerke Kok
+ * gkokmdam@zonnet.nl ( for as long as Zonnet stays for free :-) )
  *
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package sql.serverTypes.progress;
 
 import java.io.*;
@@ -35,7 +37,6 @@ import sql.serverTypes.*;
  *  This class will retieve the text of a give stored procedure
  *
  * @author     gkokmdam
- * @created    23 Feb 2003
  */
 public class StoredProcedureObjectType extends CodeObjectType
 {
