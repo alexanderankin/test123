d0ecf75fc5a006a7a2911f500d45be41ab9292d0
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-12-27 22:58:41 +0000

    Added the option to show all inherited members of the selected class as well as those defined in the class itself. Minimal support has been added for this: 1. The inherited members list is ordered by the defining class, from the class itself up to the root (most distant ancestor). There are no options for different ordering. 2. Accessibility (private/public/..) is not taken into account. 3. Tag types are not taken into account. Each tag name may appear only once in the inherited member list.

--- plugins/ClassBrowser/trunk/browser/ClassHierarchy.java
+++ plugins/ClassBrowser/trunk/browser/ClassHierarchy.java
@@ -1,5 +1,6 @@
 package browser;
 
+
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Toolkit;
@@ -10,8 +11,10 @@ import java.awt.event.MouseEvent;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Vector;
 
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -54,12 +57,14 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 
 	private JPanel topPanel;
 
-	private JToolBar toolbar;
-
+	private JToolBar hierarchyToolbar;
 	private JToggleButton superTypeHierButton;
-
 	private JToggleButton subTypeHierButton;
 
+	private JToolBar memberToolbar;
+	private JToggleButton selfMembersButton;
+	private JToggleButton derivedMembersButton;
+	
 	DefaultMutableTreeNode superTypeRoot;
 
 	DefaultMutableTreeNode subTypeRoot;
@@ -71,7 +76,9 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 	private Vector<String> derivedClasses = new Vector<String>();
 
 	private Hashtable<String, Vector<Object>> membersHash = new Hashtable<String, Vector<Object>>();
-
+	private Hashtable<String, Vector<Object>> derivedMembersHash =
+		new Hashtable<String, Vector<Object>>();
+	
 	public ClassHierarchy(View view) {
 		super(new BorderLayout());
 
@@ -83,14 +90,17 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 		members.setCellRenderer(new MemberCellRenderer());
 		members.addMouseListener(new MemberCellActionHandler(members));
 		
-		buildToolbar();
-
+		buildHierarchyToolbar();
+		buildMemberToolbar();
+		
 		JPanel treePanel = new JPanel();
 		treePanel.setLayout(new BorderLayout());
+		treePanel.add(hierarchyToolbar, BorderLayout.NORTH);
 		treePanel.add(new JScrollPane(tree), BorderLayout.CENTER);
 
 		JPanel memberPanel = new JPanel();
 		memberPanel.setLayout(new BorderLayout());
+		memberPanel.add(memberToolbar, BorderLayout.NORTH);
 		memberPanel.add(new JScrollPane(members), BorderLayout.CENTER);
 
 		splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, treePanel,
@@ -98,7 +108,6 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 		splitPane.setOneTouchExpandable(true);
 
 		topPanel = new JPanel(new BorderLayout());
-		topPanel.add(BorderLayout.NORTH, toolbar);
 		topPanel.add(BorderLayout.CENTER, splitPane);
 
 		setLayout(new BorderLayout());
@@ -201,11 +210,61 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 		}
 		updateTree();
 		getMembers(classesInHierarchy);
-		setMembers(clazz);
+		setMembers();
 	}
 
+	private String tagName(Object obj)
+	{
+		if (obj instanceof Record)
+			return ((Record)obj).getName();
+		return obj.toString();
+	}
+	private void buildDerivedMembers(String clazz)
+	{
+		HashSet<String> seen = new HashSet<String>();
+		Vector<Object> derivedMembers = new Vector<Object>();
+		LinkedList<String> classes = new LinkedList<String>();
+		classes.add(clazz);
+		while (! classes.isEmpty())
+		{
+			String curClass = classes.removeFirst();
+			Vector<Object> classMembers = membersHash.get(curClass);
+			if (classMembers == null)
+				continue;
+			for (int i = 0; i < classMembers.size(); i++)
+			{
+				Object member = classMembers.get(i);
+				String memberName = tagName(member);
+				if (! seen.contains(memberName))
+				{
+					derivedMembers.add(member);
+					seen.add(memberName);
+				}
+			}
+			Record classTag = findClass(curClass);
+			String inheritsStr = classTag.get(TagDB.INHERITS_COL);
+			if (inheritsStr != null)
+			{
+				String [] children = inheritsStr.split(",");
+				for (int i = 0; i < children.length; i++)
+					classes.add(children[i]);
+			}
+		}
+		derivedMembersHash.put(clazz, derivedMembers);
+	}
+	private String getSelectedClassName()
+	{
+		DefaultMutableTreeNode node =
+			(DefaultMutableTreeNode)tree.getLastSelectedPathComponent();
+		if (node == null)
+			return null;
+		Object obj = node.getUserObject();
+		String clazz = tagName(obj);
+		return clazz;
+	}
 	private void getMembers(HashSet<String> classes) {
 		membersHash.clear();
+		derivedMembersHash.clear();
 		StringBuffer classStr = new StringBuffer("(");
 		Iterator<String> it = classes.iterator();
 		while (it.hasNext())
@@ -227,12 +286,25 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 			classMembers.add(member);
 		}
 	}
-
-	private void setMembers(String clazz) {
-		if (membersHash.containsKey(clazz))
-			members.setListData(membersHash.get(clazz));
+	private void setMembers() {
+		String clazz = getSelectedClassName();
+		if (clazz == null)
+			return;
+		if (derivedMembersButton.isSelected())
+		{
+			if (clazz == null)
+				return;
+			if (! derivedMembersHash.containsKey(clazz))
+				buildDerivedMembers(clazz);
+			members.setListData(derivedMembersHash.get(clazz));
+		}
 		else
-			members.setListData(new Vector<String>());
+		{
+			if (membersHash.containsKey(clazz))
+				members.setListData(membersHash.get(clazz));
+			else
+				members.setListData(new Vector<String>());
+		}
 	}
 
 	public static void set(View view) {
@@ -258,28 +330,41 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 		instance.setClass(view, selected);
 	}
 	
-	private void buildToolbar() {
-		toolbar = new JToolBar();
-		toolbar.setFloatable(false);
+	private void buildHierarchyToolbar() {
+		hierarchyToolbar = new JToolBar();
+		hierarchyToolbar.setFloatable(false);
 		superTypeHierButton = new JToggleButton("Base", true);
 		ActionListener l = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				boolean superType;
-				if (e.getSource() == superTypeHierButton) {
-					superType = superTypeHierButton.isSelected();
-					subTypeHierButton.setSelected(!superType);
-				} else {
-					superType = (!subTypeHierButton.isSelected());
-					superTypeHierButton.setSelected(superType);
-				}
 				updateTree();
 			}
 		};
 		superTypeHierButton.addActionListener(l);
 		subTypeHierButton = new JToggleButton("Derived", false);
 		subTypeHierButton.addActionListener(l);
-		toolbar.add(superTypeHierButton);
-		toolbar.add(subTypeHierButton);
+		ButtonGroup buttons = new ButtonGroup();
+		buttons.add(superTypeHierButton);
+		buttons.add(subTypeHierButton);
+		hierarchyToolbar.add(superTypeHierButton);
+		hierarchyToolbar.add(subTypeHierButton);
+	}
+	private void buildMemberToolbar() {
+		memberToolbar = new JToolBar();
+		memberToolbar.setFloatable(false);
+		selfMembersButton = new JToggleButton("Self", true);
+		derivedMembersButton = new JToggleButton("Inherited", false);
+		ActionListener l = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setMembers();
+			}
+		};
+		selfMembersButton.addActionListener(l);
+		derivedMembersButton.addActionListener(l);
+		ButtonGroup buttons = new ButtonGroup();
+		buttons.add(selfMembersButton);
+		buttons.add(derivedMembersButton);
+		memberToolbar.add(selfMembersButton);
+		memberToolbar.add(derivedMembersButton);
 	}
 
 	private void updateTree() {
@@ -307,18 +392,7 @@ public class ClassHierarchy extends JPanel implements DefaultFocusComponent {
 				TreeSelectionModel.SINGLE_TREE_SELECTION);
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
 			public void valueChanged(TreeSelectionEvent arg0) {
-				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree
-						.getLastSelectedPathComponent();
-				if (node == null)
-					return;
-
-				String clazz;
-				Object obj = node.getUserObject();
-				if (obj instanceof Record)
-					clazz = ((Record) obj).getName();
-				else
-					clazz = (String) obj;
-				setMembers(clazz);
+				setMembers();
 			}
 		});
 		// tree.addMouseListener(new MouseHandler());
