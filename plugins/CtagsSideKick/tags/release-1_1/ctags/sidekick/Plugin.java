a508649005b96592f40c54d9a12132990345774d
Alan Ezust <1688991+ezust@users.noreply.github.com>
2007-01-08 18:09:33 +0000

    Tagging 1.1 for release.

---
+++ plugins/CtagsSideKick/tags/release-1_1/ctags/sidekick/Plugin.java
@@ -0,0 +1,43 @@
+/*
+Copyright (C) 2006  Shlomy Reinstein
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+package ctags.sidekick;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+
+import sidekick.SideKickPlugin;
+
+
+public class Plugin extends EditPlugin {
+	public static String NAME = "CtagsSideKick";
+	public static String OPTION_PREFIX = "options.CtagsSideKick.";
+
+	public static void groupBy(View view, String mapper)
+	{
+		jEdit.setProperty(OptionPane.MAPPER, mapper);
+		SideKickPlugin.parse(view, true);
+	}
+	public static void setSorting(View view, boolean sort, boolean foldsFirst)
+	{
+		jEdit.setBooleanProperty(OptionPane.SORT, sort);
+		if (sort)
+			jEdit.setBooleanProperty(OptionPane.FOLDS_BEFORE_LEAFS, foldsFirst);
+		SideKickPlugin.parse(view, true);
+	}
+}
