b97b938a93e53c0221c44fa97398cab8e5767a0a
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-01-10 07:18:43 +0000

    Added license and updated the documentation.

--- plugins/ClassBrowser/trunk/browser/ClassBrowserPlugin.java
+++ plugins/ClassBrowser/trunk/browser/ClassBrowserPlugin.java
@@ -1,46 +1,33 @@
 /*
- * TagsPlugin.java
- * Copyright (c) 2001, 2002 Kenrick Drew (kdrew@earthlink.net)
- * Copyright (c) 2003, 2004 Ollie Rutherfurd (oliver@jedit.org)
- *
- * This file is part of the Tags plugin.
- *
- * TagsPlugin is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * TagsPlugin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA	02111-1307, USA.
- *
- * $Id: TagsPlugin.java 7768 2006-10-26 23:24:52Z ezust $
- */
+Copyright (C) 2007  Shlomy Reinstein
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 package browser;
 
-//{{{ imports
 import org.gjt.sp.jedit.EditPlugin;
 
 public class ClassBrowserPlugin extends EditPlugin
 {
-	//{{{ EBPlugin methods
-
-	//{{{ start() method
 	public void start()
 	{
-	} //}}}
+	}
 
-	//{{{ stop()
 	public void stop()
 	{
-	} //}}}
+	}
 
 }
-
-// :collapseFolds=1:noTabs=false:lineSeparator=\r\n:tabSize=4:indentSize=4:deepIndent=false:folding=explicit:
