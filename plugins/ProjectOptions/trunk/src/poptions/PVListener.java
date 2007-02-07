a5ab0626b49190bbc99d10bbfab280d6622366c5
vanza <vanza@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-02-07 05:55:40 +0000

    Initial import of ProjectOptions plugin

---
+++ plugins/ProjectOptions/trunk/src/poptions/PVListener.java
@@ -0,0 +1,57 @@
+/*
+ * :tabSize=4:indentSize=4:noTabs=true:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * (c) 2007 Marcelo Vanzin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package poptions;
+
+import org.gjt.sp.jedit.jEdit;
+
+import projectviewer.event.ProjectViewerAdapter;
+import projectviewer.event.ProjectViewerEvent;
+
+/**
+ *  Listens for PV events and updates the options.
+ *
+ *  @author     Marcelo Vanzin
+ *  @version    $Id$
+ *  @since      POP 0.1.0
+ */
+public class PVListener extends ProjectViewerAdapter
+{
+
+    private ProjectOptionsPlugin plugin;
+
+    public PVListener()
+    {
+        plugin = (ProjectOptionsPlugin)
+                    jEdit.getPlugin("poptions.ProjectOptionsPlugin");
+    }
+
+    public void groupActivated(ProjectViewerEvent evt)
+    {
+        plugin.setProjectOptions(null);
+    }
+
+    public void projectLoaded(ProjectViewerEvent evt)
+    {
+        plugin.setProjectOptions(evt.getProject());
+    }
+
+}
+
