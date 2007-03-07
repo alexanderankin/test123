6a0d49433b1bfce0fd3fe999ca01917b8c4491b8
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-03-07 14:27:51 +0000

    Added support for children of watches.

--- plugins/GdbPlugin/trunk/gdb/views/Watches.java
+++ plugins/GdbPlugin/trunk/gdb/views/Watches.java
@@ -7,6 +7,7 @@ import gdb.Parser.ResultHandler;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -42,7 +43,6 @@ public class Watches extends JPanel {
 					return;
 				Watch w = new Watch(expr);
 				watches.add(w);
-				w.eval();
 				root.add(w);
 				updateTree();
 			}
@@ -85,7 +85,7 @@ public class Watches extends JPanel {
 	public void update() {
 		root.removeAllChildren();
 		for (int i = 0; i < watches.size(); i++) {
-			watches.get(i).eval();
+			watches.get(i).update();
 		}
 	}
 	public void sessionEnded() {
@@ -101,18 +101,25 @@ public class Watches extends JPanel {
 	
 	private class Watch extends DefaultMutableTreeNode {
 		private String name;
-		private String value;
+		private String value = null;
 		private String gdbName = null;
 		
 		public Watch(String name) {
 			this.name = name;
-			this.value = value;
+			createGdbVar();
+		}
+		public Watch(String name, String gdbName) {
+			this.name = name;
+			this.gdbName = gdbName;
+			createChildren();
+			getValue();
 		}
 		public String toString() {
-			return name + " = " + value;
+			return name + (value != null ? " = " + value : "");
 		}
-		public void getValue() {
-			commandManager.add("-var-update " + gdbName);
+		private void getValue() {
+			if (commandManager == null)
+				return;
 			commandManager.add("-var-evaluate-expression " + gdbName,
 					new ResultHandler() {
 					public void handle(String msg, GdbResult res) {
@@ -125,22 +132,53 @@ public class Watches extends JPanel {
 					}
 			});
 		}
-		public void eval() {
+		private void createChildren() {
+			commandManager.add("-var-list-children " + gdbName,
+				new ResultHandler() {
+					public void handle(String msg, GdbResult res) {
+						if (! msg.equals("done"))
+							return;
+						int nc = Integer.parseInt(res.getStringValue("numchild"));
+						for (int i = 0; i < nc; i++) {
+							String base = "children/" + i + "/child/";
+							String cname = res.getStringValue(base + "name");
+							String cexp = res.getStringValue(base + "exp");
+							Watch child = new Watch(cexp, cname);
+							add(child);
+						}
+					}
+				}
+			);
+		}
+		public void update() {
+			if (gdbName == null)
+				createGdbVar();
+			else {
+				if (commandManager == null)
+					return;
+				commandManager.add("-var-update " + gdbName);
+				getValue();
+				Enumeration<Watch> c = this.children();
+				while (c.hasMoreElements()) {
+					Watch child = c.nextElement();
+					child.update();
+				}
+			}
+		}
+		private void createGdbVar() {
 			if (commandManager == null)
 				return;
-			if (gdbName != null)
-				getValue();
-			else {
-				commandManager.add("-var-create - * " + name,
-						new ResultHandler() {
-							public void handle(String msg, GdbResult res) {
-								if (! msg.equals("done"))
-									return;
-								gdbName = res.getStringValue("name");
-								getValue();
-							}
-				});
-			}
+			commandManager.add("-var-create - * " + name,
+				new ResultHandler() {
+					public void handle(String msg, GdbResult res) {
+						if (! msg.equals("done"))
+							return;
+						gdbName = res.getStringValue("name");
+						createChildren();
+						getValue();
+					}
+				}
+			);
 		}
 	}
 }
