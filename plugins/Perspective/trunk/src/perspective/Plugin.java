6b60b5c5d9c265ea35a2d9bdf9ec4e73af084122
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-03-26 12:11:44 +0000

    Added a heading and removed an obsolete method.

--- plugins/Perspective/trunk/src/perspective/Plugin.java
+++ plugins/Perspective/trunk/src/perspective/Plugin.java
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2007  Shlomy Reinstein
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 package perspective;
 
 import java.io.File;
@@ -31,8 +49,7 @@ public class Plugin extends EditPlugin {
 	}
 	public void stop() {
 	}
-	static public void write() {
-	}
+	
 	static public void save(View view) {
 		String name = JOptionPane.showInputDialog(null, "Perspective name:",
 				"Save perspective", JOptionPane.QUESTION_MESSAGE);
