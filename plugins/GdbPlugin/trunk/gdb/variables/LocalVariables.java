cbb058155652e62840e683c8bcde031ecb30a34e
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-04-01 21:05:25 +0000

    Created a Console class for gdb/program output.

--- plugins/GdbPlugin/trunk/gdb/variables/LocalVariables.java
+++ plugins/GdbPlugin/trunk/gdb/variables/LocalVariables.java
@@ -12,7 +12,6 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
