94b88fdf7ae36ce959325ce5bb9910b9aa4da0eb
Dale <danson@grafidog.com>
2007-04-02 04:07:28 +0000

    updates

--- plugins/JavaSideKick/trunk/src/sidekick/java/node/TigerNode.java
+++ plugins/JavaSideKick/trunk/src/sidekick/java/node/TigerNode.java
@@ -2,27 +2,27 @@
 Copyright (c) 2005, Dale Anson
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-  * Redistributions of source code must retain the above copyright notice, 
+  * Redistributions of source code must retain the above copyright notice,
   this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright notice, 
-  this list of conditions and the following disclaimer in the documentation 
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
-  * Neither the name of the <ORGANIZATION> nor the names of its contributors 
-  may be used to endorse or promote products derived from this software without 
+  * Neither the name of the <ORGANIZATION> nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
   specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package sidekick.java.node;
@@ -35,11 +35,11 @@ import sidekick.Asset;
 
 
 /**
- * Base class for all "node's" in a file.  A single file is represented by a 
+ * Base class for all "node's" in a file.  A single file is represented by a
  * CompilationUnit, which may contain one or more interfaces or classes, which
  * in turn may contain one or more fields and/or one or more methods and/or
- * one or more enum types, or any other kind of member element.  This base class 
- * provides common functionality for all subtypes, including the name, modifiers, 
+ * one or more enum types, or any other kind of member element.  This base class
+ * provides common functionality for all subtypes, including the name, modifiers,
  * and line number for the node, as well as parent/child relationships.
  * <p>
  * These are used as the UserObject in each TreeNode in the tree in the main
@@ -97,6 +97,10 @@ public class TigerNode extends Asset {
     private ArrayList children;
 
     private Type type = null;
+
+    // if filled in, this should contain the full package and class name of the
+    // type of this node, e.g. java.lang.String or sidekick.java.node.TigerNode
+    private String fullyQualifiedTypeName = null;
 
     // parent node, will only be null for CUNode, all other nodes will have a
     // parent node.
@@ -183,15 +187,23 @@ public class TigerNode extends Asset {
     }
 
     public void setType(Type type) {
-        this.type = type;   
+        this.type = type;
     }
-    
+
     public String getType() {
         return type == null ? "" : type.getType();
     }
-    
+
     public String getTypeParams() {
-        return type == null ? "" : type.getTypeParams();   
+        return type == null ? "" : type.getTypeParams();
+    }
+
+    public void setFullyQualifiedTypeName(String name) {
+        fullyQualifiedTypeName = name;
+    }
+
+    public String getFullyQualifiedTypeName() {
+        return fullyQualifiedTypeName;
     }
 
     public int hashCode() {
@@ -199,7 +211,7 @@ public class TigerNode extends Asset {
     }
 
     /**
-     * @return line: modifiers name, e.g. "83: public int getLineNumber"    
+     * @return line: modifiers name, e.g. "83: public int getLineNumber"
      */
     public String toString() {
         StringBuffer sb = new StringBuffer();
@@ -208,8 +220,8 @@ public class TigerNode extends Asset {
     }
 
     /**
-     * Subclasses should override, default implementation returns true.  This 
-     * method is to check whether the given node can be added to this node as 
+     * Subclasses should override, default implementation returns true.  This
+     * method is to check whether the given node can be added to this node as
      * a child node.
      * @param node the node to add
      * @return true if it is allowed to add the given node to this node
@@ -217,13 +229,13 @@ public class TigerNode extends Asset {
     public boolean canAdd( TigerNode node ) {
         return true;
     }
-    
+
     public boolean isVisible() {
-        return isVisible;   
+        return isVisible;
     }
-    
+
     public void setVisible(boolean b) {
-        isVisible = b;   
+        isVisible = b;
     }
 
     /**
@@ -242,6 +254,9 @@ public class TigerNode extends Asset {
             child.setParent( this );
             children.add( child );
         }
+        else {
+            System.out.println("+++++ not allowed to add child: " + child.getClass().getName() + ", " + child.toString() + " to " + getClass().getName());
+        }
     }
 
     /**
@@ -257,8 +272,10 @@ public class TigerNode extends Asset {
             children = new ArrayList();
         for ( Iterator it = kids.iterator(); it.hasNext(); ) {
             Object o = it.next();
-            if ( ! ( o instanceof TigerNode ) )
+            if ( ! ( o instanceof TigerNode ) ) {
+                System.out.println("+++++ child not added, not a TigerNode: " + o.getClass().getName() + ", " + o.toString());
                 continue;
+            }
             TigerNode child = ( TigerNode ) o;
             addChild( child );
         }
@@ -291,7 +308,7 @@ public class TigerNode extends Asset {
     }
 
     /**
-     * @return the CUNode that is the top-level parent of this node.    
+     * @return the CUNode that is the top-level parent of this node.
      */
     public CUNode getCompilationUnit() {
         TigerNode parent = getParent();
@@ -313,6 +330,30 @@ public class TigerNode extends Asset {
 
     public Icon getIcon() {
         return TigerLabeler.getIcon( this );
+    }
+
+    /**
+     * Dumps the entire tree starting with this node.
+     */
+    public String dump() {
+        return dump(0);
+    }
+
+    protected String dump(int level) {
+        StringBuilder tabs = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            tabs.append("\t");
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(tabs).append( this.getClass().getName()).append(":").append(this.toString() ).append( '\n' );
+        List children = getChildren();
+        if ( children != null ) {
+            for ( Iterator it = children.iterator(); it.hasNext(); ) {
+                TigerNode child = ( TigerNode ) it.next();
+                sb.append(tabs).append(child.dump(level + 1));
+            }
+        }
+        return sb.toString();
     }
 
 }
