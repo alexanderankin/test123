5323c5394b191a95153ebd6f47745f9057bb7f28
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-04-06 11:28:13 +0000

    Added the ability to enter commands in the gdb/mi console. Also added auto-scroll for gdb/mi and program consoles.

--- plugins/GdbPlugin/trunk/gdb/output/Console.java
+++ plugins/GdbPlugin/trunk/gdb/output/Console.java
@@ -5,17 +5,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
 @SuppressWarnings("serial")
-public class Console extends JPanel {
+public class Console extends JPanel implements ActionListener {
 
-	JTextArea textarea;
+	private JTextArea textarea;
+	private JTextField userText = null;
+	private InputHandler handler;
+	
+	public interface InputHandler {
+		void handle(String line);
+	}
 	
 	public Console() {
+		this(null);
+	}
+	public Console(InputHandler handler) {
+		this.handler = handler;
 		setLayout(new BorderLayout());
 		JToolBar tb = new JToolBar();
 		tb.setFloatable(false);
@@ -26,14 +38,31 @@ public class Console extends JPanel {
 			}
 		});
 		tb.add(clear);
+		if (handler != null) {
+			tb.add(new JLabel("Command:"));
+			userText = new JTextField(30);
+			tb.add(userText);
+			JButton send = new JButton("Send");
+			send.addActionListener(this);
+			tb.add(send);
+		}
 		add(tb, BorderLayout.NORTH);
 		textarea = new JTextArea();
 		add(new JScrollPane(textarea), BorderLayout.CENTER);
 	}
 	public synchronized void append(String s) {
 		textarea.append(s);
+		// Auto-scroll
+		textarea.setCaretPosition(textarea.getDocument().getLength());
 	}
 	public void clear() {
 		textarea.setText("");
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		String line = userText.getText();
+		if (line.length() > 0) {
+			handler.handle(line);
+			userText.setText("");
+		}
 	}
 }
