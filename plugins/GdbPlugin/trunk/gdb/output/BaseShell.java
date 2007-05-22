34b4cd9ce92036eb7fc5a7a4732d4a3e707787c3
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-05-22 13:04:32 +0000

    Replaced the Program Output dockable with yet another Console shell.

---
+++ plugins/GdbPlugin/trunk/gdb/output/BaseShell.java
@@ -0,0 +1,42 @@
+package gdb.output;
+
+import gdb.core.CommandManager;
+import gdb.core.Debugger;
+
+import org.gjt.sp.jedit.jEdit;
+
+import console.Console;
+import console.ConsolePlugin;
+import console.Output;
+import console.Shell;
+
+public abstract class BaseShell extends Shell {
+
+	private Output output = null;
+	
+	public BaseShell(String arg0) {
+		super(arg0);
+		getConsole().setShell(this);	// The only way to create an Output
+		output = getConsole().getOutput();
+	}
+
+	protected Console getConsole() {
+		return ConsolePlugin.getConsole(jEdit.getActiveView());
+	}
+	protected CommandManager getCommandManager() {
+		return Debugger.getInstance().getCommandManager();
+	}
+	protected Output getOutput() {
+		return output;
+	}
+	protected void print(String s) {
+		getOutput().print(getConsole().getPlainColor(), s);
+	}
+	protected void printError(String s) {
+		getOutput().print(getConsole().getErrorColor(), s);
+	}
+	public void clear() {
+		getConsole().setShell(this);
+		getConsole().clear();
+	}
+}
