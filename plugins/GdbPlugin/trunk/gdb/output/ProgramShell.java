17177d9de316dc067fc9efa3a39abf1a759c4d66
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-05-24 21:15:09 +0000

    Enable entering input in the Program shell of the Console. Also override printPrompt() to avoid the default prompt of the Console.

--- plugins/GdbPlugin/trunk/gdb/output/ProgramShell.java
+++ plugins/GdbPlugin/trunk/gdb/output/ProgramShell.java
@@ -1,5 +1,10 @@
 package gdb.output;
 
+import gdb.core.Debugger;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import org.gjt.sp.jedit.jEdit;
 
 import console.Console;
@@ -11,7 +16,8 @@ public class ProgramShell extends BaseShell {
 	static final String PREFIX = Plugin.OPTION_PREFIX;
 	static final String MI_SHELL_INFO_MSG_PROP = PREFIX + "program_shell_info_msg";
 	public static String NAME = "Program";
-
+	private OutputStreamWriter writer = null;
+	
 	public ProgramShell() {
 		super(NAME);
 	}
@@ -20,9 +26,21 @@ public class ProgramShell extends BaseShell {
 		super(arg0);
 	}
 
+	private OutputStreamWriter getWriter() {
+		if (writer == null)
+	        writer = new OutputStreamWriter(
+	        		Debugger.getInstance().getGdbProcess().getOutputStream());
+		return writer;
+	}
+	
 	public void printInfoMessage (Output output) {
 		output.print(getConsole().getPlainColor(),
 				jEdit.getProperty(MI_SHELL_INFO_MSG_PROP));
+	}
+	
+	public void printPrompt(Console console, Output output)
+	{
+		// No prompt - this is the stdin of the debugged program
 	}
 	
 	public void append(String s) {
@@ -38,7 +56,11 @@ public class ProgramShell extends BaseShell {
 	@Override
 	public void execute(Console console, String input,
 			Output output, Output error, String command) {
-		// TO DO
+		try {
+			getWriter().append(command);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
