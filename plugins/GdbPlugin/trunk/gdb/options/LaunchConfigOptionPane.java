d798b544c94811c985e6a33c3633a34689d92563
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-05-29 07:46:06 +0000

    Bug fix: The gdb process should inherit the parent process environment

--- plugins/GdbPlugin/trunk/gdb/options/LaunchConfigOptionPane.java
+++ plugins/GdbPlugin/trunk/gdb/options/LaunchConfigOptionPane.java
@@ -31,7 +31,6 @@ import java.util.Vector;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
