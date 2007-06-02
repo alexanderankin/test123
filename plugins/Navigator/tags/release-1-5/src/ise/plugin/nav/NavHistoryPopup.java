150d60b77a4860cebda3335b2b771acf02bec25b
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-06-02 03:44:11 +0000

    Tagging for release.

---
+++ plugins/Navigator/tags/release-1-5/src/ise/plugin/nav/NavHistoryPopup.java
@@ -0,0 +1,270 @@
+/*
+ * ChooseTagListPopup.java
+ * Copyright (c) 2001, 2002 Kenrick Drew, Slava Pestov
+ *
+ * This file is part of TagsPlugin
+ *
+ * TagsPlugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * TagsPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id: ChooseTagListPopup.java,v 1.10 2004/11/07 15:52:34 orutherfurd Exp $
+ */
+
+/* This is pretty much ripped from gui/CompleteWord.java */
+
+package ise.plugin.nav;
+
+//{{{ imports
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.gui.KeyEventWorkaround;
+import org.gjt.sp.util.Log;
+//}}}
+
+class NavHistoryPopup extends JWindow
+{
+	//{{{ private declarations
+	private JList list;
+	private JLabel helpLabel;
+	private Vector positions;
+	private View view;
+	private boolean numberKeyProcessed = false;
+	private boolean openNewView;
+	private Navigator navigator = null;
+	//}}}
+
+	//{{{ ChooseTagListPopup constructor
+	public NavHistoryPopup(View view, Navigator navigator, Vector positions, boolean newView)
+	{
+		super(view);
+		this.navigator = navigator;
+		this.positions = positions;
+		this.view = view;
+		this.openNewView = newView;
+
+		// create components
+		list = new JList(positions);
+		helpLabel = new JLabel(jEdit.getProperty("Help"));
+		// setup components
+		list.addMouseListener(new MouseHandler());
+		helpLabel.setBorder(BorderFactory.createEmptyBorder(0,4,2,0));
+		// place components
+		/*
+		 * stupid scrollbar policy is an attempt to work around
+		 * bugs people have been seeing with IBM's JDK -- 7 Sep 2000
+		 * Comment from Slava
+		 */
+		JScrollPane scroller = new JScrollPane(list,
+			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		getContentPane().add(scroller, BorderLayout.CENTER);
+		getContentPane().add(helpLabel, BorderLayout.SOUTH);
+
+		GUIUtilities.requestFocus(this, list);
+
+		pack();
+		setLocation();
+		setVisible();
+
+		KeyHandler keyHandler = new KeyHandler();
+		addKeyListener(keyHandler);
+		getRootPane().addKeyListener(keyHandler);
+		list.addKeyListener(keyHandler);
+		this.view.setKeyEventInterceptor(keyHandler);
+	} //}}}
+
+	//{{{ setLocation() method
+	public void setLocation()
+	{
+		JEditTextArea textArea = view.getTextArea();
+
+		int caretLine = textArea.getCaretLine();
+		int lineIdx = textArea.getCaretPosition() - // offsets from beg of file
+		textArea.getLineStartOffset(caretLine);
+
+		Rectangle rect = view.getGraphicsConfiguration().getBounds();
+		Dimension d = getSize();
+		Point location = new Point(rect.x + (rect.width - d.width) / 2,
+				rect.y + (rect.height - d.height) / 2);
+		// make sure it fits on screen
+		Dimension screenSize = rect.getSize();
+		if(location.x + d.width > screenSize.width)
+		{
+			if(d.width >= screenSize.width)
+				/* In this intance we should actually resize the number of columns in
+				 * the tag index filename, but for now just position it so that you
+				 * can at least read the left side of the dialog
+				 */
+				location.x = rect.x;
+			else
+				location.x = rect.x + rect.width - d.width - 200;
+		}
+		if(location.y + d.height > screenSize.height)
+			location.y = screenSize.height - d.height;
+
+		setLocation(location);
+
+		textArea = null;
+		location = null;
+		d = null;
+		screenSize = null;
+	} //}}}
+
+	//{{{ dispose() method
+	public void dispose()
+	{
+		this.view.setKeyEventInterceptor(null);
+		super.dispose();
+		this.view.getTextArea().requestFocus();
+	} //}}}
+
+	//{{{ selected() method
+	private void selected()
+	{
+		navigator.jump(list.getSelectedIndex());
+		dispose();
+	} //}}}
+
+	//{{{ KeyHandler class
+	class KeyHandler extends KeyAdapter
+	{
+		//{{{ keyTyped() method
+		public void keyTyped(KeyEvent evt)
+		{
+			evt = KeyEventWorkaround.processKeyEvent(evt);
+			if (evt == null)
+				return;
+
+			switch (evt.getKeyChar())
+			{
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					if (numberKeyProcessed) // Since many components have this handler
+						return;
+
+					/* There may actually be more than 9 items in the list, but since
+					 * the user would have to scroll to see them either with the mouse
+					 * or with the arrow keys, then they can select the item they want
+					 * with those means.
+					 */
+					int selected = Character.getNumericValue(evt.getKeyChar()) - 1;
+					if (selected >= 0 &&
+						selected < list.getModel().getSize())
+					{
+						list.setSelectedIndex(selected);
+						selected();
+						numberKeyProcessed = true;
+					}
+					evt.consume();
+			}
+
+			evt = null;
+		} //}}}
+
+		//{{{ keyPressed() method
+		public void keyPressed(KeyEvent evt)
+		{
+			evt = KeyEventWorkaround.processKeyEvent(evt);
+			if (evt == null)
+				return;
+			//{{{ evt.getKeyCode() switch
+			switch(evt.getKeyCode())
+			{
+				case KeyEvent.VK_TAB:
+				case KeyEvent.VK_ENTER:
+					selected();
+					evt.consume();
+					break;
+				case KeyEvent.VK_ESCAPE:
+					dispose();
+					evt.consume();
+					break;
+				case KeyEvent.VK_UP:
+					int selected = list.getSelectedIndex();
+					if (selected == 0)
+						selected = list.getModel().getSize() - 1;
+					else if (getFocusOwner() == list)
+						return; // Let JList handle the event
+					else
+						selected = selected - 1;
+
+					list.setSelectedIndex(selected);
+					list.ensureIndexIsVisible(selected);
+
+					evt.consume();
+					break;
+				case KeyEvent.VK_DOWN:
+					selected = list.getSelectedIndex();
+					if (selected == list.getModel().getSize() - 1)
+						selected = 0;
+					else if (getFocusOwner() == list)
+						return; // Let JList handle the event
+					else
+						selected = selected + 1;
+
+					list.setSelectedIndex(selected);
+					list.ensureIndexIsVisible(selected);
+
+					evt.consume();
+					break;
+				case KeyEvent.VK_SPACE:
+				case KeyEvent.VK_1:
+				case KeyEvent.VK_2:
+				case KeyEvent.VK_3:
+				case KeyEvent.VK_4:
+				case KeyEvent.VK_5:
+				case KeyEvent.VK_6:
+				case KeyEvent.VK_7:
+				case KeyEvent.VK_8:
+				case KeyEvent.VK_9:
+					evt.consume();  /* so that we don't automatically dismiss */
+					break;
+
+				case KeyEvent.VK_PAGE_UP:
+				case KeyEvent.VK_PAGE_DOWN:
+					break;
+
+				default:
+					dispose();
+					evt.consume();
+					break;
+			} //}}}
+			evt = null;
+		} //}}}
+	} //}}}
+
+	//{{{ MouseHandler
+	class MouseHandler extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent evt)
+		{
+			selected();
+		}
+	} //}}}
+}
+
+// :collapseFolds=1:noTabs=false:lineSeparator=\r\n:tabSize=4:indentSize=4:deepIndent=false:folding=explicit:
---
+++ plugins/Navigator/tags/release-1-5/src/ise/plugin/nav/NavigatorPlugin.java
@@ -0,0 +1,309 @@
+// $Id$
+package ise.plugin.nav;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import java.util.Iterator;
+
+import javax.swing.JComponent;
+
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.EBMessage;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.EBPlugin;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.msg.CaretChanging;
+import org.gjt.sp.jedit.msg.EditPaneUpdate;
+import org.gjt.sp.jedit.msg.TextAreaUpdate;
+import org.gjt.sp.jedit.msg.ViewUpdate;
+
+
+/**
+ * NavigatorPlugin, mostly static methods, allows one Navigator per View.
+ * 
+ * @author Dale Anson, danson@germane-software.com
+ * @version $Revision$
+ * @since Oct 25, 2003
+ */
+public class NavigatorPlugin extends EBPlugin
+{
+	
+	public final static String NAME = "Navigator";
+	
+	
+	/** EditPane / Navigator map */
+	private final static HashMap<EditPane, Navigator> map = new HashMap<EditPane, Navigator>();
+
+	public void start()
+	{
+		View[] views = jEdit.getViews();
+		for (int i = 0; i < views.length; ++i)
+		{
+			createNavigator(views[i]);
+		}
+		setToolBars();
+	}
+
+	public static void showButtons()
+	{
+		jEdit.setBooleanProperty("navigator.showOnToolbar", true);
+		setToolBars();
+	}
+
+	public static void hideButtons()
+	{
+		jEdit.setBooleanProperty("navigator.showOnToolbar", false);
+		clearToolBars();
+	}
+
+	public static void revalidateViews()
+	{
+		View views[] = jEdit.getViews();
+		for (int i = 0; i < views.length; ++i)
+		{
+			if (views[i] != null)
+			{
+				views[i].getRootPane().revalidate();
+			}
+		}
+	}
+
+	public static void setToolBars()
+	{
+
+		if (jEdit.getBooleanProperty("navigator.showOnToolbar"))
+		{
+			String toolBarActions = jEdit.getProperty("view.toolbar");
+			StringTokenizer st = new StringTokenizer(toolBarActions);
+			boolean found = false;
+			while (st.hasMoreTokens())
+			{
+				String actionName = st.nextToken();
+				if (actionName.equals("navigator.back")
+					|| actionName.equals("navigator.forward"))
+				{
+					found = true;
+					break;
+				}
+			}
+
+			if (!found)
+			{
+				toolBarActions = "navigator.back navigator.forward - "
+					+ toolBarActions;
+				jEdit.setProperty("view.toolbar", toolBarActions);
+			}
+		}
+		revalidateViews();
+	}
+
+	public void stop()
+	{
+		map.clear();
+		clearToolBars();
+	}
+
+	public static void clearToolBars()
+	{
+		if (jEdit.getBooleanProperty("navigator.showOnToolbar"))
+		{
+			String toolBarActions = jEdit.getProperty("view.toolbar");
+			StringTokenizer st = new StringTokenizer(toolBarActions);
+			LinkedList<String> ll = new LinkedList<String>();
+			boolean found = false;
+			while (st.hasMoreTokens())
+			{
+				String actionName = st.nextToken();
+				if (actionName.equals("navigator.back")
+					|| actionName.equals("navigator.forward"))
+				{
+					found = true;
+				}
+				else
+					ll.add(actionName);
+			}
+
+			if (found)
+			{
+				StringBuffer sb = new StringBuffer();
+				Iterator itr = ll.iterator();
+				while (itr.hasNext())
+				{
+					sb.append(itr.next() + " ");
+				}
+				jEdit.setProperty("view.toolbar", sb.toString());
+			}
+		}
+		revalidateViews();
+
+	}
+
+	/**
+	 * Adds a Navigator. Navigators are tracked by view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 * @param navigator
+	 *                The Navigator
+	 */
+	public static void addNavigator(View view, Navigator navigator)
+	{
+		if (view == null)
+		{
+			return;
+		}
+		EditPane pane = view.getEditPane();
+		if (pane == null) return;
+		if (map.containsKey(pane))
+			return;
+		map.put(pane, navigator);
+	}
+
+	public static JComponent getToolBar(View view)
+	{
+		Navigator nav = getNavigator(view);
+		NavToolBar toolBar = new NavToolBar(nav);
+		return toolBar;
+	}
+
+	public static void removeNavigator(View view)
+	{
+		if (view == null)
+		{
+			return;
+		}
+		for (EditPane pane : view.getEditPanes()) { 
+			if (map.containsKey(pane))
+				map.remove(pane);
+		}
+	}
+
+	
+	public static Navigator getNavigator(View view)
+	{
+		EditPane pane = view.getEditPane();
+		return  (Navigator) map.get(pane);
+	}
+
+	public static Navigator createNavigator(View view)
+	{
+		Navigator navigator = getNavigator(view);
+		if (navigator == null)
+		{
+			navigator = new Navigator(view);
+			addNavigator(view, navigator);
+		}
+		return navigator;
+	}
+
+	/**
+	 * Wrapper for the 'backList' method of the Navigator for the given view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void backList(View view)
+	{
+		Navigator navigator = getNavigator(view);
+		if (navigator != null)
+		{
+			navigator.backList();
+		}
+	}
+
+	/**
+	 * Wrapper for the 'goBack' method of the Navigator for the given view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void goBack(View view)
+	{
+		Navigator navigator = getNavigator(view);
+		if (navigator != null)
+		{
+			navigator.goBack();
+		}
+	}
+
+	/**
+	 * Wrapper for the 'forwardList' method of the Navigator for the given
+	 * view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void forwardList(View view)
+	{
+		
+		Navigator navigator = getNavigator(view);
+		if (navigator != null)
+		{
+			navigator.forwardList();
+		}
+	}
+
+	/**
+	 * Wrapper for the 'goForward' method of the Navigator for the given
+	 * view.
+	 * 
+	 * @param view
+	 *                The view for the Navigator
+	 */
+	public static void goForward(View view)
+	{
+		
+		Navigator navigator = getNavigator(view);
+		if (navigator != null)
+		{
+			navigator.goForward();
+		}
+	}
+
+	
+	public void handleMessage(EBMessage message)
+	{
+		System.out.println(message.toString());
+		/* When we create a new View, create a new navigator for it */
+		if (message instanceof ViewUpdate)
+		{
+			ViewUpdate vu = (ViewUpdate) message;
+			View v = vu.getView();
+			Object what = vu.getWhat();
+			if (what == vu.CREATED || what == vu.EDIT_PANE_CHANGED) 
+				createNavigator(v);
+			else if (what.equals(ViewUpdate.CLOSED))
+				removeNavigator(v);
+		}
+
+		/* If the editpane changes its current buffer, we want to know
+		     just before it happens.  */
+		
+		
+		else if (message instanceof CaretChanging) 
+		{
+			CaretChanging cc = (CaretChanging) message;
+			View v = cc.getTextArea().getView();
+			Navigator n = getNavigator(v);
+			if (n != null) n.update();
+		}
+		else if (message instanceof EditPaneUpdate) 
+		{
+			EditPaneUpdate epu = (EditPaneUpdate) message;
+			if (epu.getWhat() == EditPaneUpdate.BUFFER_CHANGING) 
+			{	
+				View v = epu.getEditPane().getView();
+				Navigator n = getNavigator(v);
+				if (n != null) 
+				{
+					n.update();
+				}
+			}
+			else if (epu.getWhat() == epu.DESTROYED) {
+				map.remove(epu.getEditPane());
+			}
+		}
+	}
+}
