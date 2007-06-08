d530fd3ab645a33809f10ae6919d973ac44da70b
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-06-08 21:04:04 +0000

    added LSys Music Generation files.

---
+++ plugins/JFuguePlugin/trunk/src/java/com/townsfolkdesigns/jfugue/gen/Composition.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.townsfolkdesigns.jfugue.gen;
+
+import org.jfugue.Pattern;
+
+/**
+ * @author elberry
+ * 
+ */
+
+public interface Composition {
+	public String getTitle();
+
+	public Pattern getPattern();
+}
