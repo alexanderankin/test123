5746b8e2f0d21fff2d4351fdd6167c7b494cf1af
Dale <danson@grafidog.com>
2007-06-20 16:41:35 +0000

    added ability to reuse previous comments

--- plugins/SVNPlugin/src/ise/plugin/svn/gui/CommitDialog.java
+++ plugins/SVNPlugin/src/ise/plugin/svn/gui/CommitDialog.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.border.EmptyBorder;
 import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.jEdit;
@@ -38,6 +39,8 @@ public class CommitDialog extends JDialog {
 
     private CommitData commitData = null;
 
+    private static Stack<String> previousComments = null;
+
     public CommitDialog( View view, List<VPTNode> nodes ) {
         super( ( JFrame ) view, "Commit", true );
         if ( nodes == null ) {
@@ -50,6 +53,19 @@ public class CommitDialog extends JDialog {
 
     /** Initialises the option pane. */
     protected void _init() {
+
+        // load previous comments
+        if ( previousComments == null ) {
+            previousComments = new Stack<String>();
+        }
+
+        for ( int i = 0; i < 10; i++ ) {
+            String comment = jEdit.getProperty( "ise.plugin.svn.comment." + i );
+            if ( comment == null ) {
+                break;
+            }
+            previousComments.push( comment );
+        }
 
         commitData = new CommitData();
 
@@ -66,35 +82,46 @@ public class CommitDialog extends JDialog {
                 if ( node.isDirectory() || node.isRoot() ) {
                     recursive = true;
                 }
-                paths.add(node.getNodePath());
+                paths.add( node.getNodePath() );
             }
         }
-        commitData.setPaths(paths);
-        commitData.setRecursive(recursive);
+        commitData.setPaths( paths );
+        commitData.setRecursive( recursive );
 
-        JLabel file_label = new JLabel("Committing these files:");
-        final JPanel file_panel = new JPanel(new GridLayout(0, 1, 2, 3));
-        file_panel.setBackground(Color.WHITE);
-        file_panel.setBorder(new EmptyBorder(3, 3, 3, 3));
-        for(String path : paths) {
-            JCheckBox cb = new JCheckBox(path);
-            cb.setSelected(true);
-            cb.setBackground(Color.WHITE);
-            file_panel.add(cb);
+        JLabel file_label = new JLabel( "Committing these files:" );
+        final JPanel file_panel = new JPanel( new GridLayout( 0, 1, 2, 3 ) );
+        file_panel.setBackground( Color.WHITE );
+        file_panel.setBorder( new EmptyBorder( 3, 3, 3, 3 ) );
+        for ( String path : paths ) {
+            JCheckBox cb = new JCheckBox( path );
+            cb.setSelected( true );
+            cb.setBackground( Color.WHITE );
+            file_panel.add( cb );
         }
 
-        final JCheckBox recursive_cb = new JCheckBox("Recursively commit?");
-        recursive_cb.setSelected(recursive);
-        recursive_cb.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent ae) {
-                    commitData.setRecursive(recursive_cb.isSelected());
+        final JCheckBox recursive_cb = new JCheckBox( "Recursively commit?" );
+        recursive_cb.setSelected( recursive );
+        recursive_cb.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        commitData.setRecursive( recursive_cb.isSelected() );
+                    }
                 }
-        });
+                                      );
 
         JLabel label = new JLabel( "Enter comment for this commit:" );
         comment = new JTextArea( 10, 50 );
         comment.setLineWrap( true );
         comment.setWrapStyleWord( true );
+
+        // list for previous comments
+        final JList commentList = new JList( previousComments );
+        commentList.setSelectionMode( ListSelectionModel.SINGLE_INTERVAL_SELECTION );
+        commentList.addListSelectionListener( new ListSelectionListener() {
+                    public void valueChanged( ListSelectionEvent e ) {
+                        comment.setText( commentList.getSelectedValue().toString() );
+                    }
+                }
+                                            );
 
         // buttons
         KappaLayout kl = new KappaLayout();
@@ -110,26 +137,30 @@ public class CommitDialog extends JDialog {
                         // get the paths
                         List<String> paths = new ArrayList<String>();
                         Component[] files = file_panel.getComponents();
-                        for (Component file : files) {
-                            JCheckBox cb = (JCheckBox)file;
-                            if (cb.isSelected()) {
-                                paths.add(cb.getText());
+                        for ( Component file : files ) {
+                            JCheckBox cb = ( JCheckBox ) file;
+                            if ( cb.isSelected() ) {
+                                paths.add( cb.getText() );
                             }
                         }
-                        if (paths.size() == 0) {
+                        if ( paths.size() == 0 ) {
                             // nothing to commit, bail out
                             commitData = null;
                         }
                         else {
-                            commitData.setPaths(paths);
+                            commitData.setPaths( paths );
                             String msg = comment.getText();
-                            if (msg == null || msg.length() == 0) {
+                            if ( msg == null || msg.length() == 0 ) {
                                 msg = "no comment";
                             }
-                            commitData.setCommitMessage(msg);
+                            else {
+                                previousComments.push( msg );
+                            }
+                            commitData.setCommitMessage( msg );
                         }
                         CommitDialog.this.setVisible( false );
                         CommitDialog.this.dispose();
+                        CommitDialog.this._save();
                     }
                 }
                                 );
@@ -146,7 +177,7 @@ public class CommitDialog extends JDialog {
         // add the components to the option panel
         panel.add( "0, 0, 1, 1, W,  , 3", file_label );
         panel.add( "0, 1, 1, 1, W, wh, 3", new JScrollPane( file_panel ) );
-        panel.add( "1, 1, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 120, true));
+        panel.add( "1, 1, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 120, true ) );
         panel.add( "0, 2, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 6, true ) );
 
         panel.add( "0, 3, 1, 1, W,  , 3", recursive_cb );
@@ -155,12 +186,28 @@ public class CommitDialog extends JDialog {
         panel.add( "0, 5, 1, 1, W,  , 3", label );
         panel.add( "0, 6, 1, 1, W, wh, 3", new JScrollPane( comment ) );
 
-        panel.add( "0, 7, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 10, true ) );
-        panel.add( "0, 8, 1, 1, E,  , 0", btn_panel );
+        if ( previousComments != null && previousComments.size() > 0 ) {
+            panel.add( "0, 7, 1, 1, W,  , 3", new JLabel( "Select a previous comment:" ) );
+            panel.add( "0, 8, 1, 1, W, w, 3", commentList );
+        }
+
+        panel.add( "0, 9, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 10, true ) );
+        panel.add( "0, 10, 1, 1, E,  , 0", btn_panel );
 
         setContentPane( panel );
         pack();
 
+    }
+
+    protected void _save() {
+        if ( previousComments != null ) {
+            for ( int i = Math.min( previousComments.size(), 10 ); i > 0 ; i-- ) {
+                String comment = previousComments.pop();
+                if ( comment != null && comment.length() > 0 ) {
+                    jEdit.setProperty( "ise.plugin.svn.comment." + i, comment );
+                }
+            }
+        }
     }
 
     public CommitData getCommitData() {
