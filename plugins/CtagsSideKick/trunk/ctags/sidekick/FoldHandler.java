7d05010e490f2ed86d8e4b1256156cce452bc4df
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-06-25 13:11:40 +0000

    Added a CtagsSideKick fold handler

---
+++ plugins/CtagsSideKick/trunk/ctags/sidekick/FoldHandler.java
@@ -0,0 +1,44 @@
+package ctags.sidekick;
+
+import javax.swing.text.Segment;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+
+import org.gjt.sp.jedit.buffer.JEditBuffer;
+
+import sidekick.IAsset;
+import sidekick.SideKickParsedData;
+import sidekick.SideKickPlugin;
+
+public class FoldHandler extends org.gjt.sp.jedit.buffer.FoldHandler {
+
+	protected FoldHandler() {
+		super("CtagsSideKick");
+	}
+
+	@Override
+	public int getFoldLevel(JEditBuffer buffer, int lineIndex, Segment seg) {
+		if(lineIndex == 0 || lineIndex == buffer.getLineCount() - 1)
+			return 0;
+
+		SideKickParsedData data = (SideKickParsedData)buffer.getProperty(
+			SideKickPlugin.PARSED_DATA_PROPERTY);
+		if(data == null)
+			return 0;
+		int offset = buffer.getLineStartOffset(lineIndex);
+		TreePath path = data.getTreePathForPosition(offset);
+		if(path == null)
+			return 0;
+		DefaultMutableTreeNode node =
+			(DefaultMutableTreeNode)path.getLastPathComponent();
+		Object obj = node.getUserObject();
+		if (obj instanceof Tag) {
+			Tag tag = (Tag)(node.getUserObject());
+			if (lineIndex == tag.getLine())
+				return 0;
+			return 1;
+		}
+		return 0;
+	}
+
+}
