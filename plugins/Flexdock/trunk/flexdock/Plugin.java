2679df32c172702985e3696094cf302bea98f2b4
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-06-30 22:41:06 +0000

    Many small improvements

--- plugins/Flexdock/trunk/flexdock/Plugin.java
+++ plugins/Flexdock/trunk/flexdock/Plugin.java
@@ -18,64 +18,73 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package flexdock;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 
 import org.flexdock.docking.DockingConstants;
 import org.flexdock.docking.DockingManager;
+import org.flexdock.docking.DockingPort;
 import org.flexdock.docking.defaults.DefaultDockingPort;
 import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.View;
-import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.gui.DockableLayout;
 import org.gjt.sp.jedit.gui.DockableWindowManager;
 import org.gjt.sp.jedit.gui.PanelWindowContainer;
 
 
 public class Plugin extends EditPlugin {
 	public static final String NAME = "Flexdock";
-	private static View view;
+	static DefaultDockingPort mainPort = null;
+	private static Container mainContainer;
 	
 	public static void doStart(View view) {
-		Plugin.view = view;
-		Container c = view.getContentPane();
-		JPanel dockPanel = new JPanel(new BorderLayout());
+		mainContainer = view.getContentPane();
  		DockableWindowManager dockMan = view.getDockableWindowManager();
  		DockingManager.setSingleTabsAllowed(true);
-		DefaultDockingPort topPort = createDockingPort(100, dockMan.getTopDockingArea().getDimension());
-		dockPanel.add(topPort, BorderLayout.NORTH);
-		/*DefaultDockingPort bottomPort = createDockingPort(100, dockMan.getBottomDockingArea().getDimension());
-		dockPanel.add(bottomPort, BorderLayout.SOUTH);
-		*/
-		DefaultDockingPort leftPort = createDockingPort(dockMan.getLeftDockingArea().getDimension(), 100);
-		dockPanel.add(leftPort, BorderLayout.WEST);
-		DefaultDockingPort rightPort = createDockingPort(dockMan.getRightDockingArea().getDimension(), 100);
-		dockPanel.add(rightPort, BorderLayout.EAST);
-		dockPanel.add(BorderLayout.CENTER, c);
-		view.setContentPane(dockPanel);
-		convertDockables(topPort, dockMan.getTopDockingArea());
-		//convertDockables(bottomPort, dockMan.getBottomDockingArea());
-		convertDockables(leftPort, dockMan.getLeftDockingArea());
-		convertDockables(rightPort, dockMan.getRightDockingArea());
-		dockMan.close();
+		mainPort = createDockingPort(100, 100);
+		mainPort.setSingleTabAllowed(false);
+		DockingManager.registerDockable(mainContainer, "Main");
+		DockingManager.dock(mainContainer, (DockingPort)mainPort);
+		boolean alternateLayout = ((DockableLayout)dockMan.getLayout()).setAlternateLayout();
+		if (alternateLayout) {
+			convertDockables(view, DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
+			convertDockables(view, DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
+			convertDockables(view, DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
+			convertDockables(view, DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
+		} else {
+			convertDockables(view, DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
+			convertDockables(view, DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
+			convertDockables(view, DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
+			convertDockables(view, DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
+		}
+		view.setContentPane(mainPort);
 	}
-	private static void convertDockables(DefaultDockingPort port,
+	private static void convertDockables(View view, String region,
 			PanelWindowContainer dockingArea) {
  		DockableWindowManager dockMan = view.getDockableWindowManager();
-		String [] dockables = dockingArea.getDockables(); 
+		String [] dockables = dockingArea.getDockables();
+		JComponent first = null;
 		for (int i = 0; i < dockables.length; i++) {
 			String name = dockables[i];
 			String title = dockMan.getDockableTitle(name);
-			JComponent dockable = createDockableComponent(name, title);
-			connectToDockingPort(dockable, port);
+			JComponent dockable = createDockableComponent(view, name, title);
+			if (i == 0) {
+				first = dockable;
+				DockingManager.dock(first, mainContainer, region);
+				float split;
+				if (region.equals(DockingConstants.WEST_REGION) ||
+						region.equals(DockingConstants.NORTH_REGION))
+					split = 0.2f;
+				else
+					split = 0.75f;
+				DockingManager.setSplitProportion(first, split);
+			}
+			else
+				DockingManager.dock(dockable, first);
 		}
-	}
-	private static void connectToDockingPort(JComponent component, DefaultDockingPort port) {
-		port.dock(component, DockingConstants.CENTER_REGION);
 	}
 	private static DefaultDockingPort createDockingPort(int w, int h) {
 		DefaultDockingPort port = new DefaultDockingPort();
@@ -84,14 +93,18 @@ public class Plugin extends EditPlugin {
 		return port;
 	}
 
-	private static JComponent createDockableComponent(String name, String title) {
-		JComponent c = view.getDockableWindowManager().getDockable(name);
+	private static JComponent createDockableComponent(View view,
+			String name, String title) {
+		JPanel p = new JPanel(new BorderLayout());
+		DockableWindowManager dockMan = view.getDockableWindowManager();
+		JComponent c = dockMan.getDockable(name);
 		if (c == null) {
-			view.getDockableWindowManager().showDockableWindow(name);
-			c = view.getDockableWindowManager().getDockable(name);
+			dockMan.showDockableWindow(name);
+			c = dockMan.getDockable(name);
 		}
-		view.getDockableWindowManager().hideDockableWindow(name);
-		DockingManager.registerDockable(c, title);
-		return c;
+		p.add(c, BorderLayout.CENTER);
+		dockMan.hideDockableWindow(name);
+		DockingManager.registerDockable(p, title);
+		return p;
 	}
 }
