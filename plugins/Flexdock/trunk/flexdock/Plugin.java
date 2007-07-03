0637f8c60ee368caf83db971f8484dab897752e6
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-07-03 14:33:58 +0000

    Somewhat closer to persistence, not there yet...

--- plugins/Flexdock/trunk/flexdock/Plugin.java
+++ plugins/Flexdock/trunk/flexdock/Plugin.java
@@ -18,8 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package flexdock;
 import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.io.File;
 import java.io.IOException;
 
@@ -29,11 +28,11 @@ import org.flexdock.docking.Dockable;
 import org.flexdock.docking.DockableFactory;
 import org.flexdock.docking.DockingConstants;
 import org.flexdock.docking.DockingManager;
-import org.flexdock.docking.DockingPort;
-import org.flexdock.docking.defaults.DefaultDockingPort;
 import org.flexdock.docking.defaults.DockableComponentWrapper;
 import org.flexdock.docking.drag.effects.EffectsManager;
 import org.flexdock.docking.drag.preview.GhostPreview;
+import org.flexdock.docking.props.DockingPortPropertySet;
+import org.flexdock.docking.props.PropertyManager;
 import org.flexdock.docking.state.PersistenceException;
 import org.flexdock.perspective.LayoutSequence;
 import org.flexdock.perspective.Perspective;
@@ -42,6 +41,7 @@ import org.flexdock.perspective.PerspectiveManager;
 import org.flexdock.perspective.persist.FilePersistenceHandler;
 import org.flexdock.perspective.persist.PersistenceHandler;
 import org.flexdock.perspective.persist.xml.XMLPersister;
+import org.flexdock.view.Viewport;
 import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.gui.DockableLayout;
@@ -52,40 +52,49 @@ import org.gjt.sp.jedit.gui.PanelWindowContainer;
 public class Plugin extends EditPlugin {
 	public static final String NAME = "Flexdock";
 	private static final String PERSPECTIVE_FILE = "jedit.xml";
-	static DefaultDockingPort mainPort = null;
-	private static Container mainContainer;
 	private static final String MAIN_VIEW = "Main";
-	public static final String JEDIT_PERSPECTIVE = "jedit";
-	private static final String MAIN_PORT = "MainPort";
-	public static LayoutSequence seq = null;
+	private static final String MAIN_PERSPECTIVE = "jEdit";
+	private static org.flexdock.view.View mainView;
+	private static Viewport viewport;
 	
-	public static void doStart(View view) {
+	public static void doStart(final View view) {
 		configureDocking(view);
-		Perspective jedit = new DemoPerspectiveFactory().getPerspective("jedit");
-		PerspectiveManager.getInstance().setCurrentPerspective(jedit.getPersistentId());
-		PerspectiveManager.getInstance().setDefaultPerspective(jedit.getPersistentId());
-		mainPort = createDockingPort(100, 100);
-		mainPort.setSingleTabAllowed(false);
-		mainContainer = view.getContentPane();
- 		DockableWindowManager dockMan = view.getDockableWindowManager();
- 		DockingManager.setSingleTabsAllowed(true);
-		DockingManager.registerDockable(mainContainer, MAIN_VIEW);
-		DockingManager.dock(mainContainer, (DockingPort)mainPort);
-		seq.add(MAIN_VIEW);
-		boolean alternateLayout = ((DockableLayout)dockMan.getLayout()).isAlternateLayout();
-		if (! alternateLayout) {
-			convertDockables(view, DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
-			convertDockables(view, DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
-			convertDockables(view, DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
-			convertDockables(view, DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
-		} else {
-			convertDockables(view, DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
-			convertDockables(view, DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
-			convertDockables(view, DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
-			convertDockables(view, DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
-		}
-		view.setContentPane(mainPort);
-		DockingManager.setMainDockingPort(mainPort, MAIN_PORT);
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				viewport = new Viewport();
+				mainView = createMainView(view);
+				viewport.dock(mainView);
+				viewport.setSingleTabAllowed(false);
+				view.setContentPane(viewport);
+				DockingManager.restoreLayout();
+				PerspectiveManager.getInstance().loadPerspective(MAIN_PERSPECTIVE);
+			}
+		});
+
+	}
+	private static org.flexdock.view.View createMainView(View view) {
+		org.flexdock.view.View mainView =
+			new org.flexdock.view.View(MAIN_VIEW, null, null);
+
+		mainView.setTerritoryBlocked(DockingConstants.CENTER_REGION, true);
+		mainView.setTitlebar(null);
+		mainView.setContentPane(view.getContentPane());
+		return mainView;
+	}
+
+	private static void configureDocking(View view) {
+		// setup the DockingManager to work with our application
+		DockingManager.setDockableFactory(new ViewFactory(view));
+		DockingManager.setFloatingEnabled(true);
+        EffectsManager.setPreview(new GhostPreview());
+        DockingManager.setSingleTabsAllowed(true);
+		// configure the perspective manager
+		PerspectiveManager.setFactory(new DemoPerspectiveFactory(view));
+		PerspectiveManager.setRestoreFloatingOnLoad(true);
+		// load any previously persisted layouts
+		PersistenceHandler persister = new FilePersistenceHandler(new File(FilePersistenceHandler.DEFAULT_PERSPECTIVE_DIR, PERSPECTIVE_FILE), XMLPersister.newDefaultInstance());
+		PerspectiveManager.setPersistenceHandler(persister);
 		try {
 			DockingManager.loadLayoutModel();
 		} catch(IOException ex) {
@@ -93,71 +102,48 @@ public class Plugin extends EditPlugin {
 		} catch (PersistenceException ex) {
             ex.printStackTrace();
         }
-	}
-	private static void configureDocking(View view) {
-		// setup the DockingManager to work with our application
-		DockingManager.setDockableFactory(new ViewFactory(view));
-		DockingManager.setFloatingEnabled(true);
-        EffectsManager.setPreview(new GhostPreview());
-        
-		// configure the perspective manager
-		PerspectiveManager.setFactory(new DemoPerspectiveFactory());
-		PerspectiveManager.setRestoreFloatingOnLoad(true);
-
-		File xml = new File(FilePersistenceHandler.DEFAULT_PERSPECTIVE_DIR, PERSPECTIVE_FILE);
-		PersistenceHandler persister = new FilePersistenceHandler(xml, XMLPersister.newDefaultInstance());
-		PerspectiveManager.setPersistenceHandler(persister);
 		// remember to store on shutdown
 		DockingManager.setAutoPersist(true);
 	}
-	private static void convertDockables(View view, String region,
-			PanelWindowContainer dockingArea) {
-		String [] dockables = dockingArea.getDockables();
-		Dockable first = null;
-		for (int i = 0; i < dockables.length; i++) {
-			String name = dockables[i];
-			Dockable dockable = createDockableComponent(view, name);
-			if (i == 0) {
-				first = dockable;
-				DockingManager.dock(dockable, mainPort, region);
-				float split;
-				if (region.equals(DockingConstants.WEST_REGION) ||
-						region.equals(DockingConstants.NORTH_REGION))
-					split = 0.2f;
-				else
-					split = 0.75f;
-				DockingManager.setSplitProportion(first, split);
-				seq.add(name, MAIN_VIEW, region, split);
+	private static class DemoPerspectiveFactory implements PerspectiveFactory {
+		View view;
+		LayoutSequence sequence;
+		public DemoPerspectiveFactory(View view) {
+			this.view = view;
+		}
+		public Perspective getPerspective(String persistentId) {
+			if(! MAIN_PERSPECTIVE.equals(persistentId))
+				return null;
+			Perspective perspective = new Perspective(MAIN_PERSPECTIVE, "jEdit");
+			sequence = perspective.getInitialSequence(true);
+			DockableWindowManager dockMan = view.getDockableWindowManager();
+			boolean alternateLayout = ((DockableLayout)dockMan.getLayout()).isAlternateLayout();
+			sequence.add(MAIN_VIEW);
+			if (! alternateLayout) {
+				addDockables(DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
+				addDockables(DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
+				addDockables(DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
+				addDockables(DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
+			} else {
+				addDockables(DockingConstants.WEST_REGION, dockMan.getLeftDockingArea());
+				addDockables(DockingConstants.EAST_REGION, dockMan.getRightDockingArea());
+				addDockables(DockingConstants.NORTH_REGION, dockMan.getTopDockingArea());
+				addDockables(DockingConstants.SOUTH_REGION, dockMan.getBottomDockingArea());
 			}
-			else {
-				DockingManager.dock(dockable, first);
-				seq.add(dockable, first);
-			}
-
+			return perspective;
+		}
+		private void addDockables(String region, PanelWindowContainer dockingArea) {
+			String [] dockables = dockingArea.getDockables();
+			if (dockables.length == 0)
+				return;
+			float split = (region.equals(DockingConstants.WEST_REGION) ||
+				region.equals(DockingConstants.NORTH_REGION)) ? 0.2f : 0.75f;
+			sequence.add(dockables[0], MAIN_VIEW, region, split);
+			for (int i = 1; i < dockables.length; i++)
+				sequence.add(dockables[i], dockables[0]);
 		}
 	}
-	private static DefaultDockingPort createDockingPort(int w, int h) {
-		DefaultDockingPort port = new DefaultDockingPort();
-		port.setSingleTabAllowed(true);
-		port.setPreferredSize(new Dimension(w, h));
-		return port;
-	}
-
-	private static Dockable createDockableComponent(View view,
-			String name) {
-		DockableWindowManager dockMan = view.getDockableWindowManager();
-		String title = dockMan.getDockableTitle(name);
-		JComponent c = dockMan.getDockable(name);
-		if (c == null) {
-			dockMan.showDockableWindow(name);
-			c = dockMan.getDockable(name);
-		}
-		dockMan.hideDockableWindow(name);
-		Dockable p = DockableComponentWrapper.create(c, name, title);
-		DockingManager.registerDockable(p);
-		return p;
-	}
-
+	
 	private static class ViewFactory extends DockableFactory.Stub {
 		
 		View view;
@@ -166,21 +152,32 @@ public class Plugin extends EditPlugin {
 			this.view = view;
 		}
 		public Component getDockableComponent(String dockableId) {
-			System.err.println("ViewFactory.getDockable: " + dockableId);
 			if(MAIN_VIEW.equals(dockableId))
-				return view.getContentPane();
-			return view.getDockableWindowManager().getDockable(dockableId);
+				return createMainView();
+			return createView(dockableId);
 		}
-	}
-	private static class DemoPerspectiveFactory implements PerspectiveFactory {
+		private Component createView(String id) {
+			DockableWindowManager dockMan = view.getDockableWindowManager();
+			JComponent c = getJEditDockable(id, dockMan);
+			String title = dockMan.getDockableTitle(id);
+			Dockable d = DockableComponentWrapper.create(c, id, title);
+			DockingManager.registerDockable(d);
+			return c;
+		}
 		
-		public Perspective getPerspective(String persistentId) {
-			if (! persistentId.equals("jedit"))
-				return null;
-			Perspective perspective = new Perspective(JEDIT_PERSPECTIVE, "jEdit");
-			seq = perspective.getInitialSequence(true);
-			return perspective;
+		private org.flexdock.view.View createMainView() {
+			return mainView;
 		}
+		private JComponent getJEditDockable(String id, DockableWindowManager dockMan) {
+			JComponent c = dockMan.getDockable(id);
+			if (c == null) {
+				dockMan.showDockableWindow(id);
+				c = dockMan.getDockable(id);
+			}
+			dockMan.hideDockableWindow(id);
+			return c;
+		}
+		
 	}
 	
 }
