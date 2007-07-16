971e2e62656a0654cbe4624f750539b1d9fc8d32
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-07-16 19:48:12 +0000

    Checkout now works correctly from plugin menu and allows user to import the checked out files directly into ProjectViewer as a new project.

--- plugins/SVNPlugin/src/ise/plugin/svn/action/CheckoutAction.java
+++ plugins/SVNPlugin/src/ise/plugin/svn/action/CheckoutAction.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 import java.util.Stack;
 import javax.swing.*;
 import projectviewer.ProjectManager;
@@ -25,12 +27,13 @@ import ise.plugin.svn.library.swingworker.SwingWorker;
 import org.gjt.sp.jedit.View;
 
 
-public class CheckoutAction implements ActionListener {
+public class CheckoutAction implements ActionListener, PropertyChangeListener {
 
     private View view = null;
     private String url = null;
     private String username = null;
     private String password = null;
+    private CheckoutData cd = null;
 
     /**
      * @param view the View in which to display results
@@ -57,12 +60,19 @@ public class CheckoutAction implements ActionListener {
         this.password = data.getPassword();
     }
 
+    public void propertyChange( PropertyChangeEvent pce ) {
+        // check for done
+        if ( "done".equals( pce.getPropertyName() ) ) {
+            createProject( pce.getNewValue().toString() );
+        }
+    }
+
     public void actionPerformed( ActionEvent ae ) {
         NodeActor.setupLibrary();
         CheckoutDialog dialog = new CheckoutDialog( view, url );
         GUIUtils.center( view, dialog );
         dialog.setVisible( true );
-        final CheckoutData cd = dialog.getValues();
+        cd = dialog.getValues();
         if ( cd == null ) {
             return ;        // user cancelled
         }
@@ -103,81 +113,117 @@ public class CheckoutAction implements ActionListener {
                         throw new Exception( "Checkout failed." );
                     }
                     cd.getOut().print( "Checkout completed, revision " + revision );
-                    int make_project = JOptionPane.showConfirmDialog( view,
-                            "Checkout complete at revision " + revision + ".\n" +
-                            "Would you like to create a project from these files?",
-                            "Create Project?",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE );
-                    if ( make_project == JOptionPane.YES_OPTION ) {
-                        createProject();
-                    }
+                    firePropertyChange( "done", revision.toString(), "false" );
                 }
                 catch ( Exception e ) {
                     cd.getOut().printError( e.getMessage() );
                 }
             }
 
-            private void createProject() {
-                int index = cd.getURL().lastIndexOf( "/" );
-                index = index == -1 ? 0 : index + 1;
-                String project_name = cd.getURL().substring( index );
-                final VPTProject project = new VPTProject( project_name );
-                project.setRootPath( cd.getPaths().get( 0 ) );
-                final ProjectPropertiesPane pp_pane = new ProjectPropertiesPane( project, true, cd.getPaths().get( 0 ) );
-                final JDialog dialog = new JDialog( view, "Create Project", true );
-                JPanel panel = new JPanel( new BorderLayout() );
-                panel.setBorder( BorderFactory.createEmptyBorder( 6, 6, 6, 6 ) );
-                panel.add( pp_pane, BorderLayout.CENTER );
-                JPanel btn_panel = new JPanel( new FlowLayout( FlowLayout.RIGHT, 3, 3 ) );
-                JButton ok_btn = new JButton( " OK " );
-                ok_btn.addActionListener( new ActionListener() {
-                            public void actionPerformed( ActionEvent ae ) {
-                                boolean ok = false;
+        }
+        Runner runner = new Runner();
+        runner.addPropertyChangeListener( this );
+        runner.execute();
+    }
+
+    private void createProject( String revision ) {
+        int make_project = JOptionPane.showConfirmDialog( view,
+                "Checkout complete at revision " + revision + ".\n" +
+                "Would you like to create a project from these files?",
+                "Create Project?",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE );
+        if ( make_project != JOptionPane.YES_OPTION ) {
+            return ;
+        }
+
+        // use the directory name that the user entered for the location of the
+        // checkout as the default project name.  The UI will let the user
+        // change it if they want.
+        String path = cd.getPaths().get(0);
+        int index = path.lastIndexOf( "/" );
+        index = index == -1 ? 0 : index + 1;
+        String project_name = path.substring( index );
+        final VPTProject project = new VPTProject( project_name );
+
+        project.setRootPath( path );
+
+        // dialog for the project properties
+        final ProjectPropertiesPane pp_pane = new ProjectPropertiesPane( project, true, path );
+        final JDialog dialog = new JDialog( view, "Create Project", true );
+        JPanel panel = new JPanel( new BorderLayout() );
+        panel.setBorder( BorderFactory.createEmptyBorder( 6, 6, 6, 6 ) );
+        panel.add( pp_pane, BorderLayout.CENTER );
+        JPanel btn_panel = new JPanel( new FlowLayout( FlowLayout.RIGHT, 3, 3 ) );
+        JButton ok_btn = new JButton( " OK " );
+        ok_btn.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        dialog.setVisible( false );
+                        dialog.dispose();
+
+                        boolean ok = false;
+                        try {
+                            // save the panel and check the 'isOK' method result
+                            pp_pane.save();
+                            Boolean b = ( Boolean ) PrivilegedAccessor.invokeMethod( pp_pane, "isOK", null );
+                            ok = b.booleanValue();
+                        }
+                        catch ( Exception e ) {
+                            e.printStackTrace();
+                            return ;
+                        }
+                        if ( ok ) {
+                            final ProjectManager pm = ProjectManager.getInstance();
+                            if ( pm != null ) {
                                 try {
-                                    System.out.println("1");
-                                    pp_pane.save();
-                                    System.out.println("2");
-                                    Boolean b = ( Boolean ) PrivilegedAccessor.invokeMethod( pp_pane, "isOK", null );
-                                    System.out.println("3");
-                                    ok = b.booleanValue();
-                                    System.out.println("4");
-                                }
-                                catch ( Exception e ) {
-                                    e.printStackTrace();
-                                    return ;
-                                }
-                                System.out.println("5");
-                                if ( ok ) {
-                                    ProjectManager.getInstance().addProject( project, ( VPTGroup ) project.getObjectProperty( "projectviewer.new-parent" ) );
-                                    dialog.setVisible( false );
-                                    dialog.dispose();
+                                    // add the project to ProjectViewer, but first
+                                    // get the group. If the user picked one, it will be
+                                    // a project property, otherwise, default to the root project group
+                                    VPTGroup group = ( VPTGroup ) project.getObjectProperty( "projectviewer.new-parent" );
+                                    if (group == null) {
+                                        group = VPTRoot.getInstance();
+                                    }
+                                    pm.addProject( project, group );
+
+                                    // offer to import the files just checked out into ProjectViewer
+                                    // note that Importers are runnable, so add the importer to the
+                                    // event queue for handling.  If Importer.doImport is called here,
+                                    // there will be a deadlock.
                                     RootImporter importer = new RootImporter( project, ProjectViewer.getViewer( view ), true );
-                                    importer.doImport();
+                                    SwingUtilities.invokeLater(importer);
+
+                                    // I wonder about this -- if the import happens later
+                                    // (because of the invokeLater), will the save happen correctly?
+                                    pm.save();
+
+                                    // set ProjectViewer to show the new node
                                     ProjectViewer.setActiveNode( view, project );
                                 }
+                                catch ( Exception e ) {
+                                    e.printStackTrace( System.err );
+                                }
                             }
                         }
-                                        );
-                JButton cancel_btn = new JButton( "Cancel" );
-                cancel_btn.addActionListener( new ActionListener() {
-                            public void actionPerformed( ActionEvent ae ) {
-                                dialog.setVisible( false );
-                                dialog.dispose();
-                            }
-                        }
-                                            );
-                btn_panel.add( ok_btn );
-                btn_panel.add( cancel_btn );
+                    }
+                }
+                                );
+        JButton cancel_btn = new JButton( "Cancel" );
+        cancel_btn.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        dialog.setVisible( false );
+                        dialog.dispose();
+                    }
+                }
+                                    );
+        btn_panel.add( ok_btn );
+        btn_panel.add( cancel_btn );
 
-                panel.add( btn_panel, BorderLayout.SOUTH );
-                dialog.setContentPane( panel );
-                pp_pane.init();
-                dialog.pack();
-                GUIUtils.center( view, dialog );
-                dialog.setVisible( true );
-            }
-        }
-        ( new Runner() ).execute();
+        panel.add( btn_panel, BorderLayout.SOUTH );
+        dialog.setContentPane( panel );
+        pp_pane.init();
+        dialog.pack();
+        GUIUtils.center( view, dialog );
+        dialog.setVisible( true );
     }
+
 }
