d78a6c4170b940f310fe1bec7cf9f4c670d8d634
Matthieu Casanova <chocolat.mou@gmail.com>
2007-07-27 10:14:43 +0000

    fixed a NPE

--- plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/BinaryExpression.java
+++ plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/BinaryExpression.java
@@ -1,3 +1,21 @@
+/*
+ * BinaryExpression.java - A Binary Expression
+ * Copyright (C) 2004-2007 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package net.sourceforge.phpdt.internal.compiler.ast;
 
 import gatchan.phpparser.parser.PHPParserConstants;
@@ -11,115 +29,124 @@ import java.util.List;
  * @author Matthieu Casanova
  * @version $Id$
  */
-public final class BinaryExpression extends OperatorExpression {
-  /** The left expression. */
-  private final Expression left;
-  /** The right expression. */
-  private final Expression right;
+public final class BinaryExpression extends OperatorExpression
+{
+    /** The left expression. */
+    private final Expression left;
+    /** The right expression. */
+    private final Expression right;
 
-  public BinaryExpression(Expression left,
-                          Expression right,
-                          int operator,
-                          int sourceStart,
-                          int sourceEnd,
-                          int beginLine,
-                          int endLine,
-                          int beginColumn,
-                          int endColumn) {
-    super(Type.UNKNOWN, operator, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
-    this.left = left;
-    this.right = right;
-    switch (operator) {
-      case PHPParserConstants.OR_OR :
-      case PHPParserConstants.AND_AND :
-      case PHPParserConstants._ORL :
-      case PHPParserConstants.XOR :
-      case PHPParserConstants._ANDL:
-      case PHPParserConstants.EQUAL_EQUAL:
-      case PHPParserConstants.GT:
-      case PHPParserConstants.LT:
-      case PHPParserConstants.LE:
-      case PHPParserConstants.GE:
-      case PHPParserConstants.NOT_EQUAL:
-      case PHPParserConstants.DIF:
-      case PHPParserConstants.BANGDOUBLEEQUAL:
-      case PHPParserConstants.TRIPLEEQUAL:
-        type = Type.BOOLEAN;
-      break;
-      case PHPParserConstants.DOT:
-        type = Type.STRING;
-      break;
-      case PHPParserConstants.BIT_AND:
-      case PHPParserConstants.BIT_OR:
-      case PHPParserConstants.BIT_XOR:
-      case PHPParserConstants.LSHIFT:
-      case PHPParserConstants.RSIGNEDSHIFT:
-      case PHPParserConstants.RUNSIGNEDSHIFT:
-      case PHPParserConstants.PLUS:
-      case PHPParserConstants.MINUS:
-      case PHPParserConstants.STAR:
-      case PHPParserConstants.SLASH:
-      case PHPParserConstants.REMAINDER:
-        type = Type.INTEGER;
-      break;
-      case PHPParserConstants.INSTANCEOF:
-        type = Type.BOOLEAN;
-      break;
+    public BinaryExpression(Expression left,
+                            Expression right,
+                            int operator,
+                            int sourceStart,
+                            int sourceEnd,
+                            int beginLine,
+                            int endLine,
+                            int beginColumn,
+                            int endColumn)
+    {
+        super(Type.UNKNOWN, operator, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
+        this.left = left;
+        this.right = right;
+        switch (operator)
+        {
+            case PHPParserConstants.OR_OR:
+            case PHPParserConstants.AND_AND:
+            case PHPParserConstants._ORL:
+            case PHPParserConstants.XOR:
+            case PHPParserConstants._ANDL:
+            case PHPParserConstants.EQUAL_EQUAL:
+            case PHPParserConstants.GT:
+            case PHPParserConstants.LT:
+            case PHPParserConstants.LE:
+            case PHPParserConstants.GE:
+            case PHPParserConstants.NOT_EQUAL:
+            case PHPParserConstants.DIF:
+            case PHPParserConstants.BANGDOUBLEEQUAL:
+            case PHPParserConstants.TRIPLEEQUAL:
+                type = Type.BOOLEAN;
+                break;
+            case PHPParserConstants.DOT:
+                type = Type.STRING;
+                break;
+            case PHPParserConstants.BIT_AND:
+            case PHPParserConstants.BIT_OR:
+            case PHPParserConstants.BIT_XOR:
+            case PHPParserConstants.LSHIFT:
+            case PHPParserConstants.RSIGNEDSHIFT:
+            case PHPParserConstants.RUNSIGNEDSHIFT:
+            case PHPParserConstants.PLUS:
+            case PHPParserConstants.MINUS:
+            case PHPParserConstants.STAR:
+            case PHPParserConstants.SLASH:
+            case PHPParserConstants.REMAINDER:
+                type = Type.INTEGER;
+                break;
+            case PHPParserConstants.INSTANCEOF:
+                type = Type.BOOLEAN;
+                break;
+        }
     }
-  }
 
-  public String toStringExpression() {
-    String leftString = left.toStringExpression();
-    String operatorString = operatorToString();
-    String rightString = right.toStringExpression();
-    StringBuffer buff = new StringBuffer(leftString.length() + operatorString.length() + rightString.length());
-    buff.append(leftString);
-    buff.append(operatorString);
-    buff.append(rightString);
-    return buff.toString();
-  }
-
-  /**
-   * Get the variables from outside (parameters, globals ...)
-   *
-   * @param list the list where we will put variables
-   */
-  public void getOutsideVariable(List list) {
-  }
-
-  /**
-   * get the modified variables.
-   *
-   * @param list the list where we will put variables
-   */
-  public void getModifiedVariable(List list) {
-    left.getModifiedVariable(list);
-    if (right != null) {
-      right.getModifiedVariable(list);
+    public String toStringExpression()
+    {
+        StringBuilder buff = new StringBuilder();
+        buff.append(left.toStringExpression());
+        buff.append(operatorToString());
+        if (right != null)
+            buff.append(right.toStringExpression());
+        return buff.toString();
     }
-  }
 
-  /**
-   * Get the variables used.
-   *
-   * @param list the list where we will put variables
-   */
-  public void getUsedVariable(List list) {
-    left.getUsedVariable(list);
-    if (right != null) {
-      right.getUsedVariable(list);
+    /**
+     * Get the variables from outside (parameters, globals ...)
+     *
+     * @param list the list where we will put variables
+     */
+    public void getOutsideVariable(List list)
+    {
     }
-  }
 
-  public Expression expressionAt(int line, int column) {
-    if (left.isAt(line, column)) return left;
-    if (right != null && right.isAt(line, column)) return right;
-    return null;
-  }
+    /**
+     * get the modified variables.
+     *
+     * @param list the list where we will put variables
+     */
+    public void getModifiedVariable(List list)
+    {
+        left.getModifiedVariable(list);
+        if (right != null)
+        {
+            right.getModifiedVariable(list);
+        }
+    }
 
-  public void analyzeCode(PHPParser parser) {
-    left.analyzeCode(parser);
-    if (right != null) right.analyzeCode(parser);
-    // todo analyze binary expression
-  }
+    /**
+     * Get the variables used.
+     *
+     * @param list the list where we will put variables
+     */
+    public void getUsedVariable(List list)
+    {
+        left.getUsedVariable(list);
+        if (right != null)
+        {
+            right.getUsedVariable(list);
+        }
+    }
+
+    public Expression expressionAt(int line, int column)
+    {
+        if (left.isAt(line, column)) return left;
+        if (right != null && right.isAt(line, column)) return right;
+        return null;
+    }
+
+    public void analyzeCode(PHPParser parser)
+    {
+        left.analyzeCode(parser);
+        if (right != null) right.analyzeCode(parser);
+        // todo analyze binary expression
+    }
 }
