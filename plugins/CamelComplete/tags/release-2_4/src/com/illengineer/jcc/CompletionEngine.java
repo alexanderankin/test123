60ba3208dbe2ad6b0803db4cc1cf5a0fc052b8ab
Jesse Pavel <jpavel@alum.mit.edu>
2007-07-28 02:33:37 +0000

    Release 2.4

---
+++ plugins/CamelComplete/tags/release-2_4/src/com/illengineer/jcc/CompletionEngine.java
@@ -0,0 +1,279 @@
+package com.illengineer.jcc;
+
+import java.util.*;
+import java.util.regex.*;
+import java.util.zip.*;
+import java.io.*;
+
+public class CompletionEngine
+{
+    // {{{ member variables
+    private static int HASH_CAPACITY = 30;
+    private static float HASH_LOAD_FACTOR = 0.8f;
+    
+    // lengthArray[n] will contain the tree of HashMaps for identifiers with n
+    // prefixes. It will be resized as necessary
+    private HashMap<?,?>[] lengthArray;
+    
+    /*  I'm going to cache all the identifiers that this CompletionEngine knows about,
+	in case we want to retokenize, or maybe for some future reason. I can't see
+	this list taking more than 100K of memory or so.
+    */
+    private ArrayList<String> allIdentifiers;
+    
+    private boolean ignoreCase;
+    
+    // }}}
+    
+    // {{{ constructor(s)
+    public CompletionEngine() {
+	initDataStore();
+    }
+    // }}}
+
+    // {{{ public methods
+    
+    //	  {{{ loadIdentifiers()
+    /**
+	Reads in a list of identifiers from provider, and generates
+	its internal data structures using the splitting rules given
+	by each tokenizer in tokenizers. Note that you can call this method
+	multiple times to build up a big store of identifiers.
+	
+	@param provider Used to load a list of identifiers.
+	@param tokenizers Used to split identifiers into their constituent parts.
+	@param minparts Any identifiers with fewer than minparts tokens are discarded from
+			our internal store.
+	@param maxparts Any identifiers with more than maxparts tokens are discarded from
+			our internal store.
+	@param ignoreCase Should our matching be case insensitive? If subsequent calls
+			  to this method pass different values of ignoreCase, completion
+			  probabaly won't work well.
+	@param filterRegex If this is not-null, any identifier matching this regular expression
+			   will not be processed.
+    */
+    public void loadIdentifiers(IdentifierProvider provider,
+    				List<Tokenizer> tokenizers,
+				int minparts, int maxparts,
+				boolean ignoreCase,
+				String filterRegex) {
+	if (provider == null) {
+	    provider = new InternalIdentifierProvider(allIdentifiers);
+	    initDataStore();
+	}
+	    
+	this.ignoreCase = ignoreCase;
+	
+	Pattern pattern = null;
+	Matcher matcher;
+	
+	if (filterRegex != null) {
+	    try {
+		pattern = Pattern.compile(filterRegex);
+	    } catch (Exception ex) {
+		pattern = null;
+	    }
+	}
+	
+	for(String identifier : provider) {
+	    if (pattern != null) {
+		matcher = pattern.matcher(identifier);
+		if (matcher.lookingAt())
+		    continue;  // on to the next identifier
+	    }
+	
+	    for(Tokenizer t : tokenizers) {
+		char[] prefixes = t.splitIdentifer(identifier);
+		if (prefixes.length >= minparts && prefixes.length <= maxparts) {
+		    if (ignoreCase) {
+			for (int i = 0; i < prefixes.length; i++)
+			    prefixes[i] = Character.toLowerCase(prefixes[i]);
+		    }
+		    addIdentifer(identifier, prefixes);
+		    allIdentifiers.add(identifier);
+		}
+	    }
+	}
+    }
+    //		}}}
+        
+    //	  {{{ retokenize()
+    /**
+	If you want to change the method of tokenizing the identifiers,
+	but don't have the original IdentifierProvider(s) hanging around,
+	you can call this method to retokenize all the existing identifiers
+	in this CompletionEngine.
+	
+	@see loadIdentifiers
+    */
+    public void retokenize(List<Tokenizer> tokenizers, int minparts, int maxparts,
+			    boolean ignoreCase, String filterRegex) {
+	loadIdentifiers(null, tokenizers, minparts, maxparts, ignoreCase, filterRegex);
+    }
+    //    }}}
+    
+    //    {{{ clearTokens()
+    /**
+	Makes this CompletionEngine forget all the tokens that were fed to it
+	using loadIdentifiers(). In effect, it resets it to its newly created
+	state.
+    */
+    public void clearTokens() {
+	initDataStore();
+    }
+    //    }}}
+    
+    //	  {{{ complete()
+    /**
+	Completes the identifier suggested by acronym.
+	
+	@param acronym The acronym of the identifier one wishes to find.
+			For instance, acronym would be "AIOOBE" if you wanted
+			ArrayIndexOutOfBoundsException.
+	@param sort Should we sort the resulting list in natural order?
+	@return A list of possible completions, or null if there are none.
+    */
+    public List<String> complete(String acronym, boolean sort) {
+	int n = acronym.length();
+	if (lengthArray.length < (n+1) || lengthArray[n] == null)
+	    return null;
+	HashMap<Character,Object> currentMap = (HashMap<Character,Object>)lengthArray[n];
+	ArrayList<String> completions = null;
+	
+	if (ignoreCase)
+	    acronym = acronym.toLowerCase();
+	
+	for (int cntr = 0; cntr < n; cntr++) {
+	    Character c = new Character(acronym.charAt(cntr));
+	    if (!currentMap.containsKey(c))
+		return null;
+	    if (cntr < (n-1))
+		currentMap = (HashMap<Character,Object>)currentMap.get(c);
+	    else  // we're at the end
+		completions = (ArrayList<String>)currentMap.get(c);
+	}
+	if (sort) {
+	    // Yes, we're sorting the list in place, but it doesn't matter if the original
+	    // list is modified in this way.
+	    Collections.sort(completions);
+	}
+	return completions;
+    }
+    //	   }}}
+    
+    //	  {{{ numIdentifiers()
+    public int numIdentifiers() {
+	return allIdentifiers.size();
+    }
+    //    }}}
+    
+    //	  {{{ getIdentifiers()
+    public List<String> getIdentifiers() {
+	return allIdentifiers;
+    }
+    //	  }}}
+    
+    //	  {{{ Serialization Routines
+    /**
+	This method writes the data store to stream in some undefined
+	serialization format.
+	
+	@param stream The OutputStream to which we'll save our data store.
+    */
+    public void serializeData(OutputStream stream) throws IOException {
+	GZIPOutputStream gzipper = new GZIPOutputStream(stream);
+	ObjectOutputStream o = new ObjectOutputStream(gzipper);
+	o.writeObject(lengthArray);
+	o.writeObject(allIdentifiers);
+	gzipper.finish();
+    }
+    
+    /**
+	Reconstructs the data store from stream, which should be opened to
+	a resource previously written by serializeData().
+	
+	@param stream The InputStream from which to read serialized data.
+    */
+    public void deserializeData(InputStream stream) throws IOException  {
+	ObjectInputStream i = new ObjectInputStream(new GZIPInputStream(stream));
+	try {
+	    lengthArray = (HashMap<?,?>[])i.readObject();
+	    allIdentifiers = (ArrayList<String>)i.readObject();
+	} catch (ClassNotFoundException ex) {
+	    // This shouldn't happen unless we're hopelessly borked, in which
+	    // case it doesn't matter too much.
+	    initDataStore();
+	}
+    }
+    //    }}}
+    
+    
+    // }}}
+    
+    // {{{ support methods
+    private void addIdentifer(String identifier, char[] prefixes) {
+	int n = prefixes.length;
+	if (n == 0) return;
+	ensureLengthMapSize(n+1);
+	if (lengthArray[n] == null)
+	    lengthArray[n] = new HashMap<Character,Object>(HASH_CAPACITY, HASH_LOAD_FACTOR);
+	HashMap<Character,Object> currentMap = (HashMap<Character,Object>)lengthArray[n];
+	
+	// Every prefix except for the last will map to a new HashMap.
+	for (int cntr = 0; cntr < (prefixes.length-1); cntr++) {
+	    Character c = new Character(prefixes[cntr]);
+	    if (!currentMap.containsKey(c))
+		currentMap.put(c, new HashMap<Character,Object>(HASH_CAPACITY, HASH_LOAD_FACTOR));
+	    currentMap = (HashMap<Character,Object>)currentMap.get(c);
+	}
+	
+	// The last prefix will map to a List of Strings
+	Character c = new Character(prefixes[prefixes.length-1]);
+	if (!currentMap.containsKey(c))
+	    currentMap.put(c, new ArrayList<String>());
+	ArrayList<String> identifiers = (ArrayList<String>)currentMap.get(c);
+	if (!identifiers.contains(identifier))
+	    identifiers.add(identifier);
+    }
+    
+    
+    private void ensureLengthMapSize(int size) {
+	if (lengthArray.length >= size)
+	    return;
+	HashMap<?,?>[] newMap = new HashMap<?,?>[(int)(size*1.4)];
+	System.arraycopy(lengthArray, 0, newMap, 0, lengthArray.length);
+	lengthArray = newMap;
+    }
+    
+    private void initDataStore() {
+	lengthArray = new HashMap<?,?>[10];
+	allIdentifiers = new ArrayList<String>();
+    }
+    
+    // }}}
+
+    // {{{ inner classes
+    
+    // An implementation of IdentifierProvider that collects and iterates through
+    // all of the identifiers passed to it.
+    private class InternalIdentifierProvider implements IdentifierProvider
+    {
+	private ArrayList<String> identifiers;
+	
+	InternalIdentifierProvider(ArrayList<String> ids) {
+	    identifiers = ids;
+	}
+	
+	public void process() {};
+    
+	public Iterator<String> iterator() {
+	    return identifiers.iterator();
+	}
+	
+	public void forget() {};
+    }
+    
+    // }}}
+}
+
+// :folding=explicit:
---
+++ plugins/CamelComplete/tags/release-2_4/src/com/illengineer/jcc/jedit/CamelCompletePlugin.java
@@ -0,0 +1,365 @@
+package com.illengineer.jcc.jedit;
+
+import com.illengineer.jcc.*;
+
+import java.util.*;
+import java.io.*;
+
+import org.gjt.sp.jedit.textarea.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.StatusBar;
+import org.gjt.sp.jedit.gui.BeanShellErrorDialog;
+
+import bsh.Interpreter;
+
+public class CamelCompletePlugin extends EditPlugin {
+
+	// {{{ Member Variables
+	public static final String NAME = "camelcomplete";
+	public static final String OPTION_PREFIX = "options.camelcomplete.";
+
+	public static final String DEFAULT_ENGINE_NAME = "default";
+	
+	private static boolean debug = false;
+	private static PrintWriter debugWriter;
+	
+	/*  This Map will contain all the options and configuration set in the OptionPane
+	    Keys/Vals:
+	      engines -> Map<String (enginename), (providers)>
+	      	(providers) -> List<OptionPanel.OptionGroup>
+	      engine-opts -> Map<String (enginename), OptionPanel.EngineOpts>
+	      groups -> Map<String (groupname), List<OptionPanel.OptionGroup>>
+	      cache -> Boolean
+	      update -> Boolean
+	      popup-rows -> Integer
+	      remove-dups -> Boolean
+	*/
+	private static HashMap<String,Object> optionsMap;
+	
+	private static HashMap<String,List<OptionPanel.OptionGroup>> enginesOptionsMap;
+	private static HashMap<String, OptionPanel.EngineOpts> eoMap;
+	private static HashMap<String,EngineGroup> engineMap;
+	private static ArrayList<CompletionEngine> engines;
+	
+	
+	private static File homeDir;
+	// }}}
+	
+	// {{{ Lifecycle methods
+	
+	public void start() {
+	    if (debug) {
+		try {
+		    debugWriter = new PrintWriter(new FileWriter(new File("/Users/jpavel/Prog/JCodeComplete/debug.log")));
+		} catch (IOException ex) {}
+	    }
+	    
+	    /* For debugging! */
+	    // try {
+	    
+	    optionsMap = null;
+	    InputStream i;
+
+	    i = getResourceAsStream(CamelCompletePlugin.class, "options");
+	    if (i != null) {
+		boolean failed = false;
+		try {
+		    ObjectInputStream ois = new ObjectInputStream(i);
+		    optionsMap = (HashMap<String,Object>)ois.readObject();
+		    ois.close();
+		} catch (IOException ex) {
+		    failed = true;
+		} catch (ClassNotFoundException ex) {
+		    failed = true;
+		}
+		if (failed)
+		    optionsMap = null;
+	    }
+	    if (optionsMap == null || !optionsMap.containsKey("engines")) {
+		optionsMap = new HashMap<String,Object>();
+		enginesOptionsMap = new HashMap<String,List<OptionPanel.OptionGroup>>();
+		enginesOptionsMap.put(DEFAULT_ENGINE_NAME, new ArrayList<OptionPanel.OptionGroup>());
+		optionsMap.put("engines", enginesOptionsMap);
+	    } else {
+		enginesOptionsMap = (HashMap<String,List<OptionPanel.OptionGroup>>)optionsMap.get("engines");
+	    }
+	    
+	    engineMap = new HashMap<String,EngineGroup>();
+	    engines = new ArrayList<CompletionEngine>();
+
+	    if (!optionsMap.containsKey("cache"))
+		optionsMap.put("cache", Boolean.TRUE);
+	    if (!optionsMap.containsKey("update"))
+		optionsMap.put("update", Boolean.FALSE);
+	    if (!optionsMap.containsKey("popup-rows"))
+		optionsMap.put("popup-rows", new Integer(12));
+	    if (!optionsMap.containsKey("remove-dups"))
+		optionsMap.put("remove-dups", Boolean.FALSE);
+	    if (!optionsMap.containsKey("engine-opts")) {
+		eoMap = new HashMap<String, OptionPanel.EngineOpts>();
+		// We'll sync our maps.
+		for (String engineName : enginesOptionsMap.keySet())
+		    eoMap.put(engineName, new OptionPanel.EngineOpts());
+		optionsMap.put("engine-opts", eoMap);
+	    } else
+		eoMap = (HashMap<String, OptionPanel.EngineOpts>)optionsMap.get("engine-opts");
+		
+
+	    
+	    for (String engineName : enginesOptionsMap.keySet()) {
+		EngineGroup eg = new EngineGroup();
+		eg.engine = new CompletionEngine();
+		eg.modified = false;
+		
+		if (((Boolean)optionsMap.get("cache")).booleanValue()) {
+		    i = getResourceAsStream(CamelCompletePlugin.class, "cache/engine-"+engineName);
+		    if (i != null) {
+			try {
+			    eg.engine.deserializeData(i);
+			    i.close();
+			} catch (IOException ex) {
+			    eg.engine = new CompletionEngine();
+			}
+		    }
+		}
+		engineMap.put(engineName, eg);
+		engines.add(eg.engine);
+	    }
+	    
+	    homeDir = getPluginHome();
+	    
+	    if (((Boolean)optionsMap.get("update")).booleanValue())
+		processConfiguration();
+	    
+	    /* For debugging! */
+	    // } catch (Exception ex) {
+		// debugPrintStacktrace(ex);
+	    // }
+	}
+	
+	public void stop() {
+	    // Persist CompletionEngine data and optionsMap
+	    OutputStream out;
+	    
+	    if ((((Boolean)optionsMap.get("cache")).booleanValue())) {
+		for (String engineName : engineMap.keySet()) {
+		    EngineGroup eg = engineMap.get(engineName);
+		    if (eg.modified) {
+			out = getResourceAsOutputStream(CamelCompletePlugin.class, "cache/engine-"+engineName);
+			if (out != null) {
+			    try {
+				eg.engine.serializeData(out);
+				out.close();
+			    } catch (IOException ex) {}
+			}
+		    }
+		}
+	    }
+	    
+	    out = getResourceAsOutputStream(CamelCompletePlugin.class, "options");
+	    if (out != null) {
+		try {
+		    ObjectOutputStream oos = new ObjectOutputStream(out);
+		    oos.writeObject(optionsMap);
+		    oos.close();
+		} catch (IOException ex) {
+		}
+	    }
+	    
+	    if (debug)
+		debugWriter.close();
+
+	    optionsMap = null;
+	    enginesOptionsMap = null;
+	    engineMap = null;
+	    engines = null;
+	}
+	
+	// }}}
+	
+	// {{{ processConfiguration() & helpers
+	
+	/**
+	    This routine reads all the configuration options in the optionsMap and creates the
+	    appropriate IdentifierProviders and Tokenizers, and sends them to the CompletionEngine.
+	*/
+	static void processConfiguration() {
+	    for (String engineName : enginesOptionsMap.keySet())
+		processConfiguration(engineName);
+	    
+	}  
+	
+	static void processConfiguration(String engineName) {
+	    EngineGroup eg;
+	    if (engineMap.containsKey(engineName))
+		eg = engineMap.get(engineName);
+	    else {
+		eg = new EngineGroup();
+		eg.engine = new CompletionEngine();
+		eg.modified = false;
+		engineMap.put(engineName, eg);
+		engines.add(eg.engine);
+	    }
+
+	    List<OptionPanel.OptionGroup> groups = 
+			(List<OptionPanel.OptionGroup>)enginesOptionsMap.get(engineName);
+	    if (groups != null) {
+		IdentifierProvider provider = null;
+		ArrayList<Tokenizer> tokenizers;
+		eg.engine.clearTokens();
+		for (OptionPanel.OptionGroup og : groups) {
+		    tokenizers = new ArrayList<Tokenizer>(og.tokenizers.size());
+		    
+		    if (og.provider.equals("ctags"))
+			provider = new CTagsFileProvider(new File(og.fileName));
+		    else if (og.provider.equals("jar"))
+			provider = new JarFileProvider(new File(og.fileName));
+		    else if (og.provider.equals("text"))
+			provider = new TextFileProvider(new File(og.fileName));
+		    else if (og.provider.equals("code")) {
+			boolean failed = false;
+			Interpreter bsh = new Interpreter();
+			try {
+			    bsh.eval("import com.illengineer.jcc.*;");
+			    bsh.eval("import com.illengineer.jcc.jedit.*;");
+			    bsh.eval("IdentifierProvider __ip;");
+			    bsh.eval("__ip = (IdentifierProvider)" + og.extra);
+			    provider = (IdentifierProvider)bsh.get("__ip");
+			} catch (bsh.EvalError ex) {
+			    failed = true;
+			    View v = jEdit.getActiveView();
+			    if (v != null)
+				new BeanShellErrorDialog(v, ex);
+			    debugPrintStacktrace(ex);
+			} catch (Exception ex) {
+			    failed = true;
+			    View v = jEdit.getActiveView();
+			    if (v != null)
+				new BeanShellErrorDialog(v, ex);
+			    debugPrintStacktrace(ex);
+			}
+			if (failed)
+			    provider = new NullProvider();
+		    } else if (og.provider.equals("buffer")) {
+			provider = new BufferWordsProvider(og.extra, 
+			    (og.config != null ? ((Boolean)og.config).booleanValue() : true));
+		    }
+		    for (String [] t : og.tokenizers) {
+			if (t[0].equals("camelcase"))
+			    tokenizers.add(new CamelCaseTokenizer());
+			else if (t[0].equals("regex")) 
+			    tokenizers.add(new RegexpTokenizer(t[1], t[2].equals("y")));
+		    }
+		    provider.process();
+		    eg.engine.loadIdentifiers(provider, tokenizers, og.minparts, og.maxparts,
+					      og.ignoreCase, og.filterRegex);
+		    provider.forget();
+		}
+	    }
+	    eg.modified = true;
+	}
+	
+	static void deleteEngine(String engineName) {
+	    if (enginesOptionsMap.size() <= 1)
+		return;
+		
+	    enginesOptionsMap.remove(engineName);
+	    EngineGroup eg = engineMap.remove(engineName);
+	    if (eg != null)
+		engines.remove(eg.engine);
+	}
+	
+	// }}}
+	
+	// {{{ Option routines
+	public static void setOption(String key, Object val) {
+	    optionsMap.put(key, val);
+	}
+	
+	public static Object getOption(String key) {
+	    return optionsMap.get(key);
+	}
+	// }}}
+	
+	// {{{ completion methods
+	public static void complete(View view, JEditTextArea textArea, boolean normal) {
+	    CompleteWord.completeWord(view, normal);
+	}
+	
+	public static List<String> getCompletions(String word) {
+	    TreeSet<String> t = new TreeSet<String>();
+	    for (CompletionEngine engine : engines) {
+		List<String> c = engine.complete(word, false);
+		if (c != null)
+		    t.addAll(c);
+	    }
+	    return new ArrayList<String>(t);
+	}
+	
+	public static List<String> getNormalCompletions(String word) {
+	    TreeSet<String> t = new TreeSet<String>();
+	    for (String engineName : eoMap.keySet()) {
+		OptionPanel.EngineOpts eo = eoMap.get(engineName);
+		if (eo.normalCompletionMode != 0) {
+		    CompletionEngine engine = engineMap.get(engineName).engine;
+		    List<String> ids = engine.getIdentifiers();
+		    for (String id : ids) {
+			if (eo.normalCompletionMode == 1) { // starts with
+			    if (id.startsWith(word))
+				t.add(id);
+			} else if (eo.normalCompletionMode == 2) {  // contains
+			    if (id.contains(word))
+				t.add(id);
+			}
+		    }
+		}
+	    }
+	    return new ArrayList<String>(t);
+	}
+	
+	// }}}
+	
+	// {{{ Debugging methods
+	public static void debugPrint(String s) {
+	    if (debug) {
+		debugWriter.println(s);
+		debugWriter.flush();
+	    }
+	}
+
+	public static void debugPrintStacktrace(Exception ex) {
+	    if (debug) {
+		ex.printStackTrace(debugWriter);
+		debugWriter.flush();
+	    }
+	}
+
+	// }}}
+	
+	// {{{ Inner Classes
+	private static class EngineGroup
+	{
+	    CompletionEngine engine;
+	    boolean modified;
+	}
+	// }}}
+
+	// {{{ Misc public methods
+	public static Set<String> getEngineNames() {
+	    return enginesOptionsMap.keySet();
+	}
+	
+	public static void clearCacheDir() {
+	    if (homeDir != null) {
+		File cacheDir = new File(homeDir, "cache");
+		if (cacheDir.exists()) {
+		    File [] caches = cacheDir.listFiles();
+		    for (File f : caches)
+			f.delete();
+		}
+	    }
+	}
+	// }}}
+}
+
+//:folding=explicit:collapseFolds=1:
