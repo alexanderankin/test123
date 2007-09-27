263e417838403a54364e69293f799ab2fb0df6ac
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-09-27 13:37:36 +0000

    Replaced the ModeOptionPanel, that inherited JPanel for no reason other than allow UI classes to derive from it, with a ModeOptionPaneController that is used as a helper class inside UI classes rather than being derived by them. This enables the UI classes to derive either from JPanel, AbstractOptionPane, or anything else.

--- plugins/CtagsSideKick/trunk/ctags/sidekick/options/CtagsCmdOptionsPane.java
+++ plugins/CtagsSideKick/trunk/ctags/sidekick/options/CtagsCmdOptionsPane.java
@@ -1,6 +1,8 @@
 package ctags.sidekick.options;
 
+import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.gjt.sp.jedit.jEdit;
@@ -8,7 +10,8 @@ import org.gjt.sp.jedit.jEdit;
 import ctags.sidekick.Plugin;
 
 @SuppressWarnings("serial")
-public class CtagsCmdOptionsPane extends ModeOptionPanel {
+public class CtagsCmdOptionsPane extends JPanel
+	implements ModeOptionPaneController.ModeOptionPane {
 
 	public static class Props {
 		Props(String s) {
@@ -25,29 +28,28 @@ public class CtagsCmdOptionsPane extends ModeOptionPanel {
 		add(ctagsCmdOptions = new JTextField(30));
 	}
 	
-	@Override
-	protected Object createModeProps(String mode) {
+	public JComponent getComponent() {
+		return this;
+	}
+
+	public Object createModeProps(String mode) {
 		return new Props(SideKickModeOptionsPane.getProperty(mode,
 			Plugin.CTAGS_MODE_OPTIONS));
 	}
 
-	@Override
-	protected void resetModeProps(String mode) {
+	public void resetModeProps(String mode) {
 		SideKickModeOptionsPane.clearModeProperty(mode, Plugin.CTAGS_MODE_OPTIONS);
 	}
 
-	@Override
-	protected void saveModeProps(String mode, Object props) {
+	public void saveModeProps(String mode, Object props) {
 		SideKickModeOptionsPane.setProperty(mode, Plugin.CTAGS_MODE_OPTIONS, ((Props)props).options);
 	}
 
-	@Override
-	protected void updatePropsFromUI(Object props) {
+	public void updatePropsFromUI(Object props) {
 		((Props)props).options = ctagsCmdOptions.getText();
 	}
 
-	@Override
-	protected void updateUIFromProps(Object props) {
+	public void updateUIFromProps(Object props) {
 		ctagsCmdOptions.setText(((Props)props).options);
 	}
 
