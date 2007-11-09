6c8991abd17401d582d0d4d59b1568460c313f33
Alan Ezust <1688991+ezust@users.noreply.github.com>
2007-11-09 18:47:27 +0000

    Initial import.

---
+++ plugins/SshConsole/trunk/console/ssh/Shell.java
@@ -0,0 +1,76 @@
+package console.ssh;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.gjt.sp.util.Log;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import console.Console;
+import console.ConsolePane;
+import console.ErrorOutput;
+import console.Output;
+import ftp.ConnectionInfo;
+
+/**
+ * Secure shell interface for jEdit console. A singleton exists for the whole jedit process.
+ * @author ezust
+ *
+ */
+public class Shell extends console.Shell {
+
+	
+	public void closeConsole(Console console)
+	{
+		// TODO Auto-generated method stub
+		super.closeConsole(console);
+	}
+
+	@Override
+	public void openConsole(Console console)
+	{
+	
+	}
+
+	public Shell() {
+		super("ssh");
+	}
+	
+	public void execute(Console console, String input, Output output, Output error, String command)
+	{
+		try { 
+			ConsoleState ss = ConnectionManager.getConsoleState(console);
+			// Expected to be non-null if you connected via FSB already
+			ConnectionInfo info = ConnectionManager.getConnectionInfo(ss.path);
+			Session session=ConnectionManager.client.getSession(info.user, info.host, 22);
+			Connection c = ConnectionManager.getShellConnection(console, info);
+			session.setUserInfo(c);
+			Channel channel = c.channel;
+			OutputStream os = channel.getOutputStream();
+			Log.log (Log.MESSAGE, this, "Command: " + command + "  input: " + input);
+			os.write((command + "\n").getBytes() );
+			os.flush();
+		}
+		catch (Exception e) {
+			Log.log (Log.WARNING, this, "no ssh session:", e);
+			printPrompt(console, output);
+		}
+	}
+
+	public void printPrompt(Console console, Output output)
+	{
+		ConsoleState s = ConnectionManager.getConsoleState(console);
+		String promptString = "[no sftp:// connections?] >";
+		if (s.path != null && s.path.length() > 0) {
+			promptString = "[" + s.path + "]> ";
+		}
+		output.writeAttrs(ConsolePane.colorAttributes(console.getPlainColor()), 
+			"\n" + promptString);
+	}    
+	
+};
+
