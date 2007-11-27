44403ae40ebff261da0616be7eca459d45cf6d5d
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-11-27 05:58:09 +0000

    Small cleanup

--- plugins/InfoNodeDW/trunk/src/infonode/WindowManager.java
+++ plugins/InfoNodeDW/trunk/src/infonode/WindowManager.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.util.HashMap;
-import java.util.Vector;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
