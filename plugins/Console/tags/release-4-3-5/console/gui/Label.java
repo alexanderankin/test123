46d682a11b984731a3e0e4902a24e06bc09f441e
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2007-11-29 19:54:01 +0000

    Retagging 4.3.5

---
+++ plugins/Console/tags/release-4-3-5/console/gui/Label.java
@@ -0,0 +1,22 @@
+package console.gui;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+import org.gjt.sp.jedit.jEdit;
+
+public class Label extends JLabel
+{
+	public Label(String propertyName) 
+	{
+		String text = jEdit.getProperty(propertyName);
+		setText(text);
+		String tooltip = jEdit.getProperty(propertyName + ".tooltip");
+		setToolTipText(tooltip);
+	}
+	public Label(String propertyName, int halign) {
+		this(propertyName);
+		setHorizontalAlignment(halign);
+	}
+}
