ee136f795bed53f7c0992176901fae31cf8f131b
Alan Ezust <1688991+ezust@users.noreply.github.com>
2007-12-08 01:13:52 +0000

    Retagging

---
+++ plugins/SshConsole/tags/release-1.0/console/ssh/SshConsolePlugin.java
@@ -0,0 +1,43 @@
+package console.ssh;
+
+
+import org.gjt.sp.jedit.EBMessage;
+import org.gjt.sp.jedit.EBPlugin;
+import org.gjt.sp.jedit.msg.VFSPathSelected;
+
+import console.Console;
+import console.ConsolePlugin;
+
+/**
+  SshConsole - a jEdit plugin that offers a ssh shell to the Console that responds
+  to VFSPathSelected events from the VFSBrowser. 
+  @author Alan Ezust
+  @version $Id$
+*/  
+public class SshConsolePlugin extends EBPlugin {
+
+    public void start()
+	{
+		ConnectionManager.setup();
+	}
+
+	public void stop()
+	{
+		ConnectionManager.cleanup();
+	}
+    
+	public void handleMessage(EBMessage msg)
+	{
+		if (msg instanceof VFSPathSelected) {
+			VFSPathSelected vps = (VFSPathSelected) msg;
+			String path = vps.getPath();
+			if (!path.startsWith("sftp://")) return;
+			if (!vps.isDirectory())
+				path = path.substring(0, path.lastIndexOf('/'));
+			Console c = ConsolePlugin.getConsole(vps.getView());
+			if (!c.isVisible()) return;
+			ConsoleState cs = ConnectionManager.getConsoleState(c);
+			cs.setPath(path, true);
+		}
+	}
+}
