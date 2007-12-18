dd95672e7537af4318a2f230b7acf4e681292eb4
Dale <danson@grafidog.com>
2007-12-18 05:07:30 +0000

    copying

---
+++ plugins/SVNPlugin/tags/20071217.2/src/ise/plugin/svn/command/Log.java
@@ -0,0 +1,255 @@
+/*
+Copyright (c) 2007, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package ise.plugin.svn.command;
+
+import java.io.*;
+import java.util.*;
+
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNLogEntryPath;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.wc.SVNLogClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.ISVNOptions;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+import org.tmatesoft.svn.cli.command.SVNCommandEventProcessor;
+import org.tmatesoft.svn.core.wc.SVNInfo;
+
+import org.tmatesoft.svn.core.SVNException;
+
+import ise.plugin.svn.data.LogData;
+import ise.plugin.svn.data.LogResults;
+
+
+public class Log {
+
+    private LogResults results = new LogResults();
+    private TreeMap < String, List < SVNLogEntry >> entries = new TreeMap < String, List < SVNLogEntry >> ();
+
+    private PrintStream out = null;
+
+    /**
+     * Fills a LogResults based on the given LogData.
+     * @param data LogData containing the information necessary to fetch an svn log.
+     */
+    public void doLog( LogData data ) throws CommandInitializationException, SVNException {
+        SVNKit.setupLibrary();
+
+        // validate data values
+        if ( data.getPaths() == null ) {
+            return ;     // nothing to do
+        }
+        if ( data.getOut() == null ) {
+            throw new CommandInitializationException( "Invalid output stream." );
+        }
+        if ( data.getErr() == null ) {
+            data.setErr( data.getOut() );
+        }
+
+        // convert paths to Files
+        List<String> paths = data.getPaths();
+        File[] localPaths = null;
+        if ( !data.pathsAreURLs() ) {
+            localPaths = new File[ paths.size() ];
+            for ( int i = 0; i < paths.size(); i++ ) {
+                localPaths[ i ] = new File( paths.get( i ) );
+                // check for file existence?
+            }
+        }
+
+        // use default svn config options
+        ISVNOptions options = SVNWCUtil.createDefaultOptions( true );
+
+        // use the svnkit client manager
+        SVNClientManager clientManager = SVNClientManager.newInstance( options, data.getUsername(), data.getPassword() );
+
+        // get a commit client
+        SVNLogClient client = clientManager.getLogClient();
+        SVNWCClient wc_client = clientManager.getWCClient();
+
+        // set an event handler so that messages go to the commit data streams for display
+        client.setEventHandler( new SVNCommandEventProcessor( data.getOut(), data.getErr(), false ) );
+
+        out = data.getOut();
+
+        if ( data.pathsAreURLs() ) {
+            for (String path : data.getPaths()) {
+                SVNURL svnurl = SVNURL.parseURIDecoded(path);
+                LogHandler handler = new LogHandler( path );
+                // files for logs, paths, peg revision, start revision,
+                // end revision, stop on copy, report paths, number of entries, handler
+                client.doLog( svnurl, null, SVNRevision.create( 0L ), data.getStartRevision(),
+                    data.getEndRevision(), data.getStopOnCopy(), data.getShowPaths(), data.getMaxLogs(), handler );
+                entries.put( handler.getPath(), handler.getEntries() );
+            }
+        }
+        else {
+            for ( File file : localPaths ) {
+                // this feels like a kludge, I shouldn't have to do a substring
+                // call to figure out the path of the file
+                LogHandler handler = new LogHandler( file );
+                SVNInfo info = wc_client.doInfo(file, SVNRevision.WORKING);
+                results.setInfo(info);
+                //System.out.println("+++++ repository url = " + info.getRepositoryRootURL());
+                //System.out.println("+++++ url = " + info.getURL());
+                //System.out.println("+++++ url.getPath = " + info.getURL().getPath());
+                String rep_url_string = info.getRepositoryRootURL().toString();
+                String file_url_string = info.getURL().toString();
+                String path = file_url_string.substring(rep_url_string.length());
+                SVNURL rep_url = SVNURL.parseURIEncoded(rep_url_string);
+                String[] rep_paths = new String[]{path};
+                // I should also be able to set the peg revision, but it seems that
+                // using anything beside 0 fails.
+                client.doLog( rep_url, rep_paths, SVNRevision.create(0L), data.getStartRevision(),
+                    data.getEndRevision(), data.getStopOnCopy(), data.getShowPaths(), data.getMaxLogs(), handler );
+                entries.put( handler.getPath(), handler.getEntries() );
+            }
+        }
+        results.setEntries(entries);
+        out.flush();
+        out.close();
+    }
+
+    public class LogHandler implements ISVNLogEntryHandler {
+
+        private String path = "";
+        private List<SVNLogEntry> logEntries = null;
+
+        public LogHandler( File f ) {
+            path = f.toString();
+        }
+
+        public LogHandler( String p ) {
+            path = p;
+        }
+
+        public void handleLogEntry( SVNLogEntry logEntry ) {
+            if ( logEntries == null ) {
+                logEntries = new ArrayList<SVNLogEntry>();
+            }
+            logEntries.add( logEntry );
+            Log.this.printLogEntry( path, logEntry );
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public List<SVNLogEntry> getEntries() {
+            return logEntries;
+        }
+    }
+
+    /**
+     * @return a log results object
+     */
+    public LogResults getLogEntries() {
+        return results;
+    }
+
+    public void printLogEntry( String path, SVNLogEntry logEntry ) {
+        if ( out == null )
+            return ;
+
+        out.println( "path: " + path );
+        out.println( "revision: " + logEntry.getRevision() );
+        out.println( "author: " + logEntry.getAuthor() );
+        out.println( "date: " + logEntry.getDate() );
+        out.println( "log message: " + logEntry.getMessage() );
+
+        // displaying all paths that were changed in that revision, changed
+        // path information is represented by SVNLogEntryPath.
+        if ( logEntry.getChangedPaths().size() > 0 ) {
+            out.println();
+            out.println( "changed paths:" );
+
+            // keys are changed paths
+            Set changedPathsSet = logEntry.getChangedPaths().keySet();
+
+            for ( Iterator changedPaths = changedPathsSet.iterator(); changedPaths.hasNext(); ) {
+                SVNLogEntryPath entryPath = ( SVNLogEntryPath ) logEntry.getChangedPaths().get( changedPaths.next() );
+
+                /*
+                 SVNLogEntryPath.getPath returns the changed path itself
+
+                 SVNLogEntryPath.getType returns a character describing
+                 how the path was changed ('A' - added, 'D' - deleted or
+                 'M' - modified);
+
+                 If the path was copied from another one (branched) then
+                 SVNLogEntryPath.getCopyPath and
+                 SVNLogEntryPath.getCopyRevision tells where it was copied
+                 from and what revision the origin path was at.
+                 */
+                out.println( " "
+                        + entryPath.getType()
+                        + " "
+                        + entryPath.getPath()
+                        + ( ( entryPath.getCopyPath() != null ) ? " (from "
+                            + entryPath.getCopyPath() + " revision "
+                            + entryPath.getCopyRevision() + ")" : "" ) );
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        // for testing
+        LogData data = new LogData();
+        data.setUsername("daleanson");
+        data.setPassword("");
+        List<String> paths = new ArrayList<String>();
+        paths.add("/home/danson/src/plugins/SVNPlugin/src/ise/plugin/svn/command/Log.java");
+        data.setPaths(paths);
+        data.setOut(new ise.plugin.svn.io.ConsolePrintStream(new ise.plugin.svn.io.LogOutputStream(null)));
+        //long start_rev = 9795L;
+        //long end_rev = 9810L;
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        cal.set(2007, 5, 1);
+        //SVNRevision start = SVNRevision.parse(String.valueOf(start_rev));
+        SVNRevision start = SVNRevision.create(cal.getTime());
+        cal.set(2007, 6, 1);
+        //SVNRevision end = SVNRevision.parse(String.valueOf(end_rev));
+        SVNRevision end = SVNRevision.create(cal.getTime());
+        data.setStartRevision(start);
+        data.setEndRevision(end);
+        Log log = new Log();
+        try {
+            org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory.setup();
+            org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl.setup();
+            org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory.setup();
+            log.doLog(data);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ plugins/SVNPlugin/tags/20071217.2/src/ise/plugin/svn/gui/CopyDialog.java
@@ -0,0 +1,251 @@
+/*
+Copyright (c) 2007, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package ise.plugin.svn.gui;
+
+// imports
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.io.File;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.swing.border.EmptyBorder;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.util.*;
+import org.gjt.sp.jedit.browser.VFSBrowser;
+
+import ise.java.awt.*;
+import ise.plugin.svn.pv.SVNAction;
+import ise.plugin.svn.data.*;
+import ise.plugin.svn.command.*;
+import ise.plugin.svn.library.*;
+
+import org.tmatesoft.svn.core.SVNURL;
+
+public class CopyDialog extends JDialog {
+    // instance fields
+    private View view = null;
+    private List<File> toCopy = null;
+    private JTextField path = null;
+    private TableModel fileTableModel = null;
+    private String defaultDestination = null;
+
+    private boolean usingFile = true;
+    private boolean cancelled = false;
+
+    public CopyDialog( View view, List<File> files, String defaultDestination ) {
+        super( ( JFrame ) view, "Copy", true );
+        if ( files == null || files.size() == 0 ) {
+            throw new IllegalArgumentException( "no source file(s) to copy" );
+        }
+        this.view = view;
+        this.toCopy = files;
+        this.defaultDestination = defaultDestination == null ? "" : defaultDestination;
+        init();
+    }
+
+    protected void init() {
+        KappaLayout layout = new KappaLayout();
+        JPanel panel = new JPanel( layout );
+        panel.setBorder( new EmptyBorder( 6, 6, 6, 6 ) );
+
+        JLabel to_copy_label = new JLabel( "Copy " + ( toCopy.size() == 1 ? "this file:" : "these files:" ) );
+
+        // 2 column table, column 0 is list of filenames/directory names,
+        // column 1 is boolean to set recursive on directories
+        JTable file_table = new JTable();
+
+        // create table model
+        fileTableModel = new DefaultTableModel( new String[]{"File"}, toCopy.size() ) ;
+
+        // fill table model.  If directory, add a checkbox defaulting to checked
+        // indicating the copy should recursively copy the directory.
+        for ( int row = 0; row < toCopy.size(); row++ ) {
+            File file = toCopy.get( row );
+            fileTableModel.setValueAt( file.getAbsolutePath(), row, 0 );
+        }
+        file_table.setModel( fileTableModel );
+        file_table.getColumnModel().getColumn( 0 ).setPreferredWidth( 600 );
+
+
+        // local destination directory.  TODO: need to be able to set remote url
+        JLabel path_label = new JLabel( "To this location:" );
+        path = new JTextField( defaultDestination , 30 );
+        JButton browse_local_btn = new JButton( "Browse Local..." );
+        browse_local_btn.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        String[] dirs = GUIUtilities.showVFSFileDialog( view, defaultDestination, toCopy.size() == 1 ? VFSBrowser.OPEN_DIALOG : VFSBrowser.CHOOSE_DIRECTORY_DIALOG, false );
+                        if ( dirs != null && dirs.length > 0 ) {
+                            String filename = dirs[ 0 ];
+                            File f = new File( filename );
+                            if ( toCopy.size() == 1 && f.exists() ) {
+                                int overwrite = JOptionPane.showConfirmDialog( view, "File exists, okay to overwrite?", "File exists", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE );
+                                if ( overwrite == JOptionPane.NO_OPTION ) {
+                                    return ;
+                                }
+                            }
+                            path.setText( f.getAbsolutePath() );
+                            usingFile = true;
+                        }
+                    }
+                }
+                                          );
+        JButton browse_remote_btn = new JButton( "Browse Remote..." );
+        browse_remote_btn.addActionListener(
+            new ActionListener() {
+                public void actionPerformed( ActionEvent ae ) {
+                    final JDialog dialog = new JDialog( view, "Select Repository Destination" );
+                    dialog.setModal( true );
+                    JPanel panel = new JPanel( new LambdaLayout() );
+                    panel.setBorder( BorderFactory.createEmptyBorder( 6, 6, 6, 6 ) );
+                    final BrowseRepositoryPanel burp = new BrowseRepositoryPanel( view );
+                    panel.add( "0, 0, 1, 1, 0, wh, 3", burp );
+                    KappaLayout btn_layout = new KappaLayout();
+                    JPanel button_panel = new JPanel( btn_layout );
+                    JButton ok_btn = new JButton( "OK" );
+                    ok_btn.addActionListener(
+                        new ActionListener() {
+                            public void actionPerformed( ActionEvent ae ) {
+                                String selection = burp.getSelectionPath();
+                                dialog.setVisible( false );
+                                dialog.dispose();
+                                if ( selection != null && selection.length() > 0 ) {
+                                    path.setText( selection );
+                                    usingFile = false;
+                                }
+                            }
+                        }
+                    );
+                    JButton cancel_btn = new JButton( "Cancel" );
+                    cancel_btn.addActionListener(
+                        new ActionListener() {
+                            public void actionPerformed( ActionEvent ae ) {
+                                dialog.setVisible( false );
+                                dialog.dispose();
+                            }
+                        }
+                    );
+                    button_panel.add( "0, 0, 1, 1, 0, w, 3", ok_btn );
+                    button_panel.add( "1, 0, 1, 1, 0, w, 3", cancel_btn );
+                    btn_layout.makeColumnsSameWidth( 0, 1 );
+
+                    panel.add( "0, 1, 1, 1", KappaLayout.createStrut( 350, 11, false ) );
+                    panel.add( "0, 2, 1, 1, E, , 3", button_panel );
+                    dialog.setContentPane( panel );
+                    dialog.pack();
+                    GUIUtils.center( view, dialog );
+                    dialog.setVisible( true );
+                }
+            }
+        );
+
+        // ok and cancel buttons
+        KappaLayout kl = new KappaLayout();
+        JPanel btn_panel = new JPanel( kl );
+        JButton ok_btn = new JButton( "Ok" );
+        JButton cancel_btn = new JButton( "Cancel" );
+        btn_panel.add( "0, 0, 1, 1, 0, w, 3", ok_btn );
+        btn_panel.add( "1, 0, 1, 1, 0, w, 3", cancel_btn );
+        kl.makeColumnsSameWidth( 0, 1 );
+
+        ok_btn.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        if ( path == null || path.getText().length() == 0 ) {
+                            JOptionPane.showMessageDialog( CopyDialog.this, "Directory is required.", "Error", JOptionPane.ERROR_MESSAGE );
+                            return ;
+                        }
+                        cancelled = false;
+                        CopyDialog.this.setVisible( false );
+                        CopyDialog.this.dispose();
+                    }
+                }
+                                );
+
+        cancel_btn.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        cancelled = true;
+                        CopyDialog.this.setVisible( false );
+                        CopyDialog.this.dispose();
+                    }
+                }
+                                    );
+
+
+        // add the components to the option panel
+        panel.add( "0, 0, 8, 1, W,  , 3", to_copy_label );
+        JScrollPane file_scroller = new JScrollPane( file_table );
+        file_scroller.getViewport().setPreferredSize( new Dimension( 600, 200 ) );
+        panel.add( "0, 1, 8, 1, W, w, 3", file_scroller );
+
+        panel.add( "0, 2, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 10, true ) );
+
+        panel.add( "0, 3, 1, 1, W,  , 3", path_label );
+        panel.add( "0, 4, 8, 1, 0, w, 3", path );
+        panel.add( "0, 5, 1, 1, 0, w, 3", browse_local_btn );
+        panel.add( "1, 5, 1, 1, 0, w, 3", browse_remote_btn );
+        layout.makeColumnsSameWidth( 0, 1 );
+
+        panel.add( "0, 6, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 10, true ) );
+        panel.add( "0, 7, 8, 1, E,  , 0", btn_panel );
+
+        setContentPane( panel );
+        pack();
+
+    }
+
+    public CopyData getData() {
+        if ( cancelled ) {
+            return null;
+        }
+        CopyData cd = new CopyData();
+        List<File> files = new ArrayList<File>();
+        for ( int row = 0; row < fileTableModel.getRowCount(); row++ ) {
+            String filename = ( String ) fileTableModel.getValueAt( row, 0 );
+            files.add( new File( filename ) );
+        }
+
+        cd.setSourceFiles( files );
+
+        if ( usingFile ) {
+            cd.setDestinationFile( new File( path.getText() ) );
+        }
+        else {
+            try {
+                cd.setDestinationURL( SVNURL.parseURIDecoded( path.getText() ) );
+            }
+            catch ( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+        return cd;
+    }
+}
