5aec49744176b042fa92ef01294ed3054b0de095
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-01-07 21:29:25 +0000

    Release 0.1

---
+++ plugins/CtagsInterface/tags/release-0.1/src/options/DirsOptionPane.java
@@ -0,0 +1,99 @@
+package options;
+
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.MiscUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.gui.RolloverButton;
+
+import ctags.CtagsInterfacePlugin;
+import db.TagDB;
+
+@SuppressWarnings("serial")
+public class DirsOptionPane extends AbstractOptionPane {
+
+	private static final String DIR_ORIGIN = TagDB.DIR_ORIGIN;
+	static public final String OPTION = CtagsInterfacePlugin.OPTION;
+	static public final String MESSAGE = CtagsInterfacePlugin.MESSAGE;
+	static public final String DIRS = OPTION + "dirs.";
+	JList dirs;
+	DefaultListModel dirsModel;
+	
+	public DirsOptionPane() {
+		super("CtagsInterface-Dirs");
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		dirsModel = new DefaultListModel();
+		Vector<String> trees = getDirs();
+		for (int i = 0; i < trees.size(); i++)
+			dirsModel.addElement(trees.get(i));
+		dirs = new JList(dirsModel);
+		JScrollPane scroller = new JScrollPane(dirs);
+		scroller.setBorder(BorderFactory.createTitledBorder(
+				jEdit.getProperty(MESSAGE + "dirs")));
+		addComponent(scroller, GridBagConstraints.HORIZONTAL);
+		JPanel buttons = new JPanel();
+		JButton add = new RolloverButton(GUIUtilities.loadIcon("Plus.png"));
+		buttons.add(add);
+		JButton remove = new RolloverButton(GUIUtilities.loadIcon("Minus.png"));
+		buttons.add(remove);
+		JButton tag = new JButton("Tag");
+		buttons.add(tag);
+		addComponent(buttons);
+
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				JFileChooser fc = new JFileChooser();
+				fc.setDialogTitle("Select root of source tree");
+				fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int ret = fc.showOpenDialog(DirsOptionPane.this);
+				if (ret != JFileChooser.APPROVE_OPTION)
+					return;
+				String dir = fc.getSelectedFile().getAbsolutePath();
+				dirsModel.addElement(MiscUtilities.resolveSymlinks(dir));
+			}
+		});
+		remove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				int i = dirs.getSelectedIndex();
+				if (i >= 0)
+					dirsModel.removeElementAt(i);
+			}
+		});
+		tag.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				int i = dirs.getSelectedIndex();
+				if (i >= 0) {
+					String tree = (String) dirsModel.getElementAt(i);
+					CtagsInterfacePlugin.refreshOrigin(DIR_ORIGIN, tree);
+				}
+			}
+		});
+	}
+
+	public void save() {
+		Vector<String> names = new Vector<String>();
+		int nDirs = dirsModel.size(); 
+		for (int i = 0; i < nDirs; i++)
+			names.add((String) dirsModel.getElementAt(i));
+		CtagsInterfacePlugin.updateOrigins(DIR_ORIGIN, names);
+	}
+	
+	static public Vector<String> getDirs() {
+		return CtagsInterfacePlugin.getDB().getOrigins(DIR_ORIGIN);
+	}
+}
