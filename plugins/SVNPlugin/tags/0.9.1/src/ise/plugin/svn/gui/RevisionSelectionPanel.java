c9061a8bd3cc2f35338d9939a84b3cccbb933a6f
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-01-14 06:16:35 +0000

    tagging for release

---
+++ plugins/SVNPlugin/tags/0.9.1/src/ise/plugin/svn/gui/RevisionSelectionPanel.java
@@ -0,0 +1,358 @@
+/*
+Copyright (c) 2007, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+package ise.plugin.svn.gui;
+
+import java.awt.Dimension;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.*;
+
+import ise.java.awt.KappaLayout;
+
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+
+/**
+ * A panel to let the user pick a revision by number or date.  HEAD and BASE
+ * are also offered as choices.
+ */
+public class RevisionSelectionPanel extends JPanel {
+
+    private ButtonGroup bg = null;
+    private JRadioButton head_rb = new JRadioButton( "HEAD" );
+    private JRadioButton base_rb = new JRadioButton( "BASE" );
+    private JRadioButton working_rb = new JRadioButton( "WORKING" );
+    private JRadioButton revision_number_rb = new JRadioButton( "Revision:" );
+    private JSpinner revision_number = null;
+    private JRadioButton date_rb = new JRadioButton( "Date:" );
+    private JSpinner date_spinner = null;
+
+
+    private String title;
+    private int layout = SwingConstants.VERTICAL;
+    private boolean showHead = true;
+    private boolean showWorking = false;
+    private boolean showBase = true;
+    private boolean showDate = true;
+    private boolean showNumber = true;
+
+    private SVNRevision defaultRevision;
+    private SVNRevision revision = SVNRevision.HEAD;
+
+
+    /**
+     * Revision selection panel with a vertical layout and doesn't show the
+     * "working" revision choice.
+     * @param title this panel is displayed in an etched border with a title.
+     */
+    public RevisionSelectionPanel( String title ) {
+        this.title = title;
+        init();
+    }
+
+    public void init() {
+        KappaLayout kl = new KappaLayout();
+        setLayout( kl );
+        switch ( layout ) {
+            case SwingConstants.HORIZONTAL:
+            case SwingConstants.VERTICAL:
+                break;
+            default:
+                layout = SwingConstants.VERTICAL;
+        }
+
+        setBorder( BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder(), title ) );
+
+        bg = new ButtonGroup();
+        bg.add( revision_number_rb );
+        bg.add( date_rb );
+        bg.add( head_rb );
+        bg.add( base_rb );
+        bg.add( working_rb );
+
+        if ( SVNRevision.BASE.equals( defaultRevision ) ) {
+            base_rb.setSelected( true );
+        }
+        else if ( showWorking && SVNRevision.WORKING.equals( defaultRevision ) ) {
+            working_rb.setSelected( true );
+        }
+        else {
+            head_rb.setSelected( true );
+        }
+
+        // set up the revision number chooser
+        revision_number = getRevisionChooser();
+
+        // set up date chooser
+        date_spinner = getDateChooser();
+
+        // add action listeners
+        ActionListener al = new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        revision_number.setEnabled ( RevisionSelectionPanel.this.revision_number_rb.isSelected() );
+                        date_spinner.setEnabled ( RevisionSelectionPanel.this.date_rb.isSelected() );
+                    }
+                };
+
+        revision_number_rb.addActionListener( al );
+        date_rb.addActionListener( al );
+        head_rb.addActionListener( al );
+        base_rb.addActionListener( al );
+        if ( showWorking ) {
+            working_rb.addActionListener( al );
+        }
+
+        revision_number_rb.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        RevisionSelectionPanel.this.revision_number.setEnabled( RevisionSelectionPanel.this.revision_number_rb.isSelected() );
+                        if ( RevisionSelectionPanel.this.revision_number.isEnabled() ) {
+                            Number number = ( Number ) revision_number.getValue();
+                            RevisionSelectionPanel.this.setRevision( SVNRevision.create( number.longValue() ) );
+                        }
+                    }
+                }
+                                            );
+        date_rb.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        RevisionSelectionPanel.this.date_spinner.setEnabled( RevisionSelectionPanel.this.date_rb.isSelected() );
+                        if ( RevisionSelectionPanel.this.date_spinner.isEnabled() ) {
+                            Date date = ( Date ) date_spinner.getValue();
+                            RevisionSelectionPanel.this.setRevision( SVNRevision.create( date ) );
+                        }
+                    }
+                }
+                                 );
+        head_rb.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        RevisionSelectionPanel.this.setRevision( SVNRevision.HEAD );
+                    }
+                }
+                                 );
+        base_rb.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent ae ) {
+                        RevisionSelectionPanel.this.setRevision( SVNRevision.BASE );
+                    }
+                }
+                                 );
+        if ( showWorking ) {
+            working_rb.addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent ae ) {
+                            RevisionSelectionPanel.this.setRevision( SVNRevision.WORKING );
+                        }
+                    }
+                                        );
+        }
+
+        revision_number.setEnabled( false );
+        date_spinner.setEnabled( false );
+
+        repaint();
+    }
+
+    private JSpinner getRevisionChooser() {
+        SpinnerNumberModel model = new SpinnerNumberModel();
+        model.setMinimum(0);
+        revision_number = new JSpinner(model);
+        revision_number.setPreferredSize( new Dimension( 150, revision_number.getPreferredSize().height ) );
+        revision_number.setEnabled(false);
+        revision_number.addChangeListener( new ChangeListener() {
+                    public void stateChanged( ChangeEvent ce ) {
+                        if ( RevisionSelectionPanel.this.revision_number.isEnabled() ) {
+                            Number number = ( Number ) revision_number.getValue();
+                            RevisionSelectionPanel.this.revision = SVNRevision.create( number.longValue() );
+                        }
+                    }
+                }
+                                         );
+        return revision_number;
+    }
+
+
+    private JSpinner getDateChooser() {
+        Calendar calendar = Calendar.getInstance();
+        Date initDate = calendar.getTime();
+        Date latestDate = calendar.getTime();
+        calendar.add( Calendar.YEAR, -10 );
+        Date earliestDate = calendar.getTime();
+        SpinnerDateModel model = new SpinnerDateModel( initDate, earliestDate, latestDate, Calendar.DAY_OF_MONTH );
+        date_spinner = new JSpinner( model );
+        JSpinner.DateEditor date_editor = new JSpinner.DateEditor( date_spinner, "dd MMM yyyy HH:mm" );
+        date_spinner.setEditor( date_editor );
+        date_spinner.addChangeListener( new ChangeListener() {
+                    public void stateChanged( ChangeEvent ce ) {
+                        if ( RevisionSelectionPanel.this.date_spinner.isEnabled() ) {
+                            Date date = ( Date ) date_spinner.getValue();
+                            RevisionSelectionPanel.this.setRevision( SVNRevision.create( date ) );
+                        }
+                    }
+                }
+                                      );
+        date_spinner.setPreferredSize( new Dimension( 150, date_spinner.getPreferredSize().height ) );
+        date_spinner.setEnabled(false);
+        return date_spinner;
+    }
+
+    public void setTitle( String title ) {
+        this.title = title;
+    }
+
+    public void setLayout( int layout ) {
+        switch ( layout ) {
+            case SwingConstants.HORIZONTAL:
+            case SwingConstants.VERTICAL:
+                this.layout = layout;
+                break;
+            default:
+                this.layout = SwingConstants.VERTICAL;
+        }
+        repaint();
+    }
+
+    public void setRevision( SVNRevision revision ) {
+        this.revision = revision;
+    }
+
+    public SVNRevision getRevision() {
+        return revision;
+    }
+
+    public void setShowHead( boolean b ) {
+        showHead = b;
+        if ( !b ) {
+            remove( head_rb );
+            repaint();
+        }
+    }
+
+    public void setShowWorking( boolean b ) {
+        showWorking = b;
+        if ( !b ) {
+            remove( working_rb );
+            repaint();
+        }
+    }
+
+    public void setShowBase( boolean b ) {
+        showBase = b;
+        if ( !b ) {
+            remove( base_rb );
+            repaint();
+        }
+    }
+
+    public void setShowDate( boolean b ) {
+        showDate = b;
+        if ( !b ) {
+            remove( date_rb );
+            remove( date_spinner );
+            repaint();
+        }
+    }
+
+    public void setShowNumber( boolean b ) {
+        showNumber = b;
+        if ( !b ) {
+            remove( revision_number_rb );
+            remove( revision_number );
+            repaint();
+        }
+    }
+
+    @Override
+    public void setEnabled( boolean b ) {
+        super.setEnabled( b );
+    }
+
+    @Override
+    public void repaint() {
+        removeAll();
+
+        KappaLayout kl = new KappaLayout();
+        if ( layout == SwingConstants.HORIZONTAL ) {
+            add( "0, 0, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 6, true ) );
+            if ( showHead ) {
+                add( "0, 1, 1, 1, W, , 3", head_rb );
+            }
+            if ( showBase ) {
+                add( "0, 2, 1, 1, W, , 3", base_rb );
+            }
+            if ( showWorking ) {
+                add( "0, 3, 1, 1, W, , 3", working_rb );
+            }
+            add( "1, 1, 1, 1, 0,  , 0", KappaLayout.createHorizontalStrut( 21, true ) );
+            if ( showNumber ) {
+                add( "2, 1, 1, 1, W, , 3", revision_number_rb );
+                add( "3, 1, 1, 1, W, , 3", getRevisionChooser() );
+            }
+            if ( showDate ) {
+                add( "2, 2, 1, 1, W, , 3", date_rb );
+                add( "3, 2, 1, 1, W, , 3", getDateChooser() );
+            }
+        }
+        else {
+            add( "0, 0, 1, 1, 0,  , 0", KappaLayout.createVerticalStrut( 6, true ) );
+            if ( showHead ) {
+                add( "0, 1, 2, 1, W, , 3", head_rb );
+            }
+            if ( showBase ) {
+                add( "0, 2, 2, 1, W, , 3", base_rb );
+            }
+            if ( showWorking ) {
+                add( "0, 3, 2, 1, W, , 3", working_rb );
+            }
+            if ( showNumber ) {
+                add( "0, 4, 1, 1, W, w, 3", revision_number_rb );
+                add( "1, 4, 1, 1, W, , 3", getRevisionChooser() );
+            }
+            if ( showDate ) {
+                add( "0, 5, 1, 1, W, , 3", date_rb );
+                add( "1, 5, 1, 1, W, , 3", getDateChooser() );
+            }
+        }
+        super.repaint();
+    }
+
+    // for testing
+    public static void main ( String[] args ) {
+        RevisionSelectionPanel panel = new RevisionSelectionPanel( "some title" );
+        //panel.setShowWorking( true );
+        //panel.setShowHead( true );
+        //panel.setShowBase( false );
+        //panel.setLayout( SwingConstants.HORIZONTAL );
+        JFrame frame = new JFrame();
+        frame.setContentPane( panel );
+        frame.pack();
+        frame.setVisible( true );
+        SVNRevision revision = panel.getRevision();
+        System.out.println("+++++ RSP, revision = " + revision);
+    }
+}
---
+++ plugins/SVNPlugin/tags/0.9.1/src/ise/plugin/svn/pv/SVNAction.java
@@ -0,0 +1,193 @@
+/*
+Copyright (c) 2007, Dale Anson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package ise.plugin.svn.pv;
+
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.tree.TreePath;
+import projectviewer.action.Action;
+import projectviewer.vpt.VPTNode;
+import ise.plugin.svn.PVHelper;
+import ise.plugin.svn.command.*;
+import ise.plugin.svn.library.PasswordHandler;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
+
+/**
+ * ProjectViewer Action to be added to the PV context menu.  This class serves
+ * as the menu for a pull-out menu containing the subversion commands.
+ */
+public class SVNAction extends projectviewer.action.Action {
+
+    public final static String PREFIX = "ise.plugin.svn.pv.";
+
+    private JMenu menu = null;
+
+    public SVNAction() {
+        // set up the menu to be added to Project Viewer's context menu
+        menu = new JMenu( "Subversion" );
+
+        // Each subversion command to be added to the context
+        // menu has 2 properties, a label and a command.  The properties have a numeric
+        // suffix, the commands are added to the menu in the order of the suffix.  The
+        // label property is the displayed name of the command, e.g. "Commit", and the
+        // code property is the fully qualified classname of a class in the
+        // ise.plugin.svn.action package.
+        String pbase = "ise.plugin.svn.action.";
+        for ( int i = 1; i < 100; i++ ) {
+            String label = jEdit.getProperty( pbase + "label." + i );
+            if ( label == null ) {
+                break;
+            }
+            if ( label.equals("-")) {
+                menu.addSeparator();
+                continue;
+            }
+            String classname = jEdit.getProperty( pbase + "code." + i );
+            if ( classname == null ) {
+                continue;
+            }
+            JMenuItem item = null;
+            try {
+                NodeActor action = ( NodeActor ) Class.forName( classname ).newInstance();
+                item = new JMenuItem( label );
+                item.addActionListener( ( ActionListener ) action );
+                menu.add( item );
+            }
+            catch ( Exception e ) {
+                // class not found or instantiation exception, don't worry
+                // about it, assume it's a typo
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+
+    // this will be displayed in the PV context menu
+    public String getText() {
+        return "Subversion";
+    }
+
+    // returns the menu 'Subversion' with a pull-out submenu containing the
+    // subversion commands.
+    public JComponent getMenuItem() {
+        return menu;
+    }
+
+    // called by ProjectViewer to let us know the currently selected node in
+    // the PV tree.  This method updates the various action listeners that
+    // execute the subversion commands so they know the current node and can
+    // act accordingly.
+    public void prepareForNode( VPTNode node ) {
+        View view = viewer.getView();
+        String project_name = PVHelper.getProjectName( view );
+        String project_root = PVHelper.getProjectRoot( view );
+        String username = jEdit.getProperty( PREFIX + project_name + ".username" );
+        String password = jEdit.getProperty( PREFIX + project_name + ".password" );
+        if ( password != null && password.length() > 0 ) {
+            try {
+                PasswordHandler ph = new PasswordHandler();
+                password = ph.decrypt( password );
+            }
+            catch ( Exception e ) {
+                password = "";
+            }
+        }
+        for ( int i = 0; i < menu.getItemCount(); i++ ) {
+            try {
+                JMenuItem actor = ( JMenuItem ) menu.getItem( i );
+                if (actor == null) {
+                    continue;
+                }
+                ActionListener[] listeners = actor.getActionListeners();
+                for ( ActionListener al : listeners ) {
+                    if ( al instanceof NodeActor ) {
+                        ( ( NodeActor ) al ).prepareForNode( getSelectedNodes(), view, project_root, username, password );
+                    }
+                }
+            }
+            catch ( ClassCastException e ) {
+                // ignored, move on
+            }
+        }
+    }
+
+    public void actionPerformed( ActionEvent ae ) {
+        // does nothing, this is the top of a pull out menu so has no specific
+        // action other than to display the pull out.
+    }
+
+    private List<VPTNode> getSelectedNodes() {
+        List<VPTNode> list = new ArrayList<VPTNode>();
+        JTree tree = viewer.getCurrentTree();
+
+        switch ( tree.getSelectionCount() ) {
+            case 0:
+                // no Selection, shouldn't happen, but just in case...
+                break;
+
+            case 1: {
+                    // single selection
+                    list.add( ( VPTNode ) tree.getLastSelectedPathComponent() );
+                    break;
+                }
+
+            default: {
+                    list = getSelectedArtifacts( tree.getSelectionPaths() );
+                    break;
+                }
+        }
+        return list;
+    }
+
+    /**
+     *  Receives a collection of TreePath objects and returns the underlying
+     *  objects selected, removing a child when its parent has also been
+     *  selected.
+     */
+    private List<VPTNode> getSelectedArtifacts( TreePath[] paths ) {
+        TreePath last = null;
+        List<VPTNode> objs = new ArrayList<VPTNode>();
+
+        for ( int i = 0; i < paths.length; i++ ) {
+            if ( last != null && !last.isDescendant( paths[ i ] ) ) {
+                last = null;
+            }
+
+            if ( last == null ) {
+                last = paths[ i ];
+                objs.add( ( VPTNode ) paths[ i ].getLastPathComponent() );
+            }
+        }
+
+        return objs;
+    }
+
+}
