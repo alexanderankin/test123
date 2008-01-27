7dc9396dd0e85923519dd6f11a55930ad8d95975
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-01-27 07:00:42 +0000

    Formatted code with Jalopy.

--- plugins/LucenePlugin/trunk/src/java/com/townsfolkdesigns/lucene/util/FileUtils.java
+++ plugins/LucenePlugin/trunk/src/java/com/townsfolkdesigns/lucene/util/FileUtils.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2008 Eric Berry <elberry@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,8 +22,10 @@
 package com.townsfolkdesigns.lucene.util;
 
 import java.io.File;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 /**
  *
@@ -37,9 +39,11 @@ public abstract class FileUtils {
       String fileName = file.getName();
       String extension = null;
       Matcher extensionMatcher = FILE_EXTENSION_PATTERN.matcher(fileName);
+
       if (extensionMatcher.matches()) {
          extension = extensionMatcher.group(1);
       }
+
       return extension;
    }
 }
