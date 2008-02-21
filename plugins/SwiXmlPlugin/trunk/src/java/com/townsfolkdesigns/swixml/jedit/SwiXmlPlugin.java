0b99ea5d47d8c6bfb42040627a100389cfa8b172
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-02-21 22:44:49 +0000

    Added some logic to make the test frame better. It centers itself now, and takes into consideration the window add-ons (top bar, sides and bottom bar)when calculating size.

--- plugins/SwiXmlPlugin/trunk/src/java/com/townsfolkdesigns/swixml/jedit/SwiXmlPlugin.java
+++ plugins/SwiXmlPlugin/trunk/src/java/com/townsfolkdesigns/swixml/jedit/SwiXmlPlugin.java
@@ -1,11 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2008 Eric Berry <elberry@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.townsfolkdesigns.swixml.jedit;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.File;
 import java.io.StringReader;
 import javax.swing.JFileChooser;
@@ -23,6 +41,8 @@ import org.swixml.SwingEngine;
  * @author eberry
  */
 public class SwiXmlPlugin extends EBPlugin {
+   
+   public static final Dimension DEFAULT_CONTAINER_SIZE = new Dimension(640, 480);
 
    public SwiXmlPlugin() {
    }
@@ -62,10 +82,32 @@ public class SwiXmlPlugin extends EBPlugin {
       // if the container isn't an instance of JFrame, put it in one.
       if ((container instanceof JFrame) == false) {
          Dimension containerSize = container.getSize();
+         if(containerSize == null) {
+            containerSize = DEFAULT_CONTAINER_SIZE;
+         } 
+         if (containerSize.width == 0) {
+            containerSize.width = DEFAULT_CONTAINER_SIZE.width;
+         } 
+         if (containerSize.height == 0) {
+            containerSize.height = DEFAULT_CONTAINER_SIZE.height;
+         }
+         Log.log(Log.DEBUG, SwiXmlPlugin.class, "Container Size - w: " + containerSize.width + " | h: " + containerSize.height);
          JFrame frame = new JFrame("Test Frame");
          frame.setSize(containerSize);
          frame.setContentPane(container);
+         frame.pack();
+         int diffW = frame.getWidth() - container.getWidth();
+         int diffH = frame.getHeight() - container.getHeight();
+         Log.log(Log.DEBUG, SwiXmlPlugin.class, "Window Size Differences - w: " + diffW + " | h: " + diffH);
+         containerSize.width += diffW;
+         containerSize.height += diffH;
+         frame.setSize(containerSize);
          container = frame;
+         Toolkit toolkit = Toolkit.getDefaultToolkit();
+         Dimension screenSize = toolkit.getScreenSize();
+         int frameX = (screenSize.width - containerSize.width) / 2;
+         int frameY = (screenSize.height - containerSize.height) / 2;
+         frame.setLocation(frameX, frameY);
       }
       container.setVisible(true);
    }
