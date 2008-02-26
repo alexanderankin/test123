b07d5abcdfa7f1ce4c79d0a16f4def05d6e4ca5b
eyebex <eyebex@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-02-26 20:55:10 +0000

    Minor indenting fixes.

--- jEditLauncher/trunk/src/jecfg.cpp
+++ jEditLauncher/trunk/src/jecfg.cpp
@@ -7,9 +7,9 @@ CAppModule _Module;
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
 {
     HRESULT hRes = ::CoInitialize(NULL);
-// If you are running on NT 4.0 or higher you can use the following call instead to
-// make the EXE free threaded. This means that calls come in on a random RPC thread.
-//  HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    // If you are running on NT 4.0 or higher you can use the following call instead to
+    // make the EXE free threaded. This means that calls come in on a random RPC thread.
+    //HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
     ATLASSERT(SUCCEEDED(hRes));
 
     // this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
