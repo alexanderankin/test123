cb11973ce4a17a69123dfec4e0fe6831c222833a
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-04-12 02:11:26 +0000

    [ 1938179 ] Prevent log spew from Activator plugin on reloading a plugin -- Dale Anson

--- plugins/Activator/trunk/src/main/activator/Reload.java
+++ plugins/Activator/trunk/src/main/activator/Reload.java
@@ -10,41 +10,50 @@ import org.gjt.sp.util.Log;
 
 import common.gui.actions.CustomAction;
 
-class Reload extends CustomAction {
+class Reload extends CustomAction
+{
 	private String name;
 	private PluginList.Plugin plugin;
 	private PluginJAR jar;
-	
-	public Reload(PluginJAR pluginJAR, String dispName) {
+
+	public Reload(PluginJAR pluginJAR, String dispName)
+	{
 		super(dispName);
 		name = dispName;
 		jar = pluginJAR;
 		setToolTipText("Click to reload: " + name);
 	}
-	
-	public Reload(PluginList.Plugin plugin) {
+
+	public Reload(PluginList.Plugin plugin)
+	{
 		super(plugin.getJAR().getPlugin().getClassName());
 		this.plugin = plugin;
 		jar = plugin.getJAR();
 		name = jar.getPlugin().getClassName();
 		setToolTipText("Click to reload: " + name);
-//		setName(jar.getPlugin().getClassName());
-		
+		// setName(jar.getPlugin().getClassName());
 	}
-	
-	public void actionPerformed(ActionEvent event) {
 
-		Log.log(Log.DEBUG,this,"Reloading "+jar);
+	public void actionPerformed(ActionEvent event)
+	{
+		Log.log(Log.DEBUG, this, "Reloading " + jar);
 		Stack<String> unloaded = PluginManager.unloadPluginJAR(jar);
+		if (unloaded.empty())
+		{
+			return;
+		}
 		Set<String> reloaded = new HashSet<String>();
 		jar = null;
 		String path = null;
-		do {
+		do
+		{
 			path = unloaded.pop();
-			if (path != null && !reloaded.contains(path)) {
+			if (path != null && !reloaded.contains(path))
+			{
 				PluginManager.loadPluginJAR(path);
 				reloaded.add(path);
 			}
-		} while (path != null);
+		}
+		while (path != null && !unloaded.empty());
 	}
 }
