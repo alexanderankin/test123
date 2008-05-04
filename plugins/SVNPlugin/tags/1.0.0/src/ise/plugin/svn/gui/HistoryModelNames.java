2ef5b32e48f477407158a5b466f3438e7ec722b0
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-05-04 22:19:48 +0000

    premature tag

--- plugins/SVNPlugin/tags/1.0.0/src/ise/plugin/svn/gui/HistoryModelNames.java
+++
@@ -1,8 +0,0 @@
-package ise.plugin.svn.gui;
-
-public class HistoryModelNames {
-    public static final String REPOSITORY_NAME = "svnplugin.repositoryname";
-    public static final String URL = "svnplugin.url";
-    public static final String USERNAME = "svnplugin.username";
-    public static final String PATH = "svnplugin.path";
-}
