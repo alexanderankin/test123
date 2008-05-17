df9fa2530f50d51d2719ef3dd8df3a602629a541
vanza <vanza@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-05-17 05:49:50 +0000

    Fix NPE when re-importing files from directories not under the project root.

--- plugins/ProjectViewer/trunk/projectviewer/importer/RootImporter.java
+++ plugins/ProjectViewer/trunk/projectviewer/importer/RootImporter.java
@@ -102,6 +102,7 @@ public class RootImporter extends FileImporter {
 		if (!id.isApproved()) {
 			return null;
 		}
+		fnf = id.getImportFilter();
 
 		String state = null;
 		if (viewer != null) {
