f3eca554dba98b4ce2ac0629a8aec07b557db394
Eric Le Lay <222469+elelay@users.noreply.github.com>
2008-05-27 16:55:43 +0000

    - adding missing file Validator.java - adding copyright notice and license

--- plugins/SpellCheck/trunk/src/main/cswilly/jeditPlugins/spell/SpellCheckOptionPane.java
+++ plugins/SpellCheck/trunk/src/main/cswilly/jeditPlugins/spell/SpellCheckOptionPane.java
@@ -4,6 +4,7 @@
  * $Author$
  *
  * Copyright (C) 2001 C. Scott Willy
+ * Copyright (C) 2008 Eric Le Lay
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
