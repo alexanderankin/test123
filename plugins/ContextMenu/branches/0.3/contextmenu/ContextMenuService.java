dbb7fdbda6aedf95286e98f63d308fde5ea48138
oswaldcz <oswaldcz@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-06-29 19:34:11 +0000

    minor changes

--- plugins/ContextMenu/branches/0.3/contextmenu/ContextMenuService.java
+++ plugins/ContextMenu/branches/0.3/contextmenu/ContextMenuService.java
@@ -11,6 +11,9 @@ import org.gjt.sp.util.Log;
 
 public class ContextMenuService extends DynamicContextMenuService{
 	public JMenuItem createMenu(JEditTextArea ta) {
+		if (ta == null || ta.getBuffer() == null) {
+			return null;
+		}
 		String mode = ta.getBuffer().getMode().getName();
 		JMenuItem item = ContextMenuPlugin.getMenuForMode(mode, ContextMenuPlugin.CACHE_POPUP);
 		if (item == null) {
