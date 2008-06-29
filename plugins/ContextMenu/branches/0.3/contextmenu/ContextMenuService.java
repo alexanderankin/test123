bfaed0bc4145b6ee0c2d3f31a29136c88a192817
oswaldcz <oswaldcz@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-06-29 19:08:11 +0000

    Using the new DynamicContextMenuService API for creating pop-up menu

---
+++ plugins/ContextMenu/branches/0.3/contextmenu/ContextMenuService.java
@@ -0,0 +1,21 @@
+
+
+package contextmenu;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.gui.DynamicContextMenuService;
+import javax.swing.JMenuItem;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.util.Log;
+
+
+public class ContextMenuService extends DynamicContextMenuService{
+	public JMenuItem createMenu(JEditTextArea ta) {
+		String mode = ta.getBuffer().getMode().getName();
+		JMenuItem item = ContextMenuPlugin.getMenuForMode(mode, ContextMenuPlugin.CACHE_POPUP);
+		if (item == null) {
+			item = ContextMenuPlugin.getCustomizeModeItem(jEdit.getProperty("contextmenu.customize-mode"));
+		}
+		return item;
+	}
+}
