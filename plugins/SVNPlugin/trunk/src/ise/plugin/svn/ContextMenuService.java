778bbc2e3bbfa084a56de0bdfce8bbce1cbbcaec
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-06-30 03:39:11 +0000

    updates for DynamicContextMenuService.

--- plugins/SVNPlugin/trunk/src/ise/plugin/svn/ContextMenuService.java
+++ plugins/SVNPlugin/trunk/src/ise/plugin/svn/ContextMenuService.java
@@ -1,8 +1,10 @@
 package ise.plugin.svn;
 
+import java.util.HashMap;
 import javax.swing.JMenuItem;
 import org.gjt.sp.jedit.gui.DynamicContextMenuService;
 import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
 
 import ise.plugin.svn.gui.TextAreaContextMenu;
@@ -15,10 +17,18 @@ import ise.plugin.svn.gui.TextAreaContextMenu;
  * add the SVN context menu, this class is declared in the services.xml file.
  */
 public class ContextMenuService extends DynamicContextMenuService {
+
+    // cache menu for quicker response
+    private HashMap<View, TextAreaContextMenu> menus = new HashMap<View, TextAreaContextMenu>();
+
+    // context menu is per View
     public JMenuItem createMenu( JEditTextArea textArea ) {
-        if (textArea != null) {
-            return new TextAreaContextMenu( textArea.getView() );
+        View view = textArea.getView();
+        TextAreaContextMenu menu = menus.get(view);
+        if (menu == null) {
+            menu = textArea == null ? new TextAreaContextMenu(jEdit.getFirstView()) : new TextAreaContextMenu(view);
+            menus.put(view, menu);
         }
-        return new TextAreaContextMenu( jEdit.getFirstView());
+        return menu;
     }
 }
