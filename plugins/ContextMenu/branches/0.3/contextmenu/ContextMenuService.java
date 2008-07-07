5e32b6273068315395a7fa356d92d8ab1639dec6
oswaldcz <oswaldcz@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-07-07 09:36:12 +0000

    updated DynamicContextMenuService service

--- plugins/ContextMenu/branches/0.3/contextmenu/ContextMenuService.java
+++ plugins/ContextMenu/branches/0.3/contextmenu/ContextMenuService.java
@@ -7,18 +7,25 @@ import org.gjt.sp.jedit.gui.DynamicContextMenuService;
 import javax.swing.JMenuItem;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
 import org.gjt.sp.util.Log;
+import java.awt.event.MouseEvent;
 
 
-public class ContextMenuService extends DynamicContextMenuService{
-	public JMenuItem createMenu(JEditTextArea ta) {
-		if (ta == null || ta.getBuffer() == null) {
+public class ContextMenuService extends DynamicContextMenuService {
+	
+	public JMenuItem[] createMenu(JEditTextArea ta, MouseEvent evt) {
+		if (!jEdit.getBooleanProperty("contextmenu.in-popup")
+			|| ta == null || ta.getBuffer() == null)
+		{
 			return null;
 		}
+		JMenuItem[] items = new JMenuItem[1];
 		String mode = ta.getBuffer().getMode().getName();
 		JMenuItem item = ContextMenuPlugin.getMenuForMode(mode, ContextMenuPlugin.CACHE_POPUP);
 		if (item == null) {
 			item = ContextMenuPlugin.getCustomizeModeItem(jEdit.getProperty("contextmenu.customize-mode"));
 		}
-		return item;
+		items[0] = item;
+		return items;
 	}
+	
 }
