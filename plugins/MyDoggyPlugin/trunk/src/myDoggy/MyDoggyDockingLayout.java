42992e4fa0324038f1e0e36b91ee847e4820aa83
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-02 12:37:45 +0000

    Started development of MyDoggy plugin

---
+++ plugins/MyDoggyPlugin/trunk/src/myDoggy/MyDoggyDockingLayout.java
@@ -0,0 +1,34 @@
+package myDoggy;
+
+import java.io.IOException;
+
+import org.gjt.sp.jedit.SettingsXML.Saver;
+import org.gjt.sp.jedit.gui.DockableWindowManagerBase.DockingLayout;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MyDoggyDockingLayout extends DockingLayout {
+
+	@Override
+	public DefaultHandler getPerspectiveHandler() {
+		return new DefaultHandler();
+	}
+
+	@Override
+	public void move(int dx, int dy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void savePerspective(Saver out, String lineSep) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPlainView(boolean plain) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
