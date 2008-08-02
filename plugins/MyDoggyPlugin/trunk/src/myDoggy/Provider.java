81588ff7c9efea6686971820f44764cab50ccb2c
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-02 22:05:39 +0000

    Make split/unsplit completely independent of the docking framework by hiding it in a main panel handled exclusively by the view.

--- plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
+++ plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
@@ -1,5 +1,7 @@
 package myDoggy;
 
+import javax.swing.JPanel;
+
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.View.ViewConfig;
 import org.gjt.sp.jedit.gui.DockableWindowFactory;
@@ -9,14 +11,12 @@ import org.gjt.sp.jedit.gui.DockableWindowManagerBase.DockingLayout;
 
 public class Provider implements IDockingFrameworkProvider {
 
-	@Override
 	public DockableWindowManagerBase create(View view,
 			DockableWindowFactory instance, ViewConfig config)
 	{
 		return new MyDoggyWindowManager(view, instance, config);
 	}
 
-	@Override
 	public DockingLayout createDockingLayout() {
 		return new MyDoggyDockingLayout();
 	}
