4a53e07f6fa3927b17ba43c700fc077f6f477754
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-04 07:30:41 +0000

    Renamed DockableWindowManagerBase to DockableWindowManager, to maintain backward compatibility of source code with macros and plugins.

--- plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
+++ plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
@@ -5,13 +5,13 @@ import javax.swing.JPanel;
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.View.ViewConfig;
 import org.gjt.sp.jedit.gui.DockableWindowFactory;
-import org.gjt.sp.jedit.gui.DockableWindowManagerBase;
+import org.gjt.sp.jedit.gui.DockableWindowManager;
 import org.gjt.sp.jedit.gui.IDockingFrameworkProvider;
-import org.gjt.sp.jedit.gui.DockableWindowManagerBase.DockingLayout;
+import org.gjt.sp.jedit.gui.DockableWindowManager.DockingLayout;
 
 public class Provider implements IDockingFrameworkProvider {
 
-	public DockableWindowManagerBase create(View view,
+	public DockableWindowManager create(View view,
 			DockableWindowFactory instance, ViewConfig config)
 	{
 		return new MyDoggyWindowManager(view, instance, config);
