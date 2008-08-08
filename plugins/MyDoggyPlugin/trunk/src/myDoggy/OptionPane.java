9409875f9959bd5960a2d73862904af251757b7a
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-08 04:11:19 +0000

    Added an option pane to allow changing the push away mode.

---
+++ plugins/MyDoggyPlugin/trunk/src/myDoggy/OptionPane.java
@@ -0,0 +1,45 @@
+package myDoggy;
+
+import javax.swing.JComboBox;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+import org.noos.xing.mydoggy.PushAwayMode;
+
+@SuppressWarnings("serial")
+public class OptionPane extends AbstractOptionPane {
+
+	private static final String PREFIX = "options.mydoggy.";
+	private static final String PUSH_AWAY_MODE_PROP = PREFIX + "pushAwayMode";
+	JComboBox pushAwayMode;
+	
+	public OptionPane() {
+		super("mydoggy");
+	}
+
+	public static PushAwayMode getPushAwayModeProp() {
+		String selected = jEdit.getProperty(PUSH_AWAY_MODE_PROP,
+				PushAwayMode.HORIZONTAL.toString());
+		return PushAwayMode.valueOf(selected);
+	}
+	
+	@Override
+	protected void _init() {
+		String [] pushAwayModes = new String[] {
+				PushAwayMode.HORIZONTAL.toString(),
+				PushAwayMode.VERTICAL.toString(),
+				PushAwayMode.ANTICLOCKWISE.toString(),
+				PushAwayMode.MOST_RECENT.toString()
+		};
+		pushAwayMode = new JComboBox(pushAwayModes);
+		pushAwayMode.setSelectedItem(getPushAwayModeProp().toString());
+		addComponent("Push away mode:", pushAwayMode);
+	}
+
+	@Override
+	protected void _save() {
+		jEdit.setProperty(PUSH_AWAY_MODE_PROP, (String)pushAwayMode.getSelectedItem());
+		jEdit.propertiesChanged();
+	}
+
+}
