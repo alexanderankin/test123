b78a70e71355dc16a1bac1542376580cd09a230a
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-12 00:29:07 +0000

    Re-tagging release 0.1

---
+++ plugins/Qt4jEdit/tags/release-0.1/qt4jedit/Qt4jEditOptions.java
@@ -0,0 +1,46 @@
+package qt4jedit;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+
+public class Qt4jEditOptions extends AbstractOptionPane {
+	
+	public JTextField pathToAssistantField;
+	String oldPath;
+	
+	public Qt4jEditOptions() {
+		super("Qt4jEdit");
+	}
+
+	protected void _init() {
+		oldPath = jEdit.getProperty("qt4jedit.path-to-assistant", "assistant");
+		pathToAssistantField = new JTextField(oldPath);
+		addComponent(jEdit.getProperty("options.qt4jedit.path-to-assistant"), pathToAssistantField);
+        
+        JTextArea info = new JTextArea();
+		info.setLineWrap(true);
+		info.setWrapStyleWord(true);
+		info.setOpaque(false);
+		String text = jEdit.getProperty("options.qt4jedit.assistant.info");
+		info.append(text);
+		info.setEditable(false);
+		 
+        addComponent(info, GridBagConstraints.BOTH);
+        
+	}
+
+	protected void _save() {
+		String newPath = pathToAssistantField.getText();
+		if (newPath != oldPath) {
+			jEdit.setProperty("qt4jedit.path-to-assistant", newPath);
+			Qt4jEditPlugin.instance().updateCommand();
+		}
+	}
+}
+
