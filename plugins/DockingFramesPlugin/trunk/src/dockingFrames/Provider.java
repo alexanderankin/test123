aa39a677411d6c412a7929e4f1fb06632661ddfb
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-12 03:21:49 +0000

    First version of DockingFramesPlugin. Still don't know how to use it correctly...

---
+++ plugins/DockingFramesPlugin/trunk/src/dockingFrames/Provider.java
@@ -0,0 +1,22 @@
+package dockingFrames;
+
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.View.ViewConfig;
+import org.gjt.sp.jedit.gui.DockableWindowFactory;
+import org.gjt.sp.jedit.gui.DockableWindowManager;
+import org.gjt.sp.jedit.gui.IDockingFrameworkProvider;
+import org.gjt.sp.jedit.gui.DockableWindowManager.DockingLayout;
+
+public class Provider implements IDockingFrameworkProvider {
+
+	public DockableWindowManager create(View view,
+			DockableWindowFactory instance, ViewConfig config)
+	{
+		return new WindowManager(view, instance, config);
+	}
+
+	public DockingLayout createDockingLayout() {
+		return new dockingFrames.DockingLayout();
+	}
+
+}
