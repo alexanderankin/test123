5a6e43de11cb42a5153692b7ea8746bb44474ac3
keeleyt83 <keeleyt83@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-08-14 06:22:52 +0000

    Reverted back to 12474.

--- plugins/FindFile/tags/rel-1-0/src/findfile/SearchOptions.java
+++ plugins/FindFile/tags/rel-1-0/src/findfile/SearchOptions.java
@@ -1,36 +1,42 @@
+package findfile;
+
 /**
- * SearchOptions.java - A set of options to define the file search to be performed.
- * :tabSize=8:indentSize=8:noTabs=false:
- * :folding=explicit:collapseFolds=1:
- *
+ * A set of options to define the file search to be performed.
  * @author Nicholas O'Leary
  * @version $Id: SearchOptions.java,v 1.1.1.1 2003/11/20 17:19:14 olearyni Exp $
  */
-
-package findfile;
-
-public class SearchOptions {
-    public int sortOrder;
-
-    /** The root path for the search. */
-    public String path;
-
-    /** The file filter to apply. */
-    public String filter;
-
-    /** Whether to recurse into subdirectories. */
-    public boolean recursive;
-
-    /** Whether to open the files automatically. */
-    public boolean openResults;
-
-    private int resultCount = 0;
-
-    public void setResultCount(int i) {
-	resultCount = i;
-    }
-
-    @Override public String toString() {
-	return filter + " in " + path + " (" + resultCount + " result" + ((resultCount != 1) ? "s" : "") + ")";
-    }
+public class SearchOptions
+{
+   public int sortOrder;
+   
+   /** The root path for the search. */
+   public String path;
+   
+   /** The file filter to apply. */
+   public String filter;
+   
+   /** Whether to recurse into subdirectories. */
+   public boolean recursive;
+   
+   /** Whether to open the files automatically. */
+   public boolean openResults;
+   
+   private int resultCount = 0;
+   
+   public void setResultCount(int i)
+   {
+      resultCount = i;
+   }
+   
+   public String toString()
+   {
+      return filter +
+             " in " +
+             path +
+             " (" + 
+             resultCount + 
+             " result" + 
+             ((resultCount!=1)?"s":"") + 
+             ")";
+   }
 }
