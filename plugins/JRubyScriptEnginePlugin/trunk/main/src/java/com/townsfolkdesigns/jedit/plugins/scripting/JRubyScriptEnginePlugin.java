3bade12fbc411291ca6ff383065423c8d839e392
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-09-01 08:44:48 +0000

    copying

---
+++ plugins/JRubyScriptEnginePlugin/trunk/main/src/java/com/townsfolkdesigns/jedit/plugins/scripting/JRubyScriptEnginePlugin.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2008 TownsfolkDesigns.com
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package com.townsfolkdesigns.jedit.plugins.scripting;
+
+import com.sun.script.jruby.JRubyScriptEngine;
+import com.sun.script.jruby.JRubyScriptEngineFactory;
+
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.Mode;
+import org.gjt.sp.jedit.jEdit;
+
+
+/**
+ * The JRubyScriptEnginePlugin provides the Groovy ScriptEngine to jEdit plugins.
+ * @author elberry
+ */
+public class JRubyScriptEnginePlugin extends EditPlugin implements ScriptEngineService {
+
+   private JRubyScriptEngineFactory factory = new JRubyScriptEngineFactory();
+
+   public Class getEngineFactoryClass() {
+		JRubyScriptEngine engine = (JRubyScriptEngine)factory.getScriptEngine();
+      return factory.getClass();
+   }
+
+   public Mode getMode() {
+      return jEdit.getMode("ruby");
+   }
+
+
+   @Override
+   public void start() {
+   }
+
+   @Override
+   public void stop() {
+   }
+
+}
