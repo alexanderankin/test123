c78a294ec7e1d49ba10a4d04de6bf401b08fe89e
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-09-01 08:35:14 +0000

    copying

---
+++ plugins/JavascriptScriptEnginePlugin/trunk/test/resources/test.js
@@ -0,0 +1,22 @@
+var author = 'Eric Berry';
+if (view == null) {
+	throw "view is null";
+}
+if (buffer == null) {
+	throw "buffer is null";
+}
+if (editPane == null) {
+	throw "editPane is null";
+}
+if (textArea == null) {
+	throw "textArea is null";
+}
+if (wm == null) {
+	throw "wm is null";
+}
+if (scriptPath == null) {
+	throw "scriptPath is null";
+}
+var text = 'Author: ' + author + ' | Script Path: ' + scriptPath;
+text;
+
