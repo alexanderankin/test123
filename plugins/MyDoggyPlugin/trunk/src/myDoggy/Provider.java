4eeec353b342f5c7ea11985754d25f34942ddd29
Alan Ezust <1688991+ezust@users.noreply.github.com>
2008-09-03 20:07:11 +0000

    Small fixes.

--- plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
+++ plugins/MyDoggyPlugin/trunk/src/myDoggy/Provider.java
@@ -4,10 +4,10 @@ import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.View.ViewConfig;
 import org.gjt.sp.jedit.gui.DockableWindowFactory;
 import org.gjt.sp.jedit.gui.DockableWindowManager;
-import org.gjt.sp.jedit.gui.IDockingFrameworkProvider;
+import org.gjt.sp.jedit.gui.DockingFrameworkProvider;
 import org.gjt.sp.jedit.gui.DockableWindowManager.DockingLayout;
 
-public class Provider implements IDockingFrameworkProvider {
+public class Provider implements DockingFrameworkProvider {
 
 	public DockableWindowManager create(View view,
 			DockableWindowFactory instance, ViewConfig config)
