7329180629b984f06355c19d05208e7ee278a8e4
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-09-14 17:17:52 +0000

    Tagging 0.3

---
+++ plugins/ContextMenu/tags/release-0.3/contextmenu/ContextMenuService.java
@@ -0,0 +1,31 @@
+
+
+package contextmenu;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.gui.DynamicContextMenuService;
+import javax.swing.JMenuItem;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.util.Log;
+import java.awt.event.MouseEvent;
+
+
+public class ContextMenuService extends DynamicContextMenuService {
+	
+	public JMenuItem[] createMenu(JEditTextArea ta, MouseEvent evt) {
+		if (!jEdit.getBooleanProperty("contextmenu.in-popup")
+			|| ta == null || ta.getBuffer() == null)
+		{
+			return null;
+		}
+		JMenuItem[] items = new JMenuItem[1];
+		String mode = ta.getBuffer().getMode().getName();
+		JMenuItem item = ContextMenuPlugin.getMenuForMode(mode, ContextMenuPlugin.CACHE_POPUP);
+		if (item == null) {
+			item = ContextMenuPlugin.getCustomizeModeItem(jEdit.getProperty("contextmenu.customize-mode"));
+		}
+		items[0] = item;
+		return items;
+	}
+	
+}
