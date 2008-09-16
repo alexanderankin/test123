37d9f3171d2e760feef0e2aac1e6bc2390898119
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-09-16 23:14:02 +0000

    Updated the basic scripts a bit

--- tests/Marathon/TestCases/file.py
+++ tests/Marathon/TestCases/file.py
@@ -3,26 +3,32 @@ useFixture(default)
 def test():
     java_recorded_version = '1.6.0_07'
 
-    # Test File->Open
+    inputFile = r'd:\jedit\tests\marathon\testcases\file.py'
     if window(r'/jEdit - .*'):
+        select_menu('File>>Close All')
+        # Test File->Open
         click('document-open')
 
         if window('File Browser'):
-            select('File name', r'd:\jedit\tests\marathon\testcases\file.py')
+            select('File name', inputFile)
             click('Open')
         close()
     close()
 
     if window(r'/jEdit - .*[/\\]?file.py'):
         s = getComponent('JEditTextArea').getText()
-        assert s.find('if window(r\'/jEdit - .*[/\\\\]?file.py\'):') >= 0
+        l1 = s.splitlines(true)
+        l2 = open(inputFile).readlines()
+        l2.pop()
+        assert l1 == l2
+
         # Test File-New
         click('document-new')
-    close()
 
-    if window(r'/jEdit - Untitled-1'):
-        s = getComponent('JEditTextArea').getText()
-        assert len(s) == 0
+        if window(r'/jEdit - Untitled-1'):
+            s = getComponent('JEditTextArea').getText()
+            assert len(s) == 0
+        close()
     close()
 
 
