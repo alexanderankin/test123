df3399a0f23957bdb40d6d4c07ff4e9f0c20cf2d
voxmea <voxmea@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-10-27 16:18:00 +0000

    tagged release-1.0.3

---
+++ plugins/VoxSpell/tags/release-1.0.3/voxspellcheck/VoxSpellMenuService.java
@@ -0,0 +1,57 @@
+
+package voxspellcheck;
+
+import java.lang.StringBuffer;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.JMenuItem;
+
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.gui.DynamicContextMenuService;
+import org.gjt.sp.util.Log;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.menu.EnhancedMenuItem;
+
+public class VoxSpellMenuService extends DynamicContextMenuService
+{
+	public JMenuItem[] createMenu(JEditTextArea ta, MouseEvent evt)
+    {
+        EditPane ep = ta.getView().getEditPane();
+        if (ep == null)
+            return null;
+        VoxSpellPainter painter = VoxSpellPlugin.getVoxSpellPainter(ep);
+        if (painter == null)
+            return null;
+        
+        int pos;
+        if (evt == null) {
+            pos = ta.getCaretPosition();
+        } else {
+            pos = ta.xyToOffset(evt.getX(), evt.getY());
+        }
+        
+        StringBuffer word = new StringBuffer();
+        JMenuItem[] items = null;
+        if (painter.check(ta, pos, word)) {
+            // Check to see if it was ignored or added to the user dictionary.
+            // If it was then add the ability to reset.
+            if (painter.check(ta, pos, word, true)) {
+                items = new EnhancedMenuItem[1];
+                items[0] = new EnhancedMenuItem("VoxSpell: reset - \""+word+"\"", "voxspellcheck.resetWord",
+                                                jEdit.getActionContext());
+            }
+        } else {
+            items = new EnhancedMenuItem[3];
+            items[0] = new EnhancedMenuItem("VoxSpell: add - \""+word+"\"", "voxspellcheck.ignoreWord",
+                                            jEdit.getActionContext());
+            items[1] = new EnhancedMenuItem("VoxSpell: ignore - \""+word+"\"", "voxspellcheck.addWord",
+                                            jEdit.getActionContext());
+            items[2] = new EnhancedMenuItem("VoxSpell: suggest", "voxspellcheck.suggest",
+                                            jEdit.getActionContext());
+
+        }
+        return items;
+    }
+}
