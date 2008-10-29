58ba6cafb21a7f99e1f3359f909b09a70b5d699e
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-10-29 21:17:34 +0000

    Refactoring: Moved DbRecord and DbDescriptor to top level. Fixed the BeanShell expression for creating the table-specific dockables.

--- plugins/SourceNavigator/trunk/src/sn/DbAccess.java
+++ plugins/SourceNavigator/trunk/src/sn/DbAccess.java
@@ -5,10 +5,8 @@ import java.util.Vector;
 
 import javax.swing.JOptionPane;
 
-import org.gjt.sp.jedit.MiscUtilities;
 import org.gjt.sp.jedit.jEdit;
 
-import sn.SourceNavigatorPlugin.DbDescriptor;
 
 import com.sleepycat.db.Cursor;
 import com.sleepycat.db.Database;
@@ -77,62 +75,6 @@ public class DbAccess {
 			JOptionPane.showMessageDialog(jEdit.getActiveView(),
 				"Message: " + e1.getMessage() + "Stack trace:\n" +
 				e1.getStackTrace());
-		}
-	}
-	
-	/* Table-specific record lookup */
-	static public class DbRecord {
-		private DbDescriptor desc;
-		private String [] key;
-		private String baseDir;
-		private SourceLink link;
-		public DbRecord(DbDescriptor desc, String [] key) {
-			this.desc = desc;
-			this.key = key;
-		}
-		public void setBaseDir(String baseDir) {
-			this.baseDir = baseDir;
-		}
-		private int getColumnIndex(String columnName) {
-			for (int i = 0; i < desc.columnNames.length; i++)
-				if (desc.columnNames[i].equals(columnName))
-					return i;
-			return (-1);
-		}
-		public String getColumn(String columnName) {
-			return getColumn(getColumnIndex(columnName));
-		}
-		public String getColumn(int columnIndex) {
-			if (columnIndex < 0 || columnIndex >= key.length)
-				return null;
-			return key[columnIndex];
-		}
-		public SourceLink getSourceLink() {
-			if (link != null)
-				return link;
-			if (desc.fileColumn < 0)
-				return null;
-			String file = getColumn(desc.fileColumn);
-			if (file == null)
-				return null;
-			int line = 0;
-			int offset = 0;
-			if (desc.lineColumn >= 0) {
-				String lineStr = (String) getColumn(desc.lineColumn);
-				if (lineStr == null)
-					return null;
-				try {
-					String [] pos = lineStr.split("\\.");
-					line = Integer.valueOf(pos[0]);
-					offset = (pos.length > 1) ? Integer.valueOf(pos[1]) : 0;
-				} catch (Exception e) {
-					return null;
-				}
-			}
-			if (! MiscUtilities.isAbsolutePath(file))
-				file = baseDir + "/" + file;
-			link = new SourceLink(file, line, offset);
-			return link;
 		}
 	}
 	
---
+++ plugins/SourceNavigator/trunk/src/sn/DbDescriptor.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package sn;
+
+import org.gjt.sp.jedit.jEdit;
+
+public class DbDescriptor {
+	static private final String SN_SEP = "\\?";
+	public String name, label, db, columns;
+	public String [] columnNames;
+	int fileColumn, lineColumn;
+	public DbDescriptor(String base) {
+		name = jEdit.getProperty(base + "name");
+		label = jEdit.getProperty(base + "label");
+		db = jEdit.getProperty(base + "db");
+		columns = jEdit.getProperty(base + "columns");
+		columnNames = columns.split(SN_SEP);
+		fileColumn = lineColumn = -1;	// None by default
+		for (int i = 0; i < getColumnCount(); i++) {
+			String columnName = getColumn(i);
+			if (columnName.equals("File"))
+				fileColumn = i;
+			else if (columnName.equals("Line"))
+				lineColumn = i;
+		}
+	}
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+	public String getColumn(int index) {
+		if (index < 0 || index >= columnNames.length)
+			return null;
+		return columnNames[index];
+	}
+	public String toString() {
+		return label;
+	}
+}
