59f0ca2a71b25174805ff5cae8afc7ee12e0ea89
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-11-03 04:17:58 +0000

    The completion popup now sorts the names and avoid duplicates.

--- plugins/SourceNavigator/trunk/src/sn/DbRecordCompletionPopup.java
+++ plugins/SourceNavigator/trunk/src/sn/DbRecordCompletionPopup.java
@@ -6,6 +6,7 @@ package sn;
 import java.awt.Component;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
+import java.util.Collections;
 import java.util.Vector;
 
 import javax.swing.DefaultListCellRenderer;
@@ -21,7 +22,7 @@ import org.gjt.sp.jedit.textarea.JEditTextArea;
 class DbRecordCompletionPopup extends CompletionPopup {
 	private View view;
 	private String text;
-	private Vector<Candidate> candidates;
+	private Vector<DbRecord> records;
 	private CompleteAction action;
 	
 	public DbRecordCompletionPopup(View view, String text, Vector<DbRecord> records,
@@ -30,11 +31,9 @@ class DbRecordCompletionPopup extends CompletionPopup {
 		super(view, getLocation(view.getTextArea(), text));
 		this.view = view;
 		this.text = text;
+		this.records = records;
 		this.action = action;
-		candidates = new Vector<Candidate>();
-		for (DbRecord record: records)
-			candidates.add(new Candidate(record));
-		reset(new TagCandidates(), true);
+		reset(new TagCandidates(text, records), true);
 	}
 	
 	// Position the popup below the caret line, aligned with the text being completed
@@ -45,42 +44,37 @@ class DbRecordCompletionPopup extends CompletionPopup {
 		return location;
 	}
 
-	private class Candidate {
-		private DbRecord record;
-		public Candidate(DbRecord record) {
-			this.record = record;
-		}
-		public String toString() {
-			return record.getName();
-		}
-		public String getDescription() {
-			return record.getSourceLink().toString();
-		}
-	}
-	
 	private class TagCandidates implements Candidates {
 		private final ListCellRenderer renderer;
-		public TagCandidates() {
+		private Vector<String> completions;
+		public TagCandidates(String text, Vector<DbRecord> records) {
 			renderer = new DefaultListCellRenderer();
+			completions = new Vector<String>();
+			for (DbRecord record: records) {
+				String name = record.getName();
+				if (! completions.contains(name))
+					completions.add(name);
+			}
+			Collections.sort(completions);
 		}
 		public void complete(int index) {
-			String selected = candidates.get(index).toString();
+			String selected = completions.get(index);
 			String insertion = selected.substring(text.length());
 			SourceNavigatorPlugin.getEditorInterface().insertAtCaret(view, insertion);
 		}
 		public Component getCellRenderer(JList list, int index,
 				boolean isSelected, boolean cellHasFocus) {
 			return renderer.getListCellRendererComponent(list,
-				candidates.get(index), index, isSelected, cellHasFocus);
+				completions.get(index), index, isSelected, cellHasFocus);
 		}
 		public String getDescription(int index) {
-			return candidates.get(index).getDescription();
+			return "";
 		}
 		public int getSize() {
-			return candidates.size();
+			return completions.size();
 		}
 		public boolean isValid() {
-			return (! candidates.isEmpty());
+			return (! completions.isEmpty());
 		}
 	}
 	
