4b8d4a753e25b987c30a9da42ffc632ac79aeaec
lanciaux <lanciaux@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-11-16 11:35:49 +0000

    copying

---
+++ plugins/CookViewer/src/CookViewerPlugin.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import org.gjt.sp.jedit.EditPlugin;
+
+public class CookViewerPlugin extends EditPlugin {
+    public static final String NAME = "cookviewer";
+}
+
+/* :folding=explicit:tabSize=2:indentSize=2: */
