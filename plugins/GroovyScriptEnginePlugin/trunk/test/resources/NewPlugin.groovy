0e4e28dafc73cb5ca38ae9a7e95dc7fcb49c0807
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-12-03 21:12:29 +0000

    minor improvements.

--- plugins/GroovyScriptEnginePlugin/trunk/test/resources/NewPlugin.groovy
+++ plugins/GroovyScriptEnginePlugin/trunk/test/resources/NewPlugin.groovy
@@ -5,30 +5,35 @@ import net.miginfocom.swing.*;
 
 SCRIPTVALUE = ""
 
-def formData = {}
+def newPluginData = {}
 
 def swingBuilder = SwingBuilder.build() {
 	frame(id: "newPluginForm", title:"Create a new jEdit Plugin", pack:true, visible:true, defaultCloseOperation:JFrame.DISPOSE_ON_CLOSE) {
 		panel(layout:new MigLayout('fill')) {
-			label(text:"Plugin Name:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
-			textField(id:"pluginNameField", columns:20, constraints: "span 2, wrap, grow", keyReleased: {
+			label(text:"Plugin Name:", horizontalAlignment: JLabel.RIGHT, constraints: "growx")
+			textField(id:"pluginNameField", columns:20, constraints: "span 2, wrap, growx", keyReleased: {
 				packageField.text = pluginNameField.text.toLowerCase();
 			})
-			label(text:"Package:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
-			textField(id:"packageField", columns:20, constraints: "span 2, wrap, grow")
-			label(text:"Summary:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
-			textField(id:"summaryField", columns:20, constraints: "span 2, wrap, grow")
-			label(text:"Description:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
-			textField(id:"descriptionField", columns:20, constraints: "span 2, wrap, grow")
+			label(text:"Package:", horizontalAlignment: JLabel.RIGHT, constraints: "growx")
+			textField(id:"packageField", columns:20, constraints: "span 2, wrap, growx")
+			label(text:"Summary:", horizontalAlignment: JLabel.RIGHT, constraints: "growx")
+			textArea(id:"summaryField", columns:20, constraints: "span 2, wrap, grow")
+			label(text:"Description:", horizontalAlignment: JLabel.RIGHT, constraints: "growx")
+			textArea(id:"descriptionField", columns:20, constraints: "span 2, wrap, grow")
 			button(text:"Create", constraints: "span 2, align right", actionPerformed: {
-				println formData.pluginName
-				println formData.packageName
+				println "Plugin Name: ${newPluginData.pluginName}"
+				println "    Package: ${newPluginData.packageName}"
+				println "    Summary: ${newPluginData.summary}"
+				println "Description: ${newPluginData.description}"
+				newPluginForm.dispose();
 			})
 			button(text:"Cancel", constraints: "align right", actionPerformed: {
 				newPluginForm.dispose();
 			})
 		}
 	}
-	bind(source:pluginNameField, sourceProperty:"text", target:formData, targetProperty:"pluginName")
-	bind(source:packageField, sourceProperty:"text", target:formData, targetProperty:"packageName")
+	bind(source:pluginNameField, sourceProperty:"text", target:newPluginData, targetProperty:"pluginName")
+	bind(source:packageField, sourceProperty:"text", target:newPluginData, targetProperty:"packageName")
+	bind(source:summaryField, sourceProperty:"text", target:newPluginData, targetProperty:"summary")
+	bind(source:descriptionField, sourceProperty:"text", target:newPluginData, targetProperty:"description")
 };
