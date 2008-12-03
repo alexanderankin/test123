03586203675f498092678215c1b192601462fa3b
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-12-03 20:54:51 +0000

    moving versioned jars to unversioned ones, and adding miglayout-swing jar as a dependency.

--- plugins/GroovyScriptEnginePlugin/trunk/test/resources/NewPlugin.groovy
+++ plugins/GroovyScriptEnginePlugin/trunk/test/resources/NewPlugin.groovy
@@ -8,10 +8,12 @@ SCRIPTVALUE = ""
 def formData = {}
 
 def swingBuilder = SwingBuilder.build() {
-	frame(title:"Create a new jEdit Plugin", pack:true, visible:true, defaultCloseOperation:JFrame.DISPOSE_ON_CLOSE) {
+	frame(id: "newPluginForm", title:"Create a new jEdit Plugin", pack:true, visible:true, defaultCloseOperation:JFrame.DISPOSE_ON_CLOSE) {
 		panel(layout:new MigLayout('fill')) {
 			label(text:"Plugin Name:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
-			textField(id:"pluginNameField", columns:20, constraints: "span 2, wrap, grow")
+			textField(id:"pluginNameField", columns:20, constraints: "span 2, wrap, grow", keyReleased: {
+				packageField.text = pluginNameField.text.toLowerCase();
+			})
 			label(text:"Package:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
 			textField(id:"packageField", columns:20, constraints: "span 2, wrap, grow")
 			label(text:"Summary:", horizontalAlignment: JLabel.RIGHT, constraints: "grow")
@@ -22,7 +24,9 @@ def swingBuilder = SwingBuilder.build() {
 				println formData.pluginName
 				println formData.packageName
 			})
-			button(text:"Cancel", constraints: "align right")
+			button(text:"Cancel", constraints: "align right", actionPerformed: {
+				newPluginForm.dispose();
+			})
 		}
 	}
 	bind(source:pluginNameField, sourceProperty:"text", target:formData, targetProperty:"pluginName")
