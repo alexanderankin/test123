4ac8f67bba8903e188426b088de5aadc4c4f716c
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2008-12-12 17:50:18 +0000

    A branch for the ProjectViewer trunk version

---
+++ plugins/MyDoggyPlugin/branches/ProjectViewerTrunk/src/myDoggy/Plugin.java
@@ -0,0 +1,14 @@
+package myDoggy;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class Plugin extends EditPlugin {
+
+	public static final String NAME = "MyDoggyPlugin";
+	public void start()
+	{
+	}
+	public void stop()
+	{
+	}
+}
