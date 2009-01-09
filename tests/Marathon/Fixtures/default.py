53ea0cb7caf076aed0cb21bd28a507749e539c1d
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-09 20:27:30 +0000

    Make the test more environment independent using a test-specific settings directory.

--- tests/Marathon/Fixtures/default.py
+++ tests/Marathon/Fixtures/default.py
@@ -1,11 +1,16 @@
 from org.gjt.sp.jedit import jEdit
 from marathon.playback import *
 
+import os
+from java.lang import System
+
+settingsDir = System.getProperty('user.home') + System.getProperty('file.separator') + '.jEditTest'
+
 class Fixture:
 	def start_application(self):
-		args = ["-reuseview"]
+		args = ["-reuseview", "-settings=" + settingsDir]
 		jEdit.main(args)
-
+		
 	def teardown(self):
 		pass
 
