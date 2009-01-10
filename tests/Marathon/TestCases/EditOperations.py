3887b117935f92dacf609a49942b7f03eeec789f
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-10 19:18:54 +0000

    Fixed the EditOperations to work correctly with the clipboard.

--- tests/Marathon/TestCases/EditOperations.py
+++ tests/Marathon/TestCases/EditOperations.py
@@ -4,7 +4,12 @@ from util import *
 from java.lang import *
 from javax.swing import SwingUtilities
 from java.awt.datatransfer import *
+from org.gjt.sp.jedit.textarea import Selection
 #}}} Marathon Fixture
+
+def enterKeystroke(key, count):
+	for i in range(1, count + 1):
+		keystroke('JEditTextArea', key)
 
 def test():
 	java_recorded_version = '1.6.0_11'
@@ -12,17 +17,26 @@ def test():
 
 	class ClipboardAccess(java.lang.Runnable):
 		def get(self):
+			self.setString = ''
 			SwingUtilities.invokeAndWait(self)
 			return self.text
+
+		def setText(self, st):
+			self.setString = st
+			SwingUtilities.invokeAndWait(self)
+
 		def run(self):
-			self.text = clipboard.getContents(None).getTransferData(DataFlavor.stringFlavor)
+			if (self.setString != ''):
+				clipboard.setContents(StringSelection(self.setString), None)
+			else:
+				self.text = clipboard.getContents(None).getTransferData(DataFlavor.stringFlavor)
 	
 	if window('jEdit - TestPlan.txt'):
-		click('TextAreaPainter', 177, 195)
+		selection = Selection.Range(284, 327)
+		get_component('JEditTextArea').setSelection(selection)
 		select_menu('Edit>>Cut')
 		clipObj = ClipboardAccess()
 		s = clipObj.get()
-		print "Cut: " + s
 		assert s.startswith('test plan of the open source software jEdit'), 'Cut failure'
 	close()
 
@@ -31,5 +45,20 @@ def test():
 	close()
 
 	if window('jEdit - TestPlan.txt'):
-		window_closed('jEdit - TestPlan.txt')
+		selection = Selection.Range(1565, 1590)
+		get_component('JEditTextArea').setSelection(selection)
+		select_menu('Edit>>Copy')
+		clipObj = ClipboardAccess()
+		s = clipObj.get()
+		assert s.startswith('Basic features of the GUI'), 'Copy failure'
+
+		selection = Selection.Range(1583, 1590)
+		get_component('JEditTextArea').setSelection(selection)
+		clipObj = ClipboardAccess()
+		s = clipObj.setText('jEdit')
+		select_menu('Edit>>Paste')
+		selection = Selection.Range(1565, 1590)
+		get_component('JEditTextArea').setSelection(selection)
+		s = get_component('JEditTextArea').getSelectedText()
+		assert s.startswith('Basic features of jEdit'), 'Paste failure'
 	close()
