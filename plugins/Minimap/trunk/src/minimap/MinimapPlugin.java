ccb4958f2992351b0a7d932eb762019225062e49
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-14 13:26:16 +0000

    A slightly improved version...

--- plugins/Minimap/trunk/src/minimap/MinimapPlugin.java
+++ plugins/Minimap/trunk/src/minimap/MinimapPlugin.java
@@ -1,21 +1,8 @@
 package minimap;
-import java.awt.Container;
-import java.awt.FlowLayout;
-
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JSplitPane;
-
 import org.gjt.sp.jedit.EBMessage;
 import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.EditPane;
 import org.gjt.sp.jedit.View;
-import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.textarea.JEditEmbeddedTextArea;
-import org.gjt.sp.jedit.textarea.JEditTextArea;
-import org.gjt.sp.jedit.textarea.TextArea;
-import org.gjt.sp.jedit.visitors.JEditVisitorAdapter;
 
 public class MinimapPlugin extends EBPlugin {
 
@@ -34,23 +21,8 @@ public class MinimapPlugin extends EBPlugin {
 	}
 	static public void show(View view) {
 		EditPane editPane = view.getEditPane();
-		JEditTextArea textArea = editPane.getTextArea();
-		TextArea text = new JEditEmbeddedTextArea();
-        text.getBuffer().setProperty("folding","explicit");
-        text.getPainter().setFont(text.getPainter().getFont().deriveFont((float) 2.0));
-		Container c = textArea.getParent();
-		int i;
-		for (i = 0; i < c.getComponentCount(); i++)
-			if (c.getComponent(i) == textArea)
-				break;
-		c.remove(textArea);
-		JSplitPane splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		splitter.add(textArea);
-		splitter.add(text);
-		c.add(splitter);
-		c.validate();
-		splitter.setDividerLocation(0.7);
-		text.setBuffer(textArea.getBuffer());
+		editPane.add(new Minimap(editPane));
+		editPane.validate();
 	}
 
 }
