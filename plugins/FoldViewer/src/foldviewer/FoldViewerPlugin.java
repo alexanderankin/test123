9d4cfb3e185387f1ead9a9c7f5b63a0603ffdad2
Matthieu Casanova <chocolat.mou@gmail.com>
2009-01-14 10:51:51 +0000

    removed deprecated method use and unused imports

--- plugins/FoldViewer/src/foldviewer/FoldViewerPlugin.java
+++ plugins/FoldViewer/src/foldviewer/FoldViewerPlugin.java
@@ -20,14 +20,9 @@
 
 package foldviewer;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
-
 import org.gjt.sp.jedit.EBMessage;
 import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.EditPane;
-import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.msg.EditPaneUpdate;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
