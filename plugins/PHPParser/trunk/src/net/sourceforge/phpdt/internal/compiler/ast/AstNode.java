4331430b61b56c8b8eb2454475e98cd2f2f13609
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-19 13:57:26 +0000

    licence added and fixed coding style

--- plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/AstNode.java
+++ plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/AstNode.java
@@ -1,3 +1,24 @@
+/*
+* AstNode.java
+* :tabSize=8:indentSize=8:noTabs=false:
+* :folding=explicit:collapseFolds=1:
+*
+* Copyright (C) 2003, 2009 Matthieu Casanova
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 package net.sourceforge.phpdt.internal.compiler.ast;
 
 import gatchan.phpparser.parser.PHPParser;
@@ -10,178 +31,212 @@ import java.io.Serializable;
  *
  * @author Matthieu Casanova
  */
-public abstract class AstNode implements Serializable {
-  /** Starting and ending position of the node in the sources. */
-  protected int sourceStart;
-  protected int sourceEnd;
+public abstract class AstNode implements Serializable
+{
+	/** Starting and ending position of the node in the sources. */
+	protected int sourceStart;
+	protected int sourceEnd;
 
-  protected int beginLine;
-  protected int endLine;
-  protected int beginColumn;
-  protected int endColumn;
+	protected int beginLine;
+	protected int endLine;
+	protected int beginColumn;
+	protected int endColumn;
 
-  protected AstNode() {
-  }
+	//{{{ AstNode constructors
+	protected AstNode()
+	{
+	}
 
-  /**
-   * Create a node.
-   *
-   * @param sourceStart starting offset
-   * @param sourceEnd   ending offset
-   * @param beginLine   begin line
-   * @param endLine     ending line
-   * @param beginColumn begin column
-   * @param endColumn   ending column
-   */
-  protected AstNode(int sourceStart,
-                    int sourceEnd,
-                    int beginLine,
-                    int endLine,
-                    int beginColumn,
-                    int endColumn) {
-    this.sourceStart = sourceStart;
-    this.sourceEnd = sourceEnd;
-    this.beginLine = beginLine;
-    this.endLine = endLine;
-    this.beginColumn = beginColumn;
-    this.endColumn = endColumn;
-  }
+	/**
+	 * Create a node.
+	 *
+	 * @param sourceStart starting offset
+	 * @param sourceEnd	ending offset
+	 * @param beginLine	begin line
+	 * @param endLine	ending line
+	 * @param beginColumn begin column
+	 * @param endColumn	ending column
+	 */
+	protected AstNode(int sourceStart,
+			  int sourceEnd,
+			  int beginLine,
+			  int endLine,
+			  int beginColumn,
+			  int endColumn)
+	{
+		this.sourceStart = sourceStart;
+		this.sourceEnd = sourceEnd;
+		this.beginLine = beginLine;
+		this.endLine = endLine;
+		this.beginColumn = beginColumn;
+		this.endColumn = endColumn;
+	} //}}}
 
-  /**
-   * Add some tabulations.
-   *
-   * @param tab the number of tabulations
-   *
-   * @return a String containing some spaces
-   */
-  public static String tabString(int tab) {
-    if (tab == 0) return "";
-    StringBuffer s = new StringBuffer(2 * tab);
-    for (int i = tab; i > 0; i--) {
-      s.append("  ");
-    }
-    return s.toString();
-  }
+	//{{{ tabString() method
+	/**
+	 * Add some tabulations.
+	 *
+	 * @param tab the number of tabulations
+	 *
+	 * @return a String containing some spaces
+	 */
+	public static String tabString(int tab)
+	{
+		if (tab == 0)
+			return "";
+		StringBuilder s = new StringBuilder(2 * tab);
+		for (int i = tab; i > 0; i--)
+		{
+			s.append("  ");
+		}
+		return s.toString();
+	} //}}}
 
-  /**
-   * Return the object into String. It should be overriden
-   *
-   * @return a String
-   */
-  public String toString() {
-    return "****" + super.toString() + "****";
-  }
+	//{{{ toString() method
+	/**
+	 * Return the object into String. It should be overriden
+	 *
+	 * @return a String
+	 */
+	public String toString() {
+		return "****" + super.toString() + "****";
+	} //}}}
 
-  /**
-   * Return the object into String.
-   *
-   * @param tab how many tabs (not used here
-   *
-   * @return a String
-   */
-  public abstract String toString(int tab);
+	/**
+	 * Return the object into String.
+	 *
+	 * @param tab how many tabs (not used here
+	 *
+	 * @return a String
+	 */
+	public abstract String toString(int tab);
 
-  /**
-   * Get the variables from outside (parameters, globals ...)
-   *
-   * @param list the list where we will put variables
-   */
-  public abstract void getOutsideVariable(List list);
+	/**
+	 * Get the variables from outside (parameters, globals ...)
+	 *
+	 * @param list the list where we will put variables
+	 */
+	public abstract void getOutsideVariable(List list);
 
-  /**
-   * get the modified variables.
-   *
-   * @param list the list where we will put variables
-   */
-  public abstract void getModifiedVariable(List list);
+	/**
+	 * get the modified variables.
+	 *
+	 * @param list the list where we will put variables
+	 */
+	public abstract void getModifiedVariable(List list);
 
-  /**
-   * Get the variables used.
-   *
-   * @param list the list where we will put variables
-   */
-  public abstract void getUsedVariable(List list);
+	/**
+	 * Get the variables used.
+	 *
+	 * @param list the list where we will put variables
+	 */
+	public abstract void getUsedVariable(List list);
 
-  /** This method will analyze the code. by default it will do nothing */
-  public abstract void analyzeCode(PHPParser parser);
+	/** This method will analyze the code. by default it will do nothing */
+	public abstract void analyzeCode(PHPParser parser);
 
-  /**
-   * Check if the array array contains the object o.
-   *
-   * @param array an array
-   * @param o     an obejct
-   *
-   * @return true if the array contained the object o
-   */
-  public final boolean arrayContains(Object[] array, Object o) {
-    for (int i = 0; i < array.length; i++) {
-      if (array[i].equals(o)) {
-        return true;
-      }
-    }
-    return false;
-  }
+	//{{{ arrayContains() method
+	/**
+	 * Check if the array array contains the object o.
+	 *
+	 * @param array an array
+	 * @param o	  an obejct
+	 *
+	 * @return true if the array contained the object o
+	 */
+	public final boolean arrayContains(Object[] array, Object o)
+	{
+		for (int i = 0; i < array.length; i++)
+		{
+			if (array[i].equals(o))
+			{
+				return true;
+			}
+		}
+		return false;
+	} //}}}
 
-  public int getSourceStart() {
-    return sourceStart;
-  }
+	//{{{ getSourceStart() method
+	public int getSourceStart()
+	{
+		return sourceStart;
+	} //}}}
 
-  public int getSourceEnd() {
-    return sourceEnd;
-  }
+	//{{{ getSourceEnd() method
+	public int getSourceEnd()
+	{
+		return sourceEnd;
+	} //}}}
 
-  public int getBeginLine() {
-    return beginLine;
-  }
+	//{{{ getBeginLine() method
+	public int getBeginLine()
+	{
+		return beginLine;
+	} //}}}
 
-  public int getEndLine() {
-    return endLine;
-  }
+	//{{{ getEndLine() method
+	public int getEndLine()
+	{
+		return endLine;
+	} //}}}
 
-  public int getBeginColumn() {
-    return beginColumn;
-  }
+	//{{{ getBeginColumn() method
+	public int getBeginColumn()
+	{
+		return beginColumn;
+	} //}}}
 
-  public int getEndColumn() {
-    return endColumn;
-  }
+	//{{{ getEndColumn() method
+	public int getEndColumn()
+	{
+		return endColumn;
+	} //}}}
 
-  public void setSourceEnd(int sourceEnd) {
-    this.sourceEnd = sourceEnd;
-  }
+	//{{{ setSourceEnd() method
+	public void setSourceEnd(int sourceEnd)
+	{
+		this.sourceEnd = sourceEnd;
+	} //}}}
 
-  public void setEndLine(int endLine) {
-    this.endLine = endLine;
-  }
+	//{{{ setEndLine() method
+	public void setEndLine(int endLine)
+	{
+		this.endLine = endLine;
+	} //}}}
 
-  public void setEndColumn(int endColumn) {
-    this.endColumn = endColumn;
-  }
+	//{{{ setEndColumn() method
+	public void setEndColumn(int endColumn)
+	{
+		this.endColumn = endColumn;
+	} //}}}
 
-  /**
-   * Returns true if the line and column position are contained by this node.
-   *
-   * @param line   the line
-   * @param column the column
-   *
-   * @return true if the line and column position are contained by this node.
-   */
-  public boolean isAt(int line, int column) {
-    return isAt(this, line, column);
-  }
+	//{{{  isAt() methods
+	/**
+	 * Returns true if the line and column position are contained by this node.
+	 *
+	 * @param line   the line
+	 * @param column the column
+	 *
+	 * @return true if the line and column position are contained by this node.
+	 */
+	public boolean isAt(int line, int column)
+	{
+		return isAt(this, line, column);
+	}
 
-  /**
-   * Returns true if the line and column position are contained by the given node.
-   *
-   * @param node   the node
-   * @param line   the line
-   * @param column the column
-   *
-   * @return true if the line and column position are contained by the given node.
-   */
-  public static boolean isAt(AstNode node, int line, int column) {
-    return (line == node.getBeginLine() && column > node.getBeginColumn()) ||
-           (line == node.getEndLine() && column < node.getEndColumn()) ||
-           (line > node.getBeginLine() && line < node.getEndLine());
-  }
+	/**
+	 * Returns true if the line and column position are contained by the given node.
+	 *
+	 * @param node   the node
+	 * @param line   the line
+	 * @param column the column
+	 *
+	 * @return true if the line and column position are contained by the given node.
+	 */
+	public static boolean isAt(AstNode node, int line, int column)
+	{
+		return (line == node.getBeginLine() && column > node.getBeginColumn()) ||
+		(line == node.getEndLine() && column < node.getEndColumn()) ||
+		(line > node.getBeginLine() && line < node.getEndLine());
+	} //}}}
 }
