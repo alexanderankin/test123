c77bd7ba608c26d1862ddd93e3e9ceb66058c723
rfrancois <rfrancois@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-01-28 15:39:09 +0000

    removed old command line stufff and make the plugin work without redifining jedit classes

--- plugins/Code2HTML/trunk/code2html/generic/GenericPainter.java
+++ plugins/Code2HTML/trunk/code2html/generic/GenericPainter.java
@@ -28,7 +28,6 @@ import javax.swing.text.Segment;
 import org.gjt.sp.jedit.syntax.SyntaxStyle;
 import org.gjt.sp.jedit.syntax.Token;
 
-import code2html.SyntaxToken;
 import code2html.line.LinePosition;
 import code2html.line.LineTabExpander;
 import code2html.line.LineWrapper;
@@ -80,7 +79,7 @@ public abstract class GenericPainter {
     }
 
 
-		public void paintPlainLine(Writer out, int lineNumber, Segment line, SyntaxToken tokens)
+		public void paintPlainLine(Writer out, int lineNumber, Segment line, Token tokens)
     {
         try {
             if (this.showGutter) {
@@ -124,7 +123,7 @@ public abstract class GenericPainter {
     }
 
     public void paintSyntaxLine(
-            Writer out, int lineNumber, Segment line, SyntaxToken tokens
+            Writer out, int lineNumber, Segment line, Token tokens
     ) {
         
 				try {
