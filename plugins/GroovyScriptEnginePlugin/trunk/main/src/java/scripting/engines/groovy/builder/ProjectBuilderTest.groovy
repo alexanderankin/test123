d280aa4b720db99d152af9c40d659757026823bc
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-02-03 01:52:37 +0000

    Finishing up ProjectBuilder files.

--- plugins/GroovyScriptEnginePlugin/trunk/main/src/java/scripting/engines/groovy/builder/ProjectBuilderTest.groovy
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package scripting.engines.groovy.builder
-
-def pb = ProjectBuilder.build("PluginDeveloper", "/Users/eberry/development/projects/jedit/plugins");
