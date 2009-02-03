99b7730b0062662badaffc72bf236a4bb08817a1
Matthieu Casanova <chocolat.mou@gmail.com>
2009-02-03 15:50:51 +0000

    fixed coding style, highlights are updated immediately when changing them and highlight the word at caret in the Hypersearch results

--- plugins/Highlight/trunk/src/gatchan/highlight/HighlightHypersearchResults.java
+++ plugins/Highlight/trunk/src/gatchan/highlight/HighlightHypersearchResults.java
@@ -18,95 +18,94 @@ import org.gjt.sp.jedit.search.HyperSearchResults;
 import org.gjt.sp.jedit.search.SearchMatcher;
 import org.gjt.sp.jedit.search.SearchMatcher.Match;
 
-public class HighlightHypersearchResults {
-
+public class HighlightHypersearchResults implements HighlightChangeListener
+{
 	public static final String HYPERSEARCH = "hypersearch-results";
 	private View view;
 
-	public HighlightHypersearchResults(View view) {
+	public HighlightHypersearchResults(View view)
+	{
 		this.view = view;
 	}
 
-	private JTree getHyperSearchTree() {
+	private JTree getHyperSearchTree()
+	{
 		JComponent dockable = view.getDockableWindowManager().getDockable(HYPERSEARCH);
 		if (dockable == null)
 			return null;
-		if (! (dockable instanceof HyperSearchResults))
+		if (!(dockable instanceof HyperSearchResults))
 			return null;
 		HyperSearchResults hrd = (HyperSearchResults) dockable;
 		return hrd.getTree();
 	}
 
-	public void start() {
+	public void start()
+	{
+		HighlightManagerTableModel.getManager().addHighlightChangeListener(this);
 		JTree tree = getHyperSearchTree();
 		if (tree == null)
 			return;
 		TreeCellRenderer renderer = tree.getCellRenderer();
-		if (! (renderer instanceof HighlightTreeCellRenderer))
+		if (!(renderer instanceof HighlightTreeCellRenderer))
 			tree.setCellRenderer(new HighlightTreeCellRenderer(renderer));
 	}
-	
-	public void stop() {
+
+	public void stop()
+	{
+		HighlightManagerTableModel.getManager().removeHighlightChangeListener(this);
 		JTree tree = getHyperSearchTree();
 		if (tree == null)
 			return;
 		TreeCellRenderer renderer = tree.getCellRenderer();
 		if (renderer instanceof HighlightTreeCellRenderer)
-			tree.setCellRenderer(((HighlightTreeCellRenderer)renderer).getOriginal());
+			tree.setCellRenderer(((HighlightTreeCellRenderer) renderer).getOriginal());
 	}
-	
+
+	public void highlightUpdated(boolean highlightEnabled)
+	{
+		JTree tree = getHyperSearchTree();
+		if (tree != null)
+			tree.repaint();
+	}
+
 	@SuppressWarnings("serial")
 	class HighlightTreeCellRenderer extends DefaultTreeCellRenderer
 	{
 		private TreeCellRenderer renderer;
-		
-		public HighlightTreeCellRenderer(TreeCellRenderer renderer) {
+
+		public HighlightTreeCellRenderer(TreeCellRenderer renderer)
+		{
 			this.renderer = renderer;
 		}
-		public TreeCellRenderer getOriginal() {
+
+		public TreeCellRenderer getOriginal()
+		{
 			return renderer;
 		}
+
 		@Override
 		public Component getTreeCellRendererComponent(JTree tree, Object value,
-				boolean sel, boolean expanded, boolean leaf, int row,
-				boolean hasFocus)
+		                                              boolean sel, boolean expanded, boolean leaf, int row,
+		                                              boolean hasFocus)
 		{
 			Component defaultComponent = renderer.getTreeCellRendererComponent(tree,
-				value, sel, expanded, leaf, row, hasFocus);
-			if (! (value instanceof DefaultMutableTreeNode))
+			                                                                   value, sel, expanded, leaf, row, hasFocus);
+			if (!(value instanceof DefaultMutableTreeNode))
 				return defaultComponent;
-			if (! jEdit.getBooleanProperty(HighlightOptionPane.PROP_HIGHLIGHT_HYPERSEARCH_RESULTS))
+			if (!jEdit.getBooleanProperty(HighlightOptionPane.PROP_HIGHLIGHT_HYPERSEARCH_RESULTS))
 				return defaultComponent;
 			DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
 			Object obj = node.getUserObject();
-			if (! (obj instanceof HyperSearchResult))
+			if (!(obj instanceof HyperSearchResult))
 				return defaultComponent;
 			HyperSearchResult result = (HyperSearchResult) obj;
 			String s = getHighlightedString(result.toString());
 			return renderer.getTreeCellRendererComponent(tree,
-				new DefaultMutableTreeNode(s), sel, expanded, leaf, row, hasFocus);
+			                                             new DefaultMutableTreeNode(s), sel, expanded, leaf, row, hasFocus);
 		}
+
 		private String getHighlightedString(String s)
 		{
-			class HighlightPosition implements Comparable<HighlightPosition> {
-				public int pos;
-				public Highlight highlight;
-				public boolean start;
-				public HighlightPosition(int p, Highlight h, boolean s)
-				{
-					pos = p;
-					highlight = h;
-					start = s;
-				}
-				public int compareTo(HighlightPosition hlPos)
-				{
-					if (pos < hlPos.pos)
-						return (-1);
-					if (pos > hlPos.pos)
-						return 1;
-					return 0;
-				}
-			}
 			HighlightManager manager = HighlightManagerTableModel.getManager();
 			Vector<HighlightPosition> highlights = new Vector<HighlightPosition>();
 			try
@@ -115,26 +114,20 @@ public class HighlightHypersearchResults {
 				for (int hi = 0; hi < manager.countHighlights(); hi++)
 				{
 					Highlight highlight = manager.getHighlight(hi);
-					SearchMatcher matcher = highlight.getSearchMatcher();
-					int i = 0;
-					Match m = null;
-					while ((m = matcher.nextMatch(s.substring(i), true, true, true, false)) != null)
-					{
-						highlights.add(new HighlightPosition(i + m.start, highlight, true));
-						highlights.add(new HighlightPosition(i + m.end, highlight, false));
-						i += m.end;
-					}
+					addHighlight(highlights, s, highlight);
 				}
+				if (manager.isHighlightWordAtCaret())
+					addHighlight(highlights, s, HighlightManagerTableModel.currentWordHighlight);
 			}
 			finally
 			{
 				manager.releaseLock();
 			}
 			Collections.sort(highlights);
-			StringBuffer sb = new StringBuffer("<html><body>");
+			StringBuilder sb = new StringBuilder("<html><body>");
 			int i = 0;
 			Vector<Highlight> stack = new Vector<Highlight>();
-			for (HighlightPosition hlPos: highlights)
+			for (HighlightPosition hlPos : highlights)
 			{
 				appendString2html(sb, s.substring(i, hlPos.pos));
 				if (hlPos.start)
@@ -166,7 +159,21 @@ public class HighlightHypersearchResults {
 			sb.append("</body></html>");
 			return sb.toString();
 		}
-		private void appendColorSpec(StringBuffer sb, Color c)
+
+		private void addHighlight(Vector<HighlightPosition> highlights, String s, Highlight highlight)
+		{
+			SearchMatcher matcher = highlight.getSearchMatcher();
+			int i = 0;
+			Match m;
+			while ((m = matcher.nextMatch(s.substring(i), true, true, true, false)) != null)
+			{
+				highlights.add(new HighlightPosition(i + m.start, highlight, true));
+				highlights.add(new HighlightPosition(i + m.end, highlight, false));
+				i += m.end;
+			}
+		}
+
+		private void appendColorSpec(StringBuilder sb, Color c)
 		{
 			sb.append(Integer.toHexString(c.getRed()));
 			if (c.getGreen() <= 0xf)
@@ -174,23 +181,60 @@ public class HighlightHypersearchResults {
 			sb.append(Integer.toHexString(c.getGreen()));
 			if (c.getBlue() <= 0xf)
 				sb.append("0");
-			sb.append(Integer.toHexString(c.getBlue()));			
+			sb.append(Integer.toHexString(c.getBlue()));
 		}
-		private void appendString2html(StringBuffer sb, String s)
+
+		private void appendString2html(StringBuilder sb, String s)
 		{
 			for (int i = 0; i < s.length(); i++)
 			{
 				char c = s.charAt(i);
 				String r;
-				switch (c) {
-				case '"': r = "&quot;"; break;
-				case '\'': r = "&apos;"; break;
-				case '&': r = "&amp;"; break;
-				case '<': r = "&lt;"; break;
-				case '>': r = "&gt;"; break;
-				default: r = String.valueOf(c); break;
+				switch (c)
+				{
+					case '"':
+						r = "&quot;";
+						break;
+					case '\'':
+						r = "&apos;";
+						break;
+					case '&':
+						r = "&amp;";
+						break;
+					case '<':
+						r = "&lt;";
+						break;
+					case '>':
+						r = "&gt;";
+						break;
+					default:
+						r = String.valueOf(c);
+						break;
 				}
 				sb.append(r);
+			}
+		}
+
+		private class HighlightPosition implements Comparable<HighlightPosition>
+		{
+			public int pos;
+			public Highlight highlight;
+			public boolean start;
+
+			public HighlightPosition(int p, Highlight h, boolean s)
+			{
+				pos = p;
+				highlight = h;
+				start = s;
+			}
+
+			public int compareTo(HighlightPosition hlPos)
+			{
+				if (pos < hlPos.pos)
+					return (-1);
+				if (pos > hlPos.pos)
+					return 1;
+				return 0;
 			}
 		}
 	}
