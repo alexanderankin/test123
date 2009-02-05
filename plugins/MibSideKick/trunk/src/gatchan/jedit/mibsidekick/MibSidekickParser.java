9e872a9b23d09bec97d1f43ab1e0c0fe3d4eb99a
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-02-05 23:56:03 +0000

    first commit of MibSidekick plugin

---
+++ plugins/MibSideKick/trunk/src/gatchan/jedit/mibsidekick/MibSidekickParser.java
@@ -0,0 +1,103 @@
+/*
+* MibSideKickPlugin.java - The Mib sidekick plugin
+* :tabSize=8:indentSize=8:noTabs=false:
+* :folding=explicit:collapseFolds=1:
+*
+* Copyright (C) 2009 Matthieu Casanova
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+package gatchan.jedit.mibsidekick;
+
+import errorlist.DefaultErrorSource;
+import errorlist.ErrorSource;
+import net.percederberg.mibble.Mib;
+import net.percederberg.mibble.MibLoader;
+import net.percederberg.mibble.MibLoaderException;
+import net.percederberg.mibble.MibLoaderLog;
+import net.percederberg.mibble.browser.MibTreeBuilder;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.util.Log;
+import sidekick.SideKickParsedData;
+import sidekick.SideKickParser;
+
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * @author Matthieu Casanova
+ */
+public class MibSidekickParser extends SideKickParser
+{
+
+	public MibSidekickParser()
+	{
+		super("MibParser");
+	}
+
+	public SideKickParsedData parse(Buffer buffer, DefaultErrorSource errorSource)
+	{
+		MibLoader loader = new MibLoader();
+		try
+		{
+			File file = new File(buffer.getPath());
+			loader.addDir(file.getParentFile());
+			loader.addResourceDir("mibs/iana");
+			loader.addResourceDir("mibs/ietf");
+			Mib mib = loader.load(file);
+			SideKickParsedData datas = new SideKickParsedData(buffer.getPath());
+			MibTreeBuilder tree = MibTreeBuilder.getInstance();
+			tree.addMib(mib);
+			TreeModel model = tree.getTree().getModel();
+			Object root = model.getRoot();
+			int count = model.getChildCount(root);
+			for (int i =0;i<count;i++)
+			{
+				Object child = model.getChild(root, i);
+				datas.root.add((MutableTreeNode) child);
+			}
+			return datas;
+		}
+		catch (IOException e)
+		{
+			Log.log(Log.ERROR, e, e);
+		}
+		catch (MibLoaderException e)
+		{
+			MibLoaderLog mibLoaderLog = e.getLog();
+			Iterator<MibLoaderLog.LogEntry> iterator = mibLoaderLog.entries();
+			while (iterator.hasNext())
+			{
+				MibLoaderLog.LogEntry logEntry =  iterator.next();
+				if (logEntry.getLineNumber() == -1)
+				{
+					continue;
+				}
+				int line = logEntry.getLineNumber() -1;
+				int endColumn = buffer.getLineLength(line);
+				errorSource.addError(ErrorSource.ERROR, buffer.getPath(),
+						     line,
+						     logEntry.getColumnNumber() -1,
+						     endColumn,
+						     logEntry.getMessage());
+			}
+
+
+		}
+		return null;
+	}
+}
