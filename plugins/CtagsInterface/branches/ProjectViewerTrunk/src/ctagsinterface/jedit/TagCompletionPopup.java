4a6ed5b43fdc91e61a6b1a3735b242d83acad8f3
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-03-31 16:38:39 +0000

    Include all tag attributes in the tag description that comes with the completion popup.

--- plugins/CtagsInterface/branches/ProjectViewerTrunk/src/ctagsinterface/jedit/TagCompletionPopup.java
+++ plugins/CtagsInterface/branches/ProjectViewerTrunk/src/ctagsinterface/jedit/TagCompletionPopup.java
@@ -6,6 +6,7 @@ import java.awt.Point;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import org.gjt.sp.jedit.View;
@@ -30,7 +31,7 @@ public class TagCompletionPopup extends CompletionPopup {
 		Container c = getContentPane();
 		JPanel p = new JPanel(new GridLayout(1, 0));
 		p.add(c);
-		p.add(desc);
+		p.add(new JScrollPane(desc));
 		setContentPane(p);
 	}
 	
@@ -38,7 +39,11 @@ public class TagCompletionPopup extends CompletionPopup {
 	{
 		if (! GeneralOptionPane.getCompleteDesc())
 			return;
-		desc.setText(tag.getFile() + "\n" + tag.getLine());
+		StringBuffer sb = new StringBuffer();
+		sb.append(tag.getFile() + ":" + tag.getLine() + "\n");
+		for (String ext: tag.getExtensions())
+			sb.append(ext + ": " + tag.getExtension(ext) + "\n");
+		desc.setText(sb.toString());
 	}
 	
 }
