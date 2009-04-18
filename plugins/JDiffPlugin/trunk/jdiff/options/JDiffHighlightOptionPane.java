7539a7314f98f6b5edf16c7e262512e0ce5a81d6
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-04-18 15:00:16 +0000

    Combined the color option panes to make it easier to set the highlight colors. Added dependency on CommonControls 0.9.5 to take advantage of the new version of KappaLayout, which is used in the option panes.

--- plugins/JDiffPlugin/trunk/jdiff/options/JDiffHighlightOptionPane.java
+++ plugins/JDiffPlugin/trunk/jdiff/options/JDiffHighlightOptionPane.java
@@ -1,95 +1,128 @@
 /*
- * JDiffHighlightOptionPane.java
- * Copyright (c) 2000, 2001, 2002 Andre Kaplan
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+* JDiffHighlightOptionPane.java
+* Copyright (c) 2000, 2001, 2002 Andre Kaplan
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 
 
 package jdiff.options;
 
+import java.awt.Component;
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.OptionPane;
 import org.gjt.sp.jedit.gui.ColorWellButton;
 import org.gjt.sp.jedit.jEdit;
 
+// from CommonControls
+import ise.java.awt.KappaLayout;
 
-public class JDiffHighlightOptionPane extends AbstractOptionPane
-{
+/**
+ * @deprecated The functionality of this class has been combined with
+ * JDiffOverviewOptionPane so colors are managed in one place.
+ */
+@Deprecated
+public class JDiffHighlightOptionPane implements OptionPane {
+
+    private JPanel panel = null;
+
     private ColorWellButton highlightChangedLineColor;
     private ColorWellButton highlightDeletedLineColor;
     private ColorWellButton highlightInsertedLineColor;
-
     private ColorWellButton highlightInvalidLineColor;
 
-
-    public JDiffHighlightOptionPane() {
-        super("jdiff-highlight");
+    /**
+     * @return the name of this panel
+     */
+    public String getName() {
+        return jEdit.getProperty( "jdiff.highlight" );
     }
 
+    /**
+     * @return the panel to display the options for JDiff
+     */
+    public Component getComponent() {
+        if ( panel == null ) {
+            createPanel();
+        }
+        return panel;
+    }
 
-    public void _init() {
-        this.highlightChangedLineColor  = new ColorWellButton(jEdit.getColorProperty("jdiff.highlight-changed-color"));
-        this.highlightDeletedLineColor  = new ColorWellButton(jEdit.getColorProperty("jdiff.highlight-deleted-color"));
-        this.highlightInsertedLineColor = new ColorWellButton(jEdit.getColorProperty("jdiff.highlight-inserted-color"));
+    /**
+     * Initialize the panel, create the components and apply current settings.
+     */
+    public void init() {
+        if ( panel == null ) {
+            createPanel();
+        }
+        highlightChangedLineColor.setSelectedColor( jEdit.getColorProperty( "jdiff.highlight-changed-color" ) );
+        highlightDeletedLineColor.setSelectedColor( jEdit.getColorProperty( "jdiff.highlight-deleted-color" ) );
+        highlightInsertedLineColor.setSelectedColor( jEdit.getColorProperty( "jdiff.highlight-inserted-color" ) );
+        highlightInvalidLineColor.setSelectedColor( jEdit.getColorProperty( "jdiff.highlight-invalid-color" ) );
+    }
 
-    this.highlightInvalidLineColor  = new ColorWellButton(jEdit.getColorProperty("jdiff.highlight-invalid-color"));
+    // actually create and layout the option panel here
+    private void createPanel() {
+        highlightChangedLineColor = new ColorWellButton( jEdit.getColorProperty( "jdiff.highlight-changed-color" ) );
+        highlightDeletedLineColor = new ColorWellButton( jEdit.getColorProperty( "jdiff.highlight-deleted-color" ) );
+        highlightInsertedLineColor = new ColorWellButton( jEdit.getColorProperty( "jdiff.highlight-inserted-color" ) );
+        highlightInvalidLineColor = new ColorWellButton( jEdit.getColorProperty( "jdiff.highlight-invalid-color" ) );
 
         // Highlight colors
-        addComponent(this.createLabel("options.jdiff.highlight"));
-        addComponent(
-            jEdit.getProperty("options.jdiff.highlight-changed-color"),
-            this.highlightChangedLineColor
-        );
-        addComponent(
-            jEdit.getProperty("options.jdiff.highlight-deleted-color"),
-            this.highlightDeletedLineColor
-        );
-        addComponent(
-            jEdit.getProperty("options.jdiff.highlight-inserted-color"),
-            this.highlightInsertedLineColor
-        );
+        panel = new JPanel();
+        panel.setLayout( new KappaLayout() );
+        panel.setBorder( BorderFactory.createEmptyBorder( 12, 11, 11, 12 ) );
 
-        addComponent(
-            jEdit.getProperty("options.jdiff.highlight-invalid-color"),
-            this.highlightInvalidLineColor
-        );
+        panel.add( "0, 0, 2, 1, W, w, 3", createLabel( "options.jdiff.highlight" ) );
+
+        panel.add( "0, 1, 1, 1, W,  , 3", createLabel( "options.jdiff.overview-changed-color" ) );
+        panel.add( "1, 1, 1, 1, 0, w, 3", highlightChangedLineColor );
+
+        panel.add( "0, 2, 1, 1, W,  , 3", createLabel( "options.jdiff.highlight-deleted-color" ) );
+        panel.add( "1, 2, 1, 1, 0, w, 3", highlightDeletedLineColor );
+
+        panel.add( "0, 3, 1, 1, W,  , 3", createLabel( "options.jdiff.highlight-inserted-color" ) );
+        panel.add( "1, 3, 1, 1, 0, w, 3", highlightInsertedLineColor );
+
+        panel.add( "0, 4, 1, 1, W,  , 3", createLabel( "options.jdiff.highlight-invalid-color" ) );
+        panel.add( "1, 4, 1, 1, 0, w, 3", highlightInvalidLineColor );
     }
 
 
-    public void _save() {
-        jEdit.setColorProperty("jdiff.highlight-changed-color",
-            this.highlightChangedLineColor.getSelectedColor()
-        );
-        jEdit.setColorProperty("jdiff.highlight-deleted-color",
-            this.highlightDeletedLineColor.getSelectedColor()
-        );
-        jEdit.setColorProperty("jdiff.highlight-inserted-color",
-            this.highlightInsertedLineColor.getSelectedColor()
-        );
+    public void save() {
+        jEdit.setColorProperty( "jdiff.highlight-changed-color",
+                this.highlightChangedLineColor.getSelectedColor()
+                              );
+        jEdit.setColorProperty( "jdiff.highlight-deleted-color",
+                this.highlightDeletedLineColor.getSelectedColor()
+                              );
+        jEdit.setColorProperty( "jdiff.highlight-inserted-color",
+                this.highlightInsertedLineColor.getSelectedColor()
+                              );
 
-        jEdit.setColorProperty("jdiff.highlight-invalid-color",
-            this.highlightInvalidLineColor.getSelectedColor()
-        );
+        jEdit.setColorProperty( "jdiff.highlight-invalid-color",
+                this.highlightInvalidLineColor.getSelectedColor()
+                              );
     }
 
 
-    private JLabel createLabel(String property) {
-        return new JLabel(jEdit.getProperty(property));
+    private JLabel createLabel( String property ) {
+        return new JLabel( jEdit.getProperty( property ) );
     }
 
 }
-
