4b6ee9b20a3a1d9630b7f3ddf280cfd6d42f84e2
Herman Vierendeels <2022213+hvierendeels@users.noreply.github.com>
2009-05-07 12:04:44 +0000

    test 1st commit

---
+++ plugins/h4Jmf/tags/start/h4JmfPlugin.java
@@ -0,0 +1,12 @@
+/*
+h4Jmf.java
+program is free software GNU General Public License
+author Herman Vierendeels,Ternat,Belgium
+*/
+import org.gjt.sp.jedit.EditPlugin;
+
+public class h4JmfPlugin extends EditPlugin
+{
+ public static final String NAME="h4Jmf";
+ public static final String OPTION_PREFIX="options.h4Jmf.";
+}
