ce65a74b363471350950b27183f12cc7e36470c8
Herman Vierendeels <2022213+hvierendeels@users.noreply.github.com>
2009-05-11 14:25:47 +0000

    more info about preliminary jmf

--- plugins/h4Jmf/trunk/h4JmfPlugin.java
+++ plugins/h4Jmf/trunk/h4JmfPlugin.java
@@ -3,10 +3,23 @@ h4Jmf.java
 program is free software GNU General Public License
 author Herman Vierendeels,Ternat,Belgium
 */
+import java.util.logging.*;
+
 import org.gjt.sp.jedit.EditPlugin;
 
 public class h4JmfPlugin extends EditPlugin
 {
+ private static final Logger logger=Logger.getLogger(h4JmfPlugin.class.getName());
+
  public static final String NAME="h4Jmf";
  public static final String OPTION_PREFIX="options.h4Jmf.";
+
+ public void start()
+ {
+  logger.info("start plugin");
+ }//start
+ public void stop()
+ {
+  logger.info("stop plugin");
+ }//stop
 }
