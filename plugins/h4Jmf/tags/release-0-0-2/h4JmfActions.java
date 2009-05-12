5ca76f3cf0f586cc9ec9a641459e849fe31ffa8a
tshvr <tshvr@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-05-12 18:11:34 +0000

    workable version

---
+++ plugins/h4Jmf/tags/release-0-0-2/h4JmfActions.java
@@ -0,0 +1,17 @@
+/*
+h4Jmf.java
+program is free software GNU General Public License
+author Herman Vierendeels,Ternat,Belgium
+*/
+interface h4JmfActions
+{
+ void chooseFile();
+ void player_begin();
+ void player_start();
+ void player_stop();
+ void player_rewind();
+ void player_forward();
+ void player_close();
+ void insert_time();
+ void player_position();
+}
