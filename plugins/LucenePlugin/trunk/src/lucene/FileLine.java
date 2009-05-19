19253bb04c452401fb713d07cbc59b1dadf5ddf0
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-05-19 11:01:33 +0000

    A very simplistic version (but working...) of a Lucene plugin, that allows indexing a single directory, and presents the search results in a source link tree.

---
+++ plugins/LucenePlugin/trunk/src/lucene/FileLine.java
@@ -0,0 +1,13 @@
+package lucene;
+
+public class FileLine {
+	String file;
+	int line;
+	String content;
+	public FileLine(String f, int l, String c)
+	{
+		file = f;
+		line = l;
+		content = c;
+	}
+}
