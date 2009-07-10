19c1d98703841c4ef244b0d7f2f71fc135458b1f
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-07-10 14:48:03 +0000

    Applied Eric's patch from tracker 2819322 to set foreground color in cell renderers. Tracker 2819322

--- plugins/SVNPlugin/trunk/src/ise/plugin/svn/gui/NoWrapCellRenderer.java
+++ plugins/SVNPlugin/trunk/src/ise/plugin/svn/gui/NoWrapCellRenderer.java
@@ -15,10 +15,12 @@ public class NoWrapCellRenderer extends JTextArea implements TableCellRenderer {
 
     private static Color background = jEdit.getColorProperty( "view.bgColor", Color.WHITE );
     private static Color selection = jEdit.getColorProperty( "view.selectionColor", Color.LIGHT_GRAY );
+    private static Color foreground = jEdit.getColorProperty( "view.fgColor", Color.BLACK );
 
     public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column ) {
         setText( value == null ? "" : value.toString().trim() );
         setBackground( isSelected ? selection : background );
+        setForeground( foreground );
         return this;
     }
 }
