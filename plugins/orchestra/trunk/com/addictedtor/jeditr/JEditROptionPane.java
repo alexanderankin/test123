6f634620007ae508e16f1e5390d507df07f0e9fe
rfrancois <rfrancois@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-08-08 06:15:47 +0000

    name switch s/jeditr/orchestra/

---
+++ plugins/orchestra/trunk/com/addictedtor/jeditr/JEditROptionPane.java
@@ -0,0 +1,140 @@
+package com.addictedtor.jeditr;
+
+import org.af.jhlir.tools.DirectoryGuesser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Hashtable;
+
+
+/**
+ * Options panel
+ *
+ * @author Bernd Bischl <bernd_bischl@gmx.net>
+ * @author Romain Francois <francoisromain@free.fr>
+ */
+
+public class JEditROptionPane extends AbstractOptionPane implements ActionListener {
+    protected static final Log logger = LogFactory.getLog(JEditROptionPane.class);
+
+    public static final String OPTION_PREFIX = "options.jeditr.";
+    private Hashtable<String, JTextField> pathNames = new Hashtable<String, JTextField>();
+    private JTextField tfRHome = new JTextField();
+    private JTextField tfShortcut = new JTextField();
+
+
+    public JEditROptionPane() {
+        super("jeditr-optionpane");
+    }
+
+
+    public void _init() {
+        addPathPanel("rhome", tfRHome);
+        addPathPanel("shortcut", tfShortcut);
+        guessDirs();
+    }
+
+    private void addPathPanel(String name, JTextField tf) {
+        pathNames.put(name, tf);
+        addSeparator(OPTION_PREFIX + name + ".title");
+        addComponent("", makePathPanel(name, tf));
+    }
+
+    private void guessDirs() {
+//        if (tfJavaHome.getText().trim().equals(""))
+//            tfJavaHome.setText(DirectoryGuesser.guessJavaHome());
+        if (tfRHome.getText().trim().equals("")) {
+            String rHome = DirectoryGuesser.guessRHome();
+            logger.info("Guessing R_HOME : " + rHome);
+            tfRHome.setText(rHome);
+        }
+        if (tfShortcut.getText().trim().equals("")) {
+            String desktop = DirectoryGuesser.guessDesktop();
+            logger.info("Guessing shortcut dir : " + desktop);
+            tfShortcut.setText(desktop);
+        }
+    }
+
+    private JPanel makePathPanel(String name, JTextField tf) {
+        tf.setText(jEdit.getProperty(OPTION_PREFIX + name + ".path"));
+        JButton pickPath = new JButton(jEdit.getProperty(OPTION_PREFIX + "choose-dir"));
+        pickPath.setActionCommand("choose-" + name);
+        JPanel pathPanel = new JPanel(new BorderLayout(0, 0));
+        pathPanel.add(tf, BorderLayout.CENTER);
+        pathPanel.add(pickPath, BorderLayout.EAST);
+        pickPath.addActionListener(this);
+        return pathPanel;
+    }
+
+
+
+    private void selectDir(String name) {
+        JFileChooser fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int res = fc.showOpenDialog(this);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            String dir = fc.getSelectedFile().getAbsolutePath();
+            pathNames.get(name).setText(dir);
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String ac = e.getActionCommand();
+        if (ac.startsWith("choose-")) {
+            selectDir(ac.substring(7));
+        }
+    }
+
+
+
+    public void _save() {
+
+        logger.info("Saving options.");
+
+        String rhome = tfRHome.getText();
+        String javahome = System.getProperty("java.home");
+        String shortcut = tfShortcut.getText();
+
+        File rhomeFile = new File(rhome);
+        File shortcutFile = new File(shortcut);
+
+        String err = null;
+        if (!rhomeFile.exists() || !rhomeFile.isDirectory()) {
+            err = "R home directory does not exist or is not a proper directory!";
+        }
+        if (!shortcutFile.exists() || !shortcutFile.isDirectory()) {
+            err = "Directory for shortcut does not exist or is not a proper directory!";
+        }
+
+        if (err != null) {
+            JOptionPane.showMessageDialog(this, err);
+        } else {
+            try {
+//                RCmdBatch rCmdBatch = new RCmdBatch(rhome);
+//                rCmdBatch.retrieveRInfo();
+//                RPackage rp = rCmdBatch.getInstalledPackInfo("rJava");
+                Installer i = new Installer(
+                        jEdit.getJEditHome(),
+                        JEditRPlugin.getPluginHomePath(), 
+                        rhome,
+//                        rp.getLibpath().getAbsolutePath(),
+//                        rCmdBatch.getLibPaths(),
+//                        javahome,
+                        shortcut
+                );
+                i.install();
+            } catch (Exception e) {
+                // todo handle
+                e.printStackTrace();
+            }
+        }
+        logger.info("Saving options done.");
+    }
+}
---
+++ plugins/orchestra/trunk/jeditr_starter.r
@@ -0,0 +1,110 @@
+#!/bin/env Rscript
+
+# {{{ Copyright/licence notice
+# :tabSize=2:indentSize=2:noTabs=false:folding=explicit:collapseFolds=1:
+# Copyright (c) 2009, Romain Francois <francoisromain@free.fr>
+# Copyright (c) 2009, Bernd Bischl <bernd_bischl@gmx.net>
+#
+# This file is part of the jeditr R package
+#
+# The jeditr R package is free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# The jeditr R package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the jeditr R package. If not, see <http://www.gnu.org/licenses/>.
+# }}}
+
+# {{{ environment variables
+SEP <- .Platform$path.sep
+
+# {{{ R_HOME
+R_HOME <- R.home()
+Sys.setenv(R_HOME = R_HOME )
+# }}}
+
+# {{{ add the plugin home to R_LIBS
+R_LIBS <- Sys.getenv( "R_LIBS", unset = "" )
+JEDIT_RLIBS <- "@PLUGIN_HOME@/library"
+# set by java jeditr installer plugin
+R_LIBS <- if( R_LIBS == "" ){
+	JEDIT_RLIBS
+} else{
+	paste( R_LIBS, JEDIT_RLIBS, sep = ":" )
+}
+Sys.setenv( R_LIBS = R_LIBS )
+# }}}
+
+# {{{ JAVA_HOME and java
+# set by java jeditr installer plugin, so we always know java_home
+JAVA_HOME <- "@JAVA_HOME@"
+java_exe <- "@JAVA_EXE@"
+java <- file.path( JAVA_HOME, "bin", java_exe )
+if( !file.exists( java ) ){
+	stop(paste("no", java_exe, "in bin dir of", JAVA_HOME))
+}
+# }}}
+
+# {{{ JEDIT_HOME and jeddit.jar
+# set by java jeditr installer plugin, so we always know jedit_home
+JEDIT_HOME <- "@JEDIT_HOME@"
+jedit.jar <- file.path( JEDIT_HOME, "jedit.jar" )
+if( !file.exists( jedit.jar ) ){
+	stop( "the jedit home directory does not contain a file called jedit.jar" )
+}
+# }}}
+
+# {{{ PATH
+Sys.setenv( PATH =
+	paste(
+		file.path( R.home(), "bin") ,
+		file.path( JAVA_HOME, "bin" ) ,
+		Sys.getenv('PATH'),
+	sep= SEP) )
+# }}}
+
+# {{{ LD_LIBRARY_PATH
+LD_LIBRARY_PATH <- Sys.getenv( "LD_LIBRARY_PATH", unset = "" )
+if( LD_LIBRARY_PATH == "" ){
+	LD_LIBRARY_PATH <- Sys.getenv( "JAVA_LIBRARY_PATH", unset = "" )
+}
+# add JRI
+JRI_LD_PATH <- paste(
+	system.file( "jri", package = "rJava" ),
+	file.path( R_HOME, "lib")
+, sep = SEP )
+LD_LIBRARY_PATH <- if( LD_LIBRARY_PATH == "" ){
+	JRI_LD_PATH
+} else {
+	paste( LD_LIBRARY_PATH, JRI_LD_PATH, sep = SEP )
+}
+Sys.setenv( LD_LIBRARY_PATH = LD_LIBRARY_PATH )
+# }}}
+
+# {{{ JEDITR_HOME
+JEDITR_HOME = system.file( package = "jeditr" )
+# }}}
+# }}}
+
+# {{{ the command
+cmd <- sprintf( '"%s" -Djava.library.path="%s" -Djeditr.home="%s" -Drhome="%s" -jar "%s" ',
+	java, LD_LIBRARY_PATH, JEDITR_HOME, R_HOME, jedit.jar )
+
+# additional parameters
+params <- commandArgs( TRUE )
+if( length( params ) ){
+	cmd <- paste( cmd, params, sep = " ")
+}
+
+# run asynchronously to suppress shell - at least necessary on windows 
+system(cmd, wait=FALSE)
+
+# }}}
+
