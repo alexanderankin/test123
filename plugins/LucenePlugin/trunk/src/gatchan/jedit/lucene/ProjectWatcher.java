afb3a833223dd2ecf26e6729b9ee8b169e06869a
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-08-23 11:07:19 +0000

    added licence and format code

--- plugins/LucenePlugin/trunk/src/gatchan/jedit/lucene/ProjectWatcher.java
+++ plugins/LucenePlugin/trunk/src/gatchan/jedit/lucene/ProjectWatcher.java
@@ -1,3 +1,23 @@
+/*
+ * ProjectWatcher.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2009 Shlomy Reinstein
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package gatchan.jedit.lucene;
 
 import javax.swing.JOptionPane;
@@ -11,30 +31,35 @@ import projectviewer.event.StructureUpdate;
 
 public class ProjectWatcher implements EBComponent
 {
-	public ProjectWatcher() {
+	public ProjectWatcher()
+	{
 		EditBus.addToBus(this);
 	}
 
-	public void handleMessage(EBMessage message) {
-		if (message instanceof StructureUpdate) {
+	public void handleMessage(EBMessage message)
+	{
+		if (message instanceof StructureUpdate)
+		{
 			StructureUpdate su = (StructureUpdate) message;
 			if (su.getType() == StructureUpdate.Type.PROJECT_REMOVED)
 				checkRemoveProjectIndex(su.getNode().getName());
 		}
 	}
 
-	private void checkRemoveProjectIndex(String project) {
+	private void checkRemoveProjectIndex(String project)
+	{
 		Index index = LucenePlugin.instance.getIndex(project);
 		if (index == null)
 			return;
-		int res = JOptionPane.showConfirmDialog(jEdit.getActiveView(), 
-			"Remove Lucene index of project '" + project + "'?",
-			"Lucene plugin", JOptionPane.YES_NO_OPTION);
+		int res = JOptionPane.showConfirmDialog(jEdit.getActiveView(),
+												"Remove Lucene index of project '" + project + "'?",
+												"Lucene plugin", JOptionPane.YES_NO_OPTION);
 		if (res == JOptionPane.YES_OPTION)
 			LucenePlugin.instance.removeIndex(project);
 	}
-	
-	public void stop() {
+
+	public void stop()
+	{
 		EditBus.removeFromBus(this);
 	}
 }
