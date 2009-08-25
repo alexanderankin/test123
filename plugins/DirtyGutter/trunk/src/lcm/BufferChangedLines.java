89218d5b75a01755fec6cca576f741268faab3f3
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-08-25 12:08:48 +0000

    When activated in the middle of a jEdit session, when buffers are dirty, only undo up to a point where the buffer is non-dirty.

--- plugins/DirtyGutter/trunk/src/lcm/BufferChangedLines.java
+++ plugins/DirtyGutter/trunk/src/lcm/BufferChangedLines.java
@@ -120,7 +120,7 @@ public class BufferChangedLines extends BufferAdapter
 			if (undoExists)
 				undoCount++;
 		}
-		while (undoExists);
+		while (undoExists && buffer.isDirty());
 		initUndo = false;	// Forced full-undo done
 		// 2. Run 'diff' against the saved file
 		if (buffer.isDirty())
