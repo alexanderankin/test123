de1c9b98a8300374801f5c32be6b1516cdadb4ce
Eric Le Lay <222469+elelay@users.noreply.github.com>
2009-08-29 17:25:48 +0000

    enforce EntityResolver2 usage in CompletionHandler

--- plugins/XML/trunk/xml/completion/CompletionInfo.java
+++ plugins/XML/trunk/xml/completion/CompletionInfo.java
@@ -208,7 +208,7 @@ public class CompletionInfo
 			{
 				XMLReader parser = new org.apache.xerces.parsers.SAXParser();
 				parser.setFeature("http://apache.org/xml/features/validation/dynamic",true);
-				//parser.setFeature("http://xml.org/sax/features/use-entity-resolver2", true);
+				parser.setFeature("http://xml.org/sax/features/use-entity-resolver2", true);
 				parser.setErrorHandler(handler);
 				parser.setEntityResolver(handler);
 				parser.setContentHandler(handler);
