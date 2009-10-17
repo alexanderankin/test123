7305421702323bb70b71a5e4dad0b54d33802864
rfrancois <rfrancois@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-10-17 16:05:21 +0000

    deploy the r modes on start()

--- plugins/orchestra/trunk/com/addictedtor/orchestra/OrchestraPlugin.java
+++ plugins/orchestra/trunk/com/addictedtor/orchestra/OrchestraPlugin.java
@@ -1,5 +1,13 @@
 package com.addictedtor.orchestra ;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import org.af.commons.io.FileTransfer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.FileAppender;
@@ -10,9 +18,6 @@ import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.options.PluginOptions;
-
-import javax.swing.*;
-import java.io.File;
 
 /**
  * Main class of the orchestra installer plugin
@@ -36,7 +41,12 @@ public class OrchestraPlugin extends EBPlugin {
      */
     @Override
     public void start() {
-        configureLog4J();
+    	configureLog4J();
+        
+    	try{
+    		deployModes() ;
+    	} catch( IOException e){}
+    	
     	String orchestra_rpackage_home = System.getProperty("orchestra.home", "") ;
         if( orchestra_rpackage_home.equals("") ){
         	if( isConfigured() ){
@@ -122,6 +132,24 @@ public class OrchestraPlugin extends EBPlugin {
     	return startup.exists() ;
     }
     
-    
+    private static void deployModes() throws IOException {
+    	String path = getPluginHomePath() ;
+    	File modes_dir = new File( path + "/modes" ) ;
+    	if( !modes_dir.exists() ){
+    		modes_dir.mkdirs() ;
+    	}
+    	StringTokenizer modes = new StringTokenizer( jEdit.getProperty("orchestra.rmodes") );
+    	String mode = null ;
+    	while( modes.hasMoreElements() ){
+    		mode = modes.nextToken() ;
+    		FileTransfer.copyResourceToLocalDir( 
+    				OrchestraPlugin.class.getResource("/modes/"+ mode + ".xml"),
+    				mode + ".xml", modes_dir );
+
+    	}
+    	FileTransfer.copyResourceToLocalDir( 
+				OrchestraPlugin.class.getResource("/modes/catalog"), "catalog", modes_dir );
+
+    }
 }
 
