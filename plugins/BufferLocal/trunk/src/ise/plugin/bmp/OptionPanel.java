cab106075601ab9718a9673b40e517724430a552
Dale <danson@grafidog.com>
2009-10-18 04:38:17 +0000

    optionally stop timer when jEdit is inactive

--- plugins/BufferLocal/trunk/src/ise/plugin/bmp/OptionPanel.java
+++ plugins/BufferLocal/trunk/src/ise/plugin/bmp/OptionPanel.java
@@ -16,12 +16,15 @@ public class OptionPanel extends AbstractOptionPane {
 
     private JCheckBox removeStale = null;
     private JSpinner spinner = null;
+    private JCheckBox whileActive = null;
 
     public OptionPanel( String name ) {
         super( name );
     }
 
     public void _init() {
+        setBorder( BorderFactory.createEmptyBorder( 6, 6, 6, 6 ) );
+
         //setName("BufferLocalOptions");
         addComponent( new JLabel( "<html><h3>BufferLocal</h3>" ) );
         removeStale = new JCheckBox( "Close files not used" );
@@ -30,14 +33,20 @@ public class OptionPanel extends AbstractOptionPane {
         int stale_value = jEdit.getIntegerProperty( "bufferlocal.staleTime", 30 );
         if ( stale_value < 1 ) {
             stale_value = 30;
-            jEdit.setIntegerProperty("bufferlocal.staleTime", 30);
+            jEdit.setIntegerProperty( "bufferlocal.staleTime", 30 );
         }
         spinner = new JSpinner( new SpinnerNumberModel( stale_value, 1, Integer.MAX_VALUE, 1 ) );
         addComponent( "for this many minutes", spinner );
+
+        whileActive = new JCheckBox( "while window is active." );
+        whileActive.setSelected( jEdit.getBooleanProperty( "bufferlocal.whileActive", false ) );
+        addComponent( whileActive );
+
     }
 
     public void _save() {
         jEdit.setBooleanProperty( "bufferlocal.removeStale", removeStale.isSelected() );
         jEdit.setIntegerProperty( "bufferlocal.staleTime", ( ( Number ) spinner.getValue() ).intValue() );
+        jEdit.setBooleanProperty( "bufferlocal.whileActive", whileActive.isSelected() );
     }
 }
