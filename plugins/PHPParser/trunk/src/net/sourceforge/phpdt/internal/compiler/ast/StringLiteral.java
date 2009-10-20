2a2b84b838d10ff4fc703e0512b0914019da3547
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-10-20 16:26:55 +0000

    fixed coding style

--- plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/StringLiteral.java
+++ plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/StringLiteral.java
@@ -1,83 +1,103 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2001, 2002 International Business Machines Corp. and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v0.5
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
+/*
+ * StringLiteral.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- ******************************************************************************/
+ * Copyright (C) 2003, 2009 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package net.sourceforge.phpdt.internal.compiler.ast;
 
 import java.util.List;
 
 import gatchan.phpparser.parser.Token;
-import gatchan.phpparser.parser.Token;
 import gatchan.phpparser.parser.PHPParser;
 
-public final class StringLiteral extends Literal {
-  private String source;
+public final class StringLiteral extends Literal
+{
+	private String source;
 
-  private AbstractVariable[] variablesInside;
+	private Expression[] expressions;
 
-  public StringLiteral(Token token) {
-    this(token.image,
-         token.sourceStart,
-         token.sourceEnd,
-         token.beginLine,
-         token.endLine,
-         token.beginColumn,
-         token.endColumn,
-         null);
-  }
+	public StringLiteral(Token token)
+	{
+		this(token.image,
+		     token.sourceStart,
+		     token.sourceEnd,
+		     token.beginLine,
+		     token.endLine,
+		     token.beginColumn,
+		     token.endColumn,
+		     null);
+	}
 
-  public StringLiteral(String source,
-                       int sourceStart,
-                       int sourceEnd,
-                       int beginLine,
-                       int endLine,
-                       int beginColumn,
-                       int endColumn) {
-    this(source, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn, null);
-  }
+	public StringLiteral(String source,
+			     int sourceStart,
+			     int sourceEnd,
+			     int beginLine,
+			     int endLine,
+			     int beginColumn,
+			     int endColumn)
+	{
+		this(source, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn, null);
+	}
 
-  public StringLiteral(String source,
-                       int sourceStart,
-                       int sourceEnd,
-                       int beginLine,
-                       int endLine,
-                       int beginColumn,
-                       int endColumn,
-                       AbstractVariable[] variablesInside) {
-    super(Type.STRING, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
-    this.source = source;
-    this.variablesInside = variablesInside;
-  }
+	public StringLiteral(String source,
+			     int sourceStart,
+			     int sourceEnd,
+			     int beginLine,
+			     int endLine,
+			     int beginColumn,
+			     int endColumn,
+			     Expression[] expressions)
+	{
+		super(Type.STRING, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
+		this.source = source;
+		this.expressions = expressions;
+	}
 
-  /**
-   * Return the expression as String.
-   *
-   * @return the expression
-   */
-  public String toStringExpression() {
-    return source;
-  }
+	/**
+	 * Return the expression as String.
+	 *
+	 * @return the expression
+	 */
+	public String toStringExpression()
+	{
+		return source;
+	}
 
-  public void getUsedVariable(List list) {
-    if (variablesInside != null) {
-      for (int i = 0; i < variablesInside.length; i++) {
-        variablesInside[i].getUsedVariable(list);
-      }
-    }
-  }
+	public void getUsedVariable(List list)
+	{
+		if (expressions != null)
+		{
+			for (Expression expression : expressions)
+			{
+				expression.getUsedVariable(list);
+			}
+		}
+	}
 
-  public void analyzeCode(PHPParser parser) {
-    if (variablesInside != null) {
-      for (int i = 0; i < variablesInside.length; i++) {
-        variablesInside[i].analyzeCode(parser);
-      }
-    }
-  }
+	public void analyzeCode(PHPParser parser)
+	{
+		if (expressions != null)
+		{
+			for (Expression expression : expressions)
+			{
+				expression.analyzeCode(parser);
+			}
+		}
+	}
 
 }
