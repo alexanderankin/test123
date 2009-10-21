a5b172900a02b0a20d9684b51bd70a6aef17926e
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-10-21 13:41:07 +0000

    first commit

---
+++ plugins/Checkstyle/trunk/src/com/kpouer/jedit/checkstyle/CheckstylePlugin.java
@@ -0,0 +1,56 @@
+/*
+ * CheckstylePlugin.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2009 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.kpouer.jedit.checkstyle;
+
+import errorlist.DefaultErrorSource;
+import errorlist.ErrorSource;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.io.VFSManager;
+import org.gjt.sp.jedit.jEdit;
+
+/**
+ * @author Matthieu Casanova
+ */
+public class CheckstylePlugin extends EditPlugin
+{
+	private DefaultErrorSource errorSource;
+	@Override
+	public void start()
+	{
+		errorSource = new DefaultErrorSource("checkstyle");
+		ErrorSource.registerErrorSource(errorSource);
+	}
+
+	@Override
+	public void stop()
+	{
+		ErrorSource.unregisterErrorSource(errorSource);
+		errorSource = null;
+	}
+
+	public static void checkBuffer(Buffer buffer)
+	{
+		CheckstylePlugin plugin = (CheckstylePlugin) jEdit.getPlugin(CheckstylePlugin.class.getName());
+		CheckstyleParse parser = new CheckstyleParse(buffer, plugin.errorSource);
+		VFSManager.runInWorkThread(parser);
+	}
+}
