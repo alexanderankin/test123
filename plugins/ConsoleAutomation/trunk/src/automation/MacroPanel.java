7feda43a846ed09d282554d70037a9375981c628
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-11-16 13:58:36 +0000

    Added an interface to abort a running script. This is done by generating an InterruptedException. Didn't get to test it yet. Added a "Refresh" button to the macros dockable.

--- plugins/ConsoleAutomation/trunk/src/automation/MacroPanel.java
+++ plugins/ConsoleAutomation/trunk/src/automation/MacroPanel.java
@@ -1,12 +1,15 @@
 package automation;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Vector;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -21,26 +24,17 @@ public class MacroPanel extends JPanel {
 	private JTree tree;
 	private DefaultTreeModel model;
 	private DefaultMutableTreeNode root;
+	private JButton refresh;
+	private ConsoleAutomationPlugin plugin;
 
 	public MacroPanel()
 	{
-		setLayout(new BorderLayout());
-		final ConsoleAutomationPlugin plugin = (ConsoleAutomationPlugin)
+		plugin = (ConsoleAutomationPlugin)
 			jEdit.getPlugin("automation.ConsoleAutomationPlugin");
+		setLayout(new BorderLayout());
 		root = new DefaultMutableTreeNode();
 		model = new DefaultTreeModel(root);
-		HashMap<String, Vector<String>> macros = plugin.getMacros();
-		Set<String> keys = macros.keySet();
-		for (String key: keys)
-		{
-			DefaultMutableTreeNode child = new DefaultMutableTreeNode(key);
-			root.add(child);
-			Vector<String> keyMacros = macros.get(key);
-			if (keyMacros == null)
-				continue;
-			for (String macro: keyMacros)
-				child.add(new DefaultMutableTreeNode(macro));
-		}
+		populateTree();
 		tree = new JTree(model);
 		add(tree, BorderLayout.CENTER);
 		tree.setRootVisible(false);
@@ -60,5 +54,31 @@ public class MacroPanel extends JPanel {
 					plugin.editMacro(key, macro);
 			}
 		});
+		refresh = new JButton("Refresh");
+		add(refresh, BorderLayout.SOUTH);
+		refresh.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				populateTree();
+				model.nodeStructureChanged(root);
+			}
+		});
+	}
+
+	private void populateTree()
+	{
+		root.removeAllChildren();
+		HashMap<String, Vector<String>> macros = plugin.getMacros();
+		Set<String> keys = macros.keySet();
+		for (String key: keys)
+		{
+			DefaultMutableTreeNode child = new DefaultMutableTreeNode(key);
+			root.add(child);
+			Vector<String> keyMacros = macros.get(key);
+			if (keyMacros == null)
+				continue;
+			for (String macro: keyMacros)
+				child.add(new DefaultMutableTreeNode(macro));
+		}
 	}
 }
