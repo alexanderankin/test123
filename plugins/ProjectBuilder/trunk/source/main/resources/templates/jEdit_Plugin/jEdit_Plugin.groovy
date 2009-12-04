0766f79c1714b26ae91a01fd6aae9234fe0bf781
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2009-12-04 22:54:10 +0000

    Updating jEdit plugin templates so the directory structure is simpler, and the rendered build.xml file has examples on how to add dependencies.
    Also updated the props file to set the name to "Project Builder" not "ProjectBuilder" (no space)

--- plugins/ProjectBuilder/trunk/source/main/resources/templates/jEdit_Plugin/jEdit_Plugin.groovy
+++ plugins/ProjectBuilder/trunk/source/main/resources/templates/jEdit_Plugin/jEdit_Plugin.groovy
@@ -11,27 +11,25 @@ templateData.project = project
 templateData.userName = System.getProperty("user.name")
 
 PB.build(project.name, project.directory.path) {
-   d("conf") {
-      f("${project.name}.props", template: "${templateDir}/PluginProps.template", templateData: templateData)
-      f("actions.xml")
-      f("dockables.xml")
-      f("services.xml")
-   }
    d("docs") {
       f("${project.name}.html")
    }
+   d("lib")
    d("src") {
-      d("main") {
-         d(pluginPackage) {
-            f("${pluginClass}.java", template: "${templateDir}/PluginClass.template", templateData: templateData)
-         }
-      }
-      d("test") {
-         d(pluginPackage) {
-            f("${pluginClass}Tests.java")
-         }
+      d(pluginPackage) {
+         f("${pluginClass}.java", template: "${templateDir}/PluginClass.template", templateData: templateData)
       }
    }
+   d("test") {
+      d(pluginPackage) {
+         f("${pluginClass}Tests.java")
+      }
+   }
+   f("actions.xml")
+   f("dockables.xml")
+   f("services.xml")
+   f("${project.name}.props", template: "${templateDir}/PluginProps.template", templateData: templateData)
    f("build.xml", template: "${templateDir}/PluginBuild.template", templateData: templateData)
    f("sample.build.properties")
 }
+/* ::mode=groovy:folding=indent:noTabs=true:: */
