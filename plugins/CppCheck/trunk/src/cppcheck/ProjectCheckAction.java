9569cae8d65269932b0f19a183e6909c2bfe0d7c
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-01-25 07:37:30 +0000

    Updated for the change in ProjectViewer API which removed the project locking methods.

--- plugins/CppCheck/trunk/src/cppcheck/ProjectCheckAction.java
+++ plugins/CppCheck/trunk/src/cppcheck/ProjectCheckAction.java
@@ -7,10 +7,6 @@ import java.awt.event.ActionEvent;
 import java.util.Collection;
 import java.util.Vector;
 
-import javax.swing.JOptionPane;
-
-import org.gjt.sp.jedit.jEdit;
-
 import projectviewer.action.Action;
 import projectviewer.vpt.VPTNode;
 import projectviewer.vpt.VPTProject;
@@ -36,12 +32,6 @@ public class ProjectCheckAction extends Action
 			VPTProject project = VPTNode.findProjectFor(node);
 			if (project == null)
 				return;
-			if (! project.tryLock())
-			{
-				JOptionPane.showMessageDialog(viewer.getView(),
-					jEdit.getProperty(Plugin.CANNOT_LOCK_PROJECT_PROP));
-				return;
-			}
 			Collection<VPTNode> nodes = project.getOpenableNodes();
 			Vector<String> files = new Vector<String>();
 			for (VPTNode n: nodes)
@@ -49,7 +39,6 @@ public class ProjectCheckAction extends Action
 				if (n.isFile())
 					files.add(n.getNodePath());
 			}
-			project.unlock();
 			Plugin.checkPaths(viewer.getView(), files);
 		}
 	}
