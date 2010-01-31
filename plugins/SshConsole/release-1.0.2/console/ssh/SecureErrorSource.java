68d886fb158dbf10bb22b1266996a1e6e0706aa3
Alan Ezust <1688991+ezust@users.noreply.github.com>
2010-01-31 22:47:13 +0000

    Tagging 1.0.2 for release.

---
+++ plugins/SshConsole/release-1.0.2/console/ssh/SecureErrorSource.java
@@ -0,0 +1,71 @@
+package console.ssh;
+// :folding=explicit:
+import javax.swing.SwingUtilities;
+
+import org.gjt.sp.jedit.EditBus;
+import org.gjt.sp.util.Log;
+
+import errorlist.DefaultErrorSource;
+import errorlist.ErrorSource;
+import errorlist.ErrorSourceUpdate;
+
+
+// {{{ class SecureErrorSource
+/**
+    A customized ErrorSource which prepends the proper sftp:// 
+    address that corresponds to the current connection
+    @author ezust
+    @version $Id$
+*/
+public class SecureErrorSource extends DefaultErrorSource
+{
+	
+	ConsoleState consoleState;
+	
+	public SecureErrorSource(ConsoleState cs) {
+		super("sshconsole");
+		ErrorSource.registerErrorSource(this);
+		consoleState = cs;
+	}
+
+	
+    // {{{ addError() method
+	public synchronized void addError(final DefaultError error)
+	{
+		if (error.getErrorSource() != this) {
+			Log.log(Log.WARNING, this, "Why different error source?");
+			ErrorSource.registerErrorSource(error.getErrorSource());
+		}
+		
+		String path = error.getFilePath();
+		if (!path.startsWith("sftp:")) {
+			String base = ConnectionManager.extractBase(consoleState.getPath());
+			path = base + path;
+			error.setFilePath(path);
+		}
+		ErrorListForPath list = errors.get(path);
+		if(list == null)
+		{
+			list = new ErrorListForPath();
+			errors.put(path, list);
+		}
+		if(list.add(error))
+		{
+			errorCount++;
+			removeOrAddToBus();
+			if(registered)
+			{
+				SwingUtilities.invokeLater(new Runnable()
+				{
+					public void run()
+					{
+						ErrorSourceUpdate message = new ErrorSourceUpdate(SecureErrorSource.this,
+							ErrorSourceUpdate.ERROR_ADDED,error);
+						EditBus.send(message);
+					}
+				});
+			}
+		}
+	} //}}}
+
+} // }}}
