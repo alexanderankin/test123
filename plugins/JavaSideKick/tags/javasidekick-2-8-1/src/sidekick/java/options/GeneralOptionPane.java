5464b73ad42c1de70ca3882ff3683c17d1a40096
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-02-02 19:17:00 +0000

    Tagging for release.

---
+++ plugins/JavaSideKick/tags/javasidekick-2-8-1/src/sidekick/java/options/GeneralOptionPane.java
@@ -0,0 +1,42 @@
+package sidekick.java.options;
+
+import javax.swing.*;
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+public class GeneralOptionPane extends AbstractOptionPane {
+
+    JLabel titleLabel = new JLabel( jEdit.getProperty("options.sidekick.java.generalOptions", "<html><b>General Options</b>") );
+    private JCheckBox showErrorsInErrorList;
+
+    public GeneralOptionPane() {
+        super( jEdit.getProperty("options.sidekick.java.general.label", "General") );
+    }
+
+    protected void _init() {
+        installComponents();
+        installDefaults();
+    }
+
+    /**
+     * Create and add all GUI components.    
+     */
+    private void installComponents() {
+        setBorder( BorderFactory.createEmptyBorder( 11, 11, 12, 12 ) );
+        addComponent( titleLabel );
+        addComponent( Box.createVerticalStrut( 11 ) );
+        showErrorsInErrorList = new JCheckBox( jEdit.getProperty( "options.sidekick.java.showErrors", "Show parse errors in ErrorList" ) );
+        addComponent( showErrorsInErrorList );
+    }
+
+    /**
+     * Set/restore values from jEdit properties.    
+     */
+    private void installDefaults() {
+        showErrorsInErrorList.setSelected( jEdit.getBooleanProperty( "sidekick.java.showErrors", true ) );
+    }
+
+    protected void _save() {
+        jEdit.setBooleanProperty( "sidekick.java.showErrors", showErrorsInErrorList.isSelected() );
+    }
+}
