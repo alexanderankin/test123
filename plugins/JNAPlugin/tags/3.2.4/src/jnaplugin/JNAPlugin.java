8a298eee304e48f668866d6d31d47f6ad473f15a
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-02-04 00:42:00 +0000

    First release of JNA dependency plugin.

---
+++ plugins/JNAPlugin/tags/3.2.4/src/jnaplugin/JNAPlugin.java
@@ -0,0 +1,16 @@
+package jnaplugin;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class JNAPlugin extends EditPlugin {
+
+	public void start() {
+		// Nothing to do here - this is a dependency plugin.
+	}
+
+	public void stop() {
+		// Nothing to do here - this is a dependency plugin.
+	}
+
+}
+/* ::mode=java:: */
