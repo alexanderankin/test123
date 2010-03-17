e3bbe04ad607ba0134a39f15b12eb556e4f8a8f0
Eric Le Lay <222469+elelay@users.noreply.github.com>
2010-03-17 19:26:22 +0000

    tagging for release

---
+++ plugins/XML/tags/release-2-6-1/sidekick/ecmascript/parser/Scope.java
@@ -0,0 +1,21 @@
+/*
+ Copyright (c) 2004-2005, The Dojo Foundation
+ All Rights Reserved.
+
+ Licensed under the Academic Free License version 2.1 or above OR the
+ modified BSD license. For more information on Dojo licensing, see:
+
+ http://dojotoolkit.org/community/licensing.shtml <http://dojotoolkit.org/community/licensing.shtml>
+
+ Code donated to the Dojo Foundation by AOL LLC under the terms of
+ the Dojo CCLA (http://dojotoolkit.org/ccla.txt).
+
+ */
+package sidekick.ecmascript.parser;
+
+/**
+ * Empty interface marks AST nodes as nodes defining javascript scope
+ * boundaries.
+ */
+public interface Scope {
+}
