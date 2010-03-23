becf293719a871d9cc4bcc9feb2ba91454b1e723
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-03-23 17:07:55 +0000

    Initial commit.

---
+++ plugins/ClojureScriptEnginePlugin/trunk/src/scripting/engines/clojure/ClojureScriptEnginePlugin.java
@@ -0,0 +1,29 @@
+package scripting.engines.clojure;
+
+import clojure.contrib.jsr223.ClojureScriptEngineFactory;
+import com.townsfolkdesigns.jedit.plugins.scripting.ScriptEngineService;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.Mode;
+import org.gjt.sp.jedit.jEdit;
+
+public class ClojureScriptEnginePlugin extends EditPlugin implements ScriptEngineService {
+   
+   private ClojureScriptEngineFactory factory = new ClojureScriptEngineFactory();
+
+   
+   public Class getEngineFactoryClass() {
+      return factory.getClass();
+   }
+
+   public Mode getMode() {
+      return jEdit.getMode("clojure");
+   }
+
+   public void start() {
+   }
+
+   public void stop() {
+   }
+
+}
+/* ::mode=java:noTabs=true:tabSize=3:indentSize=3:: */
