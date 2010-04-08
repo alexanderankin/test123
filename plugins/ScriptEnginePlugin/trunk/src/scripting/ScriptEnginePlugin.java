3d2fa043310de8bbb8a29157021a68f130b10e4a
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-04-08 19:28:31 +0000

    Reorganizing ScriptEnginePlugin. Shortening packages, moving to more standard plugin layout.

--- plugins/ScriptEnginePlugin/trunk/src/scripting/ScriptEnginePlugin.java
+++ plugins/ScriptEnginePlugin/trunk/src/scripting/ScriptEnginePlugin.java
@@ -19,7 +19,7 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
-package com.townsfolkdesigns.jedit.plugins.scripting;
+package scripting;
 
 
 import org.gjt.sp.jedit.EditPlugin;
