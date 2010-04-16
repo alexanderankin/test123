0a9dd23377a90f158b459904c9f887dcbcd63f22
elberry <elberry@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-04-16 20:10:29 +0000

    Initial import

---
+++ plugins/BZRPlugin/trunk/src/bzrplugin/BZRPlugin.groovy
@@ -0,0 +1,16 @@
+package bzrplugin
+
+import org.gjt.sp.jedit.EditPlugin
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: eberry
+ * Date: Apr 16, 2010
+ * Time: 12:57:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class BZRPlugin extends EditPlugin {
+   void start() {}
+
+   void stop() {}
+}
