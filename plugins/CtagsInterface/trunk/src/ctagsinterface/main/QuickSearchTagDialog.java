692c35775f7e9a252210fa08002e9f3b520681ae
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-05-18 04:52:44 +0000

    Applied patch #3002514: Custom ctags search actions

--- plugins/CtagsInterface/trunk/src/ctagsinterface/main/QuickSearchTagDialog.java
+++ plugins/CtagsInterface/trunk/src/ctagsinterface/main/QuickSearchTagDialog.java
@@ -42,22 +42,33 @@ public class QuickSearchTagDialog extends JDialog {
 		SUBSTRING,
 		PREFIX
 	};
-	Mode mode;
-	JTextField name;
-	JList tags;
-	DefaultListModel model;
-	View view;
-	Vector<QuickSearchTag> tagNames;
-	Query baseQuery;
-	Timer filterTimer;
-	
+	private Mode mode;
+	private JTextField name;
+	private JList tags;
+	private DefaultListModel model;
+	private View view;
+	private Vector<QuickSearchTag> tagNames;
+	private Query baseQuery;
+	private Timer filterTimer;
+	private String query;
+	private boolean showImmediately;
+
 	/** This window will contains the scroll with the items. */
-	private final JWindow window = new JWindow(this);
-	
-	public QuickSearchTagDialog(View view, Mode mode) {
-		super(view, "Search tag", false);
+	final JWindow window = new JWindow(this);
+
+	public QuickSearchTagDialog(View view, Mode mode)
+	{
+		this(view, mode, "Search tag", null, false);
+	}
+
+	public QuickSearchTagDialog(View view, Mode mode, String title,
+		String query, boolean showImmediately)
+	{
+		super(view, title, false);
 		this.view = view;
 		this.mode = mode;
+		this.query = query;
+		this.showImmediately = showImmediately;
 		JPanel p = new JPanel();
 		p.add(new JLabel("Type part of the tag name:"));
 		name = new JTextField(30);
@@ -101,7 +112,7 @@ public class QuickSearchTagDialog extends JDialog {
 			public void keyTyped(KeyEvent e) {
 				name.dispatchEvent(e);
 			}
-				    
+
 			public void keyPressed(KeyEvent e) {
 				if (!handledByList(e)) {
 					name.dispatchEvent(e);
@@ -114,7 +125,7 @@ public class QuickSearchTagDialog extends JDialog {
 		});
 		filterTimer = new Timer(500, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				delayedSetFilter();
+				applyFilter();
 			}
 		});
 		filterTimer.setRepeats(false);
@@ -122,33 +133,40 @@ public class QuickSearchTagDialog extends JDialog {
 		pack();
 		setLocationRelativeTo(view);
 		setVisible(true);
+		if (showImmediately)
+			applyFilter();
 	}
 
 	private void prepareData() {
 		TagDB db = CtagsInterfacePlugin.getDB();
-		Query q = new Query();
-		q.setColumns(new Object [] {TagDB.TAGS_TABLE + ".*", TagDB.FILES_NAME});
-		q.setTables(new Object [] {TagDB.TAGS_TABLE, TagDB.FILES_TABLE});
-		q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
-			db.field(TagDB.FILES_TABLE, TagDB.FILES_ID));
-		
-		if (ProjectsOptionPane.getSearchActiveProjectOnly()) {
-			String project = CtagsInterfacePlugin.getProjectWatcher().getActiveProject(view);
-			if (project != null) {
-				q.addTable(TagDB.MAP_TABLE);
-				q.addTable(TagDB.ORIGINS_TABLE);
-				q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
-					db.field(TagDB.FILES_TABLE, TagDB.FILES_ID));
-				q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
-					db.field(TagDB.MAP_TABLE, TagDB.MAP_FILE_ID));
-				q.addCondition(db.field(TagDB.MAP_TABLE, TagDB.MAP_ORIGIN_ID) + "=" +
-					db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_ID));
-				q.addCondition(db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_TYPE) + "=" +
-					TagDB.quote(TagDB.PROJECT_ORIGIN));
-				q.addCondition(db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_NAME) + "=" +
-					TagDB.quote(project));
+		Query q;
+		if (query == null) {
+			q = new Query();
+			q.setColumns(new Object [] {TagDB.TAGS_TABLE + ".*", TagDB.FILES_NAME});
+			q.setTables(new Object [] {TagDB.TAGS_TABLE, TagDB.FILES_TABLE});
+			q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
+				db.field(TagDB.FILES_TABLE, TagDB.FILES_ID));
+	
+			if (ProjectsOptionPane.getSearchActiveProjectOnly()) {
+				String project = CtagsInterfacePlugin.getProjectWatcher().getActiveProject(view);
+				if (project != null) {
+					q.addTable(TagDB.MAP_TABLE);
+					q.addTable(TagDB.ORIGINS_TABLE);
+					q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
+						db.field(TagDB.FILES_TABLE, TagDB.FILES_ID));
+					q.addCondition(db.field(TagDB.TAGS_TABLE, TagDB.TAGS_FILE_ID) + "=" +
+						db.field(TagDB.MAP_TABLE, TagDB.MAP_FILE_ID));
+					q.addCondition(db.field(TagDB.MAP_TABLE, TagDB.MAP_ORIGIN_ID) + "=" +
+						db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_ID));
+					q.addCondition(db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_TYPE) + "=" +
+						TagDB.quote(TagDB.PROJECT_ORIGIN));
+					q.addCondition(db.field(TagDB.ORIGINS_TABLE, TagDB.ORIGINS_NAME) + "=" +
+						TagDB.quote(project));
+				}
 			}
 		}
+		else
+			q = new Query(query);
 		switch (mode) {
 		case SUBSTRING:
 			try {
@@ -169,39 +187,43 @@ public class QuickSearchTagDialog extends JDialog {
 		}
 	}
 
-	protected void jumpToSelected() {
+	private void jumpToSelected() {
 		QuickSearchTag t = (QuickSearchTag) tags.getSelectedValue();
 		if (t != null)
 			CtagsInterfacePlugin.jumpTo(view, t.file, t.line);
 		dispose();
 	}
 
-	protected void setFilter() {
+	private void setFilter() {
 		if (filterTimer.isRunning())
 			filterTimer.restart();
 		else
 			filterTimer.start();
 	}
-	
-	protected void delayedSetFilter() {
+
+	private void applyFilter() {
 		model.removeAllElements();
-		String input = name.getText();
-		if (! input.isEmpty()) {
+		String input = name.getText().toLowerCase();
+		if (showImmediately || (! input.isEmpty())) {
 			switch (mode) {
 			case SUBSTRING:
 				for (int i = 0; i < tagNames.size(); i++) {
 					QuickSearchTag t = tagNames.get(i);
-					if (t.name.contains(input))
+					if (t.name.toLowerCase().contains(input.toLowerCase()))
 						model.addElement(t);
 				}
 				break;
 			case PREFIX:
 				TagDB db = CtagsInterfacePlugin.getDB();
 				Vector<Object> conditions = baseQuery.getConditions();
-				Object prefixCondition = db.field(TagDB.TAGS_TABLE, TagDB.TAGS_NAME) +
-					" LIKE " + TagDB.quote(input + "%"); 
-				conditions.add(prefixCondition);
-				baseQuery.setConditions(conditions);
+				Object prefixCondition = null;
+				if (! input.isEmpty()) {
+					prefixCondition = "LOWER(" +
+						db.field(TagDB.TAGS_TABLE, TagDB.TAGS_NAME) +
+						") LIKE " + TagDB.quote(input.toLowerCase() + "%");
+					conditions.add(prefixCondition);
+					baseQuery.setConditions(conditions);
+				}
 				try {
 					ResultSet rs = db.query(baseQuery);
 					if (rs == null)
@@ -212,8 +234,10 @@ public class QuickSearchTagDialog extends JDialog {
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-				conditions.remove(prefixCondition);
-				baseQuery.setConditions(conditions);
+				if (prefixCondition != null) {
+					conditions.remove(prefixCondition);
+					baseQuery.setConditions(conditions);
+				}
 				break;
 			}
 		}
@@ -228,14 +252,14 @@ public class QuickSearchTagDialog extends JDialog {
 			window.setVisible(true);
 		}
 	}
-	
+
 	private static boolean handledByList(KeyEvent e) {
 		return e.getKeyCode() == KeyEvent.VK_DOWN ||
 		e.getKeyCode() == KeyEvent.VK_UP ||
 		e.getKeyCode() == KeyEvent.VK_PAGE_DOWN ||
 		e.getKeyCode() == KeyEvent.VK_PAGE_UP;
 	}
-	
+
 	public void setVisible(boolean b) {
 		Rectangle bounds = getBounds();
 		window.setLocation(bounds.x, bounds.y + bounds.height);
@@ -243,7 +267,7 @@ public class QuickSearchTagDialog extends JDialog {
 		window.setVisible(false);
 		super.setVisible(b);
 	}
-	
+
 	static private class QuickSearchTag {
 		String file;
 		int line;
