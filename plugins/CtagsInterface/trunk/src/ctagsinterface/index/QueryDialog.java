beb40f8da46983e911a95c64d248bd64642b34ab
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-05-24 03:06:15 +0000

    Added the query dialog.

---
+++ plugins/CtagsInterface/trunk/src/ctagsinterface/index/QueryDialog.java
@@ -0,0 +1,65 @@
+package ctagsinterface.index;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Fieldable;
+
+import ctagsinterface.index.TagIndex.DocHandler;
+import ctagsinterface.main.CtagsInterfacePlugin;
+
+public class QueryDialog extends JFrame {
+
+	private JTextField query;
+	private DefaultListModel model;
+	private JList list;
+
+	public QueryDialog(JFrame parent)
+	{
+		setTitle("Query Dialog");
+		setLayout(new BorderLayout());
+		JPanel p = new JPanel();
+		add(p, BorderLayout.NORTH);
+		p.setLayout(new BorderLayout());
+		p.add(new JLabel("Query:"), BorderLayout.WEST);
+		query = new JTextField();
+		p.add(query, BorderLayout.CENTER);
+		model = new DefaultListModel();
+		list = new JList(model);
+		add(new JScrollPane(list), BorderLayout.CENTER);
+		JButton go = new JButton("Search");
+		p.add(go, BorderLayout.EAST);
+		go.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				model.removeAllElements();
+				CtagsInterfacePlugin.getIndex().runQuery(query.getText(),
+					1000000, new DocHandler()
+				{
+					public void handle(Document doc)
+					{
+						String s = "";
+						for (Fieldable f: doc.getFields())
+							s += f.name() + ":" + f.stringValue() + "  ||  ";
+						model.addElement(s);
+					}
+				});
+			}
+		});
+		pack();
+		setVisible(true);
+	}
+}
