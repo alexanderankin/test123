42277aa15a0ab5f31c3f47468d93f4e846187e4b
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-05-27 15:25:21 +0000

    Adding a missing file.

---
+++ plugins/CtagsInterface/trunk/src/ctagsinterface/main/Logger.java
@@ -0,0 +1,25 @@
+package ctagsinterface.main;
+
+import ctagsinterface.dockables.Progress;
+
+public class Logger
+{
+	private String name;
+	private Progress progress;
+
+	public Logger(String name, Progress progress)
+	{
+		this.name = name;
+		this.progress = progress;
+	}
+
+	public String name()
+	{
+		return name;
+	}
+
+	public void log(String s)
+	{
+		progress.add(this, s);
+	}
+}
