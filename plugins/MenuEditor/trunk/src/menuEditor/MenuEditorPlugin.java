bf99f9563eca7f41eda1b9ba0f2d879e2d673700
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-08-03 20:36:53 +0000

    use the MenuEditor directly in the action.

--- plugins/MenuEditor/trunk/src/menuEditor/MenuEditorPlugin.java
+++ plugins/MenuEditor/trunk/src/menuEditor/MenuEditorPlugin.java
@@ -1,13 +1,9 @@
 package menuEditor;
 
-import javax.swing.JDialog;
-
 import org.gjt.sp.jedit.EditPlugin;
-import org.gjt.sp.jedit.View;
 
 public class MenuEditorPlugin extends EditPlugin
 {
-	
 	static public final String OPTION = "options.menuEditor.";
 	static public final String MESSAGE = "messages.menuEditor.";
 
@@ -17,10 +13,5 @@ public class MenuEditorPlugin extends EditPlugin
 
 	public void stop()
 	{
-	}
-
-	public static void showMenuEditor(View view)
-	{
-		new MenuEditor(view);
 	}
 }
