379c099ece52b57f8f9423bae8c807ec219e369e
Dale <danson@grafidog.com>
2010-08-16 23:07:39 +0000

    Doc updates.

--- plugins/EclipseIcons/trunk/src/eclipseicons/EclipseIconsPlugin.java
+++ plugins/EclipseIcons/trunk/src/eclipseicons/EclipseIconsPlugin.java
@@ -9,7 +9,13 @@ import javax.swing.ImageIcon;
 public class EclipseIconsPlugin extends EditPlugin {
 
     private static final HashMap<String, ImageIcon> iconCache = new HashMap<String, ImageIcon>();
-
+    
+    /**
+     * Get an ImageIcon from the given filename.
+     * @param filename The name of a file as shown in the documentation.  All that is
+     * required is the filename, e.g. methpub_obj.gif, no path is necessary.
+     * @return The corresponding ImageIcon or null if there is no icon for the filename.
+     */
     public static ImageIcon getIcon(String filename) {
         ImageIcon icon = iconCache.get(filename);
         if (icon == null) {
