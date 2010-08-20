2e9728eaab342ad0bace39c3156d07d185f0457a
Damien Radtke <git@damienradtke.com>
2010-08-20 17:44:01 +0000

    Initial upload

---
+++ plugins/ProcessShell/trunk/src/procshell/ProcessShellPlugin.java
@@ -0,0 +1,13 @@
+package procshell;
+/**
+ * @author Damien Radtke
+ * class ProcessShellPlugin
+ * TODO: comment
+ */
+//{{{ Imports
+import org.gjt.sp.jedit.EditPlugin;
+//}}}
+public class ProcessShellPlugin extends EditPlugin {
+	public void start() {}
+	public void stop() {}
+}
