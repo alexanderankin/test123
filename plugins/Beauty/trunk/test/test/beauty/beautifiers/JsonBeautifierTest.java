3fae5b24d37d6cc0e645a9a5b4a807c66aa2e244
Dale <danson@grafidog.com>
2010-08-24 22:38:20 +0000

    Added another unit test for json files.

--- plugins/Beauty/trunk/test/test/beauty/beautifiers/JsonBeautifierTest.java
+++ plugins/Beauty/trunk/test/test/beauty/beautifiers/JsonBeautifierTest.java
@@ -20,7 +20,6 @@ public class JsonBeautifierTest {
             FileUtilities.copy(reader, writer);
  
             String before = writer.toString();
-            String answer = before;
  
             Beautifier beautifier = new JsonBeautifier();
             beautifier.setEditMode("json");
@@ -87,6 +86,38 @@ public class JsonBeautifierTest {
             beautifier.setWrapMode("none");
             String after = beautifier.beautify(before);
             assertTrue("json test 3 failed.", answer.equals(after));
+        } catch (Exception e) {
+            fail(e.getMessage()); 
+        }
+    }
+    
+    @Test
+    public void test4() {
+        try {
+            String resource = "before/test4.json";
+            InputStream is = getClass().getClassLoader().getResourceAsStream(resource);
+            Reader reader = new InputStreamReader(is);
+            StringWriter writer = new StringWriter();
+            FileUtilities.copy(reader, writer);
+            String before = writer.toString();
+            
+            resource = "after/test4.json";
+            is = getClass().getClassLoader().getResourceAsStream(resource);
+            reader = new InputStreamReader(is);
+            writer = new StringWriter();
+            FileUtilities.copy(reader, writer);
+            String answer = writer.toString();
+ 
+            Beautifier beautifier = new JsonBeautifier();
+            beautifier.setEditMode("json");
+            beautifier.setLineSeparator("\n");
+            beautifier.setTabWidth(4);
+            beautifier.setIndentWidth(4);
+            beautifier.setUseSoftTabs(true);
+            beautifier.setWrapMargin(80);
+            beautifier.setWrapMode("none");
+            String after = beautifier.beautify(before);
+            assertTrue("json test 4 failed.", answer.equals(after));
         } catch (Exception e) {
             fail(e.getMessage()); 
         }
