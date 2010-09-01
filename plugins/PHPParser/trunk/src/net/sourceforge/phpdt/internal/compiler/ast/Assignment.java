23ed037c285d18439ae8c782a41ea73f309e8663
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-09-01 14:36:19 +0000

    fix type detection

--- plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/Assignment.java
+++ plugins/PHPParser/trunk/src/net/sourceforge/phpdt/internal/compiler/ast/Assignment.java
@@ -1,3 +1,24 @@
+/*
+ * Assignment.java
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2003, 2010 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package net.sourceforge.phpdt.internal.compiler.ast;
 
 import gatchan.phpparser.parser.PHPParserConstants;
@@ -43,7 +64,7 @@ public class Assignment extends Expression
 			  int beginColumn,
 			  int endColumn)
 	{
-		super(Type.STRING, sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
+		super(initialization.getType(), sourceStart, sourceEnd, beginLine, endLine, beginColumn, endColumn);
 		variable.setType(initialization.getType());
 		this.initialization = initialization;
 		target = variable;
