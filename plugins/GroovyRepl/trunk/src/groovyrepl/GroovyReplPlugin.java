56d2e217866007ffae3efd11e17e0ab85e391d4d
Damien Radtke <git@damienradtke.com>
2010-09-06 02:42:19 +0000

    Initial upload.

---
+++ plugins/GroovyRepl/trunk/src/groovyrepl/GroovyReplPlugin.java
@@ -0,0 +1,10 @@
+package repl;
+//{{{ Imports
+import org.gjt.sp.jedit.EditPlugin;
+//}}}
+public class GroovyReplPlugin extends EditPlugin {
+	public void start() {}
+	public void stop() {
+		
+	}
+}
