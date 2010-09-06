5d7160e49ba8a87daab4d4ed8fa2dc067d183c72
Damien Radtke <git@damienradtke.com>
2010-09-06 02:57:28 +0000

    Updated plugin stop() method to clean up running processes

--- plugins/GroovyRepl/trunk/src/repl/GroovyReplPlugin.java
+++ plugins/GroovyRepl/trunk/src/repl/GroovyReplPlugin.java
@@ -1,10 +1,15 @@
 package repl;
 //{{{ Imports
+import console.Shell;
+import console.GroovyShell;
 import org.gjt.sp.jedit.EditPlugin;
 //}}}
 public class GroovyReplPlugin extends EditPlugin {
 	public void start() {}
 	public void stop() {
-		
+		try {
+			GroovyShell shell = (GroovyShell) Shell.getShell("Groovy");
+			shell.stop();
+		} catch (Exception e) {}
 	}
 }
