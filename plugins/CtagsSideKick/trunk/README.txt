7598266fc28183ec0abc137dc17a50c3b9a37406
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-09-10 17:18:22 +0000

    Moved the plugin from SVN to git.

---
+++ plugins/CtagsSideKick/trunk/README.txt
@@ -0,0 +1,1 @@
+Plugin moved to git.
--- plugins/CtagsSideKick/trunk/ctags/sidekick/renderers/ITextProvider.java
+++
@@ -1,8 +0,0 @@
-package ctags.sidekick.renderers;
-
-import ctags.sidekick.IObjectProcessor;
-import ctags.sidekick.Tag;
-
-public interface ITextProvider extends IObjectProcessor {
-	String getString(Tag tag);
-}
