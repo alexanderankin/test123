8b45769712454f71c20020f5920eed1d4039204b
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-09-23 19:36:16 +0000

    arg! forgot to add these with the last commit.

---
+++ plugins/Navigator/trunk/src/ise/plugin/nav/GoToLineDialog.java
@@ -0,0 +1,86 @@
+package ise.plugin.nav;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+
+/**
+ * A JOptionPane won't quite work for this, I want the text field to only accept
+ * numbers greater than or equal to 0.
+ */
+public class GoToLineDialog extends JDialog {
+
+    static int previousEntry = - 1;
+
+    private View parent;
+    private LineNumberTextField lineEntry;
+    private JButton okButton;
+    private JButton cancelButton;
+
+    public GoToLineDialog(View parent) {
+        super(parent, jEdit.getProperty("navigator.gotoLine", "Go to line"), true);
+        this.parent = parent;
+        installUI();
+        installListeners();
+        setVisible(true);
+    }
+
+    private void installUI() {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(11, 11, 12, 12));
+
+        Icon icon = GUIUtilities.loadIcon("22x22/actions/go-jump.png");
+
+        lineEntry = new LineNumberTextField(previousEntry == - 1 ? "" : String.valueOf(previousEntry), 15);
+
+        okButton = new JButton(jEdit.getProperty("common.ok", "OK"));
+        cancelButton = new JButton(jEdit.getProperty("common.cancel", "Cancel"));
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 3, 6));
+        buttonPanel.add(okButton);
+        buttonPanel.add(cancelButton);
+
+        mainPanel.add(new JLabel(icon), BorderLayout.WEST);
+        mainPanel.add(lineEntry, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        setContentPane(mainPanel);
+
+        pack();
+        setLocationRelativeTo(parent);
+        lineEntry.requestFocus();
+        lineEntry.selectAll();
+        getRootPane().setDefaultButton(okButton);        
+    }
+
+    private void installListeners() {
+        okButton.setMnemonic(KeyEvent.VK_O);
+        okButton.addActionListener (new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                int line = Integer.parseInt(lineEntry.getText());
+                previousEntry = line >= 0 ? line : 0;
+                close();
+                parent.getTextArea().requestFocus();
+            }
+        } );
+        cancelButton.setMnemonic(KeyEvent.VK_C);
+        cancelButton.addActionListener (new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                close();
+            }
+        } );
+    }
+
+    private void close() {
+        setVisible(false);
+        dispose();
+    }
+
+    public int getLineNumber() {
+        return previousEntry - 1 >= 0 ? previousEntry - 1 : 0;
+    }
+}
