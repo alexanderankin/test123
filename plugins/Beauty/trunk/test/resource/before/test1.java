9f608cb0f024a420a84f7d5f5bdc76219cf3579f
Dale <danson@grafidog.com>
2010-09-27 20:38:33 +0000

    java test

---
+++ plugins/Beauty/trunk/test/resource/before/test1.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    int markerAnnotation() {
+        if (true) {
+
+        }
+        // testing that 'else' stays on next line
+        else {
+            // whatever
+        }
+    }
+
+}
+
