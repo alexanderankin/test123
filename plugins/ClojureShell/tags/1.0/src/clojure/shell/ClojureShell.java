afa46ab4cde4d618af4b6355e67dc7b895ac08d3
kog13 <kog13@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-09-30 05:15:09 +0000

    Tagging for first release

---
+++ plugins/ClojureShell/tags/1.0/src/clojure/shell/ClojureShell.java
@@ -0,0 +1,83 @@
+package clojure.shell;
+/**
+ * @author Damien Radtke
+ * class ClojureShell
+ * Embeds an interactive Clojure session into the console
+ */
+//{{{ Imports
+import clojure.ClojurePlugin;
+import console.Console;
+import console.Output;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Hashtable;
+import javax.swing.text.AttributeSet;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.ServiceManager;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.gui.DockableWindowManager;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.textarea.TextArea;
+import org.gjt.sp.util.Log;
+import procshell.ProcessShell;
+//}}}
+public class ClojureShell extends ProcessShell {
+	
+	private String prompt = "user=> ";
+	
+	/*
+ 	 * Constructor for ClojureShell
+ 	 */
+	public ClojureShell() {
+		super("Clojure");
+	}
+	
+	//{{{ init()
+	/**
+	 * Start up Clojure
+	 */
+	protected void init(ConsoleState state, String command) throws IOException {
+		ClojurePlugin clojure = (ClojurePlugin) jEdit.getPlugin("clojure.ClojurePlugin");
+		Log.log(Log.DEBUG,this,"Attempting to start Clojure process");
+		ProcessBuilder pb = new ProcessBuilder("java", "-cp", clojure.getClojure(),
+				"clojure.main");
+		state.p = pb.start();
+		Log.log(Log.DEBUG,this,"Clojure started.");
+	}
+	//}}}
+	
+	//{{{ eval()
+	/**
+	 * Evaluate text
+	 */
+	public void eval(Console console, String str) {
+		String cmd = "(load-string \""+str.replace("\"", "\\\"")+"\")";
+		send(console, cmd);
+	} //}}}
+	
+	//{{{ evalBuffer()
+	/**
+	 * Evaluate a buffer
+	 */
+	public void evalBuffer(Console console, Buffer buffer) {
+		String cmd = "(load-file \""+buffer.getPath()+"\")";
+		send(console, cmd);
+	} //}}}
+	
+	protected void onRead(ConsoleState state, String str, Output output) {
+		if (str.indexOf("\n") != -1) {
+			str = str.substring(str.lastIndexOf("\n")+1);
+		}
+		if (str.matches(prompt)) {
+			state.waiting = false;
+			output.commandDone();
+		}
+	}
+	
+	public void printInfoMessage(Output output) {
+		output.print(null, jEdit.getProperty("msg.clojureshell.info-message"));
+	}
+	
+}
