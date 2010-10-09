37e56b126029c585cf0c708a44580bf4636de9ed
Damien Radtke <git@damienradtke.com>
2010-10-09 01:09:49 +0000

    Cleaned up plugin and added user's guide

--- plugins/Groovy/trunk/src/groovy/GroovyPlugin.java
+++ plugins/Groovy/trunk/src/groovy/GroovyPlugin.java
@@ -16,6 +16,9 @@ import java.util.StringTokenizer;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
+
+import javax.swing.SwingUtilities;
+
 import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.MiscUtilities;
@@ -30,21 +33,45 @@ public class GroovyPlugin extends EditPlugin {
 
 	public static final String includedJarPath = MiscUtilities.constructPath(
 		jEdit.getSettingsDirectory(),
-		"jars/"+jEdit.getProperty("options.groovy.groovy-jar"));
+		"jars/"+jEdit.getProperty("groovy.included-jar"));
+
+	public static final String groovyProp = "groovy.path";
 
 	private String installedJar = null;
-	private String oldBin = null;
 
 	public void start() {
-		installedJar = new String(includedJarPath);
+		String path = jEdit.getProperty(groovyProp);
+		if (path != null) {
+			jEdit.removePluginJAR(jEdit.getPluginJAR(includedJarPath), false);
+			jEdit.addPluginJAR(path);
+			installedJar = path;
+		} else {
+			installedJar = includedJarPath;
+		}
 		setVars();
-		// Add the groovy-all jar
-		if (!jEdit.getProperty("options.groovy.install").equals("jar")) {
-			String installPath = jEdit.getProperty(
-				"options.groovy.groovy-path");
-			setGroovyJar(getEmbeddableJar(installPath).getPath());
+
+		if (jEdit.getPlugin("console.ConsolePlugin") != null) {
+			final File commando = new File(console.ConsolePlugin.getUserCommandDirectory(), "groovy.xml");
+			if (!commando.exists()) {
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							InputStream in = getClass().getResourceAsStream("/commands/groovy.xml");
+							OutputStream out = new FileOutputStream(commando);
+							IOUtilities.copyStream(null, in, out, false);
+							IOUtilities.closeQuietly(in);
+							IOUtilities.closeQuietly(out);
+							jEdit.setProperty("mode.groovy.commando.run", "groovy");
+							console.ConsolePlugin.rescanCommands();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}	
 		}
 	}
+
 	public void stop() {}
 
 	/**
@@ -52,233 +79,35 @@ public class GroovyPlugin extends EditPlugin {
 	 * @param path the path of the new groovy jar
 	 */
 	public void setGroovyJar(String path) {
-		if (installedJar != null) {
-			jEdit.removePluginJAR(jEdit.getPluginJAR(installedJar), false);
-		}
+		jEdit.setProperty(groovyProp, path);
+		jEdit.removePluginJAR(jEdit.getPluginJAR(installedJar), false);
 		jEdit.addPluginJAR(path);
 		installedJar = path;
 	}
 
 	public void setGroovyJar() {
-		setGroovyJar(includedJarPath);
-	}
-
-	/**
-	 * Downloads groovy from the official website in a new thread
-	 */
-	public void downloadGroovy() {
-		clearGroovy();
-		download("groovy");
-	}
-
-	/**
-	 * Downloads the documentation from the official website in a new thread
-	 */
-	public void downloadDocs() {
-		clearDocs();
-		download("docs");
-	}
-
-	/**
-	 * Utility download method
-	 */
-	public void download(final String name) {
-		final String url = jEdit.getProperty("options.groovy."+name+"-url");
-		final View view = jEdit.getActiveView();
-		new Thread() {
-			public void run() {
-				try {
-					File temp = File.createTempFile("jedit", "temp.zip");
-					URLConnection con = new URL(url).openConnection();
-					InputStream in = new BufferedInputStream(
-						con.getInputStream());
-					OutputStream out = new BufferedOutputStream(
-						new FileOutputStream(temp));
-					GroovyDownloadObserver obs = new GroovyDownloadObserver(
-						view, in, out,
-						jEdit.getProperty("options.groovy.download-"+name));
-					IOUtilities.copyStream(obs, in, out, true);
-					IOUtilities.closeQuietly(out);
-					IOUtilities.closeQuietly(in);
-					if (obs.isCanceled()) {
-						temp.delete();
-						view.getStatus().setMessageAndClear(
-							"Download canceled.");
-						return;
-					}
-					// Extract
-					obs.setMessage(jEdit.getProperty(
-						"options.groovy.extracting"));
-					ZipFile zfile = new ZipFile(temp);
-					File dest = new File(home, name);
-					dest.getParentFile().mkdirs();
-					Enumeration e = zfile.entries();
-					while (e.hasMoreElements()) {
-						if (obs.isCanceled()) {
-							delete(dest);
-							temp.delete();
-							view.getStatus().setMessageAndClear(
-								"Download canceled.");
-							return;
-						}
-						ZipEntry ze = (ZipEntry) e.nextElement();
-						obs.setMessage(jEdit.getProperty(
-							"options.groovy.extracting")+" "+
-							MiscUtilities.getFileName(ze.getName()));
-						if (ze.isDirectory()) {
-							new File(dest, ze.getName()).mkdirs();
-						} else {
-							InputStream zin = zfile.getInputStream(ze);
-							OutputStream zout = new FileOutputStream(
-								new File(dest, ze.getName()));
-							IOUtilities.copyStream(null, zin, zout, false);
-							IOUtilities.closeQuietly(zout);
-						}
-					}
-					zfile.close();
-					temp.delete();
-					jEdit.setBooleanProperty("options.groovy."+name+
-						"-downloaded", true);
-					obs.setMessage(jEdit.getProperty(
-						"options.groovy.download-done"));
-					obs.done();
-					String[] paths = dest.list(new FilenameFilter() {
-							public boolean accept(File dir, String name) {
-								return name.startsWith("groovy-");
-							}
-					});
-					if (name.equals("groovy") && jEdit.getProperty(
-						"options.groovy.install").equals("full"))
-					{
-						jEdit.setProperty("options.groovy.groovy-path",
-							MiscUtilities.constructPath(
-								dest.getPath(), paths[0]));
-						jEdit.setProperty("options.groovy.install", "full");
-						setVars();
-					} else if (name.equals("docs") &&
-						jEdit.getPlugin("javadoc.JavadocPlugin") != null)
-					{
-						// Add the new docs to Javadoc's list
-						String path = MiscUtilities.constructPath(
-							dest.getPath(), paths[0]);
-						String api1 = MiscUtilities.constructPath(
-							path, "html/api");
-						String api2 = MiscUtilities.constructPath(
-							path, "html/gapi");
-						String api3 = MiscUtilities.constructPath(
-							path, "html/groovy-jdk");
-						String sep = File.pathSeparator;
-						javadoc.JavadocPlugin.addApi(api1+sep+api2+sep+api3);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					view.getStatus().setMessage(jEdit.getProperty(
-						"options.groovy.download-error"));
-				}
-			}
-		}.start();
-	}
-
-	/**
-	 * Removes any downloaded Groovy installation
-	 */
-	public void clearGroovy() {
-		delete(new File(home, "groovy"));
-		jEdit.setBooleanProperty("options.groovy.groovy-downloaded", false);
-	}
-
-	/**
-	 * Removes any downloaded documentation
-	 */
-	public void clearDocs() {
-		if (jEdit.getPlugin("javadoc.JavadocPlugin") != null) {
-			String path = jEdit.getProperty("options.javadoc.path");
-			StringTokenizer tokenizer = new StringTokenizer(path,
-				File.pathSeparator);
-			ArrayList<String> toRemove = new ArrayList<String>();
-			while (tokenizer.hasMoreTokens()) {
-				String token = tokenizer.nextToken();
-				if (token.startsWith(home)) {
-					toRemove.add(token);
-				}
-			}
-			for (String api : toRemove) {
-				javadoc.JavadocPlugin.removeApi(api);
-			}
-		}
-		delete(new File(home, "docs"));
-		jEdit.setBooleanProperty("options.groovy.docs-downloaded", false);
+		jEdit.unsetProperty(groovyProp);
+		jEdit.removePluginJAR(jEdit.getPluginJAR(installedJar), false);
+		jEdit.addPluginJAR(includedJarPath);
+		installedJar = includedJarPath;
 	}
 
 	/**
 	 * If Console is installed, set some environment variables
-	 * - Set GROOVY_HOME to the configured path of Groovy
-	 * - Add GROOVY_HOME/bin to the system path
+	 * - Set GROOVY to the path of the jar
 	 */
 	public void setVars() {
-		String groovyHome = null;
-		if (!jEdit.getProperty("options.groovy.install").equals("jar")) {
-			groovyHome = jEdit.getProperty("options.groovy.groovy-path");
-		}
 		if (jEdit.getPlugin("console.ConsolePlugin") != null) {
-			String path = console.ConsolePlugin.getSystemShellVariableValue(
-				null, "PATH");
-			if (path == null) path = "";
-			if (oldBin != null && path.indexOf(oldBin) != -1) {
-				int index = path.indexOf(oldBin);
-				path = path.substring(0, index)+path.substring(
-					index+oldBin.length());
-			}
-			if (groovyHome != null && groovyHome.length() > 0) {
-				console.ConsolePlugin.setSystemShellVariableValue(
-					"GROOVY_HOME", groovyHome);
-				String bin = MiscUtilities.constructPath(groovyHome, "bin")
-					+File.pathSeparator;
-				path = bin+path;
-				oldBin = bin;
-			} else {
-				console.ConsolePlugin.setSystemShellVariableValue(
-					"GROOVY_HOME", System.getenv("GROOVY_HOME"));
-			}
-			console.ConsolePlugin.setSystemShellVariableValue(
-					"PATH", path);
+			console.ConsolePlugin.setSystemShellVariableValue("GROOVY", getGroovy());
 		}
 	}
 
 	/**
-	 * Given a groovy install directory, return the name of the groovy-all jar
+	 * Returns the path to the groovy jar
 	 */
-	public File getEmbeddableJar(String groovy) {
-		File embeddable = new File(groovy, "embeddable");
-		if (!embeddable.exists()) {
-			return null;
-		}
-		File[] jars = embeddable.listFiles(new FilenameFilter() {
-				public boolean accept(File dir, String name) {
-					return name.startsWith("groovy-all-") &&
-							name.endsWith(".jar");
-				}
-		});
-		if (jars.length < 1) {
-			return null;
-		}
-		return jars[0];
+	public String getGroovy() {
+		String path = jEdit.getProperty(groovyProp);
+		return (path == null) ? includedJarPath : path;
 	}
 
-	/**
-	 * Recursively deletes files
-	 */
-	private void delete(File f) {
-		if (f.isDirectory()) {
-			File[] children = f.listFiles();
-			for (int i = 0; i<children.length; i++) {
-				delete(children[i]);
-			}
-		}
-		f.delete();
-	}
-
-	public static boolean isFullInstallation() {
-		return !jEdit.getProperty("options.groovy.install").equals("jar");
-	}
 }
