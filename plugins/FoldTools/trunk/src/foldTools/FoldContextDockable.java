c40f474554cafb202ff6b42003e0fed45939c531
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-10-17 01:12:57 +0000

    Allow jumping to lines from the context dockable.

--- plugins/FoldTools/trunk/src/foldTools/FoldContextDockable.java
+++ plugins/FoldTools/trunk/src/foldTools/FoldContextDockable.java
@@ -1,8 +1,11 @@
 package foldTools;
 
 import java.awt.BorderLayout;
+import java.awt.FontMetrics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,6 +13,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 
 import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+
+import foldTools.FoldContext.LineContext;
 
 @SuppressWarnings("serial")
 public class FoldContextDockable extends JPanel
@@ -25,6 +31,21 @@ public class FoldContextDockable extends JPanel
 		context = new FoldContext(view);
 		setLayout(new BorderLayout());
 		textPane = new JTextPane();
+		textPane.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+				if (e.getClickCount() < 2)
+					return;
+				FontMetrics fm = textPane.getFontMetrics(textPane.getFont());
+				int line = e.getY() / fm.getHeight();
+				LineContext lc = context.getLineContext(line);
+				if (lc == null)
+					return;
+				jumpTo(lc.line);
+			}
+		});
 		add(new JScrollPane(textPane), BorderLayout.CENTER);
 		JPanel p = new JPanel();
 		add(p, BorderLayout.NORTH);
@@ -44,5 +65,11 @@ public class FoldContextDockable extends JPanel
 		if (update)
 			context.update(view);
 		textPane.setText(context.toString());
+	}
+	public void jumpTo(int line)
+	{
+		JEditTextArea ta = view.getTextArea();
+		if (line >= 0 && line < ta.getLineCount())
+			ta.setCaretPosition(ta.getLineStartOffset(line));
 	}
 }
