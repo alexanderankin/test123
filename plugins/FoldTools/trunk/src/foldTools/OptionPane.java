2e4f0d2bbf9442aeee85d2448e16af8221ca4c47
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-10-23 01:23:47 +0000

    Created the UI for custom composite folding modes.

--- plugins/FoldTools/trunk/src/foldTools/OptionPane.java
+++ plugins/FoldTools/trunk/src/foldTools/OptionPane.java
@@ -1,6 +1,16 @@
 package foldTools;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
 
@@ -13,6 +23,9 @@ public class OptionPane extends AbstractOptionPane
 	private static final String MESSAGE = "messages.foldTools.";
 	private static final String PROP = "props.foldTools.";
 	private JSpinner before, after, delay;
+	private DefaultListModel handlerModel;
+	private JList handlers;
+	private JButton addHandler, removeHandler, editHandler;
 
 	public OptionPane()
 	{
@@ -25,6 +38,67 @@ public class OptionPane extends AbstractOptionPane
 		addComponent(jEdit.getProperty(MESSAGE + "linesAfter"), after);
 		delay = new JSpinner(new SpinnerNumberModel(getFollowCaretDelay(), 0, 5000, 50));
 		addComponent(jEdit.getProperty(MESSAGE + "followCaretDelay"), delay);
+		addComponent(new JLabel(jEdit.getProperty(MESSAGE + "compositeHandlers")));
+		handlerModel = new DefaultListModel();
+		handlers = new JList(handlerModel);
+		handlers.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		addComponent(new JScrollPane(handlers));
+		JPanel p = new JPanel();
+		addHandler = new JButton(jEdit.getProperty(MESSAGE + "addHandler"));
+		p.add(addHandler);
+		removeHandler = new JButton(jEdit.getProperty(MESSAGE + "removeHandler"));
+		p.add(removeHandler);
+		editHandler = new JButton(jEdit.getProperty(MESSAGE + "editHandler"));
+		p.add(editHandler);
+		addComponent(p);
+		addHandler.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				addHandler();
+			}
+		});
+		removeHandler.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				removeHandler();
+			}
+		});
+		editHandler.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				editHandler();
+			}
+		});
+	}
+	private void addHandler()
+	{
+		CompositeHandlerDialog d = new CompositeHandlerDialog(
+			jEdit.getActiveView());
+		d.setVisible(true);
+		if (d.wasCancelled())
+			return;
+		handlerModel.addElement(d.getHandler());
+	}
+	private void removeHandler()
+	{
+		int i = handlers.getSelectedIndex();
+		if (i >= 0)
+			handlerModel.remove(i);
+	}
+	private void editHandler()
+	{
+		int i = handlers.getSelectedIndex();
+		if (i < 0)
+			return;
+		CompositeHandlerDialog d = new CompositeHandlerDialog(
+			jEdit.getActiveView(), (HandlerItem)handlerModel.get(i));
+		d.setVisible(true);
+		if (d.wasCancelled())
+			return;
+		handlerModel.set(i, d.getHandler());
 	}
 	@Override
 	public void _save() {
