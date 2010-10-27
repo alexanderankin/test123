a2b0bfc4e4e7855a2e97bdc9700d3842b4631354
shlomy <shlomy@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-10-27 04:16:03 +0000

    Use the "tree selection follows caret" to control caret-tracking in the SideKick toolbar.

--- plugins/SideKick/trunk/sidekick/SideKickToolBar.java
+++ plugins/SideKick/trunk/sidekick/SideKickToolBar.java
@@ -1,6 +1,8 @@
 package sidekick;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
@@ -11,19 +13,34 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JToolBar;
+import javax.swing.Timer;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 
 import org.gjt.sp.jedit.EditBus;
 import org.gjt.sp.jedit.EditBus.EBHandler;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.msg.EditPaneUpdate;
+import org.gjt.sp.jedit.msg.PropertiesChanged;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+import org.gjt.sp.jedit.textarea.Selection;
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.jEdit;
+
+import sidekick.SideKickTree.CaretHandler;
 
 @SuppressWarnings("serial")
 public class SideKickToolBar extends JToolBar
 {
 	private View view;
 	private JComboBox combo;
+	private boolean followCaret;
+	private CaretListener caretListener;
+	private Timer caretTimer;
+	private SideKickParsedData data;
+
 	public SideKickToolBar(View view)
 	{
 		this.view = view;
@@ -48,6 +65,9 @@ public class SideKickToolBar extends JToolBar
 		});
 		add(combo);
 		update();
+		followCaret = SideKick.isFollowCaret();
+		if (followCaret)
+			addCaretListener();
 		EditBus.addToBus(this);
 	}
 	@EBHandler
@@ -56,9 +76,45 @@ public class SideKickToolBar extends JToolBar
 		if(msg.getView() == view)
 			update();
 	}
+	@EBHandler
+	public void handlePropertiesChanged(PropertiesChanged msg)
+	{
+		boolean newFollowCaret = SideKick.isFollowCaret();
+		if (newFollowCaret != followCaret)
+		{
+			followCaret = newFollowCaret;
+			if (followCaret)
+				addCaretListener();
+			else
+				removeCaretListener();
+		}
+	}
+	@EBHandler
+	public void handleEditPaneUpdate(EditPaneUpdate epu)
+	{
+		if (! followCaret)
+			return;
+		EditPane editPane = epu.getEditPane();
+		if (epu.getWhat() == EditPaneUpdate.CREATED)
+			editPane.getTextArea().addCaretListener(caretListener);
+		else if (epu.getWhat() == EditPaneUpdate.DESTROYED)
+			editPane.getTextArea().removeCaretListener(caretListener);
+	}
+	private void addCaretListener()
+	{
+		caretListener = new CaretHandler();
+		for (EditPane ep: view.getEditPanes())
+			ep.getTextArea().addCaretListener(caretListener);
+	}
+	private void removeCaretListener()
+	{
+		for (EditPane ep: view.getEditPanes())
+			ep.getTextArea().removeCaretListener(caretListener);
+		caretListener = null;
+	}
 	private void update()
 	{
-		SideKickParsedData data = SideKickParsedData.getParsedData(view);
+		data = SideKickParsedData.getParsedData(view);
 		combo.removeAllItems();
 		if (data == null)
 			combo.addItem(jEdit.getProperty("sidekick-tree.not-parsed"));
@@ -82,6 +138,53 @@ public class SideKickToolBar extends JToolBar
 	public void dispose()
 	{
 		EditBus.removeFromBus(this);
+	}
+
+	private void selectItemAtPosition(int position)
+	{
+		NodeWrapper selected = null;
+		for (int i = 0; i < combo.getItemCount(); i++)
+		{
+			NodeWrapper nw = (NodeWrapper) combo.getItemAt(i);
+			if (nw == null)
+				continue;
+			if (nw.contains(position))
+			{
+				if (nw.isBetterThan(selected))
+					selected = nw;
+			}
+		}
+		if (selected != null)
+			combo.setSelectedItem(selected);
+	}
+
+	private class CaretHandler implements CaretListener
+	{
+		public void caretUpdate(CaretEvent e)
+		{
+			if (e.getSource() != view.getTextArea())
+				return;
+			if (caretTimer != null)
+			{
+				caretTimer.stop();
+			}
+			else
+			{
+				caretTimer = new Timer(0,new ActionListener()
+				{
+					public void actionPerformed(ActionEvent evt)
+					{
+						JEditTextArea textArea = view.getTextArea();
+						int caret = textArea.getCaretPosition();
+						Selection s = textArea.getSelectionAtOffset(caret);
+						selectItemAtPosition(s == null ? caret : s.getStart());
+					}
+				});
+				caretTimer.setInitialDelay(500);
+				caretTimer.setRepeats(false);
+			}
+			caretTimer.start();
+		}
 	}
 
 	private static class NodeWrapper
@@ -122,6 +225,17 @@ public class SideKickToolBar extends JToolBar
 				sb.append("[" + s + "]");
 			}
 			return sb.toString();
+		}
+		public boolean contains(int position)
+		{
+			return (asset != null && asset.getStart().getOffset() <= position &&
+				asset.getEnd().getOffset() > position);
+		}
+		public boolean isBetterThan(NodeWrapper other)
+		{
+			return (other == null ||
+				asset.getStart().getOffset() > other.asset.getStart().getOffset() ||
+				asset.getEnd().getOffset() < other.asset.getEnd().getOffset());
 		}
 	}
 
