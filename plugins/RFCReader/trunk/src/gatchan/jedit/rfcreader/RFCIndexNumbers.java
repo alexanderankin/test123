ddb6040da2c261d186aeddac61584e46ca1586a5
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2010-10-30 08:29:28 +0000

    added classes that I forgot

---
+++ plugins/RFCReader/trunk/src/gatchan/jedit/rfcreader/RFCIndexNumbers.java
@@ -0,0 +1,99 @@
+/*
+ * jEdit - Programmer's Text Editor
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright © 2010 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package gatchan.jedit.rfcreader;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Matthieu Casanova
+ */
+public class RFCIndexNumbers extends AbstractRFCIndex
+{
+	/** increment this */
+	int INDEX_VERSION = 2;
+
+	public RFCIndexNumbers() throws IOException
+	{
+		RFCReaderPlugin plugin = (RFCReaderPlugin) jEdit.getPlugin("gatchan.jedit.rfcreader.RFCReaderPlugin");
+		File home = plugin.getPluginHome();
+		analyzer = new StandardAnalyzer(Version.LUCENE_30);
+		directory = FSDirectory.open(new File(home, "lucene"));
+	}
+
+	protected String[] getFields()
+	{
+		return new String[]{"number", "title"};
+	}
+
+	@Override
+	public void load() throws IOException
+	{
+		Log.log(Log.DEBUG, this, "load()");
+		if (jEdit.getIntegerProperty("rfcreader.index.version",-1) != INDEX_VERSION ||
+			!IndexReader.indexExists(directory))
+		{
+			IndexWriter writer = new IndexWriter(directory, analyzer, true,
+				IndexWriter.MaxFieldLength.UNLIMITED);
+			for (RFC rfc : rfcs.values())
+			{
+				Document document = new Document();
+				document.add(new Field("number",
+					Integer.toString(rfc.getNumber()), Field.Store.YES,
+					Field.Index.ANALYZED, Field.TermVector.NO));
+				document.add(new Field("title", rfc.getTitle(),
+					Field.Store.NO, Field.Index.ANALYZED,
+					Field.TermVector.NO));
+
+				writer.addDocument(document);
+			}
+			writer.optimize();
+			writer.close();
+			jEdit.setIntegerProperty("rfcreader.index.version",INDEX_VERSION);
+		}
+		searcher = new IndexSearcher(directory, true);
+	}
+
+
+}
