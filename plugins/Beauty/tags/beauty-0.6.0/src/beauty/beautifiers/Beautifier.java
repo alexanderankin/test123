bf3bff4066ab7599ad152204e490444c5b03be80
Dale <danson@grafidog.com>
2011-02-22 21:24:18 +0000

    Tagging for release.

---
+++ plugins/Beauty/tags/beauty-0.6.0/src/beauty/beautifiers/Beautifier.java
@@ -0,0 +1,114 @@
+package beauty.beautifiers;
+
+import beauty.parsers.ParserException;
+
+/**
+ * Base class for all beautifiers.  This class contains methods to get and set
+ * various Buffer parameters.
+ */
+public abstract class Beautifier {
+
+    // the line separator, defaults to the system line separator, but will
+    // be populated with the current line separator for the buffer being
+    // beautified
+    protected String lineSeparator = System.getProperty("line.separator");
+    
+    // buffer settings
+    protected String editMode = null;
+    protected int tabWidth = 4;
+    protected int wrapMargin = 1024;
+    protected String wrapMode = "none";
+    protected int initialLevel = 0;
+    protected int indentWidth = 4;
+    protected boolean softTabs = true;
+    protected String indent = "    ";
+    protected String doubleIndent = indent + indent;
+    
+    public static final String SERVICE_NAME = "beauty.beautifiers.Beautifier";
+    
+    /**
+     * Subclasses must have this method.
+     * @param text The text to beautify.
+     * @return The formatted text.
+     */
+    public abstract String beautify(String text) throws ParserException;
+    
+    public void setTabWidth(int w) {
+        tabWidth = w;   
+    }
+    
+    public int getTabWidth() {
+        return tabWidth;   
+    }
+
+    public void setIndentWidth(int w) {
+        indentWidth = w;
+        if (indentWidth <= 0) {
+            indentWidth = 4;
+        }
+        indent = "";
+        for (int i = 0; i < w; i++) {
+            indent += " ";
+        }
+        doubleIndent = indent + indent;
+    }
+    
+    public int getIndentWidth() {
+        return indentWidth;   
+    }
+    
+    /**
+     * <code>setIndentWidth</code> should be called before this method.    
+     */
+    public void setUseSoftTabs(boolean b) {
+        softTabs = b;
+        if (b) {
+            setIndentWidth(indentWidth);
+        }
+        else {
+            indent = "\t";
+            doubleIndent = "\t\t";
+        }
+    }
+    
+    public boolean getUseSoftTabs() {
+        return softTabs;   
+    }
+    
+    public void setLineSeparator(String ls) {
+        lineSeparator = ls;   
+    }
+    
+    public String getLineSeparator() {
+        return lineSeparator;   
+    }
+    
+    public void setWrapMargin(int i) {
+        wrapMargin = i;   
+    }
+    
+    public int getWrapMargin() {
+        return wrapMargin;   
+    }
+    
+    public void setWrapMode(String s) {
+        if (s != null && (s.equals("none") || s.equals("soft") || s.equals("hard")))
+            wrapMode = s;   
+    }
+    
+    public String getWrapMode() {
+        return wrapMode;   
+    }
+    
+    public void setEditMode(String m) {
+        editMode = m;   
+    }
+    
+    public String getEditMode() {
+        return editMode;   
+    }
+    
+    public void setInitialIndentLevel(int level) {
+        initialLevel = level;   
+    }
+}
---
+++ plugins/Beauty/tags/beauty-0.6.0/src/beauty/options/JavaOptionPane.java
@@ -0,0 +1,95 @@
+package beauty.options;
+
+import javax.swing.table.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.msg.*;
+
+import beauty.BeautyPlugin;
+import beauty.parsers.java.JavaParser;
+
+import ise.java.awt.*;
+
+
+/**
+* An option pane to configure settings for the built-in Java beautifier.
+*
+*/
+public class JavaOptionPane extends AbstractOptionPane {
+    
+    private JRadioButton attachedBrackets;
+    private JRadioButton brokenBrackets;
+    private int bracketStyle;
+    
+    private JCheckBox padParens;
+    
+    public JavaOptionPane() {
+        super( "beauty.java" );
+        bracketStyle = jEdit.getIntegerProperty("beauty.java.bracketStyle", JavaParser.ATTACHED);
+    }
+    
+    // called when this class is first accessed
+    public void _init() {
+        installComponents();
+    }
+    
+    
+    // create the user interface components and do the layout
+    private void installComponents() {
+        setLayout( new KappaLayout() );
+        setBorder( BorderFactory.createEmptyBorder( 6, 6, 6, 6 ) );
+        
+        // create the components
+        JLabel description = new JLabel( "<html><b>Java Options" );
+        
+        attachedBrackets = new JRadioButton("<html>Use attached brackets, e.g.<br> try {");
+        brokenBrackets = new JRadioButton("<html>Use broken brackets, e.g.<br>try<br>{");
+        
+        switch(bracketStyle) {
+        case JavaParser.ATTACHED:
+            attachedBrackets.setSelected(true);
+            brokenBrackets.setSelected(false);
+            break;
+        case JavaParser.BROKEN:
+            attachedBrackets.setSelected(false);
+            brokenBrackets.setSelected(true);
+            break;
+        }
+        
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(attachedBrackets);
+        bg.add(brokenBrackets);
+        
+        ActionListener al = new ActionListener(){
+            public void actionPerformed(ActionEvent ae) {
+                if (attachedBrackets.equals(ae.getSource())) {
+                    bracketStyle = JavaParser.ATTACHED;   
+                }
+                else if (brokenBrackets.equals(ae.getSource())) {
+                    bracketStyle = JavaParser.BROKEN;   
+                }
+            }
+        };
+        attachedBrackets.addActionListener(al);
+        brokenBrackets.addActionListener(al);
+        
+        padParens = new JCheckBox("Pad parenthesis");
+        padParens.setSelected(jEdit.getBooleanProperty("beauty.java.padParens", false));
+        
+        
+        add("0, 0, 1, 1, W, w, 3", description);
+        add("0, 1, 1, 1, W, w, 3", attachedBrackets);
+        add("0, 2, 1, 1, W, w, 3", brokenBrackets);
+        add("0, 3, 1, 1, W, w, 3", padParens);
+    }
+    
+    public void _save() {
+        jEdit.setIntegerProperty("beauty.java.bracketStyle", bracketStyle); 
+        jEdit.setBooleanProperty("beauty.java.padParens", padParens.isSelected());
+    }
+}
