f7969681805a2af37acf2a074dbb2b63db0c311a
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-08-19 17:56:07 +0000

    Removed unnecessary line.

--- plugins/Calculator/trunk/src/ise/calculator/GUIUtils.java
+++ plugins/Calculator/trunk/src/ise/calculator/GUIUtils.java
@@ -48,7 +48,6 @@ public class GUIUtils {
      */
     public static void centerOnScreen(Component me) {
         Point center_point = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
-        Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
         Dimension window_size = me.getSize();
         me.setBounds(center_point.x - (window_size.width / 2), center_point.y - (window_size.height / 2), window_size.width, window_size.height);
     }
