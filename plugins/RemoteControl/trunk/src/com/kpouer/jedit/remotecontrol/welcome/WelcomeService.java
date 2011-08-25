525cb88755534124022cb7389f39fb3d2669ee37
Matthieu Casanova <chocolat.mou@gmail.com>
2011-08-25 17:19:35 +0000

    added welcome service

---
+++ plugins/RemoteControl/trunk/src/com/kpouer/jedit/remotecontrol/welcome/WelcomeService.java
@@ -0,0 +1,40 @@
+/*
+ * jEdit - Programmer's Text Editor
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright © 2011 Matthieu Casanova
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.kpouer.jedit.remotecontrol.welcome;
+
+import com.kpouer.jedit.remotecontrol.MessageHandler;
+import com.kpouer.jedit.remotecontrol.RemoteClient;
+
+import java.nio.channels.SocketChannel;
+
+/**
+ * A Welcome service is a service that handle the "handshake".
+ * It can be a challenge for some security, a handshake for a minimum, and no security at all.
+ *
+ * @author Matthieu Casanova
+ */
+public interface WelcomeService extends MessageHandler
+{
+	void setClient(RemoteClient client);
+
+	void setChannel(SocketChannel sChannel);
+}
