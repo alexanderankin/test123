3a9aee619caa0759d47d94d3bf250d2f4ccb0bd4
Matthieu Casanova <chocolat.mou@gmail.com>
2011-08-29 12:56:15 +0000

    added viewId and editPaneId to the context

--- plugins/RemoteControl/trunk/src/com/kpouer/jedit/remotecontrol/executionengine/BeanshellEngine.java
+++ plugins/RemoteControl/trunk/src/com/kpouer/jedit/remotecontrol/executionengine/BeanshellEngine.java
@@ -21,13 +21,14 @@
 
 package com.kpouer.jedit.remotecontrol.executionengine;
 
-import com.kpouer.jedit.remotecontrol.CommandResponse;
-import com.kpouer.jedit.remotecontrol.RemoteClient;
-import com.kpouer.jedit.remotecontrol.RemoteServer;
-import com.kpouer.jedit.remotecontrol.SingleLineCommandHandler;
+import com.kpouer.jedit.remotecontrol.*;
 import org.gjt.sp.jedit.BeanShell;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.bsh.NameSpace;
+import org.gjt.sp.jedit.bsh.UtilEvalError;
 import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.visitors.JEditVisitorAdapter;
 import org.gjt.sp.util.Log;
 
 import java.awt.*;
@@ -45,13 +46,52 @@ public class BeanshellEngine implements Engine
 			@Override
 			public void run()
 			{
-				NameSpace ns = new NameSpace(
+				final NameSpace ns = new NameSpace(
 						BeanShell.getNameSpace(),
 						"RemoteClient namespace");
 
+				final jEditListener jEditListener = RemoteControlPlugin.server.getjEditListener();
 				Object ret;
 				try
 				{
+					jEdit.visit(new JEditVisitorAdapter()
+					{
+						@Override
+						public void visit(View view)
+						{
+							String viewId = jEditListener.getViewId(view);
+							if (viewId != null)
+							{
+								try
+								{
+									ns.setVariable(viewId, view);
+								}
+								catch (UtilEvalError utilEvalError)
+								{
+									Log.log(Log.ERROR, this, utilEvalError,
+										utilEvalError);
+								}
+							}
+						}
+
+						@Override
+						public void visit(EditPane editPane)
+						{
+							String editPaneId = jEditListener.getViewId(editPane);
+							if (editPaneId != null)
+							{
+								try
+								{
+									ns.setVariable(editPaneId, editPane);
+								}
+								catch (UtilEvalError utilEvalError)
+								{
+									Log.log(Log.ERROR, this, utilEvalError,
+										utilEvalError);
+								}
+							}
+						}
+					});
 					ret = BeanShell.eval(jEdit.getActiveView(), ns, script);
 				}
 				catch (Throwable e)
