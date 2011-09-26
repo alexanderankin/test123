4a8a36556aa5a1e16b46a92a32a1297894f37a5f
Alan Ezust <1688991+ezust@users.noreply.github.com>
2011-09-26 20:07:14 +0000

    Remove duplicate entry in importers list.

--- plugins/SVNPlugin/trunk/src/ise/plugin/svn/pv/VersionControlState.java
+++ plugins/SVNPlugin/trunk/src/ise/plugin/svn/pv/VersionControlState.java
@@ -199,7 +199,7 @@ public class VersionControlState implements VersionControlService {
     }
     
     public ImporterFileFilter getFilter() {
-        return new ImportFilter();   
+        return null;   
     }
 
 
