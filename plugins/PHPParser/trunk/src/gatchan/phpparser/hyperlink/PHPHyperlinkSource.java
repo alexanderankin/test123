aacb4fb471ea85d60f5f2c66bda6388cbfc16385
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-10-11 15:40:23 +0000

    fix hyperlinks

--- plugins/PHPParser/trunk/src/gatchan/phpparser/hyperlink/PHPHyperlinkSource.java
+++ plugins/PHPParser/trunk/src/gatchan/phpparser/hyperlink/PHPHyperlinkSource.java
@@ -1,23 +1,23 @@
 /*
- * jEdit - Programmer's Text Editor
- * :tabSize=8:indentSize=8:noTabs=false:
- * :folding=explicit:collapseFolds=1:
- *
- * Copyright © 2011 Matthieu Casanova
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+* jEdit - Programmer's Text Editor
+* :tabSize=8:indentSize=8:noTabs=false:
+* :folding=explicit:collapseFolds=1:
+*
+* Copyright © 2011 Matthieu Casanova
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 
 package gatchan.phpparser.hyperlink;
 
@@ -37,170 +37,181 @@ import java.util.List;
  */
 public class PHPHyperlinkSource implements HyperlinkSource
 {
-    @Override
-    public Hyperlink getHyperlink(Buffer buffer, int offset)
-    {
-        PHPDocument phpDocument = (PHPDocument) buffer.getProperty(PHPSideKickParser.PHPDOCUMENT_PROPERTY);
-        if (phpDocument == null)
-        {
-            return null;
-        }
-        int line = buffer.getLineOfOffset(offset);
-        int lineStartOffset = buffer.getLineStartOffset(line);
-        int lineOffset = offset - lineStartOffset;
-        Statement statement = phpDocument.getStatementAt(line + 1  /* +1 because first line in my parser is 1*/,
-            lineOffset);
-        if (statement == null)
-            return null;
-        return processStatement(buffer, line, lineOffset, statement);
-    }
+	//{{{ getHyperlink() method
+	@Override
+	public Hyperlink getHyperlink(Buffer buffer, int offset)
+	{
+		PHPDocument phpDocument = (PHPDocument) buffer.getProperty(PHPSideKickParser.PHPDOCUMENT_PROPERTY);
+		if (phpDocument == null)
+		{
+			return null;
+		}
+		int line = buffer.getLineOfOffset(offset);
+		int lineStartOffset = buffer.getLineStartOffset(line);
+		int lineOffset = offset - lineStartOffset;
+		Statement statement = phpDocument.getStatementAt(line + 1  /* +1 because first line in my parser is 1*/,
+			lineOffset);
+		if (statement == null)
+			return null;
+		return processStatement(buffer, line, lineOffset, statement);
+	} //}}}
 
-    private static Hyperlink processStatement(Buffer buffer, int line, int lineOffset, Statement statement)
-    {
-        while (true)
-        {
-            if (statement instanceof FunctionCall)
-            {
-                FunctionCall functionCall = (FunctionCall) statement;
-                return processFunctionCall(functionCall, buffer, line, lineOffset);
-            }
-            else if (statement instanceof ClassAccess)
-            {
-                ClassAccess classAccess = (ClassAccess) statement;
-                return processClassAccess(classAccess, buffer, line, lineOffset);
-            }
-            else if (statement instanceof ClassInstantiation)
-            {
-                ClassInstantiation classInstantiation = (ClassInstantiation) statement;
-                return processClassInstantiation(classInstantiation, buffer, line, lineOffset);
-            }
-            Expression expression = statement.expressionAt(line + 1, lineOffset);
-            if (expression == null || expression == statement)
-                return null;
-            statement = expression;
-        }
-    }
+	
+	//{{{ processStatement() method
+	private static Hyperlink processStatement(Buffer buffer, int line, int lineOffset, Statement statement)
+	{
+		while (true)
+		{
+			if (statement instanceof FunctionCall)
+			{
+				FunctionCall functionCall = (FunctionCall) statement;
+				return processFunctionCall(functionCall, buffer, line, lineOffset);
+			}
+			else if (statement instanceof ClassAccess)
+			{
+				ClassAccess classAccess = (ClassAccess) statement;
+				return processClassAccess(classAccess, buffer, line, lineOffset);
+			}
+			else if (statement instanceof ClassInstantiation)
+			{
+				ClassInstantiation classInstantiation = (ClassInstantiation) statement;
+				return processClassInstantiation(classInstantiation, buffer, line, lineOffset);
+			}
+			Expression expression = statement.expressionAt(line + 1, lineOffset);
+			if (expression == null || expression == statement)
+				return null;
+			statement = expression;
+		}
+	} //}}}
+	
+	//{{{ processClassInstantiation() method
+	private static Hyperlink processClassInstantiation(ClassInstantiation classInstantiation, Buffer buffer, int line, int lineOffset)
+	{
+		String className = classInstantiation.getType().getClassName();
+		if (className == null)
+			return null;
+		Project project = ProjectManager.getInstance().getProject();
+		ClassHeader classHeader = project.getClass(className);
+		if (classHeader == null)
+			return null;
+		CharSequence lineSegment = buffer.getLineSegment(line);
+		String noWordSep = buffer.getStringProperty("noWordSep");
+		int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
+		int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
+		int lineStartOffset = buffer.getLineStartOffset(line);
+		Hyperlink hyperlink = new PHPHyperlink(classHeader.getName(), classHeader.getPath(), classHeader.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
+		return hyperlink;
+	} //}}}
 
-    private static Hyperlink processClassInstantiation(ClassInstantiation classInstantiation, Buffer buffer, int line, int lineOffset)
-    {
-        String className = classInstantiation.getType().getClassName();
-        if (className == null)
-            return null;
-        Project project = ProjectManager.getInstance().getProject();
-        ClassHeader classHeader = project.getClass(className);
-        if (classHeader == null)
-            return null;
-        CharSequence lineSegment = buffer.getLineSegment(line);
-        String noWordSep = buffer.getStringProperty("noWordSep");
-        int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
-        int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
-        int lineStartOffset = buffer.getLineStartOffset(line);
-        Hyperlink hyperlink = new PHPHyperlink(classHeader.getName(), classHeader.getPath(), classHeader.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
-        return hyperlink;
-    }
+	//{{{ processFunctionCall() method
+	private static Hyperlink processFunctionCall(FunctionCall functionCall, Buffer buffer, int line, int lineOffset)
+	{
+		Expression functionName = functionCall.getFunctionName();
+		Expression expressionAt = functionCall.expressionAt(line + 1, lineOffset);
+		if (expressionAt != functionName)
+		{
+			if (expressionAt == null)
+				return null;
+			// the cursor is not on the function name
+			return processStatement(buffer, line, lineOffset, expressionAt);
+		}
+		String name = functionName.toString();
+		Project project = ProjectManager.getInstance().getProject();
+		Object o = project.getMethods().get(name);
+		if (o == null)
+			return null;
+		MethodHeader header = null;
+		if (o instanceof List)
+		{
+			List<MethodHeader> headers = (List<MethodHeader>) o;
+			for (MethodHeader methodHeader : headers)
+			{
+				header = methodHeader;
+				break;
+			}
+		}
+		else
+		{
+			header = (MethodHeader) o;
+		}
+		if (header == null)
+			return null;
+		CharSequence lineSegment = buffer.getLineSegment(line);
+		String noWordSep = buffer.getStringProperty("noWordSep");
+		int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
+		int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
+		int lineStartOffset = buffer.getLineStartOffset(line);
+		Hyperlink hyperlink = new PHPHyperlink(header.getName(), header.getPath(), header.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
+		return hyperlink;
+	} //}}}
+	
+	//{{{ processClassAccess() method
+	private static Hyperlink processClassAccess(ClassAccess classAccess, Buffer buffer, int line, int lineOffset)
+	{
+		Project project = ProjectManager.getInstance().getProject();
+		if (project == null)
+			return null;
 
-    private static Hyperlink processFunctionCall(FunctionCall functionCall, Buffer buffer, int line, int lineOffset)
-    {
-        Expression functionName = functionCall.getFunctionName();
-        Expression expressionAt = functionCall.expressionAt(line + 1, lineOffset);
-        if (expressionAt != functionName)
-        {
-            if (expressionAt == null)
-                return null;
-            // the cursor is not on the function name
-            return processStatement(buffer, line, lineOffset, expressionAt);
-        }
-        String name = functionName.toString();
-        Project project = ProjectManager.getInstance().getProject();
-        Object o = project.getMethods().get(name);
-        if (o == null)
-            return null;
-        MethodHeader header = null;
-        if (o instanceof List)
-        {
-            List<MethodHeader> headers = (List<MethodHeader>) o;
-            for (MethodHeader methodHeader : headers)
-            {
-                header = methodHeader;
-                break;
-            }
-        }
-        else
-        {
-            header = (MethodHeader) o;
-        }
-        if (header == null)
-            return null;
-        CharSequence lineSegment = buffer.getLineSegment(line);
-        String noWordSep = buffer.getStringProperty("noWordSep");
-        int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
-        int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
-        int lineStartOffset = buffer.getLineStartOffset(line);
-        Hyperlink hyperlink = new PHPHyperlink(header.getName(), header.getPath(), header.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
-        return hyperlink;
-    }
+		String noWordSep = buffer.getStringProperty("noWordSep");
+		CharSequence lineSegment = buffer.getLineSegment(line);
+		int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
+		int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
+		
+		Expression prefix = classAccess.getPrefix();
+		
+		ClassHeader classHeader = null;
+		if (prefix instanceof ConstantIdentifier)
+		{
+			classHeader = project.getClass(classAccess.getName());
+		}
+		else if (prefix instanceof Variable)
+		{
+			Variable variable = (Variable) prefix;
+			Type type = variable.getType();
+			String className = type.getClassName();
+			if (className != null)
+				classHeader = project.getClass(className);
+		}
+		if (classHeader == null)
+			return null;
+		int lineStartOffset = buffer.getLineStartOffset(line);
+		Expression expression = classAccess.expressionAt(line + 1, lineOffset);
+		if (expression == classAccess.getPrefix())
+		{
+			Hyperlink hyperlink = new PHPHyperlink(classHeader.getName(),
+				classHeader.getPath(),
+				classHeader.getBeginLine(), lineStartOffset + 
+				wordStart, lineStartOffset + wordEnd, line);
+			return hyperlink;
+		}
+		Expression suffix = classAccess.getSuffix();
+		if (suffix instanceof FunctionCall)
+		{
+			FunctionCall functionCall = (FunctionCall) suffix;
+			Expression functionName = functionCall.getFunctionName();
+			Expression expr = functionCall.expressionAt(line + 1, lineOffset);
+			if (expr != functionName)
+			{
+				return processStatement(buffer, line, lineOffset, expr);
+			}
+			List<MethodHeader> methodsHeaders = classHeader.getMethodsHeaders();
+			MethodHeader header = null;
+			for (MethodHeader methodsHeader : methodsHeaders)
+			{
+				if (methodsHeader.getName().equals(functionName.toString()))
+				{
+					header = methodsHeader;
+					break;
+				}
+			}
+			if (header == null)
+				return null;
+			
+			Hyperlink hyperlink = new PHPHyperlink(header.getName(), header.getPath(), header.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
+			return hyperlink;
+		}
+		System.out.println(suffix);
+		return null;
+	} //}}}
 
-    private static Hyperlink processClassAccess(ClassAccess classAccess, Buffer buffer, int line, int lineOffset)
-    {
-        String noWordSep = buffer.getStringProperty("noWordSep");
-        CharSequence lineSegment = buffer.getLineSegment(line);
-        int wordStart = TextUtilities.findWordStart(lineSegment, lineOffset, noWordSep);
-        int wordEnd = TextUtilities.findWordEnd(lineSegment, lineOffset, noWordSep);
-
-        Expression prefix = classAccess.getPrefix();
-        ClassHeader classHeader = null;
-        if (prefix instanceof ConstantIdentifier)
-        {
-            Project project = ProjectManager.getInstance().getProject();
-            classHeader = project.getClass(classAccess.getName());
-        }
-        else if (prefix instanceof Variable)
-        {
-            Variable variable = (Variable) prefix;
-            Type type = variable.getType();
-            String className = type.getClassName();
-            if (className != null)
-            {
-                Project project = ProjectManager.getInstance().getProject();
-                classHeader = project.getClass(className);
-            }
-        }
-        if (classHeader == null)
-            return null;
-         int lineStartOffset = buffer.getLineStartOffset(line);
-        Expression expression = classAccess.expressionAt(line + 1, lineOffset);
-        if (expression == classAccess.getPrefix())
-        {
-            Hyperlink hyperlink = new PHPHyperlink(classHeader.getName(), classHeader.getPath(), classHeader.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
-            return hyperlink;
-        }
-        Expression suffix = classAccess.getSuffix();
-        if (suffix instanceof FunctionCall)
-        {
-            FunctionCall functionCall = (FunctionCall) suffix;
-            Expression functionName = functionCall.getFunctionName();
-            Expression expr = functionCall.expressionAt(line + 1, lineOffset);
-            if (expr != functionName)
-            {
-                return processStatement(buffer, line, lineOffset, expr);
-            }
-            List<MethodHeader> methodsHeaders = classHeader.getMethodsHeaders();
-            MethodHeader header = null;
-            for (MethodHeader methodsHeader : methodsHeaders)
-            {
-                if (methodsHeader.getName().equals(functionName.toString()))
-                {
-                    header = methodsHeader;
-                    break;
-                }
-            }
-            if (header == null)
-                return null;
-
-            Hyperlink hyperlink = new PHPHyperlink(header.getName(), header.getPath(), header.getBeginLine(), lineStartOffset + wordStart, lineStartOffset + wordEnd, line);
-            return hyperlink;
-        }
-        System.out.println(suffix);
-        return null;
-    }
 }
