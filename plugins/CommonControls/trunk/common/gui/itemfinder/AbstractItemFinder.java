fa4186ca4d435ef2ca411fadbd49db5541e87889
kpouer <kpouer@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2011-11-03 23:18:51 +0000

    api for quicksearch gui

--- plugins/CommonControls/trunk/common/gui/itemfinder/AbstractItemFinder.java
+++ plugins/CommonControls/trunk/common/gui/itemfinder/AbstractItemFinder.java
@@ -1,15 +1,14 @@
 /*
- * ItemFinder.java
+ * AbstractItemFinder.java
  * :tabSize=8:indentSize=8:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
- * Copyright (C) 2011 Matthieu Casanova
+ * Copyright © 2011 Matthieu Casanova
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,47 +18,32 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package common.gui.itemfinder;
 
 import javax.swing.*;
 
 /**
+ * An abstract ItemFinder, it has no label, default cellrenderer and width.
  * @author Matthieu Casanova
  */
-public interface ItemFinder<E>
+public abstract class AbstractItemFinder<E> implements ItemFinder<E>
 {
-	String getLabel();
+	@Override
+	public String getLabel()
+	{
+		return null;
+	}
 
-	/**
-	 * Returns the width of the window
-	 * Return -1 if you want default value
-	 * @return the width of the list
-	 */
-	int getWidth();
+	@Override
+	public int getWidth()
+	{
+		return -1;
+	}
 
-	/**
-	 * The list model that is used by the ItemFinderWindow
-	 * @return a listmodel
-	 */
-	ListModel getModel();
-
-	/**
-	 * Returns a ListCellRenderer that will render the celles of the dropdown list.
-	 * If null, the default one is used
-	 * @return the ListCellRenderer
-	 */
-	ListCellRenderer getListCellRenderer();
-
-	/**
-	 * Some chars were typed, update the list
-	 * @param s the searched string
-	 */
-	void updateList(String s);
-
-	/**
-	 * A selection has been made.
-	 * Do an action
-	 * @param item the selected item
-	 */
-	void selectionMade(E item);
+	@Override
+	public ListCellRenderer getListCellRenderer()
+	{
+		return null;
+	}
 }
--- plugins/CommonControls/trunk/common/gui/itemfinder/PathCellRenderer.java
+++ plugins/CommonControls/trunk/common/gui/itemfinder/PathCellRenderer.java
@@ -1,15 +1,14 @@
 /*
- * ItemFinder.java
+ * jEdit - Programmer's Text Editor
  * :tabSize=8:indentSize=8:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
- * Copyright (C) 2011 Matthieu Casanova
+ * Copyright © 2011 Matthieu Casanova
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,47 +18,30 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package common.gui.itemfinder;
 
-import javax.swing.*;
+import java.awt.Component;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+
+import org.gjt.sp.jedit.MiscUtilities;
 
 /**
+ * A list cell renderer that renders path.
  * @author Matthieu Casanova
  */
-public interface ItemFinder<E>
+public class PathCellRenderer extends DefaultListCellRenderer
 {
-	String getLabel();
-
-	/**
-	 * Returns the width of the window
-	 * Return -1 if you want default value
-	 * @return the width of the list
-	 */
-	int getWidth();
-
-	/**
-	 * The list model that is used by the ItemFinderWindow
-	 * @return a listmodel
-	 */
-	ListModel getModel();
-
-	/**
-	 * Returns a ListCellRenderer that will render the celles of the dropdown list.
-	 * If null, the default one is used
-	 * @return the ListCellRenderer
-	 */
-	ListCellRenderer getListCellRenderer();
-
-	/**
-	 * Some chars were typed, update the list
-	 * @param s the searched string
-	 */
-	void updateList(String s);
-
-	/**
-	 * A selection has been made.
-	 * Do an action
-	 * @param item the selected item
-	 */
-	void selectionMade(E item);
+	@Override
+	public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+						      boolean cellHasFocus)
+	{
+		super.getListCellRendererComponent(list, value, index, isSelected,
+							  cellHasFocus);
+		String path = String.valueOf(value);
+		String fileName = MiscUtilities.getFileName(path);
+		setText("<html><b>"+fileName+"</b> <gray>("+path+")</gray></html>");
+		return this;
+	}
 }
