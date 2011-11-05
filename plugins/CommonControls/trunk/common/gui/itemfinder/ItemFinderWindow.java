f29d5b5ba09fcc7b226b537a604c6af6e7ccc25b
Matthieu Casanova <chocolat.mou@gmail.com>
2011-11-05 19:00:41 +0000

    major cleanup

--- plugins/CommonControls/trunk/common/gui/itemfinder/ItemFinderWindow.java
+++ plugins/CommonControls/trunk/common/gui/itemfinder/ItemFinderWindow.java
@@ -21,18 +21,14 @@
  */
 package common.gui.itemfinder;
 
-import org.gjt.sp.jedit.GUIUtilities;
-import org.gjt.sp.jedit.View;
-
 //{{{ Imports
-import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
+import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.gjt.sp.jedit.View;
 //}}}
 
 /**
@@ -42,53 +38,27 @@ import java.awt.event.MouseEvent;
  * To use it, you have to implement {@link ItemFinder}.
  *
  * Then you have two choice : instantiate {@link ItemFinderWindow} and make it visible.
- * or use {@link ItemFinderWindow#showWindow()} that will create the window and show it.
+ * or use {@link ItemFinderWindow#showWindow(View, ItemFinder)} that will create the window and show it.
  * @author Matthieu Casanova
  */
 public class ItemFinderWindow<E> extends JFrame
 {
-	/**
-	 * This window will contains the scroll with the items.
-	 */
-	private final JWindow window;
-	private final JTextField searchField;
-	private final JList itemList;
-
-	public final RequestFocusWorker requestFocusWorker;
-	private final ItemFinder<E> itemFinder;
+	private ItemFinderPanel<E> itemFinderPanel;
 
 	//{{{ ItemFinderWindow constructor
 	public ItemFinderWindow(ItemFinder<E> itemFinder)
 	{
-		this.itemFinder = itemFinder;
 		setUndecorated(true);
-		window = new JWindow(this);
-		searchField = new JTextField(50);
-
-		itemList = new JList(itemFinder.getModel());
-		itemList.setBorder(BorderFactory.createEtchedBorder());
-		if (itemFinder.getListCellRenderer() != null)
-			itemList.setCellRenderer(itemFinder.getListCellRenderer());
-		itemList.addKeyListener(new ItemListKeyAdapter(searchField));
-		itemList.addMouseListener(new MyMouseAdapter());
-
-		searchField.addKeyListener(new SearchFieldKeyAdapter());
-		searchField.getDocument().addDocumentListener(new MyDocumentListener());
-		JScrollPane scroll = new JScrollPane(itemList);
-		window.setContentPane(scroll);
+		itemFinderPanel = new ItemFinderPanel<E>(this, itemFinder);
 		JPanel panel = new JPanel(new BorderLayout());
 
 		String label = itemFinder.getLabel();
 		if (label != null)
 			panel.add(new JLabel(label), BorderLayout.NORTH);
 
-		panel.add(searchField, BorderLayout.CENTER);
+		panel.add(itemFinderPanel, BorderLayout.CENTER);
 		setContentPane(panel);
-		window.pack();
-		requestFocusWorker = new RequestFocusWorker(searchField);
 		pack();
-		if (itemFinder.getWidth() != -1)
-			setSize(itemFinder.getWidth(), getHeight());
 	} //}}}
 
 	//{{{ showWindow() method
@@ -99,25 +69,6 @@ public class ItemFinderWindow<E> extends JFrame
 		itemFinderWindow.setVisible(true);
 	} //}}}
 
-	//{{{ setVisible() method
-	@Override
-	public void setVisible(boolean b)
-	{
-		Rectangle bounds = getBounds();
-		window.setLocation(bounds.x, bounds.y + bounds.height);
-		GUIUtilities.requestFocus(this, searchField);
-		window.setVisible(false);
-		super.setVisible(b);
-	} //}}}
-
-	//{{{ dispose()
-	@Override
-	public void dispose()
-	{
-		window.dispose();
-		super.dispose();
-	} //}}}
-
 	//{{{ handledByList() method
 	private static boolean handledByList(KeyEvent e)
 	{
@@ -126,131 +77,4 @@ public class ItemFinderWindow<E> extends JFrame
 			e.getKeyCode() == KeyEvent.VK_PAGE_DOWN ||
 			e.getKeyCode() == KeyEvent.VK_PAGE_UP;
 	} //}}}
-
-	//{{{ select() method
-	private void select()
-	{
-		E value = (E) itemList.getSelectedValue();
-		if (value != null)
-		{
-			itemFinder.selectionMade(value);
-			dispose();
-		}
-	} //}}}
-
-	//{{{ SearchFieldKeyAdapter class
-	private class SearchFieldKeyAdapter extends KeyAdapter
-	{
-		@Override
-		public void keyPressed(KeyEvent e)
-		{
-			if (handledByList(e))
-			{
-				itemList.dispatchEvent(e);
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
-			{
-				dispose();
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_ENTER)
-			{
-				select();
-			}
-		}
-	} //}}}
-
-	//{{{ ItemListKeyAdapter class
-	private static class ItemListKeyAdapter extends KeyAdapter
-	{
-		private final JTextField searchField;
-
-		private ItemListKeyAdapter(JTextField searchField)
-		{
-			this.searchField = searchField;
-		}
-
-		@Override
-		public void keyTyped(KeyEvent e)
-		{
-			searchField.dispatchEvent(e);
-		}
-
-		@Override
-		public void keyPressed(KeyEvent e)
-		{
-			if (!handledByList(e))
-			{
-				searchField.dispatchEvent(e);
-			}
-		}
-	} //}}}
-
-	//{{{ MyMouseAdapter class
-	private class MyMouseAdapter extends MouseAdapter
-	{
-		@Override
-		public void mouseClicked(MouseEvent e)
-		{
-			if (e.getClickCount() == 2)
-			{
-				select();
-			}
-		}
-	} //}}}
-
-	//{{{ RequestFocusWorker class
-	protected static class RequestFocusWorker implements Runnable
-	{
-		private final JTextField searchField;
-
-		private RequestFocusWorker(JTextField searchField)
-		{
-			this.searchField = searchField;
-		}
-
-		public void run()
-		{
-			searchField.requestFocus();
-		}
-	} //}}}
-
-	//{{{ MyDocumentListener class
-	private class MyDocumentListener implements DocumentListener
-	{
-		public void insertUpdate(DocumentEvent e)
-		{
-			updateList(searchField.getText());
-		}
-
-		public void removeUpdate(DocumentEvent e)
-		{
-			updateList(searchField.getText());
-		}
-
-		public void changedUpdate(DocumentEvent e)
-		{
-			updateList(searchField.getText());
-		}
-
-		private void updateList(String s)
-		{
-			itemFinder.updateList(s);
-			int size = itemList.getModel().getSize();
-			if (size == 0)
-			{
-				itemList.clearSelection();
-			}
-			else
-			{
-				window.pack();
-				window.setVisible(true);
-				if (itemList.getSelectedIndex() == -1)
-				{
-					itemList.setSelectedIndex(0);
-				}
-			}
-			EventQueue.invokeLater(requestFocusWorker);
-		}
-	} //}}}
-
 }
