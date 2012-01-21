c4f0e8283dd00dd89c09df1639e60387bd30dac4
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-01-21 18:23:27 +0000

    prep for release

--- plugins/SynchroScroll/trunk/synchroscroll/SynchroScrollPlugin.java
+++ plugins/SynchroScroll/trunk/synchroscroll/SynchroScrollPlugin.java
@@ -7,43 +7,51 @@ import java.util.HashMap;
 
 public class SynchroScrollPlugin extends EBPlugin {
 
+    // key for component property that stores the initial top line of the text area
     static final String BASELINE = "synchroscroll.BaseLine";
-    
+
+    // key for component property to store a reference to the current scroll
+    // handler for a text area. This wouldn't be necessary if text area had a
+    // "getScrollListeners" method.
+    static final String SCROLLHANDLER = "synchroscroll.ScrollHandler";
+
+    // map to keep track of which views are currently using syncroscrolling
     private static HashMap<View, Boolean> scrollingMap = new HashMap<View, Boolean>();
-    private static HashMap<TextArea, ScrollHandler> scrollHandlerMap = new HashMap<TextArea, ScrollHandler>(); 
 
     public void start() { }
 
-    public void stop() { 
-        for (TextArea textArea : scrollHandlerMap.keySet()) {
-            textArea.removeScrollListener(scrollHandlerMap.get(textArea)); 
-            textArea.putClientProperty(BASELINE, null);
+    public void stop() {
+        // remove scrollhandlers from all text areas and reset client properties
+        View[] views = jEdit.getViews();
+        for (View view : views) {
+            EditPane[] editPanes = view.getEditPanes();
+            for (EditPane editPane : editPanes) {
+                TextArea textArea = editPane.getTextArea();
+                removeScrollHandler( textArea );
+                textArea.putClientProperty( BASELINE, null );
+            }
         }
-        scrollHandlerMap.clear();
         scrollingMap.clear();
     }
 
     public void handleMessage( EBMessage message ) {
-        if (message instanceof ViewUpdate) {
-            ViewUpdate vu = (ViewUpdate)message;
-            if (vu.CLOSED.equals(vu.getWhat())) {
-                scrollingMap.remove(vu.getView());   
+        if ( message instanceof ViewUpdate ) {
+            ViewUpdate vu = ( ViewUpdate ) message;
+            if ( vu.CLOSED.equals( vu.getWhat() ) ) {
+                scrollingMap.remove( vu.getView() );
             }
-        }
-        else if (message instanceof EditPaneUpdate) {
-            EditPaneUpdate epu = (EditPaneUpdate)message;
-            if (epu.DESTROYED.equals(epu.getWhat())) {
-                scrollHandlerMap.remove(epu.getEditPane().getTextArea());   
-            }
-            else if (epu.CREATED.equals(epu.getWhat())) {
+        } else if ( message instanceof EditPaneUpdate ) {
+            EditPaneUpdate epu = ( EditPaneUpdate ) message;
+            if ( epu.CREATED.equals( epu.getWhat() ) ) {
                 EditPane editPane = epu.getEditPane();
                 View view = editPane.getView();
-                if (scrollingMap.containsKey(view) && scrollingMap.get(view)) {
+                if ( scrollingMap.containsKey( view ) && scrollingMap.get( view ) ) {
                     TextArea textArea = editPane.getTextArea();
-                    ScrollHandler scrollHandler = new ScrollHandler(view);
-                    textArea.addScrollListener(scrollHandler);
-                    textArea.putClientProperty(BASELINE, textArea.getFirstLine());
-                    scrollHandlerMap.put(textArea, scrollHandler);
+                    removeScrollHandler( textArea );
+                    ScrollHandler scrollHandler = new ScrollHandler( view );
+                    textArea.addScrollListener( scrollHandler );
+                    textArea.putClientProperty( SCROLLHANDLER, scrollHandler );
+                    textArea.putClientProperty( BASELINE, textArea.getFirstLine() );
                 }
             }
         }
@@ -53,28 +61,31 @@ public class SynchroScrollPlugin extends EBPlugin {
         boolean scrolling = false;
         if ( scrollingMap.containsKey( view ) ) {
             scrolling = !scrollingMap.get( view );
-            scrollingMap.put(view, scrolling);
-        }
-        else {
+            scrollingMap.put( view, scrolling );
+        } else {
             scrolling = true;
-            scrollingMap.put(view, scrolling);
+            scrollingMap.put( view, scrolling );
         }
         EditPane[] editPanes = view.getEditPanes();
         for ( EditPane editPane : editPanes ) {
             TextArea textArea = editPane.getTextArea();
+            removeScrollHandler( textArea );
             if ( scrolling ) {
-                ScrollHandler scrollHandler = new ScrollHandler(view);
-                textArea.addScrollListener(scrollHandler);
-                textArea.putClientProperty(BASELINE, textArea.getFirstLine());
-                scrollHandlerMap.put(textArea, scrollHandler);
+                ScrollHandler scrollHandler = new ScrollHandler( view );
+                textArea.addScrollListener( scrollHandler );
+                textArea.putClientProperty( SCROLLHANDLER, scrollHandler );
+                textArea.putClientProperty( BASELINE, textArea.getFirstLine() );
             } else {
-                ScrollHandler scrollHandler = scrollHandlerMap.get(textArea);
-                if (scrollHandler != null) {
-                    textArea.removeScrollListener(scrollHandler);
-                    textArea.putClientProperty(BASELINE, null);
-                    scrollHandlerMap.remove(textArea);
-                }
+                textArea.putClientProperty( BASELINE, null );
             }
         }
+    }
+
+    private static void removeScrollHandler( TextArea textArea ) {
+        ScrollHandler scrollHandler = ( ScrollHandler ) textArea.getClientProperty( SCROLLHANDLER );
+        if ( scrollHandler != null ) {
+            textArea.removeScrollListener( scrollHandler );
+        }
+        textArea.putClientProperty( SCROLLHANDLER, null );
     }
 }
