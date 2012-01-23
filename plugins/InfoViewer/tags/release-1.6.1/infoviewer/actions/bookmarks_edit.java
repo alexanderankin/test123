20ab41b96c8e19ea80a1eb4e2758a7593129233c
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-01-23 16:50:08 +0000

    Tagging 1.6.1 for release.

---
+++ plugins/InfoViewer/tags/release-1.6.1/infoviewer/actions/bookmarks_edit.java
@@ -0,0 +1,40 @@
+/*
+ * bookmarks_edit.java - open "Edit Bookmarks" dialog in InfoViewer
+ * Copyright (C) 1999 Dirk Moebius
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package infoviewer.actions;
+
+import infoviewer.*;
+import java.awt.event.*;
+
+
+public class bookmarks_edit extends InfoViewerAction {
+
+
+	private static final long serialVersionUID = 8814698507050714830L;
+
+	public bookmarks_edit() {
+        super("infoviewer.bookmarks_edit");
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        new BookmarksDialog(getFrame(evt));
+    }
+
+}
+
