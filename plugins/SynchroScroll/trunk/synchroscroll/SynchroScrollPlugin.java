ac11b5d16ca38e832bd050e3ff7d7d7a7074486f
Dale <danson@grafidog.com>
2012-02-10 22:21:41 +0000

    General clean up -- fixed some typos, added licenses, bumped version number, better handling of scroll listeners, don't hassle with JDiff.

--- plugins/SynchroScroll/trunk/synchroscroll/SynchroScrollPlugin.java
+++ plugins/SynchroScroll/trunk/synchroscroll/SynchroScrollPlugin.java
@@ -1,39 +1,69 @@
+/*
+ * Copyright (C) 2012 Dale Anson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+ 
 package synchroscroll;
 
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.ScrollListener;
 import org.gjt.sp.jedit.textarea.TextArea;
+
 import java.util.HashMap;
 
+import javax.swing.event.EventListenerList;
+
+
+/**
+ * Handles adding and removing scroll handlers to the text areas in the 
+ * various view to facilitate synchronized scrolling between text areas.
+ */
 public class SynchroScrollPlugin extends EBPlugin {
 
     // key for component property that stores the initial top line of the text area
     static final String BASELINE = "synchroscroll.BaseLine";
-
-    // key for component property to store a reference to the current scroll
-    // handler for a text area. This wouldn't be necessary if text area had a
-    // "getScrollListeners" method.
-    static final String SCROLLHANDLER = "synchroscroll.ScrollHandler";
 
     // map to keep track of which views are currently using syncroscrolling
     private static HashMap<View, Boolean> scrollingMap = new HashMap<View, Boolean>();
 
     public void start() { }
 
+    // remove scrollhandlers from all text areas
     public void stop() {
-        // remove scrollhandlers from all text areas and reset client properties
         View[] views = jEdit.getViews();
-        for (View view : views) {
+        for ( View view : views ) {
             EditPane[] editPanes = view.getEditPanes();
-            for (EditPane editPane : editPanes) {
+            for ( EditPane editPane : editPanes ) {
                 TextArea textArea = editPane.getTextArea();
                 removeScrollHandler( textArea );
-                textArea.putClientProperty( BASELINE, null );
             }
         }
         scrollingMap.clear();
     }
-
+    
+    /**
+     * Adjusts synchroscrolling based on the message:
+     * <ul>
+     * <li>Turns off synchroscroll for a View when the View is closed.</li>
+     * <li>Adds scroller to new edit pane in View.</li>
+     * <li>Turns off synchroscroll if JDiff is on.</li>
+     * </ul>
+     * @param message The edit bus message to take action for.
+     */
     public void handleMessage( EBMessage message ) {
         if ( message instanceof ViewUpdate ) {
             ViewUpdate vu = ( ViewUpdate ) message;
@@ -48,15 +78,33 @@ public class SynchroScrollPlugin extends EBPlugin {
                 if ( scrollingMap.containsKey( view ) && scrollingMap.get( view ) ) {
                     TextArea textArea = editPane.getTextArea();
                     removeScrollHandler( textArea );
-                    ScrollHandler scrollHandler = new ScrollHandler( view );
-                    textArea.addScrollListener( scrollHandler );
-                    textArea.putClientProperty( SCROLLHANDLER, scrollHandler );
-                    textArea.putClientProperty( BASELINE, textArea.getFirstLine() );
+                    addScrollHandler( view, textArea );
                 }
+            }
+        } else {
+            try {
+                // turn off synchroscrolling if JDiff is diffing.
+                // Sort of a hack since I don't want to have to have JDiff as a
+                // dependency, either at compile time or run time.
+                String classname = message.getClass().getName();
+                if ( classname != null && "jdiff.DiffMessage".equals( classname ) ) {
+                    String what = ( String ) PrivilegedAccessor.invokeMethod( message, "getWhat", null );
+                    String on = ( String ) PrivilegedAccessor.getValue( message, "ON" );
+                    if ( what != null && what.equals( on ) ) {
+                        View view = ( View ) message.getSource();
+                        SynchroScrollPlugin.setScrolling( view, false );
+                    }
+                }
+            } catch ( Exception e ) {                // NOPMD
+                // don't worry about it
             }
         }
     }
-
+    
+    /**
+     * Turn on sychroscrolling if it was off, turn it off if it was on
+     * @param view The View to toggle synchroscrolling for.
+     */
     public static void toggleSynchroScroll( View view ) {
         boolean scrolling = false;
         if ( scrollingMap.containsKey( view ) ) {
@@ -66,26 +114,52 @@ public class SynchroScrollPlugin extends EBPlugin {
             scrolling = true;
             scrollingMap.put( view, scrolling );
         }
+        setScrolling( view, scrolling );
+    }
+    
+    /**
+     * Turn off synchroscrolling for the given View.
+     * @param view The View to stop synchroscrolling.
+     */
+    public static void setSynchroscrollOff(View view) {
+        setScrolling(view, false);   
+    }
+
+    // turn synchroscrolling on or off
+    private static void setScrolling( View view, boolean scrolling ) {
+        scrollingMap.put( view, scrolling );
         EditPane[] editPanes = view.getEditPanes();
         for ( EditPane editPane : editPanes ) {
             TextArea textArea = editPane.getTextArea();
             removeScrollHandler( textArea );
             if ( scrolling ) {
-                ScrollHandler scrollHandler = new ScrollHandler( view );
-                textArea.addScrollListener( scrollHandler );
-                textArea.putClientProperty( SCROLLHANDLER, scrollHandler );
-                textArea.putClientProperty( BASELINE, textArea.getFirstLine() );
-            } else {
-                textArea.putClientProperty( BASELINE, null );
+                addScrollHandler( view, textArea );
             }
         }
     }
 
+    // adds a new ScrollHandler to the given textArea
+    private static void addScrollHandler( View view, TextArea textArea ) {
+        ScrollHandler scrollHandler = new ScrollHandler( view );
+        textArea.addScrollListener( scrollHandler );
+        textArea.putClientProperty( BASELINE, textArea.getFirstLine() );
+    }
+
+    // remove all of our ScrollHandlers from the given text area. There should
+    // be only one. Another use of PrivilegedAccessor since TextArea doesn't
+    // have a method to get the scroll handlers directly.
     private static void removeScrollHandler( TextArea textArea ) {
-        ScrollHandler scrollHandler = ( ScrollHandler ) textArea.getClientProperty( SCROLLHANDLER );
-        if ( scrollHandler != null ) {
-            textArea.removeScrollListener( scrollHandler );
+        try {
+            EventListenerList listenerList = ( EventListenerList ) PrivilegedAccessor.getValue( textArea, "listenerList" );
+            ScrollListener[] listeners = listenerList.getListeners( ScrollListener.class );
+            for ( ScrollListener listener : listeners ) {
+                if ( listener instanceof ScrollHandler ) {
+                    textArea.removeScrollListener( listener );
+                }
+            }
+            textArea.putClientProperty( BASELINE, null );
+        } catch ( Exception e ) {            // NOPMD
+            // don't worry about it
         }
-        textArea.putClientProperty( SCROLLHANDLER, null );
     }
 }
