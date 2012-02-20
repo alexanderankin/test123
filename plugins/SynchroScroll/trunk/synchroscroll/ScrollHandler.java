2d8b234e8823806eafa5915b64155784fb34827d
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-02-20 17:50:17 +0000

    Added updates from Jerzy Urban for proportional scrolling.

--- plugins/SynchroScroll/trunk/synchroscroll/ScrollHandler.java
+++ plugins/SynchroScroll/trunk/synchroscroll/ScrollHandler.java
@@ -14,28 +14,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
- 
+*/
+
 package synchroscroll;
 
-
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.textarea.ScrollListener;
 import org.gjt.sp.jedit.textarea.TextArea;
-import org.gjt.sp.jedit.View;
-import org.gjt.sp.jedit.EditPane;
 
 /**
  * Synchronizes both horizontal and vertical scrolling between all text areas
  * in a View.
  */
 public class ScrollHandler implements ScrollListener {
-    
+
     View view;
 
-    ScrollHandler(View view) {
+    ScrollHandler( View view ) {
         this.view = view;
     }
-    
+
     /**
      * Synchronizes horizontal scrolling between all text areas in the View.
      * @param textArea The text area doing the scrolling.
@@ -43,9 +43,9 @@ public class ScrollHandler implements ScrollListener {
     public void scrolledHorizontally( TextArea textArea ) {
         int offset = textArea.getHorizontalOffset();
         EditPane[] editPanes = view.getEditPanes();
-        for(EditPane editPane : editPanes) {
+        for ( EditPane editPane : editPanes ) {
             TextArea other = editPane.getTextArea();
-            other.setHorizontalOffset(offset);
+            other.setHorizontalOffset( offset );
         }
     }
 
@@ -55,18 +55,39 @@ public class ScrollHandler implements ScrollListener {
      */
     public void scrolledVertically( TextArea textArea ) {
         int firstLine = textArea.getFirstLine();
-        Integer BL = (Integer)textArea.getClientProperty(SynchroScrollPlugin.BASELINE);
+        Integer BL = ( Integer ) textArea.getClientProperty( SynchroScrollPlugin.BASELINE );
         int baseline = BL == null ? 0 : BL.intValue();
         int offset = firstLine - baseline;
         EditPane[] editPanes = view.getEditPanes();
-        for(EditPane editPane : editPanes) {
-            TextArea other = editPane.getTextArea();
-            Integer otherBL = (Integer)other.getClientProperty(SynchroScrollPlugin.BASELINE);
-            int otherBaseline = otherBL == null ? 0 : otherBL.intValue();
-            int otheroffset = otherBaseline + offset;
-            otheroffset = otheroffset < 0 ? 0 : otheroffset;
-            otheroffset = otheroffset >= other.getLineCount() ? other.getLineCount() - 1 : otheroffset;
-            other.setFirstLine(otheroffset);
+
+        Boolean standardMode = jEdit.getBooleanProperty( "options.SynchroScrollPluginPane.standardMode.value" );
+
+        if ( standardMode ) {
+            for ( EditPane editPane : editPanes ) {
+                TextArea other = editPane.getTextArea();
+                Integer otherBL = ( Integer ) other.getClientProperty( SynchroScrollPlugin.BASELINE );
+                int otherBaseline = otherBL == null ? 0 : otherBL.intValue();
+                int otherOffset = otherBaseline + offset;
+                otherOffset = otherOffset < 0 ? 0 : otherOffset;
+                otherOffset = otherOffset >= other.getLineCount() ? other.getLineCount() - 1 : otherOffset;
+                other.setFirstLine( otherOffset );
+            }
+        } else {
+            // Proportional mode
+
+            // Prevents recursive scroll event invoking
+            if ( jEdit.getBooleanProperty( SynchroScrollPlugin.ISPROCESSING ) == false ) {
+                jEdit.setBooleanProperty( SynchroScrollPlugin.ISPROCESSING, true );
+
+                float sourceNormalizedScrollPosition = ( float ) firstLine / ( float ) ( textArea.getLineCount() - textArea.getVisibleLines() + 1 );
+
+                for ( EditPane otherEditPane : editPanes ) {
+                    TextArea otherTextArea = otherEditPane.getTextArea();
+                    int otherNewFirstLine = ( int ) ( sourceNormalizedScrollPosition * ( otherTextArea.getLineCount() - otherTextArea.getVisibleLines() + 1 ) );
+                    otherTextArea.setFirstLine( otherNewFirstLine );
+                }
+                jEdit.setBooleanProperty( SynchroScrollPlugin.ISPROCESSING, false );
+            }
         }
     }
 }
