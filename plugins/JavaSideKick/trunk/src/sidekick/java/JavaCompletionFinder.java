95631898f8bf533b8725627ee87464f9a68f727c
kog13 <kog13@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-02-27 04:20:42 +0000

    Adding more comments and beginning some basic code cleanup

--- plugins/JavaSideKick/trunk/src/sidekick/java/JavaCompletionFinder.java
+++ plugins/JavaSideKick/trunk/src/sidekick/java/JavaCompletionFinder.java
@@ -1,5 +1,21 @@
+/*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 package sidekick.java;
-// imports
+
+//{{{ Imports
 import java.lang.reflect.*;
 import java.util.*;
 import javax.swing.*;
@@ -15,6 +31,7 @@ import org.gjt.sp.jedit.buffer.JEditBuffer;
 
 import sidekick.SideKickParsedData;
 import sidekick.util.Location;
+//}}}
 
 /**
  * Finds 2 kinds of completions, word completions and method/field completions,
