69dab5191215393452a48b41361a927c4c4a1130
Alan Ezust <1688991+ezust@users.noreply.github.com>
2012-03-17 21:57:48 +0000

    Updated license/copyright.

--- plugins/SshConsole/trunk/console/ssh/StreamThread.java
+++ plugins/SshConsole/trunk/console/ssh/StreamThread.java
@@ -1,17 +1,3 @@
-/*            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-
- Everyone is permitted to copy and distribute verbatim or modified
- copies of this license document, and changing it is allowed as long
- as the name is changed.
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
 package console.ssh;
 
 
@@ -38,7 +24,8 @@ import errorlist.DefaultErrorSource;
 // {{{ class StreamThread
 /**
  * Thread for handing output of running remote ssh commands 
- * Based on the StreamThread.java from Console plugin: StreamThread.java 10982 2007-11-06 05:02:42Z.
+ * Based on the StreamThread.java from Console plugin: 
+ * StreamThread.java * 10982 2007-11-06 05:02:42Z.
  *
  * @version $Id$
  */
