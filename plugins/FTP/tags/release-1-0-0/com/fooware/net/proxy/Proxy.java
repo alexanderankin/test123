62b18d665af029ccb34746113fd3fdc55cfaad65
Jarek Czekalski <jarekczek@poczta.onet.pl>
2012-05-12 06:50:59 +0000

    Tagged 1.0.0 for release

---
+++ plugins/FTP/tags/release-1-0-0/com/fooware/net/proxy/Proxy.java
@@ -0,0 +1,21 @@
+package com.fooware.net.proxy;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Basic interface for socket factories aka Proxy
+ * @author Vadim Voituk
+ * @see ProxyHTTP
+ * 
+ * TODO: Implement ProxyNone (ProxyDirect) null-object
+ * TODO: Implement ProxySocks class 
+ */
+public interface Proxy {
+	
+	/**
+	 * Opens socket to specified host and port 
+	 */
+	public Socket openSocket(String hostName, int port) throws IOException;
+	
+}
