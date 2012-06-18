382fd88c903cbee191371590e2f610da2961cffd
Alan Ezust <1688991+ezust@users.noreply.github.com>
2012-06-18 22:34:14 +0000

    Tabsize=4.

--- plugins/ErrorList/trunk/errorlist/ErrorOverview.java
+++ plugins/ErrorList/trunk/errorlist/ErrorOverview.java
@@ -1,6 +1,6 @@
 /*
  * ErrorOverview.java - Error overview component
- * :tabSize=8:indentSize=8:noTabs=false:
+ * :tabSize=4:indentSize=4:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
  * Copyright (C) 2003 Slava Pestov
