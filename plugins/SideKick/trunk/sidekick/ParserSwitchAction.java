59038db181c9eb7275e83db77e10f5ccf3deab19
Alan Ezust <1688991+ezust@users.noreply.github.com>
2012-06-18 22:33:02 +0000

    Tabsize=4.

--- plugins/SideKick/trunk/sidekick/ParserSwitchAction.java
+++ plugins/SideKick/trunk/sidekick/ParserSwitchAction.java
@@ -1,6 +1,6 @@
 /*
  * ParserSwitchAction.java - Action to switch parsers in SideKick.
- * :tabSize=8:indentSize=8:noTabs=false:
+ * :tabSize=4:indentSize=4:noTabs=false:
  * :folding=explicit:collapseFolds=1:
  *
  * Copyright (C) 2006 Alan Ezust
--- plugins/SideKick/trunk/sidekick/SideKickParser.java
+++ plugins/SideKick/trunk/sidekick/SideKickParser.java
@@ -1,6 +1,6 @@
 /*
 * SideKickParser.java
-* :tabSize=8:indentSize=8:noTabs=false:
+* :tabSize=4:indentSize=4:noTabs=false:
 * :folding=explicit:collapseFolds=1:
 *
 * Copyright 2003 Slava Pestov
