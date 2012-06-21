f0a620b576ef5d070716feb77a02c000d2e71e3f
Alan Ezust <1688991+ezust@users.noreply.github.com>
2012-06-21 23:26:39 +0000

    api docs

--- plugins/ErrorList/trunk/errorlist/ErrorGutterIcon.java
+++ plugins/ErrorList/trunk/errorlist/ErrorGutterIcon.java
@@ -1,3 +1,25 @@
+/*
+ * ErrorGutterIcon.java - Error list gutter icon
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2011 Shlomy Reinstein
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package errorlist;
 
 import java.awt.Graphics2D;
@@ -9,18 +31,22 @@ import org.gjt.sp.jedit.EditPane;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
 import org.gjt.sp.jedit.textarea.TextAreaExtension;
 
+/**
+  * Gutter Icon to indicate an error.
+  * @author Shlomy Reinstein
+  */
 public class ErrorGutterIcon extends TextAreaExtension {
 
 	private EditPane editPane;
 	private static final int FOLD_MARKER_SIZE = 12;
 		// Taken from Gutter... Unfortunately it is private there
-	
+
 	//{{{ ErrorGutterIcon constructor
 	public ErrorGutterIcon(EditPane editPane)
 	{
 		this.editPane = editPane;
 	} //}}}
-	
+
 	public String getToolTipText(int x, int y) {
 		ErrorSource[] errorSources = ErrorSource.getErrorSources();
 		if(!editPane.getBuffer().isLoaded())
@@ -55,7 +81,7 @@ public class ErrorGutterIcon extends TextAreaExtension {
                 {
                     errMsg.append("__");
                     errMsg.append(extra);
-                    errMsg.append("<br>");                    
+                    errMsg.append("<br>");
                 }
 			}
 			errMsg.append("</html>");
