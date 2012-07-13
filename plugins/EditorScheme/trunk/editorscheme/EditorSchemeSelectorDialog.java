16a2929c17881e8e9b1248ed33b4e3c74dfd372e
Dale <danson@grafidog.com>
2012-07-13 22:09:49 +0000

    Made dialog modal for use outside of this plugin, specifically for use by the Look and Feel plugin.

--- plugins/EditorScheme/trunk/editorscheme/EditorSchemeSelectorDialog.java
+++ plugins/EditorScheme/trunk/editorscheme/EditorSchemeSelectorDialog.java
@@ -60,7 +60,7 @@ public class EditorSchemeSelectorDialog extends EnhancedDialog implements Action
 	//{{{ constructor
 	public EditorSchemeSelectorDialog(View view)
 	{
-		super(view, jEdit.getProperty("editor-scheme.selector.title"), false);
+		super(view, jEdit.getProperty("editor-scheme.selector.title"), true);
 
 		JPanel schemesPanel = new JPanel(new BorderLayout());
 		JPanel main = new JPanel(new GridLayout(1,2,5,5));
