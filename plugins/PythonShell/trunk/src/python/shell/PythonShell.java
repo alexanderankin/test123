45312d26cf5ebbbe709e7a87934f49ddf5b01ad0
kog13 <kog13@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-07-24 16:01:54 +0000

    Added support for multiple interpreters and optional parameters for each one.

--- plugins/PythonShell/trunk/src/python/shell/PythonShell.java
+++ plugins/PythonShell/trunk/src/python/shell/PythonShell.java
@@ -1,18 +1,34 @@
+/*
+* PythonShell is a Console shell for hosting a Python REPL.
+* Copyright (c) 2012 Damien Radtke - www.damienradtke.org
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version
+* 2.0 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* For more information, visit http://www.gnu.org/copyleft
+*/
+
 package python.shell;
-/**
- * @author Damien Radtke
- * class PythonShell
- * Embeds an interactive Python session into the console
- */
+
 //{{{ Imports
 import console.Console;
 import console.Output;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import javax.swing.text.AttributeSet;
+
 import org.gjt.sp.jedit.Buffer;
 import org.gjt.sp.jedit.ServiceManager;
 import org.gjt.sp.jedit.View;
@@ -20,39 +36,52 @@ import org.gjt.sp.jedit.gui.DockableWindowManager;
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.jedit.textarea.TextArea;
 import org.gjt.sp.util.Log;
+
 import procshell.ProcessShell;
 //}}}
+
 public class PythonShell extends ProcessShell {
 	
 	private String prompt = ">>> ";
+	private String newline;
 	
 	/*
- 	 * Constructor for SlimeShell
+ 	 * Constructor
  	 */
 	public PythonShell() {
 		super("Python");
+		this.newline = System.getProperty("line.separator");
 	}
 	
-	//{{{ init()
 	/**
-	 * Start up Python
+	 * Start up Python.
 	 */
 	protected void init(ConsoleState state, String command) throws IOException {
-		String exec = jEdit.getProperty("options.python-shell.exec");
-		Log.log(Log.DEBUG,this,"Attempting to start Python process: "+exec);
-		ProcessBuilder pb = new ProcessBuilder(exec, "-i");
-		if (exec.contains("ipython")) {
-			pb = new ProcessBuilder(exec, "-noreadline");
+		Integer selected = jEdit.getIntegerProperty("python-shell.selected-interpreter");
+		String cmd = jEdit.getProperty("python-shell.interpreter." + String.valueOf(selected));
+		Log.log(Log.DEBUG,this,"Attempting to start Python process: "+cmd);
+
+		LinkedList<String> exec = new LinkedList<String>();
+		exec.add(cmd);
+		exec.add("-i"); // force python to run in interactive mode
+		if (cmd.endsWith("ipython") || cmd.endsWith("ipython.exe")) {
+			// if we know this is ipython, disable readline
+			exec.add("-noreadline");
 		}
-		pb.environment().put("TERM", "dumb");	
+
+		String[] params = jEdit.getProperty("python-shell.parameters." + String.valueOf(selected), "").split(" ");
+		for (int i = 0; i<params.length; i++) {
+			exec.add(params[i]);
+		}
+
+		ProcessBuilder pb = new ProcessBuilder(exec);
+		pb.environment().put("TERM", "dumb");
 		state.p = pb.start();
 		Log.log(Log.DEBUG,this,"Python started.");
 	}
-	//}}}
 	
-	//{{{ eval()
 	/**
-	 * Evaluate text
+	 * Evaluate text.
 	 */
 	public void eval(Console console, String str) {
 		str += "\n";
@@ -60,28 +89,41 @@ public class PythonShell extends ProcessShell {
 		str = str.replace("\t", "\\t");
 		str = str.replace("\"", "\\\"");
 		send(console, "exec(\""+str+"\")");
-	} //}}}
+	}
 	
-	//{{{ evalBuffer()
 	/**
-	 * Evaluate a buffer
+	 * Evaluate a buffer.
 	 */
 	public void evalBuffer(Console console, Buffer buffer) {
 		send(console, "execfile(\""+buffer.getPath().replace("\\", "/")+"\")");
-	} //}}}
+	}
 	
+	/**
+	 * Called when data is available.
+	 */
 	protected void onRead(ConsoleState state, String str, Output output) {
-		if (str.indexOf("\n") != -1) {
-			str = str.substring(str.lastIndexOf("\n")+1);
+		if (str.indexOf(newline) != -1) {
+			str = str.substring(str.lastIndexOf(newline)+1);
 		}
+
 		if (str.matches(prompt)) {
 			state.waiting = false;
 			output.commandDone();
 		}
 	}
 	
+	/**
+	 * Print python shell info message.
+	 */
 	public void printInfoMessage(Output output) {
 		output.print(null, jEdit.getProperty("msg.python-shell.info-message"));
 	}
-	
+
+	/**
+	 * Restart the current Python process.
+	 */
+	public void restart(Console console) {
+		this.stop(console);
+		this.start(console, console.getOutput(), null);
+	}
 }
--- plugins/PythonShell/trunk/src/python/shell/PythonShellPlugin.java
+++ plugins/PythonShell/trunk/src/python/shell/PythonShellPlugin.java
@@ -1,19 +1,45 @@
+/*
+* PythonShell is a Console shell for hosting a Python REPL.
+* Copyright (c) 2012 Damien Radtke - www.damienradtke.org
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version
+* 2.0 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* For more information, visit http://www.gnu.org/copyleft
+*/
+
 package python.shell;
-/**
- * @author Damien Radtke
- * class PythonReplPlugin
- * Main class for the PythonShell plugin
- */
+
 //{{{ Imports
 import console.Shell;
 import org.gjt.sp.jedit.EditPlugin;
 //}}}
+
 public class PythonShellPlugin extends EditPlugin {
-	public void start() {}
+	/**
+	 * Start the plugin.
+	 */
+	public void start() {
+
+	}
+
+	/**
+	 * Stop the plugin.
+	 */
 	public void stop() {
 		try {
 			PythonShell shell = (PythonShell) Shell.getShell("Python");
 			shell.stop();
-		} catch (Exception e) {}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
