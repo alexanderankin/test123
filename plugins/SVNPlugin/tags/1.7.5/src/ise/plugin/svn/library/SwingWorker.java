5e0835162223b80a634dfdcdc641ca7d967cb579
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-10-04 02:20:25 +0000

    deleting for replacement

--- plugins/SVNPlugin/tags/1.7.5/src/ise/plugin/svn/library/SwingWorker.java
+++
@@ -1,7 +0,0 @@
-package ise.plugin.svn.library;
-
-public abstract class SwingWorker<T, V> extends javax.swing.SwingWorker<T, V> {
-    public boolean doCancel(boolean mayInterruptIfRunning) {
-        return super.cancel(mayInterruptIfRunning);   
-    }
-}
