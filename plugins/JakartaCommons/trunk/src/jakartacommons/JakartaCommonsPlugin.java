ec3e6226a06546ffdcaa61a2650412820ae794bb
Alan Ezust <1688991+ezust@users.noreply.github.com>
2012-11-01 22:51:13 +0000

    Added a doc file.

--- plugins/JakartaCommons/trunk/src/jakartacommons/JakartaCommonsPlugin.java
+++ plugins/JakartaCommons/trunk/src/jakartacommons/JakartaCommonsPlugin.java
@@ -41,27 +41,31 @@ public class JakartaCommonsPlugin extends EditPlugin {
 	/** Selects word before caret if no selection exists.
 	    @return the selected text */
 	public static String selectedText(TextArea textArea, Buffer buffer) {
-		if (textArea.getSelectionCount() == 0)
+		if (textArea.getSelectionCount() == 0) {
+			int caretPos = textArea.getCaretPosition() -1;
+			if (caretPos > -1) textArea.setCaretPosition(caretPos);
 			textArea.selectWord();
+		}
 		selections = textArea.getSelection();
 		int len = selections[0].getEnd() - selections[0].getStart();
 		return buffer.getText(selections[0].getStart(), len);
 	}
 	/** Unescape unicode characters that are selected in current TextArea
 	    (or word before caret) using Java conventions.
-	    If the word is just a 2-5 digit hexadecimal number, it implicitly places a
-	    \\u before the word before doing the unescaping.
+	    If nothing was selected, and the word is just a 2-5 digit hexadecimal number, 
+	    it implicitly strips the 0x prefix (if there is one) and then 
+	    places a \\u before the word before doing the unescaping.
 	    @author Alan Ezust
 	*/
 	public static void unescapeUnicodeSelection(TextArea textArea, Buffer buffer) {
 		String text = selectedText(textArea, buffer);
 		if (text == null) return;
 		// Pattern for a 5-digit hexadecimal number
-		final Pattern p = Pattern.compile("[0-9a-fA-F]{2,5}");
+		final Pattern p = Pattern.compile("(?:0x)?([0-9a-fA-F]{2,})");
 		Matcher m = p.matcher(text);
 		String esctext;
 		if (m.matches())
-			esctext = StringEscapeUtils.unescapeJava("\\u" + text);
+			esctext = StringEscapeUtils.unescapeJava("\\u" + m.group(1));
 		else esctext = StringEscapeUtils.unescapeJava(text);
 		buffer.remove(selections[0].getStart(), text.length());
 		buffer.insert(selections[0].getStart(), esctext);
