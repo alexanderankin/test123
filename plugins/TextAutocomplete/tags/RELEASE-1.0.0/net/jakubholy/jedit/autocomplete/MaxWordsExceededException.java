e26c7889204596c56e3c0fd4014bef3ffa9c5cdb
malyvelky <malyvelky@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-03-30 14:12:23 +0000

    Tag for release 1.0.0

---
+++ plugins/TextAutocomplete/tags/RELEASE-1.0.0/net/jakubholy/jedit/autocomplete/MaxWordsExceededException.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package net.jakubholy.jedit.autocomplete;
+
+/**
+ * Thrown when we try to remember another word (completion) but the number of
+ * remembered words would exceed the maximum allowed (set in preferences).
+ * @author Jakub Holy
+ */
+@SuppressWarnings("serial")
+public class MaxWordsExceededException extends Exception 
+{
+	/** The word that we wanted to remember but couldn't because of the limit. */
+	String lastWord = null;
+
+	/**
+	 * 
+	 */
+	public MaxWordsExceededException() 
+	{ super(); }
+
+	/**
+	 * @param lastWord The word that couldn't be added because 
+	 * the limit would be exceeded.
+	 */
+	public MaxWordsExceededException(String lastWord) 
+	{ setLastWord(lastWord); }
+
+	public MaxWordsExceededException(int maxCountOfWords) 
+	{ this( String.valueOf(maxCountOfWords) ); }
+
+	/** Return the word that we wanted to remember but couldn't because of the limit. */
+	public String getLastWord() {
+		return lastWord;
+	}
+
+	/** Set the word that we wanted to remember but couldn't because of the limit. */
+	public void setLastWord(String lastWord) {
+		this.lastWord = lastWord;
+	}
+
+}
