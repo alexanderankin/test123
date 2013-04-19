f6206684ce9415d26519426a386a9879b2710ba6
Matthieu Casanova <chocolat.mou@gmail.com>
2013-04-19 14:22:45 +0000

    use of lucene 4.2.1

--- plugins/LucenePlugin/trunk/src/gatchan/jedit/lucene/TemporaryIndex.java
+++ plugins/LucenePlugin/trunk/src/gatchan/jedit/lucene/TemporaryIndex.java
@@ -62,11 +62,11 @@ public class TemporaryIndex implements Index
 	public TemporaryIndex(String name)
 	{
 		this.name = name;
-		analyzer = new StandardAnalyzer(Version.LUCENE_41);
+		analyzer = new StandardAnalyzer(Version.LUCENE_42);
 		directory = new RAMDirectory();
 		try
 		{
-			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_41, analyzer);
+			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_42, analyzer);
 			writer = new IndexWriter(directory, indexWriterConfig);
 		}
 		catch (IOException e)
@@ -93,7 +93,7 @@ public class TemporaryIndex implements Index
 		directory = new RAMDirectory();
 		try
 		{
-			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_41, analyzer);
+			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_42, analyzer);
 			writer = new IndexWriter(directory, indexWriterConfig);
 		}
 		catch (IOException e)
@@ -202,7 +202,7 @@ public class TemporaryIndex implements Index
 		if (searcher == null)
 			return;
 		QueryParser parser =
-			new MultiFieldQueryParser(Version.LUCENE_41, new String[] { "path", "content" }, getAnalyzer());
+			new MultiFieldQueryParser(Version.LUCENE_42, new String[] { "path", "content" }, getAnalyzer());
 		try
 		{
 			Query parsedQuery = parser.parse(query);
