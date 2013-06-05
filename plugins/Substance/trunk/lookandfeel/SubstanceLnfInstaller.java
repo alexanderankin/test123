cb13fac203153335e33b5decafe2b8819e29ff21
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-06-05 17:33:05 +0000

    Updated to Substance 7.3, added menu search feature as an option.

--- plugins/Substance/trunk/lookandfeel/SubstanceLnfInstaller.java
+++ plugins/Substance/trunk/lookandfeel/SubstanceLnfInstaller.java
@@ -6,8 +6,6 @@ import org.gjt.sp.jedit.jEdit;
 
 public class SubstanceLnfInstaller implements LookAndFeelInstaller {
 
-    public final static String SUBSTANCE_THEME_PROP = "substance.theme";
-
     private static final String[] themeNames = {
                 "Autumn",
                 "Business",
@@ -47,15 +45,15 @@ public class SubstanceLnfInstaller implements LookAndFeelInstaller {
 	}
 	
     public void install() throws UnsupportedLookAndFeelException {
-        String theme = jEdit.getProperty( SUBSTANCE_THEME_PROP );
+        String theme = jEdit.getProperty( SubstanceLookAndFeelPlugin.SUBSTANCE_THEME_PROP );
         if ( theme == null ) {
             theme = "Business";
         }
-
         try {
             Class c = Class.forName( "org.pushingpixels.substance.api.skin.Substance" + theme + "LookAndFeel" );
             UIManager.setLookAndFeel( (javax.swing.LookAndFeel)c.newInstance() );
             UIManager.put( "ClassLoader", c.getClassLoader() );
+            SubstanceLookAndFeelPlugin.initMenuSearch();
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -75,6 +73,7 @@ public class SubstanceLnfInstaller implements LookAndFeelInstaller {
      */
     class OptionComponent extends AbstractOptionPane {
         JComboBox theme_choices;
+        JCheckBox showMenuSearch;
 
         /**
          * Create a new <code>OptionComponent</code>.
@@ -90,13 +89,17 @@ public class SubstanceLnfInstaller implements LookAndFeelInstaller {
         public void _init() {
             theme_choices = new JComboBox( themeNames );
             theme_choices.setEditable( false );
-            addComponent( jEdit.getProperty( SUBSTANCE_THEME_PROP + ".label", "Theme" ), theme_choices );
+            addComponent( jEdit.getProperty( SubstanceLookAndFeelPlugin.SUBSTANCE_THEME_PROP + ".label", "Theme" ), theme_choices );
 
-            String theme = jEdit.getProperty( SUBSTANCE_THEME_PROP );
+            String theme = jEdit.getProperty( SubstanceLookAndFeelPlugin.SUBSTANCE_THEME_PROP );
             if ( theme == null ) {
                 theme = "Business";
             }
             theme_choices.setSelectedItem( theme );
+            
+            showMenuSearch = new JCheckBox("Show menu search");
+            showMenuSearch.setSelected(jEdit.getBooleanProperty(SubstanceLookAndFeelPlugin.SUBSTANCE_MENU_SEARCH));
+            addComponent(showMenuSearch);
         }
 
         /**
@@ -105,16 +108,16 @@ public class SubstanceLnfInstaller implements LookAndFeelInstaller {
         public void _save() {
             String theme_setting = ( String ) theme_choices.getSelectedItem();
             if ( theme_setting == null ) {
-                jEdit.unsetProperty( SUBSTANCE_THEME_PROP );
+                jEdit.unsetProperty( SubstanceLookAndFeelPlugin.SUBSTANCE_THEME_PROP );
             }
             else {
-                jEdit.setProperty( SUBSTANCE_THEME_PROP, theme_setting );
+                jEdit.setProperty( SubstanceLookAndFeelPlugin.SUBSTANCE_THEME_PROP, theme_setting );
             }
+            jEdit.setBooleanProperty(SubstanceLookAndFeelPlugin.SUBSTANCE_MENU_SEARCH, showMenuSearch.isSelected());
             try {
                 SubstanceLnfInstaller.this.install();
             }
-            catch ( Exception e ) {
-                e.printStackTrace();
+            catch ( Exception ignored ) {       // NOPMD
             }
         }
     }
