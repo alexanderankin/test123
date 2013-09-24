d75187105c119921a478b53b152ca63b9f99ec68
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-09-24 20:47:50 +0000

    Make x11 forwarding an option.

---
+++ plugins/SshConsole/trunk/console/ssh/OptionPane.java
@@ -0,0 +1,28 @@
+package console.ssh;
+
+
+import javax.swing.JCheckBox;
+
+import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
+
+public class OptionPane extends AbstractOptionPane {
+	JCheckBox xforward;
+	
+	
+	public OptionPane() {
+		super("sshconsole");
+	}
+	
+	
+	protected void _init() {
+		xforward = new JCheckBox(jEdit.getProperty("options.sshconsole.xforward"));
+		xforward.setSelected(jEdit.getBooleanProperty("sshconsole.xforward"));
+		addComponent(xforward);
+	}
+	
+	protected void _save() {
+		jEdit.setBooleanProperty("sshconsole.xforward", xforward.isSelected());
+	}
+	
+}
