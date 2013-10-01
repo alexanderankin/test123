304ba6cd6bf2a9fedc91fb7c58dace3096acf7e8
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2013-10-01 02:15:38 +0000

    Tagging 1.7.1 for release.

---
+++ plugins/CommonControls/tags/CommonControls-1.7.1/common/gui/blame/BasicBlamePaneUI.java
@@ -0,0 +1,188 @@
+/*
+* Copyright (c) 2008, Dale Anson
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+package common.gui.blame;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.plaf.ComponentUI;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
+
+/**
+ * UI for the BlamePane to be displayed left of the right scroll bar of the
+ * main text area.
+ */
+public class BasicBlamePaneUI extends BlamePaneUI implements ChangeListener, MouseListener, MouseMotionListener {
+
+    private BlamePane blamePane;
+    private BlameRendererPane blameRendererPane;
+    private int pixelsPerLine = 12;     // default, this will be calculated later
+
+    public static ComponentUI createUI( JComponent c ) {
+        return new BasicBlamePaneUI();
+    }
+
+    public void installUI( JComponent c ) {
+        blamePane = ( BlamePane ) c;
+        blamePane.setLayout( createLayoutManager() );
+        installDefaults();
+        installComponents();
+        installListeners();
+
+    }
+
+    public void uninstallUI( JComponent c ) {
+        c.setLayout( null );
+        uninstallListeners();
+        uninstallComponents();
+        uninstallDefaults();
+    }
+
+    public void installDefaults() {}
+
+    public void installComponents() {
+        blameRendererPane = new BlameRendererPane();
+        blamePane.add( blameRendererPane, BorderLayout.CENTER );
+    }
+
+    public void installListeners() {
+        blamePane.addChangeListener( this );
+        blamePane.addMouseListener( this );
+        blamePane.addMouseMotionListener( this );
+    }
+
+    public void uninstallDefaults() {}
+
+    public void uninstallComponents() {
+        blamePane.remove( blameRendererPane );
+    }
+
+    public void uninstallListeners() {
+        blamePane.removeChangeListener( this );
+    }
+
+    protected LayoutManager createLayoutManager() {
+        return new BorderLayout();
+    }
+
+    public void stateChanged( ChangeEvent event ) {
+        blameRendererPane.repaint();
+    }
+
+    /**
+     * On mouse click on the BlamePane, move the cursor in the text area to the
+     * corresponding line.  This makes line highlight (if turned on) line up
+     * nicely with the appropriate line in the BlamePane.
+     */
+    public void mouseClicked( MouseEvent e ) {
+        BlameModel model = blamePane.getModel();
+        if ( model == null ) {
+            return ;
+        }
+        int screenLine = e.getY() / pixelsPerLine;
+        JEditTextArea textArea = model.getTextArea();
+        textArea.setCaretPosition( textArea.getScreenLineStartOffset( screenLine ), false );
+    }
+
+    public void mouseEntered( MouseEvent e ) {}
+    public void mouseExited( MouseEvent e ) {}
+    public void mousePressed( MouseEvent e ) {}
+    public void mouseReleased( MouseEvent e ) {}
+    public void mouseDragged( MouseEvent e ) {}
+    
+    public void mouseMoved( MouseEvent e ) {
+        BlameModel model = blamePane.getModel();
+        if ( model == null ) {
+            return ;
+        }
+        int screenLine = e.getY() / pixelsPerLine;
+        int index = model.getTextArea().getPhysicalLineOfScreenLine(screenLine);
+        String tooltip = model.getToolTipText(index);
+        if (tooltip != null) {
+            blamePane.setToolTipText(tooltip);   
+        }
+    }
+
+    public class BlameRendererPane extends JPanel {
+        public BlameRendererPane( ) {
+            // need a model and some blame
+            BlameModel model = blamePane.getModel();
+            if ( model == null ) {
+                throw new IllegalArgumentException( "blame model is null" );
+            }
+            if ( model.getBlame() == null ) {
+                throw new IllegalArgumentException( "no blame found in model" );
+            }
+
+            // calculate the proper width by finding the widest line of the blame
+            int max_width = 0;
+            FontMetrics fm = getFontMetrics(getFont());
+            Graphics g = model.getTextArea().getPainter().getGraphics();
+            for ( String line : model.getBlame() ) {
+                int width = (int)fm.getStringBounds( line, g ).getWidth();
+                max_width = width > max_width ? width : max_width;
+            }
+            Dimension dim = getPreferredSize();
+            dim.width = max_width + 3;  // 3 extra pixels just to give some separation from the text area
+            setPreferredSize( dim );
+        }
+
+        public void paintComponent( Graphics gfx ) {
+            super.paintComponent( gfx );
+            BlameModel model = blamePane.getModel();
+
+            // paint the background
+            Rectangle size = getBounds();
+            gfx.setColor( getBackground() );
+            gfx.fillRect( 0, 0, size.width, size.height );
+            gfx.setFont(getFont());
+            if ( !"none".equals( jEdit.getProperty( "view.antiAlias" ) ) ) {
+                ( ( java.awt.Graphics2D ) gfx ).setRenderingHint(
+                    java.awt.RenderingHints.KEY_TEXT_ANTIALIASING,
+                    java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+                );
+            }
+
+            // get the visible lines, draw the corresponding blame lines
+            JEditTextArea textArea = model.getTextArea();
+            pixelsPerLine = textArea.getPainter().getLineHeight();
+            int caretLine = textArea.getCaretLine();
+            Color foreground = jEdit.getColorProperty( "view.fgColor", Color.BLACK );
+            Color highlight = jEdit.getColorProperty( "view.lineHighlightColor", Color.WHITE );
+            gfx.setColor( foreground );
+            java.util.List<String> blame = model.getBlame();
+            int descent = gfx.getFontMetrics().getDescent();
+            for ( int screenLine = 0; screenLine <= textArea.getLastScreenLine(); screenLine++ ) {
+                int physicalLine = textArea.getPhysicalLineOfScreenLine(screenLine);
+                if ( screenLine == caretLine ) {
+                    gfx.setColor( highlight );
+                    gfx.fillRect( 0, ( screenLine ) * pixelsPerLine, size.width, pixelsPerLine );
+                    gfx.setColor( foreground );
+                }
+                if ( physicalLine >= 0 && physicalLine < blame.size() ) {
+                    gfx.drawString( blame.get( physicalLine ), 3, ( (screenLine + 1) * pixelsPerLine ) - descent );
+                }
+            }
+        }
+    }
+}
