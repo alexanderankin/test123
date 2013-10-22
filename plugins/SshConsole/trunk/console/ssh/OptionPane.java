5db37cd6d872d5df9efa73b2698f72c450ddc0b1
Alan Ezust <1688991+ezust@users.noreply.github.com>
2013-10-22 23:46:13 +0000

    changelog.

--- plugins/SshConsole/trunk/console/ssh/OptionPane.java
+++ plugins/SshConsole/trunk/console/ssh/OptionPane.java
@@ -6,6 +6,7 @@ import javax.swing.JCheckBox;
 import org.gjt.sp.jedit.AbstractOptionPane;
 import org.gjt.sp.jedit.jEdit;
 
+@SuppressWarnings("serial")
 public class OptionPane extends AbstractOptionPane {
 	JCheckBox xforward;
 	
