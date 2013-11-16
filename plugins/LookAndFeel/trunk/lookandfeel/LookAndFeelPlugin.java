977a147d115e02cdb0735ce6bed71bd060d26681
Dale <danson@grafidog.com>
2013-11-16 20:02:35 +0000

    Update to handle initPlaf screwing around with the look and feel. InitPlaf gets called on EVERY SINGLE property change in jEdit.

--- plugins/LookAndFeel/trunk/lookandfeel/LookAndFeelPlugin.java
+++ plugins/LookAndFeel/trunk/lookandfeel/LookAndFeelPlugin.java
@@ -40,115 +40,127 @@ import org.gjt.sp.util.Log;
 public class LookAndFeelPlugin extends EBPlugin {
 
     public static boolean loadedInitialLnF = false;
+    private static String lnfClassname = null;
 
     public void start() {
-        jEdit.unsetProperty("lookAndFeel");
         try {
-            String lnf = jEdit.getProperty("lookandfeel.lookandfeel");
-            if (LookAndFeelPlugin.isEmpty(lnf)) {
+            String lnf = jEdit.getProperty( "lookandfeel.lookandfeel" );
+            if ( LookAndFeelPlugin.isEmpty( lnf ) ) {
                 return;
             }
 
-            LookAndFeelInstaller installer = getInstaller(lnf);
-            if (installer == null) {
+            LookAndFeelInstaller installer = getInstaller( lnf );
+            if ( installer == null ) {
                 loadedInitialLnF = false;
                 return;
             }
-            installLookAndFeel(installer);
+            installLookAndFeel( installer );
             loadedInitialLnF = true;
 
-        } catch (Exception e) {
-            Log.log(Log.ERROR, this, e);
+        } catch ( Exception e ) {
+            Log.log( Log.ERROR, this, e );
         }
-    }
-    
-    public void stop() {
-    	jEdit.setProperty("lookAndFeel", UIManager.getLookAndFeel().getClass().getName());	
     }
 
-    public void handleMessage(EBMessage msg) {
-        if (loadedInitialLnF) {
-            return;
+    public void stop() {
+        if ( lnfClassname != null ) {
+            jEdit.setProperty( "lookAndFeel", lnfClassname );
         }
-        if (msg instanceof PluginUpdate) {
-            PluginUpdate pu = (PluginUpdate) msg;
-            if (PluginUpdate.LOADED.equals(pu.getWhat())) {
+    }
+
+    public void handleMessage( EBMessage msg ) {
+        if ( loadedInitialLnF && isStillLoaded() ) {
+            LookAndFeelInstaller installer = getInstaller( lnfClassname );
+            if ( installer == null ) {
+                return;
+            }
+            installLookAndFeel( installer );
+        }
+        if ( msg instanceof PluginUpdate ) {
+            PluginUpdate pu = ( PluginUpdate ) msg;
+            if ( PluginUpdate.LOADED.equals( pu.getWhat() ) ) {
                 start();
             }
         }
     }
 
+    private boolean isStillLoaded() {
+        String installed = UIManager.getLookAndFeel().getClass().getName();
+        return installed == lnfClassname;
+    }
+
     /**
      * Install a look and feel based on the given installer.
      */
-    public static void installLookAndFeel(final LookAndFeelInstaller installer) {
-        if (installer == null) {
+    public static void installLookAndFeel( final LookAndFeelInstaller installer ) {
+        if ( installer == null ) {
             return;
         }
-        SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater( new Runnable() {
             public void run() {
                 try {
-                    jEdit.unsetProperty("lookAndFeel");
+                    // jEdit.unsetProperty("lookAndFeel");
                     installer.install();
                     UIDefaults uid = UIManager.getDefaults();
 
                     // this is a workaround for the SkinLF; it doesn't find the
                     // the UI classes for overridden components
-                    uid.put(org.gjt.sp.jedit.menu.EnhancedMenu.class, new javax.swing.JMenu().getUI());
-                    uid.put(org.gjt.sp.jedit.menu.EnhancedMenuItem.class, new javax.swing.JMenuItem().getUI());
-                    uid.put(org.gjt.sp.jedit.menu.EnhancedCheckBoxMenuItem.class, new javax.swing.JCheckBoxMenuItem().getUI());
+                    uid.put( org.gjt.sp.jedit.menu.EnhancedMenu.class, new javax.swing.JMenu().getUI() );
+                    uid.put( org.gjt.sp.jedit.menu.EnhancedMenuItem.class, new javax.swing.JMenuItem().getUI() );
+                    uid.put( org.gjt.sp.jedit.menu.EnhancedCheckBoxMenuItem.class, new javax.swing.JCheckBoxMenuItem().getUI() );
 
-                    if (jEdit.getBooleanProperty("lookandfeel.usejeditfont", false)) {
-                        Font primaryFont = jEdit.getFontProperty("metal.primary.font");
-                        Font secondaryFont = jEdit.getFontProperty("metal.secondary.font");
+                    if ( jEdit.getBooleanProperty( "lookandfeel.usejeditfont", false ) ) {
+                        Font primaryFont = jEdit.getFontProperty( "metal.primary.font" );
+                        Font secondaryFont = jEdit.getFontProperty( "metal.secondary.font" );
 
-                        if (primaryFont != null && secondaryFont != null) {
-                            uid.put("Button.font", primaryFont);
-                            uid.put("CheckBox.font", primaryFont);
-                            uid.put("Menu.font", primaryFont);
-                            uid.put("MenuBar.font", primaryFont);
-                            uid.put("MenuItem.font", primaryFont);
-                            uid.put("Label.font", primaryFont);
-                            uid.put("CheckBoxMenuItem.font", primaryFont);
-                            uid.put("RadioButtonMenuItem.font", primaryFont);
-                            uid.put("ComboBox.font", primaryFont);
-                            uid.put("InternalFrame.font", primaryFont);
-                            uid.put("PopupMenu.font", primaryFont);
-                            uid.put("RadioButton.font", primaryFont);
-                            uid.put("TabbedPane.font", primaryFont);
-                            uid.put("TableHeader.font", primaryFont);
-                            uid.put("ToggleButton.font", primaryFont);
-                            uid.put("InternalFrame.titleFont", primaryFont);
+                        if ( primaryFont != null && secondaryFont != null ) {
+                            uid.put( "Button.font", primaryFont );
+                            uid.put( "CheckBox.font", primaryFont );
+                            uid.put( "Menu.font", primaryFont );
+                            uid.put( "MenuBar.font", primaryFont );
+                            uid.put( "MenuItem.font", primaryFont );
+                            uid.put( "Label.font", primaryFont );
+                            uid.put( "CheckBoxMenuItem.font", primaryFont );
+                            uid.put( "RadioButtonMenuItem.font", primaryFont );
+                            uid.put( "ComboBox.font", primaryFont );
+                            uid.put( "InternalFrame.font", primaryFont );
+                            uid.put( "PopupMenu.font", primaryFont );
+                            uid.put( "RadioButton.font", primaryFont );
+                            uid.put( "TabbedPane.font", primaryFont );
+                            uid.put( "TableHeader.font", primaryFont );
+                            uid.put( "ToggleButton.font", primaryFont );
+                            uid.put( "InternalFrame.titleFont", primaryFont );
 
-                            uid.put("ColorChooser.font", secondaryFont);
-                            uid.put("DesktopIcon.font", secondaryFont);
-                            uid.put("List.font", secondaryFont);
-                            uid.put("OptionPane.font", secondaryFont);
-                            uid.put("Panel.font", secondaryFont);
-                            uid.put("PasswordField.font", secondaryFont);
-                            uid.put("ProgressBar.font", secondaryFont);
-                            uid.put("ScrollPane.font", secondaryFont);
-                            uid.put("Table.font", secondaryFont);
-                            uid.put("TextArea.font", secondaryFont);
-                            uid.put("TextField.font", secondaryFont);
-                            uid.put("TextPane.font", secondaryFont);
-                            uid.put("TitledBorder.font", secondaryFont);
-                            uid.put("ToolBar.font", secondaryFont);
-                            uid.put("ToolTip.font", secondaryFont);
-                            uid.put("Tree.font", secondaryFont);
-                            uid.put("Viewport.font", secondaryFont);
+                            uid.put( "ColorChooser.font", secondaryFont );
+                            uid.put( "DesktopIcon.font", secondaryFont );
+                            uid.put( "List.font", secondaryFont );
+                            uid.put( "OptionPane.font", secondaryFont );
+                            uid.put( "Panel.font", secondaryFont );
+                            uid.put( "PasswordField.font", secondaryFont );
+                            uid.put( "ProgressBar.font", secondaryFont );
+                            uid.put( "ScrollPane.font", secondaryFont );
+                            uid.put( "Table.font", secondaryFont );
+                            uid.put( "TextArea.font", secondaryFont );
+                            uid.put( "TextField.font", secondaryFont );
+                            uid.put( "TextPane.font", secondaryFont );
+                            uid.put( "TitledBorder.font", secondaryFont );
+                            uid.put( "ToolBar.font", secondaryFont );
+                            uid.put( "ToolTip.font", secondaryFont );
+                            uid.put( "Tree.font", secondaryFont );
+                            uid.put( "Viewport.font", secondaryFont );
                         }
                     }
 
                     updateAllComponentTreeUIs();
-                    
-                    jEdit.setProperty("lookAndFeel", UIManager.getLookAndFeel().getClass().getName());
-                } catch (Exception e) {
-                    Log.log(Log.ERROR, LookAndFeelPlugin.class, e);
+
+                    lnfClassname = UIManager.getLookAndFeel().getClass().getName();
+                    jEdit.setProperty( "lookAndFeel", lnfClassname );
+                } catch ( Exception e ) {
+                    Log.log( Log.ERROR, LookAndFeelPlugin.class, e );
                 }
             }
         }
-       );
+        );
     }
 
     /**
@@ -156,10 +168,10 @@ public class LookAndFeelPlugin extends EBPlugin {
      */
     private static void updateAllComponentTreeUIs() {
         Frame[] frames = Frame.getFrames();
-        for (Frame frame : frames) {
-            if (frame != null) {
-                SwingUtilities.updateComponentTreeUI(frame);
-                updateAllDialogComponentTreeUIs(frame);
+        for ( Frame frame : frames ) {
+            if ( frame != null ) {
+                SwingUtilities.updateComponentTreeUI( frame );
+                updateAllDialogComponentTreeUIs( frame );
             }
         }
     }
@@ -169,14 +181,14 @@ public class LookAndFeelPlugin extends EBPlugin {
      * <s>This method ignores frames because all frames should be handled from
      * {@link updateAllComponentTreeUIs()}.</s>
      */
-    private static void updateAllDialogComponentTreeUIs(Window win) {
+    private static void updateAllDialogComponentTreeUIs( Window win ) {
         Window[] children = win.getOwnedWindows();
-        for (int i = 0; i < children.length; i++) {
-            if (win instanceof Frame) {
+        for ( int i = 0; i < children.length; i++ ) {
+            if ( win instanceof Frame ) {
                 continue;
             }
-            SwingUtilities.updateComponentTreeUI(win);
-            updateAllDialogComponentTreeUIs(win);
+            SwingUtilities.updateComponentTreeUI( win );
+            updateAllDialogComponentTreeUIs( win );
         }
     }
 
@@ -184,16 +196,16 @@ public class LookAndFeelPlugin extends EBPlugin {
      * Returns the list of the possible look and feel options.
      */
     public static String[] getAvailableLookAndFeels() {
-        String[] names = ServiceManager.getServiceNames(LookAndFeelInstaller.SERVICE_NAME);
-        Arrays.sort(names, new Comparator<String>() {
-            public int compare(String a, String b) {
-                if ("None".equals(a)) {
+        String[] names = ServiceManager.getServiceNames( LookAndFeelInstaller.SERVICE_NAME );
+        Arrays.sort( names, new Comparator<String>() {
+            public int compare( String a, String b ) {
+                if ( "None".equals( a ) ) {
                     return -1;
                 }
-                if ("None".equals(b)) {
+                if ( "None".equals( b ) ) {
                     return 1;
                 }
-                return a.compareTo(b);
+                return a.compareTo( b );
             }
         } );
         return names;
@@ -202,20 +214,20 @@ public class LookAndFeelPlugin extends EBPlugin {
     /**
      * Returns the installer for the named look and feel.
      */
-    public static LookAndFeelInstaller getInstaller(String name) {
-        return (LookAndFeelInstaller) ServiceManager.getService(LookAndFeelInstaller.SERVICE_NAME, name);
+    public static LookAndFeelInstaller getInstaller( String name ) {
+        return ( LookAndFeelInstaller ) ServiceManager.getService( LookAndFeelInstaller.SERVICE_NAME, name );
     }
 
     /**
      * Returns <code>true</code> if the given string is <code>null</code>
      * or empty or contains only whitespace.
      */
-    protected static boolean isEmpty(String s) {
-        if (s == null || s.length() == 0) {
+    protected static boolean isEmpty( String s ) {
+        if ( s == null || s.length() == 0 ) {
             return true;
         }
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isWhitespace(s.charAt(i))) {
+        for ( int i = 0; i < s.length(); i++ ) {
+            if ( !Character.isWhitespace( s.charAt( i ) ) ) {
                 return false;
             }
         }
