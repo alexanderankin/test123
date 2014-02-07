272720c1263c41a200359dc738ca802a074a3f89
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-02-07 22:54:25 +0000

    bring back clear directory cache action.

--- plugins/FTP/trunk/ftp/OptionPane.java
+++ plugins/FTP/trunk/ftp/OptionPane.java
@@ -3,7 +3,7 @@
  * :tabSize=4:indentSize=4:noTabs=false:
  * :folding=sidekick:collapseFolds=1:
  *
- * Copyright © 2013 Alan Ezust
+ * Copyright © 2013-2014 Alan Ezust
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,14 +24,10 @@ package ftp;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-
 import org.gjt.sp.jedit.AbstractOptionPane;
-
 import common.gui.FileTextField;
-
 import org.gjt.sp.jedit.jEdit;
 
 
@@ -39,61 +35,59 @@ import org.gjt.sp.jedit.jEdit;
 */
 @SuppressWarnings("serial")
 public class OptionPane extends AbstractOptionPane implements ActionListener {
-	
-	JCheckBox storePasswords;	
+
+	JCheckBox storePasswords;
 	JCheckBox useKeyFile;
 	FileTextField keyFile;
 	JCheckBox enableCompression;
 	JCheckBox disableWeakCrypto;
-	JComboBox<String> sftpMaxAuthTries; 
-	
+	JComboBox<String> sftpMaxAuthTries;
+
 	public OptionPane() {
 		super("ftp");
 	}
-	
-	
-	protected void _init() {						
+
+
+	protected void _init() {
 		storePasswords = new JCheckBox(jEdit.getProperty("options.ftp.savePasswords"), jEdit.getBooleanProperty("vfs.ftp.storePassword"));
 		storePasswords.addActionListener(this);
 		addComponent(storePasswords);
-		
+
 		useKeyFile = new JCheckBox(jEdit.getProperty("options.ftp.useKeyFile"), jEdit.getBooleanProperty("ftp.useKeyFile"));
 		useKeyFile.setToolTipText(jEdit.getProperty("options.ftp.useKeyFile.tooltip"));
 		useKeyFile.addActionListener(this);
-				
+
 		keyFile = new FileTextField(jEdit.getProperty("ftp.passKeyFile"), false);
 		keyFile.setToolTipText(jEdit.getProperty("options.ftp.useKeyFile.tooltip"));
 		addComponent(useKeyFile , keyFile);
-		
+
 		disableWeakCrypto = new JCheckBox(jEdit.getProperty("options.ftp.disableWeakCrypto"), jEdit.getBooleanProperty("ftp.disableWeakCrypto"));
 		addComponent(disableWeakCrypto);
-		
+
 		actionPerformed(null);
-		
+
 		enableCompression = new JCheckBox(jEdit.getProperty("options.sftp.enableCompression"),
 				jEdit.getBooleanProperty("vfs.sftp.compression"));
 		addComponent(enableCompression);
-		
-		String[] retriesValues = new String[] {"1", "2", "3", "4", "5", "6" };
-		sftpMaxAuthTries = new JComboBox<String>(retriesValues);
+
+		sftpMaxAuthTries = new JComboBox<String>(new String[] {"1", "2", "3", "4", "5", "6" });
 		sftpMaxAuthTries.setSelectedItem(jEdit.getProperty("vfs.sftp.MaxAuthTries"));
-		
 		addComponent(jEdit.getProperty("options.sftp.MaxAuthTries"), sftpMaxAuthTries);
-		
-		
+
+
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		useKeyFile.setEnabled(storePasswords.isSelected());
 		keyFile.setEnabled(useKeyFile.isSelected() && storePasswords.isSelected());
 		disableWeakCrypto.setEnabled(storePasswords.isSelected());
 	}
-	
-	protected void _save() {		
+
+	protected void _save() {
 
 		jEdit.setBooleanProperty("ftp.useKeyFile", useKeyFile.isSelected());
 		jEdit.setBooleanProperty("ftp.disableWeakCrypto", disableWeakCrypto.isSelected());
-		
+
 		if (useKeyFile.isSelected()) {
 			jEdit.setProperty("ftp.passKeyFile", keyFile.getTextField().getText());
 		}
