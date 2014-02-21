8e95cc6fb938b76a39ba018a4cff76acc7b0becc
Eric Le Lay <222469+elelay@users.noreply.github.com>
2014-02-21 08:34:35 +0000

    update to Allura json exports

---
+++ www/trunk/xml_export/jquery.dataTables.htmlnum.js
@@ -0,0 +1,18 @@
+/* 
+ * from http://datatables.net/plug-ins/sorting#numbers_html
+ * Author: Allan Jardine
+ */
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "num-html-pre": function ( a ) {
+        var x = String(a).replace( /<[\s\S]*?>/g, "" );
+        return parseFloat( x );
+    },
+ 
+    "num-html-asc": function ( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+ 
+    "num-html-desc": function ( a, b ) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
