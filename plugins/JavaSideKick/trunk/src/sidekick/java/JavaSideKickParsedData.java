817541216061e8e0ed5049ec2a85fb54ef341baa
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-04-08 20:05:22 +0000

    Added java fold handler so imports are handled correctly.

--- plugins/JavaSideKick/trunk/src/sidekick/java/JavaSideKickParsedData.java
+++ plugins/JavaSideKick/trunk/src/sidekick/java/JavaSideKickParsedData.java
@@ -15,16 +15,18 @@
 */
 package sidekick.java;
 
-//{{{ Imports
+import javax.swing.text.Segment;
 import javax.swing.tree.*;
 import java.util.*;
-
 import org.gjt.sp.util.Log;
 
 import sidekick.*;
+import sidekick.util.Range;
+import sidekick.java.node.CUNode;
 import sidekick.java.node.TigerNode;
-//}}}
 
+import org.gjt.sp.jedit.buffer.FoldHandler;
+import org.gjt.sp.jedit.buffer.JEditBuffer;
 
 /**
  * Stores a buffer structure tree.
@@ -36,6 +38,8 @@ import sidekick.java.node.TigerNode;
  */
 public class JavaSideKickParsedData extends SideKickParsedData {
 
+    private FoldHandler foldHandler = null;
+
     /**
      * @param fileName The file name being parsed, used as the root of the
      * tree.
@@ -43,7 +47,6 @@ public class JavaSideKickParsedData extends SideKickParsedData {
     public JavaSideKickParsedData( String fileName ) {
         super( fileName );
     }
-
 
     /**
      * Overridden to search TigerNodes rather than TreeNodes.  Not all tree nodes
@@ -53,7 +56,7 @@ public class JavaSideKickParsedData extends SideKickParsedData {
      */
     public IAsset getAssetAtOffset( int pos ) {
         Object userObject = ( ( DefaultMutableTreeNode ) root ).getUserObject();
-        if ( !( userObject instanceof TigerNode ) ) {
+        if ( ! ( userObject instanceof TigerNode ) ) {
             return null;
         }
 
@@ -61,9 +64,8 @@ public class JavaSideKickParsedData extends SideKickParsedData {
         return tn;
     }
 
-
     /**
-     * Drill down to the node containing the given position.  
+     * Drill down to the node containing the given position.
      * @param tn The node to check the children of.
      * @param pos The caret position within the code.
      */
@@ -74,15 +76,14 @@ public class JavaSideKickParsedData extends SideKickParsedData {
             TigerNode child = tn.getChildAt( i );
             int start = child.getStart().getOffset();
             int end = child.getEnd().getOffset();
-            // do two different checks here since 
+            // do two different checks here since
             // during code completion, the parser may not be able to completely
             // parse the buffer and won't be able to calculate the end position
             // of nodes.
             if ( end == 0 && start >= closestPosition && start <= pos ) {
                 closestPosition = start;
                 closestChild = child;
-            }
-            else if (start >= closestPosition && start <= pos && end >= pos) {
+            } else if ( start >= closestPosition && start <= pos && end >= pos ) {
                 closestPosition = start;
                 closestChild = child;
             }
@@ -93,15 +94,68 @@ public class JavaSideKickParsedData extends SideKickParsedData {
         return getTigerNodeAtOffset( closestChild, pos );
     }
 
-
     // overridden to handle Extends and Implements nodes
     protected boolean canAddToPath( TreeNode node ) {
         try {
             TigerNode tn = ( TigerNode ) getAsset( node );
             return ( tn.getOrdinal() == TigerNode.EXTENDS || tn.getOrdinal() == TigerNode.IMPLEMENTS ) ? false : true;
-        }
-        catch ( Exception e ) {
+        } catch ( Exception e ) {
             return super.canAddToPath( node );
+        }
+    }
+    
+    /**
+     * @return A special fold handler for java.
+     */
+    public FoldHandler getFoldHandler() {
+        if ( foldHandler == null ) {
+            foldHandler = new JavaFoldHandler();
+        }
+        return foldHandler;
+    }
+    
+    /**
+     * Java fold handler.    
+     */
+    private class JavaFoldHandler extends FoldHandler {
+
+        public JavaFoldHandler() {
+            super( "javasidekick" );
+        }
+
+        public int getFoldLevel( JEditBuffer buffer, int lineIndex, Segment seg ) {
+            if ( lineIndex == 0 ) {
+                return 0;
+            }
+            int lineStartOffset = buffer.getLineStartOffset( lineIndex );
+            TreePath path = getTreePathForPosition( lineStartOffset );
+            if ( path == null ) {
+                return 0;
+            } else {
+                // check if in imports range
+                Object userObject = ( ( DefaultMutableTreeNode ) root ).getUserObject();
+                if ( ! ( userObject instanceof CUNode ) ) {
+                    return 0;
+                }
+                Range importsRange = ( ( CUNode ) userObject ).getImportsRange();
+                if ( importsRange != null ) {
+                    if ( lineIndex >= importsRange.getStartLocation().line && lineIndex <= importsRange.getEndLocation().line ) {
+                        return 1;
+                    }
+                }
+
+                // otherwise, proceed as usual
+                TreeNode treeNode = ( TreeNode ) path.getLastPathComponent();
+                IAsset asset = getAsset( treeNode );
+                if (userObject.equals(asset)) {
+                    return 0;   
+                }
+                if ( asset.getStart().getOffset() == lineStartOffset ) {
+                    return path.getPathCount() - 2;
+                }
+
+                return path.getPathCount() - 1;
+            }
         }
     }
 }
