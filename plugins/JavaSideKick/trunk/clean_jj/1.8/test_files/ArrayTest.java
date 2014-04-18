8df688fbb68db7066efe23d63d02418d0c2eae9c
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-04-18 20:51:13 +0000

    Adjusted detection of 'if' statements. All tests pass at the moment.

--- plugins/JavaSideKick/trunk/clean_jj/1.8/test_files/ArrayTest.java
+++ plugins/JavaSideKick/trunk/clean_jj/1.8/test_files/ArrayTest.java
@@ -3,13 +3,18 @@ class ArrayTest {
         // System.out.println("a: { " + a[0] + ", " + a[1] + " }");
         // p = (Point)Class.forName("Point").newInstance();
         // System.out.println(hello == ("Hel"+lo).intern());
-        System.out.print("gradual underflow: " + d + "\n   ");
-        for (int i = 0; i < 4; i++) {
-            System.out.print(" " + (d /= 100000));
-        }
+        // System.out.print("gradual underflow: " + d + "\n   ");
+        // for (int i = 0; i < 4; i++) {
+        //     System.out.print(" " + (d /= 100000));
+        // }
         // System.out.print(" " + (d /= 100000));
         // int cn = rw.myNumbers();
         //int d = 7;
         //int a = b + c;
+        for (int i = 0; i < 100; i++) {
+            float z = 1.0f / i;
+            if (z * i != 1.0f)
+                System.out.print(" " + i);
+        }
     }
 }
