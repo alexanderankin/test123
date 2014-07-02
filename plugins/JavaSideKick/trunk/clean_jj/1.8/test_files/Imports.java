d461793c8e88640e8f5eefe8bb745c1f40b73833
Dale <danson@grafidog.com>
2014-07-02 03:47:52 +0000

    Back to the original grammar for Expressions. Still have left recursion in Primary.

---
+++ plugins/JavaSideKick/trunk/clean_jj/1.8/test_files/Imports.java
@@ -0,0 +1,13 @@
+// imports
+
+// single type import
+import java.util.Vector;
+
+// type import on demand
+import java.awt.*;
+
+// single static import
+import static java.lang.Math.random;
+
+// static import on demand
+import static javax.swing.*;
---
+++ plugins/JavaSideKick/trunk/clean_jj/1.8/test_files/Packages.java
@@ -0,0 +1,14 @@
+// packages
+@Any package a.b.c.d.e;
+
+/*
+@Any package a.b.c;
+package a;
+package a.b;
+package a.b.c;
+package a.b.c.d;
+package a.b.c.d.e;
+@Any package a;
+@Any package a.b;
+@Any package a.b.c.d;
+*/
