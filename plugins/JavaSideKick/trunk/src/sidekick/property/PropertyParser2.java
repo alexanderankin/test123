55feb72b78b736fde2ff95880116bb018dd3cf36
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-11-16 18:33:46 +0000

    this file is in the wrong folder

--- plugins/JavaSideKick/trunk/src/sidekick/property/PropertyParser2.java
+++ plugins/JavaSideKick/trunk/src/sidekick/property/PropertyParser2.java
@@ -1,47 +1,53 @@
 package sidekick.property.parser.property;
 
 import sidekick.util.Location;
-import java.io.Reader;
 import java.io.LineNumberReader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import org.gjt.sp.jedit.Buffer;
 
 /**
- * Rewrite of the properties parser without javacc. This seems to be easier to 
+ * Rewrite of the properties parser without javacc. This seems to be easier to
  * maintain and does a better job, especially with multi-line properties.
  */
 public class PropertyParser2 {
 
+    private Buffer buffer = null;
     private LineNumberReader reader = null;
+    private boolean useSoftTabs = true;
     private int tabSize = 4;
     private List<Property> propertyList = null;
     private Property currentProperty = null;
     private boolean inMultiline = false;
+    private int endLine = 0;
+    private int endColumn = 0;
+    private List<ParseException> exceptions = null;
 
-    public PropertyParser2( Reader reader ) {
-        if ( reader == null ) {
-            throw new IllegalArgumentException( "reader is null" );
-        }
-        this.reader = new LineNumberReader( reader );
+    // these help set the asset locations correctly
+    public void setSoftTabs( boolean b ) {
+        useSoftTabs = b;
     }
 
-    // this helps set the asset locations correctly
     public void setTabSize( int size ) {
         if ( size < 0 ) {
-            throw new IllegalArgumentException( "tab size cannot be less than 0" );
+            size = 0;
         }
         tabSize = size;
     }
 
-    // the actual parsing starts here
-    public List<Property> Properties() {
+    // the actual parsing starts here. Soft tabs and tab size should be set prior 
+    // to calling this method. This simple parser reads the buffer contents line
+    // by line. Comment liness and blank lines are ignored.
+    public List<Property> parse( Buffer buffer ) {
+        propertyList = new ArrayList<Property>();
+        if ( buffer == null ) {
+            return propertyList;
+        }
+        this.buffer = buffer;
+        this.reader = new LineNumberReader( new StringReader( buffer.getText( 0, buffer.getLength() ) ) );
         try {
-            if ( reader == null ) {
-                throw new RuntimeException( "reader is null" );
-            }
-
-            propertyList = new ArrayList<Property>();
 
             String line = null;
             while ( true ) {
@@ -54,13 +60,19 @@ public class PropertyParser2 {
 
             Collections.sort( propertyList );
         } catch ( Exception e ) {
-            e.printStackTrace();
+            addException( e );
+        } finally {
+            try {
+                reader.close();
+            } catch ( Exception e ) {
+                addException( e );
+            }
         }
         return propertyList;
     }
 
     private void processLine( String line ) {
-        if ( isBlankLine( line ) || isComment(line) ) {
+        if ( isBlankLine( line ) || isComment( line ) ) {
             return;
         }
         createProperty( line );
@@ -75,18 +87,13 @@ public class PropertyParser2 {
             boolean hasContinuation = hasContinuation( line );
             if ( ! hasContinuation ) {
                 // finish out the multi-line property
-                inMultiline = false;
-                int endLine = reader.getLineNumber();
-                int endColumn = getEndColumn( line );
-                Location end = new Location( endLine, endColumn );
-                currentProperty.setEndLocation( end );
-                String[] split = getKeyAndValue( currentProperty.getKey() );
-                currentProperty.setKey( split[0] );
-                currentProperty.setValue( split[1] );
-                propertyList.add( currentProperty );
+                endLine = reader.getLineNumber();
+                endColumn = buffer.getLineEndOffset( endLine );
+                finishCurrentProperty();
             }
         } else {
             currentProperty = new Property();
+            currentProperty.setKey( trimLine( line ) );
             int startLine = reader.getLineNumber();
             int startColumn = getLeadingWSLength( line );
             Location start = new Location( startLine, startColumn );
@@ -94,17 +101,25 @@ public class PropertyParser2 {
             boolean hasContinuation = hasContinuation( line );
             if ( hasContinuation ) {
                 inMultiline = true;
-                currentProperty.setKey( trimLine( line ) );
             } else {
-                int endColumn = getEndColumn( line );
-                Location end = new Location( startLine, endColumn );
-                currentProperty.setEndLocation( end );
-                String[] split = getKeyAndValue( line );
-                currentProperty.setKey( split[0] );
-                currentProperty.setValue( split[1] );
-                propertyList.add( currentProperty );
+                endLine = startLine;
+                endColumn = buffer.getLineEndOffset( endLine );
+                finishCurrentProperty();
             }
         }
+    }
+
+    private void finishCurrentProperty() {
+        if ( currentProperty != null ) {
+            inMultiline = false;
+            Location end = new Location( endLine, endColumn );
+            currentProperty.setEndLocation( end );
+            String[] split = getKeyAndValue( currentProperty.getKey() );
+            currentProperty.setKey( split[0] );
+            currentProperty.setValue( split[1] );
+            propertyList.add( currentProperty );
+        }
+        currentProperty = null;
     }
 
     // splits out the key and value from a single string
@@ -119,8 +134,12 @@ public class PropertyParser2 {
         }
     }
 
-    // finds the "equals", that is, the character that separates the key from the value
+    // finds the "equals", that is, the character that separates the key from the
+    // value, which is the first non-escaped =, :, or whitespace
     private int findEquals( String s ) {
+        if ( s == null ) {
+            return -1;
+        }
         for ( int i = 0; i < s.length(); i++ ) {
             char c = s.charAt( i );
             switch ( c ) {
@@ -164,6 +183,7 @@ public class PropertyParser2 {
         return slashCount > 0 && slashCount % 2 == 1;
     }
 
+    // checks if a line contains only whitespace
     private boolean isBlankLine( String s ) {
         if ( s == null || s.length() == 0 ) {
             return true;
@@ -176,6 +196,7 @@ public class PropertyParser2 {
         return true;
     }
 
+    // checks if a line is a comment line
     private boolean isComment( String line ) {
         String trimmed = trimStart( line );
         if ( trimmed.isEmpty() ) {
@@ -187,6 +208,9 @@ public class PropertyParser2 {
 
     // trim all whitespace from the start of the given string
     private String trimStart( String s ) {
+        if ( s == null ) {
+            return "";
+        }
         StringBuilder sb = new StringBuilder( s );
         while ( sb.length() > 0 && Character.isWhitespace( sb.charAt( 0 ) ) ) {
             sb.deleteCharAt( 0 );
@@ -202,16 +226,21 @@ public class PropertyParser2 {
         return s;
     }
 
+    // removes all leading whitespace and last continuation character, if any
     private String trimLine( String s ) {
         return trimContinuation( trimStart( s ) );
     }
 
+    // calculate the length of the leading whitespace for the given string
+    // taking tab width into account.
+    // TODO: check this, are hard tabs properly handled?
     private int getLeadingWSLength( String s ) {
         int length = 0;
+        int tabWidth = useSoftTabs ? tabSize : 1;
         for ( int i = 0; i < s.length(); i++ ) {
             char c = s.charAt( i );
             if ( Character.isWhitespace( c ) ) {
-                length += c == '\t' ? tabSize : 1;
+                length += c == '\t' ? tabWidth : 1;
             } else {
                 break;
             }
@@ -219,15 +248,19 @@ public class PropertyParser2 {
         return length;
     }
 
-    private int getEndColumn( String s ) {
-        int length = 0;
-        for ( int i = 0; i < s.length(); i++ ) {
-            length += s.charAt( i ) == '\t' ? tabSize : 1;
+    // accumulate any exceptions during parsing
+    private void addException( Exception e ) {
+        if ( e == null ) {
+            return;
         }
-        return length + 1;  // column is 1-based
+        ParseException pe = new ParseException( e.getMessage() );
+        if ( exceptions == null ) {
+            exceptions = new ArrayList<ParseException>();
+        }
+        exceptions.add( pe );
     }
 
     public List<ParseException> getExceptions() {
-        return null;
+        return exceptions;
     }
 }
