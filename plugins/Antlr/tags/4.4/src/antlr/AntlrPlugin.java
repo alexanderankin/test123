d35d66531bdcd5f5cfa40eb725ecd87f3ee46794
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-11-25 22:31:56 +0000

    Tag for release.

---
+++ plugins/Antlr/tags/4.4/src/antlr/AntlrPlugin.java
@@ -0,0 +1,15 @@
+package antlr;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class AntlrPlugin extends EditPlugin {
+
+	public void start() {
+		// Nothing to do here - this is a dependency plugin.
+	}
+
+	public void stop() {
+		// Nothing to do here - this is a dependency plugin.
+	}
+
+}
