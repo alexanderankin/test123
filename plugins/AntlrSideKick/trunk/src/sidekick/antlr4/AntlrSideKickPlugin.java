1dd6fb0dcfd3bb64cd148487df03665b875af86a
Dale <danson@grafidog.com>
2014-12-17 03:17:24 +0000

    Initial commit, this is a SideKick for Antlr 4 grammar files. It works, but is not yet complete.

---
+++ plugins/AntlrSideKick/trunk/src/sidekick/antlr4/AntlrSideKickPlugin.java
@@ -0,0 +1,13 @@
+package sidekick.antlr4;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class AntlrSideKickPlugin extends EditPlugin {
+    public static final String NAME = "sidekick.antlr4";
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+}
