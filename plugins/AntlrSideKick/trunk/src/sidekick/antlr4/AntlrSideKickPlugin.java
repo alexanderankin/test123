bc2c9e3c3d4dd040d8d192c220920a14c5e6de44
Dale <danson@grafidog.com>
2014-12-17 21:48:34 +0000

    Updated docs, added action to generate files from an antlr grammar.

--- plugins/AntlrSideKick/trunk/src/sidekick/antlr4/AntlrSideKickPlugin.java
+++ plugins/AntlrSideKick/trunk/src/sidekick/antlr4/AntlrSideKickPlugin.java
@@ -1,10 +1,9 @@
 package sidekick.antlr4;
 
+import org.gjt.sp.jedit.Buffer;
 import org.gjt.sp.jedit.EditPlugin;
 
-// QUESTION: add an action to allow the user to generate files from the current grammar file?
-// The antlr-4.4-complete.jar is all that is necessary, and it's a dependency of this plugin
-// via the Antlr plugin.
+import org.antlr.v4.Tool;
 
 public class AntlrSideKickPlugin extends EditPlugin {
     public static final String NAME = "sidekick.antlr4";
@@ -14,4 +13,12 @@ public class AntlrSideKickPlugin extends EditPlugin {
     public void stop() {
     }
 
+    // Generate antlr files from the current file in the buffer. 
+    public static void generateFiles(Buffer buffer) {
+        if ("antlr4".equals(buffer.getMode().getName())) {
+            String[] args = new String[]{buffer.getPath()};
+            Tool antlr = new Tool(args);
+            antlr.processGrammarsOnCommandLine();
+        } 
+    }
 }
