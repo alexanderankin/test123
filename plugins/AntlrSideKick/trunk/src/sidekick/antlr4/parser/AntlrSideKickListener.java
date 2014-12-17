3eb3489b403652b6900afdbb0e5b3bb70d2fbb6b
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-12-17 04:11:12 +0000

    Updates, still a work in progress.

--- plugins/AntlrSideKick/trunk/src/sidekick/antlr4/parser/AntlrSideKickListener.java
+++ plugins/AntlrSideKick/trunk/src/sidekick/antlr4/parser/AntlrSideKickListener.java
@@ -4,6 +4,7 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.NotNull;
 
 import sidekick.util.Location;
+import eclipseicons.EclipseIconsPlugin;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -11,11 +12,16 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 
+import javax.swing.ImageIcon;
+
 public class AntlrSideKickListener extends ANTLRv4ParserBaseListener {
     
     Deque<AntlrNode> stack = new ArrayDeque<AntlrNode>();
     List<AntlrNode> lexerRules = new ArrayList<AntlrNode>();
     List<AntlrNode> parserRules = new ArrayList<AntlrNode>();
+    
+    ImageIcon lexerIcon = EclipseIconsPlugin.getIcon("field_public_obj.gif");
+    ImageIcon parserIcon = EclipseIconsPlugin.getIcon("elements_obj.gif");
     
     public List<AntlrNode> getLexerRules() {
         Collections.sort(lexerRules);
@@ -27,18 +33,23 @@ public class AntlrSideKickListener extends ANTLRv4ParserBaseListener {
         return parserRules;   
     }
     
+    // field_public_obj.gif
 	@Override public void enterLexerRule(@NotNull ANTLRv4Parser.LexerRuleContext ctx) {
         AntlrNode node = new AntlrNode();
         node.setName(ctx.TOKEN_REF().getText());
-        node.setStartLocation(getStartLocation(ctx));
+        node.setStartLocation(new Location(ctx.TOKEN_REF().getSymbol().getLine(), ctx.TOKEN_REF().getSymbol().getStartIndex()));
         node.setEndLocation(getEndLocation(ctx));
+        node.setIcon(lexerIcon);
         lexerRules.add(node);
 	}
+	
+	// elements_obj.gif
 	@Override public void enterParserRuleSpec(@NotNull ANTLRv4Parser.ParserRuleSpecContext ctx) {
         AntlrNode node = new AntlrNode();
         node.setName(ctx.RULE_REF().getText());
-        node.setStartLocation(getStartLocation(ctx));
+        node.setStartLocation(new Location(ctx.RULE_REF().getSymbol().getLine(), ctx.RULE_REF().getSymbol().getStartIndex()));
         node.setEndLocation(getEndLocation(ctx));
+        node.setIcon(parserIcon);
         parserRules.add(node);
 	}
     
