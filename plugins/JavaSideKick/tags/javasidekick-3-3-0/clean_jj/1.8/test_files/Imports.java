90c5f0437049bd0364f2d97195f2e57c210c27ec
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2014-12-19 03:56:07 +0000

    Tagging for release.

---
+++ plugins/JavaSideKick/tags/javasidekick-3-3-0/clean_jj/1.8/test_files/Imports.java
@@ -0,0 +1,17 @@
+// imports
+
+import java.util.Vector;
+
+/*
+// type import on demand
+import java.awt.*;
+
+// single static import
+import static java.lang.Math.random;
+
+// static import on demand
+import static javax.swing.*;
+*/
+public class Imports {
+
+}
