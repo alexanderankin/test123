7ede84e4314eee7f7b48aad63c8837888ab23828
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-08-10 22:01:35 +0000

    Tag for release.

---
+++ plugins/ColumnRuler/tags/release-1_0_5/org/jedit/plugins/columnruler/ColumnRulerPlugin.java
@@ -0,0 +1,156 @@
+package org.jedit.plugins.columnruler;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.util.*;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.jedit.msg.*;
+import org.gjt.sp.jedit.textarea.*;
+import org.gjt.sp.util.*;
+
+/**
+ * Core class of ColumnRuler plugin.
+ *
+ * @author     mace
+ * @version    $Revision: 1.5 $ $Date: 2006-10-11 16:36:03 $ by $Author: k_satoda $
+ */
+public class ColumnRulerPlugin extends EBPlugin {
+	private static Map<TextArea,ColumnRuler> rulerMap = new HashMap<TextArea,ColumnRuler>();
+	public final static String NAME = "columnruler";
+	public final static String OPTION_PREFIX = "options.columnruler.";
+	public final static String PROPERTY_PREFIX = "plugin.columnruler.";
+
+	/**
+	 * Returns the ColumnRuler for the given text area, or null.
+	 */
+	public static ColumnRuler getColumnRulerForTextArea(TextArea textArea) {
+		return (textArea != null) ? (ColumnRuler) rulerMap.get(textArea) : null;
+	}
+
+	public static void toggleColumnRulerForTextArea(TextArea textArea) {
+		boolean vis = !(rulerMap.get(textArea) != null);
+		if (vis) {
+			addColumnRulerToTextArea(textArea);
+		} else {
+			removeColumnRulerFromTextArea(textArea);
+		}
+	}
+
+	private static void addColumnRulerToTextArea(TextArea textArea) {
+		if (rulerMap.containsKey(textArea)) {
+			Log.log(Log.DEBUG, textArea, "Redundant call for addColumnRulerToTextArea()");
+			return;
+		}
+		ColumnRuler columnRuler = new ColumnRuler(textArea);
+		rulerMap.put(textArea, columnRuler);
+		textArea.addTopComponent(columnRuler);
+		MarkManager.getInstance().addMarkManagerListener(columnRuler);
+		for (DynamicMark mark : getDynamicMarks()) {	
+			mark.activate(textArea);
+		}
+		textArea.revalidate();
+		textArea.repaint();
+	}
+
+	private static void removeColumnRulerFromTextArea(TextArea textArea) {
+		ColumnRuler columnRuler = rulerMap.get(textArea);
+		if (columnRuler == null) {
+			return;
+		}
+		for (DynamicMark mark : getDynamicMarks()) {
+			mark.removePositionOn(columnRuler);
+			mark.deactivate(textArea);
+		}
+		MarkManager.getInstance().removeMarkManagerListener(columnRuler);
+		textArea.removeTopComponent(columnRuler);
+		rulerMap.remove(textArea);
+	}
+
+	private static boolean isActiveByDefault() {
+		return jEdit.getProperty("plugin.org.jedit.plugins.columnruler.ColumnRulerPlugin.activate").equals("startup");
+	}
+
+	//{{{ start/stop
+	public void start() {
+		for (DynamicMark mark : getDynamicMarks()) {
+			EditBus.addToBus(mark);
+		}
+		if (isActiveByDefault()) {
+			for (View view : jEdit.getViews()) {
+				for (EditPane editPane : view.getEditPanes()) {
+					addColumnRulerToTextArea(editPane.getTextArea());
+				}
+			}
+		}
+	}
+
+	public void stop() {
+		MarkManager.getInstance().save();
+		
+		TextArea[] keys = rulerMap.keySet().toArray(new TextArea[0]);
+		for (TextArea textArea : keys) {
+			removeColumnRulerFromTextArea(textArea);
+		}
+		
+		for (DynamicMark mark : getDynamicMarks()) {
+			EditBus.removeFromBus(mark);
+			mark.shutdown();
+		}
+		
+	}
+	//}}}
+
+	//{{{ handleMessage
+	/**
+	 * Handles a message sent on the EditBus.
+	 */
+	public void handleMessage(EBMessage message) {
+		if (message instanceof EditPaneUpdate) {
+			EditPaneUpdate epu = (EditPaneUpdate) message;
+			EditPane editPane = epu.getEditPane();
+			if (epu.getWhat().equals(EditPaneUpdate.CREATED) && isActiveByDefault()) {
+				addColumnRulerToTextArea(editPane.getTextArea());
+			}
+			if (epu.getWhat().equals(EditPaneUpdate.DESTROYED)) {
+				removeColumnRulerFromTextArea(editPane.getTextArea());
+			}
+		}
+	} //}}}
+
+	public static List<DynamicMark> getDynamicMarks() {
+		List<DynamicMark> marks = new ArrayList<DynamicMark>();
+		String[] services = ServiceManager.getServiceNames("org.jedit.plugins.columnruler.DynamicMark");
+		for (String service : services) {
+			DynamicMark mark = (DynamicMark) ServiceManager.getService("org.jedit.plugins.columnruler.DynamicMark", service);
+			marks.add(mark);
+		}
+		
+		return marks;
+	}
+
+   /**
+    * Centers <code>you</code> on <code>me</code>. Useful for centering
+    * dialogs on their parent frames.
+    *
+    * @param me   Component to use as basis for centering.
+    * @param you  Component to center on <code>me</code>.
+    */
+   public static void center( Component me, Component you ) {
+      Rectangle my = me.getBounds();
+      Dimension your = you.getSize();
+      int x = my.x + ( my.width - your.width ) / 2;
+      if ( x < 0 )
+         x = 0;
+      int y = my.y + ( my.height - your.height ) / 2;
+      if ( y < 0 )
+         y = 0;
+      you.setLocation( x, y );
+   }
+
+	
+}
+
---
+++ plugins/ColumnRuler/tags/release-1_0_5/org/jedit/plugins/columnruler/MarkDialog.java
@@ -0,0 +1,101 @@
+package org.jedit.plugins.columnruler;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.ColorWellButton;
+import org.gjt.sp.jedit.textarea.TextArea;
+import org.gjt.sp.util.Log;
+
+/**
+ *  Dialog for creating new marks/guides.
+ *
+ * @author     Brad Mace
+ * @version    $Revision: 1.3 $ $Date: 2006-10-08 08:21:53 $
+ */
+public class MarkDialog extends JDialog implements ActionListener {
+    private StaticMark mark;
+    private JTextField name;
+    private JTextField column;
+    private ColorWellButton color;
+    private JButton ok;
+    private JButton cancel;
+
+    /*
+	 * Creates dialog for adding a new mark.
+    */
+    public MarkDialog( int column ) {
+        super( jEdit.getActiveView(), "Add Mark" );
+        name = new JTextField(5 );
+        this.column = new JTextField( column + "" );
+        color = new ColorWellButton( Color.WHITE );
+        init();
+    }
+
+    /**
+     * Creates dialog for editing a mark.
+     */
+    public MarkDialog( StaticMark m, String title ) {
+        super( jEdit.getActiveView(), title, true );
+        this.mark = m;
+        if ( m == null ) {
+            name = new JTextField(5 );
+            column = new JTextField(4 );
+            color = new ColorWellButton( Color.WHITE );
+        } else {
+            name = new JTextField( m.getName() );
+            column = new JTextField( m.getColumn() + "" );
+            color = new ColorWellButton( m.getColor() );
+        }
+        init();
+    }
+
+    private void init() {
+        JPanel panel = new JPanel();
+        panel.setBorder( BorderFactory.createEmptyBorder(11, 11, 11, 11 ) );
+        ok = new JButton( "OK" );
+        ok.addActionListener( this );
+        cancel = new JButton( "Cancel" );
+        cancel.addActionListener( this );
+        panel.setLayout( new GridLayout(4, 2, 6, 6 ) );
+        panel.add( new JLabel( "Name" ) );
+        panel.add( name );
+        panel.add( new JLabel( "Column" ) );
+        panel.add( column );
+        panel.add( new JLabel( "Color" ) );
+        panel.add( color );
+        panel.add( ok );
+        panel.add( cancel );
+        setContentPane( panel );
+        pack();
+        ColumnRulerPlugin.center( jEdit.getActiveView(), this );
+        setVisible( true );
+    }
+
+    public void actionPerformed( ActionEvent e ) {
+        if ( e.getSource() == ok ) {
+            if ( mark == null ) {
+                mark = new StaticMark( name.getText() );
+            } else {
+                mark.setName( name.getText() );
+            }
+            mark.setColumn( Integer.parseInt( column.getText() ) );
+            mark.setColor( color.getSelectedColor() );
+            if ( !MarkManager.getInstance().containsMark( mark ) ) {
+                Log.log( Log.DEBUG, this, "Adding mark to MarkContainer" );
+                MarkManager.getInstance().addMark( mark );
+            }
+            TextArea textArea = jEdit.getActiveView().getTextArea();
+            if ( ColumnRulerPlugin.getColumnRulerForTextArea( textArea ) != null ) {
+                ColumnRulerPlugin.getColumnRulerForTextArea( textArea ).repaint();
+            }
+            if ( mark.isGuideVisible() ) {
+                textArea.repaint();
+            }
+        }
+        MarkManager.getInstance().fireMarksUpdated();
+        dispose();
+    }
+}
