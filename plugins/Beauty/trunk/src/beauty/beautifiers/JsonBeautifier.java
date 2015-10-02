7863864fa088235ae3dc1767bd0f13f9479ab9f1
Dale <danson@grafidog.com>
2015-10-02 21:30:44 +0000

    Replaced the javacc-based JSON parser with an antlr based parser. The antlr parser should be much easier to maintain.

--- plugins/Beauty/trunk/src/beauty/beautifiers/JsonBeautifier.java
+++ plugins/Beauty/trunk/src/beauty/beautifiers/JsonBeautifier.java
@@ -6,29 +6,33 @@ import java.util.*;
 import beauty.parsers.ParserException;
 import beauty.parsers.json.*;
 
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
 public class JsonBeautifier extends Beautifier {
 
-    private static JsonParser parser = null;
+    private static JSONParser parser = null;
 
     public String beautify(String text) throws ParserException {
         try {
             // set up the parser
-            StringReader is = new StringReader(text);
-            if (parser == null) {
-                parser = new JsonParser(is);
-            } else {
-                parser.ReInit(is);
-                parser.resetTokenSource();
-            }
+            StringReader input = new StringReader( text );
+            ANTLRInputStream antlrInput = new ANTLRInputStream( input );
+            JSONLexer lexer = new JSONLexer( antlrInput );
+            CommonTokenStream tokens = new CommonTokenStream( lexer );
+            JSONParser jsonParser = new JSONParser( tokens );
 
-            // set the parser settings
-            parser.setIndentWidth(getIndentWidth());
-            parser.setTabSize(getTabWidth());
-            parser.setLineSeparator(getLineSeparator());
- 
-            // do the parse
-            parser.parse();
-            return parser.getText();
+            // add an error handler that stops beautifying on any parsing error
+            jsonParser.setErrorHandler(new JSONErrorHandler());
+            
+            // parse and beautify the buffer contents
+            ParseTree tree = jsonParser.json();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            JSONBeautyListener listener = new JSONBeautyListener(text.length());
+            walker.walk( listener, tree );
+
+            return listener.getText();
+            
         } catch (Exception e) {
             throw new ParserException(e);
         }
