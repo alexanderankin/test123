8b77712b852340c4cb633f9ca62902157d61dcc1
Dale <danson@grafidog.com>
2015-10-28 17:56:36 +0000

    no comment

--- plugins/XML/tags/release-3.0.0/tags/release-2.8.3-jedit-4.5/sidekick/ecmascript/parser/EcmaScriptVisitorDelegate.java
+++
@@ -1,9 +0,0 @@
-package sidekick.ecmascript.parser;
-
-public interface EcmaScriptVisitorDelegate {
-
-    public void willVisit(SimpleNode node, Object data);
-
-    public void didVisit(SimpleNode node, Object data);
-
-}
