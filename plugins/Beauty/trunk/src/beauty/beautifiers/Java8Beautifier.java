f08c04f4baa576d99086564716c6653dc1cb6f1c
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-11-23 19:03:20 +0000

    Threading adjustments, better notification, fixed issues with "continue" statement and "switch" blocks.

--- plugins/Beauty/trunk/src/beauty/beautifiers/Java8Beautifier.java
+++ plugins/Beauty/trunk/src/beauty/beautifiers/Java8Beautifier.java
@@ -1,17 +1,25 @@
+
 package beauty.beautifiers;
 
 import beauty.parsers.ParserException;
 import beauty.parsers.java.antlr.*;
 
+import java.awt.*;
 import java.io.*;
 import java.util.*;
 
+import javax.swing.*;
+
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.PredictionMode;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.*;
+import org.gjt.sp.jedit.GUIUtilities;
 import org.gjt.sp.jedit.jEdit;
 
 
 public class Java8Beautifier extends Beautifier {
+
     private int bracketStyle = 1;    // JavaParser.ATTACHED;
     private boolean breakElse = false;
     private boolean padParens = false;
@@ -27,7 +35,6 @@ public class Java8Beautifier extends Beautifier {
     private int blankLinesAfterMethods = 1;
     private boolean sortModifiers = true;
     private int collapseMultipleBlankLinesTo = 1;
-
 
     public void init() {
         bracketStyle = jEdit.getIntegerProperty( "beauty.java.bracketStyle", Java8BeautyListener.ATTACHED );
@@ -47,15 +54,12 @@ public class Java8Beautifier extends Beautifier {
         collapseMultipleBlankLinesTo = jEdit.getIntegerProperty( "beauty.java8.collapseMultipleBlankLinesTo", 1 );
     }
 
-
     public String beautify( String text ) throws ParserException {
 
-        // System.out.println(text);
         ErrorListener errorListener = null;
         try {
 
-            // protect unicode escaped character sequences
-            // text = text.replaceAll("\\\\u", "\\\\\\\\u");
+
             // set up the parser
             StringReader input = new StringReader( text );
             ANTLRInputStream antlrInput = new ANTLRInputStream( input );
@@ -65,18 +69,10 @@ public class Java8Beautifier extends Beautifier {
 
             // for debugging
             String trace = System.getProperty( "beauty.java8.trace" );
-            javaParser.setTrace( "true".equals( trace )  );
+            javaParser.setTrace( "true".equals( trace ) );
 
-            // add an error listener to the parser to capture any errors
-            javaParser.removeErrorListeners();
-            errorListener = new ErrorListener();
-            javaParser.addErrorListener( errorListener );
-
-            ParseTree tree = javaParser.compilationUnit();
-            ParseTreeWalker walker = new ParseTreeWalker();
+            // set up the formatting options
             Java8BeautyListener listener = new Java8BeautyListener( text.length() + 2048, tokens );
-
-            // set the formatting settings
             listener.setIndentWidth( getIndentWidth() );
             listener.setUseSoftTabs( getUseSoftTabs() );
             listener.setBracketStyle( bracketStyle );
@@ -95,13 +91,44 @@ public class Java8Beautifier extends Beautifier {
             listener.setSortModifiers( sortModifiers );
             listener.setCollapseMultipleBlankLinesTo( collapseMultipleBlankLinesTo );
 
-            // parse and beautify the buffer contents
-            walker.walk( listener, tree );
+            // parse the buffer text
+            ParseTreeWalker walker = new ParseTreeWalker();
+            ParseTree tree = null;
+            try {
 
+                // first try the faster SSL(*) parsing strategy
+                javaParser.getInterpreter().setPredictionMode( PredictionMode.SLL );
+                javaParser.removeErrorListeners();
+                javaParser.setErrorHandler( new BailErrorStrategy() );
+                tree = javaParser.compilationUnit();
+            }
+            catch ( ParseCancellationException ex ) {
+                setProgressMessage(jEdit.getProperty("beauty.java8.Encountered_possible_syntax_error,_reparsing...", "Encountered possible syntax error, reparsing..."));
+                
+                // if first parse fails, try again with slower LL(*) parse
+                // Actually, the slowness happens in the first parse, since the
+                // input was tokenized there and is reused here.
+                tokens.reset();    // rewind input stream
+                javaParser.reset();
+
+                // add an error listener to the parser to capture any real errors
+                javaParser.removeErrorListeners();
+                errorListener = new ErrorListener();
+                javaParser.addErrorListener( errorListener );
+                javaParser.setErrorHandler( new DefaultErrorStrategy() );
+
+                // reparse with full LL(*)
+                javaParser.getInterpreter().setPredictionMode( PredictionMode.LL );
+                long parseTime = System.currentTimeMillis();
+                tree = javaParser.compilationUnit();
+            }
+
+
+            walker.walk( listener, tree );
             return listener.getText();
         }
         catch ( Exception e ) {
-            List<ParserException> errors = errorListener.getErrors();
+            java.util.List <ParserException> errors = errorListener.getErrors();
             if ( errors != null && errors.size() > 0 ) {
                 throw errors.get( 0 );
             }
@@ -111,18 +138,18 @@ public class Java8Beautifier extends Beautifier {
         }
     }
 
-
     public void setBracketStyle( int style ) {
         bracketStyle = style;
     }
-
 
     public void setBreakElse( boolean b ) {
         breakElse = b;
     }
 
-
     public void setPadParens( boolean pad ) {
         padParens = pad;
     }
+
 }
+
+
