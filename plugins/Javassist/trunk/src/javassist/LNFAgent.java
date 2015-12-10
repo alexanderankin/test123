5eda0024bd3fa3303998f7bb68456cdcf6c25b43
Dale <danson@grafidog.com>
2015-12-10 04:10:31 +0000

    Code clean up and license blurb.

--- plugins/Javassist/trunk/src/javassist/LNFAgent.java
+++ plugins/Javassist/trunk/src/javassist/LNFAgent.java
@@ -1,3 +1,20 @@
+/*
+ * (c) 2015 Dale Anson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package javassist;
 
 import java.io.*;
@@ -6,7 +23,7 @@ import java.security.ProtectionDomain;
 
 /**
  * Simple java agent to fix the <code>provideErrorFeedback</code> in javax.swing.LookAndFeel.
- * This adds a line to that method to check the System property "allowBeep" before it 
+ * This adds a line to that method to check the System property "allowBeep" before it
  * proceeds with beeping. If "allowBeep" is anything other than "true", the method returns
  * immediately without providing any error feedback.
  */
@@ -19,16 +36,17 @@ public final class LNFAgent implements ClassFileTransformer {
 
         try {
             agent = new LNFAgent();
-        } catch ( Exception e ) {
-            System.out.println("LNFAgent not installed.");
+        }
+        catch ( Exception e ) {
+            System.out.println( "LNFAgent not installed." );
             return;
         }
         instrumentation.addTransformer( agent );
-        
+
         // signal the LookAndFeel plugin that this agent is installed. Since this agent loads
         // into the JVM well before the LookAndFeel plugin, setting a system property is an
         // acceptable way to do this.
-        System.setProperty("LNFAgentInstalled", "true");
+        System.setProperty( "LNFAgentInstalled", "true" );
     }
 
     @Override
@@ -41,10 +59,13 @@ public final class LNFAgent implements ClassFileTransformer {
                 CtMethod ctMethod = ctClass.getDeclaredMethod( "provideErrorFeedback" );
                 ctMethod.insertBefore( "if (!\"true\".equals(System.getProperty(\"allowBeep\"))) return;" );
                 result = ctClass.toBytecode();
-            } catch ( Exception e ) {
-                System.err.println("Unable to patch javax.swing.LookAndFeel to disable beeps.");
+            }
+            catch ( Exception e ) {
+                System.err.println( "Unable to patch javax.swing.LookAndFeel to disable beeps." );
             }
         }
+
         return result;
     }
 }
+
