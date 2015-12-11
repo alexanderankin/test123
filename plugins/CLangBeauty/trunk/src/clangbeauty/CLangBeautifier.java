e86797fc3d61c7c4c085a7a7c2359b11c11fe83e
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2015-12-11 20:51:41 +0000

    Initial commit.

---
+++ plugins/CLangBeauty/trunk/src/clangbeauty/CLangBeautifier.java
@@ -0,0 +1,28 @@
+
+/*
+ * CLangBeautifier.java - clang-format command-line
+ * :tabSize=4:indentSize=4:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2015, Dale Anson
+ *
+ * The CLangBeautifier plugin is licensed under the GNU General Public License.
+ */
+package clangbeauty;
+
+import java.util.Arrays;
+import java.util.List;
+
+import outerbeauty.OuterBeautifier;
+
+/**
+ * Execute clang-format in pipe mode
+ **/
+public class CLangBeautifier extends OuterBeautifier {
+
+	@Override
+	protected List<String> getCommandLine() {
+		String exe = CLangBeautyPlugin.getCLangFormatExe();
+		return Arrays.asList( exe, "-style=LLVM" );
+	}
+}
