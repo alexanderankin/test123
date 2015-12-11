4de38b4e53d5f91a22652eed66add75cdf8e6ad5
Dale <danson@grafidog.com>
2015-12-11 21:44:06 +0000

    Fixed method signature again.

--- plugins/Beauty/trunk/src/beauty/parsers/java/antlr/ErrorListener.java
+++ plugins/Beauty/trunk/src/beauty/parsers/java/antlr/ErrorListener.java
@@ -1,10 +1,13 @@
+
 package beauty.parsers.java.antlr;
 
-import org.antlr.v4.runtime.*;
-
-import java.util.*;
 import beauty.parsers.ParserException;
 
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.Nullable;
 
 public class ErrorListener extends BaseErrorListener {
 
@@ -15,14 +18,16 @@ public class ErrorListener extends BaseErrorListener {
     }
 
     @Override
-    public void syntaxError( Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e ) {
+    public <T extends Token> void syntaxError( @NotNull Recognizer<T, ?> recognizer, @Nullable T offendingSymbol, int line, int charPositionInLine, @NotNull String msg, @Nullable RecognitionException e ) {
         int length = 0;
         if ( e != null && e.getOffendingToken() != null ) {
             int startOffset = e.getOffendingToken().getStartIndex();
             int endOffset = e.getOffendingToken().getStopIndex();
             length = endOffset - startOffset;
         }
-        ParserException pe = new ParserException( msg, line - 1, charPositionInLine, length);
+
+        ParserException pe = new ParserException( msg, line - 1, charPositionInLine, length );
         errors.add( pe );
     }
 }
+
