791024e142eb3b933ce3dc68db5335cdf6ece3ab
kerik-sf <kerik-sf@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2017-03-04 15:50:57 +0000

    Create tag release-3.0.6

---
+++ plugins/XML/tags/release-3.0.6/xml/CacheMigrationService.java
@@ -0,0 +1,62 @@
+package xml;
+
+import java.io.File;
+
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import org.gjt.sp.jedit.MiscUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.util.Log;
+import org.gjt.sp.util.StandardUtilities;
+import org.jedit.migration.OneTimeMigrationService;
+
+/** Moves the xml cache folders from a non-standard location (jEdit settings)
+ *  into a proper place under getPluginHome().
+ *
+ * @author ezust
+ *
+ */
+public class CacheMigrationService extends OneTimeMigrationService {
+
+	public CacheMigrationService() {
+		super("xmlcache");
+	}
+
+	@Override
+	public void migrate() {
+		Hashtable<String, String> table = new Hashtable<String, String>();
+		Properties p = jEdit.getProperties();		
+		String settingsDirectory = StandardUtilities.charsToEscapes(jEdit.getSettingsDirectory(), "\\\\");
+		for (Object ko : p.keySet()) {
+			String key = ko.toString();
+			if (key.startsWith("xml.cache")) {
+				String value = jEdit.getProperty(key);
+				String v2 = value.replaceFirst(settingsDirectory, XmlPlugin.getSettingsDirectory());
+				if (!value.equals(v2))
+					table.put(key, v2);
+			}
+		}
+		for (String k : table.keySet()) {
+			jEdit.unsetProperty(k);
+			jEdit.setProperty(k, table.get(k));
+			Log.log(Log.DEBUG, this, "Migrating property " + k + " to value: " + table.get(k));
+		}
+
+		String dirsToMove[] = new String[] {"dtds", "cache", "import_schema", "relax_ng"};
+
+		for (String dir: dirsToMove) {
+			String oldDir = MiscUtilities.concatPath(jEdit.getSettingsDirectory(), dir);
+			String newDir = MiscUtilities.concatPath(XmlPlugin.getSettingsDirectory(), dir);
+			File nf = new File(newDir);
+			if (nf.isDirectory()) continue;
+			File oldf = new File(oldDir);
+			if (!oldf.isDirectory()) continue;
+			boolean success = oldf.renameTo(nf);
+			Log.log (Log.DEBUG, this, "Rename " + oldDir + " to " + newDir + " success: " + success);
+		}
+		jEdit.propertiesChanged();
+
+	}
+}
