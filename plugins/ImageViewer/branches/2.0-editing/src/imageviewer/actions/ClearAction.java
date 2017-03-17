687ab8a63455ecd96a7d55ba333cb58c8c6fcb38
Dale <danson@grafidog.com>
2017-03-17 00:37:28 +0000

    Some code clean up and refactoring.

--- plugins/ImageViewer/branches/2.0-editing/src/imageviewer/actions/ClearAction.java
+++ plugins/ImageViewer/branches/2.0-editing/src/imageviewer/actions/ClearAction.java
@@ -26,27 +26,23 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-package imageviewer;
+package imageviewer.actions;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import imageviewer.ImageViewer;
 
-/**
- * A mouse adapter that also listens to motion and wheel events. I added this
- * because Java 1.5 doesn't have such a thing and I'd inadvertently used the
- * Java 1.6 version of MouseAdapter, which means this plugin wouldn't have
- * ran on Java 1.5.  This provides Java 1.5 compatibility.
- * TODO: Java 7 has been out for some time, jEdit now requires at least Java 6, 
- * so this can be removed and use the newer java.awt.event.MouseAdapter.
- */
-public abstract class MMouseAdapter extends MouseAdapter implements MouseMotionListener, MouseWheelListener {
+public class ClearAction implements ActionListener {
     
-    public void mouseDragged( MouseEvent me ) {}
+    private ImageViewer imageViewer;
     
-    public void mouseMoved( MouseEvent me ) {}
-
-    public void mouseWheelMoved( MouseWheelEvent me ) {}
+    public ClearAction(ImageViewer imageViewer) {
+        this.imageViewer = imageViewer;
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        imageViewer.getImageLabel().setIcon( null );
+        imageViewer.getImageDataPanel().clear();
+        imageViewer.refresh();
+    }
 }
