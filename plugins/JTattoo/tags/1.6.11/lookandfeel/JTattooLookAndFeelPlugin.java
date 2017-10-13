9bcf6e1f247723bf0b83bb8ccbac8bfdec7c6500
Dale <danson@grafidog.com>
2017-10-13 21:58:44 +0000

    no comment

---
+++ plugins/JTattoo/tags/1.6.11/lookandfeel/JTattooLookAndFeelPlugin.java
@@ -0,0 +1,20 @@
+
+package lookandfeel;
+
+
+import java.awt.event.*;
+
+
+import org.gjt.sp.jedit.EBMessage;
+import org.gjt.sp.jedit.EBPlugin;
+
+
+public class JTattooLookAndFeelPlugin extends EBPlugin {
+
+    public static final String JTattoo_THEME_PROP = "JTattoo.theme";
+
+
+    public void handleMessage( EBMessage msg ) {
+    }
+}
+
