82a24138e215f8e7f5e383925439c5991e96ffba
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2019-08-18 18:58:53 +0000

    no comment

---
+++ plugins/Substance/tags/8.2.5/lookandfeel/SubstanceLookAndFeelPlugin.java
@@ -0,0 +1,56 @@
+
+package lookandfeel;
+
+
+import java.awt.event.*;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import org.gjt.sp.jedit.EBMessage;
+import org.gjt.sp.jedit.EBPlugin;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.msg.PropertiesChanged;
+import org.gjt.sp.jedit.msg.ViewUpdate;
+import org.pushingpixels.substance.api.SubstanceSlices.SubstanceWidgetType;
+import org.pushingpixels.substance.api.SubstanceCortex;
+
+
+public class SubstanceLookAndFeelPlugin extends EBPlugin {
+
+    public static final String SUBSTANCE_THEME_PROP = "substance.theme";
+    public static final String SUBSTANCE_MENU_SEARCH = "substance.menu.search";
+    private static boolean isShowingMenuSearch = jEdit.getBooleanProperty( SUBSTANCE_MENU_SEARCH, false );
+
+
+    private static void initMenuSearch( final boolean visible ) {
+        SwingUtilities.invokeLater( new Runnable(){
+
+                public void run() {
+                    for ( View view : jEdit.getViews() ) {
+                        SubstanceCortex.WindowScope.setWidgetVisible( view, visible, SubstanceWidgetType.MENU_SEARCH );
+                        view.getJMenuBar().revalidate();
+                    }
+                }
+            } );
+    }
+
+
+    public void handleMessage( EBMessage msg ) {
+        if ( msg instanceof ViewUpdate ) {
+            ViewUpdate vu = ( ViewUpdate )msg;
+            if ( ViewUpdate.CREATED == vu.getWhat() && UIManager.getLookAndFeel().getClass().getName().startsWith( "org.pushingpixels.substance.api.skin." ) ) {
+                SubstanceLookAndFeelPlugin.initMenuSearch( isShowingMenuSearch );
+            }
+        }
+        else if ( msg instanceof PropertiesChanged ) {
+            boolean visible = jEdit.getBooleanProperty( SUBSTANCE_MENU_SEARCH, false );
+            if ( visible != isShowingMenuSearch ) {
+                isShowingMenuSearch = visible;
+                initMenuSearch( isShowingMenuSearch );
+            }
+        }
+    }
+}
+
