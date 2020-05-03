d62f1a60d7143d0e7918d9ab023f08d8195009dc
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2020-05-03 19:42:11 +0000

    code clean up and documentation.

--- plugins/MarkdownSidekick/trunk/src/sidekick/markdown/Node.java
+++ plugins/MarkdownSidekick/trunk/src/sidekick/markdown/Node.java
@@ -1,15 +1,41 @@
+/*
+ * Copyright (c) 2019, Dale Anson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the <ORGANIZATION> nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package sidekick.markdown;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.text.Position;
 import sidekick.Asset;
 import sidekick.util.Location;
 import sidekick.util.SideKickElement;
 
 /**
- * Currently all nodes are header nodes, might need to change this if it is 
- * necessary to include items in the markdown file that are not header lines.
+ * Nodes for the sidekick tree, this is a pretty plain implementation of Asset.
  */
 public class Node extends Asset implements Comparable, SideKickElement {
 
@@ -36,6 +62,10 @@ public class Node extends Asset implements Comparable, SideKickElement {
         super(name);
     }
     
+    /**
+     * The 'level' is used to create the tree hierarchy, so level 3 headers fall under
+     * level 2 headers, for example.
+     */
     public void setLevel(int level) {
         this.level = level;   
     }
