55357ae8e9b9f3f40a2b716a5c8f69f4d3fdcf54
jpavel <jpavel@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2021-09-18 19:07:27 +0000

    InputReplace plugin: allow setting of minimum replacement length

--- plugins/InputReplace/trunk/src/com/illcode/jedit/inputreplace/InputReplaceOptionPane.java
+++ plugins/InputReplace/trunk/src/com/illcode/jedit/inputreplace/InputReplaceOptionPane.java
@@ -13,12 +13,17 @@ public class InputReplaceOptionPane extends AbstractOptionPane
 
     protected void _init() {
         optionPanel = new OptionPanel();
-        optionPanel.lengthSpinner.setValue(Integer.valueOf(InputReplacePlugin.maxLength));
+        optionPanel.maxLengthSpinner.setValue(Integer.valueOf(InputReplacePlugin.maxLength));
+        optionPanel.minLengthSpinner.setValue(Integer.valueOf(InputReplacePlugin.minLength));
         addComponent(optionPanel);
     }
 
     protected void _save() {
-        InputReplacePlugin.maxLength = (Integer) optionPanel.lengthSpinner.getValue();
+        InputReplacePlugin.maxLength = (Integer) optionPanel.maxLengthSpinner.getValue();
+        InputReplacePlugin.minLength = (Integer) optionPanel.minLengthSpinner.getValue();
+        if (InputReplacePlugin.minLength > InputReplacePlugin.maxLength)
+            InputReplacePlugin.minLength = InputReplacePlugin.maxLength;
         jEdit.setIntegerProperty("inputreplace.max-length", InputReplacePlugin.maxLength);
+        jEdit.setIntegerProperty("inputreplace.min-length", InputReplacePlugin.minLength);
     }
 }
