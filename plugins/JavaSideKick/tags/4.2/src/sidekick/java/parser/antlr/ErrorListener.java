cc5de53b9ac28c2f67740596776c4818b16129f6
Dale <danson@grafidog.com>
2022-06-15 19:05:19 +0000

    Tag for release.

---
+++ plugins/JavaSideKick/tags/4.2/src/sidekick/java/parser/antlr/ErrorListener.java
@@ -0,0 +1,29 @@
+
+package sidekick.java.parser.antlr;
+
+
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+
+public class ErrorListener extends BaseErrorListener {
+
+    private List<ParserException> errors = new ArrayList<ParserException>();
+
+    public List<ParserException> getErrors() {
+        return errors;
+    }
+
+    public <T extends Token> void syntaxError( Recognizer<T, ?> recognizer, T offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e ) {
+        int length = 0;
+        if ( e != null && e.getOffendingToken() != null ) {
+            int startOffset = e.getOffendingToken().getStartIndex();
+            int endOffset = e.getOffendingToken().getStopIndex();
+            length = endOffset - startOffset;
+        }
+
+        ParserException pe = new ParserException( msg, line, charPositionInLine, length );
+        errors.add( pe );
+    }
+}
+
