7ce55017e99b058fa2c8ad717a2ad6c45f0e4df1
Dale <danson@grafidog.com>
2022-06-15 19:04:35 +0000

    typo

--- plugins/JavaSideKick/tags/4.10/clean_jj/1.8/test_files/GUITest.java
+++
@@ -1,9 +0,0 @@
-class GUITest {
-    int x;
-    public void methodD() {
-        String path = "whatever";
-        int x = 12;
-        java.lang.String string = "string";
-        GUIUtils.length = "yep";
-    }
-}
--- plugins/JavaSideKick/tags/4.10/clean_jj/1.8/test_files/MethodDeclaration.java
+++
@@ -1,9 +0,0 @@
-/*
-<T> void test(T t) {
-    
-}
-*/
-
-public static void main(String[] args) {
-    
-}
