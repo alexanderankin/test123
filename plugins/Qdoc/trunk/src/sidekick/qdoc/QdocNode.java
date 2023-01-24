4939a8e24515f73d1f6c32a52914d2480c40fe90
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2023-01-24 00:07:39 +0000

    Arranged sections into a tree. Added a title node as the top level node if there is a title.

--- plugins/Qdoc/trunk/src/sidekick/qdoc/QdocNode.java
+++ plugins/Qdoc/trunk/src/sidekick/qdoc/QdocNode.java
@@ -1,27 +1,63 @@
 package sidekick.qdoc;
 
+import java.util.*;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.text.Position;
+
 import sidekick.Asset;
 import sidekick.util.Location;
 import sidekick.util.SideKickElement;
 
-public class QdocNode extends Asset implements Comparable<QdocNode>, SideKickElement {
 
+public class QdocNode extends Asset implements Comparable<QdocNode>, SideKickElement {
     private Icon icon = null;
+
     private Location startLocation = new Location();
+
     private Location endLocation = new Location();
+
     private Position startPosition = new Position() {
+
         public int getOffset() {
             return 0;
         }
     };
+
     private Position endPosition = new Position() {
+
         public int getOffset() {
             return 0;
         }
     };
+
+    /*
+     * Ordinals indicate the depth this node should be in the tree:
+     * 0 title
+     * 1 section 1
+     * 2 section 2
+     * 3 section 3
+     * 4 section 4
+     * where larger numbers are children of the next smaller number.
+     */
+    private int ordinal = 0;
+
+    private QdocNode parent = null;
+
+    private List<QdocNode> children = null;
+
+    // ordinal definitions
+    public static int TITLE = 0;
+
+    public static int SECTION1 = 1;
+
+    public static int SECTION2 = 2;
+
+    public static int SECTION3 = 3;
+
+    public static int SECTION4 = 4;
+
 
     public QdocNode() {
         super("");
@@ -60,15 +96,46 @@ public class QdocNode extends Asset implements Comparable<QdocNode>, SideKickEle
         return endPosition;
     }
 
-    public void setEndPosition( Position p ) {
+    public void setEndPosition(Position p) {
         endPosition = p;
         end = p;
     }
-    
-    public void setIcon(ImageIcon icon) {
-        this.icon = icon;   
+
+    public void setOrdinal(int i) {
+        ordinal = i;
     }
-    
+
+    public int getOrdinal() {
+        return ordinal;
+    }
+
+    public void setParent(QdocNode parent) {
+        this.parent = parent;
+    }
+
+    public QdocNode getParent() {
+        return parent;
+    }
+
+    public void addChild(QdocNode node) {
+        if (children == null) {
+            children = new ArrayList<QdocNode>();
+        }
+        children.add(node);
+    }
+
+    public List<QdocNode> getChildren() {
+        return children;
+    }
+
+    public boolean hasChildren() {
+        return (children != null && !children.isEmpty());
+    }
+
+    public void setIcon(ImageIcon icon) {
+        this.icon = icon;
+    }
+
     public Icon getIcon() {
         return icon;
     }
@@ -76,19 +143,51 @@ public class QdocNode extends Asset implements Comparable<QdocNode>, SideKickEle
     public String toString() {
         return name;
     }
-    
+
     public String getShortString() {
-        return name;   
+        return name;
     }
-    
+
     public String getLongString() {
-        return name + ": " + getStartLocation() + ":" + getEndLocation();   
+        return name + ": " + getStartLocation() + ":" + getEndLocation();
     }
 
     public int compareTo(QdocNode o) {
         if (o == null) {
-            return 1;   
+            return 1;
         }
         return toString().compareToIgnoreCase(o.toString());
+    }
+
+    public String dump() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < ordinal; i++)  {
+            sb.append('\t');
+        }
+        sb.append(getOrdinal()).append(name).append('\n');
+
+        if (hasChildren()) {
+            for ( QdocNode child : children) {
+                sb.append(dump(child));
+            }
+        }
+        return sb.toString();
+    }
+
+    private String dump(QdocNode node) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < node.getOrdinal(); i++)  {
+            sb.append('\t');
+        }
+        sb.append(node.getOrdinal()).append(node.toString()).append('\n');
+
+        if (node.hasChildren()) {
+            for (QdocNode child : node.getChildren()) {
+                sb.append(dump(child));
+            }
+        }
+        return sb.toString();
     }
 }
--- plugins/Qdoc/trunk/src/sidekick/qdoc/QdocSideKickParser.java
+++ plugins/Qdoc/trunk/src/sidekick/qdoc/QdocSideKickParser.java
@@ -41,6 +41,7 @@ import org.gjt.sp.jedit.View;
 
 import sidekick.SideKickParsedData;
 import sidekick.SideKickParser;
+import static sidekick.qdoc.QdocNode.*;
 import sidekick.util.*;
 
 
@@ -68,6 +69,14 @@ public class QdocSideKickParser extends SideKickParser {
 
     /**
      * Parse the contents of the given buffer.
+     *
+     * qdoc files are essentially flat, with no enforced tree structure. It is assumed
+     * that "\\title" will be the top level, and below title will be section1's with
+     * section2's below that and so on. There is nothing in the specification that
+     * requires this, so it is a huge assumption to think this will always be the case,
+     * but that is what I'm assuming here. The sidekick tree could look really funky
+     * if the qdoc author doesn't follow this hierarchy.
+     *
      * @param buffer the buffer to parse
      * @param errorSource where to send any error messages
      * @return data for the tree
@@ -75,7 +84,6 @@ public class QdocSideKickParser extends SideKickParser {
     public SideKickParsedData parse(Buffer buffer, DefaultErrorSource errorSource) {
         String filename = buffer.getPath();
         SideKickParsedData parsedData = new QdocSideKickParsedData(filename);
-        DefaultMutableTreeNode root = parsedData.root;
         ImageIcon section1Icon = EclipseIconsPlugin.getIcon("action1.gif");
         ImageIcon section2Icon = EclipseIconsPlugin.getIcon("action2.gif");
         ImageIcon section3Icon = EclipseIconsPlugin.getIcon("action3.gif");
@@ -89,14 +97,24 @@ public class QdocSideKickParser extends SideKickParser {
             List<QdocNode> nodes = new ArrayList<QdocNode>();
             Location startLocation;
             SideKickPosition startPosition;
+            int length;
+            // there should only be one title node, but of course, the qdoc spec
+            // doesn't enforce that, and doesn't require that there is a title node.
+            QdocNode titleNode = null;
 
             for (int lineNumber = 0; lineNumber < lineCount; lineNumber++) {
                 String lineText = buffer.getLineText(lineNumber);
                 int index = lineText.indexOf("\\section");
+                length = "\\sectionx".length();
+
+                if (index == -1) {
+                    index = lineText.indexOf("\\title");
+                    length = "\\title".length();
+                }
 
                 if (index > -1) {
-                    String section = lineText.substring(index, "\\sectionX".length());
-                    String title = lineText.substring(index + "\\sectionX".length(), lineText.length());
+                    String section = lineText.substring(index, length);
+                    String title = lineText.substring(index + length, lineText.length());
                     startLocation = new Location(lineNumber, index);
                     startPosition = new SideKickPosition(buffer.getLineStartOffset(lineNumber) + index);
                     QdocNode node = new QdocNode(title);
@@ -105,29 +123,106 @@ public class QdocSideKickParser extends SideKickParser {
                     node.setEndLocation(new Location(lineNumber, buffer.getLineEndOffset(lineNumber)));
                     node.setEndPosition(new SideKickPosition(buffer.getLineEndOffset(lineNumber)));
                     switch (section) {
+                        case "\\title":
+                            node.setIcon(null);
+                            node.setOrdinal(TITLE);
+                            titleNode = node;
+                            break;
                         case "\\section1":
                             node.setIcon(section1Icon);
+                            node.setOrdinal(SECTION1);
                             break;
                         case "\\section2":
                             node.setIcon(section2Icon);
+                            node.setOrdinal(SECTION2);
                             break;
                         case "\\section3":
                             node.setIcon(section3Icon);
+                            node.setOrdinal(SECTION3);
                             break;
                         case "\\section4":
                             node.setIcon(section4Icon);
+                            node.setOrdinal(SECTION4);
                             break;
                     }
                     nodes.add(node);
                 }
             }
 
+            // arrange the parent/child relationship of the nodes
             if (nodes.size() > 0) {
-                DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode("Qdoc");
-                root.add(treeNode);
+                Deque<QdocNode> deck = new ArrayDeque<QdocNode>();
+                QdocNode previous1 = null;
+                QdocNode previous2 = null;
+                QdocNode previous3 = null;
 
                 for ( QdocNode node : nodes) {
-                    treeNode.add(new DefaultMutableTreeNode(node));
+                    if (node.getOrdinal() == TITLE) {
+                        // there should only be one title and it's already captured
+                        continue;
+                    }
+
+                    if (node.getOrdinal() == SECTION1) {
+                        previous1 = node;
+                        deck.add(node);
+                        continue;
+                    }
+
+                    if (node.getOrdinal() == SECTION2) {
+                        if (previous1 != null) {
+                            previous1.addChild(node);
+                        }
+                        else {
+                            deck.add(node);
+                        }
+                        previous2 = node;
+                        continue;
+                    }
+
+                    if (node.getOrdinal() == SECTION3) {
+                        if (previous2 != null) {
+                            previous2.addChild(node);
+                        }
+                        else {
+                            deck.add(node);
+                        }
+                        previous3 = node;
+                        continue;
+                    }
+
+                    if (node.getOrdinal() == SECTION4) {
+                        if (previous3 != null) {
+                            previous3.addChild(node);
+                        }
+                        else {
+                            deck.add(node);
+                        }
+                        continue;
+                    }
+                }
+
+                if (titleNode == null) {
+                    String title = "Untitled";
+                    startLocation = new Location(0, 0);
+                    startPosition = new SideKickPosition(buffer.getLineStartOffset(0));
+                    titleNode = new QdocNode(title);
+                    titleNode.setStartLocation(startLocation);
+                    titleNode.setStartPosition(startPosition);
+                    titleNode.setEndLocation(new Location(0,0));
+                    titleNode.setEndPosition(new SideKickPosition(0));
+                }
+                
+                // make a tree for sidekick out of the nodes
+                DefaultMutableTreeNode root = parsedData.root;
+                DefaultMutableTreeNode titleTreeNode = new DefaultMutableTreeNode(titleNode);
+                root.add(titleTreeNode);
+                
+                for (QdocNode node : deck) {
+                    DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode(node);
+                    titleTreeNode.add(treeNode);
+                    if (node.hasChildren()) {
+                        addChildNodes(treeNode);   
+                    }
                 }
             }
         }
@@ -135,5 +230,16 @@ public class QdocSideKickParser extends SideKickParser {
             e.printStackTrace();
         }
         return parsedData;
+    }
+    
+    private void addChildNodes(DefaultMutableTreeNode parent) {
+        QdocNode node = (QdocNode)parent.getUserObject();
+        for (QdocNode child : node.getChildren()) {
+            DefaultMutableTreeNode childTreeNode = new DefaultMutableTreeNode(child);
+            parent.add(childTreeNode);
+            if (child.hasChildren()) {
+                addChildNodes(childTreeNode);
+            }
+        }
     }
 }
