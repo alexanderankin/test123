5c1e05eb4d36d33ae75dd3b44b66663f8d7a2051
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2023-08-21 00:44:19 +0000

    Tag for release, replaced previous tag to update the build files to make it easier to release the plugin.

---
+++ plugins/Beauty/tags/beauty-1.1/test/test_files/java/expected/BlockTest.java
@@ -0,0 +1,26 @@
+public class BlockTest {
+    int x;
+
+    private void method1() {
+        int y;
+    }
+
+    // a method
+    public int getC() {
+        x++;
+        switch (C) {
+            case 1:
+                return ++C;
+            case 2:
+                // do something
+                y = C + z;
+                break;
+            case 3:
+
+            default:
+                return c;
+        }
+        return C;
+    }
+
+}
