a8da0f39e18eb8786f63674c211e89cc69b1aee3
daleanson <daleanson@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2023-08-31 02:59:16 +0000

    Initial commit. Moving this plugin from the XML plugin so it's easier to maintain.

---
+++ plugins/JsonSideKick/trunk/src/sidekick/json/JsonSideKickPlugin.java
@@ -0,0 +1,13 @@
+package sidekick.json;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class JsonSideKickPlugin extends EditPlugin {
+    public static final String NAME = "sidekick.json";
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+}
